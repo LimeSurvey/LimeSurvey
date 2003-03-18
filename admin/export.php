0129c1d50bba278dceded4c6365f14a6b4f9767e
Jason Cleeland <jcleeland@users.sourceforge.net>
2003-03-18 11:46:52 +0000

    Fixed bug that caused double results when exporting short answers. Bug was caused by use of mysql_fetch_array - added ', MYSQL_ASSOC' to only receive the associative names, not the numerical. Had to rearrange script slightly because long answers still require numerical array.
    git-svn-id: file:///Users/Shitiz/Downloads/lssvn/trunk/unstable@64 b72ed6b6-b9f8-46b5-92b4-906544132732

--- admin/export.php
+++ admin/export.php
@@ -91,183 +91,188 @@ if (!$style)
 	echo "\t</tr>\n";
 	echo "</table>\n";
 	echo "</body>\n</html>";
+	exit;
 	}
-else
+
+if ($type == "doc") {header("Content-Disposition: attachment; filename=survey.doc");}
+elseif ($type == "xls") {header("Content-Disposition: attachment; filename=survey.xls");}
+elseif ($type == "csv") {header("Content-Disposition: attachment; filename=survey.csv");}
+else {header("Content-Disposition: attachment; filename=survey.doc");}
+header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");    // Date in the past
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT"); 
+                                                     // always modified
+header("Cache-Control: no-store, no-cache, must-revalidate");  // HTTP/1.1
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");                          // HTTP/1.0
+
+include ("config.php");
+//echo "$htmlheader";
+//STEP 1: First line is column headings
+$cquery = "SELECT * FROM questions, groups WHERE questions.gid=groups.gid AND questions.sid=$sid ORDER BY group_name, title";
+$cresult = mysql_query($cquery);
+$s = "\t";
+$firstline = "id$s ";
+while ($crow = mysql_fetch_array($cresult))
 	{
-	if ($type == "doc") {header("Content-Disposition: attachment; filename=survey.doc");}
-	elseif ($type == "xls") {header("Content-Disposition: attachment; filename=survey.xls");}
-	elseif ($type == "csv") {header("Content-Disposition: attachment; filename=survey.csv");}
-	else {header("Content-Disposition: attachment; filename=survey.doc");}
-	header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");    // Date in the past
-	header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT"); 
-	                                                     // always modified
-	header("Cache-Control: no-store, no-cache, must-revalidate");  // HTTP/1.1
-	header("Cache-Control: post-check=0, pre-check=0", false);
-	header("Pragma: no-cache");                          // HTTP/1.0
-	
-	include ("config.php");
-	//echo "$htmlheader";
-	//STEP 1: First line is column headings
-	$cquery = "SELECT * FROM questions, groups WHERE questions.gid=groups.gid AND questions.sid=$sid ORDER BY group_name, title";
-	$cresult = mysql_query($cquery);
-	$s = "\t";
-	$firstline = "id$s ";
-	while ($crow = mysql_fetch_array($cresult))
+	if ($style == "abrev")
 		{
-		if ($style == "abrev")
+		if ($crow['type'] != "M" && $crow['type'] != "A" && $crow['type'] != "B" && $crow['type'] != "C" && $crow['type'] != "P" && $crow['type'] != "O")
 			{
-			if ($crow['type'] != "M" && $crow['type'] != "A" && $crow['type'] != "B" && $crow['type'] != "C" && $crow['type'] != "P" && $crow['type'] != "O")
-				{
-				$firstline .= "Grp{$crow['sid']}Qst{$crow['title']}$s ";
-				}
-			elseif ($crow['type'] == "O")
-				{
-				$firstline .= "Grp{$crow['sid']}Qst{$crow['title']}$s ";
-				$firstline .= "Grp{$crow['sid']}Qst{$crow['title']}[comment]$s ";
-				}
-			else
-				{
-				$i2query="SELECT answers.*, questions.other FROM answers, questions WHERE answers.qid=questions.qid AND questions.qid={$crow['qid']} AND questions.sid=$sid ORDER BY code";
-				$i2result=mysql_query($i2query);
-				while ($i2row=mysql_fetch_array($i2result))
-					{
-					$firstline .= "Grp{$crow['sid']}Qst{$crow['title']}Opt$i2row[1]$s ";
-					if ($i2row[4] == "Y") {$otherexists="Y";}
-					if ($crow['type'] == "P") {$firstline .= "Grp{$crow['sid']}Qst{$crow['title']}Opt$i2row[1]comment$s ";}
-					}
-				if ($otherexists == "Y") 
-					{
-					$firstline .= "Grp{$crow['sid']}Qst{$crow['title']}OptOther$s ";
-					}			
-				}
+			$firstline .= "Grp{$crow['sid']}Qst{$crow['title']}$s ";
 			}
-		elseif ($style == "full")
+		elseif ($crow['type'] == "O")
 			{
-			if ($crow['type'] != "M" && $crow['type'] != "A" && $crow['type'] != "B" && $crow['type'] != "C" && $crow['type'] != "P" && $crow['type'] != "O")
+			$firstline .= "Grp{$crow['sid']}Qst{$crow['title']}$s ";
+			$firstline .= "Grp{$crow['sid']}Qst{$crow['title']}[comment]$s ";
+			}
+		else
+			{
+			$i2query="SELECT answers.*, questions.other FROM answers, questions WHERE answers.qid=questions.qid AND questions.qid={$crow['qid']} AND questions.sid=$sid ORDER BY code";
+			$i2result=mysql_query($i2query);
+			while ($i2row=mysql_fetch_array($i2result))
 				{
-				$firstline .= str_replace("<BR>", " ", str_replace("\r\n", " ", $crow['question']))."$s ";
+				$firstline .= "Grp{$crow['sid']}Qst{$crow['title']}Opt$i2row[1]$s ";
+				if ($i2row[4] == "Y") {$otherexists="Y";}
+				if ($crow['type'] == "P") {$firstline .= "Grp{$crow['sid']}Qst{$crow['title']}Opt$i2row[1]comment$s ";}
 				}
-			elseif ($crow['type'] == "O")
+			if ($otherexists == "Y") 
 				{
-				$firstline .= str_replace("<BR>", " ", str_replace("\r\n", " ", $crow['question']))."$s ";
-				$firstline .= str_replace("<BR>", " ", str_replace("\r\n", " ", $crow['question']))."[Comment]$s ";
-				}
-			else
-				{
-				$i2query = "SELECT answers.*, questions.other FROM answers, questions WHERE answers.qid=questions.qid AND questions.qid={$crow['qid']} AND questions.sid=$sid ORDER BY code";
-				$i2result = mysql_query($i2query);
-				while ($i2row = mysql_fetch_array($i2result))
-					{
-					$firstline .= str_replace("<BR>", " ", str_replace("\r\n", " ", $crow['question'])).": {$i2row['answer']}$s ";
-					if ($i2row['other'] == "Y") {$otherexists = "Y";}
-					if ($crow['type'] == "P") {$firstline .= str_replace("<BR>", " ", str_replace("\r\n", " ", $crow['question'])).": {$i2row['answer']}(comment)$s ";}
-					}
-				if ($otherexists == "Y") 
-					{
-					$firstline .= str_replace("<BR>", " ", str_replace("\r\n", " ", $crow['question'])).": Other$s ";
-					}			
-				}
+				$firstline .= "Grp{$crow['sid']}Qst{$crow['title']}OptOther$s ";
+				}			
 			}
 		}
-	$firstline = substr($firstline, 0, strlen($firstline)-2);
-	$firstline .= "\n";
-	echo $firstline;
-	
-	//Now dump the data
-	$lq = "SELECT DISTINCT qid FROM questions WHERE sid=$sid";
-	$lr = mysql_query($lq);
-	$legitqs[] = "DUMMY ENTRY";
-	while ($lw = mysql_fetch_array($lr))
+	elseif ($style == "full")
 		{
-		$legitqs[] = $lw[0]; //this creates an array of question id's'
+		if ($crow['type'] != "M" && $crow['type'] != "A" && $crow['type'] != "B" && $crow['type'] != "C" && $crow['type'] != "P" && $crow['type'] != "O")
+			{
+			$firstline .= str_replace("<BR>", " ", str_replace("\r\n", " ", $crow['question']))."$s ";
+			}
+		elseif ($crow['type'] == "O")
+			{
+			$firstline .= str_replace("<BR>", " ", str_replace("\r\n", " ", $crow['question']))."$s ";
+			$firstline .= str_replace("<BR>", " ", str_replace("\r\n", " ", $crow['question']))."[Comment]$s ";
+			}
+		else
+			{
+			$i2query = "SELECT answers.*, questions.other FROM answers, questions WHERE answers.qid=questions.qid AND questions.qid={$crow['qid']} AND questions.sid=$sid ORDER BY code";
+			$i2result = mysql_query($i2query);
+			while ($i2row = mysql_fetch_array($i2result))
+				{
+				$firstline .= str_replace("<BR>", " ", str_replace("\r\n", " ", $crow['question'])).": {$i2row['answer']}$s ";
+				if ($i2row['other'] == "Y") {$otherexists = "Y";}
+				if ($crow['type'] == "P") {$firstline .= str_replace("<BR>", " ", str_replace("\r\n", " ", $crow['question'])).": {$i2row['answer']}(comment)$s ";}
+				}
+			if ($otherexists == "Y") 
+				{
+				$firstline .= str_replace("<BR>", " ", str_replace("\r\n", " ", $crow['question'])).": Other$s ";
+				}			
+			}
 		}
-	
-	$surveytable = "survey_{$sid}";
-	
-	if ($_POST['sql']) //this applies if export has been called from the statistics package
+	}
+$firstline = substr($firstline, 0, strlen($firstline)-2);
+$firstline .= "\n";
+echo $firstline;
+
+//Now dump the data
+$lq = "SELECT DISTINCT qid FROM questions WHERE sid=$sid";
+$lr = mysql_query($lq);
+$legitqs[] = "DUMMY ENTRY";
+while ($lw = mysql_fetch_array($lr))
+	{
+	$legitqs[] = $lw['qid']; //this creates an array of question id's'
+	}
+
+$surveytable = "survey_{$sid}";
+
+if ($_POST['sql']) //this applies if export has been called from the statistics package
+	{
+	$dquery = "SELECT * FROM $surveytable WHERE ".stripcslashes($_POST['sql'])." ORDER BY id";
+	}
+else // this applies for exporting everything
+	{
+	$dquery = "SELECT * FROM $surveytable ORDER BY id";
+	}
+
+if ($answers == "short")
+	{
+	$dresult = mysql_query($dquery);
+	while ($drow = mysql_fetch_array($dresult, MYSQL_ASSOC))
 		{
-		$dquery = "SELECT * FROM $surveytable WHERE ".stripcslashes($_POST['sql'])." ORDER BY id";
+		echo implode($s, str_replace("\r\n", " ", $drow)) . "\n"; //create dump from each row
 		}
-	else // this applies for exporting everything
-		{
-		$dquery = "SELECT * FROM $surveytable ORDER BY id";
-		}
+	}
+
+elseif ($answers == "long")
+	{
 	$dresult = mysql_query($dquery);
 	$fieldcount = mysql_num_fields($dresult);
 	while ($drow = mysql_fetch_array($dresult))
 		{
-		if ($answers == "short") 
+		$i = 0;
+		for ($i; $i<$fieldcount; $i++)
 			{
-			echo implode($s, str_replace("\r\n", " ", $drow)) . "\n"; //create dump from each row
-			}
-
-		elseif ($answers == "long")
-			{
-			$i = 0;
-			for ($i; $i<$fieldcount; $i++)
+			list($fsid, $fgid, $fqid) = split("X", mysql_field_name($dresult, $i));
+			if (!$fqid) {$fqid = 0;}
+			while (!in_array($fqid, $legitqs)) //checks that the qid exists in our list
 				{
-				list($fsid, $fgid, $fqid) = split("X", mysql_field_name($dresult, $i));
-				if (!$fqid) {$fqid = 0;}
-				while (!in_array($fqid, $legitqs)) //checks that the qid exists in our list
-					{
-					$fqid = substr($fqid, 0, strlen($fqid)-1);
-					}
-				$qq = "SELECT type FROM questions WHERE qid=$fqid";
-				$qr = mysql_query($qq);
-				while ($qrow = mysql_fetch_array($qr))
-					{$ftype = $qrow['type'];}
-				switch ($ftype)
-					{
-					case "L": //DROPDOWN LIST
-						$lq = "SELECT * FROM answers WHERE qid=$fqid";
-						$lr = mysql_query($lq);
-						while ($lrow = mysql_fetch_array($lr))
-							{
-							if ($lrow['code'] == $drow[$i]) {echo $lrow['answer'];} 
-							}
-						break;
-					case "O": //DROPDOWN LIST WITH COMMENT
-						$lq = "SELECT * FROM answers WHERE qid=$fqid";
-						$lr = mysql_query($lq);
-						while ($lrow = mysql_fetch_array($lr))
-							{
-							if ($lrow['code'] == $drow[$i]) {echo $lrow['answer']; $found = "Y";}
-							}
-						if ($found != "Y") {echo str_replace("\r\n", " ", $drow[$i]);}
-						$found = "";
-						break;
-					case "Y": //YES\NO
-						switch($drow[$i])
-							{
-							case "Y": echo "Yes"; break;
-							case "N": echo "No"; break;
-							default: echo "N/A"; break;
-							}
-						break;
-					case "G": //GENDER
-						switch($drow[$i])
-							{
-							case "M": echo "Male"; break;
-							case "F": echo "Female"; break;
-							default: echo "N/A"; break;
-							}
-						break;
-					case "M": //multioption
-						switch($drow[$i])
-							{
-							case "Y": echo "Yes"; break;
-							case "": echo "No"; break;
-							default: echo "N/A"; break;
-							}
-						break;
-					default:
-						echo str_replace("\r\n", " ", $drow[$i]);
-					}
-				echo "$s";
-				$ftype = "";
+				$fqid = substr($fqid, 0, strlen($fqid)-1);
 				}
-			echo "\n";
+			$qq = "SELECT type FROM questions WHERE qid=$fqid";
+			$qr = mysql_query($qq);
+			while ($qrow = mysql_fetch_array($qr, MYSQL_ASSOC))
+				{$ftype = $qrow['type'];}
+			switch ($ftype)
+				{
+				case "L": //DROPDOWN LIST
+					$lq = "SELECT * FROM answers WHERE qid=$fqid AND code ='$drow[$i]'";
+					$lr = mysql_query($lq);
+					while ($lrow = mysql_fetch_array($lr, MYSQL_ASSOC))
+						{
+						//if ($lrow['code'] == $drow[$i]) {echo $lrow['answer'];} 
+						echo $lrow['answer'];
+						}
+					break;
+				case "O": //DROPDOWN LIST WITH COMMENT
+					$lq = "SELECT * FROM answers WHERE qid=$fqid";
+					$lr = mysql_query($lq) or die ("Could do it<br />$lq<br />".mysql_error());
+					while ($lrow = mysql_fetch_array($lr, MYSQL_ASSOC))
+						{
+						if ($lrow['code'] == $drow[$i]) {echo $lrow['answer']; $found = "Y";}
+						}
+					if ($found != "Y") {echo str_replace("\r\n", " ", $drow[$i]);}
+					$found = "";
+					break;
+				case "Y": //YES\NO
+					switch($drow[$i])
+						{
+						case "Y": echo "Yes"; break;
+						case "N": echo "No"; break;
+						default: echo "N/A"; break;
+						}
+					break;
+				case "G": //GENDER
+					switch($drow[$i])
+						{
+						case "M": echo "Male"; break;
+						case "F": echo "Female"; break;
+						default: echo "N/A"; break;
+						}
+					break;
+				case "M": //multioption
+					switch($drow[$i])
+						{
+						case "Y": echo "Yes"; break;
+						case "": echo "No"; break;
+						default: echo "N/A"; break;
+						}
+					break;
+				default:
+					echo str_replace("\r\n", " ", $drow[$i]);
+				}
+			echo "$s";
+			$ftype = "";
 			}
+		echo "\n";
 		}
 	}
 ?>
