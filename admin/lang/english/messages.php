78e72ec2162b0f7109af8f895dc494d18049bf09
Jason Cleeland <jcleeland@users.sourceforge.net>
2003-06-29 13:14:02 +0000

    English language files
    git-svn-id: file:///Users/Shitiz/Downloads/lssvn/trunk/unstable@419 b72ed6b6-b9f8-46b5-92b4-906544132732

---
+++ admin/lang/english/messages.php
@@ -0,0 +1,32 @@
+<?php
+
+
+
+
+//BUTTON MOUSEOVERS
+//administration bar
+define(_A_HOME_BT, "Click here to show the default admin page. No surveys will be shown");
+define(_A_SECURITY_BT, "Click here to modify security settings");
+define(_A_BADSECURITY_BT, "You have set security on in your config.php, but have not yet activated security settings. Click here to activate your security.");
+define(_A_DELSURVEY_BT, "Click here to delete this entire survey.");
+define(_A_ADDSURVEY_BT, "Click here to create or import a new survey.");
+//Survey bar
+define(_S_ACTIVE_BT, "This survey is currently active.");
+define(_S_INACTIVE_BT, "This survey is not currently active.");
+define(_S_ACTIVATE_BT, "Click here to activate this survey.");
+define(_S_DEACTIVATE_BT, "Click here to de-activate this survey.");
+define(_S_DOSURVEY_BT, "Click here to do the public survey.");
+define(_S_DATAENTRY_BT, "Click here for the dataentry screen for this survey.");
+define(_S_PRINTABLE_BT, "Click here for a printable version of this survey.");
+define(_S_EDIT_BT, "Click here to edit the details of this current survey.");
+define(_S_DELETE_BT, "Click here to delete this current survey.");
+define(_S_EXPORT_BT, "Click here to export this survey.");
+define(_S_BROWSE_BT, "Click here to browse the responses collected for this survey.");
+define(_S_TOKENS_BT, "Click here to activate/edit the tokens for this survey.");
+define(_S_ADDGROUP_BT, "Click here to add a new group to this survey.");
+define(_S_MINIMISE_BT, "Click here to hide the details of this survey.");
+define(_S_MAXIMISE_BT, "Click here to show the details of this survey.");
+//Group bar
+
+
+?>
