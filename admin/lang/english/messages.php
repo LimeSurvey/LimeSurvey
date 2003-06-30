c007db0a25b3959aa9c99ef46172cf880540d695
Jason Cleeland <jcleeland@users.sourceforge.net>
2003-06-30 13:28:26 +0000

    Updated button messages
    git-svn-id: file:///Users/Shitiz/Downloads/lssvn/trunk/unstable@431 b72ed6b6-b9f8-46b5-92b4-906544132732

--- admin/lang/english/messages.php
+++ admin/lang/english/messages.php
@@ -5,28 +5,47 @@
 
 //BUTTON MOUSEOVERS
 //administration bar
-define(_A_HOME_BT, "Click here to show the default admin page. No surveys will be shown");
-define(_A_SECURITY_BT, "Click here to modify security settings");
-define(_A_BADSECURITY_BT, "You have set security on in your config.php, but have not yet activated security settings. Click here to activate your security.");
-define(_A_DELSURVEY_BT, "Click here to delete this entire survey.");
-define(_A_ADDSURVEY_BT, "Click here to create or import a new survey.");
+define(_A_HOME_BT, "Default Administration Page");
+define(_A_SECURITY_BT, "Modify Security Settings");
+define(_A_BADSECURITY_BT, "Activate Security");
+define(_A_CHECKDB_BT, "Check Database");
+define(_A_DELETE_BT, "Delete Entire Survey");
+define(_A_ADDSURVEY_BT, "Create or Import New Survey");
+define(_A_HELP_BT, "Show Help");
 //Survey bar
-define(_S_ACTIVE_BT, "This survey is currently active.");
-define(_S_INACTIVE_BT, "This survey is not currently active.");
-define(_S_ACTIVATE_BT, "Click here to activate this survey.");
-define(_S_DEACTIVATE_BT, "Click here to de-activate this survey.");
-define(_S_DOSURVEY_BT, "Click here to do the public survey.");
-define(_S_DATAENTRY_BT, "Click here for the dataentry screen for this survey.");
-define(_S_PRINTABLE_BT, "Click here for a printable version of this survey.");
-define(_S_EDIT_BT, "Click here to edit the details of this current survey.");
-define(_S_DELETE_BT, "Click here to delete this current survey.");
-define(_S_EXPORT_BT, "Click here to export this survey.");
-define(_S_BROWSE_BT, "Click here to browse the responses collected for this survey.");
-define(_S_TOKENS_BT, "Click here to activate/edit the tokens for this survey.");
-define(_S_ADDGROUP_BT, "Click here to add a new group to this survey.");
-define(_S_MINIMISE_BT, "Click here to hide the details of this survey.");
-define(_S_MAXIMISE_BT, "Click here to show the details of this survey.");
+define(_S_ACTIVE_BT, "This survey is currently active");
+define(_S_INACTIVE_BT, "This survey is not currently active");
+define(_S_ACTIVATE_BT, "Activate This Survey");
+define(_S_DEACTIVATE_BT, "De-activate This Survey");
+define(_S_DOSURVEY_BT, "Do Survey");
+define(_S_DATAENTRY_BT, "Dataentry Screen For Survey");
+define(_S_PRINTABLE_BT, "Printable Version of Survey");
+define(_S_EDIT_BT, "Edit Current Survey");
+define(_S_DELETE_BT, "Delete Current Survey");
+define(_S_EXPORT_BT, "Export this Survey");
+define(_S_BROWSE_BT, "Browse Responses for this Survey");
+define(_S_TOKENS_BT, "Activate/Edit Tokens for this Survey");
+define(_S_ADDGROUP_BT, "Add New Group to Survey");
+define(_S_MINIMISE_BT, "Hide Details of this Survey");
+define(_S_MAXIMISE_BT, "Show Details of this Survey");
+define(_S_CLOSE_BT, "Close this Survey");
 //Group bar
+define(_G_EDIT_BT, "Edit Current Group");
+define(_G_DELETE_BT, "Delete Current Group");
+define(_G_ADDQUESTION_BT, "Add New Question to Group");
+define(_G_MINIMISE_BT, "Hide Details of this Group");
+define(_G_MAXIMISE_BT, "Show Details of this Group");
+define(_G_CLOSE_BT, "Close this Group");
+//Question bar
+define(_Q_EDIT_BT, "Edit Current Question");
+define(_Q_DELETE_BT, "Delete Current Question");
+define(_Q_EXPORT_BT, "Export this Question");
+define(_Q_CONDITIONS_BT, "Set Conditions for this Question");
+define(_Q_ANSWERS_BT, "Edit/Add Answers for this Question");
+define(_Q_LABELS_BT, "Edit/Add Labels to this Question");
+define(_Q_MINIMISE_BT, "Hide Details of this Question");
+define(_Q_MAXIMISE_BT, "Show Details of this Question");
+define(_Q_CLOSE_BT, "Close this Question");
 
 
 ?>
