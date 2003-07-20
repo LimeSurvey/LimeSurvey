5685e958aa762372ecdfe40dd8083742417f617e
Jason Cleeland <jcleeland@users.sourceforge.net>
2003-07-20 14:30:09 +0000

    Uses sendcacheheader() function now
    git-svn-id: file:///Users/Shitiz/Downloads/lssvn/trunk/unstable@548 b72ed6b6-b9f8-46b5-92b4-906544132732

--- admin/tokens.php
+++ admin/tokens.php
@@ -45,12 +45,7 @@ if (!isset($order)) {$order=returnglobal('order');}
 if (!isset($limit)) {$limit=returnglobal('limit');}
 if (!isset($start)) {$start=returnglobal('start');}
 
-header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");    // Date in the past
-header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT"); 
-                                                     // always modified
-header("Cache-Control: no-store, no-cache, must-revalidate");  // HTTP/1.1
-header("Cache-Control: post-check=0, pre-check=0", false);
-header("Pragma: no-cache");                          // HTTP/1.0
+sendcacheheaders();
 
 if ($action == "delete") {echo str_replace("<head>\n", "<head>\n<meta http-equiv=\"refresh\" content=\"2;URL=$PHP_SELF?action=browse&sid={$_GET['sid']}&start=$start&limit=$limit&order=$order\"", $htmlheader);}
 else {echo $htmlheader;}
