3e1a4b23637c65c36fa0cbcf15476c7bb5411bd9
Jason Cleeland <jcleeland@users.sourceforge.net>
2003-11-02 23:58:47 +0000

    Fixed bug #834447 - used square brackets for array ref.
    git-svn-id: file:///Users/Shitiz/Downloads/lssvn/trunk/unstable@761 b72ed6b6-b9f8-46b5-92b4-906544132732

--- index.php
+++ index.php
@@ -37,8 +37,8 @@
 
 session_start();
 
-if (!isset($sid)) {$sid=$_GET('sid');}
-if (!isset($sid)) {$sid=$_POST('sid');}
+if (!isset($sid)) {$sid=$_GET['sid'];}
+if (!isset($sid)) {$sid=$_POST['sid'];}
 if (phpversion() >= '4.2.0') {settype($sid, "int");} else {settype($sid, "integer");}
 
 include("./admin/config.php");
