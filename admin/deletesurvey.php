61390e27ec1839c8b64c66f113867236eb094b42
Jason Cleeland <jcleeland@users.sourceforge.net>
2004-01-28 13:22:33 +0000

    Changed to use common.php's "sendcacheheaders" function
    git-svn-id: file:///Users/Shitiz/Downloads/lssvn/trunk/unstable@849 b72ed6b6-b9f8-46b5-92b4-906544132732

--- admin/deletesurvey.php
+++ admin/deletesurvey.php
@@ -36,13 +36,9 @@
 if (isset($_GET['sid'])) {$sid = $_GET['sid'];}
 if (isset($_GET['ok'])) {$ok = $_GET['ok'];}
 
-include("config.php");
-header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");    // Date in the past
-header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT"); 
-                                                     // always modified
-header("Cache-Control: no-store, no-cache, must-revalidate");  // HTTP/1.1
-header("Cache-Control: post-check=0, pre-check=0", false);
-header("Pragma: no-cache");                          // HTTP/1.0
+require_once("config.php");
+
+sendcacheheaders();                      // HTTP/1.0
 
 echo $htmlheader;
 
