267d6b8c425a42626d275c3fed72ea564d411578
Jason Cleeland <jcleeland@users.sourceforge.net>
2004-03-21 01:14:05 +0000

    Slovenian Language File Created by Gasper Koren [gasper@fdvinfo.net]: Web Survey Methodology - http://www.websm.org/
    git-svn-id: file:///Users/Shitiz/Downloads/lssvn/trunk/unstable@956 b72ed6b6-b9f8-46b5-92b4-906544132732

---
+++ lang/slovenian.lang.php
@@ -0,0 +1,136 @@
+<?php
+/*
+	#############################################################
+	# >>> PHP Surveyor					    					#
+	#############################################################
+	# > Author:  Jason Cleeland				    				#
+	# > E-mail:  jason@cleeland.org				    			#
+	# > Mail:    Box 99, Trades Hall, 54 Victoria St,	    	#
+	# >          CARLTON SOUTH 3053, AUSTRALIA		    		#
+	# > Date: 	 20 February 2003			    				#
+	#							    							#
+	# This set of scripts allows you to develop, publish and    #
+	# perform data-entry on surveys.			    			#
+	#############################################################
+	#							    							#
+	#	Copyright (C) 2003  Jason Cleeland		    			#
+	#							    							#
+	# This program is free software; you can redistribute 	    #
+	# it and/or modify it under the terms of the GNU General    #
+	# Public License as published by the Free Software 	    	#
+	# Foundation; either version 2 of the License, or (at your  #
+	# option) any later version.				    			#
+	#							    							#
+	# This program is distributed in the hope that it will be   #
+	# useful, but WITHOUT ANY WARRANTY; without even the 	    #
+	# implied warranty of MERCHANTABILITY or FITNESS FOR A 	    #
+	# PARTICULAR PURPOSE.  See the GNU General Public License   #
+	# for more details.					    					#
+	#							    							#
+	# You should have received a copy of the GNU General 	    #
+	# Public License along with this program; if not, write to  #
+	# the Free Software Foundation, Inc., 59 Temple Place -     #
+	# Suite 330, Boston, MA  02111-1307, USA.		    		#
+	#############################################################
+	#							    							#
+	# Slovenian Language File				    				#
+	# Created by Gasper Koren [gasper@fdvinfo.net]		 	   	#
+	# Web Survey Methodology - http://www.websm.org/	    	#
+	#							    							#
+	#############################################################
+*/
+//SINGLE WORDS
+define("_YES", "Da");
+define("_NO", "Ne");
+define("_UNCERTAIN", "Neodloèen");
+define("_ADMIN", "Admin");
+define("_TOKENS", "Gesla");
+define("_FEMALE", "®enski");
+define("_MALE", "Mo¹ki");
+define("_NOANSWER", "Brez odgovora");
+define("_NOTAPPLICABLE", "N/A"); //New for 0.98rc5
+define("_OTHER", "Drugo");
+define("_PLEASECHOOSE", "Prosimo, izberite");
+define("_ERROR_PS", "Napaka");
+define("_COMPLETE", "Zakljuèeno");
+define("_INCREASE", "Poveèalo"); //NEW WITH 0.98
+define("_SAME", "Ostalo enako"); //NEW WITH 0.98
+define("_DECREASE", "Zmanj¹alo"); //NEW WITH 0.98
+//from questions.php
+define("_CONFIRMATION", "Potrditev");
+define("_TOKEN_PS", "Geslo");
+define("_CONTINUE_PS", "Nadaljuj");
+
+//BUTTONS
+define("_ACCEPT", "Sprejmi");
+define("_PREV", "Nazaj");
+define("_NEXT", "Naprej");
+define("_LAST", "Zadnje");
+define("_SUBMIT", "Po¹lji podatke");
+
+
+//MESSAGES
+//From QANDA.PHP
+define("_CHOOSEONE", "Prosimo, izberite eno izmed naslednjih mo¾nosti");
+define("_ENTERCOMMENT", "Prosimo, vpi¹ite svoj komentar");
+define("_NUMERICAL_PS", "V to polje lahko vpisujete samo ¹tevilke");
+define("_CLEARALL", "Izhod brez po¹iljanja ankete");
+define("_MANDATORY", "Na to vpra¹anje morate odgovoriti");
+define("_MANDATORY_PARTS", "Prosimo, odgovorite na vsa vpra¹anja");
+define("_MANDATORY_CHECK", "Prosimo, izberite vsaj eno moznost");
+define("_MANDATORY_RANK", "Prosimo, rangirajte vse");
+define("_MANDATORY_POPUP", "Niste odgovorili na eno ali veè obveznih vpra¹anj, zato z anketo ne morete nadaljevati!"); //NEW in 0.98rc4
+define("_DATEFORMAT", "V obliki: LLLL-MM-DD");
+define("_DATEFORMATEG", "(n.p.: 2004-12-25 za Bo¾iè)");
+define("_REMOVEITEM", "Odstrani");
+define("_RANK_1", "Kliknite na listo na levi strani. Priènite z najvi¹je");
+define("_RANK_2", "ocenjenim in nadaljujte do najni¾je ocenjenega.");
+define("_YOURCHOICES", "Va¹a izbira");
+define("_YOURRANKING", "Va¹a ocena");
+define("_RANK_3", "Kliknite na ¹karje na desni,");
+define("_RANK_4", "ce ¾elite izbrisati zadnji vnos");
+//From INDEX.PHP
+define("_NOSID", "Niste posredovali identifikacijske ¹tevilke ankete!");
+define("_CONTACT1", "Prosimo, obrnite se na");
+define("_CONTACT2", "za nadaljno pomoè in vpra¹anja");
+define("_ANSCLEAR", "Odgovori so izbrisani");
+define("_RESTART", "Ponovno zaèni z anketo");
+define("_CLOSEWIN_PS", "Zapri okno");
+define("_CONFIRMCLEAR", "Ali ste preprièani, da ¾elite izbrisati va¹e odgovore?");
+define("_EXITCLEAR", "Zapusti anketo brez po¹iljanja podatkov");
+//From QUESTION.PHP
+define("_BADSUBMIT1", "Rezultatov ni mogoèe poslati -- rezultatov ni.");
+define("_BADSUBMIT2", "Ta napaka se lahko pojavi, èe ste ¾e posredovali va¹e rezultate in nato pritisnili gumb za osve¾itev strani (<i>Refresh</i>). Va¹i odgovori so bili v tem primeru ¾e shranjeni.<br /><br />Èe se to sporoèilo pojavi med anketiranjem, morate pritisniti gumb '<- NAZAJ' ('<- BACK') v va¹em brskalniku in nato OSVE®I/REFRESH. Dokler ne odgovorite na zadnje vpra¹anje v anketi, so va¹i odgovori ¹e vedno dosegljivi. Na podobno te¾avo lahko naletite tudi, ob preobremenjenosti streznika. Za te¾ave se vam opravièujemo.");
+define("_NOTACTIVE1", "Va¹i odgovori se niso zabele¾ili. Anketa ¹e ni aktivna!");
+define("_CLEARRESP", "Zbri¹i odgovore.");
+define("_THANKS", "Hvala lepa");
+define("_SURVEYREC", "Vasi odgovori so se shranili!");
+define("_SURVEYCPL", "Anketa je konèana.");
+define("_DIDNOTSAVE", "Ni bilo shranjeno.");
+define("_DIDNOTSAVE2", "Pri¹lo je do neprièakovane napake. Va¹ih odgovorov ni mogoèe shraniti.");
+define("_DIDNOTSAVE3", "Va¹i odgovori NISO bili izgiubljeni. Poslani so bili administratorju ankete in bodo vkljuèeni v rezultate.");
+define("_DNSAVEEMAIL1", "Napaka v identifikacijski ¹tevilki");
+define("_DNSAVEEMAIL2", "Podatki za vnos");
+define("_DNSAVEEMAIL3", "Napaka v SQL kodi");
+define("_DNSAVEEMAIL4", "SPOROÈILO Z NAPAKO");
+define("_DNSAVEEMAIL5", "NAPAKA PRI SHRANJEVANJU");
+define("_SUBMITAGAIN", "Poskusite ponovno");
+define("_SURVEYNOEXIST", "Oprostite. Ta anketa ne obstaja.");
+define("_NOTOKEN1", "Ta anketa ni javno dostopna. Za sodelovanje potrebujete geslo.");
+define("_NOTOKEN2", "Èe vam je bilo geslo posredovano, ga vpi¹ite v spodnje polje in kliknite za nadaljevanje.");
+define("_NOTOKEN3", "Geslo, ki ste ga posredovali ni veljavno ali pa je bilo ze uporabljeno.");
+define("_NOQUESTIONS", "Ta anketa se nima nobenih vpra¹anj.");
+define("_FURTHERINFO", "Za dodatne informacije se obrnite na");
+define("_NOTACTIVE", "Ta anketa trenutno ni aktivna. Va¹i odgovori ne bodo shranjeni.");
+define("_SURVEYEXPIRED", "Anketa je zakljuèena.");
+
+define("_SURVEYCOMPLETE", "Na to anketo ste ze odgovorili."); //NEW FOR 0.98rc6
+
+define("_INSTRUCTION_LIST", "Izberite samo eno/enega izmed na¹tetih"); //NEW for 098rc3
+define("_INSTRUCTION_MULTI", "Mo¾nih je veè odgovorov"); //NEW for 098rc3
+
+define("_CONFIRMATION_MESSAGE1", "Anketa je bila poslana"); //NEW for 098rc5
+define("_CONFIRMATION_MESSAGE2", "Va¹a anketa je dobila nov odgovor"); //NEW for 098rc5
+define("_CONFIRMATION_MESSAGE3", "Individualni potatki do vam na voljo tukaj:"); //NEW for 098rc5
+define("_CONFIRMATION_MESSAGE4", "Osnovne statistike so vam na voljo tukaj:"); //NEW for 098rc5
+?>
