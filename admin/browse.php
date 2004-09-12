1d18ab705af105815ee8eff9eed2c50bf4a24c50
Jason Cleeland <jcleeland@users.sourceforge.net>
2004-09-12 02:53:01 +0000

    TWEAK: Uses sendcacheheaders function instead of sending headers itself.
    git-svn-id: file:///Users/Shitiz/Downloads/lssvn/trunk/unstable@1166 b72ed6b6-b9f8-46b5-92b4-906544132732

--- admin/browse.php
+++ admin/browse.php
@@ -41,12 +41,7 @@ if (!isset($id)) {$id=returnglobal('id');}
 if (!isset($action)) {$action=returnglobal('action');}
 if (!isset($order)) {$order=returnglobal('order');}
 
-header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");    // Date in the past
-header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT"); 
-                                                     // always modified
-header("Cache-Control: no-store, no-cache, must-revalidate");  // HTTP/1.1
-header("Cache-Control: post-check=0, pre-check=0", false);
-header("Pragma: no-cache");                          // HTTP/1.0
+sendcacheheaders();
 
 //Select public language file
 $query = "SELECT language FROM {$dbprefix}surveys WHERE sid=$sid";
