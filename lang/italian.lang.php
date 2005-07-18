2b3431da92d41bd1e6d660b146c72a2e365d9416
Mario Marani <mariomar@users.sourceforge.net>
2005-07-18 18:44:05 +0000

    *** empty log message ***
    git-svn-id: file:///Users/Shitiz/Downloads/lssvn/trunk/unstable@1399 b72ed6b6-b9f8-46b5-92b4-906544132732

--- lang/italian.lang.php
+++ lang/italian.lang.php
@@ -15,7 +15,7 @@
     #                                                           #
     #   Copyright (C) 2003  Jason Cleeland                      #
     #                                                           #
-    # This program is free software; you can redistribute       #
+    # Thi program is free software; you can redistribute       #
     # it and/or modify it under the terms of the GNU General    #
     # Public License as published by the Free Software          #
     # Foundation; either version 2 of the License, or (at your  #
