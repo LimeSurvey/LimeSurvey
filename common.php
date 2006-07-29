32382297ea1981d93b0ed633017aa3f9ce03a75c
Carsten Schmitz <c_schmitz@users.sourceforge.net>
2006-07-29 09:52:20 +0000

    gettext-changes/bugfixes
    git-svn-id: file:///Users/Shitiz/Downloads/lssvn/source/phpsurveyor@1816 b72ed6b6-b9f8-46b5-92b4-906544132732

--- common.php
+++ common.php
@@ -1675,9 +1675,12 @@ function GetLanguageFromSurveyID($surveyid)
 
 function SetInterfaceLanguage($languagetoset)
     {
-     //@$locale = (isset($_GET['lang']))? $_GET['lang'] : DEFAULT_LOCALE;
-     //if (function_exists('T_setlocale')) T_setlocale(LC_MESSAGES, $locale);
-//     echo setlocale(LC_ALL, $languagetoset);
+    @$locale = (isset($_GET['lang']))? $_GET['lang'] : DEFAULT_LOCALE;
+    if (function_exists('T_setlocale')) T_setlocale(LC_MESSAGES, $locale);
+    @setlocale(LC_ALL, $languagetoset);
+    @putenv("LANG=$languagetoset");
+    @putenv("LANGUAGE=$languagetoset");
+
      bindtextdomain($languagetoset, dirname(__FILE__).'/locale');
      if (!function_exists('bind_textdomain_codeset')) echo "You need at least PHP 4.2.x to run PHPSurveyor." and die;
      bind_textdomain_codeset($languagetoset,'UTF-8');
