2c19cf3e5b2a1b930d372b384fdd4d61441ad1b3
David Olivier <machaven@users.sourceforge.net>
2007-02-04 17:17:52 +0000

    Changing over to language class for translation, e.g. reading locale files via php-gettext
    git-svn-id: file:///Users/Shitiz/Downloads/lssvn/source/phpsurveyor@2248 b72ed6b6-b9f8-46b5-92b4-906544132732

--- admin/install/index.php
+++ admin/install/index.php
@@ -36,36 +36,39 @@
 
 require_once(dirname(__FILE__).'/../../config.php');  // config.php itself includes common.php
 $adminoutput='';  // Alle future output is written into this and then outputted at the end of file
+// SET THE LANGUAGE???? -> DEFAULT SET TO EN FOR NOW
+require_once($rootdir.'/classes/core/language.php');
+$clang = new phpsurveyor_lang("en");
 
 if (!$database_exists)
 {
 	$adminoutput.= "<br />\n"
 	."<table width='350' align='center' style='border: 1px solid #555555' cellpadding='1' cellspacing='0'>\n"
 	."\t<tr bgcolor='#555555'><td colspan='2' height='4'><font size='1' face='verdana' color='white'><strong>"
-	._("PHPSurveyor Setup")."</strong></td></tr>\n"
+	.$clang->gT("PHPSurveyor Setup")."</strong></td></tr>\n"
 	."\t<tr bgcolor='#CCCCCC'><td align='center'>\n"
-	."<strong>"._("Welcome to PHPSurveyor Setup!")."</strong><br /><br />\n"
-	._("The database defined in config.php does not exist.")."<br />\n"
-	._("PHPSurveyor can attempt to create this database for you.")."<br /><br />\n"
-	._("Your selected database name is:")."<strong> $databasename</strong><br />\n"
+	."<strong>".$clang->gT("Welcome to PHPSurveyor Setup!")."</strong><br /><br />\n"
+	.$clang->gT("The database defined in config.php does not exist.")."<br />\n"
+	.$clang->gT("PHPSurveyor can attempt to create this database for you.")."<br /><br />\n"
+	.$clang->gT("Your selected database name is:")."<strong> $databasename</strong><br />\n"
 	."<br /><input type='submit' value='"
-	._("Create Database")."' onClick='location.href=\"createdb.php\"' /></center>\n"
+	.$clang->gT("Create Database")."' onClick='location.href=\"createdb.php\"' /></center>\n"
 	."</td></tr></table>\n"
 	."</body>\n</html>\n";
 }
-    elseif ($dbexistsbutempty && !(returnglobal('createdbstep2')==_("Populate Database")))
+    elseif ($dbexistsbutempty && !(returnglobal('createdbstep2')==$clang->gT("Populate Database")))
 {
         $connect->database = $databasename;
 	    $connect->Execute("USE DATABASE `$databasename`");
 		$adminoutput.= "<table width='350' align='center' style='border: 1px solid #555555' cellpadding='1' cellspacing='0'>\n"
 		."\t<tr bgcolor='#555555'><td colspan='2' height='4'><font size='1' face='verdana' color='white'><strong>"
-		._("PHPSurveyor Setup")."</strong></td></tr>\n"
+		.$clang->gT("PHPSurveyor Setup")."</strong></td></tr>\n"
 		."\t<tr bgcolor='#CCCCCC'><td align='center'>\n";
 		$adminoutput.= "<br /><strong><font color='green'>\n";
 		$adminoutput.= sprintf(_('A database named "%s" does already exist.'),$databasename)."</font></strong></font><br /><br />\n";
-		$adminoutput.= _("Do you want to populate that database now by creating the necessary tables?")."<br /><br />\n";
+		$adminoutput.= $clang->gT("Do you want to populate that database now by creating the necessary tables?")."<br /><br />\n";
 		$adminoutput.= "<form method='post' action='createdb.php'>";
-		$adminoutput.= "<input type='submit' name='createdbstep2' value='"._("Populate Database")."'></form>";
+		$adminoutput.= "<input type='submit' name='createdbstep2' value='".$clang->gT("Populate Database")."'></form>";
 		}
 else
 	{
