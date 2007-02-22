94f5718c957d075064b71de5c19582ee76c8746c
David Olivier <machaven@users.sourceforge.net>
2007-02-22 18:51:16 +0000

    Fix notice on $os var
    git-svn-id: file:///Users/Shitiz/Downloads/lssvn/source/phpsurveyor@2382 b72ed6b6-b9f8-46b5-92b4-906544132732

--- common.php
+++ common.php
@@ -137,7 +137,7 @@ if (!ini_get('safe_mode') && (!eregi('shell_exec',ini_get('disable_functions')))
 {
 	// Only do this if safe_mode is OFF
 	if (isset($mysqldir)) {$mysqlbin=$mysqldir;}
-	if ((substr($OS, 0, 3) != "WIN") && (substr($OS, 0, 4) != "OS/2") )
+	if (isset($OS) && (substr($OS, 0, 3) != "WIN") && (substr($OS, 0, 4) != "OS/2") )
 	{
 		//USING LINUX: Find the location of various files and put that in the appropriate variables!
 		if (!isset($mysqlbin) || !$mysqlbin)
