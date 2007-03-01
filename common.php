1b6b6fec6ff3241ddcb47a1cc18a70b3a714f7ed
David Olivier <machaven@users.sourceforge.net>
2007-03-01 19:07:17 +0000

    fixed mistake on previous commit.
    git-svn-id: file:///Users/Shitiz/Downloads/lssvn/source/phpsurveyor@2472 b72ed6b6-b9f8-46b5-92b4-906544132732

--- common.php
+++ common.php
@@ -2648,7 +2648,7 @@ function strip_tags_full($string) {
 	$string=str_replace('??', '?', $string);
 	$string=str_replace('?~H', '?', $string);
 	
-	$string=str_replace(chr(13), "", $strTmp);
+	$string=str_replace(chr(13), "", $string);
 	
 	$string=trim($string);
 	if ($string == '-oth-') $string='';
