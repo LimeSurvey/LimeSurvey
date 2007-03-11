d1cd5278e477bfceac3ce3c7f50050cddeda3423
David Olivier <machaven@users.sourceforge.net>
2007-03-11 22:34:59 +0000

    wrong file..
    git-svn-id: file:///Users/Shitiz/Downloads/lssvn/source/phpsurveyor@2559 b72ed6b6-b9f8-46b5-92b4-906544132732

--- common.php
+++ common.php
@@ -3053,11 +3053,11 @@ function CSVUnquote($field)
 // This function removes surrounding and masking quotes from the CSV field
 // c_schmitz
 {
-	print $field.":";
+	//print $field.":";
 	$field = preg_replace ("/^\040*\"/", "", $field);
 	$field = preg_replace ("/\"\040*$/", "", $field);
     $field=str_replace('""','"',$field);
-    print $field."\n";
+    //print $field."\n";
     return $field;
 }
 
