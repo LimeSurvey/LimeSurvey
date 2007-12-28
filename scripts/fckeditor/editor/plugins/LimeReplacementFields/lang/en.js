43c470834007c635e8e2acb3a37fb264a878da6f
Thibault Le Meur <lemeur@users.sourceforge.net>
2007-12-28 15:19:27 +0000

    FCKed: LimeReplacementField small language update
    git-svn-id: file:///Users/Shitiz/Downloads/lssvn/source/limesurvey@3826 b72ed6b6-b9f8-46b5-92b4-906544132732

--- scripts/fckeditor/editor/plugins/LimeReplacementFields/lang/en.js
+++ scripts/fckeditor/editor/plugins/LimeReplacementFields/lang/en.js
@@ -20,8 +20,5 @@
  *
  * Placholder English language file.
  */
-FCKLang.PlaceholderBtn			= 'Insert/Edit Placeholder' ;
-FCKLang.PlaceholderDlgTitle		= 'Placeholder Properties' ;
-FCKLang.PlaceholderDlgName		= 'Placeholder Name' ;
-FCKLang.PlaceholderErrNoName	= 'Please type the placeholder name' ;
-FCKLang.PlaceholderErrNameInUse	= 'The specified name is already in use' ;
+FCKLang.LimeReplacementFieldsBtn	= 'Insert/Edit LimeReplacementField' ;
+FCKLang.LimeReplacementFieldsDlgTitle	= 'LimeReplacementField Properties' ;
