258841228acac2b8a70848875758b42450c54bde
Carsten Schmitz <c_schmitz@users.sourceforge.net>
2008-02-25 22:01:56 +0000

    Release 1.70 - Changed Version string
    git-svn-id: file:///Users/Shitiz/Downloads/lssvn/source/limesurvey@4351 b72ed6b6-b9f8-46b5-92b4-906544132732

--- common.php
+++ common.php
@@ -15,7 +15,7 @@
 
 //Ensure script is not run directly, avoid path disclosure
 if (!isset($dbprefix) || isset($_REQUEST['dbprefix'])) {die("Cannot run this script directly");}
-$versionnumber = "1.70RC2";
+$versionnumber = "1.70";
 $dbversionnumber = 126;
 
 
