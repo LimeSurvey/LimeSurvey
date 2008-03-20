d4b57cfd67bb559d02e99dd121dba5d660a553da
Carsten Schmitz <c_schmitz@users.sourceforge.net>
2008-03-20 14:23:42 +0000

    Fixed attribute tag filtering being negligent of quotes
    git-svn-id: file:///Users/Shitiz/Downloads/lssvn/source/limesurvey@4496 b72ed6b6-b9f8-46b5-92b4-906544132732

--- classes/inputfilter/class.inputfilter_clean.php
+++ classes/inputfilter/class.inputfilter_clean.php
@@ -119,7 +119,8 @@ class InputFilter {
 		$newSet = array();
 		for ($i = 0; $i <count($attrSet); $i++) {
 			if (!$attrSet[$i]) continue;
-			$attrSubSet = explode('=', trim($attrSet[$i]));
+			$attrSubSet[] = trim(substr($attrSet[$i],0,strpos($attrSet[$i],'=')));
+            $attrSubSet[] = trim(substr($attrSet[$i],strpos($attrSet[$i],'=')+1));
 			list($attrSubSet[0]) = explode(' ', $attrSubSet[0]);
 			if ((!eregi("^[a-z]*$",$attrSubSet[0])) || (($this->xssAuto) && ((in_array(strtolower($attrSubSet[0]), $this->attrBlacklist)) || (substr($attrSubSet[0], 0, 2) == 'on')))) 
 				continue;
