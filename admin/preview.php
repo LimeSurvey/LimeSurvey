0c15af60c2c79b97a53f4d07abfc01e7140b5770
Evan Wills <eric_t_cruiser@users.sourceforge.net>
2008-11-18 13:55:35 +0000

    fixed #2630: changed variable that defines question type.
    git-svn-id: file:///Users/Shitiz/Downloads/lssvn/source/limesurvey@6007 b72ed6b6-b9f8-46b5-92b4-906544132732

--- admin/preview.php
+++ admin/preview.php
@@ -63,7 +63,7 @@ $help=$answers[0][2];
 $questioncode=$answers[0][5];
 $content = templatereplace(file_get_contents("$thistpl/preview.pstpl"));
 $content = str_replace('{FORM_START}' , $form_start , $content);
-$content = str_replace('{QUESTION_CLASS}' , question_class($ia[2]) , $content);
+$content = str_replace('{QUESTION_CLASS}' , question_class($qrows['type']) , $content);
 echo $content;
 echo "</html>\n";
 
