a45c92a25352c2c3360fd2aff1bb2da8ec4f7506
Carsten Schmitz <c_schmitz@users.sourceforge.net>
2009-01-07 21:03:51 +0000

    Fixed issue #2761: Removed XML header tag causing issues in IE6
    git-svn-id: file:///Users/Shitiz/Downloads/lssvn/source/limesurvey@6230 b72ed6b6-b9f8-46b5-92b4-906544132732

--- common.php
+++ common.php
@@ -3217,7 +3217,7 @@ function getHeader()
 
 	if ( !$embedded )
 	{
-		$header=  "<?xml version=\"1.0\" encoding=\"UTF-8\"?><!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">\n"
+		$header=  "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">\n"
         		. "<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"".$surveylanguage."\" lang=\"".$surveylanguage."\"";
         if (getLanguageRTL($surveylanguage))
         {
