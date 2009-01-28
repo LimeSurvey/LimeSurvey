6b6b8b0743d12d72ad981b1e5b83c8dfccb263f0
Tim Wahrendorff <wahrendorff@users.sourceforge.net>
2009-01-28 08:36:31 +0000

    fixed: cmd_install now working again. Tested with postgres and mysql changed: enhanced commandline feedback for errors
    git-svn-id: file:///Users/Shitiz/Downloads/lssvn/source/limesurvey@6314 b72ed6b6-b9f8-46b5-92b4-906544132732

--- admin/install/cmd_install.php
+++ admin/install/cmd_install.php
@@ -12,7 +12,7 @@
 *
 * $Id: index.php 4729 2008-05-31 21:32:02Z nijaba $
 */
-
+$cmd_install=true;
 if (isset($argv[1]) && ($argv[1]=='install'|| $argv[1]=='upgrade') && isset($argv[2]) && isset($argv[3]))    
 {
     require_once($argv[2]);
@@ -28,25 +28,130 @@ require_once($rootdir.'/classes/core/language.php');
 $clang = new limesurvey_lang("en");
 ob_implicit_flush(true);
 
-
 if (isset($argv[1]) && $argv[1]=='install')
 {
-    if ($databasetype=='mysql') {@$connect->Execute("ALTER DATABASE `$databasename` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;");} //Set the collation also for manually created DBs
-       
-    if (modify_database(dirname(__FILE__).'/create-'.$databasetype.'.sql'))
-    {
-    	print("Creating $databasename on $databasetype\n");
-    	return 0;
-    } else {
-    	print("Could not create $databasename on $databasetype\n");
-    	return 1;
-    }
+	print("trying to create and populate $databasename on $databaselocation:$databaseport ($databasetype) \n");
+	
+	if (!$database_exists) //Database named in config.php does not exist
+	{
+		
+		if($connect->Execute("CREATE DATABASE $databasename;"))
+		{
+			print("\nDatabase $databasename on $databasetype CREATED \n");
+		}
+		else
+		{
+			print("\nDatabase $databasename on $databasetype COULD NOT BE CREATED \n");
+			print("\n".$connect->ErrorMsg());
+			return 1;
+		}
+
+	}
+	else
+	{
+		if ($databasetype=='mysql') {$connect->Execute("ALTER DATABASE `$databasename` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;");} //Set the collation also for manually created DBs
+		
+		print("\nDatabase $databasename on $databasetype EXISTS, not created \n");
+		
+	}
+	
+	// Connect to the database we created a sec ago or to the existing db. 
+	if(!$connect->Connect($databaselocation,$databaseuser,$databasepass,$databasename))
+	{
+		print("\n".$connect->ErrorMsg());
+		return 1;
+	}
+		
+	require_once($homedir."/classes/core/sha256.php");
+	
+	$success = 0;  // Let's be optimistic
+	
+	$sqlfile = dirname(__FILE__).'/create-'.$databasetype.'.sql' ;
+	
+	if (!empty($sqlfile)) {
+		if (!is_readable($sqlfile)) {
+			$success = false;
+			print "\nTried to populate database, but '". $sqlfile ."' doesn't exist!\n";
+			return $success;
+		} else {
+			$lines = file($sqlfile);
+		}
+	} else {
+		$sqlstring = trim($sqlstring);
+		if ($sqlstring{strlen($sqlstring)-1} != ";") {
+			$sqlstring .= ";"; // add it in if it's not there.
+		}
+		$lines[] = $sqlstring;
+	}
+
+	$command = '';
+	
+	$connect->SetFetchMode(ADODB_FETCH_NUM);
+	foreach ($lines as $line) {
+		$line = rtrim($line);
+		$length = strlen($line);
+
+		if ($length and $line[0] <> '#' and substr($line,0,2) <> '--') {
+			if (substr($line, $length-1, 1) == ';') {
+  				$line = substr($line, 0, $length-1);   // strip ;
+				$command .= $line;
+				$command = str_replace('prefix_', $dbprefix, $command); // Table prefixes
+				$command = str_replace('$defaultuser', $defaultuser, $command); // variables By Moses
+				$command = str_replace('$defaultpass', SHA256::hash($defaultpass), $command); // variables By Moses
+				$command = str_replace('$siteadminname', $siteadminname, $command);
+				$command = str_replace('$siteadminemail', $siteadminemail, $command); // variables By Moses
+				$command = str_replace('$defaultlang', $defaultlang, $command); // variables By Moses
+				$command = str_replace('$sessionname', 'ls'.getRandomID().getRandomID().getRandomID().getRandomID(), $command); // variables By Moses
+				$command = str_replace('$databasetabletype', $databasetabletype, $command);
+
+
+					
+					
+					if(!$connect->Execute($command,false))
+					{
+						print ("\n".$clang->gT("Executing").".....".$command."...".$clang->gT("Failed! Reason:\n ").$connect->ErrorMsg()."\n\n");
+						$success=1;
+					}
+					
+		
+
+				$command = '';
+			} else {
+				$command .= $line;
+			}
+		}
+	}
+	$connect->SetFetchMode(ADODB_FETCH_ASSOC);
+	if($success == 0)
+	{
+		print("Database $databasename on $databasetype POPULATED");
+		print("\n\neverything went fine");
+		return $success;
+	}
+	else
+	{
+		print("\n\nSomething is strange");
+		print("\nplease check you Database and Settings");
+		return $success;
+	}
+	
+	
+    // if (modify_database(dirname(__FILE__).'\create-'.$databasetype.'.sql'))
+    // {
+    	
+    	// print("\nDatabase $databasename on $databasetype POPULATED \n");
+    	// return 0;
+    // } else {
+
+    	// print("Could not populate $databasename on $databasetype\n");
+    	// return 1;
+    // }
 }
 
 elseif (isset($argv[1]) && $argv[1]=='upgrade')
 
 {
-    global $connect, $databasetype, $dbprefix, $dbversionnumber, $clang;
+   
     include ('upgrade-'.$databasetype.'.php');
     $tables = $connect->MetaTables();
     
