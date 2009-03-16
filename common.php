33c69bf5ac62b9f145ce9fe19b754296cc70045e
Thibault Le Meur <lemeur@users.sourceforge.net>
2009-03-16 20:36:11 +0000

    Preparing 1.80+ release
    git-svn-id: file:///Users/Shitiz/Downloads/lssvn/source/limesurvey@6536 b72ed6b6-b9f8-46b5-92b4-906544132732

--- common.php
+++ common.php
@@ -17,7 +17,7 @@
 
 //Ensure script is not run directly, avoid path disclosure
 if (!isset($dbprefix) || isset($_REQUEST['dbprefix'])) {safe_die("Cannot run this script directly");}
-$versionnumber = "1.80";
+$versionnumber = "1.80+";
 $dbversionnumber = 132;
 $buildnumber = "";
 
