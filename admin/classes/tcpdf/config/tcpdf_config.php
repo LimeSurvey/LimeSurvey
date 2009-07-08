29e648b0e8cd16725d9d36d7f85c72d86bf5765b
Tim Wahrendorff <wahrendorff@users.sourceforge.net>
2009-07-08 15:32:30 +0000

    dev: set tcpdf's cachedir to /tmp
    git-svn-id: file:///Users/Shitiz/Downloads/lssvn/source/limesurvey_dev@7249 b72ed6b6-b9f8-46b5-92b4-906544132732

--- admin/classes/tcpdf/config/tcpdf_config.php
+++ admin/classes/tcpdf/config/tcpdf_config.php
@@ -88,7 +88,7 @@ if (!defined('K_TCPDF_EXTERNAL_CONFIG')) {
 	/**
 	 * cache directory for temporary files (url path)
 	 */
-	define ('K_PATH_URL_CACHE', K_PATH_URL.'cache/');
+	define ('K_PATH_URL_CACHE', K_PATH_URL.'../../../tmp/');
 	
 	/**
 	 *images directory
