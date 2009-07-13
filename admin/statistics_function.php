df330937af952d3da7c5809a510a2aca3a3111a6
Tim Wahrendorff <wahrendorff@users.sourceforge.net>
2009-07-13 15:23:48 +0000

    changed: always prompt a Downloaddialog for pdf statistic
    git-svn-id: file:///Users/Shitiz/Downloads/lssvn/source/limesurvey_dev@7311 b72ed6b6-b9f8-46b5-92b4-906544132732

--- admin/statistics_function.php
+++ admin/statistics_function.php
@@ -85,7 +85,7 @@ if (isset($_REQUEST['homedir'])) {die('You cannot start this script directly');}
 	
 //generate_statistics('999','all',0,'pdf','F');
 
-function generate_statistics($surveyid, $allfields, $q2show='all', $usegraph=0, $outputType='pdf', $pdfOutput='D')
+function generate_statistics($surveyid, $allfields, $q2show='all', $usegraph=0, $outputType='pdf', $pdfOutput='DD')
 {
 	//$allfields ="";
 	global $connect, $dbprefix, $clang,
