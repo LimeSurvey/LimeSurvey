de856782e9743b5e6ddeb8475bf9bc9f40325829
Carsten Schmitz <c_schmitz@users.sourceforge.net>
2009-11-14 19:55:43 +0000

    Changed feature: Removed settings available in global settings from config.php - added $debug setting to config.php, added new database types and explanations to config.php
    git-svn-id: file:///Users/Shitiz/Downloads/lssvn/source/limesurvey@7894 b72ed6b6-b9f8-46b5-92b4-906544132732

--- config-defaults.php
+++ config-defaults.php
@@ -87,12 +87,6 @@ $defaulttemplate    =  'default';         // This setting specifys the default t
 $allowedtemplateuploads = 'gif,jpg,png';  // File types allowed to be uploaded in the templates section.
 
 $allowedresourcesuploads = '7z,aiff,asf,avi,bmp,csv,doc,fla,flv,gif,gz,gzip,ico,jpeg,jpg,mid,mov,mp3,mp4,mpc,mpeg,mpg,ods,odt,pdf,png,ppt,pxd,qt,ram,rar,rm,rmi,rmvb,rtf,sdc,sitd,swf,sxc,sxw,tar,tgz,tif,tiff,txt,vsd,wav,wma,wmv,xls,xml,zip,pstpl,css,js';   // File types allowed to be uploaded in the resources sections, and with the HTML Editor
-
-
-$debug              =   0;      // Set this to 1 if you are looking for errors. If you still get no errors after enabling this
-                                // then please check your error-logs - either in your hosting provider admin panel or in some /logs dir.
-                                // LimeSurvey developers set this to 2 to circumvent the restriction to remove the installation directory or to change the password
-                                // If you set it to 3 then in addition PHP STRICT warnings will be shown
                                 
 $memorylimit        =  '32M';   // This sets how much memory LimeSurvey can access. 16M is the minimum (M=mb) recommended.
 
