d5b51e290e0ba6a8b7e51667d7df4b8aec7ded6d
Carsten Schmitz <c_schmitz@users.sourceforge.net>
2009-12-05 21:37:40 +0000

    Fixed issue #3935: Charset problem in emails causing problems in Lotus notes
    git-svn-id: file:///Users/Shitiz/Downloads/lssvn/source/limesurvey@8081 b72ed6b6-b9f8-46b5-92b4-906544132732

--- classes/phpmailer/class.phpmailer.php
+++ classes/phpmailer/class.phpmailer.php
@@ -1271,7 +1271,7 @@ class PHPMailer {
       $encoding = $this->Encoding;
     }
     $result .= $this->TextLine('--' . $boundary);
-    $result .= sprintf("Content-Type: %s; charset = \"%s\"", $contentType, $charSet);
+    $result .= sprintf("Content-Type: %s; charset=\"%s\"", $contentType, $charSet);
     $result .= $this->LE;
     $result .= $this->HeaderLine('Content-Transfer-Encoding', $encoding);
     $result .= $this->LE;
