f4e4aff9ba3c45f4c76d205c89c8e3b3222e9c79
Carsten Schmitz <c_schmitz@users.sourceforge.net>
2009-12-11 12:11:35 +0000

    dev Check for mssql DB during DB backup in updater
    git-svn-id: file:///Users/Shitiz/Downloads/lssvn/source/limesurvey@8148 b72ed6b6-b9f8-46b5-92b4-906544132732

--- version.php
+++ version.php
@@ -1,7 +1,7 @@
 <?php
 
-    $versionnumber = "1.87RC5";
-    $dbversionnumber = 142;
-    $buildnumber = '';
+    $versionnumber = "1.87RC4";
+    $dbversionnumber = 141;
+$buildnumber = '8004';
 
 ?>
