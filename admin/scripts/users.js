5428e7a551817555098fc04d496dbf4f38e210e0
Aniessh Sethh <anishseth@users.sourceforge.net>
2010-04-17 16:52:50 +0000

    Fixed issue : 04283 Dev This patch doesn't allow a user to be a superadmin. Dev Unless all the sub-rights are not selected.
    git-svn-id: file:///Users/Shitiz/Downloads/lssvn/source/limesurvey@8604 b72ed6b6-b9f8-46b5-92b4-906544132732

--- admin/scripts/users.js
+++ admin/scripts/users.js
@@ -7,4 +7,10 @@ $(document).ready(function(){
 	    $("input[type=checkbox]").attr("checked",!tog);
 		tog=!tog;
 });
+$('#create_survey,#configurator,#create_user,#delete_user,#manage_template,#manage_label').click(function() {
+  if($(this).attr('checked')==false)
+  {
+  $('#superadmin').attr('checked', false);
+  }
+});
 });
