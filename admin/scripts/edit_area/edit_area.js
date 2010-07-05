8b695df296ba6b36f2f591f332d0d758d91cb963
Carsten Schmitz <c_schmitz@users.sourceforge.net>
2010-07-05 00:15:56 +0000

    Fixed issue #4459: Text in template HTML/CSS editor has a shadow, is hard to read in Safari 5
    git-svn-id: file:///Users/Shitiz/Downloads/lssvn/source/limesurvey@8897 b72ed6b6-b9f8-46b5-92b4-906544132732

--- admin/scripts/edit_area/edit_area.js
+++ admin/scripts/edit_area/edit_area.js
@@ -227,7 +227,8 @@
 		
 		if( t.isSafari ){
 			t.editor_area.style.position	= "absolute";
-			a.style.marginLeft		="-3px";
+			if( t.isSafari < 5)
+				a.style.marginLeft		="-3px";
 			if( t.isSafari < 3.2 ) // Safari 3.0 (3.1?)
 				a.style.marginTop	="1px";
 		}
@@ -525,3 +526,5 @@
 	function init(){		
 		setTimeout("editArea.init();  ", 10);
 	};
+
+ 	  	 
