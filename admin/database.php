530a42b0de7d189a647395b011f57ef0187f9874
Carsten Schmitz <c_schmitz@users.sourceforge.net>
2010-07-05 15:39:29 +0000

    Fixed issue #4457: When copying question, answer options are not copied
    git-svn-id: file:///Users/Shitiz/Downloads/lssvn/source/limesurvey@8901 b72ed6b6-b9f8-46b5-92b4-906544132732

--- admin/database.php
+++ admin/database.php
@@ -781,9 +781,9 @@ if(isset($surveyid))
                 while ($qr1 = $r1->FetchRow())
                 {
                     $qr1 = array_map('db_quote', $qr1);
-                    $i1 = "INSERT INTO {$dbprefix}answers (qid, code, answer, default_value, sortorder, language) "
+                    $i1 = "INSERT INTO {$dbprefix}answers (qid, code, answer, sortorder, language) "
                     . "VALUES ('$newqid', '{$qr1['code']}', "
-                    . "'{$qr1['answer']}', '{$qr1['default_value']}', "
+                    . "'{$qr1['answer']}', "
                     . "'{$qr1['sortorder']}', '{$qr1['language']}')";
                     $ir1 = $connect->Execute($i1);   // Checked
 
