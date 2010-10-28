12566a5c4f3e6ed90d672217a2c7cf310391e736
Carsten Schmitz <c_schmitz@users.sourceforge.net>
2010-10-28 20:54:58 +0000

    Fixed issue #4705: Export to excel fails when exporting token fields with data and using Y/N conversion
    git-svn-id: file:///Users/Shitiz/Downloads/lssvn/source/limesurvey@9357 b72ed6b6-b9f8-46b5-92b4-906544132732

--- admin/exportresults.php
+++ admin/exportresults.php
@@ -791,14 +791,13 @@ if ($answers == "short") //Nice and easy. Just dump the data straight
         {
             $convertyto=returnglobal('convertyto');
             foreach($drow as $key=>$dr) {
-                $fielddata=$fieldmap[$key];
-                if(isset($fielddata['type']) &&
-                ($fielddata['type'] == "M" ||
-                $fielddata['type'] == "P" ||
-                $fielddata['type'] == "Y")
-                )
+                if (isset($fieldmap[$key]))
                 {
-                    if($dr == "Y") {$dr = $convertyto;}
+                    $fielddata=$fieldmap[$key];
+                    if (isset($fielddata['type']) && ($fielddata['type'] == "M" || $fielddata['type'] == "P" || $fielddata['type'] == "Y"))
+                    {
+                        if($dr == "Y") {$dr = $convertyto;}
+                    }
                 }
                 $line[$key]=$dr;
             }
@@ -809,14 +808,13 @@ if ($answers == "short") //Nice and easy. Just dump the data straight
         {
             $convertnto=returnglobal('convertnto');
             foreach($drow as $key=>$dr) {
-                $fielddata=$fieldmap[$key];
-                if(isset($fielddata['type']) &&
-                ($fielddata['type'] == "M" ||
-                $fielddata['type'] == "P" ||
-                $fielddata['type'] == "Y")
-                )
+                if (isset($fieldmap[$key]))
                 {
-                    if($dr == "N") {$dr = $convertnto;}
+                    $fielddata=$fieldmap[$key];
+                    if (isset($fielddata['type']) && ($fielddata['type'] == "M" || $fielddata['type'] == "P" || $fielddata['type'] == "Y"))
+                    {
+                        if($dr == "N") {$dr = $convertnto;}
+                    }
                 }
                 $line[$key]=$dr;
             }
