decbe3a94c3cdfa2bbe67da550307414ca640844
Carsten Schmitz <c_schmitz@users.sourceforge.net>
2010-11-21 22:18:39 +0000

    Dev Fixed some minor notices
    git-svn-id: file:///Users/Shitiz/Downloads/lssvn/source/limesurvey_dev@9513 b72ed6b6-b9f8-46b5-92b4-906544132732

--- admin/access_denied.php
+++ admin/access_denied.php
@@ -115,8 +115,12 @@ if (isset($_SESSION['loginID']))
     }
     elseif($action == "CSRFwarn")
     {
+        $sURLID='';
+        if (isset($sid)) {
+            $sURLID="?sid={$sid}";
+        }
         $accesssummary .= "<p><span color='errortitle'>".$clang->gT("Security alert")."</span>: ".$clang->gT("Someone may be trying to use your LimeSurvey session (CSRF attack suspected). If you just clicked on a malicious link, please report this to your system administrator.").'<br>'.$clang->gT('Also this problem can occur when you are working/editing in LimeSurvey in several browser windows/tabs at the same time.')."</p>";
-        $accesssummary .= "<a href='$scriptname?sid={$sid}'>".$clang->gT("Continue")."</a><br />&nbsp;\n";
+        $accesssummary .= "<a href='{$scriptname}{$sURLID}'>".$clang->gT("Continue")."</a><br />&nbsp;\n";
     }
     elseif($action == "FakeGET")
     {
