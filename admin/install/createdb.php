e7932210f9fbca5c89296e13fa1558f268f5ab8b
Carsten Schmitz <c_schmitz@users.sourceforge.net>
2010-12-10 21:38:02 +0000

    Fixed issue #4783: If installed on Postgres 9.0 you can't login
    git-svn-id: file:///Users/Shitiz/Downloads/lssvn/source/limesurvey_dev@9622 b72ed6b6-b9f8-46b5-92b4-906544132732

--- admin/install/createdb.php
+++ admin/install/createdb.php
@@ -39,6 +39,10 @@ if (returnglobal('createdbstep2')==$clang->gT("Populate Database"))
         $createdbtype='mysql';
     }
     if ($createdbtype=='mssql_n' || $createdbtype=='odbc_mssql' || $createdbtype=='odbtp') $createdbtype='mssql';
+    if($createdbtype=='postgres' && $connect->pgVersion=='9') 
+    {
+        $connect->execute("ALTER DATABASE {$dbname} SET bytea_output='escape';");
+    }
    if($createdbtype=='mssqlnative') $createdbtype='mssqlnative';
     if (modify_database(dirname(__FILE__).'/create-'.$createdbtype.'.sql'))
     {
