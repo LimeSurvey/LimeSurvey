f0d1922bc3eeb4ed22254d7348bdd943c8bae04c
Thibault Le Meur <lemeur@users.sourceforge.net>
2011-02-12 08:49:28 +0000

    Fixed issue #04920: Notice when using INSERTANS placeholders and "all-in-one" survey format.
    git-svn-id: file:///Users/Shitiz/Downloads/lssvn/source/limesurvey_dev@9778 b72ed6b6-b9f8-46b5-92b4-906544132732

--- classes/dTexts/dFunctions/dFunctionInsertAns.php
+++ classes/dTexts/dFunctions/dFunctionInsertAns.php
@@ -2,16 +2,16 @@
 
 class dFunctionInsertAns implements dFunctionInterface
 {
-	public function __construct()
-	{
-	}
-	
-	public function run($args)
-	{
-		global $connect;
-		$field = $args[0];
-		if (isset($_SESSION['srid'])) $srid = $_SESSION['srid'];
-		$sid = $_POST['sid'];
-		return retrieve_Answer($field, $_SESSION['dateformats']['phpdate']);
-	}
+    public function __construct()
+    {
+    }
+
+    public function run($args)
+    {
+        global $connect;
+        $field = $args[0];
+        if (isset($_SESSION['srid'])) $srid = $_SESSION['srid'];
+        $sid = returnglobal('sid');
+        return retrieve_Answer($field, $_SESSION['dateformats']['phpdate']);
+    }
 }
