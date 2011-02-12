f75beca0bb0746a566458e7bd70b55c0ee0d27eb
Thibault Le Meur <lemeur@users.sourceforge.net>
2011-02-12 09:16:07 +0000

    Fixed issue: SQL error in browse response module when counting answers
    git-svn-id: file:///Users/Shitiz/Downloads/lssvn/source/limesurvey@9781 b72ed6b6-b9f8-46b5-92b4-906544132732

--- admin/browse.php
+++ admin/browse.php
@@ -729,7 +729,8 @@ elseif ($subaction == "all")
     }
     
     //LETS COUNT THE DATA
-    $dtquery = "SELECT count(*) FROM $sql_from $sql_where";
+    //$dtquery = "SELECT count(*) FROM $sql_from $sql_where";
+    $dtquery = "SELECT count(*) FROM $sql_from";
     if ($sql_where!="")
     {
         $dtquery .=" WHERE $sql_where";
