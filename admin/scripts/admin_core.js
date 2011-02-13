3ddcc9cce0f95bdb5b8e330056c2a266658a84d3
Kshitij Parajulik <parajulik@users.sourceforge.net>
2011-02-13 16:58:04 +0000

    Fixed bug #4868: Missing translation Dev Added $clang->gT() and made some changes to a javascript file.
    git-svn-id: file:///Users/Shitiz/Downloads/lssvn/source/limesurvey@9786 b72ed6b6-b9f8-46b5-92b4-906544132732

--- admin/scripts/admin_core.js
+++ admin/scripts/admin_core.js
@@ -198,14 +198,17 @@ $(document).ready(function(){
        var oldThis = this;
        var ownername_edit_id = $(this).attr('id');
        var survey_id = ownername_edit_id.slice(15);
+       var translate_to = $(this).attr('translate_to');
+       var initial_text = $(this).html();
        $.getJSON('admin.php', {
                     action: 'ajaxgetusers'
                 },function(oData)
                 {
                     old_owner =  $($(oldThis).parent()).html();
-                    old_owner = (old_owner.split(" "))[0];
+		    
+                    old_owner = (old_owner.split("("))[0];
                     $($(oldThis).parent()).html('<select class="ownername_select" id="ownername_select_'+survey_id+'"></select>'
-                    + '<input class="ownername_button" id="ownername_button_'+survey_id+'" type="button" value="Update">');
+                    + '<input class="ownername_button" id="ownername_button_'+survey_id+'" type="button" initial_text="'+initial_text+'" value="'+translate_to+'">');
                     $(oData).each(function(key,value){
                         $('#ownername_select_'+survey_id).
                           append($("<option id='opt_"+value[1]+"'></option>").
@@ -218,22 +221,26 @@ $(document).ready(function(){
 
     $(".ownername_button").live('click',function(){
        var oldThis = this;
+       var initial_text = $(this).attr('initial_text');
        var ownername_select_id = $(this).attr('id');
        var survey_id = ownername_select_id.slice(17);
        var newowner = $("#ownername_select_"+survey_id).val();
+       var translate_to = $(this).attr('value');
 
        $.getJSON('admin.php',{
             action: 'ajaxowneredit',
             newowner: newowner,
             survey_id : survey_id
        }, function (data){
-           var objToUpdate = $($(oldThis).parent());
-           if (data.record_count>0)
+	
+	    var objToUpdate = $($(oldThis).parent());
+	    
+	    if (data.record_count>0)
                $(objToUpdate).html(data.newowner);
-           else
+	    else
                $(objToUpdate).html(old_owner);
-
-           $(objToUpdate).html($(objToUpdate).html() + '(<a id="ownername_edit_69173" class="ownername_edit" href="#">Edit</a>)' );
+	       
+	    $(objToUpdate).html($(objToUpdate).html() + '(<a id="ownername_edit_69173" translate_to='+translate_to+' class="ownername_edit" href="#">'+initial_text+'</a>)' );
        });
     });
 
