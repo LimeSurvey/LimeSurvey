59a422cde39fa79c17297a145904931a9b96f385
Diogo Gonçalves <dionet@users.sourceforge.net>
2011-05-10 17:06:05 +0000

    Ported back-end class queXMLPDF as a library. Moved it to admin libraries.
    git-svn-id: file:///Users/Shitiz/Downloads/lssvn/source/limesurvey_ci@10057 b72ed6b6-b9f8-46b5-92b4-906544132732

--- application/libraries/admin/queXMLPDF.php
+++ application/libraries/admin/queXMLPDF.php
@@ -172,18 +172,7 @@ class queXMLPDF extends TCPDF {
 	 * @var string  Defaults to "<style>td.questionHelp {text-align:right; font-style:italic; font-size: 8pt;} td.responseText {text-align:right; margin-right:1mm;} td.responseAboveText {text-align:left;} td.responseLabel {text-align:center; font-size:8pt;} span.sectionTitle {font-size: 18pt} span.sectionDescription {font-size: 14pt}</style>". 
 	 * @since 2010-09-16
 	 */
-	protected $style = "<style>
-		td.questionTitle {font-weight:bold; font-size:12pt;}
-		td.questionText {font-weight:bold; font-size:12pt;} 
-		td.questionHelp {font-weight:normal; text-align:right; font-style:italic; font-size:8pt;}
-		td.questionHelpAfter {text-align:center; font-weight:bold; font-size:10pt;}
-		td.responseAboveText {font-weight:normal; font-style:normal; text-align:left; font-size:12pt;} 
-		span.sectionTitle {font-size:18pt; font-weight:bold;} 
-		span.sectionDescription {font-size:14pt; font-weight:bold;} 
-		div.sectionInfo {font-style:normal; font-size:10pt; text-align:left; font-weight:normal;}
-		td.questionnaireInfo {font-size:16pt; text-align:center; font-weight:bold;}
-		</style>";
-
+	protected $style;
 	/**
 	 * Width of the area of each single response 
 	 * 
@@ -554,7 +543,8 @@ class queXMLPDF extends TCPDF {
     
     function __construct()
     {
-        echo "hello";
+        $CI =& get_instance();
+		$this->style = $CI->load->view('libraries/quexmlpdf/style_view.php','',true);
     }
 	/**
 	 * Add a box group to the page layout system
