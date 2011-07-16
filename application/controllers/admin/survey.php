8e153d9777b8373c97baec82b3f8bad55859ed86
Shubham Sachdeva <ssachdeva@users.sourceforge.net>
2011-07-16 11:31:12 +0000

    "deactivate" action of Survey controller.
    git-svn-id: file:///Users/Shitiz/Downloads/lssvn/source/limesurvey_ci@10520 b72ed6b6-b9f8-46b5-92b4-906544132732

--- application/controllers/admin/survey.php
+++ application/controllers/admin/survey.php
@@ -91,6 +91,158 @@
         
 	}
     
+    function deactivate($surveyid)
+    {
+        
+        //$postsid=returnglobal('sid');
+        if ($this->input->post('sid'))
+        {
+            $postsid = $this->input->post('sid');
+        }
+        else
+        {
+            $postsid = $surveyid;
+        }
+        $clang = $this->limesurvey_lang;
+        $date = date('YmdHis'); //'Hi' adds 24hours+minutes to name to allow multiple deactiviations in a day
+        $_POST = $this->input->post();
+        if (!isset($_POST['ok']) || !$_POST['ok'])
+        {
+            /**$deactivateoutput .= "<br />\n<div class='messagebox ui-corner-all'>\n";
+            $deactivateoutput .= "<div class='header ui-widget-header'>".$clang->gT("Deactivate Survey")." ($surveyid)</div>\n";
+            $deactivateoutput .= "\t<div class='warningheader'>\n";
+            $deactivateoutput .= $clang->gT("Warning")."<br />".$clang->gT("READ THIS CAREFULLY BEFORE PROCEEDING");
+            $deactivateoutput .= "</div>\n";
+            $deactivateoutput .= "\t".$clang->gT("In an active survey, a table is created to store all the data-entry records.")."\n";
+            $deactivateoutput .= "\t<p>".$clang->gT("When you deactivate a survey all the data entered in the original table will be moved elsewhere, and when you activate the survey again, the table will be empty. You will not be able to access this data using LimeSurvey any more.")."</p>\n";
+            $deactivateoutput .= "\t<p>".$clang->gT("Deactivated survey data can only be accessed by system administrators using a Database data access tool like phpmyadmin. If your survey uses tokens, this table will also be renamed and will only be accessible by system administrators.")."</p>\n";
+            $deactivateoutput .= "\t<p>".$clang->gT("Your responses table will be renamed to:")." {$dbprefix}old_{$_GET['sid']}_{$date}</p>\n";
+            $deactivateoutput .= "\t<p>".$clang->gT("Also you should export your responses before deactivating.")."</p>\n";
+            $deactivateoutput .= "\t<input type='submit' value='".$clang->gT("Deactivate Survey")."' onclick=\"".get2post("$scriptname?action=deactivate&amp;ok=Y&amp;sid={$_GET['sid']}")."\" />\n";
+            $deactivateoutput .= "</div><br />\n"; */
+            
+            $data['clang'] = $clang;
+            $data['surveyid'] = $surveyid;
+            $data['date'] = $date;
+            $data['dbprefix'] = $this->db->dbprefix;
+            $data['step1'] = true;
+            $this->load->view('admin/Survey/deactivateSurvey_view',$data);
+        }
+        
+        else
+        {
+            $this->load->helper('database');
+            //See if there is a tokens table for this survey
+            if (tableExists("tokens_{$postsid}"))
+            {
+                $toldtable="tokens_{$postsid}";
+                $tnewtable="old_tokens_{$postsid}_{$date}";
+                //$tdeactivatequery = db_rename_table(db_table_name_nq($toldtable) ,db_table_name_nq($tnewtable));
+                $tdeactivateresult = db_rename_table($toldtable ,$tnewtable) or die ("Couldn't deactivate tokens table because:<br /><br /><br />Survey was not deactivated either.<br /><br /><a href='".site_url('admin/survey/view/'.$postsid)."'>".$clang->gT("Main Admin Screen")."</a>");
+                
+                if ($this->db->databasetype=='postgres')
+                {
+                    // If you deactivate a postgres table you have to rename the according sequence too and alter the id field to point to the changed sequence
+                    //$deactivatequery = db_rename_table(db_table_name_nq($toldtable).'_tid_seq',db_table_name_nq($tnewtable).'_tid_seq');
+                    $deactivateresult = db_rename_table($toldtable.'_tid_seq',$tnewtable.'_tid_seq') or die ("Could not rename the old sequence for this token table.<br /><br /><a href='".site_url('admin/survey/view/'.$postsid)."'>".$clang->gT("Main Admin Screen")."</a>");
+                    $setsequence="ALTER TABLE ".$this->db->dbprefix.$tnewtable." ALTER COLUMN tid SET DEFAULT nextval('".$this->db->dbprefix.$tnewtable."_tid_seq'::regclass);";
+                    $deactivateresult = db_execute_assosc($setsequence) or die ("Could not alter the field 'tid' to point to the new sequence name for this token table. <br /><br />Survey was not deactivated either.<br /><br /><a href='".site_url('admin/survey/view/'.$postsid)."'>".$clang->gT("Main Admin Screen")."</a>");
+                    $setidx="ALTER INDEX ".$this->db->dbprefix.$toldtable."_idx RENAME TO ".$this->db->dbprefix.$tnewtable."_idx;";
+                    $deactivateresult = db_execute_assosc($setidx) or die ("Could not alter the index for this token table. <br /><br />Survey was not deactivated either.<br /><br /><a href='".site_url('admin/survey/view/'.$postsid)."'>".$clang->gT("Main Admin Screen")."</a>");
+        
+                }
+            }
+        
+            // IF there are any records in the saved_control table related to this survey, they have to be deleted
+            $query = "DELETE FROM ".$this->db->dbprefix."saved_control WHERE sid={$postsid}";
+            $result = $connect->Execute($query);
+        
+            $oldtable=$this->db->dbprefix."survey_{$postsid}";
+            $newtable=$this->db->dbprefix."old_survey_{$postsid}_{$date}";
+        
+            //Update the auto_increment value from the table before renaming
+            $new_autonumber_start=0;
+            $query = "SELECT id FROM $oldtable ORDER BY id desc";
+            $result = db_select_limit_assoc($query, 1,-1, false, false);
+            if ($result)
+            {
+                foreach ($result->result_array() as $row)
+                {
+                    if (strlen($row['id']) > 12) //Handle very large autonumbers (like those using IP prefixes)
+                    {
+                        $part1=substr($row['id'], 0, 12);
+                        $part2len=strlen($row['id'])-12;
+                        $part2=sprintf("%0{$part2len}d", substr($row['id'], 12, strlen($row['id'])-12)+1);
+                        $new_autonumber_start="{$part1}{$part2}";
+                    }
+                    else
+                    {
+                        $new_autonumber_start=$row['id']+1;
+                    }
+                }
+            }
+            $query = "UPDATE ".$this->db->dbprefix."surveys SET autonumber_start=$new_autonumber_start WHERE sid=$surveyid";
+            @$result = db_execute_assosc($query); //Note this won't die if it fails - that's deliberate.
+        
+            //$deactivatequery = db_rename_table($oldtable,$newtable);
+            $deactivateresult = db_rename_table($oldtable,$newtable) or die ("Couldn't make backup of the survey table. Please try again. <br /><br />Survey was not deactivated either.<br /><br /><a href='".site_url('admin/survey/view/'.$postsid)."'>".$clang->gT("Main Admin Screen")."</a>");
+        
+            if ($this->db->databasetype=='postgres')
+            {
+                // If you deactivate a postgres table you have to rename the according sequence too and alter the id field to point to the changed sequence
+                //$deactivatequery = db_rename_table($oldtable.'_id_seq',$newtable.'_id_seq');
+                $deactivateresult = db_rename_table($oldtable.'_id_seq',$newtable.'_id_seq') or die ("Couldn't make backup of the survey table. Please try again. <br /><br />Survey was not deactivated either.<br /><br /><a href='".site_url('admin/survey/view/'.$postsid)."'>".$clang->gT("Main Admin Screen")."</a>");
+                $setsequence="ALTER TABLE $newtable ALTER COLUMN id SET DEFAULT nextval('{$newtable}_id_seq'::regclass);";
+                $deactivateresult = db_execute_assosc($setsequence) or die ("Couldn't make backup of the survey table. Please try again. <br /><br />Survey was not deactivated either.<br /><br /><a href='".site_url('admin/survey/view/'.$postsid)."'>".$clang->gT("Main Admin Screen")."</a>");
+            }
+        
+            //            $dict = NewDataDictionary($connect);
+            //            $dropindexquery=$dict->DropIndexSQL(db_table_name_nq($oldtable).'_idx');
+            //            $connect->Execute($dropindexquery[0]);
+        
+            $deactivatequery = "UPDATE ".$this->db->dbprefix."surveys SET active='N' WHERE sid=$surveyid";
+            $deactivateresult = db_execute_assosc($deactivatequery) or die ("Couldn't deactivate because updating of surveys table failed!<br /><br /><a href='".site_url('admin/survey/view/'.$postsid)."'>Admin</a>");
+            /**
+            $deactivateoutput .= "<br />\n<div class='messagebox ui-corner-all'>\n";
+            $deactivateoutput .= "<div class='header ui-widget-header'>".$clang->gT("Deactivate Survey")." ($surveyid)</div>\n";
+            $deactivateoutput .= "\t<div class='successheader'>".$clang->gT("Survey Has Been Deactivated")."\n";
+            $deactivateoutput .= "</div>\n";
+            $deactivateoutput .= "\t<p>\n";
+            $deactivateoutput .= "\t".$clang->gT("The responses table has been renamed to: ")." $newtable.\n";
+            $deactivateoutput .= "\t".$clang->gT("The responses to this survey are no longer available using LimeSurvey.")."\n";
+            $deactivateoutput .= "\t<p>".$clang->gT("You should note the name of this table in case you need to access this information later.")."</p>\n";
+            if (isset($toldtable) && $toldtable)
+            {
+                $deactivateoutput .= "\t".$clang->gT("The tokens table associated with this survey has been renamed to: ")." $tnewtable.\n";
+            }
+            $deactivateoutput .= "\t<p>".$clang->gT("Note: If you deactivated this survey in error, it is possible to restore this data easily if you do not make any changes to the survey structure. See the LimeSurvey documentation for further details")."</p>";
+            $deactivateoutput .= "</div><br/>&nbsp;\n";
+            */
+            
+            
+                        
+            $pquery = "SELECT savetimings FROM ".$this->db->dbprefix."surveys WHERE sid={$postsid}";
+            $presult=db_execute_assoc($pquery);
+            $prow=$presult->row_array(); //fetch savetimings value
+            if ($prow['savetimings'] == "Y")
+            {
+        		$oldtable=$this->db->dbprefix."survey_{$postsid}_timings";
+        		$newtable=$this->db->dbprefix."old_survey_{$postsid}_timings_{$date}";
+        
+        		//$deactivatequery = db_rename_table($oldtable,$newtable);
+        		$deactivateresult2 = db_rename_table($oldtable,$newtable) or die ("Couldn't make backup of the survey timings table. Please try again.<br /><br />Survey was deactivated.<br /><br /><a href='".site_url('admin/survey/view/'.$postsid)."'>".$clang->gT("Main Admin Screen")."</a>");
+        		$deactivateresult=($deactivateresult && $deactivateresult2);
+            }
+            
+            $data['clang'] = $clang;
+            $data['surveyid'] = $surveyid;
+            $data['tnewtable'] = $tnewtable;
+            $data['toldtable'] = $toldtable;
+            $this->load->view('admin/Survey/deactivateSurvey_view',$data);
+            
+        }
+    }
+    
     function activate($surveyid)
     {
         
---
+++ application/views/admin/Survey/deactivateSurvey_view.php
@@ -0,0 +1,32 @@
+        <?php if (isset($step1))
+        { ?>
+            <br /><div class='messagebox ui-corner-all'>
+            <div class='header ui-widget-header'><?php echo $clang->gT("Deactivate Survey");  echo "($surveyid)" ; ?></div>
+            <div class='warningheader'>
+            <?php echo $clang->gT("Warning"); ?><br /><?php echo $clang->gT("READ THIS CAREFULLY BEFORE PROCEEDING"); ?>
+            </div>
+            <?php echo $clang->gT("In an active survey, a table is created to store all the data-entry records."); ?>
+            <p><?php echo $clang->gT("When you deactivate a survey all the data entered in the original table will be moved elsewhere, and when you activate the survey again, the table will be empty. You will not be able to access this data using LimeSurvey any more."); ?></p>
+            <p><?php echo $clang->gT("Deactivated survey data can only be accessed by system administrators using a Database data access tool like phpmyadmin. If your survey uses tokens, this table will also be renamed and will only be accessible by system administrators."); ?></p>
+            <p><?php echo $clang->gT("Your responses table will be renamed to:"). "{$dbprefix}old_$surveyid_{$date}"; ?></p>
+            <p><?php echo $clang->gT("Also you should export your responses before deactivating."); ?></p>
+            <input type='submit' value='<?php echo $clang->gT("Deactivate Survey"); ?>' onclick="<?php echo get2post(site_url("admin/survey/deactivate/".$surveyid)."?action=deactivate&amp;ok=Y&amp;sid=$surveyid"); ?>" />
+            </div><br />
+        <?php }
+        else
+        { ?>
+            <br /><div class='messagebox ui-corner-all'>
+            <div class='header ui-widget-header'><?php echo $clang->gT("Deactivate Survey"); echo "($surveyid)"; ?></div>
+            <div class='successheader'><?php echo $clang->gT("Survey Has Been Deactivated"); ?>
+            </div>
+            <p>
+            <?php echo $clang->gT("The responses table has been renamed to: ")." ".$newtable; ?>
+            <?php echo $clang->gT("The responses to this survey are no longer available using LimeSurvey."); ?>
+            <p><?php echo $clang->gT("You should note the name of this table in case you need to access this information later."); ?></p>
+            <?php if (isset($toldtable) && $toldtable)
+            { 
+                 echo $clang->gT("The tokens table associated with this survey has been renamed to: ")." $tnewtable";
+            } ?>
+            <p><?php echo $clang->gT("Note: If you deactivated this survey in error, it is possible to restore this data easily if you do not make any changes to the survey structure. See the LimeSurvey documentation for further details"); ?></p>
+            </div><br/>&nbsp;
+        <?php } ?>
