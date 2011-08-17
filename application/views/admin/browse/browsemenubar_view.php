a06d6640175d622cb39d0b2aa039878c153cd5b3
Diogo Gonçalves <dionet@users.sourceforge.net>
2011-08-17 16:10:37 +0000

    Reverted browsemenubar view.
    git-svn-id: file:///Users/Shitiz/Downloads/lssvn/source/limesurvey_ci@10755 b72ed6b6-b9f8-46b5-92b4-906544132732

--- application/views/admin/browse/browsemenubar_view.php
+++ application/views/admin/browse/browsemenubar_view.php
@@ -29,7 +29,7 @@
     <img name='ViewLast' src='<?php echo $imageurl;?>/viewlast.png' alt='<?php echo $clang->gT("Display Last 50 Responses");?>' /></a>
 <?php }
 if (bHasSurveyPermission($surveyid,'responses','create')) { ?>
-    <a href='<?php echo site_url("admin/dataentry/$surveyid/");?>' "<?php echo $clang->gTview("Dataentry Screen for Survey");?>" >
+    <a href='<?php echo site_url("admin/dataentry/view/$surveyid/");?>' "<?php echo $clang->gTview("Dataentry Screen for Survey");?>" >
     <img name='DataEntry' src='<?php echo $imageurl;?>/dataentry.png' alt='<?php echo $clang->gT("Dataentry Screen for Survey");?>' /></a>
 <?php }
 if (bHasSurveyPermission($surveyid,'statistics','read')) { ?>
@@ -42,13 +42,13 @@ if (bHasSurveyPermission($surveyid,'statistics','read')) { ?>
 } ?>
 <img src='<?php echo $imageurl;?>/seperator.gif' alt='' />
 <?php if (bHasSurveyPermission($surveyid,'responses','export')) { ?>
-    <a href='<?php echo site_url("admin/exportresults/$surveyid/");?>' title="<?php echo $clang->gTview("Export results to application");?>">
+    <a href='<?php echo site_url("admin/export/exportresults/$surveyid/");?>' title="<?php echo $clang->gTview("Export results to application");?>">
     <img name='Export' src='<?php echo $imageurl;?>/export.png' alt='<?php echo $clang->gT("Export results to application");?>' /></a>
 
-    <a href='<?php echo site_url("admin/exportspss/$surveyid/");?>' title="<?php echo $clang->gTview("Export results to a SPSS/PASW command file");?>">
+    <a href='<?php echo site_url("admin/export/exportspss/$surveyid/");?>' title="<?php echo $clang->gTview("Export results to a SPSS/PASW command file");?>">
     <img src='<?php echo $imageurl;?>/exportspss.png' alt="<?php echo $clang->gT("Export results to a SPSS/PASW command file");?>" /></a>
 
-    <a href='<?php echo site_url("admin/exportr/$surveyid/");?>' title="<?php echo $clang->gTview("Export results to a R data file");?>" >
+    <a href='<?php echo site_url("admin/export/exportr/$surveyid/");?>' title="<?php echo $clang->gTview("Export results to a R data file");?>" >
     <img src='<?php echo $imageurl;?>/exportr.png' alt='<?php echo $clang->gT("Export results to a R data file");?>' /></a>
 <?php }
 if (bHasSurveyPermission($surveyid,'responses','create'))  
@@ -59,7 +59,7 @@ if (bHasSurveyPermission($surveyid,'responses','create'))
 <img src='<?php echo $imageurl;?>/seperator.gif' alt='' />
 
 <?php if (bHasSurveyPermission($surveyid,'responses','read')) { ?>
-    <a href='<?php echo site_url("admin/saved/$surveyid/");?>' title="<?php echo $clang->gTview("View Saved but not submitted Responses");?>" >
+    <a href='<?php echo site_url("admin/saved/view/$surveyid/");?>' title="<?php echo $clang->gTview("View Saved but not submitted Responses");?>" >
     <img src='<?php echo $imageurl;?>/saved.png' title='' alt='<?php echo $clang->gT("View Saved but not submitted Responses");?>' name='BrowseSaved' /></a>
 <?php }
 if (bHasSurveyPermission($surveyid,'responses','import'))  { ?>   
@@ -67,7 +67,7 @@ if (bHasSurveyPermission($surveyid,'responses','import'))  { ?>
     <img src='<?php echo $imageurl;?>/importvv.png' alt='<?php echo $clang->gT("Import a VV survey file");?>' /></a>
 <?php }
 if (bHasSurveyPermission($surveyid,'responses','export'))  { ?>
-    <a href='<?php echo site_url("admin/vvexport/$surveyid/");?>' title="<?php echo $clang->gTview("Export a VV survey file");?>" >
+    <a href='<?php echo site_url("admin/export/vvexport/$surveyid/");?>' title="<?php echo $clang->gTview("Export a VV survey file");?>" >
     <img src='<?php echo $imageurl;?>/exportvv.png' title='' alt='<?php echo $clang->gT("Export a VV survey file");?>' /></a>
 <?php }
 if (bHasSurveyPermission($surveyid,'responses','delete') && $thissurvey['anonymized'] == 'N' && $thissurvey['tokenanswerspersistence'] == 'Y') { ?>
