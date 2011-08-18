02937db78a50c957a89a6a3069bf3adf38222f9b
Carsten Schmitz <c_schmitz@users.sourceforge.net>
2011-08-18 16:28:31 +0000

    Fixed wrong tablename on survey import
    git-svn-id: file:///Users/Shitiz/Downloads/lssvn/source/limesurvey_ci@10767 b72ed6b6-b9f8-46b5-92b4-906544132732

--- application/models/answers_model.php
+++ application/models/answers_model.php
@@ -1,16 +1,16 @@
 <?php if ( ! defined('BASEPATH')) die('No direct script access allowed');
 
 class Answers_model extends CI_Model {
-	
+
 	function getAllRecords($condition=FALSE)
 	{
 		if ($condition != FALSE)
 		{
-			$this->db->where($condition);	
+			$this->db->where($condition);
 		}
-		
+
 		$data = $this->db->get('answers');
-		
+
 		return $data;
 	}
 
@@ -22,14 +22,14 @@ class Answers_model extends CI_Model {
 		}
 		if ($condition != FALSE)
 		{
-			$this->db->where($condition);	
+			$this->db->where($condition);
 		}
-		
+
 		$data = $this->db->get('answers');
-		
+
 		return $data;
 	}
-    
+
     function updateSortOrder($qid,$lang)
     {
         $this->db->select('qid, code, sortorder');
@@ -37,9 +37,9 @@ class Answers_model extends CI_Model {
         $this->db->where('language',$lang);
         $this->db->order_by("sortorder","asc");
         $data = $this->db->get('answers');
-		
+
 		$position=0;
-        
+
         foreach($data->result_array() as $row)
         {
             $datatoupdate = array('sortorder' => $position);
@@ -50,7 +50,7 @@ class Answers_model extends CI_Model {
             $position++;
         }
     }
-    
+
     function getAnswerCode($qid,$code,$lang)
     {
         $this->db->select('code, answer');
@@ -59,41 +59,41 @@ class Answers_model extends CI_Model {
         $this->db->where('scale_id',0);
         $this->db->where('language',$lang);
         $data = $this->db->get('answers');
-		
+
 		return $data;
     }
-	
+
 	function oldNewInsertansTags($newsid,$oldsid)
 	{
 		$sql = "SELECT a.qid, a.language, a.code, a.answer from ".$this->db->dbprefix('answers')." as a INNER JOIN ".$this->db->dbprefix('questions')." as b ON a.qid=b.qid WHERE b.sid=".$newsid." AND a.answer LIKE '%{INSERTANS:".$oldsid."X%'";
     	return $this->db->query($sql);
 	}
-    
+
     function getCountOfCode($qid,$language)
     {
         $data = $this->db->query("SELECT count(code) AS codecount FROM ".$this->db->dbprefix('answers')." WHERE qid={$qid} AND language='{$language}'");
         return $data;
     }
 
-	
+
 	function update($data, $condition=FALSE)
 	{
-	
+
 		if ($condition != FALSE)
 		{
-			$this->db->where($condition);	
+			$this->db->where($condition);
 		}
-		
+
 		$this->db->update('answers', $data);
-		
+
 	}
-    
+
     function insertRecords($data)
     {
-        
-        return $this->db->insert('groups',$data);
-    }
-    
 
-    
+        return $this->db->insert('answers',$data);
+    }
+
+
+
 }
