a47d4c4a4f309c2f062fd0588509b8b8fac2dc02
Carsten Schmitz <c_schmitz@users.sourceforge.net>
2011-08-18 16:25:09 +0000

    Fixed issue: View not found on Linux systems
    git-svn-id: file:///Users/Shitiz/Downloads/lssvn/source/limesurvey_ci@10766 b72ed6b6-b9f8-46b5-92b4-906544132732

--- application/views/admin/Survey/superview/superGeneralNewSurvey_view.php
+++ application/views/admin/Survey/superview/superGeneralNewSurvey_view.php
@@ -1,7 +1,7 @@
-<?php 
+<?php
 $data['clang'] = $clang;
 $data['action'] = $action;
-$this->load->view('admin/survey/subview/tab_view',$data); ?>
+$this->load->view('admin/Survey/subview/tab_view',$data); ?>
 <label for='language' title='<?php echo $clang->gT("This is the base language of your survey and it can't be changed later. You can add more languages after you have created the survey."); ?>'><span class='annotationasterisk'>*</span><?php echo $clang->gT("Base language:"); ?></label>
 <select id='language' name='language'>
         <?php foreach (getLanguageData () as $langkey2 => $langname) { ?>
@@ -16,5 +16,5 @@ $this->load->view('admin/survey/subview/tab_view',$data); ?>
 </select>
 
 <?php $data['owner'] = $owner;
-$this->load->view('admin/survey/subview/tabGeneralNewSurvey_view',$data);
+$this->load->view('admin/Survey/subview/tabGeneralNewSurvey_view',$data);
 ?>
