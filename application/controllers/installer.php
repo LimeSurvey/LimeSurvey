ca2cce698bd08b63f6e841d5ccd2547975a5359f
Tim Klingenberg <mot3@users.sourceforge.net>
2011-09-03 16:26:44 +0000

    Installer: Introducing DbTasks prototype.
    git-svn-id: file:///Users/Shitiz/Downloads/lssvn/source/limesurvey_ci@10943 b72ed6b6-b9f8-46b5-92b4-906544132732

--- application/controllers/installer.php
+++ application/controllers/installer.php
@@ -26,6 +26,13 @@
  */
 class Installer extends CI_Controller {
     /**
+     * dbTasks object used in installation.
+     * @var LS_Installer_DbTasks
+     */
+    private $dbTasks;
+
+
+    /**
      * Installer::__construct()
      * Constructor
      * @return
@@ -34,6 +41,11 @@ class Installer extends CI_Controller {
 	{
         parent::__construct();
         self::_checkInstallation();
+        //need to write unique encryption key before we can use session data.
+        self::_writeConfigfile();
+        require_once(APPPATH.'libraries/LS/LS.php');
+        $this->dbTasks = new LS_Installer_DbTasks;
+        $this->dbTasks->test();
 	}
 
 	/**
