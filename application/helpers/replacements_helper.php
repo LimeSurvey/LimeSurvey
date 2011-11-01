79d7515fc0f1521394ec53eaa1bb1a840f486238
Denis Chenu <shnoulle@users.sourceforge.net>
2011-11-01 18:13:40 +0000

    Dev : fixed issue : clear all button Dev TODO : replace by jquery-ui confirm
    git-svn-id: file:///Users/Shitiz/Downloads/lssvn/source/limesurvey_ci@11310 b72ed6b6-b9f8-46b5-92b4-906544132732

--- application/helpers/replacements_helper.php
+++ application/helpers/replacements_helper.php
@@ -327,7 +327,7 @@ function templatereplace($line, $replacements=array(),&$redata=array(), $debugSr
 
     if (isset($surveyid)) {
         $_clearall = "<input type='button' name='clearallbtn' value='" . $clang->gT("Exit and Clear Survey") . "' class='clearall' "
-        . "onclick=\"if (confirm('" . $clang->gT("Are you sure you want to clear all your responses?", 'js') . "')) {\nwindow.open('".$CI->config->item('publicurl')."/index.php?sid=$surveyid&amp;move=clearall&amp;lang=" . $s_lang;
+        . "onclick=\"if (confirm('" . $clang->gT("Are you sure you want to clear all your responses?", 'js') . "')) {\nwindow.open('".site_url($surveyid)."?move=clearall&amp;lang=" . $s_lang;
         if (returnglobal('token'))
         {
             $_clearall .= "&amp;token=" . urlencode(trim(sanitize_token(strip_tags(returnglobal('token')))));
