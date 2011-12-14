f3fc339931d55184a6184b8f09b5e95eaca5b155
Aaron Schmitz <ajschmitz@users.sourceforge.net>
2011-12-14 01:08:51 +0000

    Added Templates_rights.php for GCI student Yaxar.
    git-svn-id: file:///Users/Shitiz/Downloads/lssvn/source/limesurvey_yii@11655 b72ed6b6-b9f8-46b5-92b4-906544132732

---
+++ application/models/Templates_rights.php
@@ -0,0 +1,83 @@
+<?php if ( ! defined('BASEPATH')) die('No direct script access allowed');
+
+class Templates_rights extends CActiveRecord {
+public static function model()
+	{
+		return parent::model(__CLASS__);
+	}
+
+	/**
+	 * Returns the setting's table name to be used by the model
+	 *
+	 * @access public
+	 * @return string
+	 */
+	public function tableName()
+	{
+		return '{{templates_rights}}';
+	}
+	public function primaryKey()
+	{
+		return 'uid';
+	}
+	
+	
+	function getAllRecords($condition=FALSE)
+	{
+		if ($condition != FALSE)
+		{
+			$this->db->where($condition);	
+		}
+		
+		$data = $this->db->get('templates_rights');
+		
+		return $data;
+	}
+
+	function getSomeRecords($fields,$condition=FALSE)
+	{
+		foreach ($fields as $field)
+		{
+			$this->db->select($field);
+		}
+		if ($condition != FALSE)
+		{
+			$this->db->where($condition);	
+		}
+		
+		$data = $this->db->get('templates_rights');
+		
+		return $data;
+	}
+	
+	function insert($values)
+	{
+		return (bool) $this->db->insert('templates_rights', $values);
+	}
+	
+	function update($what, $where)
+	{
+		$this->db->where($where);
+		return (bool) $this->db->insert('templates_rights', $what);
+	}
+
+	/**
+	 * Returns the static model of Settings table
+	 *
+	 * @static
+	 * @access public
+	 * @return CActiveRecord
+	 */
+	
+	
+    function insertRecords($data)
+    {
+    	$tablename = $this->tableName(); 
+        //$ans = new self;
+	//	//foreach ($data as $k => $v)
+	//		$ans->$k = $v;
+		//return $ans->save();
+		return Yii::app()->db->createCommand()->insert('{{templates_rights}}', $data);
+    }
+
+}
