f24c733350071fcf471a699b029b079ac262488f
Gaurav Narula <gnarula@users.sourceforge.net>
2011-12-22 20:57:57 +0000

    Commit for GCI task #7232223 Port the frontend printanswers controller to the Yii framework by Gaurav Narula
    git-svn-id: file:///Users/Shitiz/Downloads/lssvn/source/limesurvey_yii@11738 b72ed6b6-b9f8-46b5-92b4-906544132732

--- application/controllers/printanswers.php
+++ application/controllers/printanswers.php
@@ -21,7 +21,7 @@
  * @version $Id$
  * @access public
  */
-class printanswers extends LSCI_Controller {
+class printanswers extends LSYii_Controller {
 
 	/**
 	 * printanswers::__construct()
@@ -45,57 +45,62 @@ class printanswers extends LSCI_Controller {
 
         global $siteadminname, $siteadminemail;
 
-        if($this->config->item('usepdfexport'))
+        if(Yii::app()->getConfig('usepdfexport'))
         {
-            //require_once($pdfexportdir."/extensiontcpdf.php");
-            $this->load->library('admin/pdf');
+            Yii::import('application.libraries.admin.pdf');
         }
 
         //if (!isset($surveyid)) {$surveyid=returnglobal('sid');}
         //else {
             //This next line ensures that the $surveyid value is never anything but a number.
-        $surveyid=(int)($surveyid);
+        $surveyid = (int)($surveyid);
         //}
-        $this->load->helper('database');
+        Yii::app()->loadHelper('database');
         //$clang = $this->limesurvey_lang;
 
         // Compute the Session name
         // Session name is based:
         // * on this specific limesurvey installation (Value SessionName in DB)
         // * on the surveyid (from Get or Post param). If no surveyid is given we are on the public surveys portal
-        $usquery = "SELECT stg_value FROM ".$this->db->dbprefix."settings_global where stg_name='SessionName'";
-        $usresult = db_execute_assoc($usquery,'',true);          //Checked
-        if ($usresult->num_rows > 0)
+        $usresult = Settings_global::model()->find('stg_name = "SessionName"');
+        $session = new CHttpSession;
+        if ($usresult != null)
         {
-            $usrow = $usresult->row_array();
-            $stg_SessionName=$usrow['stg_value'];
+            $stg_SessionName = $usrow->stg_value;
             if ($surveyid)
             {
-                @session_name($stg_SessionName.'-runtime-'.$surveyid);
+                $session->sessionName = $stg_SessionName.'-runtime-'.$surveyid;
             }
             else
             {
-                @session_name($stg_SessionName.'-runtime-publicportal');
+                $session->sessionName = $stg_SessionName.'-runtime-publicportal';
             }
         }
         else
         {
-            session_name("LimeSurveyRuntime-$surveyid");
+            $session->sessionName = "LimeSurveyRuntime-$surveyid";
         }
-        session_set_cookie_params(0,$this->config->item('relativeurl').'/');
-        @session_start();
+        $session->setCookieParams(array('lifetime' => 0, 'path' => Yii::app()->getConfig('relativeurl').'/'));
+        $session->open;
 
-        if (isset($_SESSION['sid'])) {$surveyid=$_SESSION['sid'];}  else show_error('Invalid survey/session');
+        if (isset($session['sid']))
+        {
+            $surveyid = $session['sid'];
+        }
+        else
+        {
+            show_error('Invalid survey/session');
+        }
 
         //Debut session time out
-        if (!isset($_SESSION['finished']) || !isset($_SESSION['srid']))
+        if (!isset($session['finished']) || !isset($session['srid']))
         // Argh ... a session time out! RUN!
         //display "sorry but your session has expired"
         {
             //require_once($rootdir.'/classes/core/language.php');
             $baselang = GetBaseLanguageFromSurveyID($surveyid);
-            $this->load->library('Limesurvey_lang',array($baselang));
-            $clang = $this->limesurvey_lang; //new limesurvey_lang($baselang);
+            Yii::import('application.libraries.Limesurvey_lang', true);
+            $clang = new Limesurvey_lang(array('langcode' => $baselang));
             //A nice exit
 
             sendcacheheaders();
@@ -111,50 +116,26 @@ class printanswers extends LSCI_Controller {
             echo templatereplace(file_get_contents(sGetTemplatePath(validate_templatedir("default"))."/endpage.pstpl"),array(),array());
             doFooter();
             exit;
-        };
+        }
         //Fin session time out
 
-        $id=$_SESSION['srid']; //I want to see the answers with this id
-        $clang = $_SESSION['s_lang'];
-
-        //A little bit of debug to see in the noodles plate
-        /*if ($debug==2)
-         {
-         echo "MonSurveyID $surveyid et ma langue ". $_SESSION['s_lang']. " et SRID = ". $_SESSION['srid'] ."<br />";
-         echo "session id".session_id()." \n"."<br />";
-
-         echo //"secanswer ". $_SESSION['secanswer']
-         "oldsid ". $_SESSION['oldsid']."<br />"
-         ."step ". $_SESSION['step']."<br />"
-         ."scid ". $_SESSION['scid']
-         ."srid ". $_SESSION['srid']."<br />"
-         ."datestamp ". $_SESSION['datestamp']."<br />"
-         ."insertarray ". $_SESSION['insertarray']."<br />"
-         ."fieldarray ". $_SESSION['fieldarray']."<br />";
-         ."holdname". $_SESSION['holdname'];
-
-         print " limit ". $limit."<br />"; //afficher les 50 dernieres reponses par ex. (pas necessaire)
-         print " surveyid ".$surveyid."<br />"; //sid
-         print " id ".$id."<br />"; //identifiant de la reponses
-         print " order ". $order ."<br />"; //ordre de tri (pas necessaire)
-         print " this survey ". $thissurvey['tablename'];
-         };   */
+        $id = $session['srid']; //I want to see the answers with this id
+        $clang = $session['s_lang'];
 
         //Ensure script is not run directly, avoid path disclosure
         //if (!isset($rootdir) || isset($_REQUEST['$rootdir'])) {die( "browse - Cannot run this script directly");}
 
         // Set the language for dispay
         //require_once($rootdir.'/classes/core/language.php');  // has been secured
-        if (isset($_SESSION['s_lang']))
+        if (isset($session['s_lang']))
         {
 
-            $clang = SetSurveyLanguage( $surveyid, $_SESSION['s_lang']);
-
-            $language = $_SESSION['s_lang'];
-
-        } else {
-
-        $language = GetBaseLanguageFromSurveyID($surveyid);
+            $clang = SetSurveyLanguage( $surveyid, $session['s_lang']);
+            $language = $session['s_lang'];
+        }
+        else
+        {
+            $language = GetBaseLanguageFromSurveyID($surveyid);
             $clang = SetSurveyLanguage( $surveyid, $language);
         }
 
@@ -164,31 +145,31 @@ class printanswers extends LSCI_Controller {
         //SET THE TEMPLATE DIRECTORY
         if (!isset($thissurvey['templatedir']) || !$thissurvey['templatedir'])
         {
-            $thistpl=validate_templatedir("default");
+            $thistpl = validate_templatedir("default");
         }
         else
         {
-            $thistpl=validate_templatedir($thissurvey['templatedir']);
+            $thistpl = validate_templatedir($thissurvey['templatedir']);
         }
 
-        if ($thissurvey['printanswers']=='N') die();  //Die quietly if print answers is not permitted
-
-
-
-
-
+        if ($thissurvey['printanswers'] == 'N')
+        {
+            die();  //Die quietly if print answers is not permitted
+        }
 
         //CHECK IF SURVEY IS ACTIVATED AND EXISTS
-        $actquery = "SELECT * FROM ".$this->db->dbprefix."surveys as a inner join ".$this->db->dbprefix."surveys_languagesettings as b on (b.surveyls_survey_id=a.sid and b.surveyls_language=a.language) WHERE a.sid=$surveyid";
-
-        $actresult = db_execute_assoc($actquery);    //Checked
-        $actcount = $actresult->num_rows();
+        $actquery = new CDbCriteria;
+        $actquery->join = 'INNER JOIN  {{surveys_languagesettings}} as b on (b.surveyls_surveyid = t.sid and b.surveyls_language = t.language)';
+        $actquery->condition = 't.sid = :sid';
+        $actquery->params(array(':sid' => $surveyid));
+        $actresult = Surveys::model()->findAll($actquery);
+        $actcount = count($actresult);
         if ($actcount > 0)
         {
-            foreach ($actresult->result_array() as $actrow)
+            foreach ($actresult as $actrow)
             {
-                $surveytable = $this->db->dbprefix."survey_".$actrow['sid'];
-                $surveyname = "{$actrow['surveyls_title']}";
+                $surveytable = $this->db->dbprefix."survey_".$actrow->sid;
+                $surveyname = "{$actrow->surveyls_title}";
             }
         }
 
@@ -196,19 +177,20 @@ class printanswers extends LSCI_Controller {
         //OK. IF WE GOT THIS FAR, THEN THE SURVEY EXISTS AND IT IS ACTIVE, SO LETS GET TO WORK.
         //SHOW HEADER
         $printoutput = '';
-        if($this->config->item('usepdfexport') == 1)
+        if(Yii::app()->getConfig('usepdfexport') == 1)
         {
-            $printoutput .= "<form action='".site_url('printanswers/view/'.$surveyid.'/pdf')."' method='post'>\n<center><input type='submit' value='".$clang->gT("PDF export")."'id=\"exportbutton\"/><input type='hidden' name='printableexport' /></center></form>";
+            $printoutput .= "<form action='".Yii::app()->createUrl('printanswers/view/'.$surveyid.'/pdf')."' method='post'>\n<center><input type='submit' value='".$clang->gT("PDF export")."'id=\"exportbutton\"/><input type='hidden' name='printableexport' /></center></form>";
         }
-        if($printableexport=='pdf')
+        if($printableexport == 'pdf')
         {
-            //$pdf = new PDF($pdforientation);
-            require ($this->config->item('rootdir').'/application/config/tcpdf_config_ci.php');
-            $this->_config = $tcpdf;
-            //$this->pdf->SetFont($pdfdefaultfont,'',$pdffontsize);
-            $this->pdf->AddPage();
+            require (Yii::app()->getConfig('rootdir').'/application/config/tcpdf_config_ci.php');
+            Yii::import('application.libraries.admin.pdf', true);
+            $pdf = new pdf();
+            $pdf->_config = $tcpdf;
+            //$pdf->SetFont($pdfdefaultfont,'',$pdffontsize);
+            $pdf->AddPage();
             //$pdf->titleintopdf($clang->gT("Survey name (ID)",'unescaped').": {$surveyname} ({$surveyid})");
-            $this->pdf->SetTitle($clang->gT("Survey name (ID)",'unescaped').": {$surveyname} ({$surveyid})");
+            $pdf->SetTitle($clang->gT("Survey name (ID)",'unescaped').": {$surveyname} ({$surveyid})");
         }
         $printoutput .= "\t<div class='printouttitle'><strong>".$clang->gT("Survey name (ID):")."</strong> $surveyname ($surveyid)</div><p>&nbsp;\n";
 
@@ -216,7 +198,7 @@ class printanswers extends LSCI_Controller {
         // Since all data are loaded, and don't need JavaScript, pretend all from Group 1
         LimeExpressionManager::StartProcessingGroup(1,($thissurvey['anonymized']!="N"),$surveyid);
 
-        $aFullResponseTable=aGetFullResponseTable($surveyid,$id,$language,true);
+        $aFullResponseTable = aGetFullResponseTable($surveyid,$id,$language,true);
 
         //Get the fieldmap @TODO: do we need to filter out some fields?
         unset ($aFullResponseTable['id']);
@@ -227,22 +209,22 @@ class printanswers extends LSCI_Controller {
         unset ($aFullResponseTable['startdate']);
 
         $printoutput .= "<table class='printouttable' >\n";
-        if($printableexport=='pdf')
+        if($printableexport == 'pdf')
         {
-            $this->pdf->intopdf($clang->gT("Question",'unescaped').": ".$clang->gT("Your answer",'unescaped'));
+            $pdf->intopdf($clang->gT("Question",'unescaped').": ".$clang->gT("Your answer",'unescaped'));
         }
 
         $oldgid = 0;
         $oldqid = 0;
         foreach ($aFullResponseTable as $sFieldname=>$fname)
         {
-            if (substr($sFieldname,0,4)=='gid_')
+            if (substr($sFieldname,0,4) == 'gid_')
             {
 
         	    if($printableexport)
         	    {
-        		    $this->pdf->intopdf(FlattenText($fname[0],false,true));
-        		    $this->pdf->ln(2);
+        		    $pdf->intopdf(FlattenText($fname[0],false,true));
+        		    $pdf->ln(2);
                 }
                 else
                 {
@@ -251,10 +233,10 @@ class printanswers extends LSCI_Controller {
         	}
             elseif (substr($sFieldname,0,4)=='qid_')
             {
-                if($printableexport=='pdf')
+                if($printableexport == 'pdf')
                 {
-                    $this->pdf->intopdf(FlattenText($fname[0].$fname[1],false,true).": ".$fname[2]);
-                    $this->pdf->ln(2);
+                    $pdf->intopdf(FlattenText($fname[0].$fname[1],false,true).": ".$fname[2]);
+                    $pdf->ln(2);
                 }
                 else
                 {
@@ -263,27 +245,28 @@ class printanswers extends LSCI_Controller {
             }
             else
             {
-                if($printableexport=='pdf')
+                if($printableexport == 'pdf')
                 {
-                    $this->pdf->intopdf(FlattenText($fname[0].$fname[1],false,true).": ".$fname[2]);
-                    $this->pdf->ln(2);
+                    $pdf->intopdf(FlattenText($fname[0].$fname[1],false,true).": ".$fname[2]);
+                    $pdf->ln(2);
                 }
-                    else
-                    {
+                else
+                {
                     $printoutput .= "\t<tr class='printanswersquestion'><td>{$fname[0]} {$fname[1]}</td><td class='printanswersanswertext'>{$fname[2]}</td></tr>";
                 }
             }
         }
 
         $printoutput .= "</table>\n";
-        if($printableexport=='pdf')
+
+        if($printableexport == 'pdf')
         {
 
             header("Pragma: public");
             header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
 
-            $sExportFileName=sanitize_filename($surveyname);
-   			$this->pdf->Output($sExportFileName."-".$surveyid.".pdf","D");
+            $sExportFileName = sanitize_filename($surveyname);
+   			$pdf->Output($sExportFileName."-".$surveyid.".pdf","D");
         }
 
         //Display the page with user answers
@@ -301,8 +284,5 @@ class printanswers extends LSCI_Controller {
 
         LimeExpressionManager::FinishProcessingGroup();
         LimeExpressionManager::FinishProcessingPage();
-
     }
-
-
 }
