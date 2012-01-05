a4079c615049b2b889d8622030151ba68e103637
Aaron Schmitz <ajschmitz@users.sourceforge.net>
2012-01-05 01:09:20 +0000

    Fix bug where sending email to added users always caused an "invalid address" artifact to show at the top of the screen and the from address to not be properly loaded.
    git-svn-id: file:///Users/Shitiz/Downloads/lssvn/source/limesurvey_yii@11905 b72ed6b6-b9f8-46b5-92b4-906544132732

--- application/controllers/admin/useraction.php
+++ application/controllers/admin/useraction.php
@@ -137,6 +137,7 @@ class UserAction extends Survey_Common_Action
                 $body .= "<a href='" . $this->getController()->createUrl("/admin") . "'>" . $clang->gT("Click here to log in.") . "</a><br /><br />\n";
                 $body .= sprintf($clang->gT('If you have any questions regarding this mail please do not hesitate to contact the site administrator at %s. Thank you!'), Yii::app()->getConfig("siteadminemail")) . "<br />\n";
 
+                Yii::app()->loadConfig('email');
                 $subject = sprintf($clang->gT("User registration at '%s'", "unescaped"), Yii::app()->getConfig("sitename"));
                 $to = $new_user . " <$new_email>";
                 $from = Yii::app()->getConfig("siteadminname") . " <" . Yii::app()->getConfig("siteadminemail") . ">";
