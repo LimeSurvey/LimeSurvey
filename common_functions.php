a63135e3da24039eb8f36ecabc5c83c65a356f21
c_schmitz <c_schmitz@b72ed6b6-b9f8-46b5-92b4-906544132732>
2012-02-05 15:06:41 +0000

    Fixed issue #5592: Wrong answer mapping for dual scale matrix in detailed admin notification email
    git-svn-id: file:///Users/Shitiz/Downloads/lssvn/source/limesurvey@12355 b72ed6b6-b9f8-46b5-92b4-906544132732

--- common_functions.php
+++ common_functions.php
@@ -1918,7 +1918,11 @@ function getextendedanswer($fieldcode, $value, $format='', $dateformatphp='d.m.Y
             case "F":
             case "H":
             case "1":
-                $query = "SELECT answer FROM ".db_table_name('answers')." WHERE qid={$fields['qid']} AND code='".$connect->escape($value)."' AND scale_id={$fields['scale_id']} AND language='".$s_lang."'";
+                $query = "SELECT answer FROM ".db_table_name('answers')." WHERE qid={$fields['qid']} AND code='".$connect->escape($value)."' AND language='".$s_lang."'";
+                if (isset($fields['scale_id']))
+                {
+                    $query.=" AND scale_id={$fields['scale_id']}";
+                }
                 $result = db_execute_assoc($query) or safe_die ("Couldn't get answer type F/H - getextendedanswer() in common.php");   //Checked
                 while($row=$result->FetchRow())
                 {
