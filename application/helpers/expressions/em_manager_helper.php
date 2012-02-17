52fccc5dd14e6d7a3eeaf4997f6284268fcdef77
Thomas White <tmswhite@users.sourceforge.net>
2012-02-17 11:16:34 -0500

    Fixed issue #05823: Confusing message to users for questions with regular expression validation

--- application/helpers/expressions/em_manager_helper.php
+++ application/helpers/expressions/em_manager_helper.php
@@ -1581,36 +1581,37 @@ class LimeExpressionManager {
             if ($preg!='')
             {
                 // do string replacement here so that curly braces within the regular expression don't trigger an EM error
-                $qtips['regex_validation']=sprintf($this->gT('Each answer must conform to this regular expression: %s'), str_replace(array('{','}'),array('{ ',' }'), $preg));
+//                $qtips['regex_validation']=sprintf($this->gT('Each answer must conform to this regular expression: %s'), str_replace(array('{','}'),array('{ ',' }'), $preg));
+                $qtips['regex_validation']=$this->gT('Please check the format of your answer.');
             }
-
+            
             if ($em_validation_sq!='')
             {
                 if ($em_validation_sq_tip =='')
                 {
-                    $stringToParse = htmlspecialchars_decode($em_validation_sq,ENT_QUOTES);
-                    $gseq = $this->questionId2groupSeq[$qinfo['qid']];
-                    $result = $this->em->ProcessBooleanExpression($stringToParse,$gseq,  $qinfo['qseq']);
-                    $_validation_tip = $this->em->GetPrettyPrintString();
-                    $qtips['sq_fn_validation']=sprintf($this->gT('Each answer must conform to this expression: %s'),$_validation_tip);
+//                    $stringToParse = htmlspecialchars_decode($em_validation_sq,ENT_QUOTES);
+//                    $gseq = $this->questionId2groupSeq[$qinfo['qid']];
+//                    $result = $this->em->ProcessBooleanExpression($stringToParse,$gseq,  $qinfo['qseq']);
+//                    $_validation_tip = $this->em->GetPrettyPrintString();
+//                    $qtips['sq_fn_validation']=sprintf($this->gT('Each answer must conform to this expression: %s'),$_validation_tip);
                 }
                 else
                 {
                     $qtips['sq_fn_validation']=$em_validation_sq_tip;
                 }
-
+                
             }
-
+            
             // em_validation_q - whole-question validation equation
             if ($em_validation_q!='')
             {
                 if ($em_validation_q_tip =='')
                 {
-                    $stringToParse = htmlspecialchars_decode($em_validation_q,ENT_QUOTES);
-                    $gseq = $this->questionId2groupSeq[$qinfo['qid']];
-                    $result = $this->em->ProcessBooleanExpression($stringToParse,$gseq,  $qinfo['qseq']);
-                    $_validation_tip = $this->em->GetPrettyPrintString();
-                    $qtips['q_fn_validation']=sprintf($this->gT('The question must conform to this expression: %s'), $_validation_tip);
+//                    $stringToParse = htmlspecialchars_decode($em_validation_q,ENT_QUOTES);
+//                    $gseq = $this->questionId2groupSeq[$qinfo['qid']];
+//                    $result = $this->em->ProcessBooleanExpression($stringToParse,$gseq,  $qinfo['qseq']);
+//                    $_validation_tip = $this->em->GetPrettyPrintString();
+//                    $qtips['q_fn_validation']=sprintf($this->gT('The question must conform to this expression: %s'), $_validation_tip);
                 }
                 else
                 {
