45d5057194ed1333cbb733e0133f931797dbca60
Thomas White <tmswhite@users.sourceforge.net>
2012-02-22 23:19:38 -0500

    Fixed issue #05793: Javascript function for "please use navigation button" don't work Dev Further fix - reloading page (e.g. via F5) now works for all pages, including first and last pages

--- application/helpers/frontend_helper.php
+++ application/helpers/frontend_helper.php
@@ -2775,6 +2775,10 @@ function display_first_page() {
     if (isset($loadsecurity)) {
         echo "\n<input type='hidden' name='loadsecurity' value='$loadsecurity' id='loadsecurity' />\n";
     }
+    $_SESSION['LEMpostKey'] = mt_rand();
+    echo "<input type='hidden' name='LEMpostKey' value='{$_SESSION['LEMpostKey']}' id='LEMpostKey' />\n";
+    echo "<input type='hidden' name='thisstep' id='thisstep' value='0' />\n";
+    
     echo "\n</form>\n";
     echo templatereplace(file_get_contents("$thistpl/endpage.pstpl"),array(),$redata,'frontend_helper[2782]');
 
