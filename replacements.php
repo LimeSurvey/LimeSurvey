8372ffa2d5fbc8932e41233941c36b04f99b8977
Thomas White <tmswhite@users.sourceforge.net>
2012-03-12 10:40:03 -0400

    Dev upgraded Goggle Analytics JavaScript to use recommended async code

--- replacements.php
+++ replacements.php
@@ -587,14 +587,15 @@ function templatereplace($line, $replacements=array(), $anonymized=false, $quest
                 // Default Google Tracking
                 $_googleAnalyticsJavaScript = <<<EOD
 <script type="text/javascript">
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-</script>
-<script type="text/javascript">
-try{
-var pageTracker = _gat._getTracker("$_googleAnalyticsAPIKey");
-pageTracker._trackPageview();
-} catch(err) { }
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', '$_googleAnalyticsAPIKey']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
 </script>
 EOD;
                 break;
@@ -623,14 +624,15 @@ EOD;
                 $_trackURL = htmlspecialchars($thissurvey['name'] . '-[' . $surveyid . ']/[' . $gseq . ']-' . $_groupname);
                 $_googleAnalyticsJavaScript = <<<EOD
 <script type="text/javascript">
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-</script>
-<script type="text/javascript">
-try{
-var pageTracker = _gat._getTracker("$_googleAnalyticsAPIKey");
-pageTracker._trackPageview("$_trackURL");
-} catch(err) { }
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', '$_googleAnalyticsAPIKey']);
+  _gaq.push(['_trackPageview','$_trackURL']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
 </script>
 EOD;
                 break;
