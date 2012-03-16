98fa6e80370aec809eb0f4e5aa35b03b985bbb4e
Thomas White <tmswhite@users.sourceforge.net>
2012-03-16 12:00:00 -0400

    Dev Added MIT-licensed Excel Reader class from http://code.google.com/p/php-excel-reader/

---
+++ admin/classes/excel/example.php
@@ -0,0 +1,41 @@
+<?php
+error_reporting(E_ALL ^ E_NOTICE);
+require_once 'excel_reader2.php';
+$data = new Spreadsheet_Excel_Reader("example.xls");
+
+?>
+<html>
+<head>
+<style>
+table.excel {
+	border-style:ridge;
+	border-width:1;
+	border-collapse:collapse;
+	font-family:sans-serif;
+	font-size:12px;
+}
+table.excel thead th, table.excel tbody th {
+	background:#CCCCCC;
+	border-style:ridge;
+	border-width:1;
+	text-align: center;
+	vertical-align:bottom;
+}
+table.excel tbody th {
+	text-align:center;
+	width:20px;
+}
+table.excel tbody td {
+	vertical-align:bottom;
+}
+table.excel tbody td {
+    padding: 0 3px;
+	border: 1px solid #EEEEEE;
+}
+</style>
+</head>
+
+<body>
+<?php echo $data->dump(true,true); ?>
+</body>
+</html>
