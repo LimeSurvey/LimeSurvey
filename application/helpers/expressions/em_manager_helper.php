3d9e32b9fb990a22cbdf305a5c17487edc8e4ae4
Thomas White <tmswhite@users.sourceforge.net>
2012-04-16 12:02:49 -0400

    Fixed issue #06007: Token variables resolving as 'undefined variable' after Version 1.91+ Build 11026 Dev This is actually a bug on the conditions editor - it should generate {TOKEN}, not {TOKEN:TOKEN}.  But to support backwards compatability,EM is letting TOKEN:TOKEN be an alias for TOKEN

--- application/helpers/expressions/em_manager_helper.php
+++ application/helpers/expressions/em_manager_helper.php
@@ -2356,6 +2356,12 @@
             {
                 //Gather survey data for tokenised surveys, for use in presenting questions
                 $_SESSION[$this->sessid]['thistoken']=getTokenData($surveyid, $_SESSION[$this->sessid]['token']);
+                $this->knownVars['TOKEN:TOKEN'] = array(
+                    'code'=>$_SESSION['token'],
+                    'jsName_on'=>'',
+                    'jsName'=>'',
+                    'readWrite'=>'N',
+                );
             }
             if (isset($_SESSION[$this->sessid]['thistoken']))
             {
@@ -2392,6 +2398,7 @@
                 $this->knownVars['TOKEN:LASTNAME'] = $blankVal;
                 $this->knownVars['TOKEN:EMAIL'] = $blankVal;
                 $this->knownVars['TOKEN:USESLEFT'] = $blankVal;
+                $this->knownVars['TOKEN:TOKEN'] = $blankVal;
                 for ($i=1;$i<=100;++$i) // TODO - is there a way to know  how many attributes are set?  Looks like max is 100
                 {
                     $this->knownVars['TOKEN:ATTRIBUTE_' . $i] = $blankVal;
