12c1af94fd82e42a1494ee6947b61daeee76aa9d
jcleeland <jason@cleeland.org>
2012-04-24 14:00:43 +1000

    Fixed issue: login screens and messages ugly Dev: put the login screens (and error screen) into the standard messagebox class, and header class for consistent look and feel (and less ugliness)

--- application/views/admin/authentication/error.php
+++ application/views/admin/authentication/error.php
@@ -1,6 +1,8 @@
-<p>
-    <?php echo $errormsg; ?><br />
-    <?php echo $maxattempts; ?><br /><br />
+<div class='messagebox ui-corner-all'>
+    <div class='header ui-widget-header'>
+        <?php echo $errormsg; ?><br /><br />
+        <?php echo $maxattempts; ?>
+    </div><br />
     <a href='<?php echo $this->createUrl("/admin/authentication/login"); ?>'><?php $clang->eT("Try again"); ?></a><br />
     <a href='<?php echo $this->createUrl("/admin/authentication/forgotpassword"); ?>'><?php $clang->eT("Forgot your password?"); ?></a><br />
-</p>
+</div>
--- application/views/admin/authentication/login.php
+++ application/views/admin/authentication/login.php
@@ -1,26 +1,28 @@
 <form name='loginform' id='loginform' method='post' action='<?php echo $this->createUrl("admin/authentication/login"); ?>' >
-    <br /><p><strong><?php echo $summary; ?></strong><br /><br /></p>
-    <ul>
-        <li><label for='user'><?php $clang->eT("Username"); ?></label>
-            <input name='user' id='user' type='text' size='40' maxlength='40' value='' /></li>
-        <li><label for='password'><?php $clang->eT("Password"); ?></label>
-            <input name='password' id='password' type='password' size='40' maxlength='40' /></li>
-        <li><label for='loginlang'><?php $clang->eT("Language"); ?></label>
-            <select id='loginlang' name='loginlang'>
-                <option value="default" selected="selected"><?php $clang->eT('Default'); ?></option>
-                <?php
-                $x = 0;
-                foreach (getLanguageDataRestricted(true) as $sLangKey => $aLanguage)
-                {
-                    //The following conditional statements select the browser language in the language drop down box and echoes the other options.
-                    ?>
-                    <option value='<?php echo $sLangKey; ?>'><?php echo $aLanguage['nativedescription'] . " - " . $aLanguage['description']; ?></option>
+    <div class='messagebox ui-corner-all'>
+        <div class='header ui-widget-header'><?php echo $summary; ?></div>
+        <br />
+        <ul style='width: 500px; margin-left: auto; margin-right: auto'>
+            <li><label for='user'><?php $clang->eT("Username"); ?></label>
+                <input name='user' id='user' type='text' size='40' maxlength='40' value='' /></li>
+            <li><label for='password'><?php $clang->eT("Password"); ?></label>
+                <input name='password' id='password' type='password' size='40' maxlength='40' /></li>
+            <li><label for='loginlang'><?php $clang->eT("Language"); ?></label>
+                <select id='loginlang' name='loginlang'>
+                    <option value="default" selected="selected"><?php $clang->eT('Default'); ?></option>
                     <?php
-                }
-                ?>
-            </select>
-        </li>
-    </ul>
+                    $x = 0;
+                    foreach (getLanguageDataRestricted(true) as $sLangKey => $aLanguage)
+                    {
+                        //The following conditional statements select the browser language in the language drop down box and echoes the other options.
+                        ?>
+                        <option value='<?php echo $sLangKey; ?>'><?php echo $aLanguage['nativedescription'] . " - " . $aLanguage['description']; ?></option>
+                        <?php
+                    }
+                    ?>
+                </select>
+            </li>
+        </ul>
     <p><input type='hidden' name='action' value='login' />
         <input class='action' type='submit' value='<?php $clang->eT("Login"); ?>' /><br />&nbsp;
         <br/>
@@ -33,6 +35,7 @@
         }
         ?>
     </p></form><br />
+    </div>
 <script type='text/javascript'>
     document.getElementById('user').focus();
 </script>
