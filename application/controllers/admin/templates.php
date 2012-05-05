fefc3f870753ceeebdf6f43c12f741eab7d85252
Carsten Schmitz <carsten.schmitz@limesurvey.org>
2012-05-05 08:32:15 +0200

    Fixed issue: Template editor not properly showing

--- application/controllers/admin/templates.php
+++ application/controllers/admin/templates.php
@@ -219,12 +219,12 @@ class templates extends Survey_Common_Action
     */
     public function index($editfile = 'startpage.pstpl', $screenname = 'welcome', $templatename = 'default')
     {
-        $this->getController()->_js_admin_includes(Yii::app()->getConfig('adminscripts') . 'templates.js');
-        $this->getController()->_css_admin_includes(Yii::app()->getConfig('adminscripts') . '/codemirror_ui/lib/CodeMirror-2.0/lib/codemirror.css');
-        $this->getController()->_css_admin_includes(Yii::app()->getConfig('adminscripts') . '/codemirror_ui/lib/CodeMirror-2.0/mode/javascript/javascript.css');
-        $this->getController()->_css_admin_includes(Yii::app()->getConfig('adminscripts') . '/codemirror_ui/css/codemirror-ui.css');
-
         $aViewUrls = $this->_initialise($templatename, $screenname, $editfile);
+        $this->getController()->_js_admin_includes(Yii::app()->getConfig('adminscripts') . 'templates.js');
+        $this->getController()->_css_admin_includes(Yii::app()->getConfig('adminscripts') . 'codemirror_ui/lib/CodeMirror-2.0/lib/codemirror.css');
+        $this->getController()->_css_admin_includes(Yii::app()->getConfig('adminscripts') . 'codemirror_ui/lib/CodeMirror-2.0/mode/javascript/javascript.css');
+        $this->getController()->_css_admin_includes(Yii::app()->getConfig('adminscripts') . 'codemirror_ui/css/codemirror-ui.css');
+
 
         $this->_renderWrappedTemplate('templates', $aViewUrls);
 
