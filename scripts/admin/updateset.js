34d84be95be15d9bccf84958111d2b09055d8d43
Carsten Schmitz <carsten.schmitz@limesurvey.org>
2012-05-07 13:02:49 +0200

    Fixed issue #6061: Duplicate codes when adding new answer options/subquestions/labels

--- scripts/admin/updateset.js
+++ scripts/admin/updateset.js
@@ -2,77 +2,77 @@
 
 $(document).ready(function(){
 
-        if ($(".answertable tbody").children().length == 0)
-            add_label(undefined);
+    if ($(".answertable tbody").children().length == 0)
+        add_label(undefined);
 
 
-	$(".btnaddanswer").live('click',add_label);
-        $(".btndelanswer").live('click',del_label);
+    $(".btnaddanswer").live('click',add_label);
+    $(".btndelanswer").live('click',del_label);
 
-        $('#neweditlblset0 .answertable tbody').sortable({
-                                            update:sort_complete,
-                                            distance:2
-                                        });
+    $('#neweditlblset0 .answertable tbody').sortable({
+        update:sort_complete,
+        distance:2
+    });
 
-        $('#quickadd').dialog({autoOpen: false,
-                                modal: true,
-                                width:600,
-                                title: $("#quickadd").attr('name')});
+    $('#quickadd').dialog({autoOpen: false,
+        modal: true,
+        width:600,
+        title: $("#quickadd").attr('name')});
 
-        $('.btnquickadd').live('click',function(){
-            $('#quickadd').dialog('open');
+    $('.btnquickadd').live('click',function(){
+        $('#quickadd').dialog('open');
+    });
+
+    $('#btnqacancel').click(function(){
+        $('#quickadd').dialog('close');
+    });
+
+
+    $('#btnqareplace').click(quickaddfunction);
+    $('#btnqainsert').click(quickaddfunction);
+
+
+
+
+    $("#mainform").submit(function(event,ui){
+
+        var dataToSend = {};
+        dataToSend['langs'] = [];
+        dataToSend['codelist'] = [];
+
+        $("#tabs>form>div").each(function(divindex,divelement){
+            var div_language = $(".lslanguage",divelement).val();
+
+            if (typeof(div_language)!="undefined")
+                dataToSend['langs'].push(div_language);
         });
 
-        $('#btnqacancel').click(function(){
-           $('#quickadd').dialog('close');
+        $("tbody>tr",$("#tabs>form>div:first")).each(function(trindex,trelement){
+
+            var tr_code = $(trelement).attr('id');
+            tr_code=tr_code.substr(3);
+            dataToSend['codelist'].push(tr_code);
+            dataToSend[tr_code] = {
+                code: $("#code_"+tr_code).val(),
+                assessmentvalue: $("#assessmentvalue_"+tr_code).val()
+            };
+
+            $(dataToSend['langs']).each(function(i,e){
+                dataToSend[tr_code]['text_'+e] = $("[name=title_"+e+"_"+tr_code+"]").val();
+            });
         });
+        $("input[name=dataToSend]").remove();
+        var encodedData = htmlspecialchars(js2php(dataToSend));
+        $("#mainform").append("<input type='hidden' name='dataToSend' value='"+encodedData+"' />");
+    });
 
-
-       $('#btnqareplace').click(quickaddfunction);
-       $('#btnqainsert').click(quickaddfunction);
-
-
-
-
-        $("#mainform").submit(function(event,ui){
-
-                 var dataToSend = {};
-                 dataToSend['langs'] = [];
-                 dataToSend['codelist'] = [];
-
-                 $("#tabs>form>div").each(function(divindex,divelement){
-                    var div_language = $(".lslanguage",divelement).val();
-
-                    if (typeof(div_language)!="undefined")
-                        dataToSend['langs'].push(div_language);
-                });
-
-                $("tbody>tr",$("#tabs>form>div:first")).each(function(trindex,trelement){
-
-                    var tr_code = $(trelement).attr('id');
-                    tr_code=tr_code.substr(3);
-                    dataToSend['codelist'].push(tr_code);
-                    dataToSend[tr_code] = {
-                        code: $("#code_"+tr_code).val(),
-                        assessmentvalue: $("#assessmentvalue_"+tr_code).val()
-                    };
-
-                    $(dataToSend['langs']).each(function(i,e){
-                        dataToSend[tr_code]['text_'+e] = $("[name=title_"+e+"_"+tr_code+"]").val();
-                        });
-                    });
-                    $("input[name=dataToSend]").remove();
-					var encodedData = htmlspecialchars(js2php(dataToSend));
-                    $("#mainform").append("<input type='hidden' name='dataToSend' value='"+encodedData+"' />");
-        });
-
-        fix_highlighting();
+    fix_highlighting();
 });
 
 function quickaddfunction(){
     var lsreplace = false;
     if ($(this).attr('id') == 'btnqareplace'){
-       lsreplace=true;
+        lsreplace=true;
     }
 
     if (lsreplace){
@@ -82,7 +82,7 @@ function quickaddfunction(){
     lsrows=$('#quickaddarea').val().split("\n");
     var seperatorchar="\t";
     if (lsrows[0].indexOf("\t")==-1){
-            seperatorchar=';'
+        seperatorchar=';'
     }
 
 
@@ -90,32 +90,32 @@ function quickaddfunction(){
     $(lsrows).each(function(index,element){
         code = undefined;
 
-       params = element.split(seperatorchar);
-       i = 0;
-       if (params.length > $(".lslanguage").length){
-           code = params[0].replace(/[^a-zA-Z 0-9]+/g,'');
+        params = element.split(seperatorchar);
+        i = 0;
+        if (params.length > $(".lslanguage").length){
+            code = params[0].replace(/[^a-zA-Z 0-9]+/g,'');
 
-           i++;
-       }
+            i++;
+        }
 
-       if (index!=0){
-           event = {};
-           event.target = $(".btnaddanswer:last");
-           var retcode = add_label(event);
-       }
-       else{
-           var retcode = add_label();
-       }
+        if (index!=0){
+            event = {};
+            event.target = $(".btnaddanswer:last");
+            var retcode = add_label(event);
+        }
+        else{
+            var retcode = add_label();
+        }
 
 
 
-      if (typeof(code)!="undefined")
-          $("#code_"+retcode).val(code);
+        if (typeof(code)!="undefined")
+            $("#code_"+retcode).val(code);
 
-      $(".lslanguage").each(function(){
-          $("input[name=title_"+$(this).val()+"_"+retcode+"]").val(params[i]);
-          i++;
-      });
+        $(".lslanguage").each(function(){
+            $("input[name=title_"+$(this).val()+"_"+retcode+"]").val(params[i]);
+            i++;
+        });
 
 
     });
@@ -142,15 +142,15 @@ function sort_complete(event, ui){
 
 
     $("#tabs div:not(:first) [name="+$(item).attr('name')+"]").each(function(index,element){
-       var backup = "<tr id='row"+$(item).attr('name')+"'>"+$(element).html()+"</tr>";
+        var backup = "<tr id='row"+$(item).attr('name')+"'>"+$(element).html()+"</tr>";
 
-       if (newposition >= 0)
+        if (newposition >= 0)
             $($(element).parent().children()[newposition]).after(backup);
 
-       else
+        else
             $(element).parent().prepend(backup);
 
-       $(element).remove();
+        $(element).remove();
     });
 
     fix_highlighting();
@@ -159,53 +159,53 @@ function sort_complete(event, ui){
 function add_label(event)
 {
 
-	if (typeof(event)=="undefined")
-            var next_code = 1;
+    next_code=getNextCode($(this).parent().parent().find('.codeval').val());
+    while ($(this).parent().parent().parent().find('input[value="'+next_code+'"]').length>0)
+    {
+        next_code=getNextCode(next_code);
+    }
+    var html = createNewLabelTR(true,true);
+
+    if (typeof(event)=="undefined")
+        var row_id = -1;
+    else
+        var row_id = ($(event.target).parent().parent().parent().children().index($(event.target).parent().parent()));
+
+    var randomid = 'new' + Math.floor(Math.random()*111111);
+
+    html = str_replace("###assessmentval###",'0',html);
+    html = str_replace("###codeval###",next_code,html);
+    html = str_replace("###next###",randomid,html);
+    html = str_replace("###lang###",$("#tabs div:first .lslanguage").val(),html);
+
+
+    if (typeof(event) == "undefined")
+        $("#tabs div:first tbody").html(html);
+    else
+        $(event.target).parent().parent().after(html);
+
+    html = createNewLabelTR(true,false);
+
+    html = str_replace("###assessmentval###",'0',html);
+    html = str_replace("###codeval###",next_code,html);
+    html = str_replace("###next###",randomid,html);
+
+    $("#tabs div:not(:first)").each(function(index,element){
+
+        var temp_html = str_replace("###lang###",$(".lslanguage",element).val(),html);
+        if (row_id >= 0){
+            $($("tbody",element).children()[row_id]).after(temp_html);
+        }
         else
-            var next_code = parseInt($('.hiddencode',$(event.target).parent().parent()).val()) + 1;
+            $(".answertable tbody",$(element)).html(temp_html);
 
-	var html = createNewLabelTR(true,true);
+    });
 
-        if (typeof(event)=="undefined")
-            var row_id = -1;
-        else
-            var row_id = ($(event.target).parent().parent().parent().children().index($(event.target).parent().parent()));
+    $("tr[name="+randomid+"]").hide().fadeIn(1000);
 
-        var randomid = 'new' + Math.floor(Math.random()*111111);
+    fix_highlighting();
 
-	html = str_replace("###assessmentval###",'0',html);
-	html = str_replace("###codeval###",next_code,html);
-	html = str_replace("###next###",randomid,html);
-	html = str_replace("###lang###",$("#tabs div:first .lslanguage").val(),html);
-
-
-	if (typeof(event) == "undefined")
-            $("#tabs div:first tbody").html(html);
-        else
-            $(event.target).parent().parent().after(html);
-
-        html = createNewLabelTR(true,false);
-
-        html = str_replace("###assessmentval###",'0',html);
-	html = str_replace("###codeval###",next_code,html);
-	html = str_replace("###next###",randomid,html);
-
-        $("#tabs div:not(:first)").each(function(index,element){
-
-            var temp_html = str_replace("###lang###",$(".lslanguage",element).val(),html);
-            if (row_id >= 0){
-                $($("tbody",element).children()[row_id]).after(temp_html);
-            }
-            else
-                $(".answertable tbody",$(element)).html(temp_html);
-
-        });
-
-        $("tr[name="+randomid+"]").hide().fadeIn(1000);
-
-	fix_highlighting();
-
-        return randomid;
+    return randomid;
 }
 
 
@@ -218,7 +218,7 @@ function del_label(event){
     fix_highlighting();
 
     if ($(".answertable tbody").children().length == 0)
-            add_label(undefined);
+        add_label(undefined);
 
 }
 
@@ -238,15 +238,15 @@ function createNewLabelTR(alternate,first){
         x = x + "<td>###codeval###</td><td>###assessmentval###</td>";
     else
         x = x + "<td><img src=" + sImageURL + "handle.png></td><td>"
-                + "<input type='hidden' class='hiddencode' value='###codeval###' />"
-                + "<input type='text' class='codeval' value='###codeval###' name='code_###next###' id='code_###next###' size='6' maxlength='5' >"
-                + "</td><td>"
-                + "<input type=\"text\" class='assessmentval' value=\"###assessmentval###\" name=\"assessmentvalue_###next###\" id=\"assessmentvalue_###next###\" style=\"text-align: right;\" size=\"6\" maxlength=\"5\" >";
+    + "<input type='hidden' class='hiddencode' value='###codeval###' />"
+    + "<input type='text' class='codeval' value='###codeval###' name='code_###next###' id='code_###next###' size='6' maxlength='5' >"
+    + "</td><td>"
+    + "<input type=\"text\" class='assessmentval' value=\"###assessmentval###\" name=\"assessmentvalue_###next###\" id=\"assessmentvalue_###next###\" style=\"text-align: right;\" size=\"6\" maxlength=\"5\" >";
 
     x = x + "<td><input name=\"title_###lang###_###next###\"  type=\"text\" value=\"\" size=\"80\" maxlength=\"3000\" >"+
-            "<a title=\"\" id=\"title_###lang###_###next###_ctrl\" href=\"javascript:start_popup_editor('title_###lang###_###next###','[Label:](###lang###)','','','','editlabel','labels')\">"+
-            "<img height=\"16\" border=\"0\" width=\"16\" src=" + sImageURL + "edithtmlpopup.png name=\"title_###lang###_###next###_popupctrlena\" id=\"title_###lang###_###next###_popupctrlena\" alt=\"\">"+
-            "<img height=\"16\" border=\"0\" align=\"top\" width=\"16\" style=\"display: none;\" src=" + sImageURL + "edithtmlpopup_disabled.png name=\"title_###lang###_###next###_popupctrldis\" id=\"title_###lang###_###next###_popupctrldis\" alt=\"Give focus to the HTML editor popup window\"></a></td>";
+    "<a title=\"\" id=\"title_###lang###_###next###_ctrl\" href=\"javascript:start_popup_editor('title_###lang###_###next###','[Label:](###lang###)','','','','editlabel','labels')\">"+
+    "<img height=\"16\" border=\"0\" width=\"16\" src=" + sImageURL + "edithtmlpopup.png name=\"title_###lang###_###next###_popupctrlena\" id=\"title_###lang###_###next###_popupctrlena\" alt=\"\">"+
+    "<img height=\"16\" border=\"0\" align=\"top\" width=\"16\" style=\"display: none;\" src=" + sImageURL + "edithtmlpopup_disabled.png name=\"title_###lang###_###next###_popupctrldis\" id=\"title_###lang###_###next###_popupctrldis\" alt=\"Give focus to the HTML editor popup window\"></a></td>";
 
     if (first)
         x = x + "<td style=\"text-align: center;\"><img class=\"btnaddanswer\" src=" + sImageURL + "addanswer.png><img class=\"btndelanswer\" src=" + sImageURL + "deleteanswer.png></td>";
@@ -260,10 +260,10 @@ function createNewLabelTR(alternate,first){
 
 function str_replace (search, replace, subject, count) {
     var i = 0, j = 0, temp = '', repl = '', sl = 0, fl = 0,
-            f = [].concat(search),
-            r = [].concat(replace),
-            s = subject,
-            ra = r instanceof Array, sa = s instanceof Array;
+    f = [].concat(search),
+    r = [].concat(replace),
+    s = subject,
+    ra = r instanceof Array, sa = s instanceof Array;
     s = [].concat(s);
     if (count) {
         this.window[count] = 0;
@@ -284,3 +284,35 @@ function str_replace (search, replace, subject, count) {
     return sa ? s : s[0];
 }
 
+function getNextCode(sourcecode)
+{
+    i=1;
+    found=true;
+    foundnumber=-1;
+    sclength = sourcecode.length;
+    while (i<=sclength && found == true)
+    {
+        found=is_numeric(sourcecode.substr(sclength-i,i));
+        if (found)
+            {
+            foundnumber=sourcecode.substr(sclength-i,i);
+            i++;
+        }
+    }
+    if (foundnumber==-1)
+        {
+        return(sourcecode);
+    }
+    else
+        {
+        foundnumber++;
+        foundnumber=foundnumber+'';
+        result=sourcecode.substr(0,sclength-foundnumber.length)+foundnumber;
+        return(result);
+    }
+
+}
+
+function is_numeric (mixed_var) {
+    return (typeof(mixed_var) === 'number' || typeof(mixed_var) === 'string') && mixed_var !== '' && !isNaN(mixed_var);
+}
