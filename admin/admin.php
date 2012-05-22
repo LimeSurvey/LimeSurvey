a964251da83f14abd16ddc1d1686fb7742cac027
mennodekker <menno.dekker@erasmusmc.nl>
2012-05-22 10:37:31 +0200

    Fixed #6122: EM seems to be holding a cache in admin dev: only set language when a surveyid is set

--- admin/admin.php
+++ admin/admin.php
@@ -45,7 +45,9 @@ else
     $refurl =  "";
 }
 LimeExpressionManager::SetSurveyId($surveyid);  // must be called early - it clears internal cache if a new survey is being used
-SetSurveyLanguage($surveyid, '');        // just to make sure we have a language for EM
+if (!is_null($surveyid)) {
+    SetSurveyLanguage($surveyid, '');        // just to make sure we have a language for EM
+}
 
 if ($action != 'showprintablesurvey' && substr($action,0,4)!= 'ajax')
 {
