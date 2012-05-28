79d57b0862dcf8af1d46d2a7c582dd57a7a16716
Carsten Schmitz <carsten.schmitz@limesurvey.org>
2012-05-28 15:44:25 +0200

    Fixed issue #6133: Race condition with concurrent upload of similar "survey structure"

--- application/controllers/admin/surveyadmin.php
+++ application/controllers/admin/surveyadmin.php
@@ -771,7 +771,7 @@ class SurveyAdmin extends Survey_Common_Action
             if ($action == 'importsurvey')
             {
 
-                $the_full_file_path = Yii::app()->getConfig('tempdir') . DIRECTORY_SEPARATOR . $_FILES['the_file']['name'];
+                $the_full_file_path = Yii::app()->getConfig('tempdir') . DIRECTORY_SEPARATOR . $_FILES['the_file']['tmp_name'];
                 if (!@move_uploaded_file($_FILES['the_file']['tmp_name'], $the_full_file_path))
                 {
                     $aData['sErrorMessage'] = sprintf($clang->gT("An error occurred uploading your file. This may be caused by incorrect permissions in your %s folder."), Yii::app()->getConfig('tempdir'));
