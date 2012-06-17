fc4fe01dd93070be0ce7f65f89e13357a44db1aa
Thomas White <tmswhite@users.sourceforge.net>
2012-06-16 23:00:36 -0400

    Fixed issue #06225: unable to pass qcode-named variables via URL Dev can now pass variables via either qcode or SGQA naming on the URL

--- classes/expressions/LimeExpressionManager.php
+++ classes/expressions/LimeExpressionManager.php
@@ -3802,7 +3802,63 @@
 
             if (isset($_SESSION['startingValues']) && is_array($_SESSION['startingValues']) && count($_SESSION['startingValues']) > 0)
             {
-                $startingValues = $_SESSION['startingValues'];
+                $startingValues = array();
+                foreach ($_SESSION['startingValues'] as $k=>$value)
+                {
+                    if (isset($LEM->knownVars[$k]))
+                    {
+                        $knownVar = $LEM->knownVars[$k];
+                    }
+                    else if (isset($LEM->qcode2sgqa[$k]))
+                    {
+                        $knownVar = $LEM->knownVars[$LEM->qcode2sgqa[$k]];
+                    }
+                    else if (isset($LEM->tempVars[$k]))
+                    {
+                        $knownVar = $LEM->tempVar[$k];
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                    if (!isset($knownVar['jsName']))
+                    {
+                        continue;
+                    }
+                    switch ($knownVar['type'])
+                    {
+                        case 'D': //DATE
+                            if (trim($value)=="")
+                            {
+                                $value = NULL;
+                            }
+                            else
+                            {
+                                $dateformatdatat=getDateFormatData($LEM->surveyOptions['surveyls_dateformat']);
+                                $datetimeobj = new Date_Time_Converter($value, $dateformatdatat['phpdate']);
+                                $value=$datetimeobj->convert("Y-m-d");
+                            }
+                            break;
+                        case 'N': //NUMERICAL QUESTION TYPE
+                        case 'K': //MULTIPLE NUMERICAL QUESTION
+                            if (trim($value)=="") {
+                                $value = NULL;
+                            }
+                            else {
+                                $value = sanitize_float($value);
+                            }
+                            break;
+                        case '|': //File Upload
+                            $value=NULL;  // can't upload a file via GET
+                            break;
+                    }
+                    $_SESSION[$knownVar['sgqa']] = $value;
+                    $startingValues[$k]=array(
+                        'type'=>$knownVar['type'],
+                        'value'=>$value,
+                    );
+
+                }
                 $LEM->_UpdateValuesInDatabase($startingValues);
             }
 
