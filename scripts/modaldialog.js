80f6915dab6fbab998dc10ebd41eb2c925623518
Thomas White <tmswhite@users.sourceforge.net>
2012-06-18 12:00:03 -0400

    Fixed issue #06211: Problems with two "file upload" question types in the same question group

--- scripts/modaldialog.js
+++ scripts/modaldialog.js
@@ -136,7 +136,7 @@ function getQueryVariable(variable, url) {
 function isValueInArray(arr, val) {
     inArray = false;
     for (i = 0; i < arr.length; i++) {
-        if (val === arr[i]) {
+        if (val.toLowerCase() == arr[i].toLowerCase()) {
             inArray = true;
         }
     }
