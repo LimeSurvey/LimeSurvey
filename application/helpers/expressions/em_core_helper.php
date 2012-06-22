10b352c480a6ce7bda63d6fcbdfa852d6b6b29cb
Thomas White <tmswhite@users.sourceforge.net>
2012-06-22 16:39:40 -0400

    Fixed issue EM unit tests broken due to  missing LEMradix variable.

--- application/helpers/expressions/em_core_helper.php
+++ application/helpers/expressions/em_core_helper.php
@@ -3131,6 +3131,7 @@ EOD;
         $pre .= "<!--\n";
         $pre .= "var LEMalias2varName= {". implode(",\n", $LEMalias2varName) ."};\n";
         $pre .= "var LEMvarNameAttr= {" . implode(",\n", $LEMvarNameAttr) . "};\n";
+        $pre .= "var LEMradix = '.';\n";
         $pre .= "//-->\n</script>\n";
 
         print $pre;
