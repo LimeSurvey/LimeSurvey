c29afb6ee2d8899a3202d38a7d12ce5d0e9b81bc
jcleeland <jason@cleeland.org>
2012-07-18 22:50:06 +1000

    Fixed issue: error when browsing linked responses if the associated token was missing - $browsedatafield does not exist line 33.

--- application/views/admin/browse/browseallrow_view.php
+++ application/views/admin/browse/browseallrow_view.php
@@ -17,12 +17,13 @@
     </td>
     <?php
         $i = 0;
+        $browsedatafield="";
         if ($surveyinfo['anonymized'] == "N" && $dtrow['token'])
         {
             if (isset($dtrow['tid']) && !empty($dtrow['tid']))
             {
                 //If we have a token, create a link to edit it
-                $browsedatafield = "<a href='" . $this->createUrl("admin/tokens/edit/surveyid/$surveyid/tokenid/{$dtrow['tid']}/") . "' title='" . $clang->gT("Edit this token") . "'>";
+                $browsedatafield .= "<a href='" . $this->createUrl("admin/tokens/edit/surveyid/$surveyid/tokenid/{$dtrow['tid']}/") . "' title='" . $clang->gT("Edit this token") . "'>";
                 $browsedatafield .= "{$dtrow['token']}";
                 $browsedatafield .= "</a>";
             }
