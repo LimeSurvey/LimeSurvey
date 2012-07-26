49b9a14ea4b80292742ef8a828b959b0e0acd67c
jcleeland <jason@cleeland.org>
2012-07-26 22:18:12 +1000

    New feature: statistics browse, etc - view file and generic sort icons

---
+++ application/views/admin/export/statistics_browse_view.php
@@ -0,0 +1,49 @@
+<div class='statisticscolumnid'>
+    <?php
+    $image="sort_none.png";
+    if($sortby=="id") {
+        switch($sortmethod) {
+            case "asc":
+                $image="sort_desc.png";
+                break;
+            case "desc":
+                $image="sort_asc.png";
+                break;
+        }
+    }
+    ?>
+    <?php $sort=(isset($sortby) && $sortby=="id" && $sortmethod=='asc') ? 'desc' : 'asc'; ?>
+    <img src='<?php echo Yii::app()->getConfig('adminimageurl') . "/" . $image ?>' class='sortorder' id='sortorder_<?php echo $column ?>_id_<?php echo $sort ?>_T' style='cursor: pointer' />
+</div>
+<div class='statisticscolumndata'>
+    <?php
+    $image="sort_none.png";
+    if($sortby==$column) {
+        switch($sortmethod) {
+            case "asc":
+                $image="sort_desc.png";
+                break;
+            case "desc":
+                $image="sort_asc.png";
+                break;
+        }
+    }
+    ?>
+    <?php $sort=(isset($sortby) && $sortby==$column && $sortmethod=='asc') ? 'desc' : 'asc'; ?>
+    <img src='<?php echo Yii::app()->getConfig('adminimageurl') . "/".$image ?>' class='sortorder' id='sortorder_<?php echo $column ?>_<?php echo $column ?>_<?php echo $sort ?>_<?php echo $sorttype ?>' style='cursor: pointer' /></div>
+<div style='clear: both'></div>
+<?php
+foreach ($data as $row) {
+?>
+<div class='statisticscolumnid'>
+    <a href='<?php echo Yii::app()->getController()->createUrl("admin/responses/view/surveyid/".$surveyid."/id/".$row['id']); ?>' target='_blank'>
+        <img src='<?php echo Yii::app()->getConfig('adminimageurl') . "/search.gif" ?>' />
+    </a>
+</div>
+<div class='statisticscolumndata'>
+    <?php echo $row['value'] ?>
+</div>
+<div style='clear: both'></div>
+<?php
+}
+?>
