ccbdb3c6bac3d07fb39e3a69109c07ea886cd48e
Aaron Schmitz <me@aaronschmitz.com>
2012-08-01 13:57:07 -0500

    Bugfix for wrong var names from MultinumericalQuestion.php causing SQGA errors.

--- application/modules/MultinumericalQuestion.php
+++ application/modules/MultinumericalQuestion.php
@@ -427,6 +427,31 @@ class MultinumericalQuestion extends QuestionModule
         return true;
     }
 
+    public function getCsuffix()
+    {
+        return $this->aid;
+    }
+
+    public function getSqsuffix()
+    {
+        return '_' . $this->aid;
+    }
+
+    public function getVarName()
+    {
+        return $this->title . '_' . $this->aid;
+    }
+
+    public function getQuestion()
+    {
+        return $this->sq;
+    }
+
+    public function getRowDivID()
+    {
+        return $this->fieldname;
+    }
+
     public function availableAttributes($attr = false)
     {
         $attrs=array("array_filter","array_filter_exclude","array_filter_style","equals_num_value","em_validation_q","em_validation_q_tip","em_validation_sq","em_validation_sq_tip","exclude_all_others","statistics_showgraph","statistics_graphtype","hide_tip","hidden","max_answers","max_num_value","max_num_value_n","maximum_chars","min_answers","min_num_value","min_num_value_n","page_break","prefix","public_statistics","random_order","parent_order","slider_layout","slider_min","slider_max","slider_accuracy","slider_default","slider_middlestart","slider_showminmax","slider_separator","suffix","text_input_width","random_group","value_range_allows_missing");
