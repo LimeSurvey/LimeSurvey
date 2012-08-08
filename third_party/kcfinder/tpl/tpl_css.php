1af5cb26dc0f0beb92a22bd951a53d87533167e6
Sam Mousa <sam@mousa.nl>
2012-08-08 22:43:05 +0200

    Added third party libaries...

---
+++ third_party/kcfinder/tpl/tpl_css.php
@@ -0,0 +1,2 @@
+<link href="css.php?type=<?php echo $this->type ?>" rel="stylesheet" type="text/css" />
+<link href="themes/<?php echo $this->config['theme'] ?>/style.css" rel="stylesheet" type="text/css" />
