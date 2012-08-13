231e2a161d44a5ab0a2a3cb5068aeecacbfed4a7
Denis Chenu <courriel@shnoulle.net>
2012-08-13 19:02:14 +0200

    Dev: first part for #6451 : give the id of the answers element according to Expression Manager

--- application/helpers/qanda_helper.php
+++ application/helpers/qanda_helper.php
@@ -2162,6 +2162,7 @@ function do_ranking($ia)
     $answer .="</ul>"
         . "<div style='display:none' id='ranking-{$ia[0]}-maxans'>{".$max_answers."}</div>"
         . "<div style='display:none' id='ranking-{$ia[0]}-minans'>{".$min_answers."}</div>"
+        . "<div style='display:none' id='ranking-{$ia[0]}-name'>javatb".$myfname."</div>"
         . "</div>";
     // The list with HTML answres
     $answer .="<div style=\"display:none\">";
