9f227bdfd976536427a958d404978a8489e39c41
Aaron Schmitz <me@aaronschmitz.com>
2012-08-15 04:23:38 -0500

    Tweak TenRadioArrayQuestion.php

--- application/modules/TenRadioArrayQuestion.php
+++ application/modules/TenRadioArrayQuestion.php
@@ -178,12 +178,6 @@ class TenRadioArrayQuestion extends RadioArrayQuestion
         return LimeExpressionManager::GetVarAttribute($name,'code',$default,$gseq,$qseq);
     }
 
-    public function availableAttributes($attr = false)
-    {
-        $attrs=array("answer_width","array_filter","array_filter_exclude","array_filter_style","em_validation_q","em_validation_q_tip","exclude_all_others","statistics_showgraph","statistics_graphtype","hide_tip","hidden","max_answers","min_answers","page_break","public_statistics","random_order","parent_order","random_group");
-        return $attr?in_array($attr,$attrs):$attrs;
-    }
-
     public function getShownJS()
     {
         return 'return value;';
@@ -194,9 +188,10 @@ class TenRadioArrayQuestion extends RadioArrayQuestion
         return 'return value;';
     }
 
-    public function availableOptions()
+    public function availableAttributes($attr = false)
     {
-        return array('other' => false, 'valid' => false, 'mandatory' => true);
+        $attrs=array("answer_width","array_filter","array_filter_exclude","array_filter_style","em_validation_q","em_validation_q_tip","exclude_all_others","statistics_showgraph","statistics_graphtype","hide_tip","hidden","max_answers","min_answers","page_break","public_statistics","random_order","parent_order","random_group");
+        return $attr?in_array($attr,$attrs):$attrs;
     }
 
     public function questionProperties($prop = false)
