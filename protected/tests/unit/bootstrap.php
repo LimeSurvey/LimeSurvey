e347c65482d3d975691e41aca78aa9c46da640ce
Menno Dekker <menno.dekker@erasmusmc.nl>
2012-10-25 10:08:00 +0200

    dev: improved bootstrap for phpunit 3.7.7 and up as it breaks on autoload changes in phpunit dev: added default config phpunit.xml to use bootstrap so running test is easier

--- protected/tests/unit/bootstrap.php
+++ protected/tests/unit/bootstrap.php
@@ -8,4 +8,13 @@ require_once APPPATH . 'core/LSYii_Application.php';
 
 define('EXT', '.php');
 
+// Fix for phpunit => 3.7.7
+require_once('PHPUnit/Runner/Version.php');
+if (version_compare('3.7.7', PHPUnit_Runner_Version::VERSION)<=0) {
+    function phpunit_autoload($class)
+    {        
+    }
+    spl_autoload_register('phpunit_autoload');
+}
+
 yii::createApplication('LSYii_Application', APPPATH . 'config/config-sample.php');
