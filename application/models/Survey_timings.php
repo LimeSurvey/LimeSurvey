db3bd59889b072e0121f904f15a69cf44ee6ae2f
Carsten Schmitz <c_schmitz@users.sourceforge.net>
2012-11-02 15:50:51 +0100

    Fixed issue #6800: No report timings results for individual questions when using MSSQL

--- application/models/Survey_timings.php
+++ application/models/Survey_timings.php
@@ -16,36 +16,43 @@
 
 class Survey_timings extends LSActiveRecord
 {
-
     protected static $sid = 0;
-    /**
-     * Returns the static model
-     *
-     * @static
-     * @access public
-     * @param int $surveyid
-     * @return CActiveRecord
-     */
-    public static function model($sid = null)
-    {
-        if (!is_null($sid))
+    
+	/**
+	 * Returns the static model
+	 *
+	 * @static
+	 * @access public
+	 * @param int $surveyid
+	 * @return CActiveRecord
+	 */
+    public static function model($sid = NULL)
+    {         
+        $refresh = false;
+        if (!is_null($sid)) {
             self::sid($sid);
-
-        return parent::model(__CLASS__);
+            $refresh = true;
+        }
+        
+        $model = parent::model(__CLASS__);
+        
+        //We need to refresh if we changed sid
+        if ($refresh === true) $model->refreshMetaData();
+        return $model;
     }
-
-    /**
-     * Sets the survey ID for the next model
-     *
-     * @static
-     * @access public
-     * @param int $sid
-     * @return void
-     */
-    public static function sid($sid)
-    {
-        self::$sid = (int) $sid;
-    }
+    
+	/**
+	 * Sets the survey ID for the next model
+	 *
+	 * @static
+	 * @access public
+	 * @param int $sid
+	 * @return void
+	 */
+	public static function sid($sid)
+	{
+		self::$sid = (int) $sid;
+	}
 
     /**
      * Returns the primary key of this table
