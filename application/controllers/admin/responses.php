6745e3d6867e13aa057eeacfd727b87c3a531d1c
Denis Chenu <courriel@shnoulle.net>
2012-11-04 12:51:47 +0100

    Dev : better translation string for browse response (answer to response) Dev: sorry for translator :)

--- application/controllers/admin/responses.php
+++ application/controllers/admin/responses.php
@@ -278,7 +278,7 @@ class responses extends Survey_Common_Action
         }
         else
         {
-            Yii::app()->session['flashmessage'] = $clang->gT("This answer doesn't exist.");
+            Yii::app()->session['flashmessage'] = $clang->gT("This response ID is invalid.");
         }
         $aViewUrls['browseidfooter_view'][] = $aData;
 
