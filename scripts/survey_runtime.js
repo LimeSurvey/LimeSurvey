eacadb501e7ad90b95fb826d50c1315f49357cc8
tpartner <tpartner@partnersinc.biz>
2012-11-15 17:54:36 -0500

    Fixed issue #06885: Long survey pages jump down the page in IE8 compatibility view Dev Forced scroll to top when in IE compatibility mode

--- scripts/survey_runtime.js
+++ scripts/survey_runtime.js
@@ -98,7 +98,8 @@ $(document).ready(function()
 // Set jquery-ui to LS Button
 function navbuttonsJqueryUi(){
     if ($.browser.msie && $.browser.version.substr(0,1)<8 && $('button.submit').length > 0) { // Get rid of the focus outline in IE7
-        $('#movenextbtn, #movesubmitbtn').focus().blur(); 
+        $('#movenextbtn, #movesubmitbtn').focus().blur();
+        $(this).scrollTop(0);  
     }
 
     $('[dir!="rtl"] #moveprevbtn').button({
