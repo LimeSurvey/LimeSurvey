86c0650bf8914573dd4ab39a05bc69e8f6830bcc
Carsten Schmitz <c_schmitz@users.sourceforge.net>
2012-11-29 18:28:20 +0100

    Dev Update arPHP to 3.5.0 - fixes partially issue #6969

---
+++ application/third_party/ar-php/Arabic/data/charset/_bug.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * A map of Arabic characters in one of the most common bug/corruption when
+ * when shift MySQL database from one server to another
+ *
+ * @category  I18N
+ * @package   I18N_Arabic
+ * @author    Khaled Al-Sham'aa <khaled@ar-php.org>
+ * @copyright 2006-2011 Khaled Al-Sham'aa
+ *
+ * @license   LGPL <http://www.gnu.org/licenses/lgpl.txt>
+ * @link      http://www.ar-php.org
+ */
+$this->bug[0]  = 'Ð';
+$this->bug[1]  = 'Ï';
+$this->bug[2]  = 'Ì';
+$this->bug[3]  = 'Í';
+$this->bug[4]  = 'Î';
+$this->bug[5]  = 'å';
+$this->bug[6]  = 'Ñ';
+$this->bug[7]  = 'Û';
+$this->bug[8]  = 'Ý';
+$this->bug[9]  = 'Þ';
+$this->bug[10] = 'Ë';
+$this->bug[11] = 'Õ';
+$this->bug[12] = 'Ö';
+$this->bug[13] = 'Ø';
+$this->bug[14] = 'ß';
+$this->bug[15] = 'ã';
+$this->bug[16] = 'ä';
+$this->bug[17] = 'Ê';
+$this->bug[18] = 'Ç';
+$this->bug[19] = 'á';
+$this->bug[20] = 'È';
+$this->bug[21] = 'í';
+$this->bug[22] = 'Ó';
+$this->bug[23] = 'Ô';
+$this->bug[24] = 'Ù';
+$this->bug[25] = 'Ò';
+$this->bug[26] = 'æ';
+$this->bug[27] = 'É';
+$this->bug[28] = 'ì';
+$this->bug[29] = 'Ñ';
+$this->bug[30] = 'Ä';
+$this->bug[31] = 'Á';
+$this->bug[32] = 'Æ';
+$this->bug[33] = 'ø';
+$this->bug[34] = 'ó';
+$this->bug[35] = 'ð';
+$this->bug[36] = 'ñ';
+$this->bug[37] = 'Å';
+$this->bug[38] = 'ö';
+$this->bug[39] = 'ò';
+$this->bug[40] = 'Ã';
+$this->bug[41] = 'Ü';
+$this->bug[42] = '¡';
+$this->bug[43] = 'ú';
+$this->bug[44] = 'Â';
+$this->bug[45] = ',';
+$this->bug[46] = '.';
+$this->bug[47] = '¿';
+$this->bug[48] = '0';
+$this->bug[49] = '1';
+$this->bug[50] = '2';
+$this->bug[51] = '3';
+$this->bug[52] = '4';
+$this->bug[53] = '5';
+$this->bug[54] = '6';
+$this->bug[55] = '7';
+$this->bug[56] = '8';
+$this->bug[57] = '9';
+
+$this->bug[58] = 'õ';
+?>
---
+++ application/third_party/ar-php/Arabic/data/charset/_windows1256.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * A map of Arabic characters in the Windows-1256 charset
+ *
+ * @category  I18N
+ * @package   I18N_Arabic
+ * @author    Khaled Al-Sham'aa <khaled@ar-php.org>
+ * @copyright 2006-2011 Khaled Al-Sham'aa
+ *
+ * @license   LGPL <http://www.gnu.org/licenses/lgpl.txt>
+ * @link      http://www.ar-php.org
+ */
+$this->windows1256[0]  = 'Ð';
+$this->windows1256[1]  = 'Ï';
+$this->windows1256[2]  = 'Ì';
+$this->windows1256[3]  = 'Í';
+$this->windows1256[4]  = 'Î';
+$this->windows1256[5]  = 'å';
+$this->windows1256[6]  = 'Ú';
+$this->windows1256[7]  = 'Û';
+$this->windows1256[8]  = 'Ý';
+$this->windows1256[9]  = 'Þ';
+$this->windows1256[10] = 'Ë';
+$this->windows1256[11] = 'Õ';
+$this->windows1256[12] = 'Ö';
+$this->windows1256[13] = 'Ø';
+$this->windows1256[14] = 'ß';
+$this->windows1256[15] = 'ã';
+$this->windows1256[16] = 'ä';
+$this->windows1256[17] = 'Ê';
+$this->windows1256[18] = 'Ç';
+$this->windows1256[19] = 'á';
+$this->windows1256[20] = 'È';
+$this->windows1256[21] = 'í';
+$this->windows1256[22] = 'Ó';
+$this->windows1256[23] = 'Ô';
+$this->windows1256[24] = 'Ù';
+$this->windows1256[25] = 'Ò';
+$this->windows1256[26] = 'æ';
+$this->windows1256[27] = 'É';
+$this->windows1256[28] = 'ì';
+$this->windows1256[29] = 'Ñ';
+$this->windows1256[30] = 'Ä';
+$this->windows1256[31] = 'Á';
+$this->windows1256[32] = 'Æ';
+$this->windows1256[33] = 'ø';
+$this->windows1256[34] = 'ó';
+$this->windows1256[35] = 'ð';
+$this->windows1256[36] = 'ñ';
+$this->windows1256[37] = 'Å';
+$this->windows1256[38] = 'ö';
+$this->windows1256[39] = 'ò';
+$this->windows1256[40] = 'Ã';
+$this->windows1256[41] = 'Ü';
+$this->windows1256[42] = '¡';
+$this->windows1256[43] = 'ú';
+$this->windows1256[44] = 'Â';
+$this->windows1256[45] = ',';
+$this->windows1256[46] = '.';
+$this->windows1256[47] = '¿';
+$this->windows1256[48] = '0';
+$this->windows1256[49] = '1';
+$this->windows1256[50] = '2';
+$this->windows1256[51] = '3';
+$this->windows1256[52] = '4';
+$this->windows1256[53] = '5';
+$this->windows1256[54] = '6';
+$this->windows1256[55] = '7';
+$this->windows1256[56] = '8';
+$this->windows1256[57] = '9';
+
+$this->windows1256[58] = 'õ';
+?>
