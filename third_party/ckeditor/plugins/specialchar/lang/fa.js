a9e070457e24f68d12c121bfd70452c893e84354
Carsten Schmitz <c_schmitz@users.sourceforge.net>
2012-11-30 15:50:40 +0100

    Fixed issue #6922: Problem with placing the cursor after placeholder in IE8 Dev Updating CKEditor to 3.6.5 - hopefull fixed issue above

--- third_party/ckeditor/plugins/specialchar/lang/fa.js
+++ third_party/ckeditor/plugins/specialchar/lang/fa.js
@@ -3,4 +3,129 @@ Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.html or http://ckeditor.com/license
 */
 
-CKEDITOR.plugins.setLang('specialchar','fa',{euro:'نشان یورو',lsquo:'علامت نقل قول تکی چپ',rsquo:'علامت نقل قول تکی راست',ldquo:'علامت دوتایی نقل قول چپ',rdquo:'علامت دوتایی نقل قول راست',ndash:'En dash',mdash:'Em dash',iexcl:'علامت گذاری به عنوان علامت تعجب وارونه',cent:'نشان سنت',pound:'نشان پوند',curren:'نشان ارز',yen:'نشان ین',brvbar:'نوار شکسته',sect:'نشان بخش',uml:'Diaeresis',copy:'نشان کپی رایت',ordf:'Feminine ordinal indicator',laquo:'Left-pointing double angle quotation mark',not:'علامت ثبت نشده',reg:'علامت ثبت شده',macr:'Macron',deg:'نشان درجه',sup2:'بالانویس دو',sup3:'بالانویس سه',acute:'لهجه غلیظ',micro:'نشان مایکرو',para:'Pilcrow sign',middot:'نقطه میانی',cedil:'Cedilla',sup1:'Superscript one',ordm:'Masculine ordinal indicator',raquo:'نشان زاویهدار دوتایی نقل قول راست چین',frac14:'Vulgar fraction one quarter',frac12:'Vulgar fraction one half',frac34:'Vulgar fraction three quarters',iquest:'Inverted question mark',Agrave:'Latin capital letter A with grave accent',Aacute:'Latin capital letter A with acute accent',Acirc:'Latin capital letter A with circumflex',Atilde:'Latin capital letter A with tilde',Auml:'Latin capital letter A with diaeresis',Aring:'Latin capital letter A with ring above',AElig:'Latin Capital letter Æ',Ccedil:'Latin capital letter C with cedilla',Egrave:'Latin capital letter E with grave accent',Eacute:'Latin capital letter E with acute accent',Ecirc:'Latin capital letter E with circumflex',Euml:'Latin capital letter E with diaeresis',Igrave:'Latin capital letter I with grave accent',Iacute:'Latin capital letter I with acute accent',Icirc:'Latin capital letter I with circumflex',Iuml:'Latin capital letter I with diaeresis',ETH:'Latin capital letter Eth',Ntilde:'Latin capital letter N with tilde',Ograve:'Latin capital letter O with grave accent',Oacute:'Latin capital letter O with acute accent',Ocirc:'Latin capital letter O with circumflex',Otilde:'Latin capital letter O with tilde',Ouml:'Latin capital letter O with diaeresis',times:'Multiplication sign',Oslash:'Latin capital letter O with stroke',Ugrave:'Latin capital letter U with grave accent',Uacute:'Latin capital letter U with acute accent',Ucirc:'Latin capital letter U with circumflex',Uuml:'Latin capital letter U with diaeresis',Yacute:'Latin capital letter Y with acute accent',THORN:'Latin capital letter Thorn',szlig:'Latin small letter sharp s',agrave:'Latin small letter a with grave accent',aacute:'Latin small letter a with acute accent',acirc:'Latin small letter a with circumflex',atilde:'Latin small letter a with tilde',auml:'Latin small letter a with diaeresis',aring:'Latin small letter a with ring above',aelig:'Latin small letter æ',ccedil:'Latin small letter c with cedilla',egrave:'Latin small letter e with grave accent',eacute:'Latin small letter e with acute accent',ecirc:'Latin small letter e with circumflex',euml:'Latin small letter e with diaeresis',igrave:'Latin small letter i with grave accent',iacute:'Latin small letter i with acute accent',icirc:'Latin small letter i with circumflex',iuml:'Latin small letter i with diaeresis',eth:'Latin small letter eth',ntilde:'Latin small letter n with tilde',ograve:'Latin small letter o with grave accent',oacute:'Latin small letter o with acute accent',ocirc:'Latin small letter o with circumflex',otilde:'Latin small letter o with tilde',ouml:'Latin small letter o with diaeresis',divide:'Division sign',oslash:'Latin small letter o with stroke',ugrave:'Latin small letter u with grave accent',uacute:'Latin small letter u with acute accent',ucirc:'Latin small letter u with circumflex',uuml:'Latin small letter u with diaeresis',yacute:'Latin small letter y with acute accent',thorn:'Latin small letter thorn',yuml:'Latin small letter y with diaeresis',OElig:'Latin capital ligature OE',oelig:'Latin small ligature oe',372:'Latin capital letter W with circumflex',374:'Latin capital letter Y with circumflex',373:'Latin small letter w with circumflex',375:'Latin small letter y with circumflex',sbquo:'Single low-9 quotation mark',8219:'Single high-reversed-9 quotation mark',bdquo:'Double low-9 quotation mark',hellip:'Horizontal ellipsis',trade:'Trade mark sign',9658:'Black right-pointing pointer',bull:'Bullet',rarr:'Rightwards arrow',rArr:'Rightwards double arrow',hArr:'جهتنمای دوتایی چپ به راست',diams:'Black diamond suit',asymp:'تقریبا برابر با'});
+/*
+Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+CKEDITOR.plugins.setLang( 'specialchar', 'fa',
+{
+	euro: 'نشان یورو',
+	lsquo: 'علامت نقل قول تکی چپ',
+	rsquo: 'علامت نقل قول تکی راست',
+	ldquo: 'علامت دوتایی نقل قول چپ',
+	rdquo: 'علامت دوتایی نقل قول راست',
+	ndash: 'En dash', // MISSING
+	mdash: 'Em dash', // MISSING
+	iexcl: 'علامت گذاری به عنوان علامت تعجب وارونه',
+	cent: 'نشان سنت',
+	pound: 'نشان پوند',
+	curren: 'نشان ارز',
+	yen: 'نشان ین',
+	brvbar: 'نوار شکسته',
+	sect: 'نشان بخش',
+	uml: 'Diaeresis', // MISSING
+	copy: 'نشان کپی رایت',
+	ordf: 'Feminine ordinal indicator', // MISSING
+	laquo: 'Left-pointing double angle quotation mark', // MISSING
+	not: 'علامت ثبت نشده',
+	reg: 'علامت ثبت شده',
+	macr: 'Macron', // MISSING
+	deg: 'نشان درجه',
+	sup2: 'بالانویس دو',
+	sup3: 'بالانویس سه',
+	acute: 'لهجه غلیظ',
+	micro: 'نشان مایکرو',
+	para: 'Pilcrow sign', // MISSING
+	middot: 'نقطه میانی',
+	cedil: 'Cedilla', // MISSING
+	sup1: 'Superscript one', // MISSING
+	ordm: 'Masculine ordinal indicator', // MISSING
+	raquo: 'نشان زاویه‌دار دوتایی نقل قول راست چین',
+	frac14: 'Vulgar fraction one quarter', // MISSING
+	frac12: 'Vulgar fraction one half', // MISSING
+	frac34: 'Vulgar fraction three quarters', // MISSING
+	iquest: 'Inverted question mark', // MISSING
+	Agrave: 'Latin capital letter A with grave accent', // MISSING
+	Aacute: 'Latin capital letter A with acute accent', // MISSING
+	Acirc: 'Latin capital letter A with circumflex', // MISSING
+	Atilde: 'Latin capital letter A with tilde', // MISSING
+	Auml: 'Latin capital letter A with diaeresis', // MISSING
+	Aring: 'Latin capital letter A with ring above', // MISSING
+	AElig: 'Latin Capital letter Æ', // MISSING
+	Ccedil: 'Latin capital letter C with cedilla', // MISSING
+	Egrave: 'Latin capital letter E with grave accent', // MISSING
+	Eacute: 'Latin capital letter E with acute accent', // MISSING
+	Ecirc: 'Latin capital letter E with circumflex', // MISSING
+	Euml: 'Latin capital letter E with diaeresis', // MISSING
+	Igrave: 'Latin capital letter I with grave accent', // MISSING
+	Iacute: 'Latin capital letter I with acute accent', // MISSING
+	Icirc: 'Latin capital letter I with circumflex', // MISSING
+	Iuml: 'Latin capital letter I with diaeresis', // MISSING
+	ETH: 'Latin capital letter Eth', // MISSING
+	Ntilde: 'Latin capital letter N with tilde', // MISSING
+	Ograve: 'Latin capital letter O with grave accent', // MISSING
+	Oacute: 'Latin capital letter O with acute accent', // MISSING
+	Ocirc: 'Latin capital letter O with circumflex', // MISSING
+	Otilde: 'Latin capital letter O with tilde', // MISSING
+	Ouml: 'Latin capital letter O with diaeresis', // MISSING
+	times: 'Multiplication sign', // MISSING
+	Oslash: 'Latin capital letter O with stroke', // MISSING
+	Ugrave: 'Latin capital letter U with grave accent', // MISSING
+	Uacute: 'Latin capital letter U with acute accent', // MISSING
+	Ucirc: 'Latin capital letter U with circumflex', // MISSING
+	Uuml: 'Latin capital letter U with diaeresis', // MISSING
+	Yacute: 'Latin capital letter Y with acute accent', // MISSING
+	THORN: 'Latin capital letter Thorn', // MISSING
+	szlig: 'Latin small letter sharp s', // MISSING
+	agrave: 'Latin small letter a with grave accent', // MISSING
+	aacute: 'Latin small letter a with acute accent', // MISSING
+	acirc: 'Latin small letter a with circumflex', // MISSING
+	atilde: 'Latin small letter a with tilde', // MISSING
+	auml: 'Latin small letter a with diaeresis', // MISSING
+	aring: 'Latin small letter a with ring above', // MISSING
+	aelig: 'Latin small letter æ', // MISSING
+	ccedil: 'Latin small letter c with cedilla', // MISSING
+	egrave: 'Latin small letter e with grave accent', // MISSING
+	eacute: 'Latin small letter e with acute accent', // MISSING
+	ecirc: 'Latin small letter e with circumflex', // MISSING
+	euml: 'Latin small letter e with diaeresis', // MISSING
+	igrave: 'Latin small letter i with grave accent', // MISSING
+	iacute: 'Latin small letter i with acute accent', // MISSING
+	icirc: 'Latin small letter i with circumflex', // MISSING
+	iuml: 'Latin small letter i with diaeresis', // MISSING
+	eth: 'Latin small letter eth', // MISSING
+	ntilde: 'Latin small letter n with tilde', // MISSING
+	ograve: 'Latin small letter o with grave accent', // MISSING
+	oacute: 'Latin small letter o with acute accent', // MISSING
+	ocirc: 'Latin small letter o with circumflex', // MISSING
+	otilde: 'Latin small letter o with tilde', // MISSING
+	ouml: 'Latin small letter o with diaeresis', // MISSING
+	divide: 'Division sign', // MISSING
+	oslash: 'Latin small letter o with stroke', // MISSING
+	ugrave: 'Latin small letter u with grave accent', // MISSING
+	uacute: 'Latin small letter u with acute accent', // MISSING
+	ucirc: 'Latin small letter u with circumflex', // MISSING
+	uuml: 'Latin small letter u with diaeresis', // MISSING
+	yacute: 'Latin small letter y with acute accent', // MISSING
+	thorn: 'Latin small letter thorn', // MISSING
+	yuml: 'Latin small letter y with diaeresis', // MISSING
+	OElig: 'Latin capital ligature OE', // MISSING
+	oelig: 'Latin small ligature oe', // MISSING
+	'372': 'Latin capital letter W with circumflex', // MISSING
+	'374': 'Latin capital letter Y with circumflex', // MISSING
+	'373': 'Latin small letter w with circumflex', // MISSING
+	'375': 'Latin small letter y with circumflex', // MISSING
+	sbquo: 'Single low-9 quotation mark', // MISSING
+	'8219': 'Single high-reversed-9 quotation mark', // MISSING
+	bdquo: 'Double low-9 quotation mark', // MISSING
+	hellip: 'Horizontal ellipsis', // MISSING
+	trade: 'Trade mark sign', // MISSING
+	'9658': 'Black right-pointing pointer', // MISSING
+	bull: 'Bullet', // MISSING
+	rarr: 'Rightwards arrow', // MISSING
+	rArr: 'Rightwards double arrow', // MISSING
+	hArr: 'جهت‌نمای دوتایی چپ به راست',
+	diams: 'Black diamond suit', // MISSING
+	asymp: 'تقریبا برابر با'
+});
