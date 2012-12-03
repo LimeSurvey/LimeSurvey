d6fd322371e8b9ada578910b7733d77cc6e45a77
ravindrakhokharia <ravindrakhokharia@gmail.com>
2012-12-03 12:05:43 +0530

    #06992 Issue Fixed
    http://bugs.limesurvey.org/view.php?id=6992#c22803
    
    06992: After registering for a token based survey - the next page is missing template css
    The {TEMPLATECSS} placeholder is not being parsed.

--- application/controllers/RegisterController.php
+++ application/controllers/RegisterController.php
@@ -257,20 +257,23 @@ class RegisterController extends LSYii_Controller {
         sendCacheHeaders();
         doHeader();
         Yii::app()->lang = $clang;
-        foreach(file("$thistpl/startpage.pstpl") as $op)
-        {
-            echo templatereplace($op);
-        }
-        foreach(file("$thistpl/survey.pstpl") as $op)
-        {
-            echo "\t".templatereplace($op);
-        }
+        // fetch the defined variables and pass it to the header footer templates.
+        $redata = compact(array_keys(get_defined_vars()));
+        $this->_printTemplateContent($thistpl.'/startpage.pstpl', $redata, __LINE__);
+        $this->_printTemplateContent($thistpl.'/survey.pstpl', $redata, __LINE__);
         echo $html;
-        foreach(file("$thistpl/endpage.pstpl") as $op)
-        {
-            echo templatereplace($op);
-        }
+        $this->_printTemplateContent($thistpl.'/endpage.pstpl', $redata, __LINE__);
+        
         doFooter();
+    }
+    
+    /**
+    * function will parse the templates data
+    * @return displays the requested template
+    */
+    function _printTemplateContent($sTemplateFile, &$redata, $iDebugLine = -1)
+    {
+        echo templatereplace(file_get_contents($sTemplateFile),array(),$redata,'survey['.$iDebugLine.']');
     }
 
 }
