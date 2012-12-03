95174bcb4486f11db2ed61ff2dfc10ac2798b159
Spiros Trougakos <trougakoss@noc.uoa.gr>
2012-12-03 11:30:46 +0200

    Fixed Issue : Remotecontrol errors in export_statistics Dev: Fixed corrupted file name in generate_statistics Dev: Sorted statistics questions output in remotecontrol Dev: Fixed question categories that did not produce proper stats Dev: Converted sql queries to Yii cdb in remotecontrol export_statistics

--- application/helpers/common_helper.php
+++ application/helpers/common_helper.php
@@ -2152,118 +2152,112 @@ function validateTemplateDir($sTemplateName)
  * @param string $sLanguage
  * @return array The summary
  */
- function createCompleteSGQA($iSurveyID,$aFilters,$sLanguage) {
+  function createCompleteSGQA($iSurveyID,$aFilters,$sLanguage) {
 
  foreach ($aFilters as $flt)
  {
-	Yii::app()->loadHelper("surveytranslator");
-	$myfield = "{$iSurveyID}X{$flt['gid']}X{$flt['qid']}";
-	$oSurvey = Survey::model()->findByPk($iSurveyID);
-	$aAdditionalLanguages = array_filter(explode(" ", $oSurvey->additional_languages));
-	if (is_null($sLanguage)|| !in_array($sLanguage,$aAdditionalLanguages))
-		$sLanguage = $oSurvey->language;
+    Yii::app()->loadHelper("surveytranslator");
+    $myfield = "{$iSurveyID}X{$flt['gid']}X{$flt['qid']}";
+    $oSurvey = Survey::model()->findByPk($iSurveyID);
+    $aAdditionalLanguages = array_filter(explode(" ", $oSurvey->additional_languages));
+    if (is_null($sLanguage)|| !in_array($sLanguage,$aAdditionalLanguages))
+        $sLanguage = $oSurvey->language;
 
-	switch ($flt['type'])
-		    {
-		        case "K": // Multiple Numerical
-		        case "Q": // Multiple Short Text
-		            //get answers
-		            $query = "SELECT title as code, question as answer FROM {{questions}} WHERE parent_qid=:flt_0 AND language = :lang ORDER BY question_order";
-		            $result =  Yii::app()->db->createCommand($query)->bindParam(":flt_0", $flt['qid'], PDO::PARAM_INT)->bindParam(":lang", $sLanguage, PDO::PARAM_STR)->queryAll();
+    switch ($flt['type'])
+            {
+                case "K": // Multiple Numerical
+                case "Q": // Multiple Short Text
+                    //get answers
+                    $result = Questions::model()->getQuestionsForStatistics('title as code, question as answer', "parent_qid=$flt[qid] AND language = '{$sLanguage}'", 'question_order');
 
-		            //go through all the (multiple) answers
-		            foreach($result as $row)
-		            {
-		                $myfield2=$flt['type'].$myfield.reset($row);
-		                $allfields[] = $myfield2;
-		            }
-		            break;
-		        case "A": // ARRAY OF 5 POINT CHOICE QUESTIONS
-		        case "B": // ARRAY OF 10 POINT CHOICE QUESTIONS
-		        case "C": // ARRAY OF YES\No\$clang->gT("Uncertain") QUESTIONS
-		        case "E": // ARRAY OF Increase/Same/Decrease QUESTIONS
-		        case "F": // FlEXIBLE ARRAY
-		        case "H": // ARRAY (By Column)
-		            //get answers
-		            $query = "SELECT title as code, question as answer FROM {{questions}} WHERE parent_qid=:flt_0 AND language = :lang ORDER BY question_order";
-					$result = Yii::app()->db->createCommand($query)->bindParam(":flt_0", $flt['qid'], PDO::PARAM_INT)->bindParam(":lang", $sLanguage, PDO::PARAM_STR)->queryAll();
+                    //go through all the (multiple) answers
+                    foreach($result as $row)
+                    {
+                        $myfield2=$flt['type'].$myfield.reset($row);
+                        $allfields[] = $myfield2;
+                    }
+                    break;
+                case "A": // ARRAY OF 5 POINT CHOICE QUESTIONS
+                case "B": // ARRAY OF 10 POINT CHOICE QUESTIONS
+                case "C": // ARRAY OF YES\No\$clang->gT("Uncertain") QUESTIONS
+                case "E": // ARRAY OF Increase/Same/Decrease QUESTIONS
+                case "F": // FlEXIBLE ARRAY
+                case "H": // ARRAY (By Column)
+                    //get answers
+                    $result = Questions::model()->getQuestionsForStatistics('title, question', "parent_qid=$flt[qid] AND language = '{$sLanguage}'", 'question_order');
 
-		            //go through all the (multiple) answers
-		            foreach($result as $row)
-		            {
-		                $myfield2 = $myfield.reset($row);
-		                $allfields[]=$myfield2;
-		            }
-		            break;
-		        // all "free text" types (T, U, S)  get the same prefix ("T")
-		        case "T": // Long free text
-		        case "U": // Huge free text
-		        case "S": // Short free text
-		            $myfield="T$myfield";
-		            $allfields[] = $myfield;
-		            break;
-		        case ";":  //ARRAY (Multi Flex) (Text)
-		        case ":":  //ARRAY (Multi Flex) (Numbers)
-                    $query = "SELECT title, question FROM {{questions}} WHERE parent_qid=:flt_0 AND language=:lang AND scale_id = 0 ORDER BY question_order";
-		            $result = Yii::app()->db->createCommand($query)->bindParam(":flt_0", $flt['qid'], PDO::PARAM_INT)->bindParam(":lang", $sLanguage, PDO::PARAM_STR)->queryAll();
-		            foreach($result as $row)
-		            {
-		                $fquery = "SELECT * FROM {{questions}} WHERE parent_qid = :flt_0 AND language = :lang AND scale_id = 1 ORDER BY question_order, title";
-		                $fresult = Yii::app()->db->createCommand($query)->bindParam(":flt_0", $flt['qid'], PDO::PARAM_INT)->bindParam(":lang", $sLanguage, PDO::PARAM_STR)->queryAll();
-		                foreach($fresult as $frow)
-		                {
-		                    $myfield2 = $myfield . reset($row) . "_" . $frow['title'];
-		                $allfields[]=$myfield2;
-		            }
-		            }
-		            break;
-		        case "R": //RANKING
-		            //get some answers
-		            $query = "SELECT code, answer FROM {{answers}} WHERE qid = :flt_0 AND language = :lang ORDER BY sortorder, answer";
-		            $result = Yii::app()->db->createCommand($query)->bindParam(":flt_0", $flt['qid'], PDO::PARAM_INT)->bindParam(":lang", $sLanguage, PDO::PARAM_STR)->queryAll();
+                    //go through all the (multiple) answers
+                    foreach($result as $row)
+                    {
+                        $myfield2 = $myfield.reset($row);
+                        $allfields[]=$myfield2;
+                    }
+                    break;
+                // all "free text" types (T, U, S)  get the same prefix ("T")
+                case "T": // Long free text
+                case "U": // Huge free text
+                case "S": // Short free text
+                    $myfield="T$myfield";
+                    $allfields[] = $myfield;
+                    break;
+                case ";":  //ARRAY (Multi Flex) (Text)
+                case ":":  //ARRAY (Multi Flex) (Numbers)
+                    $result = Questions::model()->getQuestionsForStatistics('title, question', "parent_qid=$flt[qid] AND language = '{$sLanguage}' AND scale_id = 0", 'question_order');
+                   
+                    foreach($result as $row)
+                    {
+                        $fresult = Questions::model()->getQuestionsForStatistics('title, question', "parent_qid=$flt[qid] AND language = '{$sLanguage}' AND scale_id = 1", 'question_order');
+                        foreach($fresult as $frow)
+                        {
+                            $myfield2 = $myfield . reset($row) . "_" . $frow['title'];
+                        $allfields[]=$myfield2;
+                    }
+                    }
+                    break;
+                case "R": //RANKING
+                    //get some answers
+                    $result = Answers::model()->getQuestionsForStatistics('code, answer', "qid=$flt[qid] AND language = '{$sLanguage}'", 'sortorder, answer');
+                    //get number of answers
+                    //loop through all answers. if there are 3 items to rate there will be 3 statistics
+                    $i=0;
+                    foreach($result as $row)
+                    {
+                        $i++;
+                        $myfield2 = "R" . $myfield . $i . "-" . strlen($i);
+                        $allfields[]=$myfield2;
+                    }
 
-		            //get number of answers
-		            $count = $result->num_rows();
+                    break;
+                //Boilerplate questions are only used to put some text between other questions -> no analysis needed
+                case "X":  //This is a boilerplate question and it has no business in this script
+                    break;
+                case "1": // MULTI SCALE
+                    //get answers
+                    $result = Questions::model()->getQuestionsForStatistics('title, question', "parent_qid=$flt[qid] AND language = '{$sLanguage}'", 'question_order');
+                    //loop through answers
+                    foreach($result as $row)
+                    {
+                        //----------------- LABEL 1 ---------------------
+                        $myfield2 = $myfield . reset($row)."#0";
+                        $allfields[]=$myfield2;
+                        //----------------- LABEL 2 ---------------------
+                        $myfield2 = $myfield . reset($row)."#1";
+                        $allfields[]=$myfield2;
+                    }   //end WHILE -> loop through all answers
+                    break;
 
-		            //loop through all answers. if there are 3 items to rate there will be 3 statistics
-		            for ($i=1; $i<=$count; $i++)
-		            {
-		                $myfield2 = "R" . $myfield . $i . "-" . strlen($i);
-		                $allfields[]=$myfield2;
-		            }
-		            break;
-		        //Boilerplate questions are only used to put some text between other questions -> no analysis needed
-		        case "X":  //This is a boilerplate question and it has no business in this script
-		            break;
-		        case "1": // MULTI SCALE
-		            //get answers
-		            $query = "SELECT title, question FROM {{questions}} WHERE parent_qid = :flt_0 AND language = :lang ORDER BY question_order";
-		            $result = Yii::app()->db->createCommand($query)->bindParam(":flt_0", $flt['qid'], PDO::PARAM_INT)->bindParam(":lang", $sLanguage, PDO::PARAM_STR)->queryAll();
+                case "P":  //P - Multiple choice with comments
+                case "M":  //M - Multiple choice
+                case "N":  //N - Numerical input
+                case "D":  //D - Date
+                    $myfield2 = $flt['type'].$myfield;
+                            $allfields[]=$myfield2;
+                    break;
+                default:   //Default settings
+                    $allfields[] = $myfield;
+                    break;
 
-		            //loop through answers
-		            foreach($result as $row)
-		            {
-		                //----------------- LABEL 1 ---------------------
-		                $myfield2 = $myfield . reset($row[0])."#0";
-		                $allfields[]=$myfield2;
-		                //----------------- LABEL 2 ---------------------
-		                $myfield2 = $myfield . "$row[0]#1";
-		                $allfields[]=$myfield2;
-		            }	//end WHILE -> loop through all answers
-		            break;
-
-		        case "P":  //P - Multiple choice with comments
-		        case "M":  //M - Multiple choice
-		        case "N":  //N - Numerical input
-		        case "D":  //D - Date
-		            $myfield2 = $flt['type'].$myfield;
-		                    $allfields[]=$myfield2;
-		            break;
-		        default:   //Default settings
-		            $allfields[] = $myfield;
-		            break;
-
-		} //end switch
+        } //end switch
  }
 
 return $allfields;
