e8144a36b1f78ac8bf44f46785eed047b050e735
Menno Dekker <mennodekker@users.sourceforge.net>
2012-12-06 21:10:37 +0100

    Fixed #7028: Editing a participant in the CPD gives CDbException error

--- application/models/Participants.php
+++ application/models/Participants.php
@@ -142,19 +142,29 @@ class Participants extends CActiveRecord
         Yii::app()->db->createCommand()->insert('{{participants}}', $data);
     }
 
-    /*
-     * This function updates the data edited in the jqgrid
-     * Parameters : data that is edited
-     * Return Data : None
+    /**
+     * Returns the primary key of this table
+     *
+     * @access public
+     * @return string
      */
+    public function primaryKey() {
+        return 'participant_id';
+    }
 
+    /**
+     * This function updates the data edited in the jqgrid
+     * 
+     * @param aray $data
+     */
     function updateRow($data)
     {
-        Yii::app()->db->createCommand()
-                  ->update('{{participants}}',
-                           $data,
-                           'participant_id = :participant_id',
-                           array(':participant_id'=>$data['participant_id']));
+        $record = $this->findByPk($data['participant_id']);
+        foreach ($data as $key => $value)
+        {
+            $record->$key = $value;
+        }
+        $record->save();
     }
 
     /*
