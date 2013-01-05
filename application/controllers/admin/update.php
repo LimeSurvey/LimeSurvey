54ed7595c6880fc533865879671b35416e91081d
Carsten Schmitz <c_schmitz@users.sourceforge.net>
2013-01-05 15:10:23 +0100

    Fixed issue #7139: Warning after updating from 2.00 to most current update release

--- application/controllers/admin/update.php
+++ application/controllers/admin/update.php
@@ -367,12 +367,11 @@ class update extends Survey_Common_Action
             {
                 if (is_file($rootdir.$afile['file']))
                 {
-                    unlink($rootdir.$afile['file']);
+                    @unlink($rootdir.$afile['file']);
                 }
                 else{
                     rmdirr($rootdir.$afile['file']);
                 }
-                echo sprintf($clang->gT('File deleted: %s'),$afile['file']).'<br />';
             }
         }
 
