03c7bb0caa9b3ce9832b97c59841f8c1265be87a
Menno Dekker <menno.dekker@erasmusmc.nl>
2013-01-11 12:48:32 +0100

    dev: public statistics can not switch chart on/off or from pie to bar (links don't work)

--- application/helpers/admin/statistics_helper.php
+++ application/helpers/admin/statistics_helper.php
@@ -2994,7 +2994,9 @@ class statistics_helper {
 
         //close table/output
         if($outputType=='html') {
-            if ($usegraph==1) {
+            // show this block only when we show graphs and are not in the public statics controller
+            // this is because the links don't work from that controller
+            if ($usegraph==1 && get_class(Yii::app()->getController()) !== 'Statistics_userController') {
                 $sImgUrl = Yii::app()->getConfig('adminimageurl');
 
                 $statisticsoutput .= "</td></tr><tr><td colspan='4'><div id='stats_$rt' class='graphdisplay' style=\"text-align:center\">"
