e9ef65747c9fb76d3713b5457ea81f7fa2033747
Carsten Schmitz <c_schmitz@users.sourceforge.net>
2013-01-15 09:58:06 +0100

    Fixed issue: Dataset after query was still open creating problems on previewing a survey

--- application/helpers/frontend_helper.php
+++ application/helpers/frontend_helper.php
@@ -2233,10 +2233,11 @@ function UpdateGroupList($surveyid, $language)
     }
 }
 
+/**
+* FieldArray contains all necessary information regarding the questions
+* This function is needed to update it in case the survey is switched to another language
+*/
 function UpdateFieldArray()
-//The FieldArray contains all necessary information regarding the questions
-//This function is needed to update it in case the survey is switched to another language
-
 {
     global $surveyid;
 
@@ -2249,16 +2250,14 @@ function UpdateFieldArray()
         {
             $q = clone $_SESSION['survey_'.$surveyid]['questions'][$key];
 
-            $query = "SELECT * FROM {{questions}} WHERE qid=".$q->id." AND language='".$_SESSION['survey_'.$surveyid]['s_lang']."'";
-            $result = dbExecuteAssoc($query) or safeDie ("Couldn't get question <br />$query<br />");      //Checked
-            $row = $result->read();
-            $q->title=$row['title'];
-            $q->text=$row['question'];
-
-            $_SESSION['survey_'.$surveyid]['questions'][$key] = $q;
+            $query = "SELECT title, question FROM {{questions}} WHERE qid=".$questionarray[0]." AND language='".$_SESSION['survey_'.$surveyid]['s_lang']."'";
+            $usrow = Yii::app()->db->createCommand($query)->queryRow();
+            if (!$usrow) safeDie ("Couldn't get question <br />$query<br />");      //Checked
+            $questionarray[2]=$usrow['title'];
+            $questionarray[3]=$usrow['question'];
+            unset($questionarray);
         }
     }
-
 }
 
 /**
