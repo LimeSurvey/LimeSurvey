ea930c21638c6970db20dd7abecb7c6163e633f4
Carsten Schmitz <c_schmitz@users.sourceforge.net>
2013-01-22 13:06:40 +0100

    Fixed issue #7199: Raising the token's "uses left" value from 0 to X doesn't remove the "completed" date

--- application/models/Tokens_dynamic.php
+++ application/models/Tokens_dynamic.php
@@ -339,6 +339,26 @@ class Tokens_dynamic extends LSActiveRecord
 
         return array($newtokencount,count($tkresult));
     }
+    
+    
+     /**
+     * This method is invoked before saving a record (after validation, if any).
+     * The default implementation raises the {@link onBeforeSave} event.
+     * You may override this method to do any preparation work for record saving.
+     * Use {@link isNewRecord} to determine whether the saving is
+     * for inserting or updating record.
+     * Make sure you call the parent implementation so that the event is raised properly.
+     * @return boolean whether the saving should be executed. Defaults to true.
+     */
+    public function beforeSave()
+    {
+         // Postgres delivers bytea fields as streams :-o - if this is not done it looks like Postgres saves something unexpected
+        if ($this->usesleft>0)
+        {
+            $this->completed='N'; 
+        }
+        return parent::beforeSave();
+    }
 
     public function search()
 	{
