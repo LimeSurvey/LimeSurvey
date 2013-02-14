6d7884ea39afdc08cd788e5b116d9b460de4a6c6
Carsten Schmitz <c_schmitz@users.sourceforge.net>
2013-02-14 15:41:41 +0100

    Fixed issue #7296: Print version crashes in multi language surveys on MSSQL - thanks to user 'reslogic'

--- application/controllers/admin/printablesurvey.php
+++ application/controllers/admin/printablesurvey.php
@@ -284,7 +284,7 @@ class printablesurvey extends Survey_Common_Action
                         $x=0;
 
                         $conditions1="qid={$deqrow['qid']} AND scenario={$scenariorow['scenario']}";
-                        $distinctresult=Conditions::model()->getSomeConditions(array('cqid','method', 'cfieldname', 'value'), $conditions1, array('cqid'),array('cqid', 'method'));
+                        $distinctresult=Conditions::model()->getSomeConditions(array('cqid','method', 'cfieldname', 'value'), $conditions1, array('cqid'),array('cqid', 'method','cfieldname'));
 
                         //Loop through each condition for a particular scenario.
                         foreach ($distinctresult->readAll() as $distinctrow)
