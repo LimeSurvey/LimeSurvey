e3706b1c0ee4806221a78a1676f5798023cf5cad
Carsten Schmitz <c_schmitz@users.sourceforge.net>
2013-02-15 14:56:24 +0100

    Fixed issue #7049: Webserver authentication not working when external access control is used

--- application/core/UserIdentity.php
+++ application/core/UserIdentity.php
@@ -67,10 +67,13 @@ class UserIdentity extends CUserIdentity
                 $this->errorCode = self::ERROR_NONE;
             }
         }
-        elseif(Yii::app()->getConfig("auth_webserver") === true && (isset($_SERVER['PHP_AUTH_USER'])||isset($_SERVER['LOGON_USER']))) // normal login through webserver authentication        
+        elseif(Yii::app()->getConfig("auth_webserver") === true && (isset($_SERVER['PHP_AUTH_USER'])||isset($_SERVER['LOGON_USER']) ||isset($_SERVER['REMOTE_USER']))) // normal login through webserver authentication        
         {
             if (isset($_SERVER['PHP_AUTH_USER'])) {
                 $sUser=$_SERVER['PHP_AUTH_USER'];
+            }
+            elseif (isset($_SERVER['REMOTE_USER'])) {
+                $sUser=$_SERVER['REMOTE_USER'];
             } else {
                 $sUser = $_SERVER['LOGON_USER'];
                 $sUser = substr($sUser, strrpos($sUser, "\\")+1);
