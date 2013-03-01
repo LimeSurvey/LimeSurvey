97a166e40b6360d71c8541e2d0330d4f38ca1c45
Carsten Schmitz <c_schmitz@users.sourceforge.net>
2013-03-01 14:42:21 +0100

    Fixed issue #7629: Fatal error on unexpected key/data when using RPC function add_response

--- application/controllers/admin/remotecontrol.php
+++ application/controllers/admin/remotecontrol.php
@@ -2256,6 +2256,8 @@ class remotecontrol_handle
 
             Survey_dynamic::sid($iSurveyID);
             $survey_dynamic = new Survey_dynamic;
+            $aBasicDestinationFields=$survey_dynamic->tableSchema->columnNames;
+            $aResponseData=array_intersect($aResponseData,$aBasicDestinationFields);
             $result_id = $survey_dynamic->insertRecords($aResponseData);
 
             if ($result_id)
