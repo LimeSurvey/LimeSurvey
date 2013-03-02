12875b7a7e3bd01e07ebcb6dedcf053ce5b4638a
Carsten Schmitz <c_schmitz@users.sourceforge.net>
2013-03-02 15:33:29 +0100

    Fixed issue #7328: Quotas can not be viewed / processed in an active poll

--- application/helpers/common_helper.php
+++ application/helpers/common_helper.php
@@ -4744,7 +4744,9 @@ function getQuotaCompletedCount($iSurveyId, $quotaid)
 
             if (!in_array($fieldname, $fields_list)) $fields_list[] = $fieldname;
 
-            $criteria->addColumnCondition(array($fieldname => $member), 'OR');
+                // Yii does not quote column names (duh!) so we have to do it.
+                $criteria->addColumnCondition(array(Yii::app()->db->quoteColumnName($fieldname) => $member['value']), 'OR');
+            }
 
             $fields_query[$fieldname] = $criteria;
         }
