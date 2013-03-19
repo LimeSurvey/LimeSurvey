970306ea553a7cd92e4b897753e2f35d0b9ceae1
Denis Chenu <courriel@shnoulle.net>
2013-03-19 11:44:41 +0100

    Dev: qtip2 for Plugons too Dev: have a javascript broke in jquery-bbq-plugin ($.browser)

--- application/views/layouts/main.php
+++ application/views/layouts/main.php
@@ -4,14 +4,14 @@
         <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
         <?php 
             App()->getClientScript()->registerPackage('jqueryui');
-            App()->getClientScript()->registerPackage('jquery-qtip');
+            App()->getClientScript()->registerPackage('qTip2');
             App()->getClientScript()->registerPackage('jquery-superfish');
             App()->getClientScript()->registerPackage('jquery-cookie');
             App()->getClientScript()->registerScriptFile(Yii::app()->getConfig('adminscripts') . "admin_core.js");
             App()->getClientScript()->registerCssFile(Yii::app()->getConfig('adminstyleurl') . "jquery-ui/jquery-ui.css" );
             App()->getClientScript()->registerCssFile(Yii::app()->getConfig('adminstyleurl') . "printablestyle.css", 'print');
-            App()->getClientScript()->registerCssFile(Yii::app()->getConfig('adminstyleurl') . "adminstyle.css" );
             App()->getClientScript()->registerCssFile(Yii::app()->getConfig('styleurl') . "adminstyle.css" );
+            App()->getClientScript()->registerCssFile(Yii::app()->getConfig('adminstyleurl') . "adminstyle.css" );
             App()->getClientScript()->registerPackage('jqgrid');
 
             App()->getClientScript()->registerCssFile(Yii::app()->getConfig('publicstyleurl') . 'jquery.multiselect.css');
