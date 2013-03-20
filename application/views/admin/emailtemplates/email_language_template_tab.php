b6dd0429ea1ea19af3fc0f217155eb1718d4867b
Sam Mousa <sam@mousa.nl>
2013-03-20 15:31:44 +0100

    Dev Javascript fixes.

---
+++ application/views/admin/emailtemplates/email_language_template_tab.php
@@ -0,0 +1,66 @@
+
+<div id='<?php echo "tab-$grouplang-$tab"; ?>'>
+<ul>
+    <li>
+        <label for='email_<?php echo $tab; ?>_subj_<?php echo $grouplang; ?>'><?php echo $details['subject'] ?></label>
+        
+        <input type='text' size='80' name='<?php echo "email_{$tab}_subj_$grouplang"; ?>' id='<?php echo "email_{$tab}_subj_{$grouplang}"; ?>' value="<?php echo $esrow->{$details['field']['subject']}; ?>" />
+        
+        <input 
+            type='button' 
+            value='<?php $clang->eT("Use default"); ?>' 
+            class="fillin"
+            data-target="<?php echo "email_{$tab}_subj_{$grouplang}"; ?>"
+            data-value="<?php echo $details['default']['subject']; ?>"
+        />
+    </li>
+    <li><label for='email_<?php echo $tab; ?>_<?php echo $grouplang; ?>'><?php echo $details['body']; ?></label>
+        <textarea cols='80' rows='20' name='email_<?php echo $tab; ?>_<?php echo $grouplang; ?>' id='<?php echo "email_{$tab}_{$grouplang}"; ?>'><?php echo htmlspecialchars($esrow->{$details['field']['body']}); ?></textarea>
+       <?php 
+       echo getEditor("email-$tab","email_{$tab}_$grouplang", "",$surveyid,'','','editemailtemplates'); 
+       ?>
+        <input 
+            type='button' 
+            value='<?php $clang->eT("Use default"); ?>' 
+            class="fillin"
+            data-target="<?php echo "email_{$tab}_{$grouplang}"; ?>"
+            data-value="<?php echo htmlspecialchars(conditionalNewlineToBreak($details['default']['body'],$ishtml),ENT_QUOTES); ?>"
+        />
+    </li>
+    <li>
+        <label for="attachments_<?php echo "{$grouplang}-{$tab}"; ?>"><?php echo $details['attachments']; ?></label>
+        <div style="float: left; width: 60%;">
+        <button class="add-attachment" id="add-attachment-<?php echo "{$grouplang}-{$tab}"; ?>">Add file</button>
+        
+        <table data-template="[<?php echo $grouplang; ?>][<?php echo $tab ?>]" id ="attachments-<?php echo $grouplang; ?>-<?php echo $tab ?>" class="attachments" style="width: 500px">
+            <tr>
+                <th>Action</th>
+                <th>Filename</th>
+                <th>Size</th>
+                <th>Relevance</th>
+            </tr>
+            <?php
+            
+                if (isset($esrow->attachments[$tab]))
+                {
+                    $script = array();
+                    foreach ($esrow->attachments[$tab] as $attachment)
+                    {
+                        
+                        $script[] = sprintf("addAttachment($('#attachments-%s-%s'), %s, %s, %s );", $grouplang, $tab, json_encode($attachment['url']), json_encode($attachment['relevance']), json_encode($attachment['size']));
+                    }
+                    echo '<script type="text/javascript">';
+                    echo '$(document).ready(function() {';
+                    echo implode("\n", $script);
+                    echo '});';
+                    echo '</script>';
+                }
+            ?>
+        </table>
+        </div>
+    </li>
+</ul>
+</div>
+
+
+    
