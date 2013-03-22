25deba9b85ac29a32a1449a3d189f8c4d6e7a99c
Denis Chenu <courriel@shnoulle.net>
2013-03-22 17:22:50 +0100

    Dev: don't show error with /index.php/uploader/ Dev: just quick fix

--- application/controllers/UploaderController.php
+++ application/controllers/UploaderController.php
@@ -16,7 +16,14 @@
 class UploaderController extends AdminController {
 	function run($actionID)
 	{
-        $surveyid= $_SESSION['LEMsid'];
+	    if(isset($_SESSION['LEMsid']))
+	    {
+            $surveyid= $_SESSION['LEMsid'];
+	    }
+	    else
+	    {
+	        die();// Without error
+	    }
         if (isset($_SESSION['survey_'.$surveyid]['s_lang']))
         {
             $sLanguage = $_SESSION['survey_'.$surveyid]['s_lang'];
@@ -237,6 +244,7 @@ class UploaderController extends AdminController {
 		    }
 		return;
 		}
+		$clang = Yii::app()->lang;
 		$meta ='<script type="text/javascript" src="'.Yii::app()->getConfig("generalscripts").'jquery/jquery.js"></script>';
 		$meta .= '<script type="text/javascript">
 		    var uploadurl = "'.$this->createUrl('/uploader/index/mode/upload/').'";
@@ -245,7 +253,7 @@ class UploaderController extends AdminController {
 		    var fieldname = "'.$param['fieldname'].'";
 		    var questgrppreview  = '.$param['preview'].';
 		</script>';
-		echo "<script type='text/javascript'>
+		$meta .= "<script type='text/javascript'>
 		        translt = {
 		             titleFld: '" . $clang->gT('Title','js') . "',
 		             commentFld: '" . $clang->gT('Comment','js') . "',
@@ -264,8 +272,6 @@ class UploaderController extends AdminController {
 		$meta .='<script type="text/javascript" src="'.Yii::app()->getConfig("generalscripts").'/ajaxupload.js"></script>
 		<script type="text/javascript" src="'.Yii::app()->getConfig("generalscripts").'/uploader.js"></script>
 		<link type="text/css" href="'.Yii::app()->getConfig("publicstyleurl").'uploader.css" rel="stylesheet" />';
-
-		$clang = Yii::app()->lang;
 
 		$header = getHeader($meta);
 
