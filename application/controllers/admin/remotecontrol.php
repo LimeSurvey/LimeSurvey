f2e0962d30bdf1c565c161f0a8c2ffc01ddfd4d8
trougakoss <trougakoss@noc.uoa.gr>
2013-04-16 22:42:52 +0300

    Fixed issue #7754: Retrieving list of participants with additional token information

--- application/controllers/admin/remotecontrol.php
+++ application/controllers/admin/remotecontrol.php
@@ -2031,11 +2031,11 @@ class remotecontrol_handle
 	}
 
 
-
    /**
     * RPC Routine to return the ids and info  of token/participants of a survey.
     * if $bUnused is true, user will get the list of not completed tokens (token_return functionality).
     * Parameters iStart and ilimit are used to limit the number of results of this call.
+    * Parameter aAttributes is an optional array containing more attribute that may be requested
     *
     * @access public
     * @param string $sSessionKey Auth credentials
@@ -2043,9 +2043,10 @@ class remotecontrol_handle
     * @param int $iStart Start id of the token list
     * @param int  $iLimit Number of participants to return
     * @param bool $bUnused If you want unused tokensm, set true
+    * @param bool|array $aAttributes The extented attributes that we want
     * @return array The list of tokens
     */
-	public function list_participants($sSessionKey, $iSurveyID, $iStart=0, $iLimit=10, $bUnused=false)
+	public function list_participants($sSessionKey, $iSurveyID, $iStart=0, $iLimit=10, $bUnused=false,$aAttributes=false)
 	{
        if ($this->_checkSessionKey($sSessionKey))
        {
@@ -2066,9 +2067,16 @@ class remotecontrol_handle
 				if(count($oTokens)==0)
 					return array('status' => 'No Tokens found');
 
+				if($aAttributes) {
+					$aBasicDestinationFields=Tokens_dynamic::model()->tableSchema->columnNames;
+					$aTokenProperties=array_intersect($aAttributes,$aBasicDestinationFields);
+					$currentAttributes = array('tid','token','firstname','lastname','email');
+					$extendedAttributes = array_diff($aTokenProperties, $currentAttributes);
+				}
+
 				foreach ($oTokens as $token)
 					{
-						$aData[] = array(
+						$dataArray = array(
 									'tid'=>$token->primarykey,
 									'token'=>$token->attributes['token'],
 									'participant_info'=>array(
@@ -2076,6 +2084,11 @@ class remotecontrol_handle
 														'lastname'=>$token->attributes['lastname'],
 														'email'=>$token->attributes['email'],
 														    ));
+						
+						foreach($extendedAttributes as $sAttribute)
+							$dataArray[$sAttribute]=$token->attributes[$sAttribute];								
+								    
+						$aData[]= $dataArray;								    														    
 					}
 				return $aData;
 			}
@@ -2085,6 +2098,8 @@ class remotecontrol_handle
         else
             return array('status' => 'Invalid Session Key');
 	}
+
+
 
     /**
      * RPC routine to to initialise the survey's collection of tokens where new participant tokens may be later added.
