fa89f194791e4f7ba90fc79da781f66ec3deaff1
Carsten Schmitz <c_schmitz@users.sourceforge.net>
2013-05-26 22:44:48 +0200

    Fixed issue #7073: Rubbish text after upgrading in ComfortUpdate step 4

--- application/controllers/admin/update.php
+++ application/controllers/admin/update.php
@@ -425,9 +425,7 @@ class update extends Survey_Common_Action
         setGlobalSetting('updateversion','');
         // We create this new language object here because the language files might have been overwritten earlier
         // and the pointers to the file from the application language are not valid anymore 
-        $aLanguage = new Limesurvey_lang(Yii::app()->session['adminlang']);
-        $aData['clang'] = $aLanguage;
-
+        Yii::app()->lang = $aData['clang'] = new Limesurvey_lang(Yii::app()->session['adminlang']);
         $this->_renderWrappedTemplate('update', 'step4', $aData);
     }
 
