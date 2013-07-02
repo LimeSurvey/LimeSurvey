e14b3b938c00896d048873e0bb5d91ae3982990c
Menno Dekker <menno.dekker@erasmusmc.nl>
2013-07-02 13:12:17 +0200

    Fixed #7948: Unable to change email server settings dev: maybe more scripts need a change like this due to jquery update

--- scripts/admin/globalsettings.js
+++ scripts/admin/globalsettings.js
@@ -39,11 +39,11 @@ function Emailchange(ui,evt)
     smtp_enabled=($("#emailmethod").val()=='smtp');
     if (smtp_enabled==true) {smtp_enabled='';}
     else {smtp_enabled='disabled';}
-    $("#emailsmtphost").attr('disabled',smtp_enabled);
-    $("#emailsmtpuser").attr('disabled',smtp_enabled);
-    $("#emailsmtppassword").attr('disabled',smtp_enabled);
-    $("#emailsmtpssl").attr('disabled',smtp_enabled);
-    $("#emailsmtpdebug").attr('disabled',smtp_enabled);
+    $("#emailsmtphost").prop('disabled',smtp_enabled);
+    $("#emailsmtpuser").prop('disabled',smtp_enabled);
+    $("#emailsmtppassword").prop('disabled',smtp_enabled);
+    $("#emailsmtpssl").prop('disabled',smtp_enabled);
+    $("#emailsmtpdebug").prop('disabled',smtp_enabled);
 }
 
 function Emailchanges(ui,evt)
@@ -51,10 +51,10 @@ function Emailchanges(ui,evt)
     bounce_disabled=($("#bounceaccounttype").val()=='off');
     if (bounce_disabled==true) {bounce_disabled='disabled';}
     else {bounce_disabled='';}
-    $("#bounceaccounthost").attr('disabled',bounce_disabled);
-    $("#bounceaccountuser").attr('disabled',bounce_disabled);
-    $("#bounceaccountpass").attr('disabled',bounce_disabled);
-    $("#bounceencryption").attr('disabled',bounce_disabled);
-    $("#bounceaccountmailbox").attr('disabled',bounce_disabled);
+    $("#bounceaccounthost").prop('disabled',bounce_disabled);
+    $("#bounceaccountuser").prop('disabled',bounce_disabled);
+    $("#bounceaccountpass").prop('disabled',bounce_disabled);
+    $("#bounceencryption").prop('disabled',bounce_disabled);
+    $("#bounceaccountmailbox").prop('disabled',bounce_disabled);
 }
 
