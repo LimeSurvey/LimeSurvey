4a185fe0187f2ee024fc3fa5a5165eb2c19e5987
Menno Dekker <mennodekker@users.sourceforge.net>
2013-07-03 18:04:00 +0200

    dev: added missing export plugin for r-syntax export

---
+++ application/core/plugins/ExportR/ExportR.php
@@ -0,0 +1,54 @@
+<?php
+class ExportR extends PluginBase {
+    
+    protected $storage = 'DbStorage';
+       
+    static protected $description = 'Core: R-export';
+    static protected $name = 'Export results to R';
+    
+    public function __construct(PluginManager $manager, $id) {
+        parent::__construct($manager, $id);
+        
+        /**
+         * Here you should handle subscribing to the events your plugin will handle
+         */
+        $this->subscribe('listExportPlugins');
+        $this->subscribe('newExport');
+    }
+    
+    /**
+     * Registers this export type
+     */
+    public function listExportPlugins()
+    {
+        $event = $this->getEvent();
+        $exports = $event->get('exportplugins');
+        
+        // Yes we overwrite existing classes if available
+        $exports['rsyntax'] = get_class();
+        $exports['rdata'] = get_class();
+        $event->set('exportplugins', $exports);
+    }
+    
+    /**
+     * Returns the required IWriter
+     */
+    public function newExport()
+    {
+        $event = $this->getEvent();
+        $type = $event->get('type');
+                
+        switch ($type) {
+            case 'rsyntax':
+                $writer = new RSyntaxWriter();
+                break;
+            
+            case 'rdata':
+            default:
+                $writer = new CSVWriter();
+                break;
+        }
+        
+        $event->set('writer', $writer);
+    }
+}
