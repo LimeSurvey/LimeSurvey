de373cda6a33301867aebb212a9c0460fcebaf7e
Carsten Schmitz <c_schmitz@users.sourceforge.net>
2013-09-18 15:57:46 +0200

    Fixed issue 8137: Relevance equation cannot be edited for unknown reasons

--- application/views/admin/survey/Question/editQuestion_view.php
+++ application/views/admin/survey/Question/editQuestion_view.php
@@ -227,11 +227,10 @@
                 </li>
                 <li>
                     <label for='relevance'><?php $clang->eT("Relevance equation:"); ?></label>
-                    <textarea cols='50' rows='1' id='relevance' name='relevance' 
-                    <?php if($eqrow['conditions_number']){ ?> 
-                    readonly='readonly' title='<?php $clang->eT("This question has one or more conditions. Delete them before edit the equation relevance."); ?>' 
-                    <?php } ?>
-                    ><?php echo $eqrow['relevance']; ?></textarea>
+                    <textarea cols='50' rows='1' id='relevance' name='relevance' <?php if ($eqrow['conditions_number']) {?> readonly='readonly'<?php } ?>><?php echo $eqrow['relevance']; ?></textarea>
+                     <?php if ($eqrow['conditions_number']) {?>
+                        <span class='annotation'> <?php $clang->eT("Note: You can't edit the relevance equation because there are currently conditions set for this question."); ?></span>
+                     <?php } ?>
                 </li>
 
                 <li id='Validation'>
