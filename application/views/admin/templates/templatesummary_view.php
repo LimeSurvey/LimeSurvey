775b9157a46ea581b6c9003e2c5d71c363a1b048
Alain Bearez <Alain@cuali.com.br>
2013-10-01 15:42:03 -0300

    few cosmetical changes in code

--- application/views/admin/templates/templatesummary_view.php
+++ application/views/admin/templates/templatesummary_view.php
@@ -1,7 +1,6 @@
 <?php
-Yii::app()->clientScript->registerScript('editorfiletype',"editorfiletype ='".$sEditorFileType."';",CClientScript::POS_HEAD); // Is this deprecated (2013-09-25) ?
-?>
-<?php if (is_template_editable($templatename)==true)
+    Yii::app()->clientScript->registerScript('editorfiletype',"editorfiletype ='".$sEditorFileType."';",CClientScript::POS_HEAD); // Is this deprecated (2013-09-25) ?
+    if (is_template_editable($templatename)==true)
     {
         App()->getClientScript()->registerPackage('jquery-ace');
     ?>
