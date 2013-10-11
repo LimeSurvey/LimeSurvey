326e1ab11ce4325a979b02ef2034e02a6975e299
Carsten Schmitz <c_schmitz@users.sourceforge.net>
2013-10-11 21:30:19 +0200

    Fixed issue #8227: Time difference not being applied on update check time

--- application/controllers/admin/globalsettings.php
+++ application/controllers/admin/globalsettings.php
@@ -88,7 +88,7 @@ class GlobalSettings extends Survey_Common_Action
         $data['sUpdateNotification'] = getGlobalSetting('updatenotification');
         Yii::app()->loadLibrary('Date_Time_Converter');
         $dateformatdetails = getDateFormatData(Yii::app()->session['dateformat']);
-        $datetimeobj = new date_time_converter(getGlobalSetting("updatelastcheck"), 'Y-m-d H:i:s'); 
+        $datetimeobj = new date_time_converter(dateShift(getGlobalSetting("updatelastcheck"),'Y-m-d H:i:s',getGlobalSetting('timeadjust')), 'Y-m-d H:i:s'); 
         $data['updatelastcheck']=$datetimeobj->convert($dateformatdetails['phpdate'] . " H:i:s");
         
         $data['updateavailable'] = (getGlobalSetting("updateavailable") &&  Yii::app()->getConfig("updatable"));
