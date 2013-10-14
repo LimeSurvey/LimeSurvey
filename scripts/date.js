f17167d14cfb1dd0f3e27648ea0db2710d41f6c3
mfaber <m.faber@ymail.com>
2013-10-14 22:16:18 +0200

    Fixed issue #8279: JavaScript-Error in date.js
    Dev: FF, Chrome had no problem but IE was not happy.
    Dev: hope there's a new LS release soon...
    Dev: 2.05 is not affected

--- scripts/date.js
+++ scripts/date.js
@@ -37,7 +37,7 @@ $(document).ready(function(){
                         $('#answer'+basename).val("");
                     }
                 }
-            },
+            }
         }, $.datepicker.regional[language]);
     });
 
