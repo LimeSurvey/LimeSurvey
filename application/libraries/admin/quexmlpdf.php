daee3cb3325871fe9914276e3749871adedf1b20
Adam Zammit <adam.zammit@acspri.org.au>
2013-10-14 12:44:46 +1100

    Fixed issue #8271: quexf-pdf export: "Section" is not translated

--- application/libraries/admin/quexmlpdf.php
+++ application/libraries/admin/quexmlpdf.php
@@ -41,6 +41,11 @@ class quexmlpdf extends pdf {
 	const INCH_IN_MM = 25.4;
 
 	/**
+	 * Language for translation
+	 */
+	protected $language = "en";
+
+	/**
 	 * Pixels per inch of exported document
 	 * 
 	 * @var int Defaults to 300.
@@ -715,6 +720,12 @@ class quexmlpdf extends pdf {
 	 */
 	protected $sectionHeight = 18;
 
+	public function setLanguage($language)
+	{
+		if (!empty($language))
+			$this->language = $language;
+	}
+
 	/**
 	 * Return the length of the longest word
 	 * 
@@ -1325,6 +1336,8 @@ class quexmlpdf extends pdf {
 	 */
 	public function createqueXML($quexml)
 	{
+		$clang = new limesurvey_lang($this->languauge);
+
 		$xml = new SimpleXMLElement($quexml);
 	
 		$q = array();
@@ -1356,7 +1369,7 @@ class quexmlpdf extends pdf {
 		{
 			$stmp = array();
 			$sl = $this->numberToLetter($scount);
-			$stmp['title'] = "Section " . $sl;
+			$stmp['title'] = $clang->gT("Section") . " " . $sl;
 			$stmp['info'] = "";
 			$stmp['text'] = "";
 	
