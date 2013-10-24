9b4381ec9dd17229bf1de17d161280e6c253c4ff
Carsten Schmitz <c_schmitz@users.sourceforge.net>
2013-10-24 11:52:54 +0200

    Release 2.05RC4 Build 131024

--- application/config/version.php
+++ application/config/version.php
@@ -12,7 +12,7 @@
  *
  */
 
-$config['versionnumber'] = "2.05RC3";
+$config['versionnumber'] = "2.05RC4";
 $config['dbversionnumber'] = 171;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
