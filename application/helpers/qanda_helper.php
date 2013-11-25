e985aa7b2652326c72d8921aa3319b3a2864ef0e
mfaber <m.faber@ymail.com>
2013-11-25 20:17:13 +0100

    Fixed issue #8405: Date/time doesn't work correctly
    Dev: changed i18n to the gT instead of yii-method
    Dev: to allow support for languages not supported in yii

--- application/helpers/qanda_helper.php
+++ application/helpers/qanda_helper.php
@@ -1140,13 +1140,37 @@ function do_date($ia)
                     switch ((int)trim($aQuestionAttributes['dropdown_dates_month_style']))
                     {
                         case 0: 
-                            $montharray=Yii::app()->getLocale($clang->langcode)->getMonthNames('abbreviated',false);
-                            break;
+                            $montharray=array(
+                             $clang->gT('Jan'),
+                             $clang->gT('Feb'),
+                             $clang->gT('Mar'),
+                             $clang->gT('Apr'),
+                             $clang->gT('May'),
+                             $clang->gT('Jun'),
+                             $clang->gT('Jul'),
+                             $clang->gT('Aug'),
+                             $clang->gT('Sep'),
+                             $clang->gT('Oct'),
+                             $clang->gT('Nov'),
+                             $clang->gT('Dec'));
+                             break;
                         case 1: 
-                            $montharray=Yii::app()->getLocale($clang->langcode)->getMonthNames('wide',false);
-                            break;
+                            $montharray=array(
+                             $clang->gT('January'),
+                             $clang->gT('February'),
+                             $clang->gT('March'),
+                             $clang->gT('April'),
+                             $clang->gT('May'),
+                             $clang->gT('June'),
+                             $clang->gT('July'),
+                             $clang->gT('August'),
+                             $clang->gT('September'),
+                             $clang->gT('October'),
+                             $clang->gT('November'),
+                             $clang->gT('December'));
+                             break;
                         case 2: 
-                            $montharray=array(1=>'01',2=>'02',3=>'03',4=>'04',5=>'05',6=>'06',7=>'07',8=>'08',9=>'09',10=>'10',11=>'11',12=>'12');
+                            $montharray=array('01','02','03','04','05','06','07','08','09','10','11','12');
                             break;
                     }
                     
@@ -1159,7 +1183,7 @@ function do_date($ia)
                         {
                             $i_date_selected = '';
                         }
-                        $answer .= '<option value="'.sprintf('%02d', $i).'"'.$i_date_selected.'>'.$montharray[$i].'</option>';
+                        $answer .= '<option value="'.sprintf('%02d', $i).'"'.$i_date_selected.'>'.$montharray[$i-1].'</option>';
                     }
                     $answer .= '</select>';
                     break;
