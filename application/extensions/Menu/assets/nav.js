26f026ed5cfcdb8ff9c1c8cd115be1f5db4fe19c
Denis Chenu <courriel@shnoulle.net>
2013-12-18 12:39:20 +0100

    Fixed issue #08391: Unable to use "select" survey list in Plugin page Dev: tested with 'urlFormat' => ('path'|'get') AND 'showScriptName' => (false|true) Dev: use Yii createUrl in PHP remove need of  LS.createUrl Dev: Plugin can use App()->createUrl

--- application/extensions/Menu/assets/nav.js
+++ application/extensions/Menu/assets/nav.js
@@ -2,6 +2,10 @@ $(document).ready(function() {
     $('select[data-route]').on('change', function(event) {
         var params = {}
         params[$(this).attr('name')] = $(this).val();
-        window.location.href = LS.createUrl($(this).data('route'), params);
+        if($(this).data('route').indexOf("?")>=0)
+            window.location.href=$(this).data('route')+"&"+$(this).attr('name')+"="+$(this).val();
+        else
+            window.location.href=$(this).data('route')+"?"+$(this).attr('name')+"="+$(this).val();
+        //window.location.href = LS.createUrl($(this).data('route'), params);
     });
 });
