c03c1e73e0d5501847ded794c232268b7dbc4ca4
mfaber <m.faber@ymail.com>
2013-12-22 22:33:44 +0100

    Dev: update datetimepicker to ver. 1.4.3
    Dev: bug fixes and adds another language

--- third_party/jquery-ui-timepicker-addon/jquery-ui-timepicker-addon.js
+++ third_party/jquery-ui-timepicker-addon/jquery-ui-timepicker-addon.js
@@ -1,4 +1,4 @@
-/*! jQuery Timepicker Addon - v1.4.1 - 2013-11-10
+/*! jQuery Timepicker Addon - v1.4.3 - 2013-11-30
 * http://trentrichardson.com/examples/timepicker
 * Copyright (c) 2013 Trent Richardson; Licensed MIT */
 (function ($) {
@@ -16,7 +16,7 @@
 	*/
 	$.extend($.ui, {
 		timepicker: {
-			version: "1.4.1"
+			version: "1.4.3"
 		}
 	});
 
@@ -744,15 +744,22 @@
 				microsec = parseInt(microsec, 10);
 			}
 			if (timezone !== false) {
-				timezone = parseInt(timezone, 10);
+				timezone = timezone.toString();
 			}
 
 			var ampm = o[hour < 12 ? 'amNames' : 'pmNames'][0];
 
 			// If the update was done in the input field, the input field should not be updated.
 			// If the update was done using the sliders, update the input field.
-			var hasChanged = (hour !== this.hour || minute !== this.minute || second !== this.second || millisec !== this.millisec || microsec !== this.microsec || 
-					(this.ampm.length > 0 && (hour < 12) !== ($.inArray(this.ampm.toUpperCase(), this.amNames) !== -1)) || (this.timezone !== null && timezone !== this.timezone));
+			var hasChanged = (
+						hour !== parseInt(this.hour,10) || // sliders should all be numeric
+						minute !== parseInt(this.minute,10) || 
+						second !== parseInt(this.second,10) || 
+						millisec !== parseInt(this.millisec,10) || 
+						microsec !== parseInt(this.microsec,10) || 
+						(this.ampm.length > 0 && (hour < 12) !== ($.inArray(this.ampm.toUpperCase(), this.amNames) !== -1)) || 
+						(this.timezone !== null && timezone !== this.timezone.toString()) // could be numeric or "EST" format, so use toString()
+					);
 
 			if (hasChanged) {
 
@@ -799,6 +806,7 @@
 			this.timeDefined = true;
 			if (hasChanged) {
 				this._updateDateTime();
+				this.$input.focus();
 			}
 		},
 
@@ -1597,7 +1605,7 @@
 		// object will only return the timezone offset for the current locale, so we 
 		// adjust it accordingly.  If not using timezone option this won't matter..
 		// If a timezone is different in tp, keep the timezone as is
-		if (tp_inst) {
+		if (tp_inst && tp_date) {
 			// look out for DST if tz wasn't specified
 			if (!tp_inst.support.timezone && tp_inst._defaults.timezone === null) {
 				tp_inst.timezone = tp_date.getTimezoneOffset() * -1;
@@ -2132,6 +2140,6 @@
 	/*
 	* Keep up with the version
 	*/
-	$.timepicker.version = "1.4.1";
+	$.timepicker.version = "1.4.3";
 
 })(jQuery);
