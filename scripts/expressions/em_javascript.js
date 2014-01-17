ed198e0aa1f7c46cb8789ab5aa0e5f56008a0eab
mfaber <m.faber@ymail.com>
2014-01-17 15:58:11 +0100

    Dev: Update for strtotime.js

--- scripts/expressions/em_javascript.js
+++ scripts/expressions/em_javascript.js
@@ -2056,21 +2056,20 @@ function strstr (haystack, needle, bool) {
 }
 
 function strtotime (text, now) {
-    /* 2013-06-12: taken from phpjs.org
-       and adapted for limesurvey */
-
     // Convert string representation of date and time to a timestamp
     //
-    // version: 1109.2015
+    // version: 1109.2016
     // discuss at: http://phpjs.org/functions/strtotime
     // +   original by: Caio Ariede (http://caioariede.com)
     // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
     // +      input by: David
     // +   improved by: Caio Ariede (http://caioariede.com)
-    // +   improved by: Brett Zamir (http://brett-zamir.me)
     // +   bugfixed by: Wagner B. Soares
     // +   bugfixed by: Artur Tchernychev
     // +   improved by: A. Matías Quezada (http://amatiasq.com)
+    // +   improved by: preuter
+    // +   improved by: Brett Zamir (http://brett-zamir.me)
+    // +   improved by: Mirko Faber
     // %        note 1: Examples all have a fixed timestamp to prevent tests to fail because of variable time(zones)
     // *     example 1: strtotime('+1 day', 1129633200);
     // *     returns 1: 1129719600
@@ -2078,13 +2077,16 @@ function strtotime (text, now) {
     // *     returns 2: 1130425202
     // *     example 3: strtotime('last month', 1129633200);
     // *     returns 3: 1127041200
-    // *     example 4: strtotime('2009-05-04 08:30:00');
-    // *     returns 4: 1241418600
-    if (!text)
-        return null;
+    // *     example 4: strtotime('2009-05-04 08:30:00 GMT');
+    // *     returns 4: 1241425800
+    var parsed, match, today, year, date, days, ranges, len, times, regex, i, fail = false;
+
+    if (!text) {
+        return fail;
+    }
 
     // Unecessary spaces
-    text = text.trim()
+    text = text.replace(/^\s+|\s+$/g, '')
         .replace(/\s{2,}/g, ' ')
         .replace(/[\t\r\n]/g, '')
         .toLowerCase();
@@ -2092,90 +2094,111 @@ function strtotime (text, now) {
     // in contrast to php, js Date.parse function interprets: 
     // dates given as yyyy-mm-dd as in timezone: UTC, 
     // dates with "." or "-" as MDY instead of DMY
-	// dates with two-digit years differently
+    // dates with two-digit years differently
     // etc...etc...
     // ...therefore we manually parse lots of common date formats
-    var match = text.match(/^(\d{1,4})([\-\.\/\:])(\d{1,2})([\-\.\/\:])(\d{1,4})(?:\s(\d{1,2}):(\d{2})?:?(\d{2})?)?(?:\s([A-Z]+)?)?$/);
-    if (match && match[2]==match[4]) {
-        if (match[1]>1901) {
+    match = text.match(/^(\d{1,4})([\-\.\/\:])(\d{1,2})([\-\.\/\:])(\d{1,4})(?:\s(\d{1,2}):(\d{2})?:?(\d{2})?)?(?:\s([A-Z]+)?)?$/);
+    
+    if (match && match[2] === match[4]) {
+        if (match[1] > 1901) {
             switch (match[2]) {
                 case '-': {  // YYYY-M-D
-                    if (match[3]>12 | match[5]>31) return(0);
+                    if (match[3] > 12 || match[5] > 31) {
+                        return fail;
+                    }
+                    
                     return new Date(match[1], parseInt(match[3], 10) - 1, match[5],
-                    match[6] || 0, match[7] || 0, match[8] || 0, match[9] || 0) / 1000;
-                    break;
+                        match[6] || 0, match[7] || 0, match[8] || 0, match[9] || 0) / 1000;
                 }
                 case '.': {  // YYYY.M.D is not parsed by strtotime()
-                    return(0);
-                    break;
+                    return fail;
                 }
                 case '/': {  // YYYY/M/D
-                    if (match[3]>12 | match[5]>31) return(0);
+                    if (match[3] > 12 || match[5] > 31) {
+                        return fail;
+                    }
+                    
                     return new Date(match[1], parseInt(match[3], 10) - 1, match[5],
-                    match[6] || 0, match[7] || 0, match[8] || 0, match[9] || 0) / 1000;
-                    break;
+                        match[6] || 0, match[7] || 0, match[8] || 0, match[9] || 0) / 1000;
                 }
             }
-        }
-        else if (match[5]>1901) {
+        } else if (match[5] > 1901) {
             switch (match[2]) {
                 case '-': {  // D-M-YYYY
-                    if (match[3]>12 | match[1]>31) return(0);
+                    if (match[3] > 12 || match[1] > 31) {
+                        return fail;
+                    }
+                    
                     return new Date(match[5], parseInt(match[3], 10) - 1, match[1],
-                    match[6] || 0, match[7] || 0, match[8] || 0, match[9] || 0) / 1000;
-                    break;
+                        match[6] || 0, match[7] || 0, match[8] || 0, match[9] || 0) / 1000;
                 }
                 case '.': {  // D.M.YYYY
-                    if (match[3]>12 | match[1]>31) return(0);
+                    if (match[3] > 12 || match[1] > 31) {
+                        return fail;
+                    }
+                    
                     return new Date(match[5], parseInt(match[3], 10) - 1, match[1],
-                    match[6] || 0, match[7] || 0, match[8] || 0, match[9] || 0) / 1000;
-                    break;
+                        match[6] || 0, match[7] || 0, match[8] || 0, match[9] || 0) / 1000;
                 }
                 case '/': {  // M/D/YYYY
-                    if (match[1]>12 | match[3]>31) return(0);
+                    if (match[1] > 12 || match[3] > 31) {
+                        return fail;
+                    }
+                    
                     return new Date(match[5], parseInt(match[1], 10) - 1, match[3],
-                    match[6] || 0, match[7] || 0, match[8] || 0, match[9] || 0) / 1000;
-                    break;
+                        match[6] || 0, match[7] || 0, match[8] || 0, match[9] || 0) / 1000;
                 }
             }
         }
         else {
             switch (match[2]) {
                 case '-': {  // YY-M-D
-                    if (match[3]>12 | match[5]>31 | (match[1] < 70 & match[1]>38)) return(0);
-                    var year = match[1] >= 0 && match[1] <= 38 ? +match[1] + 2000 : match[1];
+                    if (match[3] > 12 || match[5] > 31 || (match[1] < 70 && match[1] > 38)) {
+                        return fail;
+                    }
+                    
+                    year = match[1] >= 0 && match[1] <= 38 ? +match[1] + 2000 : match[1];
                     return new Date(year, parseInt(match[3], 10) - 1, match[5],
-                    match[6] || 0, match[7] || 0, match[8] || 0, match[9] || 0) / 1000;
-                    break;
+                        match[6] || 0, match[7] || 0, match[8] || 0, match[9] || 0) / 1000;
                 }
                 case '.': {  // D.M.YY or H.MM.SS
-                    if (match[5]>=70) {    // D.M.YY
-                        if (match[3]>12 | match[1]>31) return(0);
+                    if (match[5] >= 70) {    // D.M.YY
+                        if (match[3]>12 || match[1]>31) {
+                            return fail;
+                        }
+                        
                         return new Date(match[5], parseInt(match[3], 10) - 1, match[1],
-                        match[6] || 0, match[7] || 0, match[8] || 0, match[9] || 0) / 1000;
+                            match[6] || 0, match[7] || 0, match[8] || 0, match[9] || 0) / 1000;
                     }
-                    else if (match[5]<60 & !(match[6])) {  // H.MM.SS
-                        if (match[1]>23 | match[3]>59) return(0);
-                        var today = new Date();
+                    if (match[5] < 60 && !match[6]) {  // H.MM.SS
+                        if (match[1] > 23 || match[3] > 59) {
+                            return fail;
+                        }
+                        
+                        today = new Date();
                         return new Date(today.getFullYear(), today.getMonth(), today.getDate(),
-                        match[1] || 0, match[3] || 0, match[5] || 0, match[9] || 0) / 1000;
+                            match[1] || 0, match[3] || 0, match[5] || 0, match[9] || 0) / 1000;
                     }
-                    else  return(0);  // invalid format, cannot be parsed
-                    break;
+                    
+                    return fail;  // invalid format, cannot be parsed
                 }
                 case '/': {  // M/D/YY
-                    if (match[1]>12 | match[3]>31 | (match[5] < 70 & match[5]>38)) return(0);
-                    var year = match[5] >= 0 && match[5] <= 38 ? +match[5] + 2000 : match[5];
+                    if (match[1] > 12 || match[3] > 31 || (match[5] < 70 && match[5] > 38)) {
+                        return fail;
+                    }
+                    
+                    year = match[5] >= 0 && match[5] <= 38 ? +match[5] + 2000 : match[5];
                     return new Date(year, parseInt(match[1], 10) - 1, match[3],
-                    match[6] || 0, match[7] || 0, match[8] || 0, match[9] || 0) / 1000;
-                    break;
+                        match[6] || 0, match[7] || 0, match[8] || 0, match[9] || 0) / 1000;
                 }
                 case ':': {  // HH:MM:SS
-                    if (match[1]>23 | match[3]>59 | match[5]>59) return(0);
-                    var today = new Date();
+                    if (match[1] > 23 || match[3] > 59 || match[5] > 59) {
+                        return fail;
+                    }
+                    
+                    today = new Date();
                     return new Date(today.getFullYear(), today.getMonth(), today.getDate(),
-                    match[1] || 0, match[3] || 0, match[5] || 0) / 1000;
-                    break;
+                        match[1] || 0, match[3] || 0, match[5] || 0) / 1000;
                 }
             }
         }
@@ -2183,13 +2206,15 @@ function strtotime (text, now) {
     
     
     // other formats and "now" should be parsed by Date.parse()
-    if (text === 'now')
+    if (text === 'now') {
         return now === null || isNaN(now) ? new Date().getTime() / 1000 | 0 : now | 0;
-    else if (!isNaN(parse = Date.parse(text)))
-        return parse / 1000 | 0;
-    
-    var date = now ? new Date(now * 1000) : new Date();
-    var days = {
+    }
+    if (!isNaN(parsed = Date.parse(text))) {
+        return parsed / 1000 | 0;
+    }
+
+    date = now ? new Date(now * 1000) : new Date();
+    days = {
         'sun': 0,
         'mon': 1,
         'tue': 2,
@@ -2198,7 +2223,7 @@ function strtotime (text, now) {
         'fri': 5,
         'sat': 6
     };
-    var ranges = {
+    ranges = {
         'yea': 'FullYear',
         'mon': 'Month',
         'day': 'Date',
@@ -2208,64 +2233,72 @@ function strtotime (text, now) {
     };
 
     function lastNext(type, range, modifier) {
-        var day = days[range];
+        var diff, day = days[range];
 
-        if (typeof(day) !== 'undefined') {
-            var diff = day - date.getDay();
+        if (typeof day !== 'undefined') {
+            diff = day - date.getDay();
 
-            if (diff === 0)
+            if (diff === 0) {
                 diff = 7 * modifier;
-            else if (diff > 0 && type === 'last')
+            }
+            else if (diff > 0 && type === 'last') {
                 diff -= 7;
-            else if (diff < 0 && type === 'next')
+            }
+            else if (diff < 0 && type === 'next') {
                 diff += 7;
+            }
 
             date.setDate(date.getDate() + diff);
         }
     }
     function process(val) {
-        var split = val.split(' ');
-        var type = split[0];
-        var range = split[1].substring(0, 3);
-        var typeIsNumber = /\d+/.test(type);
+        var splt = val.split(' '), // Todo: Reconcile this with regex using \s, taking into account browser issues with split and regexes
+            type = splt[0],
+            range = splt[1].substring(0, 3),
+            typeIsNumber = /\d+/.test(type),
+            ago = splt[2] === 'ago',
+            num = (type === 'last' ? -1 : 1) * (ago ? -1 : 1);
 
-        var ago = split[2] === 'ago';
-        var num = (type === 'last' ? -1 : 1) * (ago ? -1 : 1);
-
-        if (typeIsNumber)
+        if (typeIsNumber) {
             num *= parseInt(type, 10);
+        }
 
-        if (ranges.hasOwnProperty(range))
+        if (ranges.hasOwnProperty(range) && !splt[1].match(/^mon(day|\.)?$/i)) {
             return date['set' + ranges[range]](date['get' + ranges[range]]() + num);
-        else if (range === 'wee')
+        }
+        
+        if (range === 'wee') {
             return date.setDate(date.getDate() + (num * 7));
+        }
 
-        if (type === 'next' || type === 'last')
+        if (type === 'next' || type === 'last') {
             lastNext(type, range, num);
-        else if (!typeIsNumber)
+        }
+        else if (!typeIsNumber) {
             return false;
-
+        }
+        
         return true;
     }
 
-    var regex = '([+-]?\\d+\\s' +
-        '(years?|months?|weeks?|days?|hours?|min|minutes?|sec|seconds?' +
-        '|sun\\.?|sunday|mon\\.?|monday|tue\\.?|tuesday|wed\\.?|wednesday' +
-        '|thu\\.?|thursday|fri\\.?|friday|sat\\.?|saturday)|(last|next)\\s' +
-        '(years?|months?|weeks?|days?|hours?|min|minutes?|sec|seconds?' +
-        '|sun\\.?|sunday|mon\\.?|monday|tue\\.?|tuesday|wed\\.?|wednesday' +
-        '|thu\\.?|thursday|fri\\.?|friday|sat\\.?|saturday))(\\sago)?';
+    times = '(years?|months?|weeks?|days?|hours?|minutes?|min|seconds?|sec' +
+        '|sunday|sun\\.?|monday|mon\\.?|tuesday|tue\\.?|wednesday|wed\\.?' +
+        '|thursday|thu\\.?|friday|fri\\.?|saturday|sat\\.?)';
+    regex = '([+-]?\\d+\\s' + times + '|' + '(last|next)\\s' + times + ')(\\sago)?';
 
     match = text.match(new RegExp(regex, 'gi'));
-    if (!match)
-        return false;
+    if (!match) {
+        return fail;
+    }
 
-    for (var i = 0, len = match.length; i < len; i++)
-        if (!process(match[i]))
-            return false;
+    for (i = 0, len = match.length; i < len; i++) {
+        if (!process(match[i])) {
+            return fail;
+        }
+    }
 
     // ECMAScript 5 only
-    //if (!match.every(process))
+    // if (!match.every(process))
     //    return false;
 
     return (date.getTime() / 1000);
