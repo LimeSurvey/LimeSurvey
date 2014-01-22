f0c8fe3e2baff2314f018e6ee9688e054c12b54a
Carsten Schmitz <c_schmitz@users.sourceforge.net>
2014-01-22 16:52:33 +0100

    Fixed issue #8554: Hover over answer options no longer showing value

--- application/helpers/qanda_helper.php
+++ application/helpers/qanda_helper.php
@@ -4717,7 +4717,8 @@ function do_array($ia)
             {
                 $answer .= "\t\t\t<td class=\"answer_cell_00$ld answer-item radio-item\">\n"
                 . "<label class=\"hide read\" for=\"answer$myfname-$ld\">{$labelans[$thiskey]}</label>\n"
-                . "\t<input class=\"radio\" type=\"radio\" name=\"$myfname\" value=\"$ld\" id=\"answer$myfname-$ld\" ";
+                . "\t<input class=\"radio\" type=\"radio\" name=\"$myfname\" value=\"$ld\" id=\"answer$myfname-$ld\" title=\""
+                . HTMLEscape(strip_tags($labelans[$thiskey])).'"';
                 if (isset($_SESSION['survey_'.Yii::app()->getConfig('surveyID')][$myfname]) && $_SESSION['survey_'.Yii::app()->getConfig('surveyID')][$myfname] == $ld)
                 {
                     $answer .= CHECKED;
