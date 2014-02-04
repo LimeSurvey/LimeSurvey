609676a42ca7f70881527277769180a797f4b403
Carsten Schmitz <c_schmitz@users.sourceforge.net>
2014-02-04 12:47:20 +0100

    Fixed issue #8614: Error during upgrade vom 2.00 to 2.05 when using Postgres

--- application/helpers/update/updatedb_helper.php
+++ application/helpers/update/updatedb_helper.php
@@ -1194,7 +1194,14 @@ function db_upgrade_all($iOldDBVersion) {
         }
         if ($iOldDBVersion < 175)
         {
-            alterColumn('{{plugins}}', 'active', "integer",false,'0');
+            switch ($sDBDriverName){
+                case 'pgsql': 
+                    // Special treatment for Postgres as it is too dumb to convert a boolean to a number without explicit being told to do so
+                    alterColumn('{{plugins}}', 'active', "INTEGER USING (active::integer)", false);
+                    break;
+                default:
+                    alterColumn('{{plugins}}', 'active', "integer",false,'0');
+            }
             $oDB->createCommand()->update('{{settings_global}}',array('stg_value'=>175),"stg_name='DBVersion'");
         }
         if ($iOldDBVersion < 176)
