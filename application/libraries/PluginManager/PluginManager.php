f81409af7f68b0002dc26e227ffd8aa9148905bd
Carsten Schmitz <c_schmitz@users.sourceforge.net>
2014-02-04 12:07:38 +0100

    Fixed issue #8622: Inputs for username and password not appearing in login mask when using Postgres

--- application/libraries/PluginManager/PluginManager.php
+++ application/libraries/PluginManager/PluginManager.php
@@ -293,7 +293,7 @@
         {
             try {
                 $pluginModel = Plugin::model();    
-                $records = $pluginModel->findAllByAttributes(array('active'=>true));
+                $records = $pluginModel->findAllByAttributes(array('active'=>1));
             
                 foreach ($records as $record) {
                     $this->loadPlugin($record->name, $record->id);
