66368b9a154952d0510e6f04b380cc6a99059da1
tpartner <tpartner@partnersinc.biz>
2014-03-03 09:37:54 -0500

    Fixed issue #08798: Editing a dropdown attribute value deletes everything but the first word Dev: Clean up JavaScript and styles for editing attributes

--- scripts/admin/viewAttribute.js
+++ scripts/admin/viewAttribute.js
@@ -60,19 +60,31 @@ $(document).ready(function() {
     var id = 1;
     $('.add').click(function(){
             html = "<tr>"+
-            "<td><input type='text' name='attribute_value_name_"+id+"' id='attribute_value_name_"+id+"' size='8' style='50%;'></td></tr>";
+            "<td colspan='2'><input type='text' name='attribute_value_name_"+id+"' id='attribute_value_name_"+id+"' size='8' style='50%;'></td></tr>";
                   $('.dd').fadeIn('slow');
         $('#ddtable tr:last').after(html);
         id++;
     });
-    $(".editable").click(function () {
-        var value_id = this.id;
-        $(this).replaceWith( "<div><input type='text' size='20' name='editbox' id='editbox' value="+$(this).text()+"><input type='hidden' id='value_id' name='value_id' value='"+value_id+"' /></div>" );
+    $(document.body).on('dblclick', '.editable', function() {
+        editAttrValue(this.id);
     });
-    $('.edit').click(function(){
-       var value_id = this.name;
-       $("#"+value_id).replaceWith( "<div><input type='text' size='20' name='editbox' id='editbox' value="+$("#"+value_id).text()+"><input type='hidden' id='value_id' name='value_id' value='"+value_id+"' /></div>" );
+    $('.actions .edit').click(function(){
+       editAttrValue(this.name);
     });
+    $('.actions .cancel').click(function(){
+	   var thisRow = $(this).closest('tr');
+	   var valueText = $('td.data', thisRow).html('<div id="'+$('td.data', thisRow).attr('data-id')+'" class="editable">'+$('td.data', thisRow).attr('data-text')+'</div>');
+		$('.actions .cancel', thisRow).hide();
+		$('.actions .edit, .actions .delete', thisRow).show();
+    });
+	function editAttrValue(valueId) {
+	   var valueText = $.trim($("#"+valueId).text());
+	   var thisRow = $("#"+valueId).closest('tr');
+       $("#"+valueId).replaceWith( "<div><input type='text' size='20' name='editbox' id='editbox"+valueId+"' /><input type='hidden' id='value_id' name='value_id' value='"+valueId+"' /></div>" );
+		$('#editbox'+valueId).val(valueText);
+		$('.actions .edit, .actions .delete', thisRow).hide();
+		$('.actions .cancel', thisRow).show();
+	}
     $('.languagesetting').click(function(){
         $(".languagesetting").css('border', '1px solid black');
         $(".languagesetting").css('background-color', 'white');
