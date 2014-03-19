729bf4c5b0cc1fb68d63bcb72dbe1d7a843fa74f
Carsten Schmitz <c_schmitz@users.sourceforge.net>
2014-03-19 18:39:17 +0100

    Fixed issue #8876: Print answers shows fatal error if session expired

--- application/controllers/PrintanswersController.php
+++ application/controllers/PrintanswersController.php
@@ -81,7 +81,7 @@
                 echo "<center><br />\n"
                 ."\t<font color='RED'><strong>".$clang->gT("Error")."</strong></font><br />\n"
                 ."\t".$clang->gT("We are sorry but your session has expired.")."<br />".$clang->gT("Either you have been inactive for too long, you have cookies disabled for your browser, or there were problems with your connection.")."<br />\n"
-                ."\t".sprintf($clang->gT("Please contact %s ( %s ) for further assistance."),$siteadminname,$siteadminemail)."\n"
+                ."\t".sprintf($clang->gT("Please contact %s ( %s ) for further assistance."), Yii::app()->getConfig("siteadminname"), Yii::app()->getConfig("siteadminemail"))."\n"
                 ."</center><br />\n";
                 echo templatereplace(file_get_contents(getTemplatePath($sTemplate).'/endpage.pstpl'),array());
                 doFooter();
