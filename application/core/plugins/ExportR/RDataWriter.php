eee2135df79208eaf3d748470f30c850fdd105a4
Menno Dekker <menno.dekker@erasmusmc.nl>
2014-03-31 15:12:30 +0200

    Fixed issue #8917: Error in R Export plugin dev: probably some more can be done for the 'not available' answers, but should be fine for now

--- application/core/plugins/ExportR/RDataWriter.php
+++ application/core/plugins/ExportR/RDataWriter.php
@@ -1,19 +1,23 @@
 <?php
 Yii::import('application.helpers.admin.export.*');
 class RDataWriter extends CsvWriter {
-    public function init(\SurveyObj $survey, $sLanguageCode, \FormattingOptions $oOptions) {       
+    /**
+     * The value to use when no data is present (for example unanswered because
+     * of relevance)
+     *
+     * @var string
+     */
+    public $na = '';
+
+    public function init(\SurveyObj $survey, $sLanguageCode, \FormattingOptions $oOptions) {
         parent::init($survey, $sLanguageCode, $oOptions);
-        
+
         // Change filename
         $this->csvFilename = 'survey_' . $survey->id .'_R_data_file.csv';
         // Skip the first line with headers
         $this->doHeaders = false;
-        
+
         $oOptions->answerFormat = "short";      // force answer codes
-        $oOptions->convertN = true;
-        $oOptions->nValue = 0;
-        $oOptions->convertY = true;
-        $oOptions->yValue = 1;
     }
 
     /**
@@ -25,17 +29,7 @@ class RDataWriter extends CsvWriter {
      * @return mixed
      */
     protected function transformResponseValue($value, $fieldType, FormattingOptions $oOptions) {
-        $value = parent::transformResponseValue($value, $fieldType, $oOptions);
-
         switch ($fieldType) {
-            case 'G':       // Gender question
-                if ($value == 'F') {
-                    return 1;
-                } elseif ($value == 'M') {
-                    return 2;
-                }
-                break;
-                
             case 'C':       // Yes/no/uncertain
                 if ($value == 'Y') {
                     return 1;
@@ -45,7 +39,7 @@ class RDataWriter extends CsvWriter {
                     return 3;
                 }
                 break;
-                
+
             case 'E':       // Increase/same/decrease
                 if ($value == 'I') {
                     return 1;
@@ -53,9 +47,40 @@ class RDataWriter extends CsvWriter {
                     return 2;
                 } elseif ($value == 'D') {
                     return 3;
-                }                
+                }
                 break;
-  
+                
+            case 'G':       // Gender question
+                if ($value == 'F') {
+                    return 1;
+                } elseif ($value == 'M') {
+                    return 2;
+                }
+                break;
+                
+            case 'M':       // Multiple choice
+            case 'P':
+                if ($value == 'Y') {
+                    return 1;
+                } elseif ($value == 'N') {
+                    return 0;
+                } else {
+                    // We can not use the $this->na value yet since we can also have a comment field
+                    return $value;
+                }
+                break;
+
+            case 'Y':       // Yes no question
+                if ($value == 'Y') {
+                    return 1;
+                } elseif ($value == 'N') {
+                    return 0;
+                } else {
+                    // No data, probably a hidden question
+                    return $this->na;
+                }
+                break;
+
             default:
                 return $value;
                 break;
