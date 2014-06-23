bdfad82dc7e9f09a7154fd38bc51d7cd9940a5ca
Denis Chenu <courriel@shnoulle.net>
2014-06-23 12:13:20 +0200

    New feature : use shortest url with urlFormat path Dev: this replace array('survey/index', 'sid' => $survey->sid, 'lang' => App()->lang->langcode) by /$sid/lang/language Dev: same behaviour than 2.0 or 1.92 with modrewrite=1 Dev: maybe some other default rules to add : actually surveyurl + statistics_ur + optout url (maybe partially) Dev: need more testing ? urlFormat get not changed

--- application/config/routes.php
+++ application/config/routes.php
@@ -18,6 +18,7 @@ $route['<_sid:\d+>/lang-<_lang:\w+[-\w]+>/tk-<_token:\w+>/*'] = "survey/index/si
 $route['<_sid:\d+>/lang-<_lang:\w+[-\w]+>/*'] = "survey/index/sid/<_sid>/lang/<_lang>";
 $route['<_sid:\d+>/tk-<_token:\w+>/*'] = "survey/index/sid/<_sid>/token/<_token>";
 $route['<_sid:\d+>/*'] = "survey/index/sid/<_sid>";
+$route['<sid:\d+>/*'] = 'survey/index';
 
 //Admin Routes
 $route['admin/index'] = "admin";
@@ -28,12 +29,16 @@ $route['admin/<action:\w+>/<sa:\w+>/*'] = 'admin/<action>/sa/<sa>';
 $route['admin/labels/<_action:\w+>'] = "admin/labels/index/<_action>";
 $route['admin/labels/<_action:\w+>/<_lid:\d+>'] = "admin/labels/index/<_action>/<_lid>";
 
-$route['<_controller:\w+>/<_action:\w+>'] = '<_controller>/<_action>';
-
 //Expression Manager tests
 $route['admin/expressions'] = "admin/expressions/index";
 
 //optout
 $route['optout/<_sid:\d+>/(:any)/(:any)'] = "optout/index/<_sid>/$2/$3";
+$route['optout/<surveyid:\d+>/*'] = "optout/index";
+
+//Public statitics
+$route['statistics_user/<surveyid:\d+>/*'] = "statistics_user/action";
+
+$route['<_controller:\w+>/<_action:\w+>'] = '<_controller>/<_action>';
 
 return $route;
