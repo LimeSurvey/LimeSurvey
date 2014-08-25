c1044dcc41d3b9c827cbe775aa0f7f121337efea
Denis Chenu <courriel@shnoulle.net>
2014-08-25 11:05:51 +0200

    Dev: Move the disable scroll on mousewheel in function to allow replaced by the template Dev: Why this is set ? Really more difficult to use on a big select and a clean browser

--- scripts/survey_runtime.js
+++ scripts/survey_runtime.js
@@ -22,6 +22,7 @@ $(document).ready(function()
     navbuttonsJqueryUi();
     showStartPopups();
     addClassEmpty();
+    noScrollOnSelect();
     if (typeof LEMsetTabIndexes === 'function') { LEMsetTabIndexes(); }
 	if (typeof checkconditions!='undefined') checkconditions();
 	if (typeof template_onload!='undefined') template_onload();
@@ -29,9 +30,6 @@ $(document).ready(function()
     {
         $(focus_element).focus();
     }
-    $(".question").find("select").each(function () {
-        hookEvent($(this).attr('id'),'mousewheel',noScroll);
-    });
 
     // Keypad functions
     var kp = $("input.num-keypad");
@@ -286,7 +284,7 @@ function manageIndex(){
     });
 }
 /**
- * Put a empty class on empty answer text item (limit to answers part)
+ * Put a empty class on empty answer text item (limit to answers part) 
  * @author Denis Chenu / Shnoulle
  */
 function addClassEmpty()
@@ -307,6 +305,16 @@ function addClassEmpty()
 	});
 }
 
+/**
+ * Disable scroll on select, put it in function to allow update in template
+ * 
+ */
+function noScrollOnSelect()
+{
+    $(".question").find("select").each(function () {
+        hookEvent($(this).attr('id'),'mousewheel',noScroll);
+    });
+}
 /**
  * Adapt cell to have a click on cell do a click on input:radio or input:checkbox (if unique)
  * Using delegate the can be outside document.ready (using .on is possible but on $(document) then : less readbale
