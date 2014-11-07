0a0633a7da0ff86d3da66e24ef1ee434ce556382
Carsten Schmitz <carsten.schmitz@limesurvey.org>
2014-11-07 14:55:05 +0100

    Fixed issue: Files cannot be downloaded if magic_quotes is activated on the server

--- application/helpers/common_helper.php
+++ application/helpers/common_helper.php
@@ -7269,7 +7269,11 @@ function fixSubquestions()
 */
 function ls_json_encode($content)
 {
-    $ans = json_encode(stripslashes($content));
+    if (is_string($content))
+    {
+        $content=stripslashes($content);
+    }
+    $ans = json_encode($content);
     $ans = str_replace(array('{','}'),array('{ ',' }'), $ans);
     return $ans;
 }
