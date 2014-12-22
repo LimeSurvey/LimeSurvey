ffafc562a10da768e8d580f90b9549802573f256
Denis Chenu <courriel@shnoulle.net>
2014-12-22 15:16:21 +0100

    Dev: one line test when we want boolean .....

--- application/controllers/admin/responses.php
+++ application/controllers/admin/responses.php
@@ -454,12 +454,7 @@ class responses extends Survey_Common_Action
 
 
 #        Yii::app()->loadHelper('surveytranslator');
-        $aData['issuperadmin'] = false;
-        if (Permission::model()->hasGlobalPermission('superadmin'))
-        {
-            $aData['issuperadmin'] = true;
-        }
-
+        $aData['issuperadmin'] = Permission::model()->hasGlobalPermission('superadmin');
         $aData['surveyid']= $iSurveyID;
         $aData['column_model_txt']= $column_model_txt;
         $aData['column_names_txt']= $column_names_txt;
