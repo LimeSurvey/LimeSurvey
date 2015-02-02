52ad26e22aceb8f8d9f35b2c786b3766a6599173
Sam <sam@mousa.nl>
2015-02-02 16:24:17 +0100

    Dev Updated build so that removals are properly pushed.

--- application/commands/BuildCommand.php
+++ application/commands/BuildCommand.php
@@ -107,7 +107,7 @@ class BuildCommand extends CConsoleCommand
         chdir("$tempDir/new");
         $this->updateVersion("$tempDir/new/application/config/version.php");
         
-        $this->git('add *');
+        $this->git('add --all *');
         $this->git("commit -a -m 'Automated release {$this->tag}'");
         $this->git("tag -a {$this->tag} -m 'Automated release of {$this->tag}'");
         $this->git("push origin {$this->branch}:{$this->branch}");
