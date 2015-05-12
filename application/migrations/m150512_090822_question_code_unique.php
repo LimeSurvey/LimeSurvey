f6d9445f81705e6181c84d238fe1f4a0eddea15d
Sam <sam@mousa.nl>
2015-05-12 11:13:41 +0200

    Dev fixed return value for migratio

--- application/migrations/m150512_090822_question_code_unique.php
+++ application/migrations/m150512_090822_question_code_unique.php
@@ -11,7 +11,6 @@ class m150512_090822_question_code_unique extends CDbMigration
     public function safeUp()
 	{
         $this->createIndex('unique_question_codes', '{{questions}}', ['sid', 'title', 'parent_qid'], true);
-        return false;
 	}
     
     /**
