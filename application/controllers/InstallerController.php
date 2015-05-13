285c295156c9db082c7cd39bc70431ed1805779a
Carsten Schmitz <carsten.schmitz@limesurvey.org>
2015-05-13 12:36:03 +0200

    Fixed issue #9569: Admin with uid>1 cannot set superadmin permissions

--- application/controllers/InstallerController.php
+++ application/controllers/InstallerController.php
@@ -663,6 +663,10 @@ class InstallerController extends CController {
                         }
                         // Save user
                         $user=new User;
+                        // Fix UserID to 1 for MySQL even if installed in master-master configuration scenario
+                        if (in_array($this->connection->getDriverName(), array('mysql', 'mysqli'))) {
+                            $user->uid=1;
+                        }
                         $user->users_name=$sAdminUserName;
                         $user->password=$sPasswordHash;
                         $user->full_name=$sAdminRealName;
