797ea7a338997e97d467035f998499a80a87faf2
Denis Chenu <courriel@shnoulle.net>
2015-05-19 13:57:28 +0200

    Fixed issue #09632: Conditions can be wrongfully substituted (answer code : 0) Dev: set $sOldCode only if request->getPost('oldcode_'.$iSortOrderID.'_'.$iScaleID) Dev: bug if getPost('oldcode_'.$iSortOrderID.'_'.$iScaleID) is empty

--- application/controllers/admin/database.php
+++ application/controllers/admin/database.php
@@ -153,12 +153,6 @@ class database extends Survey_Common_Action
                 for ($iSortOrderID=1;$iSortOrderID<$iMaxCount;$iSortOrderID++)
                 {
                     $sCode=sanitize_paranoid_string(Yii::app()->request->getPost('code_'.$iSortOrderID.'_'.$iScaleID));
-                    if (Yii::app()->request->getPost('oldcode_'.$iSortOrderID.'_'.$iScaleID)) {
-                        $sOldCode=sanitize_paranoid_string(Yii::app()->request->getPost('oldcode_'.$iSortOrderID.'_'.$iScaleID));
-                        if($sCode !== $sOldCode) {
-                            Condition::model()->updateAll(array('value'=>$sCode), 'cqid=:cqid AND value=:value', array(':cqid'=>$iQuestionID, ':value'=>$sOldCode));
-                        }
-                    }
 
                     $iAssessmentValue=(int) Yii::app()->request->getPost('assessment_'.$iSortOrderID.'_'.$iScaleID);
                     foreach ($aSurveyLanguages as $sLanguage)
@@ -179,7 +173,9 @@ class database extends Survey_Common_Action
                         {
                             Yii::app()->setFlashMessage(gT("Failed to update answers"),'error');
                         }
-                    } // foreach ($alllanguages as $language)
+                    } 
+                    // Updating code (oldcode!==null) => update condition with the new code
+                    $sOldCode=Yii::app()->request->getPost('oldcode_'.$iSortOrderID.'_'.$iScaleID);
                     if(isset($sOldCode) && $sCode !== $sOldCode) {
                         Condition::model()->updateAll(array('value'=>$sCode), 'cqid=:cqid AND value=:value', array(':cqid'=>$iQuestionID, ':value'=>$sOldCode));
                     }
