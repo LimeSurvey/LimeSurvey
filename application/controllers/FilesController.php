6c37f6dcc38d92a7bc77a89b662ae7dffeacb804
Sam <sam@mousa.nl>
2015-06-17 16:12:20 +0200

    Dev Added elfinder / tinymce integration.

---
+++ application/controllers/FilesController.php
@@ -0,0 +1,121 @@
+<?php
+namespace ls\controllers;
+use \Yii;
+use CFileHelper, CClientScript, CHtml;
+use \elFinder, elFinderConnector;
+class FilesController extends Controller
+{
+    public function actionManage($cmd = null, $dialog = false)
+    {
+        foreach (App()->log->routes as $route) {
+            if ($route instanceof \CWebLogRoute) {
+                $route->enabled = false;
+            }
+        }
+        $dir = __DIR__ . '/../vendor/studio-42/elfinder/';
+        $url = App()->assetManager->publish($dir);
+
+        if (!isset($cmd)) {
+            $clientOptions = [
+                'url' => App()->createUrl('files/manage'),
+                'lang' => 'en',
+                'resizable' => false,
+                'loadTmbs' => 100,
+                'onlyMimes' =>  ["image"]
+
+
+            ];
+            if ($dialog) {
+                $this->layout = 'bare';
+                $clientOptions['getFileCallback'] = new \CJavaScriptExpression('function(file) {
+                debugger;
+                    top.tinymce.activeEditor.windowManager.getParams().callback(file.url, {
+                        alt: file.name,
+                        width: file.width,
+                        height: file.height
+
+                    });
+                    top.tinymce.activeEditor.windowManager.close();
+
+                }');
+            }
+            $dir = __DIR__ . '/../vendor/studio-42/elfinder/';
+            $url = App()->assetManager->publish($dir);
+            App()->clientScript->registerScriptFile($url . '/js/elFinder.js');
+            App()->clientScript->registerPackage('jqueryui');
+            App()->clientScript->registerCssFile($url . '/jquery/ui-themes/smoothness/jquery-ui-1.10.1.custom.min.css');
+
+            foreach (CFileHelper::findFiles($dir . '/js', [
+                'fileTypes' => ['js'],
+                'absolutePaths' => false
+            ]) as $file) {
+                App()->clientScript->registerScriptFile($url . '/js/' . $file);
+            }
+
+            foreach (CFileHelper::findFiles($dir . '/css', [
+                'fileTypes' => ['css'],
+                'absolutePaths' => false
+            ]) as $file) {
+                App()->clientScript->registerCssFile($url . '/css/' . $file);
+            }
+            App()->clientScript->registerScript('init', "$('#elfinder').elfinder(" . \CJavaScript::encode($clientOptions) . ");",
+                CClientScript::POS_READY);
+            $this->renderText(CHtml::tag('div', [
+                'class' => 'col-md-12'
+            ], CHtml::tag('div', [
+                'id' => 'elfinder',
+                'style' => 'height: 200px;'
+            ])));
+        } else {
+
+            $finder = new elFinder([
+                'debug' => true,
+                'roots' => $this->getRoots($url)
+//                [
+//                    [
+//                        'driver' => 'LocalFileSystem',
+//                        'tmbPath' => '/tmp',
+//                        'tmbURL' => '/tmp',
+//                        'path'   => Yii::getPathOfAlias('webroot'),
+//                        'URL' => '/files'
+//                    ],
+//                    [
+//                        'driver' => 'LocalFileSystem',
+//                        'tmbPath' => App()->assetManager->basePath . '/thumbs',
+//                        'tmbURL' => App()->assetManager->baseUrl . '/thumbs',
+//                        'URL' => '/protected/messages',
+//                        'path'   => Yii::getPathOfAlias('application') . '/messages',
+//
+//                    ],
+
+//                ],
+            ]);
+
+            $connector = new elFinderConnector($finder, true);
+            $connector->run();
+        }
+    }
+
+
+    protected function getRoots($url)
+    {
+        $result = [];
+        // Get accessible surveys.
+        foreach (\Survey::model()->accessible()->findAll() as $survey) {
+            if (!is_dir(Yii::getPathOfAlias('webroot') . "/upload/surveys/{$survey->primaryKey}")) {
+                mkdir(Yii::getPathOfAlias('webroot') . "/upload/surveys/{$survey->primaryKey}");
+            }
+            $result[] = [
+                'alias' => "Survey ({$survey->primaryKey})",
+                'driver' => 'LocalFileSystem',
+                'tmbPath' => App()->assetManager->basePath . "$url/thumbs",
+                'tmbURL' => $url . '/thumbs',
+                'path'   => Yii::getPathOfAlias('webroot') . "/upload/surveys/{$survey->primaryKey}",
+                'URL' => App()->baseUrl . "/upload/surveys/{$survey->primaryKey}",
+                'icon' => "$url/img/volume_icon_local.png"
+            ];
+        }
+
+        return $result;
+    }
+}
--- scripts/htmleditor.js
+++ scripts/htmleditor.js
@@ -2,7 +2,30 @@
 
     tinymce.init({
         'selector' :'textarea.html',
+        'file_picker_callback' : function(callback, value, meta) {
+            tinymce.activeEditor.windowManager.open({
+                file: '/index.php?r=files/manage&dialog=1',
+                title: 'elFinder',
+                width: 900,
+                height: 480,
+                resizable: true
+            }, {
+                callback: callback,
+                value: value,
+                meta: meta
+            });
+            return false;
+        },
+        'file_picker_types': 'file image media',
         'menubar' : false,
+        //'toolbar' : 'file',
+        toolbar: "undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | " +
+        "bullist numlist outdent indent | link image fullscreen",
+        plugins: [
+            'link',
+            'image',
+            'fullscreen'
+        ],
         'setup' : function(editor) {
             editor.on('change', function() {
                 editor.save();
@@ -12,3 +35,18 @@
     });
 })(jQuery);
 
+
+function elFinderBrowser (field_name, url, type, win) {
+    tinymce.activeEditor.windowManager.open({
+        file: '/elfinder/elfinder.html',// use an absolute path!
+        title: 'elFinder 2.0',
+        width: 900,
+        height: 450,
+        resizable: 'yes'
+    }, {
+        setUrl: function (url) {
+            win.document.getElementById(field_name).value = url;
+        }
+    });
+    return false;
+}
