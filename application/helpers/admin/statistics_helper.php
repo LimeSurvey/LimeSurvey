ffafd5c013c7c835d7e3fabff902773dfd6a0f14
Carsten Schmitz <carsten.schmitz@limesurvey.org>
2015-07-30 11:21:41 +0200

    Fixed issue #9744: Multiple choice  question type label 'Not displayed' missing in statistics graph

--- application/helpers/admin/statistics_helper.php
+++ application/helpers/admin/statistics_helper.php
@@ -2148,7 +2148,7 @@ class statistics_helper {
             }    //end else -> noncompleted NOT checked
         }
 
-        // For multi question type, we have to check non completed with ALL sub question set to NULL
+        // For multiple choice question type, we have to check non completed with ALL sub question set to NULL
         if(($outputs['qtype'] == "M") or ($outputs['qtype'] == "P"))
         {
             $criteria = new CDbCriteria;
@@ -2194,6 +2194,7 @@ class statistics_helper {
                         $fname=gT("Not displayed");
                     }
                     $label[]= $fname;
+                    $lbl[$fname] = $multiNotDisplayed;
                     //we need some data
                     if ($results > 0)
                     {
