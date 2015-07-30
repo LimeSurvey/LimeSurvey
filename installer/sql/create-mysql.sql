a434bc9167c7a44d61c58937092650d400454c29
Carsten Schmitz <carsten.schmitz@limesurvey.org>
2015-07-30 09:44:57 +0200

    Fixed issue #9726: File browser not properly behaving due to old cached files

--- installer/sql/create-mysql.sql
+++ installer/sql/create-mysql.sql
@@ -575,4 +575,4 @@ CREATE INDEX `parent_qid_idx` ON `prefix_questions` (`parent_qid`);
 --
 -- Version Info
 --
-INSERT INTO `prefix_settings_global` VALUES ('DBVersion', '181');
+INSERT INTO `prefix_settings_global` VALUES ('DBVersion', '182');
