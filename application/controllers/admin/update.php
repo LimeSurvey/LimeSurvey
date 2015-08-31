4db8103f36c3166379d7bf588175b8565d50f009
louis <louis.gac@limesurvey.org>
2015-08-31 14:47:02 +0200

    Dev: fixed DateInterval

--- application/controllers/admin/update.php
+++ application/controllers/admin/update.php
@@ -276,7 +276,7 @@ class update extends Survey_Common_Action
                             Yii::app()->session['update_result'] = null;
                             Yii::app()->session['security_update'] = null;
                             $today = new DateTime("now");
-                            Yii::app()->session['next_update_check'] = $today->add(new DateInterval('P6h'));                            
+                            Yii::app()->session['next_update_check'] = $today->add(new DateInterval('PT6h'));                            
                             
                             // TODO : aData should contains information about each step
                             return $this->controller->renderPartial('update/updater/steps/_final', array(), false, false);    
