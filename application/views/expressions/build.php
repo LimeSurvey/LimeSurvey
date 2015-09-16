f7b1c83d284b036a038892cce38b20551f28eda2
Sam <sam@mousa.nl>
2015-09-16 16:54:52 +0200

    Dev add group level to expression builder.

--- application/views/expressions/build.php
+++ application/views/expressions/build.php
@@ -1,61 +1,66 @@
 <?php
 $tree = [];
 /** @var Survey $survey */
-foreach($survey->questions as $question) {
-    // Check subquestions.
-    if ($question->hasSubQuestions) {
-        $subQuestions = [];
-        /** @var \ls\interfaces\iSubQuestion $subQuestion */
-        foreach($question->getSubQuestions() as $subQuestion) {
-            if ($question->hasAnswers) {
-                $children = array_values(array_map(function (\ls\interfaces\iAnswer $answer) use ($subQuestion) {
-                    return [
-                        'text' => $answer->getLabel(),
-                        'icon' => 'asterisk',
-                        'tags' => ["{$subQuestion->getCode()} == \"{$answer->getCode()}\""]
-                    ];
 
-                }, $question->getAnswers()));
+foreach($survey->groups as $group) {
+    $groupTree = [];
+    foreach ($group->questions as $question) {
+        // Check subquestions.
+        if ($question->hasSubQuestions) {
+            $subQuestions = [];
+            /** @var \ls\interfaces\iSubQuestion $subQuestion */
+            foreach ($question->getSubQuestions() as $subQuestion) {
+                if ($question->hasAnswers) {
+                    $children = array_values(array_map(function (\ls\interfaces\iAnswer $answer) use ($subQuestion) {
+                        return [
+                            'text' => $answer->getLabel(),
+                            'icon' => 'asterisk',
+                            'tags' => ["{$subQuestion->getCode()} == \"{$answer->getCode()}\""]
+                        ];
+
+                    }, $question->getAnswers()));
+                }
+
+                $node = [
+                    'text' => $subQuestion->getLabel(),
+                    'children' => $question->hasAnswers ? $children : null,
+                    'icon' => !$question->hasAnswers ? 'pencil' : 'th-list'
+                ];
+
+                $subQuestions[] = $node;
             }
+            $groupTree[] = [
+                'text' => $question->displayLabel,
+                'children' => $subQuestions
+            ];
+        } elseif ($question->hasAnswers) {
+            $children = array_values(array_map(function (\ls\interfaces\iAnswer $answer) use ($question) {
+                return [
+                    'text' => $answer->getLabel(),
+                    'icon' => 'asterisk',
+                    'tags' => ["{$question->title} == \"{$answer->getCode()}\""]
+                ];
 
-            $node = [
-                'text' => $subQuestion->getLabel(),
-                'children' => $question->hasAnswers ? $children : null,
-                'icon' => !$question->hasAnswers ? 'pencil' : 'th-list'
+            }, $question->getAnswers()));
+            $groupTree[] = [
+                'text' => $question->getDisplayLabel(),
+                'children' => $children,
+
             ];
 
-            $subQuestions[] = $node;
+        } else {
+            $groupTree[] = [
+                'text' => $question->getDisplayLabel(),
+                'icon' => 'pencil'
+            ];
+
         }
-        $tree[] = [
-            'text' => $question->displayLabel,
-            'children' => $subQuestions
-        ];
-    } elseif ($question->hasAnswers) {
-        $children = array_values(array_map(function (\ls\interfaces\iAnswer $answer) use ($question) {
-            return [
-                'text' => $answer->getLabel(),
-                'icon' => 'asterisk',
-                'tags' => ["{$question->title} == \"{$answer->getCode()}\""]
-            ];
-
-        }, $question->getAnswers()));
-        $tree[] = [
-            'text' => $question->getDisplayLabel(),
-            'children' => $children,
-
-        ];
-
     }
-
-    else {
-        $tree[] = [
-            'text' => $question->getDisplayLabel(),
-            'icon' => 'pencil'
-        ];
-
-    }
+    $tree[] = [
+        'children' => $groupTree,
+        'text' => $group->title
+    ];
 }
-
 //vdd($tree);
 //
 $this->widget(\SamIT\Yii1\Widgets\BootstrapTreeView::class, [
