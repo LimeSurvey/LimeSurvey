1a9586d4252755ad29cc0f902dca397db018bc94
Sam <sam@mousa.nl>
2015-09-16 16:18:57 +0200

    Dev Fixed importing surveys exported from LS3

---
+++ application/helpers/import/importers/Import3_0.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sam
+ * Date: 6/18/15
+ * Time: 1:45 PM
+ */
+
+namespace ls\import\importers;
+
+
+use ls\import\BaseElementXmlImport;
+use ls\import\BaseImport;
+
+/**
+ * Class Import3_0
+ * @package ls\import\importers
+ */
+class Import3_0 extends Import182 {
+
+}
