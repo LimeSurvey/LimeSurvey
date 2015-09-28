9c3ac19093fd5959768f0097b341b51a36ce8153
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2015-09-28 15:59:24 +0200

    Dev: Style fixes for Edit question and Add question tabs

--- application/views/admin/survey/Question/newQuestion_subviews/_tabs.php
+++ application/views/admin/survey/Question/newQuestion_subviews/_tabs.php
@@ -31,35 +31,35 @@
     <div id="<?php echo $eqrow['language']; ?>" class="tab-pane fade in active">
 
         <!-- Question Code -->
-        <div class="form-group col-sm-12" style="padding-left: 0px;">
-            <label class="col-sm-1 control-label"  for='title' style="padding-left: 0px;">
-                <?php eT("Code:"); ?>
-            </label>
-            <div class="col-sm-4" style="padding-left: 0px;">
-                <input type='text' autofocus="autofocus" size='20' maxlength='20' id='title' required='required' name='title' pattern='<?php echo $sPattern ?>' value="<?php echo $eqrow['title']; ?>" />
-            </div>
-            <div class="col-sm-1" style="padding-left: 0px;">
-                <span class='text-warning'><?php  eT("Required"); ?> </span>
+        <div class="form-group">
+			<div class="col-lg-offset-1">
+                <label class="col-sm-1 control-label" for='title'><?php eT("Code:"); ?></label>
+
+                <div class="col-sm-5">
+                    <input class="form-control" type='text' autofocus="autofocus" size='20' maxlength='20' id='title' required='required' name='title' pattern='<?php echo $sPattern ?>' value="<?php echo $eqrow['title']; ?>" />
+                </div>
+                <div class="col-sm-6">
+                    <span class='text-warning'><?php  eT("Required"); ?> </span>
+                </div>
             </div>
         </div>
 
         <!-- Question Text -->
         <div class="form-group">
-            <label for='question_<?php echo $eqrow['language']; ?>' class=""> <?php eT("Question:"); ?></label>
-            <div class='htmleditor' >
-                <textarea cols='80' rows='100' id='question_<?php echo $eqrow['language']; ?>' name='question_<?php echo $eqrow['language']; ?>'><?php echo $eqrow['question']; ?></textarea>                
+			<div class="col-lg-offset-1">
+                <label class="control-label" for='question_<?php echo $eqrow['language']; ?>' class=""> <?php eT("Question:"); ?></label>
+                <textarea cols='80' rows='100' id='question_<?php echo $eqrow['language']; ?>' name='question_<?php echo $eqrow['language']; ?>'><?php echo $eqrow['question']; ?></textarea>
+                <?php echo getEditor("question-text","question_".$eqrow['language'], "[".gT("Question:", "js")."](".$eqrow['language'].")",$surveyid,$gid,$qid,$action); ?>
             </div>
-            <?php echo getEditor("question-text","question_".$eqrow['language'], "[".gT("Question:", "js")."](".$eqrow['language'].")",$surveyid,$gid,$qid,$action); ?>                  
         </div>
         
         <!-- Question Help -->
         <div class="form-group">
-            <label for='help_<?php echo $eqrow['language']; ?>' class=""><?php eT("Help:"); ?></label>
-            
-            <div class='htmleditor' >
-                <textarea cols='45' rows='4' id='help_<?php echo $eqrow['language']; ?>' name='help_<?php echo $eqrow['language']; ?>'><?php echo $eqrow['help']; ?></textarea>                
+			<div class="col-lg-offset-1">
+                <label class="control-label" for='help_<?php echo $eqrow['language']; ?>' class=""><?php eT("Help:"); ?></label>
+                <textarea cols='45' rows='4' id='help_<?php echo $eqrow['language']; ?>' name='help_<?php echo $eqrow['language']; ?>'><?php echo $eqrow['help']; ?></textarea>
+                <?php echo getEditor("question-help","help_".$eqrow['language'], "[".gT("Help:", "js")."](".$eqrow['language'].")",$surveyid,$gid,$qid,$action); ?>
             </div>
-            <?php echo getEditor("question-help","help_".$eqrow['language'], "[".gT("Help:", "js")."](".$eqrow['language'].")",$surveyid,$gid,$qid,$action); ?>                  
         </div>
     </div>
     
@@ -67,22 +67,23 @@
     <?php foreach  ($addlanguages as $addlanguage): ?>
         <div id="<?php echo $addlanguage; ?>"  class="tab-pane fade">
             
-
             <!-- Question Text -->
             <div class="form-group">
-                <label for='question_<?php echo $addlanguage; ?>'><?php eT("Question:"); ?></label>
-                <div class='htmleditor' >
-                    <textarea cols='80' rows='4' id='question_<?php echo $addlanguage; ?>' name='question_<?php echo $addlanguage; ?>'></textarea>                
+                <div class="col-lg-offset-1">
+                    <label class="control-label" for='question_<?php echo $addlanguage; ?>'><?php eT("Question:"); ?></label>
+                    <div class='htmleditor' >
+                        <textarea cols='80' rows='4' id='question_<?php echo $addlanguage; ?>' name='question_<?php echo $addlanguage; ?>'></textarea>
+                    </div>
+                    <?php echo getEditor("question-text","question_".$addlanguage, "[".gT("Question:", "js")."](".$addlanguage.")",$surveyid,$gid,$qid,$action); ?>
                 </div>
-                <?php echo getEditor("question-text","question_".$addlanguage, "[".gT("Question:", "js")."](".$addlanguage.")",$surveyid,$gid,$qid,$action); ?>                  
             </div>
 
             <!-- Question Help -->
             <div class="form-group">
-                <label for='help_<?php echo $addlanguage; ?>'><?php eT("Help:"); ?></label>
-                <div class='htmleditor'>
+                <div class="col-lg-offset-1">
+                    <label for='help_<?php echo $addlanguage; ?>'><?php eT("Help:"); ?></label>
                     <textarea cols='45' rows='4' id='help_<?php echo $addlanguage; ?>' name='help_<?php echo $addlanguage; ?>'></textarea>
-                    <?php echo getEditor("question-help","help_".$addlanguage, "[".gT("Help:", "js")."](".$addlanguage.")",$surveyid,$gid,$qid,$action); ?>                
+                    <?php echo getEditor("question-help","help_".$addlanguage, "[".gT("Help:", "js")."](".$addlanguage.")",$surveyid,$gid,$qid,$action); ?>
                 </div>
             </div>
         </div>
