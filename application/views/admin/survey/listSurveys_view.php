7b370100f9fae82a722d730396311ae6cd82a450
louis <louis.gac@limesurvey.org>
2015-09-29 12:46:51 +0200

    Dev : list survey form, inline presentation, active state corrected

--- application/views/admin/survey/listSurveys_view.php
+++ application/views/admin/survey/listSurveys_view.php
@@ -8,57 +8,50 @@
 ?>
 <?php $pageSize=Yii::app()->user->getState('pageSize',Yii::app()->params['defaultPageSize']);?>
 <div class="col-lg-12 list-surveys">
-	<h3><?php eT('Survey list'); ?></h3>
+    <h3><?php eT('Survey list'); ?></h3>
 
     <!-- Search Box -->
     <div class="row">
         <div class="col-lg-12">
-            <div class="form">
+            <div class="form text-right">
                 <!-- Begin Form -->
                 <?php $form=$this->beginWidget('CActiveForm', array(
                     'action' => Yii::app()->createUrl('admin/survey/sa/listsurveys/'),
                     'method' => 'get',
                         'htmlOptions'=>array(
-                            'class'=>'form-horizontal',
+                            'class'=>'form-inline',
                         ),
                 )); ?>
                 
                     <!-- search input -->
                     <div class="form-group">
-                        <?php echo $form->label($model, 'search: ', array('class'=>'col-sm-2 control-label text-right col-sm-offset-6')); ?>
-                        <div class="col-sm-2 text-right">
-                            <?php echo $form->textField($model, 'searched_value', array('class'=>'form-control')); ?>
-                        </div>
+                        <?php echo $form->label($model, 'search: ', array('class'=>'control-label')); ?>
+                        <?php echo $form->textField($model, 'searched_value', array('class'=>'form-control')); ?>
                     </div>
 
                     <!-- select state -->
                     <div class="form-group">
-                        <?php echo $form->label($model, 'Active:', array('class'=>'col-sm-2 control-label text-right col-sm-offset-6')); ?>
-                        <div class="col-sm-2 text-right">
+                        <?php echo $form->label($model, 'Active:', array('class'=>'control-label')); ?>
                             <select name="active" class="form-control">
-                                <option value=""><?php eT('any state');?></option>
+                                <option value="" <?php if( $model->active!="Y" && $model->active!="N" ){echo "selected";}?>><?php eT('any state');?></option>
                                 <option value="Y" <?php if( $model->active=="Y"){echo "selected";}?>><?php eT('Yes');?></option>
                                 <option value="N" <?php if( $model->active=="N"){echo "selected";}?>><?php eT('No');?></option>
                             </select>
-                        </div>
-                        <div class="col-sm-2">
-                            <?php echo CHtml::submitButton('Search', array('class'=>'btn btn-success')); ?>
-                            <a href="<?php echo Yii::app()->createUrl('admin/survey/sa/listsurveys');?>" class="btn btn-warning"><?php eT('reset');?></a>
-                        </div>                        
                     </div>    
-    
+                            <?php echo CHtml::submitButton('Search', array('class'=>'btn btn-success')); ?>
+                            <a href="<?php echo Yii::app()->createUrl('admin/survey/sa/listsurveys');?>" class="btn btn-warning"><?php eT('reset');?></a>    
                     
                 <?php $this->endWidget(); ?>
             </div>                
         </div>
     </div>
 
-    <!-- Grid -->	
-	<div class="row">
-		<div class="col-lg-12 content-right">
-        	<?php
-				$this->widget('bootstrap.widgets.TbGridView', array(
-					'dataProvider' => $model->search(),
+    <!-- Grid -->    
+    <div class="row">
+        <div class="col-lg-12 content-right">
+            <?php
+                $this->widget('bootstrap.widgets.TbGridView', array(
+                    'dataProvider' => $model->search(),
 
                     // Number of row per page selection
                     'id' => 'survey-grid',
@@ -68,79 +61,79 @@
                             $pageSize,
                             Yii::app()->params['pageSizeOptions'],
                             array('class'=>'changePageSize form-control', 'style'=>'display: inline; width: auto')) .
-                        gT(' rows per page'),                   					
+                        gT(' rows per page'),                                       
 
-					'columns' => array(
-						array(
-							'name' => 'Survey id',
-							'value'=>'$data->sid',
-							'htmlOptions' => array('class' => 'col-md-1'),
-						),
-						
-						array(
-							'name' => 'Title',
-							'value'=>'$data->defaultlanguage->surveyls_title',
-							'htmlOptions' => array('class' => 'col-md-1'),
-						),
-														
-						array(
-							'name' => 'Creation date',
-							'value'=>'$data->creationdate',
-							'htmlOptions' => array('class' => 'col-md-1'),
-						),
+                    'columns' => array(
+                        array(
+                            'name' => 'Survey id',
+                            'value'=>'$data->sid',
+                            'htmlOptions' => array('class' => 'col-md-1'),
+                        ),
+                        
+                        array(
+                            'name' => 'Title',
+                            'value'=>'$data->defaultlanguage->surveyls_title',
+                            'htmlOptions' => array('class' => 'col-md-1'),
+                        ),
+                                                        
+                        array(
+                            'name' => 'Creation date',
+                            'value'=>'$data->creationdate',
+                            'htmlOptions' => array('class' => 'col-md-1'),
+                        ),
 
-						array(
-							'name' => 'Owner',
-							'value'=>'$data->owner->users_name',
-							'htmlOptions' => array('class' => 'col-md-1'),
-						),
+                        array(
+                            'name' => 'Owner',
+                            'value'=>'$data->owner->users_name',
+                            'htmlOptions' => array('class' => 'col-md-1'),
+                        ),
 
-						array(
-							'name' => 'Anonymized responses',
-							'value'=>'$data->anonymizedResponses',
-							'htmlOptions' => array('class' => 'col-md-1'),
-						),
+                        array(
+                            'name' => 'Anonymized responses',
+                            'value'=>'$data->anonymizedResponses',
+                            'htmlOptions' => array('class' => 'col-md-1'),
+                        ),
 
-						array(
-							'name' => 'Active',
-							'value'=>'$data->activeWord',
-							'htmlOptions' => array('class' => 'col-md-1'),
-						),						
+                        array(
+                            'name' => 'Active',
+                            'value'=>'$data->activeWord',
+                            'htmlOptions' => array('class' => 'col-md-1'),
+                        ),                        
 
-						array(
-							'name' => 'Partial',
-							'value'=>'$data->countPartialAnswers',
-							'htmlOptions' => array('class' => 'col-md-1'),
-						),
-																																						
-						array(
-							'name' => 'Full',
-							'value'=>'$data->countFullAnswers',
-							'htmlOptions' => array('class' => 'col-md-1'),
-						),																																								
+                        array(
+                            'name' => 'Partial',
+                            'value'=>'$data->countPartialAnswers',
+                            'htmlOptions' => array('class' => 'col-md-1'),
+                        ),
+                                                                                                                                                        
+                        array(
+                            'name' => 'Full',
+                            'value'=>'$data->countFullAnswers',
+                            'htmlOptions' => array('class' => 'col-md-1'),
+                        ),                                                                                                                                                                
 
-						array(
-							'name' => 'Total',
-							'value'=>'$data->countTotalAnswers',
-							'htmlOptions' => array('class' => 'col-md-1'),
-						),
+                        array(
+                            'name' => 'Total',
+                            'value'=>'$data->countTotalAnswers',
+                            'htmlOptions' => array('class' => 'col-md-1'),
+                        ),
 
-						array(
-							'name' => '',
-							'value'=>'$data->buttons',
-						    'type'=>'raw',
-							'htmlOptions' => array('class' => 'col-md-1'),
-						),
+                        array(
+                            'name' => '',
+                            'value'=>'$data->buttons',
+                            'type'=>'raw',
+                            'htmlOptions' => array('class' => 'col-md-1'),
+                        ),
 
-					),
+                    ),
 
                     'htmlOptions'=>array('style'=>'cursor: pointer;', 'class'=>'hoverAction'),
                     'selectionChanged'=>"function(id){window.location='" . Yii::app()->urlManager->createUrl('admin/survey/sa/view/surveyid' ) . '/' . "' + $.fn.yiiGridView.getSelection(id.split(',', 1));}",
                     'ajaxUpdate' => true,
-   				)); 
-        	?>
-		</div>
-	</div>
+                   )); 
+            ?>
+        </div>
+    </div>
 </div>
 
 <!-- To update rows per page via ajax -->
