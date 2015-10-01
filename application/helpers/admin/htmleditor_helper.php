5323d0261a69112c13682ba91373c79ea3138a63
Denis Chenu <courriel@shnoulle.net>
2015-10-01 16:47:17 +0200

    Fixed issue #09899: HTML editor launched even for email set to text Dev: fix existing control Dev: Now test start of textarea for "email-"

--- application/helpers/admin/htmleditor_helper.php
+++ application/helpers/admin/htmleditor_helper.php
@@ -148,14 +148,7 @@
         {
             $htmleditormode = $session['htmleditormode'];
         }
-
-        if ( ($fieldtype == 'email-inv' ||
-        $fieldtype == 'email-reg' ||
-        $fieldtype == 'email-admin-notification' ||
-        $fieldtype == 'email-admin-resp' ||
-        $fieldtype == 'email-conf' ||
-        $fieldtype == 'email-rem' ) &&
-        getEmailFormat($surveyID) != 'html')
+        if ( $surveyID && getEmailFormat($surveyID) != 'html' && substr($fieldtype,0,6)==="email-" )// email but survey as text email
         {
             return '';
         }
