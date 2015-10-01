5a2810abd5548a3e3232f2fd92600c23633b5c64
louis <louis.gac@limesurvey.org>
2015-10-01 13:00:04 +0200

    Dev : first step, change the default admin language page

--- application/views/admin/super/welcome.php
+++ application/views/admin/super/welcome.php
@@ -31,6 +31,43 @@
         </div>
     </div>
 
+    <!-- Message when first start -->
+    <?php if($countSurveyList==0):?>
+        <script type="text/javascript">
+            $(window).load(function(){
+                $('#welcomeModal').modal('show');
+            });
+        </script>        
+                
+        <div class="modal fade" id="welcomeModal">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                      <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                      <h4 class="modal-title"><?php echo sprintf(gT("Welcome to %s!"), 'LimeSurvey'); ?></h4>
+                    </div>
+                    <div class="modal-body">
+                        <p><?php eT("Some piece-of-cake steps to create your very own first survey:"); ?></p>
+                        <ol>
+                            <li><?php echo sprintf(gT('Create a new survey clicking on the %s icon.'), "<img src='" . Yii::app()->getConfig('adminimageurl') . "add_20.png' name='ShowHelp' title='' alt='" . gT("Add survey") . "'/>"); ?></li>
+                            <li><?php eT('Create a new question group inside your survey.'); ?></li>
+                            <li><?php eT('Create one or more questions inside the new question group.'); ?></li>
+                            <li><?php echo sprintf(gT('Done. Test your survey using the %s icon.'), "<img src='" . Yii::app()->getConfig('adminimageurl') . "do_20.png' name='ShowHelp' title='' alt='" . gT("Test survey") . "'/>"); ?></li>
+                        </ol>        
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-default" data-dismiss="modal"><?php eT('Close');?></button>
+                      <button type="button" class="btn btn-primary"><?php eT('Create a new survey');?></button>
+                    </div>
+                </div><!-- /.modal-content -->
+            </div><!-- /.modal-dialog -->
+        </div><!-- /.modal -->        
+        
+        
+        
+        
+    <?php endif;?>
+
     <!-- Last visited survey/question -->
     <?php if($showLastSurvey || $showLastQuestion):?>
         <div class="row text-right">
