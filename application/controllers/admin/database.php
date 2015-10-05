67cf6daa9a03a373189d09d492253f221326cd2d
louis <louis.gac@limesurvey.org>
2015-10-05 13:39:54 +0200

    Dev: removed notice while saving subquestions

--- application/controllers/admin/database.php
+++ application/controllers/admin/database.php
@@ -286,7 +286,10 @@ class database extends Survey_Common_Action
                                 $oSubQuestion->parent_qid=$iQuestionID;
                                 $oSubQuestion->language=$sLanguage;
                                 $oSubQuestion->scale_id=$iScaleID;
-                                $oSubQuestion->relevance=$aRelevance[0][$iPosition];
+                                if(isset($aRelevance[0][$iPosition]))
+                                {
+                                    $oSubQuestion->relevance=$aRelevance[0][$iPosition];
+                                }
                             }
                             else                                                //new record: additional language
                             {
