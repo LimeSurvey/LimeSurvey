9b209b93f490b48cc0d7bbe79f9cabdb1450c77e
Carsten Schmitz <carsten.schmitz@limesurvey.org>
2015-11-03 10:22:00 +0100

    Dev Removed duplicate code

--- application/models/Participant.php
+++ application/models/Participant.php
@@ -127,7 +127,16 @@ class Participant extends LSActiveRecord
 
     static function gen_uuid()
     {
-        return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x', mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0x0fff) | 0x4000, mt_rand(0, 0x3fff) | 0x8000, mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
+        return sprintf(
+                '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+                mt_rand(0, 0xffff),
+                mt_rand(0, 0xffff),
+                mt_rand(0, 0xffff),
+                mt_rand(0, 0x0fff) | 0x4000,
+                mt_rand(0, 0x3fff) | 0x8000,
+                mt_rand(0, 0xffff),
+                mt_rand(0, 0xffff),
+                mt_rand(0, 0xffff)
         );
     }
 
