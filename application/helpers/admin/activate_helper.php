ce31ce0449f3f159048411b0cf412a126f2f67e7
kairavesloot <ravesloot@lime-support.com>
2015-11-05 12:02:49 +0100

    New feature: Plugin Event "after Table create"
    refactor variable names

--- application/helpers/admin/activate_helper.php
+++ application/helpers/admin/activate_helper.php
@@ -382,12 +382,12 @@ function activateSurvey($iSurveyID, $simulate = false)
     // If last question is of type MCABCEFHP^QKJR let's get rid of the ending coma in createsurvey
     //$createsurvey = rtrim($createsurvey, ",\n")."\n"; // Does nothing if not ending with a comma
 
-    $tabname = "{{survey_{$iSurveyID}}}";
+    $sTabname = "{{survey_{$iSurveyID}}}";
     Yii::app()->loadHelper("database");
     try
     {   
-        $execresult = Yii::app()->db->createCommand()->createTable($tabname, $createsurvey);
-        Yii::app()->db->schema->getTable($tabname, true); // Refresh schema cache just in case the table existed in the past
+        $execresult = Yii::app()->db->createCommand()->createTable($sTabname, $createsurvey);
+        Yii::app()->db->schema->getTable($sTabname, true); // Refresh schema cache just in case the table existed in the past
     }
     catch (CDbException $e)
     {
@@ -402,7 +402,7 @@ function activateSurvey($iSurveyID, $simulate = false)
     }
     try
     {
-        if (isset($createsurvey['token'])) Yii::app()->db->createCommand()->createIndex("idx_survey_token_{$iSurveyID}_".rand(1,50000),$tabname,'token');
+        if (isset($createsurvey['token'])) Yii::app()->db->createCommand()->createIndex("idx_survey_token_{$iSurveyID}_".rand(1,50000),$sTabname,'token');
     }
         catch (CDbException $e)
     {
@@ -439,31 +439,31 @@ function activateSurvey($iSurveyID, $simulate = false)
     {
         $timingsfieldmap = createTimingsFieldMap($iSurveyID,"full",false,false,getBaseLanguageFromSurveyID($iSurveyID));
 
-        $success = true;
-        $column = array();
-        $column['id'] = $createsurvey['id'];
+        $bSuccess = true;
+        $aColumn = array();
+        $aColumn['id'] = $createsurvey['id'];
         foreach ($timingsfieldmap as $field=>$fielddata)
         {
-            $column[$field] = 'FLOAT';
+            $aColumn[$field] = 'FLOAT';
         }
 
-        $tabname = "{{survey_{$iSurveyID}_timings}}";
+        $sTabname = "{{survey_{$iSurveyID}_timings}}";
         try
         {
-            $execresult = Yii::app()->db->createCommand()->createTable($tabname,$column);
-            Yii::app()->db->schema->getTable($tabname, true); // Refresh schema cache just in case the table existed in the past
+            $execresult = Yii::app()->db->createCommand()->createTable($sTabname,$aColumn);
+            Yii::app()->db->schema->getTable($sTabname, true); // Refresh schema cache just in case the table existed in the past
         }
         catch (CDbException $e)
         {
-            $success = false;
+            $bSuccess = false;
             return array('error'=>'timingstablecreation');
         }
-        if ($success){
-            $event = new PluginEvent('afterTableCreate');
-            $event->set('surveyId', $iSurveyID);
-            $event->set('type', 'timings');
-            $event->set('name', $tabname);
-            App()->getPluginManager()->dispatchEvent($event);
+        if ($bSuccess){
+            $oEvent = new PluginEvent('afterTableCreate');
+            $oEvent->set('surveyId', $iSurveyID);
+            $oEvent->set('type', 'timings');
+            $oEvent->set('name', $sTabname);
+            App()->getPluginManager()->dispatchEvent($oEvent);
         }
     }
 
