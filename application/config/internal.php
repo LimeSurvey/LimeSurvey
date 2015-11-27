b214a06380a8c60bcbb3d01dbebad1c1a1417a4f
LouisGac <louis.gac@limesurvey.org>
2015-11-27 10:15:16 +0100

    Remote Control merge

--- application/config/internal.php
+++ application/config/internal.php
@@ -31,7 +31,7 @@ $internalConfig = array(
     ),
 
     'params'=>array(
-        'defaultPageSize'=>10,
+        'defaultPageSize'=>10	,
         'pageSizeOptions'=>array(5=>5,10=>10,20=>20,50=>50,100=>100),
     ),
 
