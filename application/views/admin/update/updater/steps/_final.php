7a7a6e23e7492837c44f1e9f0179f6903a33f11b
LouisGac <louis.gac@limesurvey.org>
2015-12-03 16:40:12 +0100

    Dev: force logout after update for Yii cache issues

--- application/views/admin/update/updater/steps/_final.php
+++ application/views/admin/update/updater/steps/_final.php
@@ -9,20 +9,19 @@
 <div class="updater-background">
     <?php
         echo sprintf(gT('Buildnumber was successfully updated to %s.'),Yii::app()->session['updateinfo']['toversion']).'<br />';
-        eT('The update is now complete!'); 
-    ?> 
+        eT('The update is now complete!');
+    ?>
     <br/>
     <?php
         eT("if needed the database will be updated as a last step.");
     ?>
-    <br /> 
-<?php  eT('As a last step you should clear your browser cache now.'); ?> 
-  <br /> 
+    <br />
+    <?php  eT('As a last step you should clear your browser cache now.'); ?>
+  <br />
 
-	<a id="backToMainMenu" class="btn btn-default" href="<?php echo Yii::app()->createUrl("admin/update"); ?>" role="button" aria-disabled="false">
-		<?php eT('Back to main menu'); ?>
-	</a>	    
-		
+	<a id="backToMainMenu" class="btn btn-default" href="<?php echo Yii::app()->createUrl("admin/authentication/sa/logout"); ?>" role="button" aria-disabled="false">
+		<?php eT('Please, click this button to logout '); ?>
+	</a>
+
 
 </div>
-	
