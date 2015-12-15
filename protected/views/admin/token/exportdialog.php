1b3d8c05d8b5201b874c4b199709561328a7f739
Sam Mousa <sam@mousa.nl>
2015-12-15 10:32:53 +0100

    Removed old files.

--- protected/views/admin/token/exportdialog.php
+++
@@ -1,9 +0,0 @@
-<div class='header ui-widget-header'><?php eT("Token export options"); ?></div>
-<?php
-$this->widget('ext.SettingsWidget.SettingsWidget', array(
-    'settings' => $aSettings,
-    'action'=>$sAction,
-    'form' => true,
-    'title' => gT("Token export options"),
-    'buttons' => $aButtons,
-));
