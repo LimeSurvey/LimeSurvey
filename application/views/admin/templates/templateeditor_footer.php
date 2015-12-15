4f32a5a3cf3561105d96e0d56cbf1c482e9efc92
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2015-12-15 15:57:29 +0100

    Fixed issue #10121: Add 'Previous' button to question page

--- application/views/admin/templates/templateeditor_footer.php
+++ application/views/admin/templates/templateeditor_footer.php
@@ -4,6 +4,7 @@
     <hr>
 </div>
 <div class="col-xs-6 save-all text-left">
+    <button type="submit" id="moveprevbtn" value="moveprev" name="moveprev" accesskey="p" class="submit button btn btn-default btn-lg ">Previous</button>
 </div>
 <div class="col-xs-6 save-all text-right">
     <button type="submit" id="movenextbtn" value="movenext" name="movenext" accesskey="n" class="submit button btn btn-default btn-lg ">Next</button>
