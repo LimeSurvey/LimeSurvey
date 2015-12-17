c3b308b984ebaa8227e9414fd65df02404a1a37f
LouisGac <louis.gac@limesurvey.org>
2015-12-17 18:12:27 +0100

    Dev: statistics display

--- application/views/admin/export/statistics_subviews/_response_filter_choice.php
+++ application/views/admin/export/statistics_subviews/_response_filter_choice.php
@@ -4,41 +4,40 @@
         <?php foreach ($aGroups as $groupKey => $aGroup):?>
 
             <!-- Group Title -->
-            <div class="row">
-                <div class="col-sm-12">
-                    <div class='header'>
+            <div class="row group-container">
+                <div class="col-sm-12 box" >
+                    <div class='header '>
                         <input type="checkbox" id='btn_<?php echo $aGroup['gid']; ?>' onclick="selectCheckboxes('grp_<?php echo $aGroup['gid']; ?>', 'summary[]', 'btn_<?php echo $aGroup['gid']; ?>');" />
-                        <span class='smalltext'>
-                            <strong>
+
+                        <span class="groupTitle">
+
                                 <?php echo $aGroup['name']; ?>
-                            </strong>
+
                             (<?php echo gT("Question group").$aGroup['gid']; ?>)
                         </span>
                     </div>
                 </div>
-            </div>
 
-            <!-- Questions container -->
-            <div class="row" id="questionContainer">
-                <div class="col-sm-12">
+
+                <!-- Questions container -->
+                <div class="col-sm-12 questionContainer" >
                     <div id='grp_<?php echo $aGroup['gid']; ?>' class="row filtertable ">
 
                     <?php foreach($aGroup['questions'] as  $key1 => $flt ): ?>
 
                         <!-- Questions -->
                         <?php $this->renderPartial('/admin/export/statistics_subviews/_question', array(
-                                                                                                            'key1'=>$key1,
-                                                                                                            'flt'=>$flt,
-                                                                                                            'filterchoice_state'=>$filterchoice_state,
-                                                                                                            'filters'=>$filters,
-                                                                                                            'aGroups'=>$aGroups,
-                                                                                                            'surveyid'=>$surveyid,
-                                                                                                            'result'=>$result,
-                                                                                                            'fresults'=>$fresults,
-                                                                                                            'summary'=>$summary,
-                                                                                                            'oStatisticsHelper'=>$oStatisticsHelper
-
-                                                                                                         )) ; ?>
+                                'key1'=>$key1,
+                                'flt'=>$flt,
+                                'filterchoice_state'=>$filterchoice_state,
+                                'filters'=>$filters,
+                                'aGroups'=>$aGroups,
+                                'surveyid'=>$surveyid,
+                                'result'=>$result,
+                                'fresults'=>$fresults,
+                                'summary'=>$summary,
+                                'oStatisticsHelper'=>$oStatisticsHelper
+                             )) ; ?>
                     <?php endforeach; ?>
                     </div>
                 </div>
