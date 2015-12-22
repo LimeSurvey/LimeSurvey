4af3da14fba6e42a82125d9006573a4214300dbc
LouisGac <louis.gac@limesurvey.org>
2015-12-22 12:54:21 +0100

    Dev: manual merge with 2.06 - scripts

--- scripts/date.js
+++ scripts/date.js
@@ -32,6 +32,7 @@ function doDropDownDate(qId){
     $(document).on("change",'#question'+qId+' select',dateUpdater);
     $(document).ready(function(){
         $("#question"+qId+" select").filter(':first').trigger("change");
+        //dateUpdater();
     });
 }
 /* This function is called each time shortly before the picker pops up.
@@ -200,9 +201,10 @@ function dateUpdater() {
         ($('#minute'+thisid).length && $('#minute'+thisid).val()==''))
         {
             //incomplete
-            $('#qattribute_answer'+thisid).val(translt.infoCompleteAll);
             $('#answer'+thisid).val('INVALID');
             $('#answer'+thisid).change();
+            // QCODE.NAOK return "" if real value is INVALID (because can be shown to user), then do it manually (line 721 em_javascript)
+            $("#vmsg_22_dropdown_dates").removeClass('good').addClass('error');
         }
         else
         {
@@ -251,7 +253,6 @@ function dateUpdater() {
             parseddate=parseddate.format($('#dateformat'+thisid).val());
             $('#answer'+thisid).val(parseddate); 
             $('#answer'+thisid).change();
-            $('#qattribute_answer'+thisid).val('');
         }
 }
 
--- scripts/numeric-slider.js
+++ scripts/numeric-slider.js
@@ -84,10 +84,11 @@ function doNumericSlider(qID,jsonOptions) {
 				}
 				$(thisinput).triggerHandler("keyup"); // Needed for EM
 			});
-			// Replace default em tip
+			// Replace default em tip 
 			$("#question"+qID).find(".em_default").text(jsonOptions.lang.tip);
 		});
 	}
     //Fix buggy chrome/webkit engine which doesn't properly apply the css rules after this insertion
     $("#question"+qID).hide().show(0);
 }
+
