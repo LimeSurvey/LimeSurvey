151cbe2f3408c65f8757bab3ea61455a52b61559
LouisGac <louis.gac@limesurvey.org>
2016-01-07 09:49:34 +0100

    Dev: corrected item_row for 0 column in multiplechoice question

--- application/views/survey/questions/multiplechoice/item_row.php
+++ application/views/survey/questions/multiplechoice/item_row.php
@@ -15,25 +15,29 @@
  * @var $sValue
  */
 ?>
-<div id='javatbd<?php echo $myfname; ?>' class='question-item answer-item checkbox-item form-group <?php echo $extra_class; ?>' <?php echo $sDisplayStyle; ?> >
-    <div class="col-lg-<?php echo $nbColInputLg; ?> col-xs-<?php echo $nbColInputXs; ?>">
-        <input
-            class="checkbox"
-            type="checkbox"
-            name="<?php echo $ia[1].$ansrow['title']; ?>"
-            id="answer<?php echo $ia[1].$ansrow['title']; ?>"
-            value="Y"
-            <?php echo $checkedState; ?>
-            onclick='cancelBubbleThis(event); <?php echo $sCheckconditionFunction; ?>'
-         />
-
-         <label for="answer<?php echo $ia[1]{$ansrow['title']}; ?>" class="answertext hidden">
-             <?php echo $ansrow['question']; ?>
-         </label>
-
-         <input type="hidden" name="java<?php echo $myfname; ?>" id="java<?php echo $myfname; ?>" value="<?php echo $sValue; ?>" />
+<div class="col-sm-12">
+    <div class='row'>
+        <div id='javatbd<?php echo $myfname; ?>' class='question-item answer-item checkbox-item form-group <?php echo $extra_class; ?>' <?php echo $sDisplayStyle; ?> >
+            <div class="col-lg-<?php echo $nbColInputLg; ?> col-xs-<?php echo $nbColInputXs; ?>">
+                <input
+                class="checkbox"
+                type="checkbox"
+                name="<?php echo $ia[1].$ansrow['title']; ?>"
+                id="answer<?php echo $ia[1].$ansrow['title']; ?>"
+                value="Y"
+                <?php echo $checkedState; ?>
+                onclick='cancelBubbleThis(event); <?php echo $sCheckconditionFunction; ?>'
+                />
+                
+                <label for="answer<?php echo $ia[1]{$ansrow['title']}; ?>" class="answertext hidden">
+                    <?php echo $ansrow['question']; ?>
+                </label>
+                
+                <input type="hidden" name="java<?php echo $myfname; ?>" id="java<?php echo $myfname; ?>" value="<?php echo $sValue; ?>" />
+            </div>
+            <label for="answer<?php echo $ia[1]{$ansrow['title']}; ?>" class="control-label col-xs-<?php echo $nbColLabelXs; ?> col-lg-<?php echo $nbColLabelLg; ?> answertext">
+                <?php echo $ansrow['question']; ?>
+            </label>
+        </div>
     </div>
-    <label for="answer<?php echo $ia[1]{$ansrow['title']}; ?>" class="control-label col-xs-<?php echo $nbColLabelXs; ?> col-lg-<?php echo $nbColLabelLg; ?> answertext">
-        <?php echo $ansrow['question']; ?>
-    </label>    
 </div>
