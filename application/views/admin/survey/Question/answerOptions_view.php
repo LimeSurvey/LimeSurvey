55f2b4a14382ec9a8f2a137f17f85a87afaade7e
LouisGac <louis.gac@limesurvey.org>
2016-01-11 10:27:22 +0100

    Dev: js vars identation in answer/subquestions

--- application/views/admin/survey/Question/answerOptions_view.php
+++ application/views/admin/survey/Question/answerOptions_view.php
@@ -1,30 +1,31 @@
-<script type='text/javascript'>assessmentvisible=<?php echo $assessmentvisible?'true':'false'; ?>;
-    var cancel = '<?php eT('Cancel'); ?>';
-    var check = true;
-    var lafail = '<?php eT('Sorry, the request failed!'); ?>';
-    var lanameurl = '<?php echo Yii::app()->createUrl('/admin/labels/sa/getAllSets'); ?>';
-    var langs='<?php echo implode(';',$anslangs); ?>';
-    var languagecount=<?php echo count($anslangs); ?>;
-    var lasaveurl = '<?php echo Yii::app()->createUrl('/admin/labels/sa/ajaxSets'); ?>';
-    var lasuccess = '<?php eT('The records have been saved successfully!'); ?>';
-    var lsbrowsertitle='<?php eT('Label set browser','js'); ?>';
-    var lsdetailurl = '<?php echo Yii::app()->createUrl('/admin/questions/sa/ajaxlabelsetdetails'); ?>';
-    var lspickurl = '<?php echo Yii::app()->createUrl('/admin/questions/sa/ajaxlabelsetpicker'); ?>';
-    var newansweroption_text='<?php eT('New answer option','js'); ?>';
-    var ok = '<?php eT('Ok'); ?>';
-    var quickaddtitle='<?php eT('Quick-add answers','js'); ?>';
-    var saveaslabletitle  = '<?php eT('Save as label set','js'); ?>';
-    var sCheckLabelURL = '<?php echo Yii::app()->createUrl('/admin/questions/sa/ajaxchecklabel'); ?>';
-    var sImageURL ='<?php echo Yii::app()->getConfig('adminimageurl'); ?>';
-    var sLabelSetName='<?php eT('Label set name','js'); ?>';
-    var strCantDeleteLastAnswer='<?php eT('You cannot delete the last answer option.','js'); ?>';
-    var strcode='<?php eT('Code','js'); ?>';
-    var strlabel='<?php eT('Label','js'); ?>';
-    var strNoLabelSet='<?php eT('There are no label sets which match the survey default language','js'); ?>';
+<script type='text/javascript'>
+    var cancel                  = '<?php eT('Cancel'); ?>';
+    var check                   = true;
+    var lafail                  = '<?php eT('Sorry, the request failed!'); ?>';
+    var lanameurl               = '<?php echo Yii::app()->createUrl('/admin/labels/sa/getAllSets'); ?>';
+    var langs                   = '<?php echo implode(';',$anslangs); ?>';
+    var languagecount           = <?php echo count($anslangs); ?>;
+    var lasaveurl               = '<?php echo Yii::app()->createUrl('/admin/labels/sa/ajaxSets'); ?>';
+    var lasuccess               = '<?php eT('The records have been saved successfully!'); ?>';
+    var lsbrowsertitle          = '<?php eT('Label set browser','js'); ?>';
+    var lsdetailurl             = '<?php echo Yii::app()->createUrl('/admin/questions/sa/ajaxlabelsetdetails'); ?>';
+    var lspickurl               = '<?php echo Yii::app()->createUrl('/admin/questions/sa/ajaxlabelsetpicker'); ?>';
+    var newansweroption_text    = '<?php eT('New answer option','js'); ?>';
+    var ok                      = '<?php eT('Ok'); ?>';
+    var quickaddtitle           = '<?php eT('Quick-add answers','js'); ?>';
+    var saveaslabletitle        = '<?php eT('Save as label set','js'); ?>';
+    var sCheckLabelURL          = '<?php echo Yii::app()->createUrl('/admin/questions/sa/ajaxchecklabel'); ?>';
+    var sImageURL               = '<?php echo Yii::app()->getConfig('adminimageurl'); ?>';
+    var sLabelSetName           = '<?php eT('Label set name','js'); ?>';
+    var strCantDeleteLastAnswer = '<?php eT('You cannot delete the last answer option.','js'); ?>';
+    var strcode                 = '<?php eT('Code','js'); ?>';
+    var strlabel                = '<?php eT('Label','js'); ?>';
+    var strNoLabelSet           = '<?php eT('There are no label sets which match the survey default language','js'); ?>';
 
-    var duplicateanswercode='<?php eT('Error: You are trying to use duplicate answer codes.','js'); ?>';
-    var sAssessmentValue='<?php eT('Assessment value','js'); ?>';
-    var scalecount=<?php echo $scalecount; ?>;
+    var assessmentvisible       = <?php echo $assessmentvisible?'true':'false'; ?>;
+    var duplicateanswercode     = '<?php eT('Error: You are trying to use duplicate answer codes.','js'); ?>';
+    var sAssessmentValue        = '<?php eT('Assessment value','js'); ?>';
+    var scalecount              = <?php echo $scalecount; ?>;
 </script>
 
 <div class="side-body" id="edit-question-body">
--- application/views/admin/survey/Question/subQuestion_view.php
+++ application/views/admin/survey/Question/subQuestion_view.php
@@ -1,29 +1,29 @@
 <script type='text/javascript'>
-    var cancel = '<?php eT('Cancel'); ?>';
-    var check = true;
-    var lafail = '<?php eT('Sorry, the request failed!'); ?>';
-    var lanameurl = '<?php echo Yii::app()->createUrl('/admin/labels/sa/getAllSets'); ?>';
-    var langs='<?php echo implode(';',$anslangs); ?>';
-    var languagecount=<?php echo count($anslangs); ?>;
-    var lasaveurl = '<?php echo Yii::app()->createUrl('/admin/labels/sa/ajaxSets'); ?>';
-    var lasuccess = '<?php eT('The records have been saved successfully!'); ?>';
-    var lsbrowsertitle='<?php eT('Label set browser','js'); ?>';
-    var lsdetailurl = '<?php echo Yii::app()->createUrl('/admin/questions/sa/ajaxlabelsetdetails'); ?>';
-    var lspickurl = '<?php echo Yii::app()->createUrl('/admin/questions/sa/ajaxlabelsetpicker'); ?>';
-    var newansweroption_text='<?php eT('New subquestion','js'); ?>';
-    var ok = '<?php eT('Ok'); ?>';
-    var quickaddtitle='<?php eT('Quick-add subquestions','js'); ?>';
-    var saveaslabletitle  = '<?php eT('Save as label set','js'); ?>';
-    var sCheckLabelURL = '<?php echo Yii::app()->createUrl('/admin/questions/sa/ajaxchecklabel'); ?>';
-    var sImageURL ='<?php echo Yii::app()->getConfig('adminimageurl'); ?>';
-    var sLabelSetName='<?php eT('Label set name','js'); ?>';
-    var strCantDeleteLastAnswer='<?php eT('You cannot delete the last subquestion.','js'); ?>';
-    var strcode='<?php eT('Code','js'); ?>';
-    var strlabel='<?php eT('Label','js'); ?>';
-    var strNoLabelSet='<?php eT('There are no label sets which match the survey default language','js'); ?>';
+    var cancel                  = '<?php eT('Cancel'); ?>';
+    var check                   = true;
+    var lafail                  = '<?php eT('Sorry, the request failed!'); ?>';
+    var lanameurl               = '<?php echo Yii::app()->createUrl('/admin/labels/sa/getAllSets'); ?>';
+    var langs                   = '<?php echo implode(';',$anslangs); ?>';
+    var languagecount           = <?php echo count($anslangs); ?>;
+    var lasaveurl               = '<?php echo Yii::app()->createUrl('/admin/labels/sa/ajaxSets'); ?>';
+    var lasuccess               = '<?php eT('The records have been saved successfully!'); ?>';
+    var lsbrowsertitle          = '<?php eT('Label set browser','js'); ?>';
+    var lsdetailurl             = '<?php echo Yii::app()->createUrl('/admin/questions/sa/ajaxlabelsetdetails'); ?>';
+    var lspickurl               = '<?php echo Yii::app()->createUrl('/admin/questions/sa/ajaxlabelsetpicker'); ?>';
+    var newansweroption_text    = '<?php eT('New subquestion','js'); ?>';
+    var ok                      = '<?php eT('Ok'); ?>';
+    var quickaddtitle           = '<?php eT('Quick-add subquestions','js'); ?>';
+    var saveaslabletitle        = '<?php eT('Save as label set','js'); ?>';
+    var sCheckLabelURL          = '<?php echo Yii::app()->createUrl('/admin/questions/sa/ajaxchecklabel'); ?>';
+    var sImageURL               = '<?php echo Yii::app()->getConfig('adminimageurl'); ?>';
+    var sLabelSetName           = '<?php eT('Label set name','js'); ?>';
+    var strCantDeleteLastAnswer = '<?php eT('You cannot delete the last subquestion.','js'); ?>';
+    var strcode                 = '<?php eT('Code','js'); ?>';
+    var strlabel                = '<?php eT('Label','js'); ?>';
+    var strNoLabelSet           = '<?php eT('There are no label sets which match the survey default language','js'); ?>';
 
-    var duplicatesubquestioncode='<?php eT('Error: You are trying to use duplicate subquestion codes.','js'); ?>';
-    var otherisreserved='<?php ($aParentQuestion['other']=="Y") ? printf(gT("'%s' can not be used if question allow other.",'js'),'other') : ""; ?>';    
+    var duplicatesubquestioncode    ='<?php eT('Error: You are trying to use duplicate subquestion codes.','js'); ?>';
+    var otherisreserved             ='<?php ($aParentQuestion['other']=="Y") ? printf(gT("'%s' can not be used if question allow other.",'js'),'other') : ""; ?>';
 </script>
 <?php echo PrepareEditorScript(); ?>
 
