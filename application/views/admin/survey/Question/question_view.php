b7ef28813d92b1fce4d4ac9dd4ccd9e7e900238e
LouisGac <louis.gac@limesurvey.org>
2016-01-29 14:13:26 +0100

    Dev: fixed quick button add question to this group

--- application/views/admin/survey/Question/question_view.php
+++ application/views/admin/survey/Question/question_view.php
@@ -189,20 +189,20 @@
             </table>
 
             <!-- Quick Actions -->
-            <h3 id="survey-action-title"><?php eT('Survey quick actions'); ?></h3>
+            <h3 id="survey-action-title"><?php eT('Question quick actions'); ?></h3>
             <div class="row welcome survey-action">
                 <div class="col-lg-12 content-right">
 
                     <!-- create new question in this group -->
                     <div class="col-lg-3">
-                        <div class="panel panel-primary <?php if ($surveyIsActive) { echo 'disabled'; } else { echo 'panel-clickable'; } ?>" id="pannel-1" aria-data-url="<?php echo $this->createUrl('admin/questions/sa/newquestion/surveyid/'.$surveyid.'/gid/'.$gid); ?>">
+                        <div class="panel panel-primary <?php if ($surveyIsActive) { echo 'disabled'; } else { echo 'panel-clickable'; } ?>" id="pannel-1" data-url="<?php echo $this->createUrl('admin/questions/sa/newquestion/surveyid/'.$surveyid.'/gid/'.$gid); ?>">
                             <div class="panel-heading">
                                 <h4 class="panel-title"><?php eT("Add new question to group");?></h4>
                             </div>
                             <div class="panel-body">
                                 <span class="icon-add text-success"  style="font-size: 3em;"></span>
                                 <p class='btn-link'>
-                                    <?php eT("Add new question to group");?>
+                                        <?php eT("Add new question to group");?>
                                 </p>
                             </div>
                         </div>
