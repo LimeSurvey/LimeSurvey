09e89b92481f244a2a56b04d16c5e398818f39ae
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2016-02-04 15:56:57 +0100

    Dev: Add missing re-direct after attribute mapping

--- scripts/admin/attributeMapToken.js
+++ scripts/admin/attributeMapToken.js
@@ -157,8 +157,7 @@ $(document).ready(function(){
         }, function(msg){
             alert(msg);
 
-            // TODO: Enable after debugging done
-            //$(location).attr('href',redUrl);
+            $(location).attr('href',redUrl);
         });
     });
 
