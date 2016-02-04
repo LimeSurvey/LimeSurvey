e2f70bb948cf8dda5a98e79bdfd366beadca03a4
Matthew Favetti <zerwalter@gmail.com>
2016-02-04 11:23:55 -0800

    Fixed Issue #10302: Unable to rank by date in survey listing

--- application/views/admin/survey/listSurveys_view.php
+++ application/views/admin/survey/listSurveys_view.php
@@ -79,7 +79,7 @@
 
                         array(
                             'header' => gT('Created'),
-                            'name' => 'created',
+                            'name' => 'creation_date',
                             'value'=>'$data->creationdate',
                             'htmlOptions' => array('class' => 'col-md-1 hidden-xs'),
                         ),
