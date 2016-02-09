8d434b7b1a2102f086f4e36e6fb11795b5941b23
LouisGac <louis.gac@limesurvey.org>
2016-02-09 10:48:00 +0100

    Dev: removed img field from box model

--- application/models/Boxes.php
+++ application/models/Boxes.php
@@ -8,102 +8,101 @@
  * @property integer $position
  * @property string $url
  * @property string $title
- * @property string $img
  * @property string $desc
  * @property string $page
  */
 class Boxes extends CActiveRecord
 {
-	/**
-	 * @return string the associated database table name
-	 */
-	public function tableName()
-	{
-		return '{{boxes}}';
-	}
+    /**
+     * @return string the associated database table name
+     */
+    public function tableName()
+    {
+        return '{{boxes}}';
+    }
 
-	/**
-	 * @return array validation rules for model attributes.
-	 */
-	public function rules()
-	{
-		// NOTE: you should only define rules for those attributes that
-		// will receive user inputs.
-		return array(
-			array('url, title, img, desc, page', 'required'),
-			array('position', 'numerical', 'integerOnly'=>true),
-			// The following rule is used by search().
-			// @todo Please remove those attributes that should not be searched.
-			array('id, position, url, title, img, desc, page', 'safe', 'on'=>'search'),
-		);
-	}
+    /**
+     * @return array validation rules for model attributes.
+     */
+    public function rules()
+    {
+        // NOTE: you should only define rules for those attributes that
+        // will receive user inputs.
+        return array(
+            array('url, title, ico, desc, page', 'required'),
+            array('position', 'numerical', 'integerOnly'=>true),
+            // The following rule is used by search().
+            // @todo Please remove those attributes that should not be searched.
+            array('id, position, url, title, ico, desc, page', 'safe', 'on'=>'search'),
+        );
+    }
 
-	/**
-	 * @return array relational rules.
-	 */
-	public function relations()
-	{
-		// NOTE: you may need to adjust the relation name and the related
-		// class name for the relations automatically generated below.
-		return array(
-		);
-	}
+    /**
+     * @return array relational rules.
+     */
+    public function relations()
+    {
+        // NOTE: you may need to adjust the relation name and the related
+        // class name for the relations automatically generated below.
+        return array(
+        );
+    }
 
-	/**
-	 * @return array customized attribute labels (name=>label)
-	 */
-	public function attributeLabels()
-	{
-		return array(
-			'id' => 'ID',
-			'position' => 'position of the box',
-			'url' => 'url that the box points',
-			'title' => 'title of the box',
-			'img' => 'name of the image of the box',
-			'desc' => 'description of the box',
-			'page' => 'name of the page where the box should be shown ',
-		);
-	}
+    /**
+     * @return array customized attribute labels (name=>label)
+     */
+    public function attributeLabels()
+    {
+        return array(
+            'id' => 'ID',
+            'position' => 'position of the box',
+            'url' => 'url that the box points',
+            'title' => 'title of the box',
+            'ico' => 'icon to use in the box (you can use font-awesome, bootstrap glyphicons, or limesurvey icons)',
+            'desc' => 'description of the box',
+            'page' => 'name of the page where the box should be shown ',            
+        );
+    }
 
-	/**
-	 * Retrieves a list of models based on the current search/filter conditions.
-	 *
-	 * Typical usecase:
-	 * - Initialize the model fields with values from filter form.
-	 * - Execute this method to get CActiveDataProvider instance which will filter
-	 * models according to data in model fields.
-	 * - Pass data provider to CGridView, CListView or any similar widget.
-	 *
-	 * @return CActiveDataProvider the data provider that can return the models
-	 * based on the search/filter conditions.
-	 */
-	public function search()
-	{
-		// @todo Please modify the following code to remove attributes that should not be searched.
+    /**
+     * Retrieves a list of models based on the current search/filter conditions.
+     *
+     * Typical usecase:
+     * - Initialize the model fields with values from filter form.
+     * - Execute this method to get CActiveDataProvider instance which will filter
+     * models according to data in model fields.
+     * - Pass data provider to CGridView, CListView or any similar widget.
+     *
+     * @return CActiveDataProvider the data provider that can return the models
+     * based on the search/filter conditions.
+     */
+    public function search()
+    {
+        // @todo Please modify the following code to remove attributes that should not be searched.
 
-		$criteria=new CDbCriteria;
+        $criteria=new CDbCriteria;
 
-		$criteria->compare('id',$this->id);
-		$criteria->compare('position',$this->position);
-		$criteria->compare('url',$this->url,true);
-		$criteria->compare('title',$this->title,true);
-		$criteria->compare('img',$this->img,true);
-		$criteria->compare('desc',$this->desc,true);
-		$criteria->compare('page',$this->page,true);
+        $criteria->compare('id',$this->id);
+        $criteria->compare('position',$this->position);
+        $criteria->compare('url',$this->url,true);
+        $criteria->compare('title',$this->title,true);
+        $criteria->compare('ico',$this->ico,true);
+        $criteria->compare('desc',$this->desc,true);
+        $criteria->compare('page',$this->page,true);
 
-		return new CActiveDataProvider($this, array(
-			'criteria'=>$criteria,
-		));
-	}
+        return new CActiveDataProvider($this, array(
+            'criteria'=>$criteria,
+        ));
+    }
 
-	/**
-	 * Returns the static model of the specified AR class.
-	 * Please note that you should have this exact method in all your CActiveRecord descendants!
-	 * @param string $className active record class name.
-	 * @return Boxes the static model class
-	 */
-	public static function model($className=__CLASS__)
-	{
-		return parent::model($className);
-	}
+    /**
+     * Returns the static model of the specified AR class.
+     * Please note that you should have this exact method in all your CActiveRecord descendants!
+     * @param string $className active record class name.
+     * @return Boxes the static model class
+     */
+    public static function model($className=__CLASS__)
+    {
+        return parent::model($className);
+    }
 }
