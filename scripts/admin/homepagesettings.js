0eb8b5b49f3a4892e0b72c0a7c69d085063bf38b
LouisGac <louis.gac@limesurvey.org>
2016-02-10 13:01:52 +0100

    Dev: prepared actions for home page settings

---
+++ scripts/admin/homepagesettings.js
@@ -0,0 +1,8 @@
+/**
+ * JavaScript functions for HomePage Settings
+ */
+$(document).ready(function(){
+    $('#show_logo').on('switchChange.bootstrapSwitch', function(event, state) {});
+    $('#show_last_survey_and_question').on('switchChange.bootstrapSwitch', function(event, state) {});
+    $('#save_boxes_setting').on('click', function(){});
+});
