4933aaa3677299be476a362912879caa3bfc1828
LouisGac <louis.gac@limesurvey.org>
2016-02-10 17:02:37 +0100

    Dev: create new box

--- application/views/admin/homepagesettings/create.php
+++ application/views/admin/homepagesettings/create.php
@@ -180,8 +180,9 @@ $icons_length = count($icons);
                     </div>
                 </div>
 
+                <!-- Page -->
                 <div class="form-group">
-                    <?php echo $form->hiddenField($model,'page',array()); ?>
+                    <input name="Boxes[page]" id="Boxes_page" type="hidden" value="welcome">
                 </div>
 
                 <div class="form-group">
@@ -190,10 +191,15 @@ $icons_length = count($icons);
                         <?php echo $form->dropDownList(
                             $model,
                             'usergroup',
-                            CHtml::listData(UserGroup::model()->findAll(), 'ugid', 'name'),
+                            array_merge(
+                                array(
+                                    '-1' => gT('Everybody'),
+                                    '-2' => gT('Nobody'),
+                                ),
+                                CHtml::listData(UserGroup::model()->findAll(), 'ugid', 'name')
+                            ),
                             array(
                                 'class' => 'form-control',
-                                'prompt' => eT('All groups'),
                                 'options' => array(
                                     $model['usergroup'] => array('selected' => true)
                                 )
