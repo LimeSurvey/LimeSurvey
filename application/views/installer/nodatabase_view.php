e1dab3baf695beba36c18968db2729370b4e3398
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2016-02-16 17:26:36 +0100

    Dev: Alert styling for database warning (installer)

--- application/views/installer/nodatabase_view.php
+++ application/views/installer/nodatabase_view.php
@@ -1,4 +1,6 @@
-<strong><?php eT("Database doesn't exist!"); ?></strong><br /><br />
+<div class='alert alert-warning'>
+    <strong><?php eT("Database doesn't exist!"); ?></strong>
+</div>
 <?php eT("The database you specified does not exist:"); ?>
 <br /><br />
 <strong><?php echo $dbname; ?></strong><br /><br />
