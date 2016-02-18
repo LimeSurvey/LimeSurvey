36efdba64157f8478ee90c3652d7c8bd5fd5c5c2
Carsten Schmitz <carsten.schmitz@limesurvey.org>
2016-02-18 13:06:02 +0100

    Fixed issue #10490: missing translation Fixed issue: Error when returning from action button configuration

--- application/views/admin/survey/Question/listquestions.php
+++ application/views/admin/survey/Question/listquestions.php
@@ -62,7 +62,6 @@
                         // Number of row per page selection
                         'id' => 'question-grid',
                         'type'=>'striped',
-
                         'emptyText'=>gT('No questions found.'),
                         'summaryText'=>gT('Displaying {start}-{end} of {count} result(s).') .' '.sprintf(gT('%s rows per page'),
                             CHtml::dropDownList(
