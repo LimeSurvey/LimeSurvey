31edf1836381d72fadc31710c0d071c67514308a
Carsten Schmitz <carsten.schmitz@limesurvey.org>
2016-02-18 12:20:22 +0100

    Fixed issue #10511: Missing translation

--- application/views/admin/survey/QuestionGroups/listquestiongroups.php
+++ application/views/admin/survey/QuestionGroups/listquestiongroups.php
@@ -25,7 +25,7 @@
                             ),
                         )); ?>
                         <div class="form-group">
-                            <?php echo $form->label($model, 'name', array('class'=>'col-sm-2 control-label text-right col-sm-offset-6')); ?>
+                            <?php echo CHtml::label(gT('Search by group name:'), 'group_name', array('class'=>'col-sm-2 control-label text-right col-sm-offset-6')); ?>
                             <div class="col-sm-1 text-right">
                                 <?php echo $form->textField($model, 'group_name', array('class'=>'form-control')); ?>
                             </div>
@@ -77,7 +77,6 @@
 
                             // Group Name
                             array(
-                                'header'=>gT('Group name'),
                                 'name'=>'group_name',
                                 'value'=>'$data->group_name',
                                 'htmlOptions' => array('class' => 'col-md-2'),
