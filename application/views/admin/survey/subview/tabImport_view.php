c4836b2e3f233fb9054e4ee448aad5f138e1e589
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2016-02-19 11:29:12 +0100

    Dev: Adapt import survey form to bootstrap

--- application/views/admin/survey/subview/tabImport_view.php
+++ application/views/admin/survey/subview/tabImport_view.php
@@ -5,27 +5,33 @@
 ?>
 <!-- tab import survey -->
     <!-- import form -->
-    <?php echo CHtml::form(array('admin/survey/sa/copy'), 'post', array('id'=>'importsurvey', 'name'=>'importsurvey', 'class'=>'form30', 'enctype'=>'multipart/form-data', 'onsubmit'=>'return validatefilename(this,"'. gT('Please select a file to import!', 'js').'");')); ?>
-        <ul class="list-unstyled col-lg-12">
+    <?php echo CHtml::form(array('admin/survey/sa/copy'), 'post', array('id'=>'importsurvey', 'name'=>'importsurvey', 'class'=>'form-horizontal', 'enctype'=>'multipart/form-data', 'onsubmit'=>'return validatefilename(this,"'. gT('Please select a file to import!', 'js').'");')); ?>
+        <div class="row">
 
             <!-- Select file -->
-            <li>
-                <label for='the_file'><?php  eT("Select survey structure file (*.lss, *.csv, *.txt) or survey archive (*.lsa):");  ?> </label>
-                <input id='the_file' name="the_file" type="file" />
-            </li>
+            <div class='form-group '>
+                <label class='control-label col-sm-3' for='the_file'><?php  eT("Select survey structure file (*.lss, *.csv, *.txt) or survey archive (*.lsa):");  ?> </label>
+                <div class='col-sm-4'>
+                    <input id='the_file' name="the_file" type="file" />
+                </div>
+            </div>
 
             <!-- Convert resource links and INSERTANS fields? -->
-            <li>
-                <label for='translinksfields'><?php  eT("Convert resource links and INSERTANS fields?"); ?> </label>
-                <input id='translinksfields' name="translinksfields" type="checkbox" checked='checked'/>
-            </li>
+            <div class='form-group'>
+                <label class='control-label col-sm-3' for='translinksfields'><?php  eT("Convert resource links and INSERTANS fields?"); ?> </label>
+                <div class='col-sm-4'>
+                    <input id='translinksfields' name="translinksfields" type="checkbox" checked='checked'/>
+                </div>
+            </div>
 
             <!-- Submit -->
-            <li>
-                <input type='submit' class="btn btn-default" value='<?php  eT("Import survey"); ?>' />
-            </li>
+            <div class='form-group'>
+                <div class='col-sm-4 col-sm-offset-3'>
+                    <input type='submit' class="btn btn-default" value='<?php  eT("Import survey"); ?>' />
+                </div>
+            </div>
 
             <?php if (isset($surveyid)) echo '<input type="hidden" name="sid" value="'.$surveyid.'" />'; ?>
             <input type='hidden' name='action' value='importsurvey' />
-        </ul>
+        </div>
     </form>
