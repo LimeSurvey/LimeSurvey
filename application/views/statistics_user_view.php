1acb6d4c676a3bced80e8d4f6f502463593f8fa0
LouisGac <louis.gac@limesurvey.org>
2016-03-04 14:32:56 +0100

    Fixed issue #10087: User stats page have invalid link

--- application/views/statistics_user_view.php
+++ application/views/statistics_user_view.php
@@ -8,7 +8,4 @@
             </div>
             <?php if (isset($statisticsoutput) && $statisticsoutput) { echo $statisticsoutput; } ?><br />
         </div>
-<?php
-    $oTemplate = Template::model()->getInstance();
-    echo templatereplace(file_get_contents($oTemplate->viewPath."/endpage.pstpl"),array(), $redata);
-?>
+        <input type='hidden' class='hidemenubutton'/>
