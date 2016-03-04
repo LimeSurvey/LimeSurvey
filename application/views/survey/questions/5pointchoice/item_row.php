e8d10207bb5b5a84bd978af4f4d29b000cd80627
LouisGac <louis.gac@limesurvey.org>
2016-03-04 12:26:39 +0100

    Fixed issue #10530: HTML for radio button form items inconsistent

--- application/views/survey/questions/5pointchoice/item_row.php
+++ application/views/survey/questions/5pointchoice/item_row.php
@@ -10,16 +10,16 @@
 ?>
 <!-- 5 point choice item -->
 <div class="col-xs-2 answer-item radio-item">
+    <input
+        class="radio"
+        type="radio"
+        name="<?php echo $ia[1]; ?>"
+        id="answer<?php echo $ia[1].$fp; ?>"
+        value="<?php echo $fp;?>"
+        <?php echo $checkedState; ?>
+        onclick="<?php echo $checkconditionFunction; ?>(this.value, this.name, this.type)"
+    />
     <label for="answer<?php echo $ia[1].$fp; ?>" class="answertext radio-label">
-        <input
-            class="radio"
-            type="radio"
-            name="<?php echo $ia[1]; ?>"
-            id="answer<?php echo $ia[1].$fp; ?>"
-            value="<?php echo $fp;?>"
-            <?php echo $checkedState; ?>
-            onclick="<?php echo $checkconditionFunction; ?>(this.value, this.name, this.type)"
-        />
         <?php echo $fp; ?>
     </label>
 </div>
