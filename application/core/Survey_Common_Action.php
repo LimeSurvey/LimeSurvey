df9e73af5d4e91f5bbae200b01ec35e8ce73decb
LouisGac <louis.gac@limesurvey.org>
2016-03-09 17:03:40 +0100

    Fixed issue #10522: Redirect loop between "add" and "edit" assessment

--- application/core/Survey_Common_Action.php
+++ application/core/Survey_Common_Action.php
@@ -852,6 +852,7 @@ class Survey_Common_Action extends CAction
             {
                 $sAlternativeUrl = $aData['surveybar']['closebutton']['url'];
                 $aForbiddenWordsInUrl = isset($aData['surveybar']['closebutton']['forbidden'])?$aData['surveybar']['closebutton']['forbidden']:array();
+                $aForbiddenWordsInUrl[]='assessmentedit';
                 $aData['surveybar']['closebutton']['url'] = Yii::app()->request->getUrlReferrer( Yii::app()->createUrl($sAlternativeUrl), $aForbiddenWordsInUrl );
             }
 
