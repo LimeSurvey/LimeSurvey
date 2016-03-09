29b235e20d7cc15e5afc6d953967ec8c3df74a5d
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2016-03-09 12:27:18 +0100

    Dev: Type-cast int to string in postgresql query

--- application/helpers/admin/statistics_helper.php
+++ application/helpers/admin/statistics_helper.php
@@ -2552,8 +2552,8 @@ class statistics_helper {
                         //                $query = "SELECT count(*) FROM {{survey_$surveyid}} WHERE (".sanitize_int($rt)." IS NULL "
                         $query = "SELECT count(*) FROM {{survey_$surveyid}} WHERE ( "
                         //                                    . "OR ".sanitize_int($rt)." = '' "
-                        . " ".Yii::app()->db->quoteColumnName($rt)." = '' "
-                        . "OR ".Yii::app()->db->quoteColumnName($rt)." = ' ') ";
+                        . " ".Yii::app()->db->quoteColumnName($rt)."::text = '' "
+                        . "OR ".Yii::app()->db->quoteColumnName($rt)."::text = ' ') ";
                 }
 
             }
