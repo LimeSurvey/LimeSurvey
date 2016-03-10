88a7e83db9cc94aceccdbf4f55a1b9c7f7cb62dd
LouisGac <louis.gac@limesurvey.org>
2016-03-10 17:19:30 +0100

    Dev: scrutinizer fix

--- application/controllers/admin/dataentry.php
+++ application/controllers/admin/dataentry.php
@@ -2401,18 +2401,25 @@ class dataentry extends Survey_Common_Action
         if(!empty($qidattributes['array_filter']))
         {
 
-            $newquestiontext = Question::model()->findByAttributes(array('title' => $qidattributes['array_filter'], 'language' => $surveyprintlang, 'sid' => $surveyid))->getAttribute('question');
-            $output .= "\n<p class='extrahelp'>
-            ".sprintf(gT("Only answer this question for the items you selected in question %s ('%s')"),$qidattributes['array_filter'], flattenText(breakToNewline($newquestiontext)))."
-            </p>\n";
+            $newquestiontext = Question::model()->findByAttributes(array('title' => $qidattributes['array_filter'], 'language' => $surveyprintlang, 'sid' => $surveyid));
+            if(is_object($newquestiontext))
+            {
+                $newquestiontext->getAttribute('question');
+                $output .= "\n<p class='extrahelp'>
+                ".sprintf(gT("Only answer this question for the items you selected in question %s ('%s')"),$qidattributes['array_filter'], flattenText(breakToNewline($newquestiontext)))."
+                </p>\n";
+            }
         }
         if(!empty($qidattributes['array_filter_exclude']))
         {
-            $newquestiontext = Question::model()->findByAttributes(array('title' => $qidattributes['array_filter_exclude'], 'language' => $surveyprintlang, 'sid' => $surveyid))->getAttribute('question');
-
-            $output .= "\n    <p class='extrahelp'>
-            ".sprintf(gT("Only answer this question for the items you did not select in question %s ('%s')"),$qidattributes['array_filter_exclude'], breakToNewline($newquestiontext))."
-            </p>\n";
+            $newquestiontext = Question::model()->findByAttributes(array('title' => $qidattributes['array_filter_exclude'], 'language' => $surveyprintlang, 'sid' => $surveyid));
+            if(is_object($newquestiontext))
+            {
+                $newquestiontext->getAttribute('question');
+                $output .= "\n    <p class='extrahelp'>
+                ".sprintf(gT("Only answer this question for the items you did not select in question %s ('%s')"),$qidattributes['array_filter_exclude'], breakToNewline($newquestiontext))."
+                </p>\n";
+            }
         }
         return $output;
     }
