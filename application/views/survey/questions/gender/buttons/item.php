4bb886dc4495076d38d56b3dff002b2d1559aaeb
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2016-03-17 14:51:06 +0100

    Dev: Make gender buttons same width with btn-group-justified

--- application/views/survey/questions/gender/buttons/item.php
+++ application/views/survey/questions/gender/buttons/item.php
@@ -12,64 +12,67 @@
 ?>
 <div class="answers-list radio-list gender-button">
 
-    <button class="btn btn-danger btn-lg ls-icons" type="button" data-id="answer<?php echo $name;?>F"  >
-        <span class="fa fa-venus lead gender-icon"  ></span>
-        <span class="gender-text">
-            <?php eT('Female');?>
-        <span>
-    </button>
+    <div class='hidden'>  <!-- Hide this for now -->
+        <button class="btn btn-danger btn-lg ls-icons" type="button" data-id="answer<?php echo $name;?>F"  >
+            <span class="fa fa-venus lead gender-icon"  ></span>
+            <span class="gender-text">
+                <?php eT('Female');?>
+            <span>
+        </button>
 
-    <button class="btn btn-info btn-lg ls-icons" type="button"  data-id="answer<?php echo $name;?>M" >
-        <span class="fa fa-mars lead gender-icon" ></span>
-        <span class="gender-text">
-            <?php eT('Male');?>
-        </span>
-    </button>
+        <button class="btn btn-info btn-lg ls-icons" type="button"  data-id="answer<?php echo $name;?>M" >
+            <span class="fa fa-mars lead gender-icon" ></span>
+            <span class="gender-text">
+                <?php eT('Male');?>
+            </span>
+        </button>
 
-    <?php if($noAnswer):?>
-    <button class="btn btn-default btn-lg ls-icons" type="button"  data-id="answer<?php echo $name;?>" >
-        <span class="fa fa-genderless lead gender-icon"  ></span>
-        <span class="gender-text">
-            <?php eT('No answer'); ?>
-        </span>
-    </button>
-    <?php endif; ?>
+        <?php if($noAnswer):?>
+        <button class="btn btn-default btn-lg ls-icons" type="button"  data-id="answer<?php echo $name;?>" >
+            <span class="fa fa-genderless lead gender-icon"  ></span>
+            <span class="gender-text">
+                <?php eT('No answer'); ?>
+            </span>
+        </button>
+        <?php endif; ?>
+    </div>
 
-    <div class="btn-group hidden" data-toggle="buttons">
+    <div class='col-xs-12 col-sm-6'>  <!-- Full width on Phone; otherwise half width -->
+        <div class="btn-group btn-group-justified" data-toggle="buttons">
 
-      <!-- Female -->
-      <label class="btn btn-primary btn-lg <?php if($fChecked!=''){echo 'active';}?>">
-          <input
-            class="radio"
-            type="radio"
-            name="<?php echo $name;?>"
-            id="answer<?php echo $name;?>F"
-            value="F"
-            <?php echo $fChecked; ?>
-            onclick="<?php echo $checkconditionFunction; ?>"
-           />
-           <span class="fa fa-female"></span>
-          <?php eT('Female');?>
-      </label>
+          <!-- Female -->
+          <label class="btn btn-primary <?php if($fChecked!=''){echo 'active';}?>">
+              <input
+                class="radio"
+                type="radio"
+                name="<?php echo $name;?>"
+                id="answer<?php echo $name;?>F"
+                value="F"
+                <?php echo $fChecked; ?>
+                onclick="<?php echo $checkconditionFunction; ?>"
+               />
+               <span class="fa fa-venus"></span>
+              <?php eT('Female');?>
+          </label>
 
-      <!-- Male -->
-      <label class="btn btn-primary  btn-lg <?php if($mChecked!=''){echo 'active';}?> ">
-            <input
-              class="radio"
-              type="radio"
-              name="<?php echo $name;?>"
-              id="answer<?php echo $name;?>M"
-              value="M"
-              <?php echo $mChecked;?>
-              onclick="<?php echo $checkconditionFunction; ?>"
-            />
-            <span class="fa fa-male"></span>
-            <?php eT('Male');?>
-      </label>
+          <!-- Male -->
+          <label class="btn btn-primary  <?php if($mChecked!=''){echo 'active';}?> ">
+                <input
+                  class="radio"
+                  type="radio"
+                  name="<?php echo $name;?>"
+                  id="answer<?php echo $name;?>M"
+                  value="M"
+                  <?php echo $mChecked;?>
+                  onclick="<?php echo $checkconditionFunction; ?>"
+                />
+                <span class="fa fa-mars"></span>
+                <?php eT('Male');?>
+          </label>
 
-      <!-- No answer -->
-      <?php if($noAnswer):?>
-      <label class="btn btn-primary  btn-lg  <?php if($naChecked!=''){echo 'active';}?>">
+          <!-- No answer -->
+          <?php if($noAnswer):?>
+              <label class="btn btn-primary  <?php if($naChecked!=''){echo 'active';}?>">
                   <input
                       class="radio"
                       type="radio"
@@ -79,10 +82,12 @@
                       <?php echo $naChecked;?>
                       onclick="<?php echo $checkconditionFunction; ?>"
                   />
-                  <span class="glyphicon glyphicon-user"></span>
-                  <?php eT('No answer'); ?>
-      </label>
-    <?php endif;?>
+                  <span class="fa fa-genderless"></span>
+                  <span class='wrap-normal'><?php eT('No answer'); ?></span>
+              </label>
+            <?php endif;?>
+        </div>
+    </div>
 
     <!-- Value -->
     <input
@@ -91,7 +96,6 @@
         id="java<?php echo $name; ?>"
         value="<?php echo $value;?>"
     />
-    </div>
 </div>
 
 <script>
