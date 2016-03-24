c55c3d014a28714a47050d973a6ab709379e6dd6
LouisGac <louis.gac@limesurvey.org>
2016-03-24 11:57:55 +0100

    Dev: moved template.css to styles-public

--- application/controllers/survey/index.php
+++ application/controllers/survey/index.php
@@ -62,6 +62,7 @@ class index extends CAction {
         if($oTemplate->cssFramework == 'bootstrap')
         {
             App()->bootstrap->register();
+            App()->getClientScript()->registerCssFile( Yii::app()->getBaseUrl(true).'/styles-public/template.css' );
         }
 
         useFirebug();
