470889508bcaa7ea7151dff4459dc68d4bd38a17
LouisGac <louis.gac@limesurvey.org>
2016-04-01 15:47:04 +0200

    Dev: various fixes

---
+++ application/views/survey/questions/arrays/10point/rows/cells/answer_td_answertext.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @var $answerwidth
+ * @var $answertext2
+ */
+?>
+
+<!-- td_answertext -->
+<td class="answertextright" style='text-align:left; width: <?php echo $answerwidth; ?>%;'>
+    <?php echo $answertext2;?>
+</td>
+<!-- endof td_answertext -->
