22854eaac62b3420f09feaf6245ed9856faa9896
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2016-04-05 15:24:42 +0200

    Dev: Style add new answer to quota

--- application/views/admin/quotas/newanswer_view.php
+++ application/views/admin/quotas/newanswer_view.php
@@ -1,4 +1,4 @@
-<div class="side-body col-lg-8">
+<div class="side-body">
     <div class="row">                             
         <div class="col-lg-12 content-right">
             <h3>
@@ -7,28 +7,34 @@
 
 
             <div class="jumbotron message-box">
+                <div class='row'>
                     <h2><?php echo sprintf(gT("New answer for quota '%s'"), $quota_name);?></h2>
                     <p class="lead">
                         <?php eT("Select question");?>:
                     </p>
-                    <?php echo CHtml::form(array("admin/quotas/sa/new_answer/surveyid/{$iSurveyId}/subaction/new_answer_two"), 'post'); ?>                    
-                    <p>
-                        <select name="quota_qid" size="15">
-                            <?php foreach ($newanswer_result as $questionlisting) { ?>
-                                <option value="<?php echo $questionlisting['qid'];?>">
-                                    <?php echo $questionlisting['title'];?>: <?php echo strip_tags(substr($questionlisting['question'],0,40));?>
-                                </option>
-                                <?php } ?>
-                        </select>
+                    <?php echo CHtml::form(array("admin/quotas/sa/new_answer/surveyid/{$iSurveyId}/subaction/new_answer_two"), 'post', array('class' => 'form-horizontal')); ?>
+                        <div class='form-group'>
+                            <div class='col-sm-5 col-sm-offset-4'>
+                                <select class='form-control' name="quota_qid" size="15">
+                                    <?php foreach ($newanswer_result as $questionlisting) { ?>
+                                        <option value="<?php echo $questionlisting['qid'];?>">
+                                            <?php echo $questionlisting['title'];?>: <?php echo strip_tags(substr($questionlisting['question'],0,40));?>
+                                        </option>
+                                        <?php } ?>
+                                </select>
+                            </div>
+                        </div>
+                        <div class='form-group'>
+                            <div class='col-sm-5 col-sm-offset-4'>
+                                <input name="submit" type="submit" class="submit btn btn-default" value="<?php eT("Next");?>" />
+                            </div>
+                        </div>
                         <input type="hidden" name="sid" value="'.$iSurveyId.'" />
                         <input type="hidden" name="action" value="quotas" />
                         <input type="hidden" name="subaction" value="new_answer_two" />
                         <input type="hidden" name="quota_id" value="<?php echo sanitize_int($_POST['quota_id']);?>" />
-                    </p>
-                    <p>
-                        <input name="submit" type="submit" class="submit btn btn-default" value="<?php eT("Next");?>" />
-                    </p>
                     </form>
+                </div>
             </div>
         </div>
     </div>
--- application/views/admin/quotas/newanswertwo_view.php
+++ application/views/admin/quotas/newanswertwo_view.php
@@ -1,4 +1,4 @@
-<div class="side-body col-lg-8">
+<div class="side-body">
     <div class="row">
         <div class="col-lg-12 content-right">
             <h3>
@@ -14,32 +14,38 @@
                 </div>
             <?php else:?>
                 <div class="jumbotron message-box">
+                    <div class='row'>
                         <h2><?php echo sprintf(gT("New answer for quota '%s'"), $quota_name);?></h2>
                         <p class="lead"><?php eT("Select answer:");?></p>
-                        <?php echo CHtml::form(array("admin/quotas/sa/insertquotaanswer/surveyid/{$iSurveyId}"), 'post', array('#'=>'quota_'.sanitize_int($_POST['quota_id']))); ?>
-                            <p>
-                                <select name="quota_anscode" size="15">
-                                    <?php
-                                        while (list($key,$value) = each($question_answers))
-                                        {
-                                            if (!isset($value['rowexists'])) echo '<option value="'.$key.'">'.strip_tags(substr($value['Display'],0,40)).'</option>';
-                                        }
-                                    ?>
-                                </select>
-                            </p>
-                            <p>
-
-                                <input class="btn btn-lg btn-success" name="submit" type="submit" class="submit btn btn-default" value="<?php eT("Next");?>" />
+                        <?php echo CHtml::form(array("admin/quotas/sa/insertquotaanswer/surveyid/{$iSurveyId}"), 'post', array('#'=>'quota_'.sanitize_int($_POST['quota_id']), 'class' => 'form-horizontal')); ?>
+                            <div class='form-group'>
+                                <div class='col-sm-5 col-sm-offset-4'>
+                                    <select class='form-control' name="quota_anscode" size="15">
+                                        <?php
+                                            while (list($key,$value) = each($question_answers))
+                                            {
+                                                if (!isset($value['rowexists'])) echo '<option value="'.$key.'">'.strip_tags(substr($value['Display'],0,40)).'</option>';
+                                            }
+                                        ?>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class='form-group'>
+                                <div class='col-sm-5 col-sm-offset-4'>
+                                    <input class="btn btn btn-success" name="submit" type="submit" class="submit btn btn-default" value="<?php eT("Next");?>" />
+                                </div>
+                            </div>
+                            <div class='form-group'>
                                 <input type="hidden" name="sid" value="<?php echo $iSurveyId;?>" />
                                 <input type="hidden" name="action" value="quotas" />
                                 <input type="hidden" name="subaction" value="insertquotaanswer" />
                                 <input type="hidden" name="quota_qid" value="<?php echo sanitize_int($_POST['quota_qid']);?>" />
                                 <input type="hidden" name="quota_id" value="<?php echo sanitize_int($_POST['quota_id']);?>" />
-                                <br/>
                                 <?php eT("Save this, then create another:");?>
                                 <input type="checkbox" name="createanother">
-                            </p>
+                            </div>
                        </form>
+                   </div>
                 </div>
             <?php endif;?>
         </div>
