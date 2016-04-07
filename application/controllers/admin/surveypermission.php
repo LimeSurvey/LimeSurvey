f0efa6a6091cf59ffd298a50d5ff3fece4314d64
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2016-04-07 14:01:06 +0200

    Fixed issue #10883: Error 500 survey permission

--- application/controllers/admin/surveypermission.php
+++ application/controllers/admin/surveypermission.php
@@ -593,8 +593,10 @@ class surveypermission extends Survey_Common_Action {
         $action = App()->getRequest()->getParam('action');
 
         $imageurl = Yii::app()->getConfig('imageurl');
-        $postuserid = (!empty(App()->getRequest()->getParam('uid'))) ? App()->getRequest()->getParam('uid') : false;
-        $postusergroupid = (!empty(App()->getRequest()->getParam('gid'))) ? App()->getRequest()->getParam('uid') : false;// Not used
+        $uid = App()->getRequest()->getParam('uid');
+        $gid = App()->getRequest()->getParam('gid');
+        $postuserid = (!empty($uid)) ? $uid : false;
+        $postusergroupid = (!empty($gid)) ? $gid : false;// Not used
 
         if($postuserid && !in_array($postuserid,getUserList('onlyuidarray')))
         {
