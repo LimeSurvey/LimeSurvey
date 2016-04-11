85d9ba5d556b7253bc4a00d5ada0a93b43b589d9
LouisGac <louis.gac@limesurvey.org>
2016-04-11 10:28:38 +0200

    Dev: Browse resources, replaced form submission by link

--- application/views/admin/survey/subview/accordion/_resources_panel.php
+++ application/views/admin/survey/subview/accordion/_resources_panel.php
@@ -6,22 +6,23 @@
 
 <!-- ressources panel -->
 <div id='resources' class="tab-pane fade in">
-    <?php echo CHtml::form(Yii::app()->getConfig('publicurl') . "third_party/kcfinder/browse.php?language=".sTranslateLangCode2CK( App()->language), 'get', array('id'=>'browsesurveyresources', 'name'=>'browsesurveyresources', 'target'=>'_blank', 'class'=>'form30')); ?>
-        <ul class="list-unstyled">
-            <li>
-                <label>&nbsp;</label>
-                <?php echo CHtml::dropDownList('type', 'files', array('files' =>  gT('Files','unescaped'), 'flash' =>  gT('Flash','unescaped'), 'images' =>  gT('Images','unescaped'))); ?>
-                <input type='submit' class="btn btn-default" value="<?php  eT("Browse Uploaded Resources") ?>" />
-            </li>
-            <li>
-                <label>&nbsp;</label>
-                <input type='button'<?php echo $disabledIfNoResources; ?>
-                        class="btn btn-default"
-                       onclick='window.open("<?php echo $this->createUrl("admin/export/sa/resources/export/survey/surveyid/$surveyid"); ?>", "_blank")'
-                       value="<?php  eT("Export Resources As ZIP Archive") ?>"  />
-            </li>
-        </ul>
-    </form>
+    <ul class="list-unstyled">
+        <li>
+            <label>&nbsp;</label>
+            <?php echo CHtml::dropDownList('type', 'files', array('files' =>  gT('Files','unescaped'), 'flash' =>  gT('Flash','unescaped'), 'images' =>  gT('Images','unescaped'))); ?>                
+            <a href="<?php echo Yii::app()->getConfig('publicurl') ; ?>third_party/kcfinder/browse.php?language='<?php echo sTranslateLangCode2CK( App()->language); ?>'" type='btn btn-default btn-lg'  target='_blank'>
+                <?php  eT("Browse Uploaded Resources") ?>
+            </a>
+        </li>
+        <li>
+            <label>&nbsp;</label>
+            <input type='button'<?php echo $disabledIfNoResources; ?>
+                class="btn btn-default"
+                   onclick='window.open("<?php echo $this->createUrl("admin/export/sa/resources/export/survey/surveyid/$surveyid"); ?>", "_blank")'
+                   value="<?php  eT("Export Resources As ZIP Archive") ?>"  />
+        </li>
+    </ul>
+
 
     <?php echo CHtml::form(array('admin/survey/sa/importsurveyresources'), 'post', array('id'=>'importsurveyresources', 'name'=>'importsurveyresources', 'class'=>'form30', 'enctype'=>'multipart/form-data', 'onsubmit'=>'return validatefilename(this,"'. gT('Please select a file to import!', 'js').'");')); ?>
         <input type='hidden' name='surveyid' value='<?php echo $surveyid; ?>' />
