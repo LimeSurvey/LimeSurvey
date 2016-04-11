8a12d3beda789cf9b74d71df57e0be5c4bcb68c3
Carsten Schmitz <carsten.schmitz@limesurvey.org>
2016-04-11 15:30:04 +0200

    Removed translation: Kazakh

--- application/helpers/surveytranslator_helper.php
+++ application/helpers/surveytranslator_helper.php
@@ -369,13 +369,6 @@
         $supportedLanguages['ja']['dateformat'] = 6;
         $supportedLanguages['ja']['radixpoint'] = 0;
 
-        // Kazakh
-        $supportedLanguages['kk']['description'] = gT('Kazakh');
-        $supportedLanguages['kk']['nativedescription'] = 'Qazaq&#351;a';
-        $supportedLanguages['kk']['rtl'] = false;
-        $supportedLanguages['kk']['dateformat'] = 1;
-        $supportedLanguages['kk']['radixpoint'] = 1;
-
         // Kinyarwanda
         $supportedLanguages['rw']['description'] = gT('Kinyarwanda');
         $supportedLanguages['rw']['nativedescription'] = 'Kinyarwanda';
