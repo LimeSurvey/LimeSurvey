82bd20679821d39a7748f7ad005cf21020eaf10f
LouisGac <louis.gac@limesurvey.org>
2016-04-12 18:46:45 +0200

    Dev: MultipleChoice, A-B-C

--- application/views/survey/questions/multiplechoice/rows/answer_row.php
+++ application/views/survey/questions/multiplechoice/rows/answer_row.php
@@ -15,19 +15,8 @@
 
 <!-- answer_row -->
 <div class="col-sm-12">
-        <div id='javatbd<?php echo $myfname; ?>' class='question-item answer-item checkbox-item form-group <?php echo $extra_class; ?>' <?php echo $sDisplayStyle; ?> >
-
-            <label for="answer<?php echo $name.$title; ?>" class="answertext hidden">
-                <?php echo $question; ?>
-            </label>
+        <div id='javatbd<?php echo $myfname; ?>' class='question-item answer-item checkbox-item form-group <?php echo $extra_class; ?> checkbox' <?php echo $sDisplayStyle; ?> >
             <input
-                type="hidden"
-                name="java<?php echo $myfname; ?>"
-                id="java<?php echo $myfname; ?>"
-                value="<?php echo $sValue; ?>"
-            />
-
-                <input
                 class="checkbox"
                 type="checkbox"
                 name="<?php echo $name.$title; ?>"
@@ -35,10 +24,26 @@
                 value="Y"
                 <?php echo $checkedState; ?>
                 onclick='cancelBubbleThis(event); <?php echo $sCheckconditionFunction; ?>'
-                />
-                <label for="answer<?php echo $name.$title; ?>" class="answertext">
+                aria-labelledby="label-answer<?php echo $name.$title; ?>"
+            />
+
+            <label for="answer<?php echo $name.$title; ?>" class="answertext"></label>
+
+            <!--
+                 The label text is provided inside a div,
+                 so final user can add paragraph, div, or whatever he wants in the subquestion text
+                 This field is related to the input thanks to attribute aria-labelledby
+            -->
+            <div class="label-text label-clickable" id="label-answer<?php echo $name.$title; ?>">
                     <?php echo $question; ?>
-                </label>
+            </div>
+
+            <input
+                type="hidden"
+                name="java<?php echo $myfname; ?>"
+                id="java<?php echo $myfname; ?>"
+                value="<?php echo $sValue; ?>"
+            />
         </div>
 </div>
 <!-- end of answer_row -->
--- application/views/survey/questions/multiplechoice/rows/answer_row_other.php
+++ application/views/survey/questions/multiplechoice/rows/answer_row_other.php
@@ -19,66 +19,80 @@
 
 <!-- answer_row_other -->
 <div class="col-sm-12">
-        <div id='javatbd<?php echo $myfname; ?>' class='question-item answer-item checkbox-item form-group' <?php echo $sDisplayStyle; ?> >
-            <label for="<?php echo $myfname;?>cbox" class="answertext control-label other-label">
-                <?php echo $othertext; ?>
-                <input
-                class="form-control text <?php echo $kpclass; ?>"
-                type="text"
-                name="<?php echo $myfname; ?>"
-                id="answer<?php echo $myfname; ?>"
-                value="<?php echo $sValue; ?>"
-                />
-            </label>
-            <input
-                class="checkbox other-checkbox dontread"
-                type="checkbox"
-                name="<?php echo $myfname; ?>cbox"
-                id="answer<?php echo $myfname; ?>cbox"
-                <?php echo $checkedState; ?>
-                />
+    <div id='javatbd<?php echo $myfname; ?>' class='question-item answer-item checkbox-item form-group checkbox' <?php echo $sDisplayStyle; ?> >
 
-                <script type='text/javascript'>
-                    $('#answer<?php echo $myfname; ?>').bind('keyup focusout',function(event)
-                    {
-                        if ($.trim($(this).val()).length>0)
-                        {
-                            $("#answer<?php echo $myfname; ?>cbox").prop("checked",true);
-                        }
-                        else
-                        {
-                            $("#answer<?php echo $myfname; ?>cbox").prop("checked",false);
-                        }
-                        $("#java<?php echo $myfname; ?>").val($(this).val());
-                        LEMflagMandOther("<?php echo $myfname; ?>",$('#answer<?php echo $myfname; ?>cbox').is(":checked"));
-                        <?php echo $oth_checkconditionFunction; ?>(this.value, this.name, this.type);
-                    });
+        <!-- checkbox -->
+        <input
+            class="checkbox other-checkbox dontread"
+            type="checkbox"
+            name="<?php echo $myfname; ?>cbox"
+            id="answer<?php echo $myfname; ?>cbox"
+            <?php echo $checkedState; ?>
+            aria-labelledby="label-<?php echo $myfname;?>cbox"
+         />
 
-                    $('#answer<?php echo $myfname; ?>cbox').click(function(event)
-                    {
-                        if (($(this)).is(':checked') && $.trim($("#answer<?php echo $myfname; ?>").val()).length==0)
-                        {
-                            $("#answer<?php echo $myfname; ?>").focus();
-                            LEMflagMandOther("<?php echo $myfname; ?>",true);
-                            return false;
-                        }
-                        else
-                        {
-                            $("#answer<?php echo $myfname; ?>").val('');
-                            <?php echo $checkconditionFunction; ?>("", "<?php echo $myfname; ?>", "text");
-                            LEMflagMandOther("<?php echo $myfname; ?>",false);
-                            return true;
-                        };
-                    });
-                </script>
+         <!-- label -->
+         <label for="<?php echo $myfname;?>cbox" class="answertext control-label other-label"></label>
+         <!--
+              The label text is provided inside a div,
+              so final user can add paragraph, div, or whatever he wants in the subquestion text
+              This field is related to the input thanks to attribute aria-labelledby
+         -->
+         <div class="label-text label-clickable" id="label-<?php echo $myfname;?>cbox">
+                 <?php echo $othertext; ?>
+         </div>
 
-                <input
-                type="hidden"
-                name="java<?php echo $myfname; ?>"
-                id="java<?php echo $myfname; ?>"
-                value="<?php echo $sValueHidden; ?>"
-                />
+         <!-- comment -->
+         <input
+            class="form-control text <?php echo $kpclass; ?>"
+             type="text"
+             name="<?php echo $myfname; ?>"
+             id="answer<?php echo $myfname; ?>"
+             value="<?php echo $sValue; ?>"
+         />
+
+        <!-- hidden input -->
+        <input
+            type="hidden"
+            name="java<?php echo $myfname; ?>"
+            id="java<?php echo $myfname; ?>"
+            value="<?php echo $sValueHidden; ?>"
+        />
 
         </div> <!-- Form group ; item row -->
 </div>
+
+<script type='text/javascript'>
+    $('#answer<?php echo $myfname; ?>').bind('keyup focusout',function(event)
+    {
+        if ($.trim($(this).val()).length>0)
+        {
+            $("#answer<?php echo $myfname; ?>cbox").prop("checked",true);
+        }
+        else
+        {
+            $("#answer<?php echo $myfname; ?>cbox").prop("checked",false);
+        }
+        $("#java<?php echo $myfname; ?>").val($(this).val());
+        LEMflagMandOther("<?php echo $myfname; ?>",$('#answer<?php echo $myfname; ?>cbox').is(":checked"));
+        <?php echo $oth_checkconditionFunction; ?>(this.value, this.name, this.type);
+    });
+
+    $('#answer<?php echo $myfname; ?>cbox').click(function(event)
+    {
+        if (($(this)).is(':checked') && $.trim($("#answer<?php echo $myfname; ?>").val()).length==0)
+        {
+            $("#answer<?php echo $myfname; ?>").focus();
+            LEMflagMandOther("<?php echo $myfname; ?>",true);
+            return false;
+        }
+        else
+        {
+            $("#answer<?php echo $myfname; ?>").val('');
+            <?php echo $checkconditionFunction; ?>("", "<?php echo $myfname; ?>", "text");
+            LEMflagMandOther("<?php echo $myfname; ?>",false);
+            return true;
+        };
+    });
+</script>
 <!-- end of answer_row_other -->
