9b204736d7a78e7c9bda756ee46b74a9eea5ae39
LouisGac <louis.gac@limesurvey.org>
2016-04-12 18:37:47 +0200

    Dev: List with comment, A-B-C

--- application/views/survey/questions/list_with_comment/list/rows/answer_row.php
+++ application/views/survey/questions/list_with_comment/list/rows/answer_row.php
@@ -2,17 +2,17 @@
 /**
  * List with comment, list layout, Html
  *
- * @var $name                           $ia[1]
- * @var $id                             answer'.$ia[1].$ansrow['code'].'
- * @var $value                          $ansrow['code']
+ * @var $name
+ * @var $id
+ * @var $value
  * @var $check_ans
- * @var $checkconditionFunction         $checkconditionFunction.'(this.value, this.name, this.type)
- * @var $labeltext                      $ansrow['answer']
+ * @var $checkconditionFunction
+ * @var $labeltext
  * @var $li_classes
  */
 ?>
 <!-- answer_row -->
-<div class="answer-item radio-item <?php if(isset($li_classes)){echo $li_classes;}?>">
+<div class="answer-item radio-item <?php if(isset($li_classes)){echo $li_classes;}?> radio">
     <div class='form-group'>
         <input
             type="radio"
@@ -22,10 +22,18 @@
             class="radio"
             <?php echo $check_ans; ?>
             onclick="<?php echo $checkconditionFunction; ?>"
+            aria-labelledby="label-<?php echo $id; ?>"
         />
-        <label for="<?php echo $id; ?>" class="answertext radio-label control-label">
-            <?php echo $labeltext;?>
-        </label>
+        <label for="<?php echo $id; ?>" class="answertext radio-label control-label"></label>
+
+        <!--
+             The label text is provided inside a div,
+             so final user can add paragraph, div, or whatever he wants in the subquestion text
+             This field is related to the input thanks to attribute aria-labelledby
+        -->
+        <div class="label-text label-clickable" id="label-<?php echo $id; ?>">
+                <?php echo $labeltext;?>
+        </div>
     </div>
 </div>
 <!-- end of answer_row -->
