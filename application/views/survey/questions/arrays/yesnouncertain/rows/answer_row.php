fac3ce8eefb192bc7e63152b4f71c0f995b696e8
LouisGac <louis.gac@limesurvey.org>
2016-04-12 18:02:50 +0200

    Dev: Array Yes-No-Uncertain, A-B-C

--- application/views/survey/questions/arrays/yesnouncertain/rows/answer_row.php
+++ application/views/survey/questions/arrays/yesnouncertain/rows/answer_row.php
@@ -31,64 +31,91 @@
     </th>
 
     <!-- Yes -->
-    <td data-title='<?php eT("Yes"); ?>' class="answer_cell_Y answer-item radio-item">
-        <label for="answer<?php echo $myfname;?>-Y">
-            <input
-                class="radio"
-                type="radio"
-                name="<?php echo $myfname;?>"
-                id="answer<?php echo $myfname;?>-Y"
-                value="Y"
-                <?php echo $Ychecked;?>
-                onclick="<?php echo $checkconditionFunction;?>(this.value, this.name, this.type)"
-                />
-            </label>
+    <td data-title='<?php eT("Yes"); ?>' class="answer_cell_Y answer-item radio-item text-center radio">
+        <input
+            class="radio"
+            type="radio"
+            name="<?php echo $myfname;?>"
+            id="answer<?php echo $myfname;?>-Y"
+            value="Y"
+            <?php echo $Ychecked;?>
+            onclick="<?php echo $checkconditionFunction;?>(this.value, this.name, this.type)"
+            aria-labelledby="label-answer<?php echo $myfname;?>-Y"
+            />
+        <label for="answer<?php echo $myfname;?>-Y"></label>
+        <!--
+             The label text is provided inside a div,
+             To respect the global HTML flow of other question types
+        -->
+        <div class="hide label-text" id="label-answer<?php echo $myfname;?>-Y">
+            <?php eT("Yes"); ?>
+        </div>
     </td>
 
     <!-- Uncertain -->
-    <td data-title='<?php eT("Uncertain");?>' class="answer_cell_U answer-item radio-item">
-        <label for="answer<?php echo $myfname; ?>-U">
-            <input
-                class="radio"
-                type="radio"
-                name="<?php echo $myfname; ?>"
-                id="answer<?php echo $myfname;?>-U"
-                value="U"
-                <?php echo $Uchecked?>
-                onclick="<?php echo $checkconditionFunction;?>(this.value, this.name, this.type)"
-            />
-        </label>
+    <td data-title='<?php eT("Uncertain");?>' class="answer_cell_U answer-item radio-item text-center radio">
+        <input
+            class="radio"
+            type="radio"
+            name="<?php echo $myfname; ?>"
+            id="answer<?php echo $myfname;?>-U"
+            value="U"
+            <?php echo $Uchecked?>
+            onclick="<?php echo $checkconditionFunction;?>(this.value, this.name, this.type)"
+            aria-labelledby="label-answer<?php echo $myfname;?>-U"
+        />
+        <label for="answer<?php echo $myfname; ?>-U"></label>
+        <!--
+             The label text is provided inside a div,
+             To respect the global HTML flow of other question types
+        -->
+        <div class="hide label-text" id="label-answer<?php echo $myfname;?>-U">
+            <?php eT("Uncertain");?>
+        </div>
     </td>
 
     <!-- No -->
-    <td data-title='<?php eT("No"); ?>' class="answer_cell_N answer-item radio-item">
-        <label for="answer<?php echo $myfname;?>-N">
-            <input
-                class="radio"
-                type="radio"
-                name="<?php echo $myfname;?>"
-                id="answer<?php echo $myfname;?>-N"
-                value="N"
-                <?php echo $Nchecked?>
-                onclick="<?php echo $checkconditionFunction;?>(this.value, this.name, this.type)"
-            />
-        </label>
+    <td data-title='<?php eT("No"); ?>' class="answer_cell_N answer-item radio-item text-center radio">
+        <input
+            class="radio"
+            type="radio"
+            name="<?php echo $myfname;?>"
+            id="answer<?php echo $myfname;?>-N"
+            value="N"
+            <?php echo $Nchecked?>
+            onclick="<?php echo $checkconditionFunction;?>(this.value, this.name, this.type)"
+            aria-labelledby="label-answer<?php echo $myfname;?>-N"
+        />
+        <label for="answer<?php echo $myfname;?>-N"></label>
+        <!--
+             The label text is provided inside a div,
+             To respect the global HTML flow of other question types
+        -->
+        <div class="hide label-text" id="label-answer<?php echo $myfname;?>-N">
+            <?php eT("No"); ?>
+        </div>
     </td>
 
     <!-- No Answer -->
     <?php if($no_answer):?>
-        <td data-title='<?php eT("No answer");?>' class="answer-item radio-item noanswer-item">
-            <label for="answer<?php echo $myfname;?>-">
-                <input
-                    class="radio"
-                    type="radio"
-                    name="<?php echo $myfname;?>"
-                    id="answer<?php echo $myfname;?>-"
-                    value=""
-                    <?php echo $NAchecked?>
-                    onclick="<?php echo $checkconditionFunction;?>(this.value, this.name, this.type)"
-                />
-            </label>
+        <td data-title='<?php eT("No answer");?>' class="answer-item radio-item noanswer-item text-center radio">
+            <input
+                class="radio"
+                type="radio"
+                name="<?php echo $myfname;?>"
+                id="answer<?php echo $myfname;?>-"
+                value=""
+                <?php echo $NAchecked?>
+                onclick="<?php echo $checkconditionFunction;?>(this.value, this.name, this.type)"
+            />
+            <label for="answer<?php echo $myfname;?>-"></label>
+            <!--
+                 The label text is provided inside a div,
+                 To respect the global HTML flow of other question types
+            -->
+            <div class="hide label-text" id="label-answer<?php echo $myfname;?>-">
+                <?php eT("No answer");?>
+            </div>
         </td>
     <?php endif;?>
 </tr>
--- application/views/survey/questions/arrays/yesnouncertain/rows/cells/thead.php
+++ application/views/survey/questions/arrays/yesnouncertain/rows/cells/thead.php
@@ -5,10 +5,10 @@
 ?>
 
 <!-- thead -->
-<th class="dontread"><?php eT('Yes');?></th>
-<th class="dontread"><?php eT('Uncertain');?></th>
-<th class="dontread"><?php eT('No');?></th>
+<th class="dontread text-center"><?php eT('Yes');?></th>
+<th class="dontread text-center"><?php eT('Uncertain');?></th>
+<th class="dontread text-center"><?php eT('No');?></th>
 <?php if($no_answer):?>
-    <th class="dontread"><?php eT('No answer'); ?></th>
+    <th class="dontread text-center"><?php eT('No answer'); ?></th>
 <?php endif;?>
 <!-- end of  thead -->
