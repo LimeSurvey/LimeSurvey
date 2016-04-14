a733eaa1a7c1c9ac9777da8003dbb0a31972c652
LouisGac <louis.gac@limesurvey.org>
2016-04-14 12:03:04 +0200

    Dev: added asset version in DB to avoid any filesystem write

--- application/config/version.php
+++ application/config/version.php
@@ -16,7 +16,7 @@ $config['versionnumber'] = "2.50+";
 $config['dbversionnumber'] = 258;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
-$config['republish_assets'] = true;
+$config['assetsversionnumber'] = 2;
 //$config['updaterversion'] = 1;
 return $config;
 
