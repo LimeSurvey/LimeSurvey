959a777c57a84ded94aa606c06c89f2f1dd98c6b
LouisGac <louis.gac@limesurvey.org>
2016-04-14 17:58:17 +0200

    New feature: better display for Open Layer map

--- application/views/survey/questions/shortfreetext/location_mapservice/item_100.php
+++ application/views/survey/questions/shortfreetext/location_mapservice/item_100.php
@@ -19,80 +19,86 @@
 
 <div class="question answer-item geoloc-item <?php echo $extraclass; ?>">
 
-    <!-- No javascript need a way to answer -->
-    <input
-        type="hidden"
-        name="<?php echo $name; ?>"
-        id="answer<?php echo $name; ?>"
-        value="<?php echo $value; ?>"
-    >
-
-    <input
-        type="hidden"
-        class="location"
-        name="<?php echo $name; ?>_c"
-        id="answer<?php echo $name; ?>_c"
-        value="<?php echo $location_value; ?>"
-    />
-
-    <ul class="list-unstyled coordinates-list">
-        <li class="coordinate-item">
-            <?php eT("Latitude:"); ?>
-            <input
-                class="coords text"
-                type="text"
-                name="<?php echo $name; ?>_c1"
-                id="answer_lat<?php echo $name; ?>_c"
-                value="<?php echo $currentLat; ?>"
-            />
-        </li>
-
-        <li class="coordinate-item">
-            <?php eT("Longitude:"); ?>
-            <input
-                class="coords text"
-                type="text"
-                name="<?php echo $name; ?>_c2"
-                id="answer_lng<?php echo $name; ?>_c"
-                value="<?php echo $currentLong; ?>"
-            />
-         </li>
-    </ul>
-
-    <input
-        type="hidden"
-        name="boycott_<?php echo $name; ?>"
-        id="boycott_<?php echo $name; ?>"
-        value = "<?php echo $strBuild; ?>"
-    />
-
-    <input
-        type="hidden"
-        name="mapservice_<?php echo $name; ?>"
-        id="mapservice_<?php echo $name; ?>"
-        class="mapservice"
-        value="<?php echo $location_mapservice; ?>"
-    />
-
-    <div>
-        <div class="geoname_restrict">
-            <input
-                type="checkbox"
-                id="restrictToExtent_<?php echo $name; ?>"
-            />
-
-            <label for="restrictToExtent_<?php echo $name; ?>">
-                <?php eT("Restrict search place to map extent"); ?>
-            </label>
+    <div class="geoname_search col-xs-12" >
+        <div class="pull-left search-icon">
+            <span class="fa fa-search"></span>
         </div>
-
-        <div class="geoname_search" >
+        <div class="geoname_search col-xs-6" >
             <input
                 id="searchbox_<?php echo $name; ?>"
                 placeholder="<?php eT("Search"); ?>"
                 width="15"
-            />
+                class="form-control"
+             />
         </div>
+
+        <div class="pull-left checkbox">
+            <input
+                type="checkbox"
+                id="restrictToExtent_<?php echo $name; ?>"
+                />
+                <label for="restrictToExtent_<?php echo $name; ?>">
+                    <?php eT("Restrict search place to map extent"); ?>
+                </label>
+        </div>
+    </div>
+
+    <div class="col-xs-12 if-no-js">
+        <!-- No javascript need a way to answer -->
+
+        <input
+            type="hidden"
+            name="<?php echo $name; ?>"
+            id="answer<?php echo $name; ?>"
+            value="<?php echo $value; ?>"
+            >
+
+            <input
+            type="hidden"
+            class="location"
+            name="<?php echo $name; ?>_c"
+            id="answer<?php echo $name; ?>_c"
+            value="<?php echo $location_value; ?>"
+            />
+
+            <ul class="list-unstyled coordinates-list col-xs-12">
+                <li class="coordinate-item">
+                    <?php eT("Latitude:"); ?>
+                    <input
+                    class="coords text"
+                    type="text"
+                    name="<?php echo $name; ?>_c1"
+                    id="answer_lat<?php echo $name; ?>_c"
+                    value="<?php echo $currentLat; ?>"
+                    />
+                </li>
+
+                <li class="coordinate-item">
+                    <?php eT("Longitude:"); ?>
+                    <input
+                    class="coords text"
+                    type="text"
+                    name="<?php echo $name; ?>_c2"
+                    id="answer_lng<?php echo $name; ?>_c"
+                    value="<?php echo $currentLong; ?>"
+                    />
+                </li>
+            </ul>
+
+            <input
+            type="hidden"
+            name="boycott_<?php echo $name; ?>"
+            id="boycott_<?php echo $name; ?>"
+            value = "<?php echo $strBuild; ?>"
+            />
+
+            <input
+            type="hidden"
+            name="mapservice_<?php echo $name; ?>"
+            id="mapservice_<?php echo $name; ?>"
+            class="mapservice"
+            value="<?php echo $location_mapservice; ?>"
+            />
     </div>
 
     <!-- Map -->
