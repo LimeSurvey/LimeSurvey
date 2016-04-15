a022a6862409753ab78e1ea568489bd1239fad18
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2016-04-15 16:15:38 +0200

    Dev: Make it possible to delete question group with 0 questions

--- application/models/QuestionGroup.php
+++ application/models/QuestionGroup.php
@@ -240,7 +240,7 @@ class QuestionGroup extends LSActiveRecord
         $iQuestionsInGroup = Question::model()->countByAttributes(array('sid' => $this->sid, 'gid' => $this->gid, 'language' => $baselang));
 
         // Delete
-        if($oSurvey->active != "Y" && Permission::model()->hasSurveyPermission($this->sid,'surveycontent','delete' ) && $iQuestionsInGroup > 0)
+        if($oSurvey->active != "Y" && Permission::model()->hasSurveyPermission($this->sid,'surveycontent','delete' ) && $iQuestionsInGroup === 0)
         {
             $condarray = getGroupDepsForConditions($this->sid, "all", $this->gid, "by-targgid");
             if(is_null($condarray))
