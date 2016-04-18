a60f2269e1388ff5cfeabb3992444056a000ce0c
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2016-04-18 11:00:29 +0200

    Dev: Font fix for upload file button

--- application/views/survey/questions/file_upload/answer.php
+++ application/views/survey/questions/file_upload/answer.php
@@ -13,11 +13,11 @@
 <div class='upload-button'>
     <a
         id='upload_<?php echo $fileid;?>'
-        class='btn btn-default upload fa fa-upload'
+        class='btn btn-default upload'
         href='#'
         onclick='javascript:upload_<?php echo $fileid;?>();'
     >
-        <?php eT('Upload files'); ?>
+        <span class='fa fa-upload'></span>&nbsp;<?php eT('Upload files'); ?>
     </a>
 </div>
 <input type='hidden' id='<?php echo $fileid;?>' name='<?php echo $fileid;?>' value='<?php echo $value;?>' />
