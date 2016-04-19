63529635c86c1896426ae53bd473564cff377fe9
Denis Chenu <denis@sondages.pro>
2016-04-19 16:35:43 +0200

    Fixed issue #10846: Checked responses are not read when load "surveys file" Dev: controller wait id, then send id Dev: update some glyphicon to fontawesome

--- scripts/admin/listresponse.js
+++ scripts/admin/listresponse.js
@@ -148,7 +148,7 @@ $(function() {
         'navButtonAdd',
         '#pager',
         {
-            buttonicon : "ui-icon-calculator",
+            buttonicon : "fa fa-columns",
             caption : "",
             title : sSelectColumns,
             onClickButton : function() {
@@ -181,18 +181,19 @@ $(function() {
             //caption:sDownLoad, // Remove it ? no it's more clear ;)
             caption:'',
             title:sDownLoad, // Todo dynamically update download selected , download all
-            buttonicon:"glyphicon glyphicon-download-alt",
+            buttonicon:"fa fa-download",
             onClickButton: function(){
                 selectedlist=jQuery("#displayresponses").getGridParam('selarrrow').join(",");//  Or send like an array ?
                 if(selectedlist!="")
                 {
-                    sendPost(jsonActionUrl,null,["oper","responseid"],["downloadzip",selectedlist]);
+                    sendPost(jsonActionUrl,null,["oper","id"],["downloadzip",selectedlist]);
                 }
                 else
                 {
                     if(confirm(sConfirmationArchiveMessage))
-                        sendPost(jsonActionUrl,null,["oper"],["downloadzip"]);;
-                    //sendPost(sDownloadUrl,null,"responseid",0);
+                    {
+                        sendPost(jsonActionUrl,null,["oper"],["downloadzip"]);
+                    }
                 }
             },
             position:"last",
