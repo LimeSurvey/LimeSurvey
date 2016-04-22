14c88ad13766d83c18d03e153208fd3c33c14354
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2016-04-22 12:18:41 +0200

    Fixed issue #11010: PHP notice when create a new survey

--- application/controllers/admin/surveyadmin.php
+++ application/controllers/admin/surveyadmin.php
@@ -1511,7 +1511,7 @@ class SurveyAdmin extends Survey_Common_Action
     */
     private function _tabPublicationAccess($esrow)
     {
-        $dateformatdetails = getDateFormatData(Yii::app()->session['dateformat']);
+        $aData['dateformatdetails'] = getDateFormatData(Yii::app()->session['dateformat']);
         $startdate = '';
         if ($esrow['startdate'])
         {
