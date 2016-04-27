f1d0555f1e65482eb903e2b4ad2d0f3402d934a7
LouisGac <louis.gac@limesurvey.org>
2016-04-27 17:11:15 +0200

    Dev: flush OP cache after update

--- application/controllers/admin/update.php
+++ application/controllers/admin/update.php
@@ -293,7 +293,8 @@ class update extends Survey_Common_Action
                                 Yii::app()->session['security_update'] = null;
                                 $today = new DateTime("now");
                                 Yii::app()->session['next_update_check'] = $today->add(new DateInterval('PT6H'));
-
+                                Yii::app()->cache->flush();
+                                opcache_reset();
                                 // TODO : aData should contains information about each step
                                 return $this->controller->renderPartial('update/updater/steps/_final', array('destinationBuild'=>$destinationBuild), false, false);
                             }
