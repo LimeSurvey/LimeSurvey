1f5bb95e22f257af83ac492d0ce9c8e3e14d325a
Carsten Schmitz <carsten.schmitz@limesurvey.org>
2016-05-03 10:39:05 +0200

    Release 2.50+ Build 160503

--- application/config/version.php
+++ application/config/version.php
@@ -16,7 +16,7 @@ $config['versionnumber'] = "2.50+";
 $config['dbversionnumber'] = 258;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
-$config['assetsversionnumber'] = 5;
+$config['assetsversionnumber'] = 6;
 return $config;
 
 ?>
