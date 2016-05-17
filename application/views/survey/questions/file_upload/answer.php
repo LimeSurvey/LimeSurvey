4104f5f3ab0cbf473453c8ac3eea4548b74d16d9
kairavesloot <ravesloot@lime-support.com>
2016-05-17 13:50:03 +0200

    Fix upload button alignment
    Use bootstarp rows consequent
    This fix the left alignment of answers

--- application/views/survey/questions/file_upload/answer.php
+++ application/views/survey/questions/file_upload/answer.php
@@ -10,18 +10,28 @@
 <!-- File Upload  -->
 
 <!--answer -->
-<div class='upload-button'>
-    <a
-        id='upload_<?php echo $fileid;?>'
-        class='btn btn-default upload'
-        href='#'
-        onclick='javascript:upload_<?php echo $fileid;?>();'
-    >
-        <span class='fa fa-upload'></span>&nbsp;<?php eT('Upload files'); ?>
-    </a>
-</div>
-<input type='hidden' id='<?php echo $fileid;?>' name='<?php echo $fileid;?>' value='<?php echo $value;?>' />
-<input type='hidden' id='<?php echo $fileid;?>_filecount' name='<?php echo $fileid;?>_filecount' value="<?php echo $filecountvalue?>" />
-<div id='<?php echo $fileid;?>_uploadedfiles'>
+<div class='col-sm-12 upload-file-wrapper'>
+    <div class='row'>
+        <div class='col-sm-3 upload-button'>
+            <a
+                id='upload_<?php echo $fileid; ?>'
+                class='btn btn-default upload'
+                href='#'
+                onclick='javascript:upload_<?php echo $fileid; ?>();'
+            >
+                <span class='fa fa-upload'></span>&nbsp;<?php eT('Upload files'); ?>
+            </a>
+        </div>
+        <input type='hidden' id='<?php echo $fileid; ?>' name='<?php echo $fileid; ?>' value='<?php echo $value; ?>'/>
+        <input type='hidden' id='<?php echo $fileid; ?>_filecount' name='<?php echo $fileid; ?>_filecount'
+               value="<?php echo $filecountvalue ?>"/>
+    </div>
+    
+    <div class='row uploaded-files-list'>
+        <div class='col-sm-12'>
+            <div id='<?php echo $fileid; ?>_uploadedfiles'>
+            </div>
+        </div>
+    </div>
 </div>
 <!-- end of answer -->
