5432178fa9ac17823f741a4b00708137658defd3
LouisGac <louis.gac@limesurvey.org>
2016-05-18 11:37:20 +0200

    Dev: use radio buttons group for complet state in statistics>data selection

--- application/views/admin/export/statistics_subviews/_dataselection.php
+++ application/views/admin/export/statistics_subviews/_dataselection.php
@@ -1,55 +1,62 @@
-    <div class="panel panel-primary" id="pannel-1">
-        <div class="panel-heading">
-            <h4 class="panel-title"><?php eT("Data selection"); ?></h4>
+<div class="panel panel-primary" id="pannel-1">
+    <div class="panel-heading">
+        <h4 class="panel-title"><?php eT("Data selection"); ?></h4>
+    </div>
+    <div class="panel-body">
+
+
+        <div class='form-group'>
+            <label for='completionstate' class="col-sm-4 control-label"><?php eT("Include:"); ?> </label>
+            <div class="btn-group hidden-sm hidden-xs  pull-left" data-toggle="buttons">
+                <label class="btn btn-default active">
+                    <input name="completionstate" value="all" type="radio"  checked >
+                    <?php eT("All responses"); ?>
+                </label>
+                <label class="btn btn-default">
+                    <input name="completionstate" value="complete" type="radio"   >
+                    <?php eT("Completed responses only"); ?>
+                </label>
+                <label class="btn btn-default">
+                    <input name="completionstate" value="incomplete" class="active" type="radio" >
+                    <?php eT("Incomplete responses only"); ?>
+                </label>
+            </div>
         </div>
-        <div class="panel-body">
 
-            <div class="form-group">
-                <label for='completionstate' class="col-sm-4 control-label"><?php eT("Include:"); ?> </label>
-
-                <div class="col-sm-4">
-                    <select name='completionstate' id='completionstate' class='form-control'>
-                        <option value='all' <?php echo $selectshow; ?>><?php eT("All responses"); ?></option>
-                        <option value='complete' <?php echo $selecthide; ?> > <?php eT("Completed responses only"); ?></option>
-                        <option value='incomplete' <?php echo $selectinc; ?> > <?php eT("Incomplete responses only"); ?></option>
-                    </select>
-                </div>
+        <div class='form-group'>
+            <label class="col-sm-4 control-label" for='viewsummaryall'><?php eT("View summary of all available fields:"); ?></label>
+            <div class='col-sm-1'>
+                <?php $this->widget('yiiwheels.widgets.switch.WhSwitch', array('name' => 'viewsummaryall', 'id'=>'viewsummaryall', 'value'=>(isset($_POST['viewsummaryall'])), 'onLabel'=>gT('On'),'offLabel'=>gT('Off')));?>
             </div>
+        </div>
 
-            <div class='form-group'>
-                <label class="col-sm-4 control-label" for='viewsummaryall'><?php eT("View summary of all available fields:"); ?></label>
-                <div class='col-sm-1'>
-                    <?php $this->widget('yiiwheels.widgets.switch.WhSwitch', array('name' => 'viewsummaryall', 'id'=>'viewsummaryall', 'value'=>(isset($_POST['viewsummaryall'])), 'onLabel'=>gT('On'),'offLabel'=>gT('Off')));?>
-                </div>
+        <div class='form-group'>
+            <label class="col-sm-4 control-label" id='noncompletedlbl' for='noncompleted' title='<?php eT("Count stats for each question based only on the total number of responses for which the question was displayed"); ?>'><?php eT("Subtotals based on displayed questions:"); ?></label>
+            <div class='col-sm-1'>
+                <?php $this->widget('yiiwheels.widgets.switch.WhSwitch', array('name' => 'noncompleted', 'id'=>'noncompleted', 'value'=>(isset($_POST['noncompleted'])), 'onLabel'=>gT('On'),'offLabel'=>gT('Off')));?>
             </div>
+        </div>
 
-            <div class='form-group'>
-                <label class="col-sm-4 control-label" id='noncompletedlbl' for='noncompleted' title='<?php eT("Count stats for each question based only on the total number of responses for which the question was displayed"); ?>'><?php eT("Subtotals based on displayed questions:"); ?></label>
-                <div class='col-sm-1'>
-                    <?php $this->widget('yiiwheels.widgets.switch.WhSwitch', array('name' => 'noncompleted', 'id'=>'noncompleted', 'value'=>(isset($_POST['noncompleted'])), 'onLabel'=>gT('On'),'offLabel'=>gT('Off')));?>
-                </div>
-            </div>
-
-            <?php
-            $language_options="";
-            foreach ($survlangs as $survlang)
+        <?php
+        $language_options="";
+        foreach ($survlangs as $survlang)
+        {
+            $language_options .= "\t<option value=\"{$survlang}\"";
+            if ($sStatisticsLanguage == $survlang)
             {
-                $language_options .= "\t<option value=\"{$survlang}\"";
-                if ($sStatisticsLanguage == $survlang)
-                {
-                    $language_options .= " selected=\"selected\" " ;
-                }
-                $temp = getLanguageNameFromCode($survlang,true);
-                $language_options .= ">".$temp[1]."</option>\n";
+                $language_options .= " selected=\"selected\" " ;
             }
+            $temp = getLanguageNameFromCode($survlang,true);
+            $language_options .= ">".$temp[1]."</option>\n";
+        }
 
-            ?>
+        ?>
 
-            <div class='form-group'>
-                <label for='statlang' class="col-sm-4 control-label" ><?php eT("Statistics report language:"); ?></label>
-                <div class='col-sm-4'>
-                    <select name="statlang" id="statlang" class="form-control"><?php echo $language_options; ?></select>
-                </div>
+        <div class='form-group'>
+            <label for='statlang' class="col-sm-4 control-label" ><?php eT("Statistics report language:"); ?></label>
+            <div class='col-sm-4'>
+                <select name="statlang" id="statlang" class="form-control"><?php echo $language_options; ?></select>
             </div>
         </div>
     </div>
+</div>
