a82603a89766a97609d49da6d38cf0462994bcfd
LouisGac <louis.gac@limesurvey.org>
2016-05-23 18:31:58 +0200

    Dev: updated assets version number

--- application/config/version.php
+++ application/config/version.php
@@ -16,7 +16,7 @@ $config['versionnumber'] = "2.50+";
 $config['dbversionnumber'] = 258;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
-$config['assetsversionnumber'] = 11;
+$config['assetsversionnumber'] = 12;
 return $config;
 
 ?>
