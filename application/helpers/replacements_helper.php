6d55b2724f9a1d1a62f235d9b2168117adf5d5d9
Carsten Schmitz <carsten.schmitz@limesurvey.org>
2016-05-26 17:44:30 +0200

    Fixed issue #11276: Google Analytics code not running

--- application/helpers/replacements_helper.php
+++ application/helpers/replacements_helper.php
@@ -612,10 +612,9 @@ EOD;
                 $_trackURL = htmlspecialchars($thissurvey['name'] . '-[' . $surveyid . ']/[' . $gseq . ']-' . $_groupname);
                 $_googleAnalyticsJavaScript = <<<EOD
 <script>
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+(function(i,s,o,g,r,a,m){ i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ (i[r].q=i[r].q||[]).push(arguments) }
+,i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+ })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
 ga('create', '$_googleAnalyticsAPIKey', 'auto');
 ga('send', 'pageview');
