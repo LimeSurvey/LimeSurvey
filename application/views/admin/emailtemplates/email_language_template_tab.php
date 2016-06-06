efb33be0848737cee64938d1b1a3a059aeecf023
Carsten Schmitz <carsten.schmitz@limesurvey.org>
2016-06-06 17:50:01 +0200

    Fixed issue #11157: Error when trying to edit email templates and using PHP7

--- application/views/admin/emailtemplates/email_language_template_tab.php
+++ application/views/admin/emailtemplates/email_language_template_tab.php
@@ -9,7 +9,9 @@
     <div class='form-group'>
         <label class='col-sm-2 control-label' for='email_<?php echo $tab; ?>_subj_<?php echo $grouplang; ?>'><?php echo $details['subject'] ?></label>
         <div class='col-sm-6'>
-            <?php echo CHtml::textField("email_{$tab}_subj_{$grouplang}",$esrow->$details['field']['subject'],array('class' => 'form-control', 'size'=>80)); ?>
+            <?php
+            $sSubjectField=$details['field']['subject'];
+            echo CHtml::textField("email_{$tab}_subj_{$grouplang}",$esrow->$sSubjectField,array('class' => 'form-control', 'size'=>80)); ?>
         </div>
         <div class='col-sm-1'>
             <?php echo CHtml::button(gT("Reset"),array('class'=>'fillin btn btn-default','data-target'=>"email_{$tab}_subj_{$grouplang}",'data-value'=>$details['default']['subject'])); ?>
@@ -19,7 +21,9 @@
     <div class='form-group'>
         <label class='col-sm-2 control-label' for='email_<?php echo $tab; ?>_<?php echo $grouplang; ?>'><?php echo $details['body']; ?></label>
         <div class='col-sm-6'>
-            <?php echo CHtml::textArea("email_{$tab}_{$grouplang}",$esrow->$details['field']['body'],array('cols'=>80,'rows'=>20, 'class'=>'form-control')); ?>
+            <?php
+            $sBodyField=$details['field']['body'];
+            echo CHtml::textArea("email_{$tab}_{$grouplang}",$esrow->$sBodyField,array('cols'=>80,'rows'=>20, 'class'=>'form-control')); ?>
             <?php echo getEditor("email-$tab","email_{$tab}_$grouplang", $details['body'].'('.$grouplang.')',$surveyid,'','','editemailtemplates'); ?>
         </div>
         <div class='col-sm-6'></div>
