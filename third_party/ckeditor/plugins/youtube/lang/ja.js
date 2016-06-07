52099cb842a13c03ae2f699aa23a1a9fbe32849c
Carsten Schmitz <carsten.schmitz@limesurvey.org>
2016-06-07 22:07:14 +0200

    New feature: Youtube plugin for question editor

---
+++ third_party/ckeditor/plugins/youtube/lang/ja.js
@@ -0,0 +1,23 @@
+CKEDITOR.plugins.setLang('youtube', 'ja', {
+	button : 'Youtube動画埋め込み',
+	title : 'Youtube動画埋め込み',
+	txtEmbed : '埋め込みコードを貼り付けてください',
+	txtUrl : 'URLを貼り付けてください',
+	txtWidth : '幅',
+	txtHeight : '高さ',
+	chkRelated : '動画が終わったら関連動画を表示する',
+	txtStartAt : 'Start at',
+	chkPrivacy : 'プライバシー強化モードを有効にする',
+	chkOlderCode : '以前の埋め込みコードを使用する',
+	chkAutoplay : '自動再生',
+	noCode : '埋め込みコードまたはURLを入力してください',
+	invalidEmbed : '不適切な埋め込みコードが入力されました',
+	invalidUrl : '不適切なURLが入力されました',
+	or : 'または',
+	noWidth : '幅を指定してください',
+	invalidWidth : '幅指定に誤りがあります',
+	noHeight : '高さを指定してください',
+	invalidHeight : '高さ指定に誤りがあります',
+	invalidTime : 'Inform a valid start time',
+	txtResponsive : 'Responsive video'
+});
