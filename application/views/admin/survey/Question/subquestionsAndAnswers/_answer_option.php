56101a812b73cb449b7156f47113d16a73a46a6c
LouisGac <louis.gac@limesurvey.org>
2016-06-15 12:36:46 +0200

    Fixed issue #11371: Cannot save/update answer options with own "codes" in list-dropdowns/-radiobutton - questions Dev: change answer code input maxlength to 5

--- application/views/admin/survey/Question/subquestionsAndAnswers/_answer_option.php
+++ application/views/admin/survey/Question/subquestionsAndAnswers/_answer_option.php
@@ -51,7 +51,7 @@
                 class='code code-title'
                 name='code_<?php echo $position; ?>_<?php echo $scale_id; ?>'
                 value="<?php echo $title; ?>"
-                maxlength='20' size='20'
+                maxlength='5' 
                 pattern='<?php echo $sPattern; ?>'
                 required='required'
             />
