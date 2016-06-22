36c9290727c5df74debcbad61e7f9c033941e107
LouisGac <louis.gac@limesurvey.org>
2016-06-22 12:30:30 +0200

    Fixed issue #11305: saved survey responses empty when equation questions are enabled Dev: added max width to responses td

--- application/config/version.php
+++ application/config/version.php
@@ -16,7 +16,7 @@ $config['versionnumber'] = "2.50+";
 $config['dbversionnumber'] = 258;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
-$config['assetsversionnumber'] = '25';
+$config['assetsversionnumber'] = '26';
 return $config;
 
 ?>
