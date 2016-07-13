0f9c4672a4240c46156560dbe427dd99b3875c9b
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2016-07-13 11:51:42 +0200

    Dev: First commit for CintLink plugin

---
+++ application/core/plugins/CintLink/CintLink.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * CintLink integration to be able to buy respondents
+ * from within LimeSurvey.
+ *
+ * @since 2016-07-13
+ * @author Olle Härstedt
+ */
+class CintLink extends \ls\pluginmanager\PluginBase
+{
+    static protected $description = 'Buy respondents inside LimeSurvey';
+    static protected $name = 'CintLink';
+
+    protected $storage = 'DbStorage';
+    //protected $settings = array();
+}
---
+++ application/core/plugins/CintLink/views/index.php
@@ -0,0 +1,5 @@
+<div class="row">
+    <div class="col-lg-12 content-right">
+        <h3><?php eT("CintLink Integration");?></h3>
+    </div>
+</div>
