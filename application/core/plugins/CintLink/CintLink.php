4eff70ed53cd383bc3b4089da3d40b240a10f640
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2016-07-14 10:49:48 +0200

    Dev: Check if user is logged in on limesurvey.org (CintLink)

--- application/core/plugins/CintLink/CintLink.php
+++ application/core/plugins/CintLink/CintLink.php
@@ -2,6 +2,8 @@
 
 use \ls\menu\MenuItem;
 
+require_once(__DIR__ . "/CintLinkAPI.php");
+
 /**
  * CintLink integration to be able to buy respondents
  * from within LimeSurvey.
@@ -17,7 +19,21 @@ class CintLink extends \ls\pluginmanager\PluginBase
     protected $storage = 'DbStorage';
     //protected $settings = array();
 
+    /**
+     * Key from Cint Link to access their widget
+     *
+     * @var string
+     */
     private $cintApiKey = "";
+
+    /**
+     * This is the key handed to you from the
+     * com_api Joomla component on limesurvey.org
+     * after login, to access Rest plugins
+     *
+     * @var string
+     */
+    private $limesurveyOrgKey = "";
 
     public function init()
     {
@@ -25,8 +41,18 @@ class CintLink extends \ls\pluginmanager\PluginBase
         $this->subscribe('newDirectRequest');
 
         $this->cintApiKey = "7809687755495";  // Sandbox
+
+        // Login session key from com_api at limesurvey.org
+        $limesurveyOrgKey = Yii::app()->user->getState('limesurveyOrgKey');
+        if (!empty($limesurveyOrgKey))
+        {
+            $this->limesurveyOrgKey = $limesurveyOrgKey;
+        }
     }
 
+    /**
+     * todo place somewhere else
+     */
     public function beforeToolsMenuRender()
     {
         $event = $this->getEvent();
@@ -57,11 +83,31 @@ class CintLink extends \ls\pluginmanager\PluginBase
     public function actionIndex($surveyId)
     {
         $data = array();
+        $data['limesurveyOrgKey'] = $this->limesurveyOrgKey;
 
         Yii::setPathOfAlias('cintLink', dirname(__FILE__));
         $content = Yii::app()->controller->renderPartial('cintLink.views.index', $data, true);
 
+        $curl = new Curl();
+
         return $content;
+    }
+
+    /**
+     * Return json result true if user is logged in on limesurvey.org
+     *
+     * @param LSHttpRequest $request
+     * @return string JSON
+     */
+    public function checkIfUserIsLoggedInOnLimesurveyorg(LSHttpRequest $request)
+    {
+        if (empty($this->limesurveyOrgKey))
+        {
+            return json_encode(array('result' => false));
+        }
+        else
+        {
+        }
     }
 
     public function newDirectRequest()
@@ -74,6 +120,11 @@ class CintLink extends \ls\pluginmanager\PluginBase
             if ($functionToCall == "actionIndex")
             {
                 $content = $this->actionIndex($request);
+                $event->setContent($this, $content);
+            }
+            else if ($functionToCall == 'checkIfUserIsLoggedInOnLimesurveyorg')
+            {
+                $content = $this->checkIfUserIsLoggedInOnLimesurveyorg($request);
                 $event->setContent($this, $content);
             }
         }
