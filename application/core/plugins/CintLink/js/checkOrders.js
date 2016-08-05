a3ab2a75dc92152ac7e30473c4afd38ec48b7b92
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2016-08-05 17:29:39 +0200

    Dev: Add JS to update Cint orders async

---
+++ application/core/plugins/CintLink/js/checkOrders.js
@@ -0,0 +1,19 @@
+/**
+ * This file is included ONLY if user tried
+ * to pay for an order. This is to update the
+ * Cint order status at each page reload using
+ * Ajax, so the page load won't be slower.
+ *
+ * @since 2016-08-05
+ */
+
+$(document).ready(function() {
+    $.ajax({
+        url: LS.plugin.cintlink.pluginBaseUrl + '&method=updateAllOrders',
+        surveyId: LS.plugin.cintlink.surveyId,
+        success: function(data) {
+            console.log('data', data);
+            // Nothing to do
+        }
+    });
+});
