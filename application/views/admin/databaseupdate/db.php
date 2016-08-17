5f934df4b0b5cab4c96840fd1765c1ec89454e5e
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2016-08-17 17:35:47 +0200

    Dev: Add flash messages div

--- application/views/admin/databaseupdate/db.php
+++ application/views/admin/databaseupdate/db.php
@@ -1,1 +1,8 @@
+<?php
+    foreach(Yii::app()->user->getFlashes() as $key => $message)
+    {
+        echo '<div class="flash-' . $key . '">' . $message . "</div>\n";
+    }
+?>
+
 <?php echo $output; ?>
