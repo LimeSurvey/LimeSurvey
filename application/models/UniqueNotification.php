a32c5becc5dd09a9ec733ca288137e47d572efad
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2016-08-18 10:30:06 +0200

    Dev: Add new class UniqueNotification (see manual for more info)

---
+++ application/models/UniqueNotification.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+* Subclass of Notification, but with unique constraint.
+* If a new message is created exactly like another one,
+* it will be marked as unread.
+ */
+
+class UniqueNotification extends Notification
+{
+    /**
+     * Check for already existing notification and
+     * update that.
+     * @return void
+     */
+    public function save($runValidation = true, $attributes = null)
+    {
+        $toHash = $this->entity . $this->entity_id . $this->title . $this->message;
+        $this->hash = hash('sha256', $toHash);
+
+        $duplicate = self::model()->findByAttributes(array(
+            'hash' => $this->hash
+        ));
+
+        if (empty($duplicate))
+        {
+            parent::save($runValidation, $attributes);
+        }
+        else
+        {
+            $duplicate->status = 'new';
+            $duplicate->update();
+        }
+
+    }
+}
