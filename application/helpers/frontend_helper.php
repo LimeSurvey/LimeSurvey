5826f715991a72142efa3f97ce2df5936c94b39c
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2016-08-30 12:33:32 +0200

    Dev: Factor out initFieldArray

--- application/helpers/frontend_helper.php
+++ application/helpers/frontend_helper.php
@@ -1079,75 +1079,7 @@ function buildsurveysession($surveyid,$preview=false)
 
     $_SESSION['survey_'.$surveyid]['fieldmap']=$fieldmap;
 
-    foreach ($fieldmap as $key => $field)
-    {
-        if (isset($field['qid']) && $field['qid']!='')
-        {
-            $_SESSION['survey_'.$surveyid]['fieldnamesInfo'][$field['fieldname']]=$field['sid'].'X'.$field['gid'].'X'.$field['qid'];
-            $_SESSION['survey_'.$surveyid]['insertarray'][]=$field['fieldname'];
-            //fieldarray ARRAY CONTENTS -
-            //            [0]=questions.qid,
-            //            [1]=fieldname,
-            //            [2]=questions.title,
-            //            [3]=questions.question
-            //                     [4]=questions.type,
-            //            [5]=questions.gid,
-            //            [6]=questions.mandatory,
-            //            [7]=conditionsexist,
-            //            [8]=usedinconditions
-            //            [8]=usedinconditions
-            //            [9]=used in group.php for question count
-            //            [10]=new group id for question in randomization group (GroupbyGroup Mode)
-
-
-
-            if (!isset($_SESSION['survey_'.$surveyid]['fieldarray'][$field['sid'].'X'.$field['gid'].'X'.$field['qid']]))
-            {
-                //JUST IN CASE : PRECAUTION!
-                //following variables are set only if $style=="full" in createFieldMap() in common_helper.
-                //so, if $style = "short", set some default values here!
-                if (isset($field['title']))
-                    $title = $field['title'];
-                else
-                    $title = "";
-
-                if (isset($field['question']))
-                    $question = $field['question'];
-                else
-                    $question = "";
-
-                if (isset($field['mandatory']))
-                    $mandatory = $field['mandatory'];
-                else
-                    $mandatory = 'N';
-
-                if (isset($field['hasconditions']))
-                    $hasconditions = $field['hasconditions'];
-                else
-                    $hasconditions = 'N';
-
-                if (isset($field['usedinconditions']))
-                    $usedinconditions = $field['usedinconditions'];
-                else
-                    $usedinconditions = 'N';
-
-                $_SESSION['survey_'.$surveyid]['fieldarray'][$field['sid'].'X'.$field['gid'].'X'.$field['qid']]=array($field['qid'],
-                $field['sid'].'X'.$field['gid'].'X'.$field['qid'],
-                $title,
-                $question,
-                $field['type'],
-                $field['gid'],
-                $mandatory,
-                $hasconditions,
-                $usedinconditions);
-            }
-            if (isset($field['random_gid']))
-            {
-                $_SESSION['survey_'.$surveyid]['fieldarray'][$field['sid'].'X'.$field['gid'].'X'.$field['qid']][10] = $field['random_gid'];
-            }
-        }
-
-    }
+    initFieldArray($surveyid, $fieldmap);
 
     // Prefill questions/answers from command line params
     $reservedGetValues= array('token','sid','gid','qid','lang','newtest','action');
@@ -1209,6 +1141,80 @@ function buildsurveysession($surveyid,$preview=false)
         }
     }
     Yii::trace('end', 'survey.buildsurveysession');
+}
+
+/**
+ * @param array $fieldmap
+ * @return void
+ */
+function initFieldArray($surveyid, array $fieldmap)
+{
+    foreach ($fieldmap as $key => $field)
+    {
+        if (isset($field['qid']) && $field['qid']!='')
+        {
+            $_SESSION['survey_'.$surveyid]['fieldnamesInfo'][$field['fieldname']]=$field['sid'].'X'.$field['gid'].'X'.$field['qid'];
+            $_SESSION['survey_'.$surveyid]['insertarray'][]=$field['fieldname'];
+            //fieldarray ARRAY CONTENTS -
+            //            [0]=questions.qid,
+            //            [1]=fieldname,
+            //            [2]=questions.title,
+            //            [3]=questions.question
+            //            [4]=questions.type,
+            //            [5]=questions.gid,
+            //            [6]=questions.mandatory,
+            //            [7]=conditionsexist,
+            //            [8]=usedinconditions
+            //            [8]=usedinconditions
+            //            [9]=used in group.php for question count
+            //            [10]=new group id for question in randomization group (GroupbyGroup Mode)
+
+            if (!isset($_SESSION['survey_'.$surveyid]['fieldarray'][$field['sid'].'X'.$field['gid'].'X'.$field['qid']]))
+            {
+                //JUST IN CASE : PRECAUTION!
+                //following variables are set only if $style=="full" in createFieldMap() in common_helper.
+                //so, if $style = "short", set some default values here!
+                if (isset($field['title']))
+                    $title = $field['title'];
+                else
+                    $title = "";
+
+                if (isset($field['question']))
+                    $question = $field['question'];
+                else
+                    $question = "";
+
+                if (isset($field['mandatory']))
+                    $mandatory = $field['mandatory'];
+                else
+                    $mandatory = 'N';
+
+                if (isset($field['hasconditions']))
+                    $hasconditions = $field['hasconditions'];
+                else
+                    $hasconditions = 'N';
+
+                if (isset($field['usedinconditions']))
+                    $usedinconditions = $field['usedinconditions'];
+                else
+                    $usedinconditions = 'N';
+
+                $_SESSION['survey_'.$surveyid]['fieldarray'][$field['sid'].'X'.$field['gid'].'X'.$field['qid']]=array($field['qid'],
+                $field['sid'].'X'.$field['gid'].'X'.$field['qid'],
+                $title,
+                $question,
+                $field['type'],
+                $field['gid'],
+                $mandatory,
+                $hasconditions,
+                $usedinconditions);
+            }
+            if (isset($field['random_gid']))
+            {
+                $_SESSION['survey_'.$surveyid]['fieldarray'][$field['sid'].'X'.$field['gid'].'X'.$field['qid']][10] = $field['random_gid'];
+            }
+        }
+    }
 }
 
 /**
