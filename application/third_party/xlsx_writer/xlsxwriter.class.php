c63cba4f3e391af334b0c5cfaaf3983f30e5d8c6
Carsten Schmitz <carsten.schmitz@limesurvey.org>
2016-09-21 10:49:16 +0200

    Fixed issue: Control characters break XML export

--- application/third_party/xlsx_writer/xlsxwriter.class.php
+++ application/third_party/xlsx_writer/xlsxwriter.class.php
@@ -582,6 +582,7 @@ class XLSXWriter
 	//------------------------------------------------------------------
 	public static function xmlspecialchars($val)
 	{
+        $val=preg_replace('/[\x00-\x08\x0B\x0C\x0E-\x1F\x80-\x9F]/u', '', $val);
 		return str_replace("'", "&#39;", htmlspecialchars($val));
 	}
 	//------------------------------------------------------------------
