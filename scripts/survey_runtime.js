06f4c83bcfa9759d315b92886c59fca9d4b6d6cd
Denis Chenu <denis@sondages.pro>
2016-10-11 17:17:28 +0200

    Dev: how he come back ?

--- scripts/survey_runtime.js
+++ scripts/survey_runtime.js
@@ -19,7 +19,6 @@ $(document).ready(function()
     limesurveySubmitHandler();
     needConfirmHandler();
     tableCellAdapters();
-    navbuttonsJqueryUi();
     showStartPopups();
     addClassEmpty();
     noScrollOnSelect();
