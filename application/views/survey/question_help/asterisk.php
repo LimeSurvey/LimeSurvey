f1882096c4ebe98f67df4b7d6f084368bed097c3
Denis Chenu <denis@sondages.pro>
2016-10-18 02:02:40 +0200

    Fixed issue #11500: asterisk for srennreader Dev: improve sentence : adding bracket in translation ? Unsure

--- application/views/survey/question_help/asterisk.php
+++ application/views/survey/question_help/asterisk.php
@@ -4,4 +4,4 @@
  */
 ?>
 <!-- Add a visual information + just Mandatory string for aria : can be improved -->
-<small class="text-danger asterisk fa fa-asterisk pull-left small" aria-hidden='true'></small><span class="sr-only text-danger asterisk"> (<?php echo gT("Mandatory"); ?>)<span>
+<small class="text-danger asterisk fa fa-asterisk pull-left small" aria-hidden='true'></small><span class="sr-only text-danger asterisk"> <?php echo gT("(This question is mandatory)"; ?>) <span>
