63c88969d40c71e985e622acc60186f138985c8f
Denis Chenu <denis@sondages.pro>
2016-10-18 09:42:36 +0200

    Dev: asterisk typo

--- application/views/survey/question_help/asterisk.php
+++ application/views/survey/question_help/asterisk.php
@@ -4,4 +4,5 @@
  */
 ?>
 <!-- Add a visual information + just Mandatory string for aria : can be improved -->
-<small class="text-danger asterisk fa fa-asterisk pull-left small" aria-hidden='true'></small><span class="sr-only text-danger asterisk"> <?php echo gT("(This question is mandatory)"; ?>) <span>
+<small class="text-danger asterisk fa fa-asterisk pull-left small" aria-hidden='true'></small>
+<span class="sr-only text-danger asterisk"> <?php echo gT("(This question is mandatory)"); ?> </span>
