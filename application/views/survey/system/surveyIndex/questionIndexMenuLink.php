c35c06f9cc1ac3942374b02a01a6b455b6a148f6
Denis Chenu <denis@sondages.pro>
2016-10-28 18:56:47 +0200

    Dev: sub menu for question index Dev: little hack, add a .dropdown-menu .dropdown-sub-menu to surclass .dropdown-menu .dropdown-menu Dev: can use .dropdown-menu .dropdown-menu in css , but more clear with a new class

--- application/views/survey/system/surveyIndex/questionIndexMenuLink.php
+++ application/views/survey/system/surveyIndex/questionIndexMenuLink.php
@@ -12,27 +12,27 @@
     </a>
     <ul class="dropdown-menu">
         <?php foreach($indexItems as $group): ?>
-            <?php if(!empty($first)): ?>
-                <li class="divider" role="separator"></li>
-            <?php else: ?>
-                <?php $first=true; ?>
-            <?php endif; ?>
-            <li class="dropdown-header">
-                <?php echo viewHelper::flatEllipsizeText($group['text'],true,30," &hellip; ",0.6); ?>
+            <li>
+                <div class="dropdown-header">
+                    <?php echo viewHelper::flatEllipsizeText($group['text'],true,30," &hellip; ",0.6); ?>
+                    <span class="caret"></span>
+                </div>
+                <ul class="dropdown-menu dropdown-sub-menu">
+                <?php foreach($group['questions'] as  $step=>$indexItem): ?>
+                    <?php
+                        /* bs class for testing : bg-danger is really great here, but only if menu is set in navigator or after */
+                        $statusClass = $indexItem['stepStatus']['index-item-unanswered']? " bg-warning":"";
+                        $statusClass.= $indexItem['stepStatus']['index-item-error']? " bg-danger":"";
+                        $statusClass.= $indexItem['stepStatus']['index-item-current']? " disabled":"";
+                    ?>
+                    <li class="<?php echo $indexItem['coreClass']; ?><?php echo $statusClass; ?>">
+                        <a href='<?php echo $indexItem['url']; ?>' data-limesurvey-submit='<?php echo $indexItem['submit']; ?>'>
+                            <?php echo viewHelper::flatEllipsizeText($indexItem['text'],true,30," &hellip; ",0.6); ?>
+                        </a>
+                    </li>
+                <?php endforeach;?>
+                </ul>
             </li>
-            <?php foreach($group['questions'] as  $step=>$indexItem): ?>
-                <?php
-                    /* bs class for testing : bg-danger is really great here, but only if menu is set in navigator or after */
-                    $statusClass = $indexItem['stepStatus']['index-item-unanswered']? " bg-warning":"";
-                    $statusClass.= $indexItem['stepStatus']['index-item-error']? " bg-danger":"";
-                    $statusClass.= $indexItem['stepStatus']['index-item-current']? " disabled":"";
-                ?>
-                <li class="<?php echo $indexItem['coreClass']; ?><?php echo $statusClass; ?>">
-                    <a href='<?php echo $indexItem['url']; ?>' data-limesurvey-submit='<?php echo $indexItem['submit']; ?>'>
-                        <?php echo viewHelper::flatEllipsizeText($indexItem['text'],true,30," &hellip; ",0.6); ?>
-                    </a>
-                </li>
-            <?php endforeach;?>
         <?php endforeach;?>
     </ul>
 </li>
