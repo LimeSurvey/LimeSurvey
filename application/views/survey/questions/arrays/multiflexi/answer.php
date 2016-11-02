c45649c15a1d2fa7442a2c1b677845fb23a7681c
Tammo ter Hark <tammo.terhark@toolsforresearch.com>
2016-11-02 12:47:36 +0100

    Dev: missing space added
    It's all a question of space! ;-)

--- application/views/survey/questions/arrays/multiflexi/answer.php
+++ application/views/survey/questions/arrays/multiflexi/answer.php
@@ -14,7 +14,7 @@
  */
 ?>
 <!-- answer -->
-<table class="<?php echo $coreClass; ?> <?php echo $extraclass; ?>table table-bordered table-hover">
+<table class="<?php echo $coreClass; ?> <?php echo $extraclass; ?> table table-bordered table-hover">
 
     <colgroup class="col-responses">
         <col class="answertext" style='width: <?php echo $answerwidth;?>%;'/>
