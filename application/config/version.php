34eadffdcaaa5975dddf531f8d9d278f7cfbae75
Carsten Schmitz <carsten.schmitz@limesurvey.org>
2016-11-17 17:23:42 +0100

    Release 2.56.1 Build 161118

--- application/config/version.php
+++ application/config/version.php
@@ -12,11 +12,11 @@
  *150413
  */
 
-$config['versionnumber'] = '2.56';
+$config['versionnumber'] = '2.56.1';
 $config['dbversionnumber'] = 260;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
-$config['assetsversionnumber'] = '42';
+$config['assetsversionnumber'] = '43';
 return $config;
 
 ?>
