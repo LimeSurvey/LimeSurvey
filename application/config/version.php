b8b823a5954f29f425412e41d4138c66041dce25
LouisGac <louis.gac@limesurvey.org>
2016-11-17 10:28:52 +0100

    Dev: increase asset version number

--- application/config/version.php
+++ application/config/version.php
@@ -16,7 +16,7 @@ $config['versionnumber'] = '2.55.3';
 $config['dbversionnumber'] = 260;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
-$config['assetsversionnumber'] = '40';
+$config['assetsversionnumber'] = '41';
 return $config;
 
 ?>
