ea34784c8b33087722a6c7441e8acf608affac6e
Carsten Schmitz <carsten.schmitz@limesurvey.org>
2016-12-02 09:55:18 +0100

    Fixed issue: [security] Possible session fixation on survey entry with token

--- application/helpers/frontend_helper.php
+++ application/helpers/frontend_helper.php
@@ -1164,6 +1164,7 @@ function buildsurveysession($surveyid,$preview=false)
         }
     }
 
+    
     //RESET ALL THE SESSION VARIABLES AND START AGAIN
     unset($_SESSION['survey_'.$surveyid]['grouplist']);
     unset($_SESSION['survey_'.$surveyid]['fieldarray']);
@@ -1172,7 +1173,9 @@ function buildsurveysession($surveyid,$preview=false)
     unset($_SESSION['survey_'.$surveyid]['fieldmap-' . $surveyid . '-randMaster']);
     unset($_SESSION['survey_'.$surveyid]['groupReMap']);
     $_SESSION['survey_'.$surveyid]['fieldnamesInfo'] = Array();
-
+    // Generate Session ID
+    Yii:app()->session->regenerateID(true);
+    
     // Multi lingual support order : by REQUEST, if not by Token->language else by survey default language
     if (returnGlobal('lang',true))
     {
