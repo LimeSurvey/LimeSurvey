c975ffdc5ce3687bfb0763e26c88dcea13de42c5
kairavesloot <ravesloot@lime-support.com>
2016-12-06 22:10:02 +0100

    add BEM naming
    add content wrapper

--- application/views/statistics_user_view.php
+++ application/views/statistics_user_view.php
@@ -2,7 +2,7 @@
     var graphUrl = "<?php echo Yii::app()->getController()->createUrl("admin/statistics/sa/graph"); ?>";
 </script>
 
-<div class="container public-stats-container">
+<div class="container public-stats">
     <div class="row">
         <div class="col-sm-12">
             <div id='statsContainer'>
@@ -10,9 +10,11 @@
                     <div class='h3 statsSurveyTitle'><?php echo $thisSurveyTitle; ?></div>
                     <div class='statsNumRecords'><?php echo gT("Total records in survey") . " : $totalrecords"; ?></div>
                 </div>
-                <?php if (isset($statisticsoutput) && $statisticsoutput) {
-                    echo $statisticsoutput;
-                } ?><br/>
+                <div class="public-stats__content">
+                    <?php if (isset($statisticsoutput) && $statisticsoutput) {
+                        echo $statisticsoutput;
+                    } ?><br/>
+                </div>
             </div>
             <input type='hidden' class='hidemenubutton'/>
         </div>
