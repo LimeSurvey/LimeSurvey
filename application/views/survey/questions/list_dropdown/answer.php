729b8305f229393179122182b6dc12d8fbe2af10
kairavesloot <ravesloot@lime-support.com>
2016-12-06 22:46:44 +0100

    add bem classes

--- application/views/survey/questions/list_dropdown/answer.php
+++ application/views/survey/questions/list_dropdown/answer.php
@@ -2,8 +2,8 @@
 /**
  * List DropDown select Html
  *
- * @var $sOptions         : the select options, generated with the view item_options.php
- * @var $sOther           : the other input field, generated with the view item_noanswer.php
+ * @var $sOptions : the select options, generated with the view item_options.php
+ * @var $sOther : the other input field, generated with the view item_noanswer.php
  *
  * @var $name
  * @var $dropdownSize
@@ -17,34 +17,36 @@
 
 <!-- answer-->
 
-<div class="answer-item dropdown-item col-sm-4">
-    <label for="answer<?php echo $name; ?>" class="hide label">
-        <?php eT('Please choose'); ?>
-    </label>
-    <select
-            class="form-control list-question-select"
-            name="<?php echo $name; ?>"
-            id="answer<?php echo $name; ?>"
-            <?php  echo ($dropdownSize) ? "size=$dropdownSize" : "" ; ?>
-            onchange="<?php echo $checkconditionFunction; ?>(this.value, this.name, this.type);<?php echo $select_show_hide; ?>"
-    >
-        <?php
+<div class="answer-dropdown answer-item dropdown-item col-sm-4">
+    <div class="answer-dropdown__inner">
+        <label for="answer<?php echo $name; ?>" class="hide label">
+            <?php eT('Please choose'); ?>
+        </label>
+        <select
+                class="form-control list-question-select answer-dropdown__select"
+                name="<?php echo $name; ?>"
+                id="answer<?php echo $name; ?>"
+            <?php echo ($dropdownSize) ? "size=$dropdownSize" : ""; ?>
+                onchange="<?php echo $checkconditionFunction; ?>(this.value, this.name, this.type);<?php echo $select_show_hide; ?>"
+        >
+            <?php
             // rows/option.php
             echo $sOptions;
-        ?>
+            ?>
 
-    </select>
+        </select>
 
-    <input
-        type="hidden"
-        name="java<?php echo $name; ?>"
-        id="java<?php echo $name; ?>"
-        value="<?php echo $value; ?>"
-    />
+        <input
+                type="hidden"
+                name="java<?php echo $name; ?>"
+                id="java<?php echo $name; ?>"
+                value="<?php echo $value; ?>"
+        />
 
-    <?php
+        <?php
         // rows/othertext.php
         echo $sOther;
-    ?>
+        ?>
+    </div>
 </div>
 <!-- end of answer  -->
