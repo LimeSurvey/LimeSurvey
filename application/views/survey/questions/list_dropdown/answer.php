4cff7715dde89dddae19507f1c5275d196f55d0e
kairavesloot <ravesloot@lime-support.com>
2016-12-06 23:20:15 +0100

    add the missing row and move BEM class to row
    bootstrap needs row - col - row - col

--- application/views/survey/questions/list_dropdown/answer.php
+++ application/views/survey/questions/list_dropdown/answer.php
@@ -16,37 +16,38 @@
 <!-- List Dropdown -->
 
 <!-- answer-->
+<div class="row answer-dropdown">
+    <div class="col-sm-4">
+        <div class="answer-dropdown__inner">
+            <label for="answer<?php echo $name; ?>" class="hide label">
+                <?php eT('Please choose'); ?>
+            </label>
+            <select
+                    class="form-control list-question-select answer-dropdown__select"
+                    name="<?php echo $name; ?>"
+                    id="answer<?php echo $name; ?>"
+                <?php echo ($dropdownSize) ? "size=$dropdownSize" : ""; ?>
+                    onchange="<?php echo $checkconditionFunction; ?>(this.value, this.name, this.type);<?php echo $select_show_hide; ?>"
+            >
+                <?php
+                // rows/option.php
+                echo $sOptions;
+                ?>
 
-<div class="answer-dropdown answer-item dropdown-item col-sm-4">
-    <div class="answer-dropdown__inner">
-        <label for="answer<?php echo $name; ?>" class="hide label">
-            <?php eT('Please choose'); ?>
-        </label>
-        <select
-                class="form-control list-question-select answer-dropdown__select"
-                name="<?php echo $name; ?>"
-                id="answer<?php echo $name; ?>"
-            <?php echo ($dropdownSize) ? "size=$dropdownSize" : ""; ?>
-                onchange="<?php echo $checkconditionFunction; ?>(this.value, this.name, this.type);<?php echo $select_show_hide; ?>"
-        >
+            </select>
+
+            <input
+                    type="hidden"
+                    name="java<?php echo $name; ?>"
+                    id="java<?php echo $name; ?>"
+                    value="<?php echo $value; ?>"
+            />
+
             <?php
-            // rows/option.php
-            echo $sOptions;
+            // rows/othertext.php
+            echo $sOther;
             ?>
-
-        </select>
-
-        <input
-                type="hidden"
-                name="java<?php echo $name; ?>"
-                id="java<?php echo $name; ?>"
-                value="<?php echo $value; ?>"
-        />
-
-        <?php
-        // rows/othertext.php
-        echo $sOther;
-        ?>
+        </div>
     </div>
 </div>
 <!-- end of answer  -->
