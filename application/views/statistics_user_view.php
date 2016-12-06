27b3aca99b0fa8419b6e7ed3c2f0764ac07008d7
kairavesloot <ravesloot@lime-support.com>
2016-12-06 22:14:32 +0100

    insert bem class to num records line
    remove br

--- application/views/statistics_user_view.php
+++ application/views/statistics_user_view.php
@@ -8,12 +8,12 @@
             <div id='statsContainer'>
                 <div id='statsHeader'>
                     <h2 class='statsSurveyTitle public-stats__title'><?php echo $thisSurveyTitle; ?></h2>
-                    <div class='statsNumRecords'><?php echo gT("Total records in survey") . " : $totalrecords"; ?></div>
+                    <div class='statsNumRecords public-stats__num-records'><?php echo gT("Total records in survey") . " : $totalrecords"; ?></div>
                 </div>
                 <div class="public-stats__content">
                     <?php if (isset($statisticsoutput) && $statisticsoutput) {
                         echo $statisticsoutput;
-                    } ?><br/>
+                    } ?>
                 </div>
             </div>
             <input type='hidden' class='hidemenubutton'/>
