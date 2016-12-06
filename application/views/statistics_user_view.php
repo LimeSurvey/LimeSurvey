d6b9ff48ac1d9e3a9c9225bad266b375ee44f161
kairavesloot <ravesloot@lime-support.com>
2016-12-06 22:08:06 +0100

    add bootstrap classes

--- application/views/statistics_user_view.php
+++ application/views/statistics_user_view.php
@@ -1,12 +1,20 @@
 <script type='text/javascript'>
-    var graphUrl="<?php echo Yii::app()->getController()->createUrl("admin/statistics/sa/graph"); ?>";
+    var graphUrl = "<?php echo Yii::app()->getController()->createUrl("admin/statistics/sa/graph"); ?>";
 </script>
 
-<div id='statsContainer'>
-    <div id='statsHeader'>
-        <div class='h3 statsSurveyTitle'><?php echo $thisSurveyTitle; ?></div>
-        <div class='statsNumRecords'><?php echo gT("Total records in survey")." : $totalrecords"; ?></div>
+<div class="container public-stats-container">
+    <div class="row">
+        <div class="col-sm-12">
+            <div id='statsContainer'>
+                <div id='statsHeader'>
+                    <div class='h3 statsSurveyTitle'><?php echo $thisSurveyTitle; ?></div>
+                    <div class='statsNumRecords'><?php echo gT("Total records in survey") . " : $totalrecords"; ?></div>
+                </div>
+                <?php if (isset($statisticsoutput) && $statisticsoutput) {
+                    echo $statisticsoutput;
+                } ?><br/>
+            </div>
+            <input type='hidden' class='hidemenubutton'/>
+        </div>
     </div>
-    <?php if (isset($statisticsoutput) && $statisticsoutput) { echo $statisticsoutput; } ?><br />
 </div>
-<input type='hidden' class='hidemenubutton'/>
