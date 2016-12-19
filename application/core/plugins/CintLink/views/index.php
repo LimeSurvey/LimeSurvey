9db367738a5538fb130f2dc33d61947911e88290
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2016-12-19 13:24:44 +0100

    Dev: Remove CintLink plugin
    Dev: Revert this commit to continue with CintLink dev

--- application/core/plugins/CintLink/views/index.php
+++
@@ -1,9 +0,0 @@
-<?php echo $common; ?>
-
-<div class="row">
-    <div class="col-lg-12 content-right">
-        <h3 id='cintlink-header'><?php eT("CintLink Integration");?></h3>
-        <div id='cintlink-container'>
-        </div>
-    </div>
-</div>
