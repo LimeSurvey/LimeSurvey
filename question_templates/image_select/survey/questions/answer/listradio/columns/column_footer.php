fae3b14cdf7c4c6c2cf2e0c91ede8509557244df
markusfluer <fluer.markus@googlemail.com>
2016-12-23 16:35:43 +0100

    Dev: Created new questiontype image_select

---
+++ question_templates/image_select/survey/questions/answer/listradio/columns/column_footer.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Multiple Choice Html : close column containing item row
+ * This view is used only if user set more than one column in the question attribute.
+ *
+ */
+ ?>
+</ul>
---
+++ question_templates/image_select/survey/questions/answer/multiplechoice/assets/scripts/imageselect.js
@@ -0,0 +1,7 @@
+"use strict";
+var IMAGESELECT = function(options){
+    var baseSettings = {
+
+    };
+    return {};
+};
