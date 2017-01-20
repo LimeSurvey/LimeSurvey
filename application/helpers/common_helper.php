ed3c5d5f37ff4d165d1660b10b6d96cd6c8a4fdb
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2017-01-20 11:25:19 +0100

    Dev: Fix undefined variable svexist

--- application/helpers/common_helper.php
+++ application/helpers/common_helper.php
@@ -5861,7 +5861,7 @@ function  doesImportArraySupportLanguage($csvarray,$idkeysarray,$langfieldnum,$l
 *
 * @param boolean $bIncludeOwner If the survey owner should be included
 * @param boolean $bIncludeSuperAdmins If Super admins should be included
-* @param int surveyid
+* @param int $surveyid
 * @return string
 */
 function getSurveyUserList($bIncludeOwner=true, $bIncludeSuperAdmins=true,$surveyid)
@@ -5885,25 +5885,37 @@ function getSurveyUserList($bIncludeOwner=true, $bIncludeSuperAdmins=true,$surve
 
     if (Yii::app()->getConfig('usercontrolSameGroupPolicy') == true)
     {
-
         $authorizedUsersList = getUserList('onlyuidarray');
     }
 
-        foreach($aSurveyIDResult as $sv)
-        {
-            if (Yii::app()->getConfig('usercontrolSameGroupPolicy') == false ||
+    $svexist = false;
+    foreach($aSurveyIDResult as $sv)
+    {
+        if (Yii::app()->getConfig('usercontrolSameGroupPolicy') == false ||
             in_array($sv['uid'],$authorizedUsersList))
-            {
-                $surveyselecter .= "<option";
-                $surveyselecter .=" value='{$sv['uid']}'>{$sv['users_name']} {$sv['full_name']}</option>\n";
-            }
+        {
+            $surveyselecter .= "<option";
+            $surveyselecter .=" value='{$sv['uid']}'>{$sv['users_name']} {$sv['full_name']}</option>\n";
+            $svexist = true;
         }
-    if (!isset($svexist)) {$surveyselecter = "<option value='-1' selected='selected'>".gT("Please choose...")."</option>\n".$surveyselecter;}
-    else {$surveyselecter = "<option value='-1'>".gT("None")."</option>\n".$surveyselecter;}
+    }
+
+    if ($svexist) {
+        $surveyselecter = "<option value='-1' selected='selected'>".gT("Please choose...")."</option>\n".$surveyselecter;
+    }
+    else {
+        $surveyselecter = "<option value='-1'>".gT("None")."</option>\n".$surveyselecter;
+    }
 
     return $surveyselecter;
 }
 
+/**
+ * Return HTML <option> list of user groups
+ * @param string $outputformat
+ * @param int $surveyid
+ * @return string
+ */
 function getSurveyUserGroupList($outputformat='htmloptions',$surveyid)
 {
 
@@ -5927,6 +5939,7 @@ function getSurveyUserGroupList($outputformat='htmloptions',$surveyid)
         $authorizedGroupsList=getUserGroupList(NULL, 'simplegidarray');
     }
 
+    $svexist = false;
     foreach($aResult as $sv)
     {
         if (Yii::app()->getConfig('usercontrolSameGroupPolicy') == false ||
@@ -5935,11 +5948,16 @@ function getSurveyUserGroupList($outputformat='htmloptions',$surveyid)
             $surveyselecter .= "<option";
             $surveyselecter .=" value='{$sv['ugid']}'>{$sv['name']}</option>\n";
             $simpleugidarray[] = $sv['ugid'];
+            $svexist = true;
         }
     }
 
-    if (!isset($svexist)) {$surveyselecter = "<option value='-1' selected='selected'>".gT("Please choose...")."</option>\n".$surveyselecter;}
-    else {$surveyselecter = "<option value='-1'>".gT("None")."</option>\n".$surveyselecter;}
+    if ($svexist) {
+        $surveyselecter = "<option value='-1' selected='selected'>".gT("Please choose...")."</option>\n".$surveyselecter;
+    }
+    else {
+        $surveyselecter = "<option value='-1'>".gT("None")."</option>\n".$surveyselecter;
+    }
 
     if ($outputformat == 'simpleugidarray')
     {
