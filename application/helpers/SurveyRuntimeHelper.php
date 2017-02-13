d1eecb2f5cc8d3a1e84574b0c2520023bd8a5e71
Denis Chenu <denis@sondages.pro>
2017-02-13 11:24:02 +0100

    [fix] Welcome can be shown with token - Re-fix https://framagit.org/Shnoulle/LimeSurvey/issues/1

--- application/helpers/SurveyRuntimeHelper.php
+++ application/helpers/SurveyRuntimeHelper.php
@@ -263,7 +263,7 @@ class SurveyRuntimeHelper {
                 elseif (isset($thissurvey['showwelcome']) && $thissurvey['showwelcome'] == 'N')
                 {
                     $moveResult = LimeExpressionManager::NavigateForwards();
-                    $_SESSION[$LEMsessid]['step']=$moveResult['seq'];
+                    $_SESSION[$LEMsessid]['step']=max(1,$moveResult['seq']);
                 }
             }
             elseif($surveyid != LimeExpressionManager::getLEMsurveyId())
