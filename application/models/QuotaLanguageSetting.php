c464b6a2e36d4b57481e0d8108af94e0ab27d0f8
Tõnis Ormisson <tonis@andmemasin.eu>
2017-02-25 22:53:48 +0200

    Add custom validator to url - must be set if autoload is ON

--- application/models/QuotaLanguageSetting.php
+++ application/models/QuotaLanguageSetting.php
@@ -21,6 +21,8 @@
  * @property string $quotals_message
  * @property string $quotals_url
  * @property string $quotals_urldescrip
+ *
+ * @property Quota $quota
  */
 class QuotaLanguageSetting extends LSActiveRecord
 {
@@ -69,8 +71,7 @@ class QuotaLanguageSetting extends LSActiveRecord
 	{
 		$alias = $this->getTableAlias();
 		return array(
-			'quota' => array(self::BELONGS_TO, 'Quota', '',
-				'on' => "quota.id = $alias.quotals_quota_id"),
+			'quota' => array(self::BELONGS_TO, 'Quota', 'quotals_quota_id'),
 		);
 	}
 
@@ -87,7 +88,13 @@ class QuotaLanguageSetting extends LSActiveRecord
             array('quotals_message','LSYii_Validators'),
             array('quotals_url','LSYii_Validators','isUrl'=>true),
             array('quotals_urldescrip','LSYii_Validators'),
+            array('quotals_url','urlValidator'),
         );
+    }
+    public function urlValidator(){
+        if($this->quota->autoload_url == 1 && !$this->quotals_url ){
+            $this->addError('quotals_url',gT('URL must be set if autoload URL is turned on!'));
+        }
     }
 
     public function attributeLabels()
