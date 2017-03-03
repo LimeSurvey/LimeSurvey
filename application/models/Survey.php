f4d97b371bbbdaba0f49448559cda4b0e49ccc8f
Carsten Schmitz <carsten.schmitz@limesurvey.org>
2017-03-03 17:06:59 +0100

    Fixed issue: Settings filter in survey list not working in MSSQL

--- application/models/Survey.php
+++ application/models/Survey.php
@@ -1111,20 +1111,17 @@ class Survey extends LSActiveRecord
 
                 if($this->active == "R")
                 {
-                    $now = new CDbExpression("NOW()");
-
                     $criteria->compare("t.active",'Y');
                     $subCriteria1 = new CDbCriteria;
                     $subCriteria2 = new CDbCriteria;
-                    $subCriteria1->addCondition($now.' > t.startdate', 'OR');
+                    $subCriteria1->addCondition("'{$sNow}' > t.startdate", 'OR');
+                    $subCriteria2->addCondition("'{$sNow}' < t.expires", 'OR');
                     $subCriteria1->addCondition('t.expires IS NULL', "OR");
                     $subCriteria1->addCondition($now.' < t.expires', 'OR');
                     $subCriteria2->addCondition($now.' < t.expires', 'OR');
                     $subCriteria2->addCondition('t.startdate IS NULL', "OR");
                     $criteria->mergeWith($subCriteria1);
                     $criteria->mergeWith($subCriteria2);
-
-
                 }
             }
         }
