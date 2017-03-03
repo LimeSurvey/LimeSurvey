54681170eac6a250abb31fee25ca53db67f910da
Fred Yankowski <fred@yankowski.com>
2017-03-03 08:21:48 -0600

    New feature: Extend LDAP authentication plugin with optional group-membership restriction

--- application/core/plugins/AuthLDAP/AuthLDAP.php
+++ application/core/plugins/AuthLDAP/AuthLDAP.php
@@ -99,6 +99,16 @@ class AuthLDAP extends ls\pluginmanager\AuthPluginBase
         'automaticsurveycreation' => array(
                 'type' => 'checkbox',
                 'label' => 'Grant survey creation permission to automatically created users'
+        	),
+        'groupsearchbase' => array(
+                'type' => 'string',
+                'label' => 'Optional base DN for group restriction',
+                'help' => 'E.g., ou=Groups,dc=example,dc=com'
+                ),
+        'groupsearchfilter' => array(
+                'type' => 'string',
+                'label' => 'Optional filter for group restriction',
+                'help' => 'Required if group search base set. E.g. (&(cn=limesurvey)(memberUid=$username))'
                 )
     );
 
@@ -440,6 +450,8 @@ class AuthLDAP extends ls\pluginmanager\AuthPluginBase
         $usersearchbase		= $this->get('usersearchbase');
         $binddn     		= $this->get('binddn');
         $bindpwd     		= $this->get('bindpwd');
+        $groupsearchbase        = $this->get('groupsearchbase');
+        $groupsearchfilter      = $this->get('groupsearchfilter');
 
         // Try to connect
         $ldapconn = $this->createConnection();
@@ -498,6 +510,21 @@ class AuthLDAP extends ls\pluginmanager\AuthPluginBase
                 $this->setAuthFailure(self::ERROR_USERNAME_INVALID);
                 ldap_close($ldapconn); // all done? close connection
                 return;
+            }
+
+            // If specifed, check group membership
+            if ($groupsearchbase != '' && $groupsearchfilter != '')
+            {
+                $filter = str_replace('$username', $username, $groupsearchfilter);
+                $groupsearchres = ldap_search($ldapconn, $groupsearchbase, $filter);
+                $grouprescount = ldap_count_entries($ldapconn, $groupsearchres);
+                if ($grouprescount < 1)
+                {
+                    $this->setAuthFailure(self::ERROR_USERNAME_INVALID,
+                    gT('Valid username but not authorized by group restriction'));
+                    ldap_close($ldapconn); // all done? close connection
+                    return;
+                }
             }
 
             // binding to ldap server with the userDN and privided credentials
