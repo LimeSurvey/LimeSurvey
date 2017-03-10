45dc11aa5a2c66d7f08bb8d206dc508bbc6860eb
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2017-03-10 14:29:31 +0100

    Dev: Hide export form

--- scripts/admin/participantpanel.js
+++ scripts/admin/participantpanel.js
@@ -117,7 +117,7 @@ LS.CPDB = (function() {
                                     .attr('value', value)
                                     .appendTo(dlForm);
                             });
-                            dlForm.appendTo('body').submit();
+                            dlForm.css('display', 'none').appendTo('body').submit();
                             $(self).modal("hide");
                         });
                         $('#attributes')
