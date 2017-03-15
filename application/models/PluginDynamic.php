66b3bfcfac66c4d277a474ba256384c83ddfba81
Tõnis Ormisson <tonis@andmemasin.eu>
2017-03-15 20:29:46 +0200

    DEV: Model DOCs: PluginDynamic properties

--- application/models/PluginDynamic.php
+++ application/models/PluginDynamic.php
@@ -1,77 +1,79 @@
 <?php
 
+/**
+ * Dynamic model used by plugins to access their table(s).
+ */
+class PluginDynamic extends LSActiveRecord
+{
+    private static $_models = array();
+
+    /** @var CActiveRecordMetaData $_md meta data*/
+    private $_md;
+
+    protected $tableName;
+
     /**
-     * Dynamic model used by plugins to access their table(s).
+     * @param string $sTableName
+     * @param string $scenario
+     * @throws Exception
      */
-    class PluginDynamic extends LSActiveRecord
+    public function __construct($sTableName = null, $scenario = 'insert')
     {
-        private static $_models = array();
-
-        private $_md;								// meta data
-	
-        protected $tableName;
-
-        /**
-         * @param string $scenario
-         * @param string $sTableName
-         */
-        public function __construct($sTableName = null, $scenario = 'insert')
+        if (!isset($sTableName))
         {
-            if (!isset($sTableName))
-            {
-                //Yii::trace('sTableName missing.');
-                throw new Exception('sTableName missing.');
+            //Yii::trace('sTableName missing.');
+            throw new Exception('sTableName missing.');
+        }
+        $this->tableName = $sTableName;
+        parent::__construct($scenario);
+    }
+
+
+    protected function instantiate($attributes = null)
+    {
+        $class=get_class($this);
+        $model=new $class($this->tableName(), null);
+        return $model;
+    }
+    /**
+     * We have a custom implementation here since the parents' implementation
+     * does not create a new model for each table name.
+     * @return Plugin
+     */
+    public static function model($sTableName = null)
+    {
+        if (isset($sTableName)) {
+            if (!isset(self::$_models[$sTableName])) {
+                $model = self::$_models[$sTableName] = new PluginDynamic($sTableName, null);
+                $model->_md = new CActiveRecordMetaData($model);
+                $model->attachBehaviors($model->behaviors());
             }
-            $this->tableName = $sTableName;
-            parent::__construct($scenario);
+            return self::$_models[$sTableName];
         }
+    }
 
+    /**
+     * Gets the tablename for the current model.
+     */
+    public function tableName() {
+        return $this->tableName;
+    }
 
-        protected function instantiate($attributes = null)
-        {
-            $class=get_class($this);
-            $model=new $class($this->tableName(), null);
-            return $model;
+    /**
+     * Override
+     * @return CActiveRecordMetaData the meta for this AR class.
+     */
+    public function getMetaData()
+    {
+        if($this->_md!==null){
+            return $this->_md;
         }
-        /**
-         * We have a custom implementation here since the parents' implementation
-         * does not create a new model for each table name.
-         * @return Plugin
-         */
-        public static function model($sTableName = null)
-        {
-            if (isset($sTableName))
-            {
-                if (!isset(self::$_models[$sTableName]))
-                {
-                    $model = self::$_models[$sTableName] = new PluginDynamic($sTableName, null);
-                    $model->_md = new CActiveRecordMetaData($model);
-                    $model->attachBehaviors($model->behaviors());
-                }
-                return self::$_models[$sTableName];
-            }
-        }
-
-        /**
-         * Gets the tablename for the current model.
-         */
-        public function tableName() {
-            return $this->tableName;
-        }
-
-        /**
-         * Override
-         * @return CActiveRecordMetaData the meta for this AR class.
-         */
-        public function getMetaData()
-        {
-            if($this->_md!==null)
-                return $this->_md;
-            else
-                return $this->_md=self::model($this->tableName())->_md;
-
+        else{
+            /** @var CActiveRecordMetaData $md */
+            $md = self::model($this->tableName())->_md;
+            return $this->_md=$md;
         }
 
     }
 
-?>
+}
