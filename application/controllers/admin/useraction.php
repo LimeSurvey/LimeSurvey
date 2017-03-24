7e49afea1e399ffc96433d43ce84e4fe3dc1aac4
markusfluer <markus.fluer@limesurvey.org>
2017-03-24 12:18:00 +0100

    Dev: removed Debug statement

--- application/controllers/admin/useraction.php
+++ application/controllers/admin/useraction.php
@@ -180,7 +180,6 @@ class UserAction extends Survey_Common_Action
 
       $csrfToken = Yii::app()->request->csrfToken;
       $transferredToken = $this->_getPostOrParam(Yii::app()->request->csrfTokenName);
-      var_dump(array($csrfToken, $transferredToken));
       if( $csrfToken != $transferredToken){
         Yii::app()->setFlashMessage(gT("Security token mismatch."),'error');
         $this->getController()->redirect(array("admin/user/sa/index"));
