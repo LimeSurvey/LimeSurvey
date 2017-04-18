fd44fc3f7931e9387e078813a7d6708f0ae5a080
LouisGac <louis.gac@limesurvey.org>
2017-04-18 13:14:53 +0200

    Dev: fixed merge

--- application/models/Response.php
+++ application/models/Response.php
@@ -1,71 +1,45 @@
 <?php
-
-/**
- * Relations
- * @property Token $token
- * @property Survey $survey
- */
-abstract class Response extends Dynamic
-{
-    /** @inheritdoc */
-    public function beforeDelete()
-    {
-        if (parent::beforeDelete()) {
-            $this->deleteFiles();
-            return true;
-        }
-        return false;
-    }
-
     /**
-     * @inheritdoc
-     * @param string|integer $className Either the classname or the survey id.
-     * @return Response
+     * Relations
+     * @property Token $token
+     * @property Survey $survey
      */
-    public static function model($className = null)
+    abstract class Response extends Dynamic
     {
-        return parent::model($className);
-    }
-
-    /**
-     *
-     * @param int $surveyId
-     * @param string $scenario
-     * @return string Response Description
-     */
-    public static function create($surveyId, $scenario = 'insert')
-    {
-        return parent::create($surveyId, $scenario);
-    }
-
-    /**
-     * Delete all files related to this response.
-     * @return array
-     */
-    public function getFiles()
-    {
-
-        $questions = Question::model()->findAllByAttributes(array('sid' => $this->dynamicId,'type' => '|','language'=>getBaseLanguageFromSurveyID($this->dynamicId)));
-        $files = array();
-        foreach ($questions as $question) {
-
-        /**
-        * Get all files related to this response and (optionally) question ID.
-        * 
-        * @param string $sQID The question ID - optional - Default 0
-        */
-        public function getFiles($sQID=0)
-        {
-            $aConditions=array('sid' => $this->dynamicId,'type' => '|','language'=>getBaseLanguageFromSurveyID($this->dynamicId));
-            if ($sQID>0)
+        public function beforeDelete() {
+            if (parent::beforeDelete())
             {
-                $aConditions['qid']=$sQID;
+                $this->deleteFiles();
+                return true;
             }
-            $aQuestions = Question::model()->findAllByAttributes($aConditions);
+            return false;
+        }
+        /**
+         *
+         * @param mixed $className Either the classname or the survey id.
+         * @return Response
+         */
+        public static function model($className = null) {
+            return parent::model($className);
+        }
+        /**
+         *
+         * @param int $surveyId
+         * @param string $scenario
+         * @return Response Description
+         */
+        public static function create($surveyId, $scenario = 'insert') {
+            return parent::create($surveyId, $scenario);
+        }
+        /**
+         * Delete all files related to this repsonse.
+         */
+        public function getFiles()
+        {
+            $questions = Question::model()->findAllByAttributes(array('sid' => $this->dynamicId,'type' => '|','language'=>getBaseLanguageFromSurveyID($this->dynamicId)));
             $files = array();
-            foreach ($aQuestions as $question)
+            foreach ($questions as $question)
             {
-
                 $field = "{$question->sid}X{$question->gid}X{$question->qid}";
                 $data = json_decode(stripslashes($this->getAttribute($field)), true);
                 if (is_array($data))
@@ -73,58 +47,41 @@ abstract class Response extends Dynamic
                     $files = array_merge($files, $data);
                 }
             }
-        return $files;
-    }
+            return $files;
+        }
+        public function deleteFiles()
+        {
+            $uploaddir = Yii::app()->getConfig('uploaddir') ."/surveys/{$this->dynamicId}/files/";
+            foreach ($this->getFiles() as $fileInfo)
+            {
+                @unlink($uploaddir . basename($fileInfo['filename']));
+            }
+        }
+        public function delete($deleteFiles = false) {
+            if ($deleteFiles) {
+                $this->deleteFiles();
+            }
+            return parent::delete();
+        }
+        public function relations()
+        {
+            $t = $this->getTableAlias();
+            $result = array(
+                'token' => array(self::BELONGS_TO, 'Token_' . $this->dynamicId, array('token' => 'token')),
+                'survey' =>  array(self::BELONGS_TO, 'Survey', '', 'on' => "sid = {$this->dynamicId}" )
+            );
+            return $result;
+        }
+        public function tableName()
+        {
+            return '{{survey_' . $this->dynamicId . '}}';
+        }
+        public function getSurveyId() {
+            return $this->dynamicId;
+        }
+        public function browse(){
+        }
+        public function search(){
 
-    public function deleteFiles()
-    {
-        $uploaddir = Yii::app()->getConfig('uploaddir') ."/surveys/{$this->dynamicId}/files/";
-        foreach ($this->getFiles() as $fileInfo) {
-            @unlink($uploaddir . basename($fileInfo['filename']));
         }
     }
-
-    /** @inheritdoc */
-    public function delete($deleteFiles = false)
-    {
-        if ($deleteFiles) {
-            $this->deleteFiles();
-        }
-        return parent::delete();
-    }
-
-    /** @inheritdoc */
-    public function relations()
-    {
-        $t = $this->getTableAlias();
-        $result = array(
-            'token' => array(self::BELONGS_TO, 'Token_' . $this->dynamicId, array('token' => 'token')),
-            'survey' =>  array(self::BELONGS_TO, 'Survey', '', 'on' => "sid = {$this->dynamicId}" )
-        );
-        return $result;
-    }
-
-    /** @inheritdoc */
-    public function tableName()
-    {
-        return '{{survey_' . $this->dynamicId . '}}';
-    }
-
-    /**
-     * @return int
-     */
-    public function getSurveyId()
-    {
-        return $this->dynamicId;
-    }
-
-    public function browse()
-    {
-
-    }
-
-    public function search()
-    {
-
-    }
-}
