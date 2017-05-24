7d735774862cc9cfcf7d997f625630f420372c6c
Denis Chenu <denis@sondages.pro>
2017-05-24 17:49:42 +0200

    New feature : add some collapse feature to remotecontrol function list

--- application/views/admin/remotecontrol/index_view.php
+++ application/views/admin/remotecontrol/index_view.php
@@ -1,11 +1,27 @@
 <div id='remotecontrol' class="row">
     <div class="col-xs-12">
-    <h3 class="pagetitle"><?php echo sprintf(gT('RemoteControl is available using %s for transport and exposes the following functionality:'),$method); ?></h3>
+    <div class="pagetitle h3"><?php echo sprintf(gT('RemoteControl is available using %s for transport and exposes the following functionality:'),$method); ?></div>
     <dl>
     <?php
     foreach ($list as $method => $info) {
-        echo \CHtml::tag("dt",array('class'=>"h4"),$method);
-        echo \CHtml::tag("dd",array(),\CHtml::tag("pre",array(),\CHtml::encode($info['description'])));
+        echo \CHtml::tag(
+            "dt",
+            array('class'=>"h4"),
+            \CHtml::link($method,"#definition-{$method}",array(
+                "role" => "button",
+                "data-toggle" => "collapse",
+                "aria-expanded" => "false",
+                "aria-controls" => "definition-{$method}",
+            ))
+        );
+        echo \CHtml::tag(
+            "dd",
+            array(
+                "class" => "collapse",
+                "id" => "definition-{$method}"
+            ),
+            \CHtml::tag("pre",array(),\CHtml::encode($info['description']))
+        );
     }
     ?>
     </dl>
