7efabeafac7335f5d73f625af9b664c0e2241f9d
Denis Chenu <shnoulle@beldaran.maison.shnoulle.net>
2017-05-24 16:08:37 +0200

    Fixed issue #12368: Remote control: properties that are available are not shown Dev: starting, but really long ..... Dev: https://www.phpdoc.org/docs/latest/references/phpdoc/basic-syntax.html allow markdown the use it

--- application/views/admin/remotecontrol/index_view.php
+++ application/views/admin/remotecontrol/index_view.php
@@ -1,9 +1,13 @@
-<div id='remotecontrol'>
-    <?php echo sprintf(gT('RemoteControl is available using %s for transport and exposes the following functionality:'),$method); ?>
-    <br/><br/>
+<div id='remotecontrol' class="row">
+    <div class="col-xs-12">
+    <h3 class="pagetitle"><?php echo sprintf(gT('RemoteControl is available using %s for transport and exposes the following functionality:'),$method); ?></h3>
+    <dl>
     <?php
     foreach ($list as $method => $info) {
-        echo sprintf('<b>%s</b><br/>%s<br/>',$method, $info['description']);
+        echo \CHtml::tag("dt",array('class'=>"h4"),$method);
+        echo \CHtml::tag("dd",array(),\CHtml::tag("pre",array(),\CHtml::encode($info['description'])));
     }
     ?>
+    </dl>
+    </div>
 </div>
