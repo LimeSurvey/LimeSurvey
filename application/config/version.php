003d0cece91df5328de5082a689ff3faf476415e
Carsten Schmitz <carsten.schmitz@limesurvey.org>
2017-06-06 17:20:33 +0200

    Release 2.65.2 Build 170606

--- application/config/version.php
+++ application/config/version.php
@@ -12,11 +12,11 @@
  *150413
  */
 
-$config['versionnumber'] = '2.65.1';
+$config['versionnumber'] = '2.65.2';
 $config['dbversionnumber'] = 261;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
-$config['assetsversionnumber'] = '2651';
+$config['assetsversionnumber'] = '2652';
 return $config;
 
 ?>
