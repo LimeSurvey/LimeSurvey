b86cf0b6873eb56d89b3b55065c55ee982ea6ecf
Carsten Schmitz <carsten.schmitz@limesurvey.org>
2017-06-06 14:20:49 +0200

    Dev Updated Yii framework to 1.1.18

--- framework/zii/widgets/assets/gridview/jquery.yiigridview.js
+++ framework/zii/widgets/assets/gridview/jquery.yiigridview.js
@@ -405,11 +405,7 @@
 			}
 			this.find('.' + settings.tableClass).children('tbody').children('tr').children('td').children('input[name="' + column_id + '"]').each(function (i) {
 				if (this.checked) {
-                    var val = keys.eq(i).text();
-                    if (val == '') {
-                        val = $(this).val();
-                    }
-					checked.push(val);
+					checked.push(keys.eq(i).text());
 				}
 			});
 			return checked;
