024323e1a059f14fa2bc2d36cbce03a13615ff8c
markusfluer <fluer.markus@googlemail.com>
2017-06-20 18:08:41 +0200

    Dev: added vuejs and started to rebuild the questiontree

---
+++ assets/packages/adminpanel/webpack.config.js
@@ -0,0 +1,80 @@
+var webpack = require('webpack');
+var path = require('path');
+
+
+// Naming and path settings
+var appName = 'lsadminpanel';
+var entryPoint = ['./src/main.js', './scss/main.scss'];
+var exportPath = path.resolve(__dirname, './build');
+
+// Enviroment flag
+var plugins = [];
+var env = process.env.WEBPACK_ENV;
+
+// Differ settings based on production flag
+if (env === 'production') {
+  var UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
+
+  plugins.push(new UglifyJsPlugin({ minimize: true }));
+  plugins.push(new webpack.DefinePlugin({
+    'process.env': {
+      NODE_ENV: '"production"'
+    }
+  }
+  ));
+
+  appName = appName + '.min.js';
+} else {
+  appName = appName + '.js';
+}
+
+// Main Settings config
+module.exports = {
+  entry: entryPoint,
+  output: {
+    path: exportPath,
+    filename: appName
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader" // creates style nodes from JS strings
+          }, {
+            loader: "css-loader" // translates CSS into CommonJS
+          }, {
+            loader: "sass-loader" // compiles Sass to CSS
+          }
+        ]
+      }
+    ],
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        loader: "sass-loader",
+        options: {
+            data: "$env: " + process.env.NODE_ENV + ";"
+        }
+    }
+    ]
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    }
+  },
+  plugins
+};
