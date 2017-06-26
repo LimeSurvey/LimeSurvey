1d6ab0bf50d04b6027672ce4b9f3c50ec16473a6
Carsten Schmitz <carsten.schmitz@limesurvey.org>
2017-06-26 10:07:38 +0200

    Release 2.67.1 Build 170626

--- application/config/version.php
+++ application/config/version.php
@@ -12,11 +12,11 @@
  *150413
  */
 
-$config['versionnumber'] = '2.67.0';
+$config['versionnumber'] = '2.67.1';
 $config['dbversionnumber'] = 263;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
-$config['assetsversionnumber'] = '2670';
+$config['assetsversionnumber'] = '2671';
 return $config;
 
 ?>
