53aedd32b5d2aa974dffc2f3e0a72e3692eb6ce4
Tõnis Ormisson <tonis@andmemasin.eu>
2017-07-04 16:22:03 +0300

    DEV: clean out survey->surveyinfo 1

--- application/models/Survey.php
+++ application/models/Survey.php
@@ -89,11 +89,11 @@ use \ls\pluginmanager\PluginEvent;
  * @property integer $countPartialAnswers
  * @property integer $countTotalAnswers
  * @property array $surveyinfo
- * @property string $localizedTitle Survey Title in current language
  * @property SurveyLanguageSetting $currentLanguageSettings Survey languagesettings in currently active language
  * @property string creationDate Creation date formatted according to user format
  * @property string startDateFormatted Start date formatted according to user format
  * @property string expiryDateFormatted Expiry date formatted according to user format
+ * @property string[] $allLanguages All survey languages
  */
 class Survey extends LSActiveRecord
 {
