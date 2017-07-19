b9fe83cd8482dd82543fb170f243c524d61d791d
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2017-07-19 12:44:39 +0200

    Dev: Correct db version in notification

--- application/helpers/update/updatedb_helper.php
+++ application/helpers/update/updatedb_helper.php
@@ -323,7 +323,7 @@ function db_upgrade_all($iOldDBVersion, $bSilent=false) {
 
     Notification::broadcast(array(
         'title' => gT('Database update'),
-        'message' => sprintf(gT('The database has been updated from version %s to version %s.'), $iOldDBVersion, '300')
+        'message' => sprintf(gT('The database has been updated from version %s to version %s.'), $iOldDBVersion, '304')
     ), $superadmins);
 
     fixLanguageConsistencyAllSurveys();
