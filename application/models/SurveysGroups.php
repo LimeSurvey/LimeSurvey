dad45f7ab07dc5adf688634fbd7e8fe1111e77e5
LouisGac <louis.gac@limesurvey.org>
2017-07-21 11:52:38 +0200

    Dev: added relation "parentgroup" in SurveysGroups model

--- application/models/SurveysGroups.php
+++ application/models/SurveysGroups.php
@@ -52,6 +52,7 @@ class SurveysGroups extends CActiveRecord
         // NOTE: you may need to adjust the relation name and the related
         // class name for the relations automatically generated below.
         return array(
+            'parentgroup' => array(self::BELONGS_TO, 'SurveysGroups', array('parent_id' => 'gsid'), 'together' => true),
         );
     }
 
