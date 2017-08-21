c04eeffce714c62020939204d26a309d1148d6e6
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2017-08-21 15:12:45 +0200

    Dev: EM unit test (WIP)

--- tests/helpers/ExpressionCoreHelperTest.php
+++ tests/helpers/ExpressionCoreHelperTest.php
@@ -17,7 +17,7 @@ class ExpressionManagerCoreTest extends TestBaseClass
     public function testCompareNumberAndEmptyString()
     {
         $em = new \ExpressionManager();
-
+        /*
         $number = [
             0 => '3',
             1 => 2,
@@ -42,5 +42,60 @@ class ExpressionManagerCoreTest extends TestBaseClass
         $this->assertTrue($noErrors);
 
         $result = $em->RDP_StackPop();
+
+        $em->RDP_StackPush($number);
+        $em->RDP_StackPush($dqString);
+        $em->RDP_StackPush($compare);
+        $em->SetJsVarsUsed([]);
+         */
+
+        $limeEm = \LimeExpressionManager::singleton();
+        $limeEm->setKnownVars(
+            [
+                '563168X136X5376' => [
+                ]
+            ]
+        );
+
+        $em->RDP_Evaluate('((563168X136X5376.NAOK >= " "))');
+        $result = $em->GetResult();
+        echo '<pre>'; var_dump($result); echo '</pre>';
+        $errors = $em->RDP_GetErrors();
+        echo '<pre>'; var_dump($errors); echo '</pre>';
+        $js = $em->GetJavaScriptEquivalentOfExpression();
+
+        $js = str_replace('"', "'", $js);
+        echo $js;
+
+        $output = [];
+        exec(
+            sprintf(
+                'node -p "%s"',
+                $js
+            ),
+            $output
+        );
+        echo '<pre>'; var_dump($output); echo '</pre>';
+
+    }
+
+    /**
+     * 
+     */
+    public function testGeneratedJavascript()
+    {
+        $pageInfo = [
+            'qid' => '5377',
+            'gseq' => 0,
+            'eqn' => '((563168X136X5376.NAOK >= \" \"))',
+            'result' => false,
+            'numJsVars' => 1,
+            'relevancejs' => '(((LEMval(\'563168X136X5376.NAOK\')  >= \" \")))',
+            'relevanceVars' => 'java563168X136X5376',
+            'jsResultVar' => 'java563168X136X5377',
+            'type' => 'N',
+            'hidden' => false,
+            'hasErrors' => false
+        ];
     }
 }
