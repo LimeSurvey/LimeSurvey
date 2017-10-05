231e375e21847abbe418b0df3506cd69383b43ff
Tõnis Ormisson <tonis@andmemasin.eu>
2017-10-05 13:43:54 +0300

    Dev: SQL params 2

--- application/helpers/expressions/em_manager_helper.php
+++ application/helpers/expressions/em_manager_helper.php
@@ -5525,11 +5525,14 @@
                         $cSave->set_answer_time();
                     }
 
-                    if ($finished)
-                    {
+                    if ($finished) {
                         // Delete the save control record if successfully finalize the submission
-                        $query = "DELETE FROM {{saved_control}} where srid=".$_SESSION[$this->sessid]['srid'].' and sid='.$this->sid;
-                        Yii::app()->db->createCommand($query)->execute();
+                        $params =[':srid'=>$_SESSION[$this->sessid]['srid'],':sid'=>$this->sid];
+                        $savedControl = SavedControl::model()
+                            ->findByAttributes(['srid'=>':srid','sid'=>':sid'],'',$params);
+                        if($savedControl){
+                            $savedControl->delete();
+                        }
 
                         if (($this->debugLevel & LEM_DEBUG_VALIDATION_SUMMARY) == LEM_DEBUG_VALIDATION_SUMMARY) {
                             $message .= ';<br />'.$query;
