afe1019f4d7a90c5948a0c4f771a6aa08577e9cc
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2017-10-16 13:59:26 +0200

    Dev: Add test class for TemplateConfiguration

---
+++ tests/models/TemplateConfigurationTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace ls\tests;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @since 2017-06-13
+ * @group tempconf
+ */
+class TemplateConfigurationTest extends TestBaseClass
+{
+
+    /**
+     * Issue #12795.
+     */
+    public function testCopyMinimalTemplate()
+    {
+        //$tempConf = new \TemplateConfiguration();
+        //$tempConf->prepareTemplateRendering();
+    }
+}
