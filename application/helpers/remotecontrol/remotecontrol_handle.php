31fe480fd468c350d257118eac375d1bda785db7
gekkedev <17025257+gekkedev@users.noreply.github.com>
2017-10-18 12:18:55 +0200

    enabled optional LDAP user login via RC-API (incomplete)

--- application/helpers/remotecontrol/remotecontrol_handle.php
+++ application/helpers/remotecontrol/remotecontrol_handle.php
@@ -36,11 +36,12 @@ class remotecontrol_handle
     * @access public
     * @param string $username
     * @param string $password
+    * @param bool $ldap
     * @return string|array
     */
-    public function get_session_key($username, $password)
+    public function get_session_key($username, $password, $ldap = false)
     {
-        if ($this->_doLogin($username, $password))
+        if ($this->_doLogin($username, $password, $ldap))
         {
             $this->_jumpStartSession($username);
             $sSessionKey = randomChars(32);
@@ -2801,9 +2802,14 @@ class remotecontrol_handle
     * @param string $sPassword password
     * @return bool
     */
-    protected function _doLogin($sUsername, $sPassword)
+    protected function _doLogin($sUsername, $sPassword, $byLDAP)
     {
-        $identity = new UserIdentity(sanitize_user($sUsername), $sPassword);
+        if ($byLDAP) {
+            $identity = new LSUserIdentity(sanitize_user($sUsername), $sPassword);
+            $identity->setPlugin("AuthLDAP");
+        } else {
+            $identity = new UserIdentity(sanitize_user($sUsername), $sPassword);
+        }
 
         if (!$identity->authenticate())
         {
