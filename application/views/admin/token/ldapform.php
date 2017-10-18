aa963fc5848753d3d1642060032779829a49f0f1
Carsten Schmitz <carsten.schmitz@limesurvey.org>
2017-10-18 11:41:57 +0200

    Dev Small HTML fix

--- application/views/admin/token/ldapform.php
+++ application/views/admin/token/ldapform.php
@@ -75,7 +75,7 @@
 
             <!-- Note -->
             <div class="alert alert-info alert-dismissible" role="alert">
-                <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                 <strong><?php echo eT("Note:"); ?></strong> <?php eT("LDAP queries are defined by the administrator in the configuration file /application/config/ldap.php ."); ?>
             </div>
 
