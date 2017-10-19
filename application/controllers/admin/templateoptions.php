a7be4ad7bb50dcd355c95bf28813eb440fa4f232
LouisGac <louis.gac@limesurvey.org>
2017-10-19 12:19:52 +0200

    Dev: permissions and redirections

--- application/controllers/admin/templateoptions.php
+++ application/controllers/admin/templateoptions.php
@@ -30,13 +30,15 @@ class templateoptions  extends Survey_Common_Action
      */
     public function view($id)
     {
-        if (!Permission::model()->hasGlobalPermission('templates','read')){
-            Yii::app()->session['flashmessage'] =gT('Access denied!');
+        if ( Permission::model()->hasGlobalPermission('templates','read')){
+            $this->_renderWrappedTemplate('templateoptions', 'read', array(
+                'model'=>$this->loadModel($id),
+            ));
+        }else{
+            Yii::app()->setFlashMessage(gT("We are sorry but you don't have permissions to do this."),'error');
             $this->getController()->redirect(App()->createUrl("/admin"));
         }
-        $this->_renderWrappedTemplate('templateoptions', 'read', array(
-            'model'=>$this->loadModel($id),
-        ));
+
     }
 
     /**
@@ -45,34 +47,192 @@ class templateoptions  extends Survey_Common_Action
      */
     public function create()
     {
-        if (!Permission::model()->hasGlobalPermission('templates','update')){
-            die('No permission');
+        if ( Permission::model()->hasGlobalPermission('templates','update')){
+            $model = new TemplateOptions;
+
+            if(isset($_POST['TemplateOptions'])){
+                $model->attributes=$_POST['TemplateOptions'];
+                if($model->save()){
+                    $this->getController()->redirect(array('admin/templateoptions/sa/update/id/'.$model->id));
+                }
+            }
+
+            $this->render('create',array(
+                'model'=>$model,
+            ));
+        }else{
+            Yii::app()->setFlashMessage(gT("We are sorry but you don't have permissions to do this."),'error');
+            $this->getController()->redirect(array("admin/templateoptions"));
         }
-
-        $model = new TemplateOptions;
-
-        // Uncomment the following line if AJAX validation is needed
-        // $this->performAjaxValidation($model);
-
-        if(isset($_POST['TemplateOptions']))
-        {
-            $model->attributes=$_POST['TemplateOptions'];
-            if($model->save())
-                $this->getController()->redirect(array('admin/templateoptions/sa/update/id/'.$model->id));
-        }
-
-        $this->render('create',array(
-            'model'=>$model,
-        ));
     }
 
 
-    private function _updateCommon($model,$sid=null){
+    /**
+     * Updates a particular model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param integer $id the ID of the model to be updated
+     */
+    public function update($id)
+    {
+        if ( Permission::model()->hasGlobalPermission('templates', 'update')){
+            $model = $this->loadModel($id);
+
+            if(isset($_POST['TemplateConfiguration'])){
+                $model->attributes=$_POST['TemplateConfiguration'];
+                if($model->save())
+                    $this->getController()->redirect(array('admin/templateoptions/sa/update/id/'.$model->id));
+            }
+
+            $this->_updateCommon($model);
+        }else{
+            Yii::app()->setFlashMessage(gT("We are sorry but you don't have permissions to do this."),'error');
+            $this->getController()->redirect(Yii::app()->getController()->createUrl("/admin/templateoptions"));
+        }
+    }
+
+    /**
+     * Updates a particular model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param integer $id the ID of the model to be updated
+     */
+    public function updatesurvey($sid)
+    {
+        if ( Permission::model()->hasGlobalPermission('templates', 'update')){
+            $model = TemplateConfiguration::getInstance(null, null, $sid);
+
+            if(isset($_POST['TemplateConfiguration'])){
+                $model->attributes=$_POST['TemplateConfiguration'];
+                if($model->save())
+                    $this->getController()->redirect(Yii::app()->getController()->createUrl("/admin/templateoptions/sa/updatesurvey",['surveyid'=>$sid,'sid'=>$sid]));
+            }
+
+            $this->_updateCommon($model, $sid);
+        }else{
+            Yii::app()->setFlashMessage(gT("We are sorry but you don't have permissions to do this."),'error');
+            $this->getController()->redirect(Yii::app()->getController()->createUrl("/admin/templateoptions/sa/updatesurvey",['surveyid'=>$sid,'sid'=>$sid]));
+        }
+    }
+
+    /**
+     * Updates a particular model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param integer $id the ID of the model to be updated
+     */
+    public function updatesurveygroup($gsid)
+    {
+        if ( Permission::model()->hasGlobalPermission('templates', 'update')){
+            $model = TemplateConfiguration::getInstance(null, $gsid);
+
+            if(isset($_POST['TemplateConfiguration'])){
+                $model->attributes=$_POST['TemplateConfiguration'];
+                if($model->save())
+                    $this->getController()->redirect(Yii::app()->getController()->createUrl("/admin/surveysgroups/sa/update/",['id'=>$gsid]));
+            }
+
+            $this->_updateCommon($model, $sid);
+        }else{
+            Yii::app()->setFlashMessage(gT("We are sorry but you don't have permissions to do this."),'error');
+            $this->getController()->redirect(Yii::app()->getController()->createUrl("/admin/surveysgroups/sa/update/",['id'=>$gsid]));
+        }
+    }
+
+    /**
+     * Lists all models.
+     */
+    public function index()
+    {
+        if ( Permission::model()->hasGlobalPermission('templates', 'read') ){
+            $model          = new TemplateConfiguration();
+            $aData          = array();
+            $aData['model'] = $model;
+            $this->_renderWrappedTemplate('templateoptions', 'index', $aData);
+        }else{
+            Yii::app()->setFlashMessage(gT("We are sorry but you don't have permissions to do this."),'error');
+            $this->getController()->redirect(App()->createUrl("/admin"));
+        }
+    }
+
+    /**
+     * Manages all models.
+     */
+    public function admin()
+    {
+        if ( Permission::model()->hasGlobalPermission('templates','read')){
+            $model=new TemplateOptions('search');
+            $model->unsetAttributes();  // clear any default values
+            if(isset($_GET['TemplateOptions'])){
+                $model->attributes=$_GET['TemplateOptions'];
+            }
+
+            $this->render('admin',array(
+                'model'=>$model,
+            ));
+        }else{
+            Yii::app()->setFlashMessage(gT("We are sorry but you don't have permissions to do this."),'error');
+            $this->getController()->redirect(App()->createUrl("/admin"));
+        }
+    }
+
+    /**
+     * Returns the data model based on the primary key given in the GET variable.
+     * If the data model is not found, an HTTP exception will be raised.
+     * @param integer $id the ID of the model to be loaded
+     * @return TemplateOptions the loaded model
+     * @throws CHttpException
+     */
+    public function loadModel($id)
+    {
+        $model=TemplateConfiguration::model()->findByPk($id);
+        if($model===null){
+            throw new CHttpException(404,'The requested page does not exist.');
+        }
+
+        return $model;
+    }
 
 
-        // Uncomment the following line if AJAX validation is needed
-        // $this->performAjaxValidation($model);
+    public function importManifest($templatename)
+    {
+        if ( Permission::model()->hasGlobalPermission('templates', 'update')){
+            TemplateManifest::importManifest($templatename);
+            $this->getController()->redirect(array("admin/templateoptions"));
+        }else{
+            Yii::app()->setFlashMessage(gT("We are sorry but you don't have permissions to do this."),'error');
+            $this->getController()->redirect(array("admin/templateoptions"));
+        }
 
+    }
+
+    public function uninstall($templatename)
+    {
+        if ( Permission::model()->hasGlobalPermission('templates','update')){
+            if (!Template::hasInheritance($templatename)){
+                TemplateConfiguration::uninstall($templatename);
+            }else{
+                Yii::app()->setFlashMessage(sprintf(gT("You can't uninstall template '%s' because some templates inherit from it."), $templatename), 'error');
+            }
+        }else{
+            Yii::app()->setFlashMessage(gT("We are sorry but you don't have permissions to do this."),'error');
+        }
+
+        $this->getController()->redirect(array("admin/templateoptions"));
+    }
+
+    /**
+     * Performs the AJAX validation.
+     * @param TemplateOptions $model the model to be validated
+     */
+    protected function performAjaxValidation($model)
+    {
+        if(isset($_POST['ajax']) && $_POST['ajax']==='template-options-form'){
+            echo CActiveForm::validate($model);
+            Yii::app()->end();
+        }
+    }
+
+
+    private function _updateCommon($model,$sid=null)
+    {
         $oModelWithInheritReplacement = TemplateConfiguration::model()->findByPk($model->id);
         $templateOptionPage           = $oModelWithInheritReplacement->optionPage;
 
@@ -88,161 +248,5 @@ class templateoptions  extends Survey_Common_Action
         }
 
         $this->_renderWrappedTemplate('templateoptions', 'update', $aData);
-    }
-
-    /**
-     * Updates a particular model.
-     * If update is successful, the browser will be redirected to the 'view' page.
-     * @param integer $id the ID of the model to be updated
-     */
-    public function update($id)
-    {
-        if (! Permission::model()->hasGlobalPermission('templates', 'update')){
-            Yii::app()->setFlashMessage(gT('Access denied!'),'error');
-            $this->getController()->redirect(Yii::app()->getController()->createUrl("/admin/templateoptions"));
-        }
-        $model = $this->loadModel($id);
-
-
-        if(isset($_POST['TemplateConfiguration'])){
-            $model->attributes=$_POST['TemplateConfiguration'];
-            if($model->save())
-                $this->getController()->redirect(array('admin/templateoptions/sa/update/id/'.$model->id));
-        }
-
-        $this->_updateCommon($model);
-
-    }
-
-    /**
-     * Updates a particular model.
-     * If update is successful, the browser will be redirected to the 'view' page.
-     * @param integer $id the ID of the model to be updated
-     */
-    public function updatesurvey($sid)
-    {
-        if (! Permission::model()->hasGlobalPermission('templates', 'update')){
-            Yii::app()->setFlashMessage(gT('Access denied!'),'error');
-            $this->getController()->redirect(Yii::app()->getController()->createUrl("/admin/templateoptions/sa/updatesurvey",['surveyid'=>$sid,'sid'=>$sid]));
-        }
-
-        $model = TemplateConfiguration::getInstance(null, null, $sid);
-
-
-        if(isset($_POST['TemplateConfiguration'])){
-            $model->attributes=$_POST['TemplateConfiguration'];
-            if($model->save())
-                $this->getController()->redirect(Yii::app()->getController()->createUrl("/admin/templateoptions/sa/updatesurvey",['surveyid'=>$sid,'sid'=>$sid]));
-        }
-
-        $this->_updateCommon($model, $sid);
-    }
-
-    /**
-     * Updates a particular model.
-     * If update is successful, the browser will be redirected to the 'view' page.
-     * @param integer $id the ID of the model to be updated
-     */
-    public function updatesurveygroup($gsid)
-    {
-        if (! Permission::model()->hasGlobalPermission('templates', 'update')){
-            Yii::app()->setFlashMessage(gT('Access denied!'),'error');
-            $this->getController()->redirect(Yii::app()->getController()->createUrl("/admin/surveysgroups/sa/update/",['id'=>$gsid]));
-        }
-
-        $model = TemplateConfiguration::getInstance(null, $gsid);
-
-
-        if(isset($_POST['TemplateConfiguration'])){
-            $model->attributes=$_POST['TemplateConfiguration'];
-            if($model->save())
-                $this->getController()->redirect(Yii::app()->getController()->createUrl("/admin/surveysgroups/sa/update/",['id'=>$gsid]));
-        }
-
-        $this->_updateCommon($model, $sid);
-    }
-
-    /**
-     * Lists all models.
-     */
-    public function index()
-    {
-        if (! Permission::model()->hasGlobalPermission('templates', 'read') ){
-            Yii::app()->session['flashmessage'] =gT('Access denied!');
-            $this->getController()->redirect(App()->createUrl("/admin"));
-        }
-
-        $aData = array();
-
-        //$model = new TemplateConfiguration('search');
-        $model = new TemplateConfiguration();
-
-        //$aFtpTemplates = ;
-
-        $aData['model'] = $model;
-        $this->_renderWrappedTemplate('templateoptions', 'index', $aData);
-    }
-
-    /**
-     * Manages all models.
-     */
-    public function admin()
-    {
-        $model=new TemplateOptions('search');
-
-        $model->unsetAttributes();  // clear any default values
-        if(isset($_GET['TemplateOptions']))
-            $model->attributes=$_GET['TemplateOptions'];
-
-        $this->render('admin',array(
-            'model'=>$model,
-        ));
-    }
-
-    /**
-     * Returns the data model based on the primary key given in the GET variable.
-     * If the data model is not found, an HTTP exception will be raised.
-     * @param integer $id the ID of the model to be loaded
-     * @return TemplateOptions the loaded model
-     * @throws CHttpException
-     */
-    public function loadModel($id)
-    {
-        $model=TemplateConfiguration::model()->findByPk($id);
-        if($model===null)
-            throw new CHttpException(404,'The requested page does not exist.');
-
-
-        return $model;
-    }
-
-
-    public function importManifest($templatename)
-    {
-        TemplateManifest::importManifest($templatename);
-        $this->getController()->redirect(array("admin/templateoptions"));
-    }
-
-    public function uninstall($templatename)
-    {
-        if (!Template::hasInheritance($templatename)){
-            TemplateConfiguration::uninstall($templatename);
-        }else{
-            Yii::app()->setFlashMessage(sprintf(gT("You can't uninstall template '%s' because some template inherit from it."), $templatename), 'error');
-        }
-        $this->getController()->redirect(array("admin/templateoptions"));
-    }
-
-    /**
-     * Performs the AJAX validation.
-     * @param TemplateOptions $model the model to be validated
-     */
-    protected function performAjaxValidation($model)
-    {
-        if(isset($_POST['ajax']) && $_POST['ajax']==='template-options-form')
-        {
-            echo CActiveForm::validate($model);
-            Yii::app()->end();
-        }
     }
 }
