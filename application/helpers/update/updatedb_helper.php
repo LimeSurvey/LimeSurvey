43290a21f1858f0eda40089fb27e829f4741dbb5
markusfluer <fluer.markus@googlemail.com>
2017-10-25 10:58:32 +0200

    Fixed updatedb for mssql & pgsql

--- application/helpers/update/updatedb_helper.php
+++ application/helpers/update/updatedb_helper.php
@@ -445,7 +445,7 @@ function db_upgrade_all($iOldDBVersion, $bSilent=false) {
 
             $oDB->createCommand()->update('{{template_configuration}}', 
                 array('packages_to_load'=>'["pjax"]'),
-                'templates_name="default" OR templates_name="material"'
+                "templates_name='default' OR templates_name='material'"
             );
 
             $oDB->createCommand()->update('{{settings_global}}',array('stg_value'=>315),"stg_name='DBVersion'");
