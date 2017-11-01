ddf1acbefea136a1c2af6ae09823e48b349b07f1
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2017-11-01 13:52:49 +0100

    Dev: Update composer to --no-dev version
    Dev: To remove packages needed for testing, run:
    Dev: composer install --no-dev --optimize-autoloader
    Dev: To include packages needed for testing, run:
    Dev: composer install

--- third_party/composer/autoload_real.php
+++ third_party/composer/autoload_real.php
@@ -23,10 +23,6 @@ class ComposerAutoloaderInitddb1a145e450f862353420acc5153e40
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInitddb1a145e450f862353420acc5153e40', 'loadClassLoader'));
 
-        $includePaths = require __DIR__ . '/include_paths.php';
-        array_push($includePaths, get_include_path());
-        set_include_path(join(PATH_SEPARATOR, $includePaths));
-
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION');
         if ($useStaticLoader) {
             require_once __DIR__ . '/autoload_static.php';
@@ -51,24 +47,6 @@ class ComposerAutoloaderInitddb1a145e450f862353420acc5153e40
 
         $loader->register(true);
 
-        if ($useStaticLoader) {
-            $includeFiles = Composer\Autoload\ComposerStaticInitddb1a145e450f862353420acc5153e40::$files;
-        } else {
-            $includeFiles = require __DIR__ . '/autoload_files.php';
-        }
-        foreach ($includeFiles as $fileIdentifier => $file) {
-            composerRequireddb1a145e450f862353420acc5153e40($fileIdentifier, $file);
-        }
-
         return $loader;
-    }
-}
-
-function composerRequireddb1a145e450f862353420acc5153e40($fileIdentifier, $file)
-{
-    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        require $file;
-
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
