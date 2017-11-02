e835192043c0722dc3c4bb6f2bef9eaa37676359
markusfluer <fluer.markus@googlemail.com>
2017-11-02 15:11:35 +0100

    Dev: updated composer files for production

--- third_party/composer/autoload_real.php
+++ third_party/composer/autoload_real.php
@@ -23,10 +23,6 @@ class ComposerAutoloaderInitddb1a145e450f862353420acc5153e40
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInitddb1a145e450f862353420acc5153e40', 'loadClassLoader'));
 
-        $includePaths = require __DIR__ . '/include_paths.php';
-        array_push($includePaths, get_include_path());
-        set_include_path(join(PATH_SEPARATOR, $includePaths));
-
         $map = require __DIR__ . '/autoload_namespaces.php';
         foreach ($map as $namespace => $path) {
             $loader->set($namespace, $path);
@@ -44,20 +40,6 @@ class ComposerAutoloaderInitddb1a145e450f862353420acc5153e40
 
         $loader->register(true);
 
-        $includeFiles = require __DIR__ . '/autoload_files.php';
-        foreach ($includeFiles as $fileIdentifier => $file) {
-            composerRequireddb1a145e450f862353420acc5153e40($fileIdentifier, $file);
-        }
-
         return $loader;
-    }
-}
-
-function composerRequireddb1a145e450f862353420acc5153e40($fileIdentifier, $file)
-{
-    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        require $file;
-
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
