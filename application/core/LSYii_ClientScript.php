83a717eb0bb788f797ecc7d8ff62e68b20a49fb9
LouisGac <louis.gac@limesurvey.org>
2017-11-02 14:05:35 +0100

    Dev: some cleaning

--- application/core/LSYii_ClientScript.php
+++ application/core/LSYii_ClientScript.php
@@ -294,78 +294,78 @@ class LSYii_ClientScript extends CClientScript {
     }
 
     /**
-	 * Inserts the scripts in the head section.
-	 * @param string $output the output to be inserted with scripts.
-	 */
-	public function renderHead(&$output)
-	{
-		$html='';
-		foreach($this->metaTags as $meta)
-			$html.=CHtml::metaTag($meta['content'],null,null,$meta)."\n";
-		foreach($this->linkTags as $link)
-			$html.=CHtml::linkTag(null,null,null,null,$link)."\n";
-		foreach($this->cssFiles as $url=>$media)
-			$html.=CHtml::cssFile($url,$media)."\n";
-		foreach($this->css as $css)
-			$html.=CHtml::css($css[0],$css[1])."\n";
-		if($this->enableJavaScript)
-		{
-			if(isset($this->scriptFiles[self::POS_HEAD]))
-			{
-				foreach($this->scriptFiles[self::POS_HEAD] as $scriptFileValueUrl=>$scriptFileValue)
-				{
+     * Inserts the scripts in the head section.
+     * @param string $output the output to be inserted with scripts.
+     */
+    public function renderHead(&$output)
+    {
+        $html='';
+        foreach($this->metaTags as $meta)
+            $html.=CHtml::metaTag($meta['content'],null,null,$meta)."\n";
+        foreach($this->linkTags as $link)
+            $html.=CHtml::linkTag(null,null,null,null,$link)."\n";
+        foreach($this->cssFiles as $url=>$media)
+            $html.=CHtml::cssFile($url,$media)."\n";
+        foreach($this->css as $css)
+            $html.=CHtml::css($css[0],$css[1])."\n";
+        if($this->enableJavaScript)
+        {
+            if(isset($this->scriptFiles[self::POS_HEAD]))
+            {
+                foreach($this->scriptFiles[self::POS_HEAD] as $scriptFileValueUrl=>$scriptFileValue)
+                {
                     if(is_array($scriptFileValue))
                     {
                         $scriptFileValue['class'] = isset($scriptFileValue['class']) ? $scriptFileValue['class']." headScriptTag" : "headScriptTag";
-						$html.=CHtml::scriptFile($scriptFileValueUrl,$scriptFileValue)."\n";
+                        $html.=CHtml::scriptFile($scriptFileValueUrl,$scriptFileValue)."\n";
                     }
                     else
                     {
-						$html.=CHtml::scriptFile($scriptFileValueUrl, array('class' => 'headScriptTag'))."\n";
+                        $html.=CHtml::scriptFile($scriptFileValueUrl, array('class' => 'headScriptTag'))."\n";
                     }
-				}
-			}
+                }
+            }
 
-			if(isset($this->scripts[self::POS_HEAD]))
-				$html.=$this->renderScriptBatch($this->scripts[self::POS_HEAD]);
-		}
-
-		if($html!=='')
-		{
-			$count=0;
-			$output=preg_replace('/(<title\b[^>]*>|<\\/head\s*>)/is','<###head###>$1',$output,1,$count);
-			if($count)
-				$output=str_replace('<###head###>',$html,$output);
-			else
-				$output=$html.$output;
-		}
-	}
-
-	/**
-	 * Inserts the scripts at the beginning of the body section.
-     * This is overwriting the core method and is exactly the same except the marked parts
-	 * @param string $output the output to be inserted with scripts.
-	 */
-	public function renderBodyBegin(&$output)
-	{
-		$html='';
-		if(isset($this->scriptFiles[self::POS_BEGIN]))
-		{
-			foreach($this->scriptFiles[self::POS_BEGIN] as $scriptFileUrl=>$scriptFileValue)
-			{
-				if(is_array($scriptFileValue))
-					$html.=CHtml::scriptFile($scriptFileUrl,$scriptFileValue)."\n";
-				else
-					$html.=CHtml::scriptFile($scriptFileUrl)."\n";
-			}
-		}
-		if(isset($this->scripts[self::POS_BEGIN]))
-        {   
-			$html.=$this->renderScriptBatch($this->scripts[self::POS_BEGIN]);
+            if(isset($this->scripts[self::POS_HEAD]))
+                $html.=$this->renderScriptBatch($this->scripts[self::POS_HEAD]);
         }
 
-		if($html!=='')
-		{
+        if($html!=='')
+        {
+            $count=0;
+            $output=preg_replace('/(<title\b[^>]*>|<\\/head\s*>)/is','<###head###>$1',$output,1,$count);
+            if($count)
+                $output=str_replace('<###head###>',$html,$output);
+            else
+                $output=$html.$output;
+        }
+    }
+
+    /**
+     * Inserts the scripts at the beginning of the body section.
+     * This is overwriting the core method and is exactly the same except the marked parts
+     * @param string $output the output to be inserted with scripts.
+     */
+    public function renderBodyBegin(&$output)
+    {
+        $html='';
+        if(isset($this->scriptFiles[self::POS_BEGIN]))
+        {
+            foreach($this->scriptFiles[self::POS_BEGIN] as $scriptFileUrl=>$scriptFileValue)
+            {
+                if(is_array($scriptFileValue))
+                    $html.=CHtml::scriptFile($scriptFileUrl,$scriptFileValue)."\n";
+                else
+                    $html.=CHtml::scriptFile($scriptFileUrl)."\n";
+            }
+        }
+        if(isset($this->scripts[self::POS_BEGIN]))
+        {
+            $html.=$this->renderScriptBatch($this->scripts[self::POS_BEGIN]);
+        }
+
+        if($html!=='')
+        {
             $count=0;
             if(preg_match('/<###begin###>/', $output)){
                 $count=1;
@@ -379,22 +379,22 @@ class LSYii_ClientScript extends CClientScript {
             else
             {
                 $output=$html.$output;
-		    }
-        } 
-        else 
+            }
+        }
+        else
         {
             $output = preg_replace('/<###begin###>/','',$output,1);
         }
-	}
+    }
 
     /**
-	 * Inserts the scripts at the end of the body section.
+     * Inserts the scripts at the end of the body section.
      * This is overwriting the core method and is exactly the same except the marked parts
-	 * @param string $output the output to be inserted with scripts.
-	 */
-	public function renderBodyEnd(&$output)
-	{
-		if(!isset($this->scriptFiles[self::POS_END]) && !isset($this->scripts[self::POS_END])
+     * @param string $output the output to be inserted with scripts.
+     */
+    public function renderBodyEnd(&$output)
+    {
+        if(!isset($this->scriptFiles[self::POS_END]) && !isset($this->scripts[self::POS_END])
             && !isset($this->scripts[self::POS_READY]) && !isset($this->scripts[self::POS_LOAD]))
         {
             str_replace('<###end###>','',$output);
@@ -406,43 +406,43 @@ class LSYii_ClientScript extends CClientScript {
             $fullPage=1;
         else
             $output=preg_replace('/(<\\/body\s*>)/is','<###end###>$1',$output,1,$fullPage);
-        
-		$html='';
-		if(isset($this->scriptFiles[self::POS_END]))
-		{
-			foreach($this->scriptFiles[self::POS_END] as $scriptFileUrl=>$scriptFileValue)
-			{
-				if(is_array($scriptFileValue))
-					$html.=CHtml::scriptFile($scriptFileUrl,$scriptFileValue)."\n";
-				else
-					$html.=CHtml::scriptFile($scriptFileUrl)."\n";
-			}
-		}
-		$scripts=isset($this->scripts[self::POS_END]) ? $this->scripts[self::POS_END] : array();
 
-		if(isset($this->scripts[self::POS_READY]))
-		{
-			if($fullPage)
-				$scripts[]="jQuery(function($) {\n".implode("\n",$this->scripts[self::POS_READY])."\n});";
-			else
-				$scripts[]=implode("\n",$this->scripts[self::POS_READY]);
-		}
-		if(isset($this->scripts[self::POS_LOAD]))
-		{
-			if($fullPage) //This part is different to reflect the changes needed in the backend by the pjax loading of pages
-				$scripts[]="jQuery(document).on('load pjax:complete',function() {\n".implode("\n",$this->scripts[self::POS_LOAD])."\n});";
-			else
-				$scripts[]=implode("\n",$this->scripts[self::POS_LOAD]);
-		}
+        $html='';
+        if(isset($this->scriptFiles[self::POS_END]))
+        {
+            foreach($this->scriptFiles[self::POS_END] as $scriptFileUrl=>$scriptFileValue)
+            {
+                if(is_array($scriptFileValue))
+                    $html.=CHtml::scriptFile($scriptFileUrl,$scriptFileValue)."\n";
+                else
+                    $html.=CHtml::scriptFile($scriptFileUrl)."\n";
+            }
+        }
+        $scripts=isset($this->scripts[self::POS_END]) ? $this->scripts[self::POS_END] : array();
+
+        if(isset($this->scripts[self::POS_READY]))
+        {
+            if($fullPage)
+                $scripts[]="jQuery(function($) {\n".implode("\n",$this->scripts[self::POS_READY])."\n});";
+            else
+                $scripts[]=implode("\n",$this->scripts[self::POS_READY]);
+        }
+        if(isset($this->scripts[self::POS_LOAD]))
+        {
+            if($fullPage) //This part is different to reflect the changes needed in the backend by the pjax loading of pages
+                $scripts[]="jQuery(document).on('load pjax:complete',function() {\n".implode("\n",$this->scripts[self::POS_LOAD])."\n});";
+            else
+                $scripts[]=implode("\n",$this->scripts[self::POS_LOAD]);
+        }
 
         //All scripts are wrapped into a section to be able to reload them accordingly
-		if(!empty($scripts))
-        {   
-			$html.=$this->renderScriptBatch($scripts);
+        if(!empty($scripts))
+        {
+            $html.=$this->renderScriptBatch($scripts);
         }
 
 
-		if($fullPage)
+        if($fullPage)
         {
             $output=str_replace('<###end###>',$html,$output);
         }
@@ -451,31 +451,31 @@ class LSYii_ClientScript extends CClientScript {
             $output=$output.$html;
         }
     }
-    
+
     /**
-	 * Renders the registered scripts.
-	 * This method is called in {@link CController::render} when it finishes
-	 * rendering content. CClientScript thus gets a chance to insert script tags
-	 * at <code>head</code> and <code>body</code> sections in the HTML output.
-	 * @param string $output the existing output that needs to be inserted with script tags
-	 */
-	public function render(&$output)
-	{
-		if(!$this->hasScripts)
-			return;
+     * Renders the registered scripts.
+     * This method is called in {@link CController::render} when it finishes
+     * rendering content. CClientScript thus gets a chance to insert script tags
+     * at <code>head</code> and <code>body</code> sections in the HTML output.
+     * @param string $output the existing output that needs to be inserted with script tags
+     */
+    public function render(&$output)
+    {
+        if(!$this->hasScripts)
+            return;
 
-		$this->renderCoreScripts();
+        $this->renderCoreScripts();
 
-		if(!empty($this->scriptMap))
-			$this->remapScripts();
+        if(!empty($this->scriptMap))
+            $this->remapScripts();
 
-		$this->unifyScripts();
+        $this->unifyScripts();
 
-		$this->renderHead($output);
-		if($this->enableJavaScript)
-		{
-			$this->renderBodyBegin($output);
-			$this->renderBodyEnd($output);
-		}
-	}
+        $this->renderHead($output);
+        if($this->enableJavaScript)
+        {
+            $this->renderBodyBegin($output);
+            $this->renderBodyEnd($output);
+        }
+    }
 }
