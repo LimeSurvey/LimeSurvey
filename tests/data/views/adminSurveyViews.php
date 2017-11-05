bd9d31d612cebe313727ae35a16abedcdaaef4f7
Tõnis Ormisson <tonis@andmemasin.eu>
2017-11-05 12:12:31 +0200

    DEV: viewtests adminSurveyViews: add surveyTexts

--- tests/data/views/adminSurveyViews.php
+++ tests/data/views/adminSurveyViews.php
@@ -8,4 +8,5 @@ return [
     // NB the import_id is needed only for the first item OR if you need to change the survey you want to work with
     'surveySummary' =>['route'=>'survey/sa/view/surveyid/{SID}','import_id'=>'454287'],
     'surveyGeneralSettings' =>['route'=>'survey/sa/rendersidemenulink/subaction/generalsettings/surveyid/{SID}'],
+    'surveyTexts' =>['route'=>'survey/sa/rendersidemenulink/subaction/surveytexts/surveyid/{SID}'],
 ];
