b44ce2d45bb5e95ef0a31fa74d4807774ca09025
Tõnis Ormisson <tonis@andmemasin.eu>
2017-11-06 08:54:22 +0200

    revert composer (again)

--- third_party/composer/autoload_real.php
+++ third_party/composer/autoload_real.php
@@ -23,7 +23,7 @@ class ComposerAutoloaderInitddb1a145e450f862353420acc5153e40
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInitddb1a145e450f862353420acc5153e40', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION');
         if ($useStaticLoader) {
             require_once __DIR__ . '/autoload_static.php';
 
