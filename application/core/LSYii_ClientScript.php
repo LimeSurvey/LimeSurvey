a623266aee9529c86d3ddf13c06e5e68343b4d90
markusfluer <fluer.markus@googlemail.com>
2017-11-14 16:38:15 +0100

    Revert "Dev: update Pjax and added position postscript"
    This reverts commit d1bddef35cb242de1df84073775e52c2dea185c5.

--- application/core/LSYii_ClientScript.php
+++ application/core/LSYii_ClientScript.php
@@ -28,9 +28,6 @@
 
 class LSYii_ClientScript extends CClientScript {
 
-    
-    const POS_POSTSCRIPT=5;
-    
     /**
      * cssFiles is protected on CClientScript. It can be useful to access it for debugin purpose
      * @return array
@@ -480,8 +477,8 @@ class LSYii_ClientScript extends CClientScript {
      */
     public function renderBodyEnd(&$output)
     {
-        if(!isset($this->scriptFiles[self::POS_END]) && !isset($this->scripts[self::POS_READY]) 
-        && !isset($this->scripts[self::POS_LOAD]) && !isset($this->scripts[self::POS_POSTSCRIPT]))
+        if(!isset($this->scriptFiles[self::POS_END]) && !isset($this->scripts[self::POS_END])
+            && !isset($this->scripts[self::POS_READY]) && !isset($this->scripts[self::POS_LOAD]))
         {
             str_replace('<###end###>','',$output);
             return;
@@ -524,7 +521,7 @@ class LSYii_ClientScript extends CClientScript {
         if(isset($this->scripts[self::POS_POSTSCRIPT]))
         {
             if($fullPage) //This part is different to reflect the changes needed in the backend by the pjax loading of pages
-                $scripts[]="jQuery(document).on('ready pjax:scriptcomplete', function() {\nconsole.log('loading on scriptcomplete');\n".implode("\n",$this->scripts[self::POS_POSTSCRIPT])."\n});";
+                $scripts[]="jQuery(document).on('load pjax:scriptcomplete',function() {\nconsole.log('loading on scriptcomplete');\n".implode("\n",$this->scripts[self::POS_POSTSCRIPT])."\n});";
             else
                 $scripts[]=implode("\n",$this->scripts[self::POS_POSTSCRIPT]);
         }
