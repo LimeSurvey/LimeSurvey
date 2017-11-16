eb30b8cca342d94e09fe591efa46287808d6e795
LouisGac <louis.gac@limesurvey.org>
2017-11-16 13:37:08 +0100

    Revert "Dev: Show error message if template can't get files to load"
    This reverts commit 8618c66622a0105d895364f1790103e49e0b7054.

--- application/models/TemplateConfiguration.php
+++ application/models/TemplateConfiguration.php
@@ -539,7 +539,7 @@ class TemplateConfiguration extends TemplateConfig
      */
     protected function getFilesToLoad($oTemplate, $sType)
     {
-        if (empty($this->aFilesToLoad)) {
+        if (empty($this->aFilesToLoad)){
             $this->aFilesToLoad = array();
         }
 
@@ -547,32 +547,23 @@ class TemplateConfiguration extends TemplateConfig
         $jFiles = $oTemplate->$sField;
         $this->aFilesToLoad[$sType] = array();
 
-        if (!empty($jFiles)) {
+
+        if(!empty($jFiles)){
             $oFiles = json_decode($jFiles, true);
-            if ($oFiles) {
-                foreach ($oFiles as $action => $aFileList) {
-                    if (is_array($aFileList)) {
-                        if ($action == "add" || $action == "replace") {
+            foreach($oFiles as $action => $aFileList){
 
-                            // Specific inheritance of one of the value of the json array
-                            if ($aFileList[0] == 'inherit') {
-                                $aParentjFiles = (array) json_decode($oTemplate->getParentConfiguration->$sField);
-                                $aFileList = $aParentjFiles[$action];
-                            }
+                if ( is_array( $aFileList ) ){
+                    if ($action == "add" || $action == "replace"){
 
-                            $this->aFilesToLoad[$sType] = array_merge($this->aFilesToLoad[$sType], $aFileList);
+                        // Specific inheritance of one of the value of the json array
+                        if ($aFileList[0] == 'inherit'){
+                            $aParentjFiles = (array) json_decode($oTemplate->getParentConfiguration->$sField);
+                            $aFileList = $aParentjFiles[$action];
                         }
+
+                        $this->aFilesToLoad[$sType] = array_merge($this->aFilesToLoad[$sType], $aFileList);
                     }
                 }
-            } else {
-                Yii::app()->setFlashMessage(
-                    sprintf(
-                        gT('Could not load file(s) for field "%s", value "%s"'),
-                        $sField,
-                        $jFiles
-                    ),
-                    'error'
-                );
             }
 
         }
