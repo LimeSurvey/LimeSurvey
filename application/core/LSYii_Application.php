0fba4ad0e5b3de19a0e839a6d9c98bbfc4b5159d
Carsten Schmitz <carsten.schmitz@limesurvey.org>
2017-11-24 15:51:39 +0100

    Dev PHPDoc fix

--- application/core/LSYii_Application.php
+++ application/core/LSYii_Application.php
@@ -18,8 +18,9 @@
 require_once(dirname(dirname(__FILE__)).'/helpers/globals.php');
 
 /**
-* Implements global  config
+* Implements global config
 * @property CLogRouter $log Log router component.
+* @property string $language Returns the language that the user is using and the application should be targeted to.
 */
 class LSYii_Application extends CWebApplication
 {
