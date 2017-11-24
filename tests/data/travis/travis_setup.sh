421d1da6b71be4ab9c9f7e077d88f614d7e3266d
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2017-11-24 11:57:44 +0100

    Dev: Fix wrong geckodriver

--- tests/data/travis/travis_setup.sh
+++ tests/data/travis/travis_setup.sh
@@ -39,7 +39,7 @@ sudo service apache2 restart
 
 # Firefox headless.
 wget "https://selenium-release.storage.googleapis.com/3.7/selenium-server-standalone-3.7.1.jar"
-wget "https://github.com/mozilla/geckodriver/releases/download/v0.19.1/geckodriver-v0.19.1-arm7hf.tar.gz"
-tar xvzf geckodriver-v0.19.1-arm7hf.tar.gz
+wget "https://github.com/mozilla/geckodriver/releases/download/v0.19.1/geckodriver-v0.19.1-linux64.tar.gz"
+tar xvzf geckodriver-v0.19.1-linux64.tar.gz
 export MOZ_HEADLESS=1
 java -jar selenium-server-standalone-3.7.1.jar -enablePassThrough false > /dev/null &
