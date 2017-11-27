39073f4e4bb4c030e88f4681ff0dad1dae211bb5
Adam Zammit <adam.zammit@acspri.org.au>
2017-11-28 10:52:44 +1100

    Dev: Fix spacing

--- application/helpers/replacements_helper.php
+++ application/helpers/replacements_helper.php
@@ -230,12 +230,9 @@ function templatereplace($line, $replacements = array(), &$redata = array(), $de
         $iscompleted = $thissurvey['iscompleted'] = false;
     }
 
-    if (isset($surveyid))
-    {
+    if (isset($surveyid)) {
         $_quexmlpdf=CHtml::link(gT("Save as PDF"),array("/printanswers/view/surveyid/{$surveyid}/printableexport/quexmlpdf"),array('data-toggle'=>'tooltip','data-placement'=>'right','title'=>gT("Note: Print will not include items on this page")));
-    }
-    else
-    {
+    } else {
         $_quexmlpdf = "";
     }
 
