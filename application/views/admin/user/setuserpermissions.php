ab6dec28208f3c1d925d285b32f55c704b6fa51f
digitmaster <francesco.petrone@hotmail.com>
2017-12-06 23:01:17 +0100

    This Patch completes the LowercaseAuthLDAP plugin

--- application/views/admin/user/setuserpermissions.php
+++ application/views/admin/user/setuserpermissions.php
@@ -28,10 +28,25 @@
                </thead>
 
                 <!-- Permissions -->
-                <?php foreach($aBasePermissions as $sPermissionKey=>$aCRUDPermissions): ?>
+                <?php 
+		       // get the array with the installed plugin list
+                        $pluginlist=App()->getPluginManager()->getInstalledPlugins();
+                        //we will search for the first active plugin contraining a specific substring in name
+                        $pstrname='AuthLDAP';
+                        // looping array to get a plugin having a substring  match in name AND having active status
+                        function matchInstalledAndActive($needle,$haystack){
+                        	foreach ($haystack as $item){
+                                         if ((strpos($item[name], $needle) !== false) && (($item[active]==1))) {
+                                                return true;
+                                         }
+                                 }
+                                 return false;
+                        }
+		       
+		       foreach($aBasePermissions as $sPermissionKey=>$aCRUDPermissions): ?>
                     <?php
-                        if ($sPermissionKey == 'auth_ldap' && !App()->getPluginManager()->isPluginActive('AuthLDAP')
-                            || $sPermissionKey == 'auth_webserver' && !App()->getPluginManager()->isPluginActive('Authwebserver'))
+                         if ($sPermissionKey == 'auth_ldap' && !matchInstalledAndActive($pstrname, $pluginlist)   
+				|| $sPermissionKey == 'auth_webserver' && !App()->getPluginManager()->isPluginActive('Authwebserver'))
                         {
                             continue;
                         }
