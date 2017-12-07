7783e3b9259031bc8f5fc62c3184ccd59153dbfa
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2017-12-07 12:00:39 +0100

    Release 2.72.6+171207

--- application/config/version.php
+++ application/config/version.php
@@ -12,11 +12,11 @@
  *150413
  */
 
-$config['versionnumber'] = '2.72.5';
+$config['versionnumber'] = '2.72.6';
 $config['dbversionnumber'] = 263;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
-$config['assetsversionnumber'] = '2725';
+$config['assetsversionnumber'] = '2726';
 return $config;
 
 ?>
