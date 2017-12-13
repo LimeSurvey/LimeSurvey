2539aa3c108f737151912b5a5fe5a52cd9e3c128
Carsten Schmitz <carsten.schmitz@limesurvey.org>
2017-12-13 08:50:02 +0100

    Dev Updated ComfortUpdate links

--- application/views/admin/update/manage/manage_key.php
+++ application/views/admin/update/manage/manage_key.php
@@ -61,7 +61,7 @@
             </p>
             <p>
                 <?php
-                    $aopen  = '<a href="https://www.limesurvey.org/limesurvey-profile" target="_blank">';
+                    $aopen  = '<a href="https://www.limesurvey.org/get-your-free-comfortupdate-trial-key" target="_blank">';
                     $aclose = '</a>';
                 ?>
                 <?php echo sprintf(gT("You can get a free trial update key from %syour account on the limesurvey.org website%s."),$aopen, $aclose); ?>
