89423f142fca13023a48d54f4c72def647f58fff
markusfluer <fluer.markus@googlemail.com>
2017-12-22 10:11:23 +0100

    Dev: uri rendering issues for tutorial

--- assets/packages/lstutorial/src/tours/global-tour-object.js
+++ assets/packages/lstutorial/src/tours/global-tour-object.js
@@ -24,7 +24,7 @@ const globalTourObject = function(){
             
             const baseUrl = (getBasedUrls || forceGet) ? '?r=admin/' : 'admin/';
             const conatainsIndex = (/\/index.php\/?/.test(window.location.href));
-            const returnUrl = window.LS.data.baseUrl+(conatainsIndex ? '/index.php/' : '/')+baseUrl+url+combineParams(params);
+            const returnUrl = window.LS.data.baseUrl+(conatainsIndex ? '/index.php' : '/')+baseUrl+url+combineParams(params);
 
             return returnUrl;
 
