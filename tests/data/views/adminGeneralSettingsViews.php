19a703e6c4af6f181706b881d538bb54eaa2ccf8
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2018-01-10 15:00:35 +0100

    Dev: Disable two tests
    Dev: Third time I disable these tests? :D
    Dev: Please test them with both urlFormat path and get.

--- tests/data/views/adminGeneralSettingsViews.php
+++ tests/data/views/adminGeneralSettingsViews.php
@@ -12,11 +12,13 @@ return [
     ['homepageSettings', ['route'=>'homepagesettings']],
     ['createNewBox', ['route'=>'homepagesettings/sa/create']],
     ['pluginManager', ['route'=>'pluginmanager/sa/index']],
-    ['configurePlugin', ['route'=>'pluginmanager/sa/configure&id=2']],
+    // TODO: Can't hardcode &id=2 if you using other urlFormat (path).
+    //['configurePlugin', ['route'=>'pluginmanager/sa/configure&id=2']],
     ['surveyMenus', ['route'=>'menus/sa/view']],
     ['surveyMenuEntries', ['route'=>'menuentries/sa/view']],
     ['templateOptions', ['route'=>'themeoptions']],
-    ['surveyTemplateOptionsUpdate', ['route'=>'themeoptions/sa/update&id=1']],
+    // TODO: Can't hardcode &id=1 if you using other urlFormat (path).
+    //['surveyTemplateOptionsUpdate', ['route'=>'themeoptions/sa/update&id=1']],
     ['themeEditor', ['route'=>'themes/sa/view&templatename=fruity']],
 
 ];
