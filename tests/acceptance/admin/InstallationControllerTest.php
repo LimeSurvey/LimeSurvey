e80d2d3f530c2b20809e4f83697f66e8ed5d5c96
Tõnis Ormisson <tonis@andmemasin.eu>
2018-01-11 11:15:34 +0200

    resoleve conflict on installationTest

--- tests/acceptance/admin/InstallationControllerTest.php
+++ tests/acceptance/admin/InstallationControllerTest.php
@@ -1,22 +1,11 @@
 <?php
-/**
- *  LimeSurvey
- * Copyright (C) 2007-2011 The LimeSurvey Project Team / Carsten Schmitz
- * All rights reserved.
- * License: GNU/GPL License v2 or later, see LICENSE.php
- * LimeSurvey is free software. This version may have been modified pursuant
- * to the GNU General Public License, and as distributed it includes or
- * is derivative of works licensed under the GNU General Public License or
- * other free or open source software licenses.
- * See COPYRIGHT.php for copyright notices and details.
- */
 
 namespace LimeSurvey\tests\acceptance\admin;
 
-use LimeSurvey\tests\TestBaseClassWeb;
-use LimeSurvey\tests\TestHelper;
 use Facebook\WebDriver\WebDriverBy;
 use Facebook\WebDriver\Exception\NoSuchElementException;
+use LimeSurvey\tests\TestBaseClassWeb;
+use LimeSurvey\tests\TestHelper;
 
 /**
  * @since 2017-11-24
@@ -36,6 +25,7 @@ class InstallationControllerTest extends TestBaseClassWeb
         self::$webDriver = self::$testHelper->getWebDriver();
         self::$domain = getenv('DOMAIN');
     }
+
     /**
      *
      */
@@ -46,6 +36,7 @@ class InstallationControllerTest extends TestBaseClassWeb
             self::$testHelper->connectToOriginalDatabase();
         }
     }
+
     /**
      *
      * @throws \CException
@@ -53,8 +44,10 @@ class InstallationControllerTest extends TestBaseClassWeb
     public function testBasic()
     {
         //$this->checkFolders();
+
         $configFile = \Yii::app()->getBasePath() . '/config/config.php';
         $databaseName = 'limesurvey';
+
         $username = getenv('ADMINUSERNAME');
         if (!$username) {
             $username = 'admin';
@@ -63,6 +56,7 @@ class InstallationControllerTest extends TestBaseClassWeb
         if (!$password) {
             $password = 'password';
         }
+
         $dbuser = getenv('DBUSER');
         if (!$dbuser) {
             $dbuser = 'root';
@@ -73,6 +67,7 @@ class InstallationControllerTest extends TestBaseClassWeb
             $dbpwd = '';
             echo 'Default to empty database password. Use DBPASSWORD=... from command-line to override this.' . PHP_EOL;
         }
+
         if (file_exists($configFile)) {
             // Delete possible previous database.
             try {
@@ -86,26 +81,34 @@ class InstallationControllerTest extends TestBaseClassWeb
                     'Could drop database. Error message: ' . $msg
                 );
             }
+
             // Remove config.php if present.
             $result = unlink($configFile);
             $this->assertTrue($result, 'Could unlink config.php');
         }
+
         // Run installer.
         $urlMan = \Yii::app()->urlManager;
         $urlMan->setBaseUrl('http://' . self::$domain . '/index.php');
         $url = $urlMan->createUrl('');
+
         try {
+
             // Installer start page.
             self::$webDriver->get($url);
+
             // Click "Start installation".
             $start = self::$webDriver->findElement(WebDriverBy::id('ls-start-installation'));
             $start->click();
+
             // Accept license.
             $accept = self::$webDriver->findElement(WebDriverBy::id('ls-accept-license'));
             $accept->click();
+
             // Click next at pre-check.
             $next = self::$webDriver->findElement(WebDriverBy::id('ls-next'));
             $next->click();
+
             // Fill in database form.
             $dbuserDbType = self::$webDriver->findElement(WebDriverBy::cssSelector('select[name="InstallerConfigForm[dbtype]"] option[value="mysql"]'));
             $dbuserInput = self::$webDriver->findElement(WebDriverBy::cssSelector('input[name="InstallerConfigForm[dbuser]"]'));
@@ -116,15 +119,19 @@ class InstallationControllerTest extends TestBaseClassWeb
             $dbuserInput->clear()->sendKeys($dbuser);
             $dbpwdInput->clear()->sendKeys($dbpwd);
             $dbnameInput->sendKeys($databaseName);
+
             // Click next.
             $next = self::$webDriver->findElement(WebDriverBy::id('ls-next'));
             $next->click();
+
             // Click "Create database".
             $button = self::$webDriver->findElement(WebDriverBy::cssSelector('input[type="submit"]'));
             $button->click();
+
             // Click "Populate".
             $button = self::$webDriver->findElement(WebDriverBy::cssSelector('input[type="submit"]'));
             $button->click();
+
             // Fill in admin username/password.
             $adminLoginName = self::$webDriver->findElement(WebDriverBy::cssSelector('input[name="InstallerConfigForm[adminLoginName]"]'));
             $adminLoginPwd  = self::$webDriver->findElement(WebDriverBy::cssSelector('input[name="InstallerConfigForm[adminLoginPwd]"]'));
@@ -132,12 +139,15 @@ class InstallationControllerTest extends TestBaseClassWeb
             $adminLoginName->clear()->sendKeys($username);
             $adminLoginPwd->clear()->sendKeys($password);
             $confirmPwd->clear()->sendKeys($password);
+
             // Confirm optional settings (admin password etc).
             $button = self::$webDriver->findElement(WebDriverBy::cssSelector('input[type="submit"]'));
             $button->click();
+
             // Go to administration.
             $button = self::$webDriver->findElement(WebDriverBy::id('ls-administration'));
             $button->click();
+
             // Set debug=2
             /* TODO: Can't write to config.php after installation.
             $configFile = \Yii::app()->getBasePath() . '/config/config.php';
@@ -151,10 +161,12 @@ class InstallationControllerTest extends TestBaseClassWeb
             $result = file_put_contents($configFile, implode('', $data));
             $this->assertTrue($result > 0, 'Wrote config');
              */
+
             // Reset urlManager to adapt to latest config.
             $config = require($configFile);
             $urlMan = \Yii::app()->urlManager;
             $urlMan->setUrlFormat($config['components']['urlManager']['urlFormat']);
+
             // Login.
             self::adminLogin($username, $password);
         } catch (NoSuchElementException $ex) {
@@ -165,6 +177,7 @@ class InstallationControllerTest extends TestBaseClassWeb
             );
         }
     }
+
     /**
      * Check that upload/tmp folders are writable.
      * @todo Does not work.
@@ -182,6 +195,7 @@ class InstallationControllerTest extends TestBaseClassWeb
             true
         );
         $this->assertTrue($tempdirIsWritable, 'Can write to tmp/');
+
         $folder = \Yii::app()->getConfig('uploaddir') . '/';
         $uploadIsWritable = $instContr->checkDirectoryWriteable(
             $folder,
