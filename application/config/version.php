a25a62b2a6b1d482d1562c49417a5fc092ecb7f3
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2018-01-18 16:25:14 +0100

    Release 3.0.5+180118

--- application/config/version.php
+++ application/config/version.php
@@ -12,9 +12,9 @@
  */
 
 
-$config['versionnumber'] = '3.0.4';  //The current version of this branch, LS3
+$config['versionnumber'] = '3.0.5';  //The current version of this branch, LS3
 $config['dbversionnumber'] = 345;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
-$config['assetsversionnumber'] = '30007';
+$config['assetsversionnumber'] = '30008';
 return $config;
