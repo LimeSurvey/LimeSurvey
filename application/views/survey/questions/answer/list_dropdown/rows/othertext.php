ea543a5d4a1f184e9763cc31c46d61b545b2052e
Denis Chenu <courriel@shnoulle.net>
2018-01-19 10:17:05 +0100

    New feature #12240: Validation settings for "array by column" question type
    New feature : Add em_validation_q to list of choice (dropdown and radio)
    Fixed issue : EM expression on other for list dropdown didn't work

--- application/views/survey/questions/answer/list_dropdown/rows/othertext.php
+++ application/views/survey/questions/answer/list_dropdown/rows/othertext.php
@@ -11,26 +11,26 @@
 <label for="othertext<?php echo $name; ?>" class="sr-only">
     <?php echo $label; ?>
 </label>
+<div class="form-group text-item other-text-item">
 <?php
 echo \CHtml::textField("{$name}other",$value,array(
-    'id' => "{$name}othertext",
-    'style' => $display,
+    'id' => "othertext{$name}",
+    'class'=>"form-control",
     'aria-labelledby' => "answer{$name}-oth-",
 ));
 ?>
-
+</div>
 <script>
 if( $("#answer<?php echo $name; ?>").val() != "-oth-" ){
     $("#othertext<?php echo $name; ?>").hide();
 }
-
 $(document).on("change","#answer<?php echo $name; ?>", function(){
     if($("#answer<?php echo $name; ?>").val() != "-oth-"){
-        $("#<?php echo $name; ?>othertext").hide();
-        $("#<?php echo $name; ?>othertext").val("").trigger("keyup");
+        $("#othertext<?php echo $name; ?>").hide();
+        $("#othertext<?php echo $name; ?>").val("").trigger("keyup");
     }else{
-        $("#<?php echo $name; ?>othertext").show();
-        $("#<?php echo $name; ?>othertext").focus();
+        $("#othertext<?php echo $name; ?>").show();
+        $("#othertext<?php echo $name; ?>").focus();
     }
 });
 </script>
