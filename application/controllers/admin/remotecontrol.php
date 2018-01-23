7e41d808755730d642088138cb6e37d89a935c1e
Tõnis Ormisson <tonis@andmemasin.eu>
2018-01-23 22:30:03 +0200

    DEV: scrutinizer issues - unused - code: prettyer disabling :)

--- application/controllers/admin/remotecontrol.php
+++ application/controllers/admin/remotecontrol.php
@@ -91,73 +91,77 @@ class remotecontrol extends Survey_Common_Action
      */
     public function test()
     {
-        die(); // Remove if you want to test this function
-        $RPCType = Yii::app()->getConfig("RPCInterface");
-        $serverUrl = App()->createAbsoluteUrl('/admin/remotecontrol');
-        $sFileToImport = dirname(Yii::app()->basePath).DIRECTORY_SEPARATOR.'docs'.DIRECTORY_SEPARATOR.'demosurveys'.DIRECTORY_SEPARATOR.'limesurvey2_sample_survey_english.lss';
+        // Enable if you want to test this function
+        $enabled = false;
+        if($enabled){
+            $RPCType = Yii::app()->getConfig("RPCInterface");
+            $serverUrl = App()->createAbsoluteUrl('/admin/remotecontrol');
+            $sFileToImport = dirname(Yii::app()->basePath).DIRECTORY_SEPARATOR.'docs'.DIRECTORY_SEPARATOR.'demosurveys'.DIRECTORY_SEPARATOR.'limesurvey2_sample_survey_english.lss';
 
-        if ($RPCType == 'xml') {
-            $cur_path = get_include_path();
-            set_include_path($cur_path.PATH_SEPARATOR.APPPATH.'helpers');
-            require_once('Zend/XmlRpc/Client.php');
-            
-            $client = new Zend_XmlRpc_Client($serverUrl);
-        } elseif ($RPCType == 'json') {
-            Yii::app()->loadLibrary('jsonRPCClient');
-            $client = new jsonRPCClient($serverUrl);
-        } else {
-            die('RPC interface not activated in global settings');
-        }
-        
+            if ($RPCType == 'xml') {
+                $cur_path = get_include_path();
+                set_include_path($cur_path.PATH_SEPARATOR.APPPATH.'helpers');
+                require_once('Zend/XmlRpc/Client.php');
 
-        $sSessionKey = $client->call('get_session_key', array('admin', 'password'));
-        if (is_array($sSessionKey)) {echo $sSessionKey['status']; die(); } else {
-            echo 'Retrieved session key'.'<br>';
-        }
-        
-        $sLSSData = base64_encode(file_get_contents($sFileToImport));
-        $iSurveyID = $client->call('import_survey', array($sSessionKey, $sLSSData, 'lss', 'Test import by JSON_RPC', 1000));
-        echo 'Created new survey SID:'.$iSurveyID.'<br>';
+                $client = new Zend_XmlRpc_Client($serverUrl);
+            } elseif ($RPCType == 'json') {
+                Yii::app()->loadLibrary('jsonRPCClient');
+                $client = new jsonRPCClient($serverUrl);
+            } else {
+                die('RPC interface not activated in global settings');
+            }
 
-        $aResult = $client->call('activate_survey', array($sSessionKey, $iSurveyID));
-        if ($aResult['status'] == 'OK') {
-            echo 'Survey '.$iSurveyID.' successfully activated.<br>';
-        }
-        $aResult = $client->call('activate_tokens', array($sSessionKey, $iSurveyID, array(1, 2)));
-        if ($aResult['status'] == 'OK') {
-            echo 'Tokens for Survey ID '.$iSurveyID.' successfully activated.<br>';
-        }
-        $aResult = $client->call('set_survey_properties', array($sSessionKey, $iSurveyID, array('faxto'=>'0800-LIMESURVEY')));
-        if (!array_key_exists('status', $aResult)) {
-            echo 'Modified survey settings for survey '.$iSurveyID.'<br>';
-        }
-        $aResult = $client->call('add_language', array($sSessionKey, $iSurveyID, 'ar'));
-        if ($aResult['status'] == 'OK') {
-            echo 'Added Arabian as additional language'.'<br>';
-        }
-        $aResult = $client->call('set_language_properties', array($sSessionKey, $iSurveyID, array('surveyls_welcometext'=>'An Arabian welcome text!'), 'ar'));
-        if ($aResult['status'] == 'OK') {
-            echo 'Modified survey locale setting welcometext for Arabian in survey ID '.$iSurveyID.'<br>';
-        }
 
-        $aResult = $client->call('delete_language', array($sSessionKey, $iSurveyID, 'ar'));
-        if ($aResult['status'] == 'OK') {
-            echo 'Removed Arabian as additional language'.'<br>';
+            $sSessionKey = $client->call('get_session_key', array('admin', 'password'));
+            if (is_array($sSessionKey)) {echo $sSessionKey['status']; die(); } else {
+                echo 'Retrieved session key'.'<br>';
+            }
+
+            $sLSSData = base64_encode(file_get_contents($sFileToImport));
+            $iSurveyID = $client->call('import_survey', array($sSessionKey, $sLSSData, 'lss', 'Test import by JSON_RPC', 1000));
+            echo 'Created new survey SID:'.$iSurveyID.'<br>';
+
+            $aResult = $client->call('activate_survey', array($sSessionKey, $iSurveyID));
+            if ($aResult['status'] == 'OK') {
+                echo 'Survey '.$iSurveyID.' successfully activated.<br>';
+            }
+            $aResult = $client->call('activate_tokens', array($sSessionKey, $iSurveyID, array(1, 2)));
+            if ($aResult['status'] == 'OK') {
+                echo 'Tokens for Survey ID '.$iSurveyID.' successfully activated.<br>';
+            }
+            $aResult = $client->call('set_survey_properties', array($sSessionKey, $iSurveyID, array('faxto'=>'0800-LIMESURVEY')));
+            if (!array_key_exists('status', $aResult)) {
+                echo 'Modified survey settings for survey '.$iSurveyID.'<br>';
+            }
+            $aResult = $client->call('add_language', array($sSessionKey, $iSurveyID, 'ar'));
+            if ($aResult['status'] == 'OK') {
+                echo 'Added Arabian as additional language'.'<br>';
+            }
+            $aResult = $client->call('set_language_properties', array($sSessionKey, $iSurveyID, array('surveyls_welcometext'=>'An Arabian welcome text!'), 'ar'));
+            if ($aResult['status'] == 'OK') {
+                echo 'Modified survey locale setting welcometext for Arabian in survey ID '.$iSurveyID.'<br>';
+            }
+
+            $aResult = $client->call('delete_language', array($sSessionKey, $iSurveyID, 'ar'));
+            if ($aResult['status'] == 'OK') {
+                echo 'Removed Arabian as additional language'.'<br>';
+            }
+
+            //Very simple example to export responses as Excel file
+            //$aResult=$client->call('export_responses', array($sSessionKey,$iSurveyID,'xls'));
+            //$aResult=$client->call('export_responses', array($sSessionKey,$iSurveyID,'pdf'));
+            //$aResult=$client->call('export_responses', array($sSessionKey,$iSurveyID,'doc'));
+            $aResult = $client->call('export_responses', array($sSessionKey, $iSurveyID, 'csv'));
+            //file_put_contents('test.xls',base64_decode(chunk_split($aResult)));
+
+            $aResult = $client->call('delete_survey', array($sSessionKey, $iSurveyID));
+            echo 'Deleted survey SID:'.$iSurveyID.'-'.$aResult['status'].'<br>';
+
+            // Release the session key - close the session
+            $Result = $client->call('release_session_key', array($sSessionKey));
+            echo 'Closed the session'.'<br>';
+
         }
-
-        //Very simple example to export responses as Excel file
-        //$aResult=$client->call('export_responses', array($sSessionKey,$iSurveyID,'xls'));
-        //$aResult=$client->call('export_responses', array($sSessionKey,$iSurveyID,'pdf'));
-        //$aResult=$client->call('export_responses', array($sSessionKey,$iSurveyID,'doc'));
-        $aResult = $client->call('export_responses', array($sSessionKey, $iSurveyID, 'csv'));
-        //file_put_contents('test.xls',base64_decode(chunk_split($aResult)));
-
-        $aResult = $client->call('delete_survey', array($sSessionKey, $iSurveyID));
-        echo 'Deleted survey SID:'.$iSurveyID.'-'.$aResult['status'].'<br>';
-
-        // Release the session key - close the session
-        $Result = $client->call('release_session_key', array($sSessionKey));
-        echo 'Closed the session'.'<br>';
 
     }
 
