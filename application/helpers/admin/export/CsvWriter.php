1174354b35d7b0480f66dde23be6da08bc0366d9
Carsten Schmitz <carsten.schmitz@limesurvey.org>
2018-02-02 10:27:20 +0100

    Fixed issue: Better Excel compatibility for multiline response fields

--- application/helpers/admin/export/CsvWriter.php
+++ application/helpers/admin/export/CsvWriter.php
@@ -34,12 +34,9 @@ class CsvWriter extends Writer
     public function init(SurveyObj $survey, $sLanguageCode, FormattingOptions $oOptions)
     {
         parent::init($survey, $sLanguageCode, $oOptions);
-        
         $this->csvFilename = "results-survey".$survey->id.".csv";
-        
         if ($oOptions->output == 'file') {
             $this->file = fopen($this->filename, 'w');
-            fwrite($this->file,"\xEF\xBB\xBF"); // Write UTF-8 Byte Order Mark (BOM)
         }
         
     }
@@ -51,6 +48,9 @@ class CsvWriter extends Writer
             if ($oOptions->output == 'display') {
                 header("Content-Disposition: attachment; filename=".$this->csvFilename);
                 header("Content-type: text/comma-separated-values; charset=UTF-8");
+                echo chr(239) . chr(187) . chr(191);
+            } else {
+                fwrite($this->file, chr(239) . chr(187) . chr(191)); // Write UTF-8 Byte Order Mark (BOM)
             }
             
             // If we don't want headers in our csv, for example in exports like r/spss etc. we suppress the header by setting this switch in the init
@@ -85,9 +85,9 @@ class CsvWriter extends Writer
     public function close()
     {
         // Output white line at the end, better for R import
-        echo "\n";
+        echo "\r\n";
         if (!is_null($this->file)) {
-            fwrite($this->file, "\n");
+            fwrite($this->file, "\r\n");
             fclose($this->file);
         }
     }
