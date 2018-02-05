82f6e69566ef654d64ef2abaf4e0ed85c6d50eca
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2018-02-05 16:37:07 +0100

    Dev: Disable textarea if user lacks permission

--- application/controllers/admin/questions.php
+++ application/controllers/admin/questions.php
@@ -397,6 +397,10 @@ class questions extends Survey_Common_Action
         $aData['sidemenu']['explorer']['state'] = true;
         $aData['sidemenu']['explorer']['gid'] = (isset($gid)) ? $gid : false;
         $aData['sidemenu']['explorer']['qid'] = (isset($qid)) ? $qid : false;
+        $aData['hasUpdatePermission'] =
+            Permission::model()->hasSurveyPermission($iSurveyID, 'surveycontent', 'update') ?
+            '' :
+            'disabled="disabled" readonly="readonly"';
 
         $this->_renderWrappedTemplate('survey/Question', 'editdefaultvalues_view', $aData);
     }
