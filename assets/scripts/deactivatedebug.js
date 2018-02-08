53e7c4f12d635a6eef1ad722cfe0d61217d49870
Joost Mul <joost@kwible.net>
2018-02-08 15:09:54 +0100

    Reflect all available console functions in deactivatedebug.js
    Some scripts make use of other console functions than log and error. These scripts kinda depend on those other functions to exist (like console.warn in the Facebook tracking pixel script) and therefor these other scripts will break otherwise.

--- assets/scripts/deactivatedebug.js
+++ assets/scripts/deactivatedebug.js
@@ -1,6 +1,8 @@
-var dummyConsole = {
-    log : function(){},
-    error : function(){}
-};
+var dummyConsole = {};
+var realConsole = console || window.console
+for (var consoleFunction in realConsole) {
+    dummyConsole[consoleFunction] = function(){};
+}
+
 console = dummyConsole;
 window.console = dummyConsole;
