9c9ac0f888aec4604241b0c387c865f5a568177d
Carsten Schmitz <carsten.schmitz@limesurvey.org>
2018-02-09 16:49:25 +0100

    Release 3.3.0 Build 180209

--- application/config/version.php
+++ application/config/version.php
@@ -12,9 +12,9 @@
  */
 
 
-$config['versionnumber'] = '3.2.1'; //The current version of this branch, LS3
+$config['versionnumber'] = '3.3.0'; //The current version of this branch, LS3
 $config['dbversionnumber'] = 347;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
-$config['assetsversionnumber'] = '30013';
+$config['assetsversionnumber'] = '30014';
 return $config;
