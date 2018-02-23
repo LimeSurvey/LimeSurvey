2651c648e1bbb321f091793cc09e49bb2e02560f
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2018-02-23 15:43:53 +0100

    Dev: Close XMl reader after use

--- application/helpers/admin/import_helper.php
+++ application/helpers/admin/import_helper.php
@@ -1686,6 +1686,7 @@ function XMLImportResponses($sFullFilePath, $iSurveyID, $aFieldReMap = array())
 
         }
     }
+    $oXMLReader->close();
 
     switchMSSQLIdentityInsert('survey_'.$iSurveyID, false);
     if (Yii::app()->db->getDriverName() == 'pgsql') {
