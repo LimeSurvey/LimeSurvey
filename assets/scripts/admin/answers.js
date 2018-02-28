2a473af41408c9c072aa32d17a6b5b0032d7a694
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2018-02-28 15:06:24 +0100

    Fixed issue #13425: Cannot add subquestions and answer options with plus button

--- assets/scripts/admin/answers.js
+++ assets/scripts/admin/answers.js
@@ -220,13 +220,11 @@ function addinput(e)
         data: datas,
         success: function(arrayofhtml) {
 
-            // arrayofhtml is a json string containing the different HTML row by language
+            // arrayofhtml is a json object containing the different HTML row by language
             // eg: {"en":"{the html of the en row}", "fr":{the html of the fr row}}
 
-            $arrayOfHtml = JSON.parse(arrayofhtml);                             // Convert the JSON to a javascript object
-
             // We insert each row for each language
-            $.each($arrayOfHtml, function(lang, htmlRow){
+            $.each(arrayofhtml, function(lang, htmlRow){
                 $elRowToUpdate = $('#row_'+lang+'_'+$commonId);                 // The row for the current language
                 $elRowToUpdate.after(htmlRow);                                  // We insert the HTML of the new row after this one
                 updaterowproperties();
