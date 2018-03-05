a53d5b147d3d23a9d9cec9561b2b8d3ffce3c0c8
LouisGac <louis.gac@limesurvey.org>
2018-03-05 18:33:02 +0100

    Fixed issue #13366: Extending a theme creates world writable directories

--- application/controllers/admin/themes.php
+++ application/controllers/admin/themes.php
@@ -499,7 +499,7 @@ class themes extends Survey_Common_Action
 
                 if ($mkdirresult == 1) {
                     // We just copy the while directory structure, but only the xml file
-                    $oFileHelper->copyDirectory($copydirname, $newdirname, array('fileTypes' => array('xml', 'png', 'jpg')));
+                    $oFileHelper->copyDirectory($copydirname, $newdirname, array('fileTypes' => array('xml', 'png', 'jpg'), 'newDirMode' => 0755, 'newFileMode' => 0644 ));
                     //TemplateConfiguration::removeAllNodes($newdirname);
                     TemplateManifest::extendsConfig($copydir, $newname);
                     TemplateManifest::importManifest($newname, ['extends' => $copydir]);
