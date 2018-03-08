7c259c4d76cad351afdb28aae4112dea94c24082
markusfluer <fluer.markus@googlemail.com>
2018-03-08 16:09:28 +0100

    New feature: added a js based filter on the data security acceptance

--- themes/survey/vanilla/scripts/theme.js
+++ themes/survey/vanilla/scripts/theme.js
@@ -33,13 +33,17 @@
  * The general Template closure.
  * This is to capsule eventual errors inside of the template function, so the general script all run as the should
  */
+// Global constructor, used in multiple twig files
+// eslint-disable-next-line no-unused-vars
 var ThemeScripts = function(){
+    // defining the console object once. Also filtered by the global setting
+    // eslint-disable-next-line no-console
     var logObject = console.ls ? (window.debugState.frontend ? console.ls : console.ls.silent) : console;
     /**
      * The function focusFirst puts the Focus on the first non-hidden element in the Survey.
      * Normally this is the first input field (the first answer).
      */
-    var focusFirst = function focusFirst(Event)
+    var focusFirst = function focusFirst()
     {
         $('#limesurvey :input:visible:enabled:first').focus();
     };
@@ -57,11 +61,12 @@ var ThemeScripts = function(){
      */
     var sliderSuffixClone = function sliderSuffixClone(){
         $('.numeric-multi .slider-item .slider-right').each(function(){
+            var colWidth='12';
+            
             if($(this).closest('.slider-item').find('.slider-left').length){
-                var colWidth='6';
-            }else{
-                var colWidth='12';
+                colWidth = '6';
             }
+
             $(this).clone().removeClass('col-xs-12').addClass('visible-xs-block col-xs-'+colWidth).prop('aria-hidden',true).insertBefore($(this).prev('.slider-container'));
             $(this).addClass('hidden-xs');
             $(this).closest('.slider-item').find('.slider-left').removeClass('col-xs-12').addClass('col-xs-6');
@@ -149,9 +154,9 @@ var ThemeScripts = function(){
             if($('.input-error').length > 0 ||  $('.ls-em-error').length > 0 ) {
                 $('#bootstrap-alert-box-modal').on('hidden.bs.modal', function () {
 
-                    $firstError = ($('.input-error').length > 0 ) ? $('.input-error').first() : $('.ls-em-error').first();
+                    var $firstError = ($('.input-error').length > 0 ) ? $('.input-error').first() : $('.ls-em-error').first();
 
-                    $pixToScroll = ( $firstError.offset().top - 100 );
+                    var $pixToScroll = ( $firstError.offset().top - 100 );
                     $('html, body').animate({
                         scrollTop: $pixToScroll + 'px'
                     }, 'fast');
@@ -159,7 +164,7 @@ var ThemeScripts = function(){
             }
 
             $('.language-changer').each(function(){
-                $that = $(this);
+                var $that = $(this);
                 if(!$.trim($that.children('div').html()))
                 {
                     $that.hide();
@@ -177,7 +182,7 @@ var ThemeScripts = function(){
                 var footerHeight = $('#surveyListFooter').outerHeight();
                 var headerHeight = 2*$('#navbar').outerHeight();
                 var bodyHeight = $(document).height()-(footerHeight+headerHeight);
-                console.log({
+                logObject.log({
                     footerHeight : footerHeight,
                     headerHeight : headerHeight,
                     bodyHeight : bodyHeight
@@ -197,7 +202,7 @@ var ThemeScripts = function(){
             // Survey list footer
             if($('#surveyListFooter').length>0)
             {
-                $surveyListFooter = $('#surveyListFooter');
+                var $surveyListFooter = $('#surveyListFooter');
                 $('#outerframeContainer').after($surveyListFooter);
             }
 
@@ -206,6 +211,8 @@ var ThemeScripts = function(){
             });
 
             // Bind language changer onclick event.
+            // This function is defined in assets/packages/limesurvey/survey.js
+            // eslint-disable-next-line no-undef
             activateLanguageChanger();
             
             //Fix the navigation overflow
@@ -267,10 +274,35 @@ var ThemeScripts = function(){
         hideEmptyPart();
     };
 
+    var initWelcomePage = function(){
+        logObject.log('Starting up for first page');
+        if($('#datasecurity_accepted').length >0) {
+            $('#ls-button-submit').prop('disabled', true).parent().on('mouseenter', function(e){
+                logObject.log('starting animation');
+                if(!$('#datasecurity_accepted').prop('checked') && $(this).data('moving') != 1){
+                    e.preventDefault();
+                    var self = this;
+                    $(self).data('moving', 1);
+                    $('label[for=datasecurity_accepted]').animate({'zoom': '1.05'}, 500, function(){
+                        $('label[for=datasecurity_accepted]').animate({'zoom': '1'}, 500, function(){
+                            $(self).data('moving', 0);
+                        });
+                    });
+                    return false;
+                }
+            });
+            $('#datasecurity_accepted').on('change', function(){
+                $('#ls-button-submit').prop('disabled', !$(this).prop('checked'));
+            });
+
+        }
+    };
+
     return {
         init: init,
         initUserForms: initUserForms,
         initGlobal: initGlobal,
+        initWelcomePage: initWelcomePage,
         focusFirst: focusFirst,
         sliderSuffixClone : sliderSuffixClone,
         fixBodyPadding : fixBodyPadding,
