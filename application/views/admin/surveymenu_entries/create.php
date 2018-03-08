e40c8b6423d35f216ab4f10dfe8954b0c81b941e
Carsten Schmitz <carsten.schmitz@limesurvey.org>
2018-03-08 10:26:24 +0100

    Dev Small text fix

--- application/views/admin/surveymenu_entries/create.php
+++ application/views/admin/surveymenu_entries/create.php
@@ -13,6 +13,6 @@ $this->menu = array(
 );
 ?>
 
-<h1><?php eT('Create SurveymenuEntries'); ?></h1>
+<h1><?php eT('Create survey menu entries'); ?></h1>
 
 <?php $this->renderPartial('_form', array('model'=>$model)); ?>
