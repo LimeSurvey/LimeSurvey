f061c0421a977ec1cd72136421cec5edbef45d4a
markusfluer <fluer.markus@googlemail.com>
2018-03-12 12:23:59 +0100

    Dev: fixed datatype in updatehelper

--- application/helpers/update/updatedb_helper.php
+++ application/helpers/update/updatedb_helper.php
@@ -2203,7 +2203,7 @@ function db_upgrade_all($iOldDBVersion, $bSilent = false)
             $oDB->createCommand()->addColumn('{{surveys_languagesettings}}', 'surveyls_policy_notice', 'text');
             $oDB->createCommand()->addColumn('{{surveys_languagesettings}}', 'surveyls_policy_error', 'text');
             $oDB->createCommand()->addColumn('{{surveys_languagesettings}}', 'surveyls_policy_notice_label', 'string(192)');
-            $oDB->createCommand()->addColumn('{{surveys}}', 'showsurveypolicynotice', 'int DEFAULT 0');
+            $oDB->createCommand()->addColumn('{{surveys}}', 'showsurveypolicynotice', 'integer DEFAULT 0');
 
             $oDB->createCommand()->update('{{settings_global}}', ['stg_value'=>348], "stg_name='DBVersion'");
             $oTransaction->commit();
