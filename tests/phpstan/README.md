8e858a589945b2cea9d7611d6e6a64f5233d9d5e
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2018-03-27 12:19:27 +0200

    Add phpstan bootstrap and config

---
+++ tests/phpstan/README.md
@@ -0,0 +1,7 @@
+PHPStan is a static analyzer. Details here: https://github.com/phpstan/phpstan
+
+PHPStan requires a fix in Yii base class: Yii::app() should @return LSYii_Application, not CApplication.
+
+Other analyzers are psalm and phan (tested but did not get to work). MessDetector and codesniffer are simpler tools that are easy to setup.
+
+Static analyzers can be used in the plugin shop to validate and check third party code.
---
+++ tests/phpstan/phpstan_bootstrap.php
@@ -0,0 +1,27 @@
+<?php
+
+// Code below copied from index.php.
+
+$system_path = "framework";
+$application_folder = dirname(__FILE__) . "/../../application";
+if (realpath($system_path) !== false) {
+    $system_path = realpath($system_path).'/';
+}
+
+// ensure there's a trailing slash
+$system_path = rtrim($system_path, '/').'/';
+
+define('BASEPATH', str_replace("\\", "/", $system_path));
+define('APPPATH', $application_folder.'/');
+define('EXT', '.php');
+
+require_once __DIR__ . '/../../third_party/autoload.php';
+require_once BASEPATH . 'yii' . EXT;
+require_once APPPATH . 'core/LSYii_Application' . EXT;
+$config = require_once(APPPATH . 'config/internal' . EXT);
+
+Yii::$enableIncludePath = false;
+Yii::createApplication('LSYii_Application', $config);
+
+//Yii::import('application.helpers.common_helper', true);
+Yii::app()->loadHelper('common');
