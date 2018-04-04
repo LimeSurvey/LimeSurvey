e6e9863468ff99d76364d083913ff8d5b888ea0e
Denis Chenu <denis@sondages.pro>
2018-04-04 12:47:18 +0200

    Fixed issue : Import old survey table HTML broken Dev: Started div, not closed

--- application/views/admin/dataentry/import.php
+++ application/views/admin/dataentry/import.php
@@ -8,13 +8,9 @@
     <h3><?php eT("Import responses from a deactivated survey table"); ?></h3>
         <div class="row">
             <div class="col-lg-12 content-right">
-
-
-                <div class="pluginsettings messagebox">
                 <?php
                   //  echo CHtml::tag('div', array('class' => 'header ui-widget-header'), gT("Import responses from a deactivated survey table"));
                     $this->widget('ext.SettingsWidget.SettingsWidget', array(
-
                             'settings' => $settings,
                             'method' => 'post',
                             'buttons' => array(
@@ -29,16 +25,12 @@
                                 )
                             )
                         ));
-                        echo CHtml::openTag('div', array('class' => 'messagebox ui-corner-all'));
-                        echo CHtml::tag('div', array('class' => 'warningheader'), gT("Warning"));
-                        eT("You can import all old responses that are compatible with your current survey. Compatibility is determined by comparing column types and names, the ID field is always ignored.");
-                        echo '<br/>';
-                        eT("Using type coercion may break your data; use with care or not at all if possible.");
-                        echo '<br/>';
-                        eT("Currently we detect and handle the following changes:");
-
+                        echo CHtml::openTag('div', array('class' => 'well'));
+                        echo CHtml::tag('strong', array('class' => 'text-warning'), gT("Warning"));
+                        echo CHtml::tag('p',array(),gT("You can import all old responses that are compatible with your current survey. Compatibility is determined by comparing column types and names, the ID field is always ignored."));
+                        echo CHtml::tag('p',array(),gT("Using type coercion may break your data; use with care or not at all if possible."));
+                        echo CHtml::tag('p',array(),gT("Currently we detect and handle the following changes:"));
                         $list = array(
-
                             gT("Question is moved to another group (result is imported correctly)."),
                             gT("Question is removed from target (result is ignored)."),
                             gT("Question is added to target (result is set to database default value).")
@@ -50,7 +42,6 @@
                         CHtml::closeTag('ul');
 
                     echo CHtml::closeTag('div');
-
                 ?>
         </div>
     </div>
