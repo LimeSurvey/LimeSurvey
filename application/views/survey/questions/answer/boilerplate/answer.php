b3d48a99da7c9ce09b117868205b1c4f125bdd9e
Dominik Vitt <dominik.vitt@limesurvey.org>
2018-05-04 11:06:04 +0200

    Dev: Changed variable name for boilerplate question type

--- application/views/survey/questions/answer/boilerplate/answer.php
+++ application/views/survey/questions/answer/boilerplate/answer.php
@@ -1,11 +1,11 @@
 <?php
 /**
  * Boilerplate Html
- * @var $ia
+ * @var $name
  */
 ?>
 <!-- boilerplate -->
 <!-- Did we need really something ? Why not return an empty string ? -->
 <!-- answer -->
-<input class="<?php echo $coreClass;?> boilerplate hidden" type="hidden" name="<?php echo $ia[1];?>" id="answer<?php echo $ia[1];?>" value="" />
+<input class="<?php echo $coreClass;?> boilerplate hidden" type="hidden" name="<?php echo $name;?>" id="answer<?php echo $name;?>" value="" />
 <!-- end of answer -->
