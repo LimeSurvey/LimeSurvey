4d1b650e1f686949f38c19967b97e5562d829d07
Denis Chenu <denis@sondages.pro>
2018-05-09 15:49:45 +0200

    Dev: move default QuestionAttribute to model and use it Dev: then remove isset

--- application/controllers/admin/database.php
+++ application/controllers/admin/database.php
@@ -569,7 +569,7 @@ class database extends Survey_Common_Action
         $aLanguages = array_merge(array(Survey::model()->findByPk($iSurveyID)->language), Survey::model()->findByPk($iSurveyID)->additionalLanguages);
         foreach ($validAttributes as $validAttribute) {
             /* Readonly attribute : disable save */
-            if((isset($validAttribute['readonly']) && !empty($validAttribute['readonly'])) || (isset($validAttribute['readonly_when_active']) && !empty($validAttribute['readonly_when_active']) && Survey::model()->findByPk($iSurveyID)->getIsActive()) ) {
+            if( $validAttribute['readonly'] || $validAttribute['readonly_when_active'] && Survey::model()->findByPk($iSurveyID)->getIsActive() ) {
                 continue;
             }
             if ($validAttribute['i18n']) {
