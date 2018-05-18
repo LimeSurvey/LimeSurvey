3dd3da2dec8a908333a0ae750b108e3d4ac23a1d
Dominik Vitt <dominik.vitt.hr@gmail.com>
2018-05-18 15:25:01 +0200

    Fixed issue #13680: Question screen = AdminController cannot find the requested view "/survey/questions/answer/listradio/columns/column_header".

--- application/views/admin/themes/templateeditor_question_answerlist_view.php
+++ application/views/admin/themes/templateeditor_question_answerlist_view.php
@@ -1,45 +1,54 @@
 <?php
 /* Set some part */
 $sRows="";
-$sRows .= App()->getController()->renderPartial('/survey/questions/answer/listradio/columns/column_header', array('iColumnWidth' => 12), true);
-$sRows .= App()->getController()->renderPartial('/survey/questions/answer/listradio/rows/answer_row', array(
+$sRows .= App()->twigRenderer->renderQuestion('/survey/questions/answer/listradio/columns/column_header', array(
+    'bIsThemeEditor' => true,
+    'iColumnWidth' => 12));
+$sRows .= App()->twigRenderer->renderQuestion('/survey/questions/answer/listradio/rows/answer_row', array(
+    'bIsThemeEditor' => true,
     'sDisplayStyle' => '',
     'name'          => '1234X56X79',
     'code'          => 'A1',
     'answer'        => gT('One'),
     'checkedState'  => '',
     'myfname'       => 'answer22549X14X68A1',
-), true);
-$sRows .= App()->getController()->renderPartial('/survey/questions/answer/listradio/rows/answer_row', array(
+));
+$sRows .= App()->twigRenderer->renderQuestion('/survey/questions/answer/listradio/rows/answer_row', array(
+    'bIsThemeEditor' => true,
     'sDisplayStyle' => '',
     'name'          => '1234X56X79',
     'code'          => 'A2',
     'answer'        => gT('Two'),
     'checkedState'  => 'checked',
     'myfname'       => 'answer22549X14X68A2',
-), true);
-$sRows .= App()->getController()->renderPartial('/survey/questions/answer/listradio/rows/answer_row', array(
+));
+$sRows .= App()->twigRenderer->renderQuestion('/survey/questions/answer/listradio/rows/answer_row', array(
+    'bIsThemeEditor' => true,
     'sDisplayStyle' => '',
     'name'          => '1234X56X79',
     'code'          => 'A3',
     'answer'        => gT('Three'),
     'checkedState'  => '',
     'myfname'       => 'answer22549X14X68A3',
-), true);
-$sRows .= App()->getController()->renderPartial('/survey/questions/answer/listradio/rows/answer_row', array(
+));
+$sRows .= App()->twigRenderer->renderQuestion('/survey/questions/answer/listradio/rows/answer_row', array(
+    'bIsThemeEditor' => true,
     'sDisplayStyle' => '',
     'name'          => '1234X56X79',
     'code'          => 'A4',
     'answer'        => gT('Four'),
     'checkedState'  => '',
     'myfname'       => 'answer22549X14X68A4',
-), true);
-$sRows .= App()->getController()->renderPartial('/survey/questions/answer/listradio/columns/column_footer', array('last'=>true), true);
+));
+$sRows .= App()->twigRenderer->renderQuestion('/survey/questions/answer/listradio/columns/column_footer', array(
+    'bIsThemeEditor' => true,
+    'last'=>true));
 /* rendering */
-App()->getController()->renderPartial('/survey/questions/answer/listradio/answer', array(
-        'sTimer'=>'',
-        'sRows' => $sRows,
-        'name'  => '1234X56X79',
-        'value' => 'A2',
-        'coreClass'=>"ls-answers answers-list radio-list",
+echo App()->twigRenderer->renderQuestion('/survey/questions/answer/listradio/answer', array(
+    'bIsThemeEditor' => true,
+    'sTimer'=>'',
+    'sRows' => $sRows,
+    'name'  => '1234X56X79',
+    'value' => 'A2',
+    'coreClass'=>"ls-answers answers-list radio-list",
 ));
