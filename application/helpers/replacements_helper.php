9dd6a1fff203baeff3742d8969b0bd1c51afe8b7
Markus Flür <fluer.markus@googlemail.com>
2018-06-12 14:19:29 +0200

    Dev: reset some getGlobalConfig to App->getConfig

--- application/helpers/replacements_helper.php
+++ application/helpers/replacements_helper.php
@@ -98,7 +98,7 @@ function templatereplace($line, $replacements = array(), &$redata = array(), $de
     if (isset($thissurvey['templatedir'])) {
         $templatename = $thissurvey['templatedir'];
     } else {
-        $templatename = getGlobalSetting('defaulttheme');
+        $templatename = App()->getConfig('defaulttheme');
     }
     if (!isset($templateurl)) {
         $templateurl = getTemplateURL($templatename)."/";
@@ -250,7 +250,7 @@ function templatereplace($line, $replacements = array(), &$redata = array(), $de
     $_return_to_survey = "";
 
     if (isset($thissurvey['googleanalyticsapikey']) && $thissurvey['googleanalyticsapikey'] === "9999useGlobal9999") {
-        $_googleAnalyticsAPIKey = trim(getGlobalSetting('googleanalyticsapikey'));
+        $_googleAnalyticsAPIKey = trim(App()->getConfig('googleanalyticsapikey'));
     } else if (isset($thissurvey['googleanalyticsapikey']) && trim($thissurvey['googleanalyticsapikey']) != '') {
         $_googleAnalyticsAPIKey = trim($thissurvey['googleanalyticsapikey']);
     } else {
