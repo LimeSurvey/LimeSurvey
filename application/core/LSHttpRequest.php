efc651ffd0a89221fe274d3d086e3089a5ac3ece
Markus Flür <fluer.markus@googlemail.com>
2018-06-22 13:42:57 +0200

    Dev: more testing and refactoring in LSHttpRequest

--- application/core/LSHttpRequest.php
+++ application/core/LSHttpRequest.php
@@ -151,18 +151,18 @@ class LSHttpRequest extends CHttpRequest
 
         $route = Yii::app()->getUrlManager()->parseUrl($this);
         if ($this->enableCsrfValidation) {
+            
+            $validationRoutes = $this->noCsrfValidationRoutes;
+            $validationParams = [];
 
-            $beforeUrlCheck = new PluginEvent('beforeUrlCheck');
-            $beforeUrlCheck->set('routes', $this->noCsrfValidationRoutes);
-            $beforeUrlCheck->set('params', []);
             if(preg_match('/admin/', $route)){
+                $beforeUrlCheck = new PluginEvent('beforeUrlCheck');
+                $beforeUrlCheck->set('routes', $validationRoutes);
+                $beforeUrlCheck->set('params', $validationParams);
                 App()->getPluginManager()->dispatchEvent($beforeUrlCheck);
                 $validationRoutes = $beforeUrlCheck->get('routes');
-                $validationParams = $beforeUrlCheck->get('params');
-            } else {
-                $validationRoutes = [];
-                $validationParams = [];
-            }
+                $validationParams = $beforeUrlCheck->get('params', []);
+            } 
 
             foreach ($validationRoutes as $cr) {
                 if (preg_match('#'.$cr.'#', $route)) {
