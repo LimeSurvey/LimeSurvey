3e3b0b1492aa805a22ccf16804dd165b3d498abf
LouisGac <louis.gac@limesurvey.org>
2018-08-01 11:48:29 +0200

    Fixed issue #13925: Unable to use QID SGQ … in validation equation tip

--- application/helpers/SurveyRuntimeHelper.php
+++ application/helpers/SurveyRuntimeHelper.php
@@ -388,7 +388,7 @@ class SurveyRuntimeHelper
                     $aGroup['aQuestions'][$qid]['mandatory']            = $qa[0]['mandatory'];
                     $aGroup['aQuestions'][$qid]['class']                = $this->getCurrentQuestionClasses($qid);
                     $aGroup['aQuestions'][$qid]['input_error_class']    = $qa[0]['input_error_class'];
-                    $aGroup['aQuestions'][$qid]['valid_message']        = $qa[0]['valid_message'];
+                    $aGroup['aQuestions'][$qid]['valid_message']        = LimeExpressionManager::ProcessString( $qa[0]['valid_message'] );
                     $aGroup['aQuestions'][$qid]['file_valid_message']   = $qa[0]['file_valid_message'];
                     $aGroup['aQuestions'][$qid]['man_message']          = $qa[0]['man_message'];
                     $aGroup['aQuestions'][$qid]['answer']               = LimeExpressionManager::ProcessString($qa[1], $qa[4], null, 3, 1, false, true, false);
@@ -754,11 +754,11 @@ class SurveyRuntimeHelper
         /* quota submitted */
         if ($this->sMove == 'confirmquota') {
             checkCompletedQuota($this->iSurveyid);
-        } 
+        }
         /* quota submitted */
         if ($this->sMove == 'returnfromquota') {
             LimeExpressionManager::JumpTo($this->param['thisstep']);
-        } 
+        }
     }
 
     /**
