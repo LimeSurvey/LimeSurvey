6e33c92311df9c10f0c910e29ee1c7fc20da25c4
Dominik Vitt <dominik.vitt.hr@gmail.com>
2018-08-30 18:34:48 +0200

    Fixed issue #13903: Get parameters do not work with tokenized survey

--- application/helpers/expressions/em_manager_helper.php
+++ application/helpers/expressions/em_manager_helper.php
@@ -5004,12 +5004,11 @@
             $LEM->indexQseq=array();
             $LEM->qrootVarName2arrayFilter=array();
 
-            // set seed key for non-activated survey mode, activated mode already have seed key,
-            // to be able to pass count of startingValues check at next IF 
-            if (!$LEM->surveyOptions['active'] || $LEM->sPreviewMode){
+            // set seed key if it doesn't exist to be able to pass count of startingValues check at next IF 
+            if (!array_key_exists('seed', $_SESSION[$LEM->sessid]['startingValues'])){
                 $_SESSION[$LEM->sessid]['startingValues']['seed'] = '';  
             }
-
+            
             // NOTE: now that we use a seed, count($_SESSION[$LEM->sessid]['startingValues']) start at 1
             if (isset($_SESSION[$LEM->sessid]['startingValues']) && is_array($_SESSION[$LEM->sessid]['startingValues']) && count($_SESSION[$LEM->sessid]['startingValues']) > 1)
             {
