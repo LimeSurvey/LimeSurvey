1b9fa2ce5f3365e0e02527bf4a308d04d122b48f
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2018-08-31 15:02:56 +0200

    Dev: Fix composer.json (composer update --no-dev)

--- third_party/composer/autoload_namespaces.php
+++ third_party/composer/autoload_namespaces.php
@@ -6,6 +6,4 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'PHPMD\\' => array($vendorDir . '/phpmd/phpmd/src/main/php'),
-    'LSS' => array($vendorDir . '/openlss/lib-array2xml'),
 );
