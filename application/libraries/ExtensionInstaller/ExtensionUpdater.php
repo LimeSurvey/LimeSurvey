2a2a1c00ec8e523fab819dba73c3afd6ccbf66d4
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2018-09-26 15:55:47 +0200

    Dev: Add license

--- application/libraries/ExtensionInstaller/ExtensionUpdater.php
+++ application/libraries/ExtensionInstaller/ExtensionUpdater.php
@@ -1,5 +1,17 @@
 <?php
 
+/*
+ * LimeSurvey
+ * Copyright (C) 2007-2015 The LimeSurvey Project Team / Carsten Schmitz
+ * All rights reserved.
+ * License: GNU/GPL License v2 or later, see LICENSE.php
+ * LimeSurvey is free software. This version may have been modified pursuant
+ * to the GNU General Public License, and as distributed it includes or
+ * is derivative of works licensed under the GNU General Public License or
+ * other free or open source software licenses.
+ * See COPYRIGHT.php for copyright notices and details.
+ */
+
 namespace LimeSurvey\ExtensionInstaller;
 
 /**
@@ -25,10 +37,14 @@ abstract class ExtensionUpdater
     /**
      * Use the version fetcher to get info about available updates for 
      * this extension.
-     * @return ?
+     * @return ExtensionUpdate[]
      */
     public function getAvailableUpdates()
     {
-        
+        if (empty($this->versionFetcher)) {
+            throw new \InvalidArgumentException('version fetcher is not set');
+        }
+
+        return [$this->versionFetcher->getLatestVersion()];
     }
 }
--- application/libraries/ExtensionInstaller/VersionFetcher.php
+++ application/libraries/ExtensionInstaller/VersionFetcher.php
@@ -1,5 +1,17 @@
 <?php
 
+/*
+ * LimeSurvey
+ * Copyright (C) 2007-2015 The LimeSurvey Project Team / Carsten Schmitz
+ * All rights reserved.
+ * License: GNU/GPL License v2 or later, see LICENSE.php
+ * LimeSurvey is free software. This version may have been modified pursuant
+ * to the GNU General Public License, and as distributed it includes or
+ * is derivative of works licensed under the GNU General Public License or
+ * other free or open source software licenses.
+ * See COPYRIGHT.php for copyright notices and details.
+ */
+
 namespace LimeSurvey\ExtensionInstaller;
 
 /**
@@ -19,20 +31,4 @@ abstract class VersionFetcher
      * @return string Semantic versioning string.
      */
     abstract public function getLatestVersion();
-
-    /**
-     * 
-     */
-    public function versionIsStable()
-    {
-        
-    }
-
-    /**
-     * 
-     */
-    public function versionIsSecurityUpdate()
-    {
-        
-    }
 }
