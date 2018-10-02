44860eaf5a90bb3f3256e671052f65a54ebb64c8
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2018-10-02 17:11:25 +0200

    Dev: Add empty git version fetcher (TODO)

---
+++ application/libraries/ExtensionInstaller/GitVersionFetcher.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace LimeSurvey\ExtensionInstaller;
+
+/**
+ * @since 2018-09-26
+ * @author Olle Haerstedt
+ */
+class GitVersionFetcher extends VersionFetcher
+{
+    /**
+     * @param string $extensionName
+     * @return ExtensionUpdateInfo
+     */
+    public function getLatestVersion()
+    {
+        // todo
+    }
+}
