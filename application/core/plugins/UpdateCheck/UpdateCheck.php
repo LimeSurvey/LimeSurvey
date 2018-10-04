767937563b67cd0956357a8999a4897d1d03bf7a
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2018-10-04 14:00:16 +0200

    Dev: New plugin to check for extension updates at super admin login

---
+++ application/core/plugins/UpdateCheck/UpdateCheck.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * LimeSurvey
+ * Copyright (C) 2007-2015 The LimeSurvey Project Team / Carsten Schmitz
+ * All rights reserved.
+ * License: GNU/GPL License v2 or later, see LICENSE.php
+ * LimeSurvey is free software. This version may have been modified pursuant
+ * to the GNU General Public License, and as distributed it includes or
+ * is derivative of works licensed under the GNU General Public License or
+ * other free or open source software licenses.
+ * See COPYRIGHT.php for copyright notices and details.
+ */
+
+/**
+ * Plugin to check for extension updates after a super admin logs in.
+ * Uses the ExtensionInstaller library.
+ *
+ * @since 2018-10-04
+ * @author Olle Haerstedt
+ */
+class UpdateCheck extends PluginBase
+{
+    /**
+     * @return void
+     */
+    public function init()
+    {
+        $this->subscribe('afterSuccessfulLogin');
+    }
+
+    /**
+     * @return void
+     */
+    public function afterSuccessfulLogin()
+    {
+    }
+}
