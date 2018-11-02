06a07ec3623dbc25b09b694bc4d4e7b4cb4ce49c
Dominik Vitt <dominik.vitt.hr@gmail.com>
2018-11-02 15:06:10 +0100

    Fixed issue #14192: Mass action on Survey listing doesn't work on second page

--- application/extensions/admin/survey/ListSurveysWidget/views/listSurveys.php
+++ application/extensions/admin/survey/ListSurveysWidget/views/listSurveys.php
@@ -147,7 +147,7 @@
                 'htmlOptions'=>array('style'=>'cursor: pointer;', 'class'=>'hoverAction grid-view'),
                 //'selectionChanged'=>"function(id){window.location='" . Yii::app()->urlManager->createUrl('admin/survey/sa/view/surveyid' ) . '/' . "' + $.fn.yiiGridView.getSelection(id.split(',', 1));}",
                 'ajaxUpdate' => true,
-                'afterAjaxUpdate' => 'window.LS.doToolTip',
+                'afterAjaxUpdate' => 'function(id, data){window.LS.doToolTip();bindListItemclick();}',
                 'template'  => $this->template,
             ));
         ?>
