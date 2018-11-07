0ce6aa9df5f4fe4c6525bfccb473762269e57a1b
LouisGac <louis.gac@limesurvey.org>
2018-11-07 11:10:31 +0100

    Release 3.15.2+181107

--- application/config/version.php
+++ application/config/version.php
@@ -12,9 +12,9 @@
  */
 
 
-$config['versionnumber'] = '3.15.1';
+$config['versionnumber'] = '3.15.2';
 $config['dbversionnumber'] = 355;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
-$config['assetsversionnumber'] = '30064';
+$config['assetsversionnumber'] = '30065';
 return $config;
