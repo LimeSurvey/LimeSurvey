4fe4cb729aaed7af31fb44eae0a50cd9fc6092b3
githubLewis <github@lewiscornick.co.uk>
2018-11-20 11:22:01 +0000

    Update to how the status message is displayed - as per review comments.

--- application/views/admin/global_settings/_emailTestResults.php
+++ application/views/admin/global_settings/_emailTestResults.php
@@ -5,14 +5,15 @@
         <div class="jumbotron message-box">
             <h2><?php eT("Test Email Results"); ?></h2>
             <p class="lead"><?php eT("If errors are showing up you might have to change configuration settings and retest."); ?></p>
-            <p>
-                <?php echo $message;?>         
-            </p>
+            <?php if($success) { ?>
+                <p class="h4 text-success"><?php echo $message;?></p>
+            <?php } else { ?>
+                <p class="h4text-danger"><?php echo $message;?></p>
+                <div> <?php echo $maildebug ?></div>
+            <?php } ?>       
             <p>
                 <a class="btn btn-lg btn-success" href='<?php echo $this->createUrl('admin/globalsettings');?>'><?php eT("Modify settings"); ?></a>
             </p>
-        </div>        
-        
+        </div>
     </div>
-</div>  
-  
+</div>
