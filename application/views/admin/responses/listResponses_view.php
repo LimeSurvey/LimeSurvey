40dda49be4a749202b6624dda533827080af9dfe
Denis Chenu <denis@sondages.pro>
2018-11-29 15:35:34 +0100

    Fixed issue #14301 : potential XSS in browse response + filter subquestion Dev: move from tooltip to popover

--- application/views/admin/responses/listResponses_view.php
+++ application/views/admin/responses/listResponses_view.php
@@ -9,8 +9,6 @@ echo viewHelper::getViewTestTag('surveyResponsesBrowse');
 ?>
 <div class='side-body <?php echo getSideBodyClass(false); ?>'>
     <h3><?php eT('Survey responses'); ?></h3>
-
-
     <!-- Display mode -->
     <div class="text-right in-title">
         <div class="pull-right">
@@ -173,26 +171,12 @@ echo viewHelper::getViewTestTag('surveyResponsesBrowse');
                         {
                             $colName = viewHelper::getFieldCode($fieldmap[$column->name],array('LEMcompat'=>true)); // This must be unique ......
                             $base64jsonFieldMap = base64_encode(json_encode($fieldmap[$column->name]));
-                            $colDetails = '';
-
-                            if(isset($fieldmap[$column->name]['subquestion'])){
-                                $colDetails .=  '<em>'.$fieldmap[$column->name]['subquestion'].'</em><br/>';
-                            }
-
-                            if(isset($fieldmap[$column->name]['subquestion1'])){
-                                $colDetails .=  '<em>'.$fieldmap[$column->name]['subquestion1'].'</em><br/>';
-                            }
-
-                            if(isset($fieldmap[$column->name]['subquestion2'])){
-                                $colDetails .=  '<em>'.$fieldmap[$column->name]['subquestion2'].'</em><br/>';
-                            }
-
-
-                            $colDetails .= ellipsize($fieldmap[$column->name]['question'], $model->ellipsize_header_value);
+                            $colDetails = viewHelper::getFieldText($fieldmap[$column->name],array('abbreviated'=>$model->ellipsize_header_value,'separator'=>array('<br>','')));
+                            $colTitle = viewHelper::getFieldText($fieldmap[$column->name],array('afterquestion'=>"<hr>"));
 
                             $aColumns[]=
                                 array(
-                                    'header' => '<span data-toggle="tooltip" data-placement="bottom" title="'.quoteText(strip_tags($fieldmap[$column->name]['question'])).'">'.$colName.' <br/> '.$colDetails.'</span>',
+                                    'header' => '<span data-toggle="popover" data-trigger="hover focus" data-placement="bottom" title="'.$colName.'" data-content="'.$colTitle.'" data-html="1">'.$colName.' <br/> '.$colDetails.'</span>',
                                     'headerHtmlOptions'=>array('style'=>'min-width: 350px;'),
                                     'name' => $column->name,
                                     'type' => 'raw',
@@ -209,7 +193,7 @@ echo viewHelper::getViewTestTag('surveyResponsesBrowse');
                         'id'            => 'responses-grid',
                         'ajaxUpdate'    => 'responses-grid',
                         'ajaxType'      => 'POST',
-                        'afterAjaxUpdate'=>'js:function(id, data){ LS.resp.bindScrollWrapper(); onUpdateTokenGrid(); }',
+                        'afterAjaxUpdate'=>'js:function(id, data){ LS.resp.bindScrollWrapper(); onUpdateTokenGrid();$(".grid-view [data-toggle=\'popover\']").popover({container:\'body\'}); }',
                         'template'      => "{items}\n<div id='ListPager'><div class=\"col-sm-12\" id=\"massive-action-container\">$massiveAction</div><div class=\"col-sm-12 pager-container ls-ba \">{pager}</div><div class=\"col-sm-12 summary-container\">{summary}</div></div>",
                         'summaryText'   => gT('Displaying {start}-{end} of {count} result(s).').' '. sprintf(gT('%s rows per page'),
                             CHtml::dropDownList(
@@ -234,6 +218,7 @@ echo viewHelper::getViewTestTag('surveyResponsesBrowse');
                 jQuery(document).on("change", "#pageSize", function(){
                     $.fn.yiiGridView.update("responses-grid",{ data:{ pageSize: $(this).val() }});
                 });
+                $(".grid-view [data-toggle=\'popover\']").popover({container:\'body\'});
                 ';
             App()->getClientScript()->registerScript('listresponses', $scriptVars, LSYii_ClientScript::POS_BEGIN);
             App()->getClientScript()->registerScript('listresponses', $script, LSYii_ClientScript::POS_POSTSCRIPT);
