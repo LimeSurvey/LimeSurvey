37988eb5b660b1e55b44113dd191be7c554179d6
Denis Chenu <denis@sondages.pro>
2018-12-22 14:54:21 +0100

    Fixed issue #14312: Import participant CSV is not possible with surveys update right Dev: fix it in Permission::model, then fixed for RC and plugin too

--- application/models/Permission.php
+++ application/models/Permission.php
@@ -680,12 +680,14 @@ class Permission extends LSActiveRecord
         if (!$oSurvey) {
             return false;
         }
-        // If the user has the permission to update all other surveys he may import/export as well
-        if ($this->hasGlobalPermission('surveys', 'update', $iUserID) && $sPermission == 'token' && ($sCRUD == 'import' || $sCRUD == 'export')) {
-            $sCRUD = 'update';
-        }
         // Get global correspondance for surveys rigth
-        $sGlobalCRUD = ($sCRUD == 'create' || ($sCRUD == 'delete' && $sPermission != 'survey')) ? 'update' : $sCRUD;
+        $sGlobalCRUD = $sCRUD;
+        if(($sCRUD == 'create' || $sCRUD == 'import')) {// Create and import (token, reponse , question content …) need only allow update surveys
+            $sGlobalCRUD = 'update';
+        }
+        if(($sCRUD == 'delete' && $sPermission != 'survey')) {// Delete (token, reponse , question content …) need only allow update surveys
+            $sGlobalCRUD = 'update';
+        }
         return $this->hasGlobalPermission('surveys', $sGlobalCRUD, $iUserID) || $this->hasPermission($iSurveyID, 'survey', $sPermission, $sCRUD, $iUserID);
     }
 
