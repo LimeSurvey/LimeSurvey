91ce9479dbb462a72d2c0a97f1f02e99824d0f50
Dominik Vitt <dominik.vitt.hr@gmail.com>
2019-01-08 11:11:57 +0100

    Fixed issue: error message when trying to export response results

--- application/helpers/admin/export/SurveyDao.php
+++ application/helpers/admin/export/SurveyDao.php
@@ -43,12 +43,14 @@ class SurveyDao
 
         $survey->groups = QuestionGroup::model()->findAll(array("condition" => 'sid='.$intId, 'order'=>'group_order'));
         $survey->questions = Question::model()->findAll(array("condition" => 'sid='.$intId, 'order'=>'question_order'));
-        $aAnswers = Answer::model()->with('question', array('condition'=>'sid='.$intId))->findAll(array('order' => 'question.question_order, t.scale_id, sortorder'));
+        $aAnswers = Answer::model()->with(array('answerL10ns', 'question'), array('condition'=>'sid='.$intId.' AND language = "'.$lang.'"'))->findAll(array('order' => 'question.question_order, t.scale_id, sortorder'));
         foreach ($aAnswers as $aAnswer) {
             if(!empty($oOptions->stripHtmlCode) && $oOptions->stripHtmlCode == 1  && Yii::app()->controller->action->id !='remotecontrol'){
-                $aAnswer=stripTagsFull($aAnswer);
+                $answer=stripTagsFull($aAnswer->answerL10ns[$lang]->answer);
+            } else {
+                $answer=$aAnswer->answerL10ns[$lang]->answer;
             }
-            $survey->answers[$aAnswer['qid']][$aAnswer['scale_id']][$aAnswer['code']] = $aAnswer;
+            $survey->answers[$aAnswer->question->qid][$aAnswer->scale_id][$aAnswer->code] = $answer;
         }
         //Load language settings for requested language
         $sQuery = 'SELECT * FROM {{surveys_languagesettings}} WHERE surveyls_survey_id = '.$intId.' AND surveyls_language = \''.$lang.'\';';
