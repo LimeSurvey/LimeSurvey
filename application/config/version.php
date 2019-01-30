97c5c5d6f618b4fd7a8d763efa7777a05cc24645
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2019-01-30 11:29:48 +0100

    Release 3.15.8+190130

--- application/config/version.php
+++ application/config/version.php
@@ -12,9 +12,9 @@
  */
 
 
-$config['versionnumber'] = '3.15.7';
+$config['versionnumber'] = '3.15.8';
 $config['dbversionnumber'] = 355;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
-$config['assetsversionnumber'] = '30073';
+$config['assetsversionnumber'] = '30074';
 return $config;
