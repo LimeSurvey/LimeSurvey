d64aa86ab2e21481b250172a4ac5b7cf24d99d48
Dominik Vitt <dominik.vitt.hr@gmail.com>
2019-02-15 16:15:11 +0100

    Fixed issue: error while submitting survey

--- application/helpers/frontend_helper.php
+++ application/helpers/frontend_helper.php
@@ -277,7 +277,7 @@ function checkUploadedFileValidity($surveyid, $move, $backok = null)
             $fields = explode("|", $_POST['fieldnames']);
 
             foreach ($fields as $field) {
-                if ($fieldmap[$field]['type'] == Question::QT_VERTICAL_FILE_UPLOAD && !strrpos($fieldmap[$field]['fieldname'], "_filecount")) {
+                if (array_key_exists($field, $fieldmap) && $fieldmap[$field]['type'] == Question::QT_VERTICAL_FILE_UPLOAD && !strrpos($fieldmap[$field]['fieldname'], "_filecount")) {
                     $validation = QuestionAttribute::model()->getQuestionAttributes($fieldmap[$field]['qid']);
 
                     $filecount = 0;
