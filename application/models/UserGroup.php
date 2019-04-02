cb81914c361f4cb070b217282522f51fed4dc8be
Denis Chenu <denis@sondages.pro>
2019-04-02 10:14:19 +0200

    Fixed issue #14713: Persistent XSS in user group management Dev: deprecate usage of CustomFormatter::formatLongText used only here Dev: fix it with encoding value (always encode or filter …) Dev: just set formatter to text since no html editor in group Dev: else can use html

--- application/models/UserGroup.php
+++ application/models/UserGroup.php
@@ -295,7 +295,6 @@ class UserGroup extends LSActiveRecord
                 'header' => gT('Description'),
                 'name' => 'description',
                 'value'=> '$data->description',
-                'type' => 'LongText',
                 'htmlOptions' => array('class' => 'col-md-5'),
             ),
 
