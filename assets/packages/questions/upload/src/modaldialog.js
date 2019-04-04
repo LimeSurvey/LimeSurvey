05988cf72a1784594b2fafb855e280ae485bcf48
Denis Chenu <denis@sondages.pro>
2019-04-04 15:25:43 +0200

    Dev: fix js on modaldialog.js

--- assets/packages/questions/upload/src/modaldialog.js
+++ assets/packages/questions/upload/src/modaldialog.js
@@ -136,7 +136,7 @@ var displayUploadedFiles = function ( filecount, fieldname, show_title, show_com
             if (show_comment != 0)
                 display += '<td class="upload comment">'+htmlspecialchars(item.comment)+'</td>';
             display +='<td class="upload edit">'+htmlspecialchars(decodeURIComponent(item.name))+'</td><td>'+'<a class="btn btn-primary" onclick="javascript:upload_'+fieldname+'();$(\'#upload_'+fieldname+'\').click();"><span class="fa fa-pencil"></span>&nbsp;'+uploadLang.editFile+'</a></td></tr>';
-        }
+        });
         display += '</tbody></table>';
 
         $('#'+fieldname+'_uploadedfiles').html(display);
