a7f084c6383b0b468fff0c2a272bbda9da437e5d
Denis Chenu <denis@sondages.pro>
2019-04-09 16:17:12 +0200

    Revert "Release 3.17.1+190408"
    This reverts commit 9d1f0ebf8152e3ab5646d679db062f246ffeab51.

--- application/config/version.php
+++ application/config/version.php
@@ -12,9 +12,9 @@
  */
 
 
-$config['versionnumber'] = '3.17.2';
+$config['versionnumber'] = '3.17.0';
 $config['dbversionnumber'] = 356;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
-$config['assetsversionnumber'] = '30083';
+$config['assetsversionnumber'] = '30082';
 return $config;
