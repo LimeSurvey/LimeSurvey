a7c4b8bb6e722db5c7b8a32d5da2bd2598091410
Markus Flür <fluer.markus@googlemail.com>
2019-04-18 13:58:18 +0200

    Dev: forgot to increment database

--- application/helpers/update/updatedb_helper.php
+++ application/helpers/update/updatedb_helper.php
@@ -2693,7 +2693,7 @@ function db_upgrade_all($iOldDBVersion, $bSilent = false)
         if ($iOldDBVersion < 410) {
             $oTransaction = $oDB->beginTransaction();            
             $oDB->createCommand()->addColumn('{{question_l10ns}}', 'script', " text NULL default NULL");
-            $oDB->createCommand()->update('{{settings_global}}',array('stg_value'=>409),"stg_name='DBVersion'");
+            $oDB->createCommand()->update('{{settings_global}}',array('stg_value'=>410),"stg_name='DBVersion'");
             $oTransaction->commit();
         }
       
