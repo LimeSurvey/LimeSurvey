d1b56a4c8a87a47bcbeecff0e918671f5c07a562
Denis Chenu <denis@sondages.pro>
2019-05-02 16:25:23 +0200

    Dev: Boxes icon need space

--- application/models/Boxes.php
+++ application/models/Boxes.php
@@ -8,6 +8,7 @@
  * @property integer $position
  * @property string $url
  * @property string $title
+ * @property string $ico the icon class
  * @property string $desc Description
  * @property string $page
  * @property integer $usergroup UserGroup ID
@@ -30,7 +31,7 @@ class Boxes extends CActiveRecord
             array('url', 'match', 'pattern'=>'/(http:\/\/)?[a-zA-Z]([a-zA-Z0-9-_?&"\'=]\/?)*/'),
             array('position', 'numerical', 'integerOnly'=>true),
             array('usergroup', 'numerical', 'integerOnly'=>true, 'min'=>-3),
-            array('ico', 'match', 'pattern'=> '/^[A-Za-z0-9_\-]+$/u','message'=> gT('Icon can contain only alphanumeric character, underscore or minus.')),
+            array('ico', 'match', 'pattern'=> '/^[A-Za-z0-9_ \-]+$/u','message'=> gT('Icon must be a simple class name (alphanumeric, space, minus and underscore).')),
             // The following rule is used by search().
             // @todo Please remove those attributes that should not be searched.
             array('id, position, url, title, ico, desc, page, usergroup', 'safe', 'on'=>'search'),
