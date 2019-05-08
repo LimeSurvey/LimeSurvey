4c56d2025a4ed48d9f73972357df68ee65fbcd8f
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2019-05-08 12:19:10 +0200

    Dev: Memoize result from RDP_Tokenize

--- application/helpers/expressions/em_core_helper.php
+++ application/helpers/expressions/em_core_helper.php
@@ -2200,6 +2200,15 @@ class ExpressionManager
      */
     private function RDP_Tokenize($sSource, $bOnEdit = false)
     {
+        /** @var array Memoize result in local cache variable. */
+        static $cache = [];
+
+        /** @var string */
+        $cacheKey = $sSource . json_encode($bOnEdit);
+        if (isset($cache[$cacheKey])) {
+            return $cache[$cacheKey];
+        }
+
         // $aInitTokens = array of tokens from equation, showing value and offset position.  Will include SPACE.
         if ($bOnEdit) {
                     $aInitTokens = preg_split($this->RDP_TokenizerRegex, $sSource, -1, (PREG_SPLIT_DELIM_CAPTURE | PREG_SPLIT_OFFSET_CAPTURE));
@@ -2227,9 +2236,10 @@ class ExpressionManager
                 }
             }
         }
+
+        $cache[$cacheKey] = $aTokens;
         return $aTokens;
     }
-
 
     /**
      * Show a table of allowable Expression Manager functions
