dc3ee5cef343e956f87a03774e3378326bfb5354
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2019-05-24 13:04:05 +0200

    Dev: emcache is always disabled by default

--- application/config/internal.php
+++ application/config/internal.php
@@ -186,8 +186,9 @@ $internalConfig = array(
             'class' => defined('YII_DEBUG') && YII_DEBUG ? 'system.caching.CDummyCache' : 'CFileCache',
         ),
         // For more info about the emcache, see application/helpers/expressions/em_cache_helper.php.
+        // Disabled by default. Enable by adding emcache in config.php after installation.
         'emcache'=>array(
-            'class' => defined('YII_DEBUG') && YII_DEBUG ? 'system.caching.CDummyCache' : 'CFileCache',
+            'class' => 'system.caching.CDummyCache'
         ),
         'db' => array(
             'schemaCachingDuration' => 3600,
