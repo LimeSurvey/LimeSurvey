1a697b317182e39a2b7a0b3650dcf742d3f7ff46
Denis Chenu <denis@sondages.pro>
2019-05-28 09:20:16 +0200

    Dev: Yii have is own clean excepetion message, different in debug or not : use it

--- application/controllers/admin/checkintegrity.php
+++ application/controllers/admin/checkintegrity.php
@@ -522,7 +522,6 @@ class CheckIntegrity extends Survey_Common_Action
         $sDBPrefix = Yii::app()->db->tablePrefix;
         $aResult = Yii::app()->db->createCommand(dbSelectTablesLike('{{survey}}\_%'))->queryColumn();
         $sSurveyIDs = Yii::app()->db->createCommand('select sid from {{surveys}}')->queryColumn();
-
         foreach ($aResult as $aRow) {
             $sTableName = (string) substr($aRow, strlen($sDBPrefix));
             if ($sTableName == 'survey_links' || $sTableName == 'survey_url_parameters') {
@@ -535,12 +534,8 @@ class CheckIntegrity extends Survey_Common_Action
                     $sDate = (string) date('YmdHis').rand(1, 1000);
                     $sOldTable = "survey_{$iSurveyID}";
                     $sNewTable = "old_survey_{$iSurveyID}_{$sDate}";
-                    try {
-                        Yii::app()->db->createCommand()->renameTable("{{{$sOldTable}}}", "{{{$sNewTable}}}");
-                        $bDirectlyFixed = true;
-                    } catch (CDbException $e) {
-                        safeDie('Couldn\'t make backup of the survey table. Please try again. The database reported the following error:<br />'.htmlspecialchars($e).'<br />');
-                    }
+                    Yii::app()->db->createCommand()->renameTable("{{{$sOldTable}}}", "{{{$sNewTable}}}");
+                    $bDirectlyFixed = true;
                 }
             }
         }
@@ -554,12 +549,8 @@ class CheckIntegrity extends Survey_Common_Action
                 $sDate = (string) date('YmdHis').rand(1, 1000);
                 $sOldTable = "tokens_{$iSurveyID}";
                 $sNewTable = "old_tokens_{$iSurveyID}_{$sDate}";
-                try {
-                    Yii::app()->db->createCommand()->renameTable("{{{$sOldTable}}}", "{{{$sNewTable}}}");
-                    $bDirectlyFixed = true;
-                } catch (CDbException $e) {
-                    safeDie('Couldn\'t make backup of the survey table. Please try again. The database reported the following error:<br />'.htmlspecialchars($e).'<br />');
-                }
+                Yii::app()->db->createCommand()->renameTable("{{{$sOldTable}}}", "{{{$sNewTable}}}");
+                $bDirectlyFixed = true;
             }
         }
 
