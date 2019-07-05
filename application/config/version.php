b9a1f7d28fbbc21c96a4d304329e8d000019409e
lacrioque <markus.fluer@limesurvey.org>
2019-07-05 17:27:46 +0200

    Release 4.0.0-RC1

--- application/config/version.php
+++ application/config/version.php
@@ -11,9 +11,9 @@
  * See COPYRIGHT.php for copyright notices and details.
  */
 
-$config['versionnumber'] ='4.0.0-beta';
+$config['versionnumber'] ='4.0.0-RC1';
 $config['dbversionnumber'] = 418;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
-$config['assetsversionnumber'] = '30089';
+$config['assetsversionnumber'] = '30090';
 return $config;
