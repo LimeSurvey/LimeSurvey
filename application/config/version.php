71de5ce107acd4a02ad8376cc704aaf83e607b86
Eddy Lackmann <a.eddy@hotmail.de>
2019-07-22 15:13:35 +0200

    Release 3.17.8+190722

--- application/config/version.php
+++ application/config/version.php
@@ -12,9 +12,9 @@
  */
 
 
-$config['versionnumber'] = '3.17.7';
+$config['versionnumber'] = '3.17.8';
 $config['dbversionnumber'] = 359;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
-$config['assetsversionnumber'] = '30089';
+$config['assetsversionnumber'] = '30090';
 return $config;
