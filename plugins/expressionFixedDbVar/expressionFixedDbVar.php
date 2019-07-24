ee28e01723bc0928e68cd10502e0f445111dbabe
Denis Chenu <denis@sondages.pro>
2019-07-24 20:33:23 +0200

    New feature #08858: Extend EM so we can get the question text of multiple choice and array questions via questioncode.question Dev: by simple plugin Dev: no subquestion.question for array/text and arry/number  (Y axis/X axis)

--- plugins/expressionFixedDbVar/expressionFixedDbVar.php
+++ plugins/expressionFixedDbVar/expressionFixedDbVar.php
@@ -5,7 +5,7 @@
  * @author Denis Chenu <denis@sondages.pro>
  * @copyright 2019 Denis Chenu <http://www.sondages.pro>
  * @license GPL version 3
- * @version 0.0.0
+ * @version 1.0.0
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
