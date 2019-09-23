554b5dd39f6a1a95dcf9a0e8c824ede0d151e709
Denis Chenu <denis@sondages.pro>
2019-09-23 09:31:58 +0200

    Fixed issue #15309: Special characters in page "Error 403" shown as entities Dev: preview survey by admin without view right

--- application/controllers/survey/index.php
+++ application/controllers/survey/index.php
@@ -227,7 +227,7 @@ class index extends CAction
 
                 App()->getPluginManager()->dispatchEvent($event);
                 if(Permission::getUserId()) {
-                    throw new CHttpException(403, gT("We are sorry but you don't have permissions to do this."));
+                    throw new CHttpException(403, gT("We are sorry but you don't have permissions to do this.",'unescaped'));
                 }
                 throw new CHttpException(401, gT("We are sorry but you don't have permissions to do this.",'unescaped'));
             }
