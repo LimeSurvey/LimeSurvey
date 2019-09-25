9a07df49249723d2642d1636a38c5505cceb982e
Jessica Höck <jessica.hoeck@limesurvey.org>
2019-09-25 10:49:39 +0200

    Update tests

--- third_party/composer/autoload_namespaces.php
+++ third_party/composer/autoload_namespaces.php
@@ -6,4 +6,7 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'PHPMD\\' => array($vendorDir . '/phpmd/phpmd/src/main/php'),
+    'LSS' => array($vendorDir . '/openlss/lib-array2xml'),
+    'JsonMapper' => array($vendorDir . '/netresearch/jsonmapper/src'),
 );
