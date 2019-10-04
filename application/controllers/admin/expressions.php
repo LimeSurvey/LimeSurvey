33bb396294cd25a981234d71c6282fddd01dc8a7
Denis Chenu <denis@sondages.pro>
2019-10-04 16:32:15 +0200

    Dev: Fixed issue PHP Notice in question logic file

--- application/controllers/admin/expressions.php
+++ application/controllers/admin/expressions.php
@@ -55,9 +55,9 @@ class Expressions extends Survey_Common_Action
             //header("Content-type: text/html; charset=UTF-8"); // needed for correct UTF-8 encoding
             $sAction = Yii::app()->request->getQuery('sa', false);
             if ($sAction) {
-                            $this->test($sAction, $aData);
+                $this->test($sAction, $aData);
             } else {
-                            $this->_renderWrappedTemplate('expressions', 'test_view', $aData);
+                $this->_renderWrappedTemplate('expressions', 'test_view', $aData);
             }
         }
     }
@@ -80,8 +80,7 @@ class Expressions extends Survey_Common_Action
         
         $gid = Yii::app()->request->getParam('gid', null);
         $qid = Yii::app()->request->getParam('qid', null);
-        
-        
+
         $oSurvey = Survey::model()->findByPk($sid);
 
         $language = Yii::app()->request->getParam('lang', null); 
@@ -93,6 +92,8 @@ class Expressions extends Survey_Common_Action
         $aData['lang'] = $language;
 
         $aData['sid'] = $sid;
+        $aData['gid'] = $gid;
+        $aData['qid'] = $qid;
         $aData['title_bar']['title'] = gT("Survey logic file");
         $aData['subaction'] = gT("Survey logic file");
         $aData['sidemenu']['state'] = false;
