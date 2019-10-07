40206ef692dd4a7d29799027bcbe805a1c3afcfb
Eddy Lackmann <eddy.lackmann@limesurvey.org>
2019-10-07 15:39:20 +0200

    Fixed issue #15354:  Add User - with non valid email adress causes no screen-message for this error

--- application/views/admin/usermanagement/partial/addedituser.php
+++ application/views/admin/usermanagement/partial/addedituser.php
@@ -34,7 +34,7 @@
         </div>
         <div class="row ls-space margin top-5">
             <?php echo $form->labelEx($oUser,'email', ['for'=>'User_Form_email']); ?>
-            <?php echo $form->textField($oUser,'email', ['id'=>'User_Form_email']); ?>
+            <?php echo $form->emailField($oUser,'email', ['id'=>'User_Form_email']); ?>
             <?php echo $form->error($oUser,'email'); ?>
         </div>
         <?php if(!$oUser->isNewRecord) { ?> 
