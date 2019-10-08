2ddc51ee8c30930dcd6101e161ca8b80712985e6
eddylackmann <eddy.lackmann@limesurvey.org>
2019-10-08 10:29:16 +0200

    Release 3.19.0+191008

--- application/config/version.php
+++ application/config/version.php
@@ -12,9 +12,9 @@
  */
 
 
-$config['versionnumber'] = '3.18.0';
+$config['versionnumber'] = '3.19.0';
 $config['dbversionnumber'] = 359;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
-$config['assetsversionnumber'] = '30100';
+$config['assetsversionnumber'] = '30101';
 return $config;
