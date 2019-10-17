548a551b6059f65184ffe69de8a4051f5ff46575
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2019-10-17 11:26:25 +0200

    Dev: Correct the workshop test for missing green bar

--- tests/functional/backend/NoGreenBarTest.php
+++ tests/functional/backend/NoGreenBarTest.php
@@ -1,49 +1,75 @@
 <?php
 
-namespace ls\tests\controllers;
+namespace ls\tests;
 
-use ls\tests\TestBaseClassView;
+use Facebook\WebDriver\Exception\NoSuchElementException;
 
+/**
+ * Small example test from workshop.
+ *
+ * @see https://bugs.limesurvey.org/view.php?id=15336
+ */
 class NoGreenBarTest extends TestBaseClassWeb
 {
-	public function testNoGreenBar()
-	{
-		// Import suprvey.
-		$surveyFile =  'tests/data/surveys/survey_archive_358746_no_green_bar.lsa';
-		self::importSurvey($surveyFile);
+    /**
+     * Setup green bar test.
+     */
+    public function setup()
+    {
+        // Import suprvey.
+        $surveyFile =  'tests/data/surveys/survey_archive_358746_no_green_bar.lsa';
+        self::importSurvey($surveyFile);
 
-		$web = self::$webDriver;
+        $username = getenv('ADMINUSERNAME');
+        if (!$username) {
+            $username = 'admin';
+        }
 
-		 try {
-		    $urlMan = \Yii::app()->urlManager;
-		    $urlMan->setBaseUrl('http://' . self::$domain . '/index.php');
-		    //http://localhost/limesurvey/index.php?r=admin/tokens/sa/addnew/surveyid/358746
-		    $url = $urlMan->createUrl(
-			'admin/tokens',
-			array(
-				'sa'=>'addnew',
-				'surveyid'=>self::$testSurvey->sid,
-			)
-		    );
-		    $web = self::$webDriver;
-		    $web->get($url);
-		    $input = $web->findById('firstname');
-		    $input->sendKey('dummy name');
-		    $savebutton = $web->findById('save');
-		    $savebutton->click();
-		    sleep(1);
-		    try {
-			    $web->findById('greenbar');
-			    $this->assertTrue(true, 'all ok');
-		    } catch (NoSuchElementException $ex) {
-			    $this->assertTrue(false, 'could not find green bar');
-		    }
-		 } catch (\Exception $exception) {
-		    self::$testHelper->takeScreenshot(self::$webDriver, __CLASS__ . '_' . __FUNCTION__);
-		    $this->assertFalse(
-			true,
-			self::$testHelper->javaTrace($ex)
-            );
-		 }
-	}
+        $password = getenv('PASSWORD');
+        if (!$password) {
+            $password = 'password';
+        }
+
+        // Browser login.
+        self::adminLogin($username, $password);
+
+        // Permission to everything.
+        \Yii::app()->session['loginID'] = 1;
+
+        $web = self::$webDriver;
+
+        $urlMan = \Yii::app()->urlManager;
+        $urlMan->setBaseUrl('http://' . self::$domain . '/index.php');
+        $url = $urlMan->createUrl(
+            'admin/tokens',
+            [
+                'sa'=>'addnew',
+                'surveyid'=>self::$testSurvey->sid,
+            ]
+        );
+        $web = self::$webDriver;
+        $web->get($url);
+        $input = $web->findById('firstname');
+        $input->sendKeys('dummy name');
+        sleep(1);
+        $savebutton = $web->findById('save-button');
+        $savebutton->click();
+        sleep(1);
+    }
+
+    /**
+     * Test that we have green bar in confirm view.
+     */
+    public function testNoGreenBar()
+    {
+        $this->markTestIncomplete('See bug #15336');
+
+        $web = self::$webDriver;
+        try {
+            $web->findById('breadcrumb-container');
+            $this->assertTrue(true, 'Found green bar');
+        } catch (NoSuchElementException $ex) {
+            $this->assertTrue(false, 'Found no green bar, NoSuchElementException');
+        }
+    }
 }
