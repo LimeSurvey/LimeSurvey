1848578e65f63599700eadcc56ba5eb18898f283
lacrioque <markus.fluer@limesurvey.org>
2019-10-30 15:47:48 +0100

    Dev: recompile embeddables for IE11 compatibility

--- assets/packages/embeddables/build/embeddables.js
+++ assets/packages/embeddables/build/embeddables.js
@@ -1865,13 +1865,7 @@
 	$(document).on('ready pjax:scriptcomplete', function () {
 	  $('oembed').each(function (i, item) {
 	    if ($(this).find(".svgcontainer").length == 0) {
-	      $(this).append(`
-                <div class="svgcontainer">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 26 26">
-                        <polygon class="play-btn__svg" points="9.33 6.69 9.33 19.39 19.3 13.04 9.33 6.69"/>
-                        <path class="play-btn__svg" d="M26,13A13,13,0,1,1,13,0,13,13,0,0,1,26,13ZM13,2.18A10.89,10.89,0,1,0,23.84,13.06,10.89,10.89,0,0,0,13,2.18Z"/>
-                    </svg>
-                </div>`);
+	      $(this).append("\n                <div class=\"svgcontainer\">\n                    <svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 26 26\">\n                        <polygon class=\"play-btn__svg\" points=\"9.33 6.69 9.33 19.39 19.3 13.04 9.33 6.69\"/>\n                        <path class=\"play-btn__svg\" d=\"M26,13A13,13,0,1,1,13,0,13,13,0,0,1,26,13ZM13,2.18A10.89,10.89,0,1,0,23.84,13.06,10.89,10.89,0,0,0,13,2.18Z\"/>\n                    </svg>\n                </div>");
 	    }
 	  });
 	  $('oembed').off('click.embeddable');
@@ -1888,10 +1882,10 @@
 	      googlemaps: true
 	    });
 	    $(this).find(".svgcontainer").remove();
-	    const url = $(this).attr('url');
-	    window.embedo.load(this, url).done(result => {
+	    var url = $(this).attr('url');
+	    window.embedo.load(this, url).done(function (result) {
 	      console.ls.log(result);
-	    }).fail(result => {
+	    }).fail(function (result) {
 	      console.ls.error(result);
 	    });
 	  });
