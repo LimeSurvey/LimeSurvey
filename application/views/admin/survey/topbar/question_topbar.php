b4282ffb3dcad1b6c007d37ed8f1967c6e09141b
Marc Leonhardt <marc.leonhardt@limesurvey.org>
2019-12-11 18:29:32 +0100

    Fixed issue #407: Disable import page/question buttons on active survey

--- application/views/admin/survey/topbar/question_topbar.php
+++ application/views/admin/survey/topbar/question_topbar.php
@@ -407,13 +407,24 @@ if ($qid == 0) {
 }
 
 if ($ownsImportButton) {
-    $buttons['import'] = [
-        'url' => $this->createUrl("admin/questions/sa/importView/", ["surveyid" => $sid]),
-        'class' => 'btn-default',
-        'id' => 'import-button',
-        'icon' => 'icon-import',
-        'name' => gT('Import Question'),
-    ];
+    if ($Activated) {
+        // survey inactive
+        $buttons['import'] = [
+            'url' => $this->createUrl("admin/questions/sa/importView/", ["surveyid" => $sid]),
+            'class' => 'btn-default',
+            'id' => 'import-button',
+            'icon' => 'icon-import',
+            'name' => gT('Import Question'),
+        ];
+    } else {
+        $buttons['import'] = [
+            'title' => gT("You can not import questions because the survey is currently active."),
+            'class' => 'btn-default',
+            'id' => 'import-button',
+            'icon' => 'icon-import',
+            'name' => gT('Import Question'),
+        ];
+    }
     array_push($topbar['alignment']['left']['buttons'], $buttons['import']);
     array_push($topbarextended['alignment']['left']['buttons'], $buttons['import']);
 }
