3f74473800828e8e7137489ac5e89b03ddf3e3c4
lacrioque <markus.fluer@limesurvey.org>
2019-12-16 10:21:57 +0100

    Dev: added tests to the LocalStorageInterface

---
+++ assets/packages/adminbasics/jest.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    moduleFileExtensions: [
+        'js',
+        'jsx',
+        'json',
+    ],
+    transform: {
+        '^.+\\.jsx?$': 'babel-jest',
+        '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
+    },
+    transformIgnorePatterns: ['<rootDir>/node_modules/'],
+    moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/src/$1'
+    },
+    testMatch: [
+        '**/tests/**/*.spec.(js|jsx|mjs|ts|tsx)|**/__tests__/*.(js|jsx|mjs|ts|tsx)'
+    ],
+    testURL: 'http://localhost/',
+    watchPlugins: [
+        'jest-watch-typeahead/filename',
+        'jest-watch-typeahead/testname'
+    ],
+    collectCoverage: true,
+    collectCoverageFrom: [
+        '<rootDir>/src/**',
+    ],
+    coverageReporters: ['lcov', 'text-summary']
+}
