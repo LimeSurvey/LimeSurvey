692f363f8b5566885e9281f098ccaf1ba5f770ca
eddylackmann <eddy.lackmann@limesurvey.org>
2019-12-16 15:53:58 +0100

    Release 4.0.0-RC12+191217

--- application/config/version.php
+++ application/config/version.php
@@ -11,10 +11,10 @@
  * See COPYRIGHT.php for copyright notices and details.
  */
 
-$config['versionnumber'] = '4.0.0-RC11';
+$config['versionnumber'] = '4.0.0-RC12';
 $config['dbversionnumber'] = 424;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
 $config['templateapiversion']  = 3;
-$config['assetsversionnumber'] = '30109';
+$config['assetsversionnumber'] = '30110';
 return $config;
