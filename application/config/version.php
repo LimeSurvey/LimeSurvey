99dfc5d0062c600efd2c98f0267a06488e12c319
Patrick Teichmann <patrick.teichmann@limesurvey.org>
2020-01-07 10:27:27 +0100

    New feature: Converting LS3 question themes to LS4

--- application/config/version.php
+++ application/config/version.php
@@ -12,7 +12,7 @@
  */
 
 $config['versionnumber'] = '4.0.0-RC14';
-$config['dbversionnumber'] = 424;
+$config['dbversionnumber'] = 425;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
 $config['templateapiversion']  = 3;
--- application/helpers/update/updatedb_helper.php
+++ application/helpers/update/updatedb_helper.php
@@ -3042,6 +3042,23 @@ function db_upgrade_all($iOldDBVersion, $bSilent = false)
             $oDB->createCommand()->update('{{settings_global}}', array('stg_value'=>424), "stg_name='DBVersion'");
             $oTransaction->commit();
         }
+        if ($iOldDBVersion < 425) {
+            $oTransaction = $oDB->beginTransaction();
+            $aUserDirectory = QuestionTheme::getAllQuestionXMLPaths(false, false, true);
+            if (!empty($aUserDirectory)) {
+                reset($aUserDirectory);
+                $aUserXMLPaths = key($aUserDirectory);
+                foreach ($aUserDirectory[$aUserXMLPaths] as $sXMLDirectoryPath) {
+                    $aSuccess = QuestionTheme::convertLS3toLS4($sXMLDirectoryPath);
+                    if ($aSuccess['success']) {
+                        $oQuestionTheme = new QuestionTheme;
+                        $oQuestionTheme->importManifest($sXMLDirectoryPath, true);
+                    };
+                }
+                $oDB->createCommand()->update('{{settings_global}}', array('stg_value' => 425), "stg_name='DBVersion'");
+            }
+            $oTransaction->commit();
+        }
     } catch (Exception $e) {
         Yii::app()->setConfig('Updating', false);
         $oTransaction->rollback();
