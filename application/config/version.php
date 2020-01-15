a85b0a7c4d30732b081846d6210a05e197fad78f
eddylackmann <eddy.lackmann@limesurvey.org>
2020-01-15 11:57:13 +0100

    Release 3.21.5+200115

--- application/config/version.php
+++ application/config/version.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * LimeSurvey
- * Copyright (C) 2007-2019 The LimeSurvey Project Team / Carsten Schmitz
+ * Copyright (C) 2007-2020 The LimeSurvey Project Team / Carsten Schmitz
  * All rights reserved.
  * License: GNU/GPL License v3 or later, see LICENSE.php
  * LimeSurvey is free software. This version may have been modified pursuant
@@ -12,10 +12,10 @@
  */
 
 
-$config['versionnumber'] = '3.21.4';
+$config['versionnumber'] = '3.21.5';
 $config['dbversionnumber'] = 359;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
 $config['templateapiversion'] = 3;
-$config['assetsversionnumber'] = '30112';
+$config['assetsversionnumber'] = '30113';
 return $config;
