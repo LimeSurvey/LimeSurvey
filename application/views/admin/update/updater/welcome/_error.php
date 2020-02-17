c59b23375b2f469d0687decfda6f340dfc0ff879
Eddy Lackmann <eddy.lackmann@limesurvey.org>
2020-02-17 17:23:32 +0100

    Fixed issue: Wrong redirection to enter new key  in Comfort update

--- application/views/admin/update/updater/welcome/_error.php
+++ application/views/admin/update/updater/welcome/_error.php
@@ -74,7 +74,7 @@ switch ($serverAnswer->error)
             <?php eT("Buy a new key"); ?>
         </a>
 
-        <a class="btn btn-default" href="<?php echo $urlNew;?>" role="button" aria-disabled="false">
+        <a class="btn btn-default" href="<?php echo Yii::app()->createUrl("admin/update/sa/managekey");?>" role="button" aria-disabled="false">
             <?php eT("Enter a new key"); ?>
         </a>
 <?php endif; ?>
