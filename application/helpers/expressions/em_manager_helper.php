b2eb85750fb8bd5411da0396c14e7154864a54e4
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2020-02-24 16:41:33 +0100

    Dev: More precise error message when response can not be found in database

--- application/helpers/expressions/em_manager_helper.php
+++ application/helpers/expressions/em_manager_helper.php
@@ -5624,7 +5624,7 @@
                     $oResponse = Response::model($this->sid)->findByPk($_SESSION[$this->sessid]['srid']);
                     if (empty($oResponse)) {
                         // This can happen if admin deletes incomple response while survey is running.
-                        $message = submitfailed('', $this->gT('Error on response update'));
+                        $message = submitfailed($this->gT('The data could not be saved because the reponse does not exist in the database.'));
                         LimeExpressionManager::addFrontendFlashMessage('error', $message, $this->sid);
                         return;
                     }
