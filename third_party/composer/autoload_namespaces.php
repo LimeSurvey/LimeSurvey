ff0f30e1d68c6178743ac618665171e739953e43
Carsten Schmitz <carsten.schmitz@limesurvey.org>
2020-03-09 20:13:59 +0100

    Fixed issue: Release completely broken

--- third_party/composer/autoload_namespaces.php
+++ third_party/composer/autoload_namespaces.php
@@ -6,7 +6,4 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'PHPMD\\' => array($vendorDir . '/phpmd/phpmd/src/main/php'),
-    'LSS' => array($vendorDir . '/openlss/lib-array2xml'),
-    'JsonMapper' => array($vendorDir . '/netresearch/jsonmapper/src'),
 );
