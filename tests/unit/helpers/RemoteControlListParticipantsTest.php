790b736feb9c192e1f910522c46264c03c79a4d0
Olle Harstedt <olleharstedt@yahoo.com>
2020-03-18 16:00:44 +0100

    Dev: Add two working tests (list_participants)

--- tests/unit/helpers/RemoteControlListParticipantsTest.php
+++ tests/unit/helpers/RemoteControlListParticipantsTest.php
@@ -43,10 +43,11 @@ class RemoteControlListParticipantsTest extends TestBaseClass
     }
 
     /**
-     * 
-     * @return 
+     * Test so that validuntil works with exact equality.
+     *
+     * @return void
      */
-    public function testConditions()
+    public function testConditionEquality()
     {
         \Yii::import('application.helpers.remotecontrol.remotecontrol_handle', true);
         \Yii::import('application.helpers.viewHelper', true);
@@ -77,8 +78,71 @@ class RemoteControlListParticipantsTest extends TestBaseClass
             0,
             999,
             false,
-            ['validuntil', 'validfrom']
+            ['validuntil', 'validfrom'],
+            ['validuntil' => '2020-04-01 15:12:00']
         );
-        var_dump($list);
+
+        $expected = [
+            [
+                'tid' => "1",
+                'token' => "c",
+                'participant_info' => [
+                    'firstname' => "a",
+                    'lastname' => "b",
+                    'email' => "a@a.a"
+                ],
+                'validuntil' => "2020-04-01 15:12:00",
+                'validfrom' => "2020-03-18 15:12:00"
+            ]
+        ];
+
+        $this->assertEquals($expected, $list);
+    }
+
+    /**
+     * Test condition with empty return result.
+     * 
+     * @return 
+     */
+    public function testConditionEmptyResult()
+    {
+        \Yii::import('application.helpers.remotecontrol.remotecontrol_handle', true);
+        \Yii::import('application.helpers.viewHelper', true);
+        \Yii::import('application.libraries.BigData', true);
+
+        /** @var CDbConnection */
+        $dbo = \Yii::app()->getDb();
+
+        /** @var string */
+        $filename = self::$surveysFolder . '/survey_archive_265351_listParticipants.lsa';
+        self::importSurvey($filename);
+
+        // Create handler.
+        $admin   = new \AdminController('dummyid');
+        $handler = new \remotecontrol_handle($admin);
+
+        // Get session key.
+        $sessionKey = $handler->get_session_key(
+            self::$username,
+            self::$password
+        );
+        $this->assertNotEquals(['status' => 'Invalid user name or password'], $sessionKey);
+
+        /** @var array */
+        $list = $handler->list_participants(
+            $sessionKey,
+            self::$surveyId,
+            0,
+            999,
+            false,
+            ['validuntil', 'validfrom'],
+            ['validuntil' => '']
+        );
+
+        $expected = [
+            'status' => 'No survey participants found.'
+        ];
+
+        $this->assertEquals($expected, $list);
     }
 }
