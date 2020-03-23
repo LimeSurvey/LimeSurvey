6691895aaf1632f2abd3857665f3a9c6e8a3a515
Patrick Teichmann <patrick.teichmann@limesurvey.org>
2020-03-23 15:06:34 +0100

    Fixed issue [security] #16019: Stored Cross Site Scripting Vulnerability in survey groups. (Thanks to Matthew Aberegg and Michael Burkey)

--- application/models/SurveysGroups.php
+++ application/models/SurveysGroups.php
@@ -41,6 +41,7 @@ class SurveysGroups extends LSActiveRecord
             array('name', 'match', 'pattern'=> '/^[A-Za-z0-9_\.]+$/u','message'=> gT('Group name can contain only alphanumeric character, underscore or dot.')),
             array('title', 'length', 'max'=>100),
             array('description, created, modified', 'safe'),
+            array('title, name, description', 'LSYii_Validators'),
             // The following rule is used by search().
             // @todo Please remove those attributes that should not be searched.
             array('gsid, name, title, description, owner_id, parent_id, created, modified, created_by', 'safe', 'on'=>'search'),
