1896d5311e289942e22456ed58024abce9548735
Carsten Schmitz <carsten.schmitz@limesurvey.org>
2020-04-29 21:51:23 +0200

    New translation: Kazakh

--- application/helpers/surveytranslator_helper.php
+++ application/helpers/surveytranslator_helper.php
@@ -388,6 +388,13 @@ function getLanguageData($bOrderByNative = false, $sLanguageCode = 'en')
     $supportedLanguages['ja']['dateformat'] = 6;
     $supportedLanguages['ja']['radixpoint'] = 0;
 
+        // Kazakh
+        $supportedLanguages['kk']['description'] = gT('Kazakh');
+        $supportedLanguages['kk']['nativedescription'] = 'Qazaq&#351;a';
+        $supportedLanguages['kk']['rtl'] = false;
+        $supportedLanguages['kk']['dateformat'] = 1;
+        $supportedLanguages['kk']['radixpoint'] = 1;
+
     // Kinyarwanda
     $supportedLanguages['rw']['description'] = gT('Kinyarwanda');
     $supportedLanguages['rw']['nativedescription'] = 'Kinyarwanda';
