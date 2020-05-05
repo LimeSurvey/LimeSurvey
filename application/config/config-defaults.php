5e29e76117bcc416ee02c5d1f84d9d2c2ae942f0
Jessica Höck <thedirtypanda001@gmail.com>
2020-05-05 11:21:56 +0200

    Fixed issue #15624: File upload not working for ios 13.2

--- application/config/config-defaults.php
+++ application/config/config-defaults.php
@@ -72,7 +72,7 @@ $config['allowexportalldb']          = 0; // 0 will only export prefixed tables 
 $config['maxdumpdbrecords']          = 500; // The maximum number of records that would be ouputted in a go during a database backup. Reduce this number if you're getting errors while backing up the entire database.
 $config['deletenonvalues']           = 1; // By default, LimeSurvey does not save responses to conditional questions that haven't been answered/shown. To have LimeSurvey save these responses change this value to 0.
 $config['stringcomparizonoperators'] = 0; // By default, LimeSurvey assumes the numrical order for comparizon operators in conditions. If you need string comparizon operators, set this parameter to 1
-$config['shownoanswer']              = 1; // Show 'no answer' for non mandatory questions ( 0 = no , 1 = yes , 2 = survey admin can choose )
+$config['shownoanswer']              = 2; // Show 'no answer' for non mandatory questions ( 0 = no , 1 = yes , 2 = overridden by survey settings )
 $config['blacklistallsurveys']       = 'N'; // Blacklist all current surveys for participant once the global field is set
 $config['blacklistnewsurveys']       = 'N'; // Blacklist participant for any new added survey once the global field is set
 $config['blockaddingtosurveys']      = 'Y'; // Don't allow blacklisted participants to be added to new survey
@@ -86,7 +86,20 @@ $config['customassetversionnumber']  = 1;        // Used to generate the path of
 
 // Please be very careful if you want to allow SVG files - there are several XSS dangerous security issues
 $config['allowedthemeuploads'] = 'gif,ico,jpg,png,css,js,map,json,eot,ttf,woff,txt,md,xml,woff2,twig'; // File types allowed to be uploaded in the themes section.
+$config['allowedfileuploads'] = [
+    //Documents
+    'xls', 'doc', 'xlsx', 'docx', 'odt', 'ods', 'pdf',
+    //Images - as mentioned above be very careful if you want to allow SVG files
+    'png', 'bmp', 'gif', 'jpg', 'jpeg', 'tif',
+    // Iphone file extensions (version 11 and above)
+    'heif', 'heic', 'heifs', 'heics', 'avci', 'avcs', 'avif', 'avifs',
+    //soundfiles
+    'wav', 'mp3', 'flac', 'aac', 'm4a', 'opus', 'ogg', 'wma', 'mka',
+    //videos
+    'mp4', 'avi', 'mkv', 'mpeg', 'mpg', 'wmv', 'h264', 'h265', 'mov', 'webm', 'divx', 'xvid',
+]; 
 $config['allowedresourcesuploads'] = '7z,aiff,asf,avi,bmp,csv,doc,docx,fla,flv,gif,gz,gzip,ico,jpeg,jpg,mid,mov,mp3,mp4,mpc,mpeg,mpg,ods,odt,pdf,png,ppt,pxd,qt,ram,rar,rm,rmi,rmvb,rtf,sdc,sitd,swf,sxc,sxw,tar,tgz,tif,tiff,txt,vsd,wav,wma,wmv,xls,xlsx,xml,zip,css,js'; // File types allowed to be uploaded in the resources sections, and with the HTML Editor
+$config['allowedpluginuploads'] = 'gif,ico,jpg,png,css,js,map,json,eot,ttf,woff,txt,md,xml,woff2,twig,php,html';
 
 $config['memory_limit'] = '256'; // This sets how much memory LimeSurvey can access in megabytes. 256 MB is the minimum recommended - if you are using PDF functions up to 512 MB may be needed
 
@@ -223,6 +236,12 @@ $config['auth_webserver_autocreate_permissions'] = Array(
 // you created for the administration of  LimeSurvey and if you want to
 // allow these users to be able to use Javascript etc. .
 $config['filterxsshtml'] = true;
+
+// disablescriptwithxss
+// Allow update of script in question
+// true : Default : follow XSS rules
+// false : allowed for all
+$config['disablescriptwithxss'] = true;
 
 // usercontrolSameGroupPolicy
 // If this option is set to true, then limesurvey operators will only 'see'
@@ -626,6 +645,12 @@ $config['proxy_host_port'] = 80;
  */
 $config['forcedsuperadmin'] = array(1);
 
+/**
+ * Set when install a post 4.0, mysql engine before was always MyIsam
+ * Need to be set for old config.php file
+ */
+$config['mysqlEngine'] = "MyISAM";
+
 /** final theme set if default theme didn't exist. Updatebale only via PHP file
  * if this theme didn't exist in standardthemerootdir and url : this can broke your instance.
  * Choose vanilla since it's parent of other's core theme.
@@ -663,6 +688,7 @@ $config['adminimageurl']          = $config['styleurl'].$config['admintheme'].'/
 $config['applicationurl']         = $config['publicurl'].'application/';
 $config['extensionsurl']          = $config['applicationurl'].'extensions/';
 $config['adminstyleurl']          = $config['styleurl'].$config['admintheme'].'/'; // Location of button bar files for admin script
+$config['userfontsurl']            = $config['uploadurl'].'/fonts'; // Location of user's fonts
 
 // Dir
 $config['publicdir']                = $config['rootdir']; // The directory path of the public scripts
@@ -673,6 +699,7 @@ $config['imagedir']                 = $config['rootdir'].DIRECTORY_SEPARATOR."as
 $config['uploaddir']                = $config['rootdir'].DIRECTORY_SEPARATOR."upload";
 $config['standardthemerootdir']     = $config['rootdir'].DIRECTORY_SEPARATOR."themes".DIRECTORY_SEPARATOR."survey"; // The directory path of the standard themes
 $config['publicstylepath']          = $config['rootdir'].DIRECTORY_SEPARATOR.$config['publicstyle'];
+$config['corequestiontypedir']      = "application".DIRECTORY_SEPARATOR."views";
 $config['corequestionthemedir']     = "themes".DIRECTORY_SEPARATOR."question";
 $config['corequestionthemerootdir'] = $config['rootdir'].DIRECTORY_SEPARATOR.$config['corequestionthemedir']; // The directory containing the core's question themes.
 $config['styledir']                 = $config['rootdir'].DIRECTORY_SEPARATOR.'themes'.DIRECTORY_SEPARATOR.'admin';
@@ -680,21 +707,26 @@ $config['questiontypedir']          = $config['rootdir'].DIRECTORY_SEPARATOR.'ap
 $config['userthemerootdir']         = $config['uploaddir'].DIRECTORY_SEPARATOR."themes".DIRECTORY_SEPARATOR."survey"; // The directory path of the user themes
 $config['usertwigextensionrootdir'] = $config['uploaddir'].DIRECTORY_SEPARATOR."twig".DIRECTORY_SEPARATOR."extensions"; // The directory path of the user custom twig extensions
 $config['userquestionthemedir']     = "themes".DIRECTORY_SEPARATOR."question"; // The directory containing the user's question themes.
-$config['userquestionthemerootdir'] = $config['uploaddir'].DIRECTORY_SEPARATOR.$config['userquestionthemedir']; // The directory containing the user's question themes.
+$config['userquestionthemerootdir'] = "upload".DIRECTORY_SEPARATOR.$config['userquestionthemedir']; // The directory containing the user's question themes.
+$config['userfontsrootdir']          = $config['uploaddir'].DIRECTORY_SEPARATOR.'fonts'; // The directory containing the user's fonts.
 
+$config['lsadminmodulesrootdir']    = $config['rootdir'].DIRECTORY_SEPARATOR."modules".DIRECTORY_SEPARATOR."admin";
+//Overwrite files with the same name on upload?
+$config['overwritefiles'] = 'off';
 
 // Use alias notation, we should move to this format everywhere.
 $config['plugindir']               = 'webroot.plugins';
 
 // (javascript) Fix automatically the value entered in numeric question type : 1: remove all non numeric caracters; 0 : leave all caracters
 $config['bFixNumAuto']             = 1;
-// (javascript) Send real value entered when using Numeric question type in Expression Manager : 0 : {NUMERIC} with bad caracters send '', 1 : {NUMERIC} send all caracters entered
+// (javascript) Send real value entered when using Numeric question type in ExpressionScript Engine : 0 : {NUMERIC} with bad caracters send '', 1 : {NUMERIC} send all caracters entered
 $config['bNumRealValue'] = 0;
 
 // Home page default Settings
 $config['show_logo'] = 'show';
 $config['show_last_survey_and_question'] = 'show';
 $config['show_survey_list_search'] = 'show';
+$config['show_survey_list'] = 'hide';
 $config['boxes_by_row'] = '3';
 $config['boxes_offset'] = '3';
 $config['boxes_in_container'] = 'yes';
@@ -707,6 +739,9 @@ $config['bounceaccountuser'] = '';
 
 // Question selector
 $config['defaultquestionselectormode'] = 'default';
+
+// Preselected Question Type
+$config['preselectquestiontype'] = 'T';
 
 // theme editor mode
 $config['defaultthemeteeditormode'] = 'default';
@@ -743,5 +778,20 @@ $config['pluginCoreList'] = [
 ];
 
 $config['pluginWhitelist'] = [];
+
+/* replaced in generated application/config/security.php if exist */
+$config['encryptionkeypair'] = '';
+$config['encryptionpublickey'] = '';
+$config['encryptionsecretkey'] = '';
+
+$config['passwordValidationRules'] = array(
+    'min' => 4,
+    'max' => 0,
+    'lower' => 0,
+    'upper' => 0,
+    'numeric' => 0,
+    'symbol' => 0,
+);
+
 return $config;
 //settings deleted
---
+++ assets/packages/questions/upload/src/uploader.js
@@ -0,0 +1,345 @@
+import './ajaxupload.js';
+import './modaldialog.js';
+
+"use strict"
+var uploadHandler = function (qid, options) {
+
+    var init = function () {
+        doFileUpload();
+    };
+    
+    var fixParentHeigth = function () {
+        return;
+    };
+
+    var renderPreviewItem = function (fieldname, item, iterator) {
+        var i = iterator;
+        var image_extensions = new Array('gif', 'jpeg', 'jpg', 'png', 'swf', 'psd', 'bmp', 'tiff', 'jp2', 'iff', 'bmp', 'xbm', 'ico', 'heic');
+
+        var previewblock = $('<li id="' + fieldname + '_li_' + i + '" class="previewblock file-element"></li>');
+        var previewContainer = $('<div class="file-preview"></div>');
+
+        if (isValueInArray(image_extensions, item.ext.toLowerCase())) {
+            previewContainer.append('<img src="' + options.uploadurl + '/filegetcontents/' + item.filename + '" class="uploaded" />');
+        } else {
+            previewContainer.append('<div class="upload-placeholder"></div>');
+        }
+
+        previewContainer.append('<span class="file-name">' + escapeHtml(decodeURIComponent(item.name)) + '</span>');
+
+        if ($('#' + fieldname + '_show_title').val() == 1 || $('#' + fieldname + '_show_comment').val() == 1) {
+
+            var previewTitleContainer = $('');
+            var previewCommentContainer = $('');
+
+            if ($('#' + fieldname + '_show_title').val() == 1) {
+                var previewTitleContainer = $('<div class="form-group"></div>');
+                $('<label class="control-label col-xs-4"></label>')
+                    .attr('for', fieldname + '_title_' + i)
+                    .text(options.uploadLang.titleFld)
+                    .appendTo(previewTitleContainer);
+                $('<input class="form-control" type="text"/>')
+                    .attr('id', fieldname + "_title_" + i)
+                    .val(item.title)
+                    .wrap('<div class="input-container"></div>')
+                    .appendTo(previewTitleContainer);
+            }
+
+            if ($('#' + fieldname + '_show_comment').val() == 1) {
+                var previewCommentContainer = $('<div class="form-group"></div>');
+                $('<label class="control-label col-xs-4"></label>')
+                    .attr('for', fieldname + '_comment_' + i)
+                    .text(options.uploadLang.commentFld)
+                    .appendTo(previewCommentContainer);
+                $('<input class="form-control" type="text"/>')
+                    .attr('id', fieldname + "_comment_" + i)
+                    .val(item.comment)
+                    .wrap('<div class="input-container"></div>')
+                    .appendTo(previewCommentContainer);
+            }
+
+        }
+
+        var previewDeleteBlock = $('<div class="form-group"></div>').append(
+            $('<a class="btn btn-danger"></a>')
+                .html('<span class="fa fa-trash"></span>&nbsp;' + options.uploadLang.deleteFile )
+                .on('click', function () {
+                    deletefile(fieldname, i);
+                })
+                .wrap('<div class="input-container text-center"></div>')
+        );
+
+        $('<fieldset></fieldset>')
+            .append(previewTitleContainer).append(previewCommentContainer).append(previewDeleteBlock)
+            .wrap('<div class="file-info"></div>')
+            .appendTo(previewContainer);
+
+            
+            $('<input type="hidden" />').attr('id', fieldname + '_size_' + i).attr('value', item.size).appendTo(previewblock);
+            $('<input type="hidden" />').attr('id', fieldname + '_name_' + i).attr('value', item.name).appendTo(previewblock);
+            $('<input type="hidden" />').attr('id', fieldname + '_file_index_' + i).attr('value', i).appendTo(previewblock);
+            $('<input type="hidden" />').attr('id', fieldname + '_filename_' + i).attr('value', item.filename).appendTo(previewblock);
+            $('<input type="hidden" />').attr('id', fieldname + '_ext_' + i).attr('value', item.ext).appendTo(previewblock);
+            
+        // add file to the list only if it doesn't exists already
+        if ($("#" + fieldname + "_li_" + i).length === 0){
+            previewblock.append(previewContainer);
+            $('#field' + fieldname + '_listfiles').append(previewblock);
+        }
+    }
+
+    var doFileUpload = function () {
+        var fieldname = options.sFieldName;
+        /* Load the previously uploaded files */
+        var filecount = $('#' + fieldname + '_filecount').val();
+
+        $('#' + fieldname + '_filecount').val(filecount);
+
+        var image_extensions = ['gif', 'jpeg', 'jpg', 'png', 'swf', 'psd', 'bmp', 'tiff', 'jp2', 'iff', 'bmp', 'xbm', 'ico', 'heic'];
+        if (filecount > 0) {
+            var jsontext = $('#' + fieldname).val();
+
+            var json = '';
+            try{
+                json = JSON.parse(jsontext);
+            } catch(e) {}
+
+            if ($('#field' + fieldname + '_listfiles').length == 0) {
+                $('<ul id="field' + fieldname + '_listfiles" class="files-list" />').insertAfter('#uploadstatus_' + qid);
+            }
+
+            $('#' + fieldname + '_licount').val(filecount);
+
+            json.forEach(function (item, iterator) {
+                renderPreviewItem(fieldname, item, iterator+1);
+            });
+        }
+
+        // The upload button
+        var button = $('#button_' + qid);
+        new AjaxUpload(button, {
+            action: options.uploadurl + '/sid/' + surveyid + '/preview/' + options.questgrppreview + '/fieldname/' + fieldname + '/',
+            name: 'uploadfile',
+            data: {
+                valid_extensions: $('#' + fieldname + '_allowed_filetypes').val(),
+                max_filesize: $('#' + fieldname + '_maxfilesize').val(),
+                preview: $('#preview').val(),
+                surveyid: surveyid,
+                fieldname: fieldname,
+                YII_CSRF_TOKEN: options.csrfToken
+            },
+            onSubmit: function (file, ext) {
+                var maxfiles = parseInt($('#' + fieldname + '_maxfiles').val());
+                var filecount = parseInt($('#' + fieldname + '_filecount').val());
+                var allowed_filetypes = $('#' + fieldname + '_allowed_filetypes').val().split(",");
+
+                /* If maximum number of allowed filetypes have already been uploaded,
+                 * do not upload the file and display an error message ! */
+                if (filecount >= maxfiles) {
+                    $('#notice').html('<p class="alert alert-danger"><span class="fa fa-exclamation-circle"></span>&nbsp;' + uploadLang.errorNoMoreFiles + '</p>');
+                    fixParentHeigth();
+                    return false;
+                }
+
+                /* If the file being uploaded is not allowed,
+                 * do not upload the file and display an error message!
+                 */
+                let allowSubmit = false;
+                for (let fileType of allowed_filetypes) {
+                    allowSubmit = allowed_filetypes.includes(fileType);
+                }
+
+                if (allowSubmit == false) {
+                    $('#notice').html('<p class="alert alert-danger"><span class="fa fa-exclamation-circle"></span>&nbsp;' + uploadLang.errorOnlyAllowed.replace('%s', $('#' + fieldname + '_allowed_filetypes').val()) + '</p>');
+                    fixParentHeigth();
+                    return false;
+                }
+
+                // change button text, when user selects file
+                button.text(options.uploadLang.uploading);
+
+                // If you want to allow uploading only 1 file at time,
+                // you can disable upload button
+                this.disable();
+
+                button.append('<i id="loading-icon-fielupload" class="fa fa-spinner fa-pulse fa-3x fa-fw"></i>');
+
+            },
+            onComplete: function (file, response) {
+                button.text(uploadLang.selectfile);
+                $('#loading-icon-fielupload').remove();
+                // enable upload button
+                this.enable();
+
+                // Once the file has been uploaded via AJAX,
+                // the preview is appended to the list of files
+                try{
+                    var metadata = JSON.parse(response);
+                } catch(e) {
+                    /* Suppose we get an HTML error ? Replace whole HTML (without head) */
+                    $('body').html(response);
+                    return;
+                }
+
+                var count = parseInt($('#' + fieldname + '_licount').val());
+                count++;
+                $('#' + fieldname + '_licount').val(count);
+
+                if (metadata.success) {
+                    $('#notice').html('<p class="alert alert-success"><span class="fa fa-success"></span>&nbsp;' + metadata.msg + '</p>');
+                    if ($('#field' + fieldname + '_listfiles').length == 0) {
+                        $("<ul id='field" + fieldname + "_listfiles' class='files-list' />").insertAfter("#uploadstatus_" + options.qid);
+                    }
+                    renderPreviewItem(fieldname, metadata, count);
+
+                    var filecount = parseInt($('#' + fieldname + '_filecount').val());
+                    var minfiles = parseInt($('#' + fieldname + '_minfiles').val());
+                    filecount++;
+                    var maxfiles = parseInt($('#' + fieldname + '_maxfiles').val());
+                    $('#' + fieldname + '_filecount').val(filecount);
+
+                    if (filecount < minfiles)
+                        $('#uploadstatus').html(options.uploadLang.errorNeedMore.replace('%s', (minfiles - filecount)));
+                    else if (filecount < maxfiles)
+                        $('#uploadstatus').html(options.uploadLang.errorMoreAllowed.replace('%s', (maxfiles - filecount)));
+                    else
+                        $('#uploadstatus').html(options.uploadLang.errorMaxReached);
+                    fixParentHeigth();
+                    if (filecount >= maxfiles)
+                        $('#notice').html('<p class="alert alert-success"><span class="fa fa-check"></span>&nbsp;' + options.uploadLang.errorTooMuch + '</p>');
+                    fixParentHeigth();
+                } else {
+                    $('#notice').html('<p class="alert alert-danger"><span class="fa fa-exclamation-circle"></span>&nbsp;' + metadata.msg + '</p>');
+                    fixParentHeigth();
+                }
+
+            }
+        });
+    }
+
+    function isValueInArray(arr, val) {
+        return arr.reduce(function (col, item) {
+            return col || (val.toLowerCase() == item.toLowerCase())
+        }, false);
+    }
+
+    // pass the JSON data from the iframe to the main survey page
+    function passJSON(fieldname, show_title, show_comment, pos) {
+        var jsonArray = [];
+        var filecount = 0;
+        var licount = parseInt($('#' + fieldname + '_licount').val());
+        var i = 1;
+
+        while (i <= licount) {
+            if ($("#" + fieldname + "_li_" + i).is(':visible')) {
+                var fileObject = {
+                    title: ($("#" + fieldname + "_show_title").val() == 1) ? $("#" + fieldname + "_title_" + i).val() : '',
+                    comment: ($("#" + fieldname + "_show_comment").val() == 1) ? $("#" + fieldname + "_comment_" + i).val() : '',
+                    size: $("#" + fieldname + "_size_" + i).val(),
+                    name: $("#" + fieldname + "_name_" + i).val(),
+                    filename: $("#" + fieldname + "_filename_" + i).val(),
+                    ext: $("#" + fieldname + "_ext_" + i).val()
+                };
+                filecount += 1;
+                jsonArray.push(fileObject);
+            }
+            i++;
+        }
+
+
+        $('#' + fieldname).val(JSON.stringify(jsonArray));
+        copyJSON(filecount, fieldname, show_title, show_comment, pos);
+    }
+
+    const copyJSON = function(filecount, fieldname, show_title, show_comment, pos) {
+        $('#'+fieldname+'_filecount').val(filecount);
+        window['uploadQuestionController_' + fieldname].displayUploadedFiles(filecount, fieldname, show_title, show_comment, pos);
+    };
+
+    var saveAndExit = function (fieldname, show_title, show_comment, pos) {
+        var filecount = parseInt($('#' + fieldname + '_filecount').val());
+        var minfiles = parseInt($('#' + fieldname + '_minfiles').val());
+
+        if (minfiles != 0 && filecount < minfiles && showpopups) {
+            var confirmans = confirm(uploadLang.errorNeedMoreConfirm.replace('%s', (minfiles - filecount)))
+            if (confirmans) {
+                passJSON(fieldname, show_title, show_comment, pos);
+                return true
+            } else
+                return false;
+        } else {
+            passJSON(fieldname, show_title, show_comment, pos);
+            return true;
+        }
+    }
+
+    var deletefile = function (fieldname, count) {
+        var file_index;
+        var filename = $("#" + fieldname + "_filename_" + count).val();
+        var name = $("#" + fieldname + "_name_" + count).val();
+
+        var filecount = parseInt($('#' + fieldname + '_filecount').val());
+        var licount = parseInt($('#' + fieldname + '_licount').val());
+
+        $.ajax({
+                method: "POST",
+                url: uploadurl,
+                data: {
+                    'delete': 1,
+                    'fieldname': fieldname,
+                    'filename': filename,
+                    'name': name,
+                    YII_CSRF_TOKEN: options.csrfToken
+                }
+            })
+            .done(function (msg) {
+                $('#notice').html('<p class="alert alert-success"><span class="fa fa-check"></span>&nbsp;' + msg + '</p>');
+                setTimeout(function () {
+                    $(".success").remove();
+                }, 5000);
+                $("#" + fieldname + "_li_" + count).hide();
+                filecount--;
+                $('#' + fieldname + '_filecount').val(filecount);
+                file_index = $("#" + fieldname + "_file_index_" + count).val();
+                for (j = count; j <= licount; j++) {
+                    if ($('#' + fieldname + '_li_' + j).is(":visible")) {
+                        $('#' + fieldname + '_file_index_' + j).val(file_index);
+                        file_index++;
+                    }
+                }
+                var minfiles = parseInt($('#' + fieldname + '_minfiles').val());
+                var maxfiles = parseInt($('#' + fieldname + '_maxfiles').val());
+
+                if (filecount < minfiles) {
+                    $('#uploadstatus').html(uploadLang.errorNeedMore.replace('%s', (minfiles - filecount)));
+                    fixParentHeigth();
+                } else {
+                    $('#uploadstatus').html(uploadLang.errorMoreAllowed.replace('%s', (maxfiles - filecount)));
+                    fixParentHeigth();
+                }
+            });
+    };
+
+    return {
+        init: init,
+        saveAndExit: saveAndExit,
+    };
+}
+
+
+function escapeHtml(unsafe) {
+    return unsafe
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .replace(/"/g, "&quot;")
+        .replace(/'/g, "&#039;");
+}
+
+window.getUploadHandler = function(qid, options){
+    if (!window.currentUploadHandler){
+        window.currentUploadHandler = new uploadHandler(qid, options);
+    }
+    window.currentUploadHandler.init();
+    return window.currentUploadHandler;
+}
