5072362982e9d9fd7b3127685ebbc8c85abd6d68
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2020-05-26 12:12:41 +0200

    Dev: Add missing dbversion after update

--- application/helpers/update/updatedb_helper.php
+++ application/helpers/update/updatedb_helper.php
@@ -3088,7 +3088,8 @@ function db_upgrade_all($iOldDBVersion, $bSilent = false)
             $oTransaction = $oDB->beginTransaction();
 
             // Menu Link needs to be updated, cause we will revert the filemanager and enable the older one.
-            $oDB->createCommand()->update('{{surveymenu_entries}}', array('menu_link' => 'admin/survey/sa/rendersidemenulink&subaction=resources'), "name='resources'");
+            $oDB->createCommand()->update('{{surveymenu_entries}}', array('menu_link' => 'index.php?r=admin/survey/sa/rendersidemenulink&subaction=resources'), "name='resources'");
+            $oDB->createCommand()->update('{{settings_global}}', array('stg_value' => 427), "stg_name='DBVersion'");
             $oTransaction->commit();
         }
     } catch (Exception $e) {
