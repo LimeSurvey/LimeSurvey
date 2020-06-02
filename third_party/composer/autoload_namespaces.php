0928361c2be6e6e4e1592c53f8ecee30d11aa64a
Patricia Stelling <patricia.stelling@limesurvey.org>
2020-06-02 11:11:32 +0200

    autoload should not be commited and pushed, reverted it to normal

--- third_party/composer/autoload_namespaces.php
+++ third_party/composer/autoload_namespaces.php
@@ -6,8 +6,4 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'org\\jsonrpcphp\\' => array($vendorDir . '/weberhofer/jsonrpcphp/src'),
-    'PHPMD\\' => array($vendorDir . '/phpmd/phpmd/src/main/php'),
-    'LSS' => array($vendorDir . '/openlss/lib-array2xml'),
-    'JsonMapper' => array($vendorDir . '/netresearch/jsonmapper/src'),
 );
