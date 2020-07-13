75f0c373084d2cf929c64bce74c8e26f5bac7ce9
olleharstedt <olle.haerstedt@limesurvey.org>
2020-07-13 11:52:34 +0200

    Release 4.3.4+200713

--- application/config/version.php
+++ application/config/version.php
@@ -11,10 +11,10 @@
  * See COPYRIGHT.php for copyright notices and details.
  */
 
-$config['versionnumber'] = '4.3.3';
+$config['versionnumber'] = '4.3.4';
 $config['dbversionnumber'] = 427;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
 $config['templateapiversion']  = 3;
-$config['assetsversionnumber'] = '30147';
+$config['assetsversionnumber'] = '30148';
 return $config;
