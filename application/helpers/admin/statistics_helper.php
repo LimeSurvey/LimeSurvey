83e0ef325d887dde3a026655808edbf1e16e3d92
GabrielJenik <gabriel.jenik@gmail.com>
2020-09-02 16:13:11 -0300

    Fixed issue #16598--Statistics--Criteria-not-cleared-for-text-question-results
    Clearing criteia on session if no filter set

--- application/helpers/admin/statistics_helper.php
+++ application/helpers/admin/statistics_helper.php
@@ -3668,6 +3668,8 @@ class statistics_helper
 
             //add filter criteria to SQL
             $query .= implode(" AND ", $selects);
+        } else {
+            unset(Yii::app()->session['statistics_selects_'.$surveyid]);
         }
 
         //get me some data Scotty
