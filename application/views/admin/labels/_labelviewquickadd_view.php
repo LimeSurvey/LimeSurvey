9ee4e178b86f6b84c467782d8373e3828d011b92
GabrielJenik <gabriel.jenik@gmail.com>
2020-09-07 19:54:27 -0300

    DEV: Rebuild Labelsets Screen
    Set height of quickadd area

--- application/views/admin/labels/_labelviewquickadd_view.php
+++ application/views/admin/labels/_labelviewquickadd_view.php
@@ -7,7 +7,7 @@
       </div>
       <div class="modal-body">
           <p><?php eT('Enter one label per line. You can provide a code by separating code and label text with a semicolon or tab. For multilingual surveys you add the translation(s) on the same line separated with a semicolon or tab.') ?></p>
-          <textarea id='quickaddarea' name='quickaddarea' class='tipme' title='<?php eT('Enter one label per line. You can provide a code by separating code and label text with a semicolon or tab. For multilingual surveys you add the translation(s) on the same line separated with a semicolon or tab.') ?>' rows='20' cols='100' style='width:570px;'></textarea>
+          <textarea id='quickaddarea' name='quickaddarea' class='tipme' title='<?php eT('Enter one label per line. You can provide a code by separating code and label text with a semicolon or tab. For multilingual surveys you add the translation(s) on the same line separated with a semicolon or tab.') ?>' rows='10' cols='100' style='width:570px;'></textarea>
       </div>
       <div class="modal-footer button-list">
         <button id='btnqareplace' type='button' class="btn btn-default"><?php eT('Replace') ?></button>
