b6e04e162b65bb81f3f320b1a997213aa5e7ec33
GabrielJenik <gabriel.jenik@gmail.com>
2020-09-07 19:15:54 -0300

    Fixed issue #16633: Error message when changing the data policy message language unless I check the box
    Making a form of its own for the language changer of the first page. This form as only have the language will not trigger server side validation of the policy. This is how it is in LS4.
    
    Language changer from menu also updated.

--- application/helpers/SurveyRuntimeHelper.php
+++ application/helpers/SurveyRuntimeHelper.php
@@ -931,6 +931,7 @@ class SurveyRuntimeHelper
     private function displayFirstPageIfNeeded()
     {
         $bDisplayFirstPage = ($this->sSurveyMode != 'survey' && $_SESSION[$this->LEMsessid]['step'] == 0);
+        $this->aSurveyInfo['move'] = isset($this->sMove) ? $this->sMove : '';
 
         if ($this->sSurveyMode == 'survey' || $bDisplayFirstPage) {
 
