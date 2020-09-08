c7369ca3c9dbdd6dcec6edbd8db2b14db39875bb
eddylackmann <eddy.lackmann@limesurvey.org>
2020-09-08 07:42:33 +0200

    Release 3.23.3+200909

--- application/config/version.php
+++ application/config/version.php
@@ -12,10 +12,10 @@
  */
 
 
-$config['versionnumber'] = '3.23.2';
+$config['versionnumber'] = '3.23.3';
 $config['dbversionnumber'] = 360;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
 $config['templateapiversion'] = 3;
-$config['assetsversionnumber'] = '30148';
+$config['assetsversionnumber'] = '30149';
 return $config;
