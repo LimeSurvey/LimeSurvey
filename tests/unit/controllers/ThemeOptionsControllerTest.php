8b8dd5e2c1e2d72000ecbab9f70aec93f0f87f56
Jessica Höck <jessica.hoeck@limesurvey.org>
2020-09-10 14:08:31 +0200

    Dev: Updated ThemeOptionsControllerTest.

--- tests/unit/controllers/ThemeOptionsControllerTest.php
+++ tests/unit/controllers/ThemeOptionsControllerTest.php
@@ -9,7 +9,7 @@ use PHPUnit\Framework\TestCase;
 class ThemeOptionsControllerTest extends TestCase
 {
     /**
-     * @var themeoptions
+     * @var ThemeOptionsController
      */
     private $controller;
 
@@ -22,7 +22,6 @@ class ThemeOptionsControllerTest extends TestCase
     {
         \Yii::import('application.controllers.ThemeOptionsController', true);
         $this->controller = new ThemeOptionsController();
-
     }
 
     public function tearDown()
