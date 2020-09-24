6f16ab836fb4be6e2a760b308502075545addb03
Jessica Höck <jessica.hoeck@limesurvey.org>
2020-09-24 16:13:20 +0200

    Dev: Added the old view for text elements.

--- application/views/admin/survey/subview/tab_edit_view.php
+++ application/views/admin/survey/subview/tab_edit_view.php
@@ -8,6 +8,8 @@
  * @var $surveyls_language
  */
 
+ print_r($data['aTabTitles']);
+ die();
 if (isset($data)) {
     extract($data);
 }
@@ -26,3 +28,28 @@ $cs->registerPackage('bootstrap-select2');
 $adminlang = Yii::app()->session['adminlang'];
 
 ?>
+<!-- Text Elements Tabs -->
+<ul class="nav nav-tabs" id="edit-survey-text-element-language-selection">
+    <?php foreach ($aTabTitles as $i => $title):?>
+        <li role="presentation" class="<?php if ($count==0) {echo "active"; }?>">
+            <a data-toggle="tab" href="#edittxtele-<?php echo $count; $count++; ?>">
+                <?php echo $title;?>
+            </a>
+        </li>
+    <?php endforeach;?>
+</ul>
+
+<br/>
+
+<div class="tab-content">
+<?php foreach ($aTabContents as $i=>$content):?>
+    <?php
+        echo $content;
+    ?>
+<?php endforeach; ?>
+</div>
+
+<?php App()->getClientScript()->registerScript("EditSurveyTextTabs", "
+$('#edit-survey-text-element-language-selection').find('a').on('shown.bs.tab', function(e){
+    try{ $(e.relatedTarget).find('textarea').ckeditor(); } catch(e){ }
+})", LSYii_ClientScript::POS_POSTSCRIPT); ?>
