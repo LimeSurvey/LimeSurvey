e65b5bc6b37837772637194e4b182cac049106ab
Olle Harstedt <olleharstedt@yahoo.com>
2020-09-24 14:41:29 +0200

    Dev: Work on new view create for question (WIP)

---
+++ application/views/questionAdministration/create.php
@@ -0,0 +1,79 @@
+<!-- Create form for question -->
+<div class="side-body">
+    <div class="container-fluid">
+        <?php echo CHtml::form(
+            array("admin/questionedit/update"),
+            'post',
+            array(
+                'class' => 'form30 ',
+                'id' => 'frmeditquestion',
+                'name' => 'frmeditquestion',
+                //'data-isvuecomponent' => 1
+            )
+        ); ?>
+            <div id="advancedQuestionEditor">
+                <div class="container-center scoped-new-questioneditor">
+                    <div class="pagetitle h3 scoped-unset-pointer-events">
+                        <x-test id="action::addQuestion"></x-test>
+                        <?= gT('Create question'); ?>
+                    </div>
+
+                    <!-- Question code and question type selector -->
+                    <div class="row">
+                        <?php $this->renderPartial(
+                            "codeAndType",
+                            [
+                                'oSurvey'             => $oSurvey,
+                                'aStructureArray'     => $aQuestionTypeGroups,
+                                'questionTypes'       => $aQuestionTypeStateList,
+                                'aQuestionTypeGroups' => $aQuestionTypeGroups
+                            ]
+                        ); ?>
+                    </div>
+
+                    <!-- Language selector -->
+                    <div class="row">
+                        <?php $this->renderPartial(
+                            "languageselector",
+                            ['oSurvey' => $oSurvey]
+                        ); ?>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-lg-7">
+                            <!-- Text elements -->
+                            <?php $this->renderPartial(
+                                "textElements",
+                                [
+                                    'oSurvey'           => $oSurvey,
+                                    'aStructureArray'   => $aQuestionTypeGroups,
+                                    'questionTypes'     => $aQuestionTypeStateList,
+                                ]
+                            ); ?>
+                        </div>
+
+                        <!-- General settings -->
+                        <div class="col-lg-5">
+                            <div class="ls-flex scope-set-min-height scoped-general-settings">
+                                <?php $this->renderPartial("generalSettings", ['generalSettings'  => $generalSettings]); ?>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="ls-flex ls-flex-row scoped-advanced-settings-block">
+                        <?php $this->renderPartial("advancedSettings", ['advancedSettings'  => $advancedSettings]); ?>
+                    </div>
+
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+
+<script>
+jQuery(document).on('ready', function () {
+    $('.ace:not(.none)').ace({
+        'mode' : 'javascript'
+    });
+});
+</script>
