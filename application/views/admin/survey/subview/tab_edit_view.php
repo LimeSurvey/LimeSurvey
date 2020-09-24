112b58827cdd0f14c95233e3acb8f17d94fee705
Jessica Höck <jessica.hoeck@limesurvey.org>
2020-09-24 13:24:13 +0200

    Dev: Removed Text Elements Vue Component from View.

--- application/views/admin/survey/subview/tab_edit_view.php
+++ application/views/admin/survey/subview/tab_edit_view.php
@@ -26,25 +26,3 @@ $cs->registerPackage('bootstrap-select2');
 $adminlang = Yii::app()->session['adminlang'];
 
 ?>
-
-<div class="container-center">
-    <div id="advancedTextEditor">
-        <lsnexttexteditor
-                :languagelist="'<?= htmlentities(json_encode(array_merge(["" => ""], getLanguageDataRestricted(false, 'short')))); ?>'"
-                :languagename="'<?= getLanguageNameFromCode($oSurvey->language, false); ?>'"
-                :defaultlanguage="'<?= $adminlang; ?>'"
-        />
-    </div>
-    <div id="textEditLoader" class="ls-flex ls-flex-column align-content-center align-items-center">
-        <div class="ls-flex align-content-center align-items-center">
-            <div class="loader-advancedquestionsettings text-center">
-                <div class="contain-pulse animate-pulse">
-                    <div class="square"></div>
-                    <div class="square"></div>
-                    <div class="square"></div>
-                    <div class="square"></div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
