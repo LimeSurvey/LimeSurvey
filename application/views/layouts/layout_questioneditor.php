f2d3ddcd379b79ec992d4e97d52c0d94a3500690
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2020-09-30 12:26:46 +0200

    Dev: Add temporary HTML topbar

--- application/views/layouts/layout_questioneditor.php
+++ application/views/layouts/layout_questioneditor.php
@@ -40,7 +40,30 @@ echo '<div '
     . '>';
 //New general top bar (VueComponent)
 //$this->_generaltopbar($aData);
-$layoutHelper->renderGeneraltopbar($aData);
+//$layoutHelper->renderGeneraltopbar($aData);
+?>
+<div id="vue-topbar-container" class="container-fluid vue-general-topbar" style="width: 100%; padding: 0px; margin: 0px;">
+    <div class="topbarpanel">
+        <nav class="navbar navbar-default scoped-topbar-nav" style="border: none;">
+            <div id="itm-9127598" class="ls-flex ls-flex-row ls-space padding top-5">
+                <ul class="nav navbar-nav scoped-topbar-nav ls-flex-item ls-flex-row grow-2 text-left">
+                    <li>
+                        <div class="topbarbutton">
+<a type="button" href="/index.php?r=surveyAdministration/activate&amp;iSurveyID=829174" id="ls-activate-survey" data-btntype="1" class="btn navbar-btn button  white btn-success">
+&nbsp;Activate this survey&nbsp;</a>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="topbarbutton">
+                            <a type="button" href="#" class="btn navbar-btn button btn-success">Save</a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</div>
+<?php
 
 echo '<div id="pjax-content" class="col-12">';
 
