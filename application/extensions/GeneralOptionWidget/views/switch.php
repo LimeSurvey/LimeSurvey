7b51414c6e88dcc47f4fd2935365bd9a1f5dd3d8
Olle Harstedt <olleharstedt@yahoo.com>
2020-10-01 16:31:59 +0200

    Dev: Make checkbox a button group in general settings

--- application/extensions/GeneralOptionWidget/views/switch.php
+++ application/extensions/GeneralOptionWidget/views/switch.php
@@ -1,7 +1,18 @@
-<div class="inputtype--toggle-container">
-    <input
-        type="checkbox"
-        name="generalSettings[<?= $this->generalOption->name; ?>]"
-        id="<?= $this->generalOption->name; ?>"
-        />
+<div class="btn-group col-12" role="group" data-toggle="buttons">
+    <label class="btn btn-default">
+        <input
+            type="radio"
+            name="generalSettings[<?= $this->generalOption->name; ?>]"
+            value="Y"
+            />
+        <?= gT('On'); ?>
+    </label>
+    <label class="btn btn-default">
+        <input
+            type="radio"
+            name="generalSettings[<?= $this->generalOption->name; ?>]"
+            value="N"
+            />
+        <?= gT('Off'); ?>
+    </label>
 </div> 
