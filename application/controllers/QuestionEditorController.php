f1fbc2c8382e9e81e2738041b4583b56751a883d
liumingjun <liumingjun@data100.com.cn>
2020-10-10 10:52:01 +0800

    Fixed issue #16728 :my account->My personal settings->Show script field option not effective

--- application/controllers/QuestionEditorController.php
+++ application/controllers/QuestionEditorController.php
@@ -590,7 +590,7 @@ class QuestionEditorController extends LSBaseController
             "editorpreset" => App()->session['htmleditormode'],
             "script" =>
                 Permission::model()->hasSurveyPermission($oQuestion->sid, 'surveycontent', 'update')
-                && SettingsUser::getUserSetting('showScriptEdit', App()->user->id),
+                && SettingsUser::getUserSetting('showScriptEdit', App()->user->id) == 1,
         ];
 
         $this->renderJSON($aPermissions);
