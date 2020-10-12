26be1b81aabd15955c2da65fdaedcfe310cbec5c
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2020-10-12 14:50:20 +0200

    Remove unused Vue code

--- application/views/questionAdministration/advancedSettings.php
+++ application/views/questionAdministration/advancedSettings.php
@@ -1,24 +1,6 @@
 <div class="col-12 scope-apply-base-style scope-min-height">
-    <div class="container-fluid" v-if="!loading && showAdvancedOptions" id="advanced-options-container">
+    <div class="container-fluid" id="advanced-options-container">
         <div class="row scoped-tablist-container">
-            <!--
-            <template v-if="showSubquestionEdit || showAnswerOptionEdit">
-                <ul class="nav nav-tabs scoped-tablist-subquestionandanswers" role="tablist">
-                    <li 
-                        v-if="showSubquestionEdit"
-                        :class="currentTabComponent == 'subquestions' ? 'active' : ''"
-                    >
-                        <a href="#" @click.prevent.stop="selectCurrentTab('subquestions')" >{{"subquestions" | translate }}</a>
-                    </li>
-                    <li 
-                        v-if="showAnswerOptionEdit"
-                        :class="currentTabComponent == 'answeroptions' ? 'active' : ''"
-                    >
-                        <a href="#" @click.prevent.stop="selectCurrentTab('answeroptions')" >{{"answeroptions" | translate }}</a>
-                    </li>
-                </ul>
-            </template>
-            -->
             <!-- Advanced settings tabs -->
             <ul class="nav nav-tabs scoped-tablist-advanced-settings" role="tablist">
                 <?php if ($oQuestion->questionType->subquestions > 0): ?>
