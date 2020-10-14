1c174370301166d2ea5970d3f12df3f945fd179b
Carsten Schmitz <carsten.schmitz@limesurvey.org>
2020-10-14 10:26:02 +0200

    Fixed issue: Update issue with invalid datetime value stored in expires field

--- application/helpers/update/updatedb_helper.php
+++ application/helpers/update/updatedb_helper.php
@@ -2502,6 +2502,8 @@ function db_upgrade_all($iOldDBVersion, $bSilent = false)
                 alterColumn('{{settings_global}}','stg_value',"mediumtext",false);
                 alterColumn('{{settings_user}}','stg_value',"mediumtext");
                 alterColumn('{{surveymenu_entries}}','data',"mediumtext");
+                // The following line fixes invalid entries having set 0000-00-00 00:00:00 as date
+                $oDB->createCommand()->update('{{surveys}}', ['expires'=>NULL], "expires=0");
                 alterColumn('{{surveys}}','attributedescriptions',"mediumtext");
                 alterColumn('{{surveys_languagesettings}}','surveyls_description',"mediumtext");
                 alterColumn('{{surveys_languagesettings}}','surveyls_welcometext',"mediumtext");
