dcb61bf1b7da83620fb29cb8134c55d0d807d1af
Olle Harstedt <olleharstedt@yahoo.com>
2020-10-29 12:15:31 +0100

    Apply old Vue HTML structure to topbar

--- application/views/questionAdministration/topbars/editQuestiontopbar_view.php
+++ application/views/questionAdministration/topbars/editQuestiontopbar_view.php
@@ -26,34 +26,57 @@
 
 ?>
 
-<div class='menubar surveybar' id="questiongroupbarid">
-    <div class='row container-fluid'>
+<style>
+/* TODO: Move to CSS */
+.topbarbutton {
+    margin-left: 1px;
+    margin-right: 1px;
+}
+.navbar ul {
+    padding-top: 4px;
+}
+</style>
 
-        <div class="col-sm-4 ">
-            <a class="btn btn-default"
-               href="<?php echo Yii::App()->createUrl('questionAdministration/importView', ['surveyid' => $surveyId]); ?>"
-               role="button">
-                <span class="icon-import"></span>
-                <?php eT('Import a question'); ?>
-            </a>
-        </div>
+<div id="vue-topbar-container" class="container-fluid vue-general-topbar" style="width: 100%; padding: 0px; margin: 0px;">
+    <div class="topbarpanel">
+        <nav class="navbar navbar-default scoped-topbar-nav" style="border: none;">
+            <div class="ls-flex ls-flex-row ls-space padding top-5">
+                <ul class="col-sm-4 nav navbar-nav scoped-topbar-nav ls-flex-item ls-flex-row grow-2 text-left">
+                    <li>
+                        <div class="topbarbutton">
+                            <a class="btn btn-default"
+                               href="<?php echo Yii::App()->createUrl('questionAdministration/importView', ['surveyid' => $surveyId]); ?>"
+                               role="button">
+                                <span class="icon-import"></span>
+                                <?php eT('Import a question'); ?>
+                            </a>
+                        </div>
+                    </li>
+                </ul>
+                <ul class="nav navbar-nav scoped-topbar-nav ls-flex-item ls-flex-row align-content-flex-end text-right padding-left scoped-switch-floats">
 
-        <!-- Right Buttons -->
-        <div class="col-sm-4 pull-right text-right">
-
-            <!-- Save and close -->
-            <a id="save-and-close-button-create-question" class="btn btn-default" role="button">
-                <i class="fa fa-check-square"></i>
-                <?php eT("Save and close");?>
-            </a>
-
-            <!-- Close -->
-            <a class="btn btn-danger" href="<?php echo $closeBtnUrl; ?>" role="button">
-                <span class="fa fa-close"></span>
-                <?php eT("Close");?>
-            </a>
-
-        </div>
+                    <!-- Right Buttons -->
+                    <li>
+                        <div class="topbarbutton">
+                            <!-- Save and close -->
+                            <a id="save-and-close-button-create-question" class="btn btn-default" role="button">
+                                <i class="fa fa-check-square"></i>
+                                <?php eT("Save and close");?>
+                            </a>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="topbarbutton">
+                            <!-- Close -->
+                            <a class="btn btn-danger" href="<?php echo $closeBtnUrl; ?>" role="button">
+                                <span class="fa fa-close"></span>
+                                <?php eT("Close");?>
+                            </a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     </div>
 </div>
 
