68e2aa7d0782db38ea319424fbf61e13eee167c0
Denis Chenu <denis@sondages.pro>
2020-10-30 19:23:10 +0100

    Dev: some information about permission for user

--- application/views/admin/surveysgroups/permission/viewCurrents.php
+++ application/views/admin/surveysgroups/permission/viewCurrents.php
@@ -1,3 +1,7 @@
+    <div class="well">
+        <p><?= gT("User listed here can see groups in lists and view descriptions and other settings. It's the minial right, youhave to use delete action to remve this right.") ?></p>
+        <p><?= gT("This surveys group are shown in list for user with rigths on survey group, user with rigths an a survey inside this group and if this groups was set as available.") ?></p>
+    </div>
     <?php if(!empty($oExistingUsers)) {
         $this->renderPartial('surveysgroups/permission/currentUsersList',array(
             'model'=>$model,
