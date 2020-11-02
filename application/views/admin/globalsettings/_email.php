a9d9bc5a36ea50614f9ada291afd9bc941b50f7a
Vilius Šumskas <vilius@sumskas.eu>
2020-11-02 14:00:27 +0200

    New feature: Make invitation/reminder sending rate configurable
    Dev: Use number input field and Yii configuration method directly.

--- application/views/admin/globalsettings/_email.php
+++ application/views/admin/globalsettings/_email.php
@@ -96,7 +96,7 @@
         <div class="form-group">
             <label class="control-label" for="sendingrate"><?php eT("Email sending rate:"); ?></label>
             <div>
-                <?php echo Chtml::textField("sendingrate", getGlobalSetting('sendingrate'), array('class' => 'form-control', 'size' => '5')); ?>
+                <?php echo Chtml::numberField("sendingrate", App()->getConfig('sendingrate'), array('class' => 'form-control', 'size' => 5, 'min' => 1)); ?>
                 <span class="hint"><?php eT("Number of seconds to wait until next email batch is sent."); ?></span>
             </div>
         </div>
