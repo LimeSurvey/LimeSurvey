22e6c23e7ce22388456fc952f547529655724b0b
a.albatayinah@psau.edu.sa <a.albatayinah@psau.edu.sa>
2020-11-02 16:23:12 +0300

    fix bypassing-file-upload-restrictions in import survey

--- application/controllers/admin/surveyadmin.php
+++ application/controllers/admin/surveyadmin.php
@@ -1161,6 +1161,8 @@ class SurveyAdmin extends Survey_Common_Action
                 $aData['sHeader'] = gT("Import survey data");
                 $aData['sSummaryHeader'] = gT("Survey structure import summary");
                 $aPathInfo = pathinfo($_FILES['the_file']['name']);
+                // check mime_type of file 
+                $mime=mime_content_type($_FILES['the_file']['tmp_name']);
 
                 if (isset($aPathInfo['extension'])) {
                     $sExtension = $aPathInfo['extension'];
@@ -1177,7 +1179,10 @@ class SurveyAdmin extends Survey_Common_Action
             if ($action == 'importsurvey') {
 
                 $sFullFilepath = Yii::app()->getConfig('tempdir').DIRECTORY_SEPARATOR.randomChars(30).'.'.$sExtension;
-                if ($_FILES['the_file']['error'] == 1 || $_FILES['the_file']['error'] == 2) {
+                if(!in_array($mime,['text/tab-separated-values','application/octet-stream','text/plain'])){
+                    $aData['sErrorMessage'] = sprintf(gT("Import failed. You specified an invalid file type '%s'."), CHtml::encode($mime));
+                    $aData['bFailed'] = true; 
+                }elseif ($_FILES['the_file']['error'] == 1 || $_FILES['the_file']['error'] == 2) {
                     $aData['sErrorMessage'] = sprintf(gT("Sorry, this file is too large. Only files up to %01.2f MB are allowed."), getMaximumFileUploadSize() / 1024 / 1024).'<br>';
                     $aData['bFailed'] = true;
                 } elseif (!in_array(strtolower($sExtension), array('lss', 'txt', 'tsv', 'lsa'))) {
