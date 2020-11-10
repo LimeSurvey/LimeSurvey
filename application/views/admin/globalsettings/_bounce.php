6421e6c36262f5d18b7d981db2629b65dfffbd34
Vilius Šumskas <vilius@sumskas.eu>
2020-11-10 09:51:13 +0200

    Fixed issue: wrong port and TLS description in bounce email/smtp settings
    Dev: Convert the values already set in the database to lowercase too.

--- application/views/admin/globalsettings/_bounce.php
+++ application/views/admin/globalsettings/_bounce.php
@@ -58,7 +58,7 @@
     <div class="">
         <?php $this->widget('yiiwheels.widgets.buttongroup.WhButtonGroup', array(
                 'name' => 'bounceencryption',
-                'value'=> getGlobalSetting('bounceencryption') ,
+                'value'=> strtolower(getGlobalSetting('bounceencryption')),
                 'selectOptions'=>array(
                 "off"=>gT("Off (insecure)",'unescaped'),
                 "ssl"=>"SSL/TLS",
