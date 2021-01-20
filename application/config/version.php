99c4e5ee8d9bc589d9d293cb2263894013416e5d
eddylackmann <eddy.lackmann@limesurvey.org>
2021-01-20 18:03:02 +0100

    Release 4.4.0-RC4+210120

--- application/config/version.php
+++ application/config/version.php
@@ -11,10 +11,10 @@
  * See COPYRIGHT.php for copyright notices and details.
  */
 
-$config['versionnumber'] = '4.4.0-RC3';
+$config['versionnumber'] = '4.4.0-RC4';
 $config['dbversionnumber'] = 437;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
 $config['templateapiversion']  = 3;
-$config['assetsversionnumber'] = '30180';
+$config['assetsversionnumber'] = '30181';
 return $config;
