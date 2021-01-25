6142626ed31031576f6644d25cddad8febde91c8
Denis Chenu <denis@sondages.pro>
2021-01-25 12:30:40 +0100

    Fixed issue #16481: Easy way to launch CDBException (#1480)
    Dev: use checkValidityAnswer to allow or not prefilling
    Dev: set other to true : can be invalid, but don't broke DB

--- application/helpers/expressions/em_manager_helper.php
+++ application/helpers/expressions/em_manager_helper.php
@@ -5058,11 +5058,18 @@
                             $value=NULL;  // can't upload a file via GET
                             break;
                     }
-                    $_SESSION[$LEM->sessid][$knownVar['sgqa']] = $value;
-                    $LEM->updatedValues[$knownVar['sgqa']]=array(
-                        'type'=>$knownVar['type'],
-                        'value'=>$value,
+                    /* Check validity # */
+                    $qinfo = array(
+                        'qid' => $knownVar['qid'],
+                        'other' => "Y", // Not known currently, but don't broke if set
                     );
+                    if (self::checkValidityAnswer($knownVar['type'],$value,$knownVar['sgqa'],$qinfo,false)) {
+                        $_SESSION[$LEM->sessid][$knownVar['sgqa']] = $value;
+                        $LEM->updatedValues[$knownVar['sgqa']]=array(
+                            'type'=>$knownVar['type'],
+                            'value'=>$value,
+                        );
+                    }
                 }
                 $LEM->_UpdateValuesInDatabase();
             }
