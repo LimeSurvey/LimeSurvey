d1624cc1834d5a96065e306fda25c4f4cda4020c
Tonis Ormisson <tonis@andmemasin.eu>
2021-02-04 09:01:23 +0200

    display actual model errors when survey import fails

--- application/helpers/admin/import_helper.php
+++ application/helpers/admin/import_helper.php
@@ -914,6 +914,7 @@ function XMLImportSurvey($sFullFilePath, $sXMLdata = null, $sNewSurveyName = nul
             $results['surveys']++;
         } else {
             $results['error'] = gT("Unable to import survey.");
+            $results['error'] .= CHtml::errorSummary($newSurvey);
             return $results;
         }
     }
