0d52047028becf0483927ca81c082d175c1198c9
eddylackmann <a.eddy@hotmail.de>
2021-02-10 18:56:08 +0100

    Release 4.4.5+210213

--- application/config/version.php
+++ application/config/version.php
@@ -11,10 +11,10 @@
  * See COPYRIGHT.php for copyright notices and details.
  */
 
-$config['versionnumber'] = '4.4.4';
+$config['versionnumber'] = '4.4.5';
 $config['dbversionnumber'] = 437;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
 $config['templateapiversion']  = 3;
-$config['assetsversionnumber'] = '30187';
+$config['assetsversionnumber'] = '30188';
 return $config;
