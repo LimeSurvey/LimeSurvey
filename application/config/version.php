2bdca82706fa7fbb00d0049d592452e7d291d154
GabrielJenik <gabriel.jenik@gmail.com>
2021-02-12 12:02:49 -0300

    Fixed issue #17040: Question editor buttons dont appear and the button general settings "pull right " ....
    Mapping old values for default html editor to new values

--- application/config/version.php
+++ application/config/version.php
@@ -12,7 +12,7 @@
  */
 
 $config['versionnumber'] = '4.4.7';
-$config['dbversionnumber'] = 437;
+$config['dbversionnumber'] = 438;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
 $config['templateapiversion']  = 3;
