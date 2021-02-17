cbb5e0735281c93f9132a9d21e9fbc8d79955ac5
olleharstedt <olleharstedt@yahoo.com>
2021-02-17 16:03:19 +0100

    Release 3.25.14+210218

--- application/config/version.php
+++ application/config/version.php
@@ -12,10 +12,10 @@
  */
 
 
-$config['versionnumber'] = '3.25.13';
+$config['versionnumber'] = '3.25.14';
 $config['dbversionnumber'] = 365;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
 $config['templateapiversion'] = 3;
-$config['assetsversionnumber'] = '30175';
+$config['assetsversionnumber'] = '30176';
 return $config;
