d439ff86473c0ee44802d795fabce4b974342203
olleharstedt <olleharstedt@yahoo.com>
2021-03-01 12:48:03 +0100

    Release 4.4.11+210301

--- application/config/version.php
+++ application/config/version.php
@@ -11,10 +11,10 @@
  * See COPYRIGHT.php for copyright notices and details.
  */
 
-$config['versionnumber'] = '4.4.10';
+$config['versionnumber'] = '4.4.11';
 $config['dbversionnumber'] = 438;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
 $config['templateapiversion']  = 3;
-$config['assetsversionnumber'] = '30193';
+$config['assetsversionnumber'] = '30194';
 return $config;
