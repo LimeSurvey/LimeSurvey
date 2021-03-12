0eb34617a312bb6006057c5cbd89f109c43cc7ce
Patrick Teichmann <patrick.teichmann@limesurvey.org>
2021-03-12 12:40:41 +0100

    Fixed issue #16575: Token not marked as answered if email encryption is turned on

--- application/views/admin/super/layout_insurvey.php
+++ application/views/admin/super/layout_insurvey.php
@@ -7,67 +7,67 @@
 
 //headers will be generated with the template file /admin/super/header.php
 $this->_showHeaders($aData, false);
-    //The adminmenu bar will be generated from /admin/super/adminmenu.php
-    $this->_showadminmenu($aData);
+//The adminmenu bar will be generated from /admin/super/adminmenu.php
+$this->_showadminmenu($aData);
 
-    // Generated through /admin/usergroup/usergroupbar_view
-    $this->_userGroupBar($aData);
+// Generated through /admin/usergroup/usergroupbar_view
+$this->_userGroupBar($aData);
 
-    echo "<!-- BEGIN LAYOUT_INSURVEY -->";
-    // Needed to evaluate EM expressions in question summary
-    // See bug #11845
-    LimeExpressionManager::StartProcessingPage(false, true);
-    $aData['debug'] = $aData;
-    $this->_titlebar($aData);
+echo "<!-- BEGIN LAYOUT_INSURVEY -->";
+// Needed to evaluate EM expressions in question summary
+// See bug #11845
+LimeExpressionManager::StartProcessingPage(false, true);
+$aData['debug'] = $aData;
+$this->_titlebar($aData);
 
-    //The load indicator for pjax
-    echo ' <div id="pjax-file-load-container" class="ls-flex-row col-12"><div style="height:2px;width:0px;"></div></div>';
+//The load indicator for pjax
+echo ' <div id="pjax-file-load-container" class="ls-flex-row col-12"><div style="height:2px;width:0px;"></div></div>';
 
-    // echo "<pre>".print_r($aData, true)."</pre>";
+// echo "<pre>".print_r($aData, true)."</pre>";
 
-    //The container to hold the vuejs application
-    echo ' <!-- Survey page, started in Survey_Common_Action::render_wrapped_template() -->
-        <div id="vue-apps-main-container" '
-    . 'class="ls-flex-row align-items-flex-begin align-content-flex-end col-12" '
+//The container to hold the vuejs application
+echo ' <!-- Survey page, started in Survey_Common_Action::render_wrapped_template() -->
+    <div id="vue-apps-main-container" '
+. 'class="ls-flex-row align-items-flex-begin align-content-flex-end col-12" '
+. '>';
+    //Rendered through /admin/super/sidemenu
+    $this->_surveysidemenu($aData);
+    echo '<div '
+    . 'class="ls-flex-column align-items-flex-start align-content-flex-start col-11 ls-flex-item transition-animate-width main-content-container" '
     . '>';
-        //Rendered through /admin/super/sidemenu
-        $this->_surveysidemenu($aData);
-        echo '<div '
-        . 'class="ls-flex-column align-items-flex-start align-content-flex-start col-11 ls-flex-item transition-animate-width main-content-container" '
-        . '>';
 
-            echo '<div id="pjax-content" class="col-12">';
-                echo LayoutHelper::renderTopbar($aData);
+        echo '<div id="pjax-content" class="col-12">';
+            echo LayoutHelper::renderTopbar($aData);
 
-                //Rendered through /admin/responses/browsemenubar_view
-                //$this->_browsemenubar($aData);
-                //Rendered through /admin/survey/Question/questionbar_view
-                $this->_organizequestionbar($aData);
+            //Rendered through /admin/responses/browsemenubar_view
+            //$this->_browsemenubar($aData);
+            //Rendered through /admin/survey/Question/questionbar_view
+            $this->_organizequestionbar($aData);
 
-                echo '<div id="in_survey_common" '
-                . 'class="container-fluid ls-flex-column col-12"'
-                . '>';
+            echo '<div id="in_survey_common" '
+            . 'class="container-fluid ls-flex-column col-12"'
+            . '>';
 
-                    //Rendered through /admin/update/_update_notification
-                    $this->_updatenotification();
-                    //Rendered through /notifications/notifications
-                    $this->_notifications();
+                //Rendered through /admin/update/_update_notification
+                $this->_updatenotification();
+                //Rendered through /notifications/notifications
+                $this->_notifications();
 
-                    //// Here the main content views.
-                    //Rendered through /admin/survey/QuestionGroups/listquestiongroups
-                    $this->_listquestiongroups($aData);
-                    //Rendered through /admin/survey/Question/listquestions
-                    $this->_listquestions($aData);
-                    //Rendered through /admin/super/sidebody
-                    //With content of /admin/survey/surveySummary_view
-                    $this->_nsurveysummary($aData);
+                //// Here the main content views.
+                //Rendered through /admin/survey/QuestionGroups/listquestiongroups
+                $this->_listquestiongroups($aData);
+                //Rendered through /admin/survey/Question/listquestions
+                $this->_listquestions($aData);
+                //Rendered through /admin/super/sidebody
+                //With content of /admin/survey/surveySummary_view
+                $this->_nsurveysummary($aData);
 
-                    echo $content;
-                    
-                echo "</div>\n";
+                echo $content;
+
             echo "</div>\n";
         echo "</div>\n";
     echo "</div>\n";
+echo "</div>\n";
 echo "<!-- END LAYOUT_INSURVEY -->";
 
 // Footer
