22e72602efef1ca99cdcb89dac7707530c6434a1
Gabriel Jenik <gabriel.jenik@gmail.com>
2021-03-25 10:08:28 -0300

    Fixed issue #16462: Relavance Equation not in Sync with Condition Designed (#1809)
    * Fixed issue #16462: Relavance Equation not in Sync with Condition Designed

--- application/extensions/GeneralOptionWidget/views/text.php
+++ application/extensions/GeneralOptionWidget/views/text.php
@@ -8,6 +8,7 @@
         name="question[<?= $this->generalOption->name; ?>]" 
         id="<?= $this->generalOption->name; ?>"
         value="<?= $this->generalOption->formElement->value; ?>"
+        <?php foreach ($this->generalOption->formElement->options['attributes'] as $attributeName => $attributeValue) echo $attributeName . '="' . $attributeValue . '"'; ?>
     />
     <?php if (isset($this->generalOption->formElement->options['inputGroup']['suffix'])) : ?>
         <div class="input-group-addon">
