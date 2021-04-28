574d662e6c443a4d0709561a6a3f56df753a7786
Carsten Schmitz <carsten.schmitz@limesurvey.org>
2021-04-28 22:27:18 +0200

    Moved twig to third-party dir

--- third_party/composer/autoload_static.php
+++ third_party/composer/autoload_static.php
@@ -8,9 +8,18 @@ class ComposerStaticInitddb1a145e450f862353420acc5153e40
 {
     public static $files = array (
         '3109cb1a231dcd04bee1f9f620d46975' => __DIR__ . '/..' . '/paragonie/sodium_compat/autoload.php',
+        '320cde22f66dd4f5d3fd621d3e88b98f' => __DIR__ . '/..' . '/symfony/polyfill-ctype/bootstrap.php',
     );
 
     public static $prefixLengthsPsr4 = array (
+        'T' => 
+        array (
+            'Twig\\' => 5,
+        ),
+        'S' => 
+        array (
+            'Symfony\\Polyfill\\Ctype\\' => 23,
+        ),
         'L' => 
         array (
             'LimeSurvey\\PluginManager\\' => 25,
@@ -23,6 +32,15 @@ class ComposerStaticInitddb1a145e450f862353420acc5153e40
     );
 
     public static $prefixDirsPsr4 = array (
+        'Twig\\' => 
+        array (
+            0 => __DIR__ . '/../..' . '/application/third_party/Twig',
+            1 => __DIR__ . '/..' . '/twig/twig/src',
+        ),
+        'Symfony\\Polyfill\\Ctype\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/polyfill-ctype',
+        ),
         'LimeSurvey\\PluginManager\\' => 
         array (
             0 => __DIR__ . '/../..' . '/application/libraries/PluginManager',
@@ -47,7 +65,21 @@ class ComposerStaticInitddb1a145e450f862353420acc5153e40
         'LimeSurvey\\Datavalueobjects\\' => 
         array (
             0 => __DIR__ . '/../..' . '/application/datavalueobjects',
+        ),        
+    );
+
+    public static $prefixesPsr0 = array (
+        'T' => 
+        array (
+            'Twig_' => 
+        array (
+                0 => __DIR__ . '/..' . '/twig/twig/lib',
+            ),
         ),
+    );
+
+    public static $classMap = array (
+        'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
     );
 
     public static function getInitializer(ClassLoader $loader)
@@ -55,6 +87,8 @@ class ComposerStaticInitddb1a145e450f862353420acc5153e40
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInitddb1a145e450f862353420acc5153e40::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInitddb1a145e450f862353420acc5153e40::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInitddb1a145e450f862353420acc5153e40::$prefixesPsr0;
+            $loader->classMap = ComposerStaticInitddb1a145e450f862353420acc5153e40::$classMap;
 
         }, null, ClassLoader::class);
     }
---
+++ third_party/composer/installed.php
@@ -0,0 +1,42 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'limesurvey/limesurvey',
+  ),
+  'versions' => 
+  array (
+    'limesurvey/limesurvey' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'symfony/polyfill-ctype' => 
+    array (
+      'pretty_version' => 'v1.22.1',
+      'version' => '1.22.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c6c942b1ac76c82448322025e084cadc56048b4e',
+    ),
+    'twig/twig' => 
+    array (
+      'pretty_version' => 'v1.42.5',
+      'version' => '1.42.5.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '87b2ea9d8f6fd014d0621ca089bb1b3769ea3f8e',
+    ),
+  ),
+);
