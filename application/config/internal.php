5244c741e6438c239bdb7d71eae1cc5a599ce469
Carsten Schmitz <carsten.schmitz@limesurvey.org>
2021-04-29 15:03:21 +0200

    Dev Removed manual autoload from Twig (done by composer now)

--- application/config/internal.php
+++ application/config/internal.php
@@ -54,15 +54,7 @@ $internalConfig = array(
         'vendor.twbs.bootstrap.dist',
 
         //Basic questiontype objects
-        'questiontypes' => realpath(__DIR__.'/../core/QuestionTypes'),
-        // Twig aliases. We don't want to change the file ETwigViewRenderer, so we can update it without difficulties
-        // However, LimeSurvey directory tree is not a standard Yii Application tree.
-        // we use 'third_party' instead of 'vendor'
-        // This line just point vendor.Twig to third_party/Twig
-        // @see: ETwigViewRenderer::$twigPathAlias
-        'application.vendor.Twig'=>'webroot.third_party.Twig.twig.lib.Twig',
-        // 'CaptchaExtendedAction' => realpath(__DIR__ . '/../extensions/captchaExtended/CaptchaExtendedAction.php'),
-        // 'CaptchaExtendedValidator' => realpath(__DIR__ . '/../extensions/captchaExtended/CaptchaExtendedValidator.php')
+        'questiontypes' => realpath(__DIR__.'/../core/QuestionTypes')
     ),
 
     /*
