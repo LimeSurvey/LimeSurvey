20e60eb96e3e650b7e66a962cec43142e47ace39
olleharstedt <olleharstedt@yahoo.com>
2021-05-03 14:12:18 +0200

    Release 4.6.0+210504

--- application/config/version.php
+++ application/config/version.php
@@ -11,10 +11,10 @@
  * See COPYRIGHT.php for copyright notices and details.
  */
 
-$config['versionnumber'] = '4.5.2';
+$config['versionnumber'] = '4.6.0';
 $config['dbversionnumber'] = 444;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
 $config['templateapiversion']  = 3;
-$config['assetsversionnumber'] = '30202';
+$config['assetsversionnumber'] = '30203';
 return $config;
