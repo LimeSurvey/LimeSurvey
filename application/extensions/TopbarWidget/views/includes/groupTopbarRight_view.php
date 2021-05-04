86161d0a464d011317a069c6bdadc4eab274046a
encuestabizdevgit <devgit@encuesta.biz>
2021-05-04 11:36:55 -0300

    Dev #T638b: After delete group behaviour

--- application/extensions/TopbarWidget/views/includes/groupTopbarRight_view.php
+++ application/extensions/TopbarWidget/views/includes/groupTopbarRight_view.php
@@ -10,7 +10,13 @@
 
     <!-- Delete -->
     <?php if( $oSurvey->active != "Y" ):?>
-        <?php if(is_null($condarray)):?>
+        <?php if($oSurvey->groupsCount == 1): ?>
+            <!-- can't delete the only group in a survey -->
+            <button type="button" class="btn btn-danger btntooltip" disabled data-toggle="tooltip" data-placement="bottom" title="<?php eT("Cannot delete this group because it's the only group in the survey."); ?>" >
+                <span class="fa fa-trash"></span>
+                <?php eT("Delete group"); ?>
+            </a>
+        <?php elseif(is_null($condarray)):?>
             <!-- can delete group and question -->
             <button
                 class="btn btn-danger"
@@ -20,20 +26,20 @@
                 data-message="<?php eT("Deleting this group will also delete any questions and answers it contains. Are you sure you want to continue?","js"); ?>"
                 >
                 <span class="fa fa-trash"></span>
-                <?php eT("Delete current group"); ?>
+                <?php eT("Delete group"); ?>
             </button>
         <?php else: ?>
             <!-- there is at least one question having a condition on its content -->
             <button type="button" class="btn btn-danger btntooltip" disabled data-toggle="tooltip" data-placement="bottom" title="<?php eT("Impossible to delete this group because there is at least one question having a condition on its content"); ?>" >
                 <span class="fa fa-trash"></span>
-                <?php eT("Delete current group"); ?>
+                <?php eT("Delete group"); ?>
             </a>
         <?php endif; ?>
     <?php else:?>
         <!-- Activated -->
         <button type="button" class="btn btn-danger btntooltip" disabled data-toggle="tooltip" data-placement="bottom" title="<?php eT("You can't delete this question group because the survey is currently active."); ?>" >
             <span class="fa fa-trash"></span>
-            <?php eT("Delete current group"); ?>
+            <?php eT("Delete group"); ?>
         </button>
     <?php endif; ?>
 <?php endif; ?>
