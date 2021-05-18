06db905aeeb784b9aa44deafff1b8c38bb583b0b
encuestabizdevgit <devgit@encuesta.biz>
2021-05-18 11:06:41 -0300

    Revert "Dev #T636: Not clear how to edit a question comparing LS3 with LS4"
    This reverts commit 084816b9c465e699212014bd298af8370243f855.

--- application/extensions/TopbarWidget/views/questionTopbar_view.php
+++ application/extensions/TopbarWidget/views/questionTopbar_view.php
@@ -7,22 +7,8 @@
  */
 $aData = get_defined_vars();
 $leftSideContentSummary = $this->render('includes/questionTopbarLeft_view', get_defined_vars(), true);
-$rightSideContentSummary = $this->render(
-    'includes/questionTopbarRight_view',
-    array_merge(
-        get_defined_vars(),
-        [
-            'showEditButton' => true,
-            'showDeleteButton' => true,
-            'showSaveButton' => false,
-            'showSaveAndCloseButton' => false,
-            'showCloseButton' => false,
-        ]
-    ),
-    true
-);
 $leftSideContentEditor = $this->render('includes/editQuestionTopbarLeft_view', get_defined_vars(), true);
-$rightSideContentEditor = $this->render('includes/editQuestionTopbarRight_view', get_defined_vars(), true);
+$rightSideContent = $this->render('includes/editQuestionTopbarRight_view', get_defined_vars(), true);
 
 ?>
 
@@ -30,25 +16,20 @@ $rightSideContentEditor = $this->render('includes/editQuestionTopbarRight_view',
     <?php if ($oQuestion->qid !== 0): ?>
         <div id="question-summary-topbar" class='row container-fluid'>
             <!-- Left Side -->
-            <div class="<?= !empty($rightSideContentSummary) ? 'col-md-8' : 'col-md-12'?>">
+            <div class="col-md-12">
                 <?= $leftSideContentSummary ?>
-            </div>
-
-            <!-- Right Side -->
-            <div class="<?= !empty($leftSideContentSummary) ? 'col-md-4' : 'col-md-12'?> pull-right text-right">
-                <?= $rightSideContentSummary ?>
             </div>
         </div>
     <?php endif; ?>
     <div id="question-create-edit-topbar" class='row container-fluid' style="display: none">
         <!-- Left Side -->
-        <div class="<?= !empty($rightSideContentEditor) ? 'col-md-8' : 'col-md-12'?>">
+        <div class="<?= !empty($rightSideContent) ? 'col-md-8' : 'col-md-12'?>">
             <?= $leftSideContentEditor ?>
         </div>
 
         <!-- Right Side -->
         <div class="<?= !empty($leftSideContentEditor) ? 'col-md-4' : 'col-md-12'?> pull-right text-right">
-            <?= $rightSideContentEditor ?>
+            <?= $rightSideContent ?>
         </div>
     </div>
 </div>
