3f2cc991b2fd970a7282447c8fa17ae56ed375db
Gabriel Jenik <gabriel.jenik@gmail.com>
2021-05-24 15:21:05 -0300

    New feature #17301: Allow whitelisting IP addresses in global settings (#1889)

--- application/models/FailedLoginAttempt.php
+++ application/models/FailedLoginAttempt.php
@@ -75,7 +75,7 @@ class FailedLoginAttempt extends LSActiveRecord
         $ip = substr(App()->getRequest()->getUserHostAddress(), 0, 40);
 
         // Return false if IP is whitelisted
-        if ($this->isWhitelisted($ip, $attemptType)) {
+        if (!empty($attemptType) && $this->isWhitelisted($ip, $attemptType)) {
             return false;
         }
 
@@ -141,28 +141,45 @@ class FailedLoginAttempt extends LSActiveRecord
      * @param string $ip
      * @param string $attemptType   'login' or 'token'
      *
+     * @throws InvalidArgumentException if an invalid attempt type is specified
      * @return boolean
      */
     private function isWhitelisted($ip, $attemptType)
     {
+        // Init
         if ($attemptType != self::TYPE_LOGIN && $attemptType != self::TYPE_TOKEN) {
+            throw new InvalidArgumentException(sprintf("Invalid attempt type: %s", $attemptType));
+        }
+        if (empty($ip)) {
             return false;
         }
+        $binaryIP = inet_pton($ip);
 
         $whiteList = Yii::app()->getConfig($attemptType . 'IpWhitelist');
         if (empty($whiteList)) {
             return false;
         }
-        if (!is_array($whiteList)) {
-            $whiteList = [$whiteList];
-        }
-        foreach ($whiteList as $whiteListEntry) {
-            if (!empty($whiteListEntry) && preg_match('/' . str_replace('/', '\/', $whiteListEntry) . '/', $ip, $m)) {
+
+        // Validating
+        $whiteListEntries = preg_split('/\n|,/', $whiteList);
+        foreach ($whiteListEntries as $whiteListEntry) {
+            if (empty($whiteListEntry)) {
+                continue;
+            }
+            // Compare directly
+            if ($whiteListEntry == $ip) {
+                // The IP is whitelisted
+                return true;
+            }
+            // Compare binary representations
+            $binaryWhiteListEntry = inet_pton($whiteListEntry);
+            if ($binaryWhiteListEntry !== false && $binaryWhiteListEntry == $binaryIP) {
                 // The IP is whitelisted
                 return true;
             }
         }
 
+        // Not whitelisted
         return false;
     }
 }
