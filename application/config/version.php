3a5b6c77cd5be49c1c1755e7e330a285b709c300
olleharstedt <olleharstedt@yahoo.com>
2021-05-25 13:41:12 +0200

    Release 5.0.0+210526

--- application/config/version.php
+++ application/config/version.php
@@ -11,10 +11,10 @@
  * See COPYRIGHT.php for copyright notices and details.
  */
 
-$config['versionnumber'] = '5.1.0-dev';
+$config['versionnumber'] = '5.0.0';
 $config['dbversionnumber'] = 447;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
 $config['templateapiversion']  = 3;
-$config['assetsversionnumber'] = '30206';
+$config['assetsversionnumber'] = '30207';
 return $config;
