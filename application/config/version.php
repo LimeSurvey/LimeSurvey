be6781b7b8450c032ec2e45b94ad13a4e3b69da8
Carsten Schmitz <carsten.schmitz@limesurvey.org>
2021-05-27 15:46:01 +0200

    Dev Updated version number

--- application/config/version.php
+++ application/config/version.php
@@ -11,7 +11,7 @@
  * See COPYRIGHT.php for copyright notices and details.
  */
 
-$config['versionnumber'] = '5.0.0';
+$config['versionnumber'] = '5.x.dev';
 $config['dbversionnumber'] = 447;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
