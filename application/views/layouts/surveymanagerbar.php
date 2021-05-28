8d03905bdbe1cd9337932d7e626f00dc3817cf66
Jessica Höck <thedirtypanda001@gmail.com>
2021-05-28 09:55:47 +0200

    Fixed issue: Page Header in Themes - Changed PageTitle to h1.

--- application/views/layouts/surveymanagerbar.php
+++ application/views/layouts/surveymanagerbar.php
@@ -1,7 +1,7 @@
 <div class="menubar surveymanagerbar">
     <div class="row container-fluid">
         <div class="col-xs-12 col-md-12">
-            <div class="h2"><?php eT($pageTitle) ?></div>
+            <div class="h1"><?php eT($pageTitle) ?></div>
         </div>
     </div>
 </div>
