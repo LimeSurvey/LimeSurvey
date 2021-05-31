b14295951afb70e3d6c6dc2c5198295233f10051
olleharstedt <olleharstedt@yahoo.com>
2021-05-31 09:37:41 +0200

    Release 3.27.1+210531

--- application/config/version.php
+++ application/config/version.php
@@ -12,10 +12,10 @@
  */
 
 
-$config['versionnumber'] = '3.27.0';
+$config['versionnumber'] = '3.27.1';
 $config['dbversionnumber'] = 365;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
 $config['templateapiversion'] = 3;
-$config['assetsversionnumber'] = '30191';
+$config['assetsversionnumber'] = '30192';
 return $config;
