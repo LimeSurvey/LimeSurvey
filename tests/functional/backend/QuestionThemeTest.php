faf1ee6718a4a6291d6766145c7f629b6db2c3a6
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2021-06-11 15:37:06 +0200

    Add new test for question theme import

--- tests/functional/backend/QuestionThemeTest.php
+++ tests/functional/backend/QuestionThemeTest.php
@@ -5,9 +5,10 @@ namespace ls\tests\controllers;
 use ls\tests\TestBaseClassWeb;
 use Facebook\WebDriver\WebDriverBy;
 use Facebook\WebDriver\Remote\LocalFileDetector;
+use QuestionTheme;
 
 /**
- *
+ * Uses test data from tpartner: https://github.com/tpartner/LimeSurvey-Range-Slider-4x
  */
 class QuestionThemeTest extends TestBaseClassWeb
 {
@@ -32,18 +33,47 @@ class QuestionThemeTest extends TestBaseClassWeb
 
     public function testImportQuestionTheme()
     {
+
+        // Delete question theme if it exists
+        $theme = QuestionTheme::model()->findByAttributes(['name' => 'Range-Slider']);
+        $themeDir = \Yii::app()->getConfig('userquestionthemerootdir') . '/Range-Slider';
+        if (!empty($theme)) {
+            \Yii::import('application.helpers.common_helper', true);
+            rmdirr($themeDir);
+            $theme->delete();
+        }
+
         $urlMan = \Yii::app()->urlManager;
         $web = self::$webDriver;
 
         // Go to theme page
         $url = $urlMan->createUrl('themeOptions/index');
         $web->get($url);
-        sleep(5);
+        sleep(1);
+
+        $button = $web->findByLinkText('Question themes');
+        $button->click();
+
+        $button = $web->findByLinkText('Upload & install');
+        $button->click();
+
+        sleep(1);
 
         // Click "Import"
-        // Choose zip file by tpartner
+        $fileInput = $web->findByCss('#importQuestionTemplate #the_file');
+        $fileInput->setFileDetector(new LocalFileDetector());
+        $file = BASEPATH . '../tests/data/file_upload/rangeslider_tpartner.zip';
+        $this->assertTrue(file_exists($file));
+        $fileInput->sendKeys($file)->submit();
+
+        sleep(1);
+
         // Check result in database
+        $theme = QuestionTheme::model()->findByAttributes(['name' => 'Range-Slider']);
+        $this->assertNotEmpty($theme, 'Found installed question theme in database');
+
         // Check result in filesystem
+        $this->assertTrue(file_exists($themeDir), 'Question theme exists in userquestionthemerootdir after install');
     }
 
     /**
