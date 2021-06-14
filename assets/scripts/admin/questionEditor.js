8715d94c9029096829b7f1fbeeca155cbea7ee8e
Gabriel Jenik <gabriel.jenik@gmail.com>
2021-06-14 10:16:35 -0300

    Dev #T774: Question Editor: Sticky Top bar (#1906)
    Co-authored-by: encuestabizdevgit <devgit@encuesta.biz>

--- assets/scripts/admin/questionEditor.js
+++ assets/scripts/admin/questionEditor.js
@@ -1924,4 +1924,23 @@ $(document).on('ready pjax:scriptcomplete', function () {
     });
     
     $('#relevance').on('keyup', showConditionsWarning);
+
+  function makeTopbarSticky() {
+    const topbar = $('#pjax-content > .menubar');
+    const editor = $('#in_survey_common');
+    const topbarOffset = topbar.offset().top;
+
+    $(window).on('scroll', () => {
+      if (window.pageYOffset >= topbarOffset) {
+        topbar.addClass('sticky');
+        topbar.css('width', topbar.parent().width());
+        editor.css('padding-top', topbar.outerHeight(true));
+      } else {
+        topbar.removeClass('sticky');
+        topbar.css('width', '');
+        editor.css('padding-top', '');
+      }
+    });
+  }
+  makeTopbarSticky();
 });
