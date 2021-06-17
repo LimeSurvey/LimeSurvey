de06569ea7418dfed22cd9836f7cee79cf02566f
Jessica Höck <thedirtypanda001@gmail.com>
2021-06-17 14:45:31 +0200

    Dev: Fixed small label sets buttons adjustment

--- application/views/admin/survey/prepareEditorScript_view.php
+++ application/views/admin/survey/prepareEditorScript_view.php
@@ -35,6 +35,8 @@ $script.="CKEDITOR.on('instanceReady', function(event) {
 
     var sReplacementFieldTitle = '".gT('Placeholder fields','js')."';
     var sReplacementFieldButton = '".gT('Insert/edit placeholder field','js')."';
+    var sSwitchToolbarFullTitle = '".gT('Show full toolbar','js')."';
+    var sSwitchToolbarBasicTitle = '".gT('Show basic toolbar','js')."';
     var editorwindowsHash = new Object();
 
     function find_popup_editor(fieldname)
