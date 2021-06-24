e34dc126770cf31b4d4124189f86dad971dcb49c
Jessica Höck <thedirtypanda001@gmail.com>
2021-06-24 09:04:38 +0200

    Dev: WIP - Added translation to pagetitle and renamed button in permission roles

--- application/views/admin/permissiontemplates/partials/_menubar.php
+++ application/views/admin/permissiontemplates/partials/_menubar.php
@@ -3,7 +3,7 @@
         <div class="col-md-9">
             <?php if(Permission::model()->hasGlobalPermission('superadmin', 'read')) { ?>
                 <button data-href="<?=App()->createUrl("admin/roles/sa/editrolemodal")?>" data-toggle="modal" title="<?php eT('Add a new permission role'); ?>" class="btn btn-default RoleControl--action--openmodal">
-                    <i class="fa fa-plus-circle text-success"></i> <?php eT("Add role"); ?>
+                    <i class="fa fa-plus-circle text-success"></i> <?php eT("Add user role"); ?>
                 </button>
                 <button data-href="<?=App()->createUrl("admin/roles/sa/showImportXML")?>" data-toggle="modal" title="<?php eT('Import permission role from XML'); ?>" class="btn btn-default RoleControl--action--openmodal">
                     <i class="fa fa-upload text-success"></i> <?php eT("Import (XML)"); ?>
