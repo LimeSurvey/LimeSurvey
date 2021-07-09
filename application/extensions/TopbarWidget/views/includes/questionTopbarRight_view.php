9bfd312e462dd4abb70c3fee4163aa211b70cbbc
Jessica Höck <thedirtypanda001@gmail.com>
2021-07-09 13:03:44 +0200

    Dev: Added Delete and Save Button to Question summary view

--- application/extensions/TopbarWidget/views/includes/questionTopbarRight_view.php
+++ application/extensions/TopbarWidget/views/includes/questionTopbarRight_view.php
@@ -1,7 +1,29 @@
+<!-- Delete -->
+<?php if(!empty($showDeleteButton)):?>
+    <?php if($oSurvey->active!='Y'): ?>
+        <button class="btn btn-danger"
+                data-toggle="modal"
+                data-target="#confirmation-modal"
+                data-btnclass="btn-danger"
+                data-btntext="<?= gt('Delete')?>"
+                data-onclick='(function() { <?php echo convertGETtoPOST(Yii::app()->createUrl("questionAdministration/delete/", ["qid" => $qid, "redirectTo" => "groupoverview"])); ?>})'
+                data-message="<?php eT("Deleting this question will also delete any answer options and subquestions it includes. Are you sure you want to continue?","js"); ?>"
+        >
+            <span class="fa fa-trash"></span>
+            <?php eT("Delete"); ?>
+        </button>
+    <?php else: ?>
+        <button class="btn btn-danger btntooltip" disabled data-toggle="tooltip" data-placement="bottom" title="<?php eT("You can't delete a question if the survey is active."); ?>">
+            <span class="fa fa-trash"></span>
+            <?php eT("Delete"); ?>
+        </button>
+    <?php endif; ?>
+<?php endif; ?>
+
 <!-- Save -->
 <?php if(!empty($showSaveButton)): ?>
     <a id="save-button" class="btn btn-success" role="button">
-        <i class="fa fa-floppy-o"></i>
+        <i class="fa fa-check"></i>
         <?php eT("Save");?>
     </a>
 <?php endif; ?>
