c5f39d86babdcbb8a9af66ca4e9ca2648656ee24
Gabriel Jenik <gabriel.jenik@gmail.com>
2021-07-22 13:56:51 -0300

    T983: Survey List - Survey Groups - Odd Scroll when clicking on tabs (#1971)
    Co-authored-by: encuestabizdevgit <devgit@encuesta.biz>

--- application/views/layouts/fullpagebar_view.php
+++ application/views/layouts/fullpagebar_view.php
@@ -43,7 +43,7 @@
                    data-tab="#surveys"
                    href="<?php echo $fullpagebar['listSurveys']['buttons']['createSurvey']['url']; ?>"
                    role="button"
-                   style="margin-top: 10px; margin-bottom: 10px; display: none;">
+                   style="margin-top: 10px; margin-bottom: 10px;">
                     <span class="icon-add text-success"></span>
                     <?php eT("Create a new survey");?>
                 </a>
