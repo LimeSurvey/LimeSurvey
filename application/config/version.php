d23d891c7025f31fd19ae1f6af01408e68d42ed2
encuestabizdevgit <devgit@encuesta.biz>
2021-07-26 10:40:57 -0300

    Dev #17375: Unify upper/lower case in question type

--- application/config/version.php
+++ application/config/version.php
@@ -12,7 +12,7 @@
  */
 
 $config['versionnumber'] = '5.0.10';
-$config['dbversionnumber'] = 450;
+$config['dbversionnumber'] = 451;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
 $config['templateapiversion']  = 3;
--- application/helpers/update/updatedb_helper.php
+++ application/helpers/update/updatedb_helper.php
@@ -4638,6 +4638,31 @@ function db_upgrade_all($iOldDBVersion, $bSilent = false)
             $oDB->createCommand()->update('{{settings_global}}', array('stg_value' => 450), "stg_name='DBVersion'");
             $oTransaction->commit();
         }
+
+        if ($iOldDBVersion < 451) {
+            $oTransaction = $oDB->beginTransaction();
+
+            $fixedTitles = [
+                '5pointchoice' => '5 point choice',
+                'arrays/10point' => 'Array (10 point choice)',
+                'arrays/5point' => 'Array (5 point choice)',
+                'hugefreetext' => 'Huge free text',
+                'multiplenumeric' => 'Multiple numerical input',
+                'multipleshorttext' => 'Multiple short text',
+                'numerical' => 'Numerical input',
+                'shortfreetext' => 'Short free text',
+                'image_select-listradio' => 'Image select list (Radio)',
+                'image_select-multiplechoice' => 'Image select multiple choice',
+                'ranking_advanced' => 'Ranking advanced'
+            ];
+
+            foreach ($fixedTitles as $themeName => $newTitle) {
+                $oDB->createCommand()->update('{{question_themes}}', array('title' => $newTitle), "name='$themeName'");
+            }
+
+            $oDB->createCommand()->update('{{settings_global}}', array('stg_value' => 451), "stg_name='DBVersion'");
+            $oTransaction->commit();
+        }
     } catch (Exception $e) {
         Yii::app()->setConfig('Updating', false);
         $oTransaction->rollback();
