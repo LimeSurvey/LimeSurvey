d8a4e84e2630ec586d4a74260b0a38634a066802
Patrick Teichmann <patrick.teichmann@limesurvey.org>
2021-07-27 12:18:16 +0200

    Dev: fix existing cpdb custom attribute with multi encryption

--- application/helpers/update/updatedb_helper.php
+++ application/helpers/update/updatedb_helper.php
@@ -4638,6 +4638,42 @@ function db_upgrade_all($iOldDBVersion, $bSilent = false)
             $oDB->createCommand()->update('{{settings_global}}', array('stg_value' => 450), "stg_name='DBVersion'");
             $oTransaction->commit();
         }
+        if ($iOldDBVersion < 451) {
+            $oTransaction = $oDB->beginTransaction();
+
+            // update wrongly encrypted custom attribute values for cpdb participants
+            $encryptedAttributes = $oDB->createCommand()
+                ->select('attribute_id')
+                ->from('{{participant_attribute_names}}')
+                ->where('encrypted = :encrypted AND core_attribute <> :core_attribute', ['encrypted' => 'Y', 'core_attribute' => 'Y'])
+                ->queryAll();
+            $nrOfAttributes = count($encryptedAttributes);
+            foreach ($encryptedAttributes as $encryptedAttribute) {
+                $attributes= $oDB->createCommand()
+                    ->select('*')
+                    ->from('{{participant_attribute}}')
+                    ->where('attribute_id = :attribute_id', ['attribute_id' => $encryptedAttribute['attribute_id']])
+                    ->queryAll();
+                foreach ($attributes as $attribute) {
+                    $attributeValue = LSActiveRecord::decryptSingle($attribute['value']);
+                    // This extra decrypt loop is needed because of wrongly encrypted attributes.
+                    // @see d1eb8afbc8eb010104f94e143173f7d8802c607d
+                    for ($i = 1; $i < $nrOfAttributes; $i++) {
+                        $attributeValue = LSActiveRecord::decryptSingleOld($attributeValue);
+                    }
+                    $recryptedValue = LSActiveRecord::encryptSingle($attributeValue);
+                    $updateArray['value'] = $recryptedValue;
+                    $oDB->createCommand()->update(
+                        '{{participant_attribute}}',
+                        $updateArray,
+                        'participant_id = :participant_id AND attribute_id = :attribute_id',
+                        ['participant_id' => $attribute['participant_id'], 'attribute_id' => $attribute['attribute_id']]
+                    );
+                }
+            }
+            $oDB->createCommand()->update('{{settings_global}}', array('stg_value' => 451), "stg_name='DBVersion'");
+            $oTransaction->commit();
+        }
     } catch (Exception $e) {
         Yii::app()->setConfig('Updating', false);
         $oTransaction->rollback();
@@ -4717,6 +4753,7 @@ function db_upgrade_all($iOldDBVersion, $bSilent = false)
 function updateEncryptedValues450(CDbConnection $oDB)
 {
     Yii::app()->sodium;
+    // All these functions decrypt and then re-encrypt the values.
     decryptarchivedtables450($oDB);
     decryptResponseTables450($oDB);
     decryptParticipantTables450($oDB);
