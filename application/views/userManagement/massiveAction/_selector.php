7a3a87d5e2d13bf2a93977fbafd69e0745bc2d04
Patricia Stelling <patricia.stelling@limesurvey.org>
2021-07-28 11:46:59 +0200

    dev: take out unused code

--- application/views/userManagement/massiveAction/_selector.php
+++ application/views/userManagement/massiveAction/_selector.php
@@ -60,25 +60,6 @@ $aActionsArray = array(
             //'htmlFooterButtons' => [],
             'htmlModalBody'     => App()->getController()->renderPartial('/userManagement/massiveAction/_updatepermissions', [], true)
         ),
-
-        // Template Permission
-        /* Decision: take this out and reimplement the action button (this massive action was never working/implemented correctly)
-        array(
-            'type' => 'action',
-            'action' => 'templatePermission',
-            'url' => Yii::app()->getController()->createUrl('userManagement/userTemplatePermissions', ['userid' => $userid]),
-            'iconClasses' => 'fa fa-paint-brush',
-            'text' => gT("Template permissions"),
-            'grid-reload' => 'yes',
-            'actionType' => 'modal',
-            'modalType' => 'cancel-apply',
-            'largeModalView' => true,
-            'keepopen' => 'yes',
-            'showSelected' => 'yes',
-            'selectedUrl' => '',
-            'sModalTitle' => gT("Template permissions"),
-             'htmlModalBody'     => App()->getController()->renderPartial('/userManagement/massiveAction/_updatepermissions', [], true),
-        ),*/
         
     ),
 );
