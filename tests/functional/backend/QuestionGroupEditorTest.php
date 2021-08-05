a07d8317edde81ee80fa2979b520232aee45194f
encuestabizdevgit <devgit@encuesta.biz>
2021-08-05 15:56:15 -0300

    Dev #17350: Question theme should be a separate database column, not an attribute
    - Try to fix the tests

--- tests/functional/backend/QuestionGroupEditorTest.php
+++ tests/functional/backend/QuestionGroupEditorTest.php
@@ -89,7 +89,8 @@ class QuestionGroupEditorTest extends TestBaseClassWeb
 
             // Switch to German tab.
             self::$webDriver->executeScript("window.scrollTo(0, 0);");  // Scroll to top because otherwise the tabs may be hidden under the topbar
-            $germanTab = self::$webDriver->wait(2)->findElement(WebDriverBy::partialLinkText('German'));
+            sleep(2);
+            $germanTab = self::$webDriver->findElement(WebDriverBy::partialLinkText('German'));
             $germanTab->click();
 
             // Edit group name in German
@@ -161,7 +162,8 @@ class QuestionGroupEditorTest extends TestBaseClassWeb
 
             // Switch to German tab.
             self::$webDriver->executeScript("window.scrollTo(0, 0);");  // Scroll to top because otherwise the tabs may be hidden under the topbar
-            $germanTab = self::$webDriver->wait(2)->findElement(WebDriverBy::partialLinkText('German'));
+            sleep(2);
+            $germanTab = self::$webDriver->findElement(WebDriverBy::partialLinkText('German'));
             $germanTab->click();
 
             // Edit group name in German
