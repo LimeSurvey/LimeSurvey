66af51e74b36117ebd86e59d4cd85e986f843251
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2021-08-09 14:16:28 +0200

    Bump version to 5.1.0

--- application/config/version.php
+++ application/config/version.php
@@ -11,7 +11,7 @@
  * See COPYRIGHT.php for copyright notices and details.
  */
 
-$config['versionnumber'] = '5.1.0-dev';
+$config['versionnumber'] = '5.1.0';
 $config['dbversionnumber'] = 470;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
