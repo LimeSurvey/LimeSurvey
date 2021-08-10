6f862e6bc5ae5945ad5ac6704e05a887deec200d
Gabriel Jenik <gabriel.jenik@gmail.com>
2021-08-10 09:55:33 -0300

    Dev #T1051: Question Summary: Move Export and Copy under tools (#1989)
    * Dev #T1051: Question Summary: Move Export and Copy under tools
    
    * Dev #T1051: Question Summary: Move Export and Copy under tools
    
    - Move export and copy options above survey logic
    
    * Dev #T1051: Question Summary: Move Export and Copy under tools
    
    - Hide Tools when dropdown is empty
    
    * Dev #T1051: Question Summary: Move Export and Copy under tools
    
    - Move Edit button to the right
    - Move Delete button inside tools dropdown
    
    Co-authored-by: encuestabizdevgit <devgit@encuesta.biz>

--- application/extensions/TopbarWidget/views/includes/questionTopbarLeft_view.php
+++ application/extensions/TopbarWidget/views/includes/questionTopbarLeft_view.php
@@ -32,14 +32,11 @@ $this->render('includes/previewSurveyAndGroupButtons_view', get_defined_vars());
     <?php endif; ?>
 <?php endif; ?>
 
-<!-- Edit button -->
-<?php if($hasSurveyContentUpdatePermission): ?>
-    <a id="questionEditorButton" class="btn btn-success pjax" href="#" role="button" onclick="LS.questionEditor.showEditor(); return false;">
-        <span class="icon-edit"></span>
-        <?php eT("Edit");?>
-    </a>
-<?php endif; ?>
-
+<?php
+    // Tools dropdown button
+    $toolsDropdownItems = $this->render('includes/questionToolsDropdownItems', get_defined_vars(), true);
+    if (!empty(trim($toolsDropdownItems))):
+?>
 <!-- Tools  -->
 <div class="btn-group hidden-xs">
 
@@ -51,66 +48,7 @@ $this->render('includes/previewSurveyAndGroupButtons_view', get_defined_vars());
 
     <!-- dropdown -->
     <ul class="dropdown-menu">
-        <!-- conditions -->
-        <?php if($hasSurveyContentUpdatePermission):?>
-            <li>
-                <a id="conditions_button" href="<?php echo Yii::App()->createUrl("admin/conditions/sa/index/subaction/editconditionsform/surveyid/$surveyid/gid/$gid/qid/$qid"); ?>">
-                    <span class="icon-conditions icon"></span>
-                    <?php eT("Condition designer"); ?>
-                </a>
-            </li>
-        <?php endif;?>
-
-        <!-- Default Values -->
-        <?php if($hasSurveyContentUpdatePermission && $hasdefaultvalues > 0):?>
-            <li>
-                <a id="default_value_button" href="<?php echo Yii::App()->createUrl("questionAdministration/editdefaultvalues/surveyid/$surveyid/gid/$gid/qid/$qid"); ?>">
-                    <span class="icon-defaultanswers icon"></span>
-                    <?php eT("Edit default answers"); ?>
-                </a>
-            </li>
-        <?php endif;?>
-
-        <?php if($hasSurveyContentReadPermission): ?>
-            <?php if (count($surveyLanguages) > 1): ?>
-                <!-- Check survey logic multilanguage -->
-                <li role="separator" class="divider"></li>
-                <li class="dropdown-header"><?php eT("Survey logic file"); ?></li>
-                <?php foreach ($surveyLanguages as $languageCode => $languageName): ?>
-                    <li>
-                        <a href="<?php echo Yii::App()->createUrl("admin/expressions/sa/survey_logic_file/sid/{$surveyid}/gid/{$gid}/qid/{$qid}/lang/" . $languageCode); ?>" >
-                            <span class="icon-expressionmanagercheck"></span>
-                            <?php echo $languageName; ?>
-                        </a>
-                    </li>
-                <?php endforeach; ?>
-            <?php else:?>
-                <!-- Check survey logic -->
-                <li>
-                    <a class="pjax" href="<?php echo Yii::App()->createUrl("admin/expressions/sa/survey_logic_file/sid/{$surveyid}/gid/{$gid}/qid/{$qid}"); ?>">
-                        <span class="icon-expressionmanagercheck"></span>
-                        <?php eT("Check logic"); ?>
-                    </a>
-                </li>
-            <?php endif; ?>
-        <?php endif; ?>
-
+        <?= $toolsDropdownItems ?>
     </ul>
 </div>
-
-<!-- Export -->
-<?php if($hasSurveyContentExportPermission):?>
-    <a class="btn btn-default " href="<?php echo Yii::App()->createUrl("admin/export/sa/question/surveyid/$surveyid/gid/$gid/qid/{$qid}");?>" role="button">
-        <span class="icon-export"></span>
-        <?php eT("Export"); ?>
-    </a>
 <?php endif; ?>
-
-<!-- Copy -->
-<?php if($hasSurveyContentCreatePermission && ($oSurvey->active!='Y')):?>
-    <a class="btn btn-default" id="copy_button" href='<?php echo Yii::App()->createUrl("questionAdministration/copyQuestion/surveyId/{$oQuestion->sid}/questionGroupId/{$oQuestion->gid}/questionId/{$oQuestion->qid}");?>'>
-        <span class="icon-copy icon"></span>
-        <?php eT("Copy"); ?>
-    </a>
-<?php endif; ?>
-
