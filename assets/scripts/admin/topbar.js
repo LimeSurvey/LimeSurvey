0c24816f9886df0d35afde3dd4f249e0bcff3c7c
encuestabizdevgit <devgit@encuesta.biz>
2021-08-31 10:14:04 -0300

    Fix failing QuestionGroupEditorTest tests
    - Don't make topbar sticky when viewport height is too small

--- assets/scripts/admin/topbar.js
+++ assets/scripts/admin/topbar.js
@@ -6,6 +6,11 @@ $(document).on('ready pjax:scriptcomplete', function () {
         }
         const topbarOffset = topbar.offset().top;
 
+        // Don't make the topbar sticky if the window height is to small
+        if (window.innerHeight < topbarOffset + topbar.outerHeight(true)) {
+            return;
+        }
+
         // When the topbar is floated, the space it occupies is released, so the rest of the content "jumps" up.
         // To avoid that, a "placeholder" div is added, with the same height as the topbar.
         // The placeholder is only visible (by css) when the topbar has the 'sticky' class.
