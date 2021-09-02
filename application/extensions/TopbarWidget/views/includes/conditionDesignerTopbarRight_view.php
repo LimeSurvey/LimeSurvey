75884d5d54273771904a9629a6e5e47f67a27534
Gabriel Jenik <gabriel.jenik@gmail.com>
2021-09-02 03:29:18 -0300

    Dev #T1137: 'save and close button' for conditions not working (#2020)
    Co-authored-by: encuestabizdevgit <devgit@encuesta.biz>

--- application/extensions/TopbarWidget/views/includes/conditionDesignerTopbarRight_view.php
+++ application/extensions/TopbarWidget/views/includes/conditionDesignerTopbarRight_view.php
@@ -1,8 +1,10 @@
-<?php echo $questionNavOptions; ?> <!-- HTML is in views/admin/conditions/includes/navigator.php -->
-
-<?php 
+<div class="baseline-align">
+<?php
     /**
      * Include the default buttons
      */
     $this->render('includes/surveyTopbarRight_view', get_defined_vars());
 ?>
+
+<?php echo $questionNavOptions; ?> <!-- HTML is in views/admin/conditions/includes/navigator.php -->
+</div>
