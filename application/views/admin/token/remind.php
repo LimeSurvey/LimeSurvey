d456b4743a03147b18ef2a15ed9786563439a6c5
Jessica Höck <thedirtypanda001@gmail.com>
2021-10-20 16:42:54 +0200

    Fixed issue #T1398: Send reminders - Inputs have wrong size - modified size

--- application/views/admin/token/remind.php
+++ application/views/admin/token/remind.php
@@ -51,7 +51,7 @@
                     <div class='form-group'>
                         <label class='control-label ' for='minreminderdelay'><?php eT("Min days between reminders:"); ?></label>
                         <div class=''>
-                            <input type="text" id="minreminderdelay" class="form-control" size="50" value="" name="minreminderdelay" />
+                            <input type="text" id="minreminderdelay" class="form-control" size="25" value="" name="minreminderdelay" />
                         </div>
                     </div>
                 </div>
@@ -61,7 +61,7 @@
                     <div class='form-group'>
                         <label class='control-label ' for='maxremindercount'><?php eT("Max reminders:"); ?></label>
                         <div class=''>
-                            <input type="text" id="maxremindercount" class="form-control" size="50" value="" name="maxremindercount" />
+                            <input type="text" id="maxremindercount" class="form-control" size="25" value="" name="maxremindercount" />
                         </div>
                     </div>
 
