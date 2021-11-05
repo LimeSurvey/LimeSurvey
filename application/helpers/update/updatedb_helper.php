b0a5b0c1f924aeb553089266ac2debcce5d57fc5
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2021-11-05 11:53:56 +0100

    Dev: Missing curly brace

--- application/helpers/update/updatedb_helper.php
+++ application/helpers/update/updatedb_helper.php
@@ -5028,6 +5028,7 @@ function db_upgrade_all($iOldDBVersion, $bSilent = false)
                 "name='responses'"
             );
             $oDB->createCommand()->update('{{settings_global}}', ['stg_value' => 477], "stg_name='DBVersion'");
+        }
     } catch (Exception $e) {
         Yii::app()->setConfig('Updating', false);
         $oTransaction->rollback();
