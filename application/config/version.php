6c8db12a8666dfec1331d4918452d656e1fba7a1
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2021-11-10 11:44:39 +0100

    Dev: Set develop version to 5.3.0-dev

--- application/config/version.php
+++ application/config/version.php
@@ -11,7 +11,7 @@
  * See COPYRIGHT.php for copyright notices and details.
  */
 
-$config['versionnumber'] = '5.2.0-dev';
+$config['versionnumber'] = '5.3.0-dev';
 $config['dbversionnumber'] = 477;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
