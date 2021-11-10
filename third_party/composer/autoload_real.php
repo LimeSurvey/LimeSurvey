90efa546999d15c92e0f1cd0aabdc487057b464c
encuestabizdevgit <devgit@encuesta.biz>
2021-11-10 12:28:18 -0300

    Revert "Fixed issue #17527: Validate before using updateAll"
    This reverts commit 830d6e4325217c92785184da2debb47a22ea30dd.

--- third_party/composer/autoload_real.php
+++ third_party/composer/autoload_real.php
@@ -22,13 +22,15 @@ class ComposerAutoloaderInitddb1a145e450f862353420acc5153e40
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInitddb1a145e450f862353420acc5153e40', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInitddb1a145e450f862353420acc5153e40', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require_once __DIR__ . '/autoload_static.php';
+            require __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInitddb1a145e450f862353420acc5153e40::getInitializer($loader));
         } else {
