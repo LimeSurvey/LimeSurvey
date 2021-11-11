b7d76a6135834c6b6172adab181ce13b8dd82fe6
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2021-11-11 15:47:26 +0100

    Dev: Query scalar, not row

--- application/helpers/update/updatedb_helper.php
+++ application/helpers/update/updatedb_helper.php
@@ -5085,7 +5085,7 @@ function db_upgrade_all($iOldDBVersion, $bSilent = false)
 
     // Inform superadmin about update
     $superadmins = User::model()->getSuperAdmins();
-    $currentDbVersion = $oDB->createCommand()->select('stg_value')->from('{{settings_global}}')->where("stg_name=:stg_name", array('stg_name' => 'DBVersion'))->queryRow();
+    $currentDbVersion = $oDB->createCommand()->select('stg_value')->from('{{settings_global}}')->where("stg_name=:stg_name", array('stg_name' => 'DBVersion'))->queryScalar();
     // Update the global config object because it is static and set at start of App
     Yii::app()->setConfig('DBVersion', $currentDbVersion['stg_value']);
 
