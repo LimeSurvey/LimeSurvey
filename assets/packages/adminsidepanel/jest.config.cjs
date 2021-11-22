475ae3c180989d4d3ed4f7729ad51a58b60f5544
Jessica Höck <thedirtypanda001@gmail.com>
2021-11-22 16:31:14 +0000

    Dev: Renamed some configuration files

--- assets/packages/adminsidepanel/jest.config.cjs
+++ assets/packages/adminsidepanel/jest.config.cjs
@@ -73,11 +73,11 @@ module.exports = {
   // An array of file extensions your modules use
    moduleFileExtensions: [
      "js",
-     "jsx",
      "ts",
      "tsx",
      "json",
-     "node"
+     "node",
+     "vue",
    ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
@@ -172,7 +172,10 @@ module.exports = {
   // timers: "real",
 
   // A map from regular expressions to paths to transformers
-   transform: {"\\.js$": ['babel-jest', {rootMode: "upward"}]},
+   transform: {
+     "^.+\\.js$": "babel-jest",
+     "^.+\\.vue$": "@vue/vue2-jest",
+    },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   transformIgnorePatterns: [
