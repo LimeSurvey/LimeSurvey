945b200b8821239740ae5d30eda0a521f5e54cb2
Jessica Höck <thedirtypanda001@gmail.com>
2021-11-23 09:04:50 +0000

    Dev: Added Browser Environment to jest config

--- assets/packages/adminsidepanel/jest.config.cjs
+++ assets/packages/adminsidepanel/jest.config.cjs
@@ -137,7 +137,7 @@ module.exports = {
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
-  // testEnvironment: "jest-environment-node",
+   testEnvironment: "jsdom",
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
