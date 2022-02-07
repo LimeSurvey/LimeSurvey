f386d69a0d6a08e726842454a86e9b25570ea095
olleharstedt <olleharstedt@yahoo.com>
2022-02-07 11:52:21 +0100

    Release 3.27.35+220208

--- application/config/version.php
+++ application/config/version.php
@@ -12,10 +12,10 @@
  */
 
 
-$config['versionnumber'] = '3.27.34';
+$config['versionnumber'] = '3.27.35';
 $config['dbversionnumber'] = 366;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
 $config['templateapiversion'] = 3;
-$config['assetsversionnumber'] = '30226';
+$config['assetsversionnumber'] = '30227';
 return $config;
