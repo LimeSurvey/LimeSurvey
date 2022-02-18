7cebc8e6f1c1216a4795d1f6477dd5fb67f84d7c
Tim Willig <tim.willig@limesurvey.org>
2022-02-18 16:07:25 +0100

    dev: Intermediate result

--- application/views/admin/export/statistics_subviews/_outputformat.php
+++ application/views/admin/export/statistics_subviews/_outputformat.php
@@ -1,18 +1,22 @@
-    <div class="panel panel-primary " id="panel-output-forma">
-        <div class="panel-heading">
-            <div class="panel-title h4"><?php eT("Output format"); ?></div>
-        </div>
-        <div class="panel-body">
-            <div class="btn-group" data-toggle="buttons">
-                <label class="btn btn-default active">
-                    <input name="outputtype" value="html" type="radio" checked='checked' id="outputtypehtml" autofocus="true" ><?php eT('HTML');?>
-                </label>
-                <label class="btn btn-default">
-                    <input name="outputtype" value="pdf" type="radio" id="outputtypepdf"><?php eT('PDF');?>
-                </label>
-                <label class="btn btn-default">
-                    <input name="outputtype" value="xls" class="active" type="radio" id="outputtypexls"  onclick='nographs();'><?php eT('Excel'); ?>
-                </label>
-            </div>
+<h4 class="h4"><?php
+    eT("Output format"); ?></h4>
+<div class="row">
+    <div class='form-group'>
+        <div class="btn-group" data-toggle="buttons">
+            <label class="btn btn-default active">
+                <input name="outputtype" value="html" type="radio" checked='checked' id="outputtypehtml">
+                <?php
+                eT('HTML'); ?>
+            </label>
+            <label class="btn btn-default">
+                <input name="outputtype" value="pdf" type="radio" id="outputtypepdf"><?php
+                eT('PDF'); ?>
+            </label>
+            <label class="btn btn-default">
+                <input name="outputtype" value="xls" class="active" type="radio" id="outputtypexls"
+                       onclick='nographs();'><?php
+                eT('Excel'); ?>
+            </label>
         </div>
     </div>
+</div>
