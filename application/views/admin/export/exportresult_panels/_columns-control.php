568a53725eb28dfc4cb1ae2a4ee93d849360a87b
Carsten Schmitz <carsten.schmitz@limesurvey.org>
2022-02-24 11:08:42 +0100

    Fixed issue: Not all response columns begin exported if PHP setting max_input_vars is too low Dev Added a warning hint if setting seems too low

--- application/views/admin/export/exportresult_panels/_columns-control.php
+++ application/views/admin/export/exportresult_panels/_columns-control.php
@@ -21,5 +21,18 @@
         <div class="col-sm-12 text-center">
           <strong id='columncount'>&nbsp;</strong>
         </div>
+        <?php if (count($aFields) + 20 > (int) ini_get('max_input_vars')): ?>
+          <div class="col-sm-12">
+            <div class="alert alert-warning alert-dismissible" role="alert">
+                <button type="button" class="close limebutton" data-dismiss="alert" aria-label="Close"><span>X</span>
+                </button>
+                <?php
+                eT("The number of fields in your survey exceed the maximum numbers of fields you can export."); ?>
+                <br/>
+                <?php
+                printf(gT("If data is missing in the exported file, please contact you system administrator to raise the setting max_input_vars to at least %s."), count($aFields)+20); ?>
+            </div>
+         </div>
+        <?php endif; ?>
   </div>
 </div>
