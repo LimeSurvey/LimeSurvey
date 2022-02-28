f7fde2ae0bbb3a67b35917174363f124e3c9ce3e
Jessica Höck <thedirtypanda001@gmail.com>
2022-02-28 16:01:24 +0000

    Dev: Build new version of adminsidepanel assets

--- assets/packages/adminsidepanel/build/js/adminsidepanel.js
+++ assets/packages/adminsidepanel/build/js/adminsidepanel.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "bf0206b9467a647a5013";
+/******/ 	var hotCurrentHash = "ce60dd532c560220ddf3";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
