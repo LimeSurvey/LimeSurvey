cb1299f26d8e8cd58ad55fd33828610c07eb117c
Carsten Schmitz <carsten.schmitz@limesurvey.org>
2022-03-09 12:21:02 +0100

    Re-applied LimeService customization so that it is clear that there was some modification

--- application/core/LSYii_Application.php
+++ application/core/LSYii_Application.php
@@ -136,7 +136,7 @@ class LSYii_Application extends CWebApplication
 
         // TODO: check the whole configuration process. It must be easier and clearer. Too many repitions
 
-        /* Default config */
+        /* Default config */ 
         $coreConfig = require(__DIR__ . '/../config/config-defaults.php');
         $emailConfig = require(__DIR__ . '/../config/email.php');
         $versionConfig = require(__DIR__ . '/../config/version.php');
