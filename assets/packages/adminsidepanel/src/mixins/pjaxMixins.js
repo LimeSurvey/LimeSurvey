f16176a8645383c485b072ea4bd70a62dfc7e121
Rodrigo Teixeira <rod@naiteon.com>
2022-03-17 01:52:49 -0300

    Reseting adminsidepanel

--- assets/packages/adminsidepanel/src/mixins/pjaxMixins.js
+++
@@ -1,8 +0,0 @@
-export const pjaxMixins = {
-    methods: {
-        updatePjaxLinks: function (store) {
-            this.$forceUpdate();
-            store.commit('newToggleKey');
-        },
-    },
-};
