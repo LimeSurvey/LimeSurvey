a50d6f4c39769ea9364cb8a0ce154d3666d06d41
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2022-03-25 12:28:59 +0100

    Use cards instead of panels (WIP)

--- application/extensions/PanelBoxWidget/views/box.php
+++ application/extensions/PanelBoxWidget/views/box.php
@@ -1,21 +1,23 @@
-<div class="<?php echo $sizeClass; ?> ls-flex-column ls-panelboxes-panelbox text-primary" >
-    <div class="panel panel-primary panel-clickable ls-panelboxes-panelbox-inner selector__<?php echo CHtml::encode(str_replace(' ', '_', strtolower(strip_tags($title)))) ?>"
-        id="panel-<?php echo $position; ?>"
+<div class="<?php echo $sizeClass; ?> ls-flex-column ls-panelboxes-panelbox" >
+    <div class="card panel-clickable ls-panelboxes-panelbox-inner selector__<?php echo CHtml::encode(str_replace(' ', '_', strtolower(strip_tags($title)))) ?>"
+        id="card-<?php echo $position; ?>"
         data-url="<?php echo CHtml::encode($url); ?>"
         <?php if ($external): ?>
             data-target="_blank"
         <?php endif; ?>
     >
-        <div class="panel-heading">
-            <div class="panel-title h4"><?php echo viewHelper::filterScript(gT($title)); ?></div>
+        <div class="card-header bg-primary text-white">
+            <div class="card-title"><?php echo viewHelper::filterScript(gT($title)); ?></div>
         </div>
-        <div class="panel-body">
-            <div class="panel-body-ico">
+        <div class="card-body">
+            <div class="card-body-ico">
                 <span class="sr-only"><?php echo viewHelper::filterScript(gT($title)); ?></span>
                 <span class="<?php echo CHtml::encode($ico); ?>" style="font-size: 4em">
                 </span>
             </div>
-            <div class="panel-body-link">
+        </div>
+        <div class="card-footer">
+            <div class="card-body-link">
                 <?php echo viewHelper::filterScript(gT($description)); ?>
                 <?php if ($external): ?>
                     &nbsp;<i class="fa fa-external-link"></i>
