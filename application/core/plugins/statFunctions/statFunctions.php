c04a5fd0d1e9fc69108ee6a4f3ec7201bfd7e0e2
Denis Chenu <denis@sondages.pro>
2022-04-01 15:23:49 +0200

    Fixed issue #18006: Unable to use statCountIf to have a "unicity" control Dev: add optionnal sel parameters to false (no function API update)

--- application/core/plugins/statFunctions/statFunctions.php
+++ application/core/plugins/statFunctions/statFunctions.php
@@ -2,9 +2,9 @@
 
 /**
  * @author Denis Chenu <denis@sondages.pro>
- * @copyright 2019 LimeSurvey - Denis Chenu
+ * @copyright 2019-2022 LimeSurvey - Denis Chenu
  * @license GPL version 3
- * @version 0.1.1
+ * @version 0.2.0
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -40,19 +40,17 @@ class statFunctions extends PluginBase
                 '\statFunctions\countFunctions::statCountIf',
                 null, // No javascript function : set as static function
                 $this->gT("Count the number of complete responses  with a value equal to a specific value"), // Description for admin
-                'integer statCountIf(QuestionCode.sgqa, value[, submitted = true])', // Extra description
-                'https://www.limesurvey.org', // Help url
-                2, // Number of argument unsure it work here … , minimum 2, allow 3
-                3
+                'integer statCountIf(QuestionCode.sgqa, value[, submitted = true][, self = true])', // Extra description
+                'https://manual.limesurvey.org/StatFunctions', // Help url
+                2, 3, 4 // Number of arguments : 2 , 3 or 4
             ),
             'statCount' => array(
                 '\statFunctions\countFunctions::statCount',
                 null, // No javascript function : set as static function
                 $this->gT("Count the number of complete responses which are not empty"), // Description for admin
-                'integer statCount(QuestionCode.sgqa[, submitted = true])', // Extra description
-                'https://www.limesurvey.org', // Help url
-                1, // Number of argument (time to make a good description of EM …) minimum 1, allow 2
-                2,
+                'integer statCount(QuestionCode.sgqa[, submitted = true][, self = true])', // Extra description
+                'https://manual.limesurvey.org/StatFunctions', // Help url
+                1, 2, 3 // Number of argument : 1 , 2 or 3
             ),
         );
         $this->getEvent()->append('functions', $newFunctions);
