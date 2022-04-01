5e9f56d8c36fa0ae35463dacb6a428df1bcac2fe
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2022-04-01 14:32:11 +0200

    Replace panel with card in global settings

--- application/views/questionAdministration/generalSettings.php
+++ application/views/questionAdministration/generalSettings.php
@@ -1,11 +1,11 @@
-<div class="panel panel-default question-option-general-container col-12" id="general-settings">
-    <div class="panel-heading" id="general-setting-heading">
-        <a class="panel-title h4 selector--questionEdit-collapse" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapse-question" aria-expanded="true" aria-controls="collapse-question">
+<div class="card question-option-general-container col-12" id="general-settings">
+    <div class="card-heading" id="general-setting-heading">
+        <a class="card-title h4 selector--questionEdit-collapse" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapse-question" aria-expanded="true" aria-controls="collapse-question">
             <?= gT('General Settings'); ?>
         </a>
     </div>
-    <div id="collapse-question" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="general-setting-heading">
-        <div class="panel-body">
+    <div id="collapse-question" class="collapse in" role="tabpanel" aria-labelledby="general-setting-heading">
+        <div class="card-body">
             <?php foreach ($generalSettings as $generalOption) : ?>
                 <?php $this->widget(
                     'ext.GeneralOptionWidget.GeneralOptionWidget',
