1515c40b9b4cc3564894d4c67d3c595198ce1cce
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2022-04-05 14:13:02 +0200

    Fix data-bs-target for accordion

--- application/views/questionAdministration/advancedSettings.php
+++ application/views/questionAdministration/advancedSettings.php
@@ -1,30 +1,36 @@
-    <?php foreach ($advancedSettings as $category => $settings) : ?>
-        <div class="accordion-item panel-advancedquestionsettings col-12" id="<?= CHtml::getIdByName($category); ?>">
-            <h2 class="accordion-header bg-primary" id="<?= CHtml::getIdByName($category); ?>-heading">
-                <button
-                    class="selector--questionEdit-collapse accordion-button"
-                    id="button-collapse-<?= CHtml::getIdByName($category); ?>"
-                    role="button"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target=""
-                    data-parent="#accordion"
-                    href="#collapse-<?= CHtml::getIdByName($category); ?>"
-                    aria-expanded="false"
-                    aria-controls="collapse-<?= CHtml::getIdByName($category); ?>"
-                >
-                    <?= gT($category); ?>
-                </button>
-            </h2>
-            <div id="collapse-<?= CHtml::getIdByName($category); ?>" class="panel-collapse collapse" role="tabpanel" aria-labelledby="<?= CHtml::getIdByName($category); ?>-heading">
-                <div class="accordion-body">
-                    <?php foreach ($settings as $setting) : ?>
-                        <?php $this->widget(
-                            'ext.AdvancedSettingWidget.AdvancedSettingWidget',
-                            ['setting' => $setting, 'survey' => $oSurvey]
-                        ); ?>
-                    <?php endforeach; ?>
-                </div>
+<?php foreach ($advancedSettings as $category => $settings) : ?>
+    <div class="accordion-item panel-advancedquestionsettings col-12" id="<?= CHtml::getIdByName($category); ?>">
+        <h2 class="accordion-header bg-primary" id="<?= CHtml::getIdByName($category); ?>-heading">
+            <button
+                class="selector--questionEdit-collapse accordion-button collapsed"
+                id="button-collapse-<?= CHtml::getIdByName($category); ?>"
+                role="button"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapse-<?= CHtml::getIdByName($category); ?>"
+                data-bs-parent="#accordion"
+                href="#collapse-<?= CHtml::getIdByName($category); ?>"
+                aria-expanded="false"
+                aria-controls="collapse-<?= CHtml::getIdByName($category); ?>"
+            >
+                <?= gT($category); ?>
+            </button>
+        </h2>
+        <div
+            id="collapse-<?= CHtml::getIdByName($category); ?>"
+            class="accordion-collapse collapse"
+            data-bs-parent="#accordion"
+            role="tabpanel"
+            aria-labelledby="<?= CHtml::getIdByName($category); ?>-heading"
+        >
+            <div class="accordion-body">
+                <?php foreach ($settings as $setting) : ?>
+                    <?php $this->widget(
+                        'ext.AdvancedSettingWidget.AdvancedSettingWidget',
+                        ['setting' => $setting, 'survey' => $oSurvey]
+                    ); ?>
+                <?php endforeach; ?>
             </div>
         </div>
-    <?php endforeach; ?>
+    </div>
+<?php endforeach; ?>
--- application/views/questionAdministration/generalSettings.php
+++ application/views/questionAdministration/generalSettings.php
@@ -1,10 +1,19 @@
 <div class="accordion-item question-option-general-container col-12" id="general-settings">
     <h2 class="accordion-header" id="general-setting-heading">
-        <button class="accordion-button selector--questionEdit-collapse" type="button" role="button" data-bs-toggle="collapse" data-bs-target="" data-parent="#accordion" href="#collapse-question" aria-expanded="true" aria-controls="collapse-question">
+        <button
+            class="accordion-button selector--questionEdit-collapse"
+            type="button"
+            role="button"
+            data-bs-toggle="collapse"
+            data-bs-parent="#accordion"
+            href="#collapse-question"
+            aria-expanded="true"
+            aria-controls="collapse-question"
+        >
             <?= gT('General Settings'); ?>
         </button>
     </h2>
-    <div id="collapse-question" class="collapse in accordion-collapse" role="tabpanel" aria-labelledby="general-setting-heading">
+    <div id="collapse-question" class="accordion-collapse" role="tabpanel" aria-labelledby="general-setting-heading">
         <div class="accordion-body collapse show">
             <?php foreach ($generalSettings as $generalOption) : ?>
                 <?php $this->widget(
