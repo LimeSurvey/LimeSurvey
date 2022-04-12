c30ac4bb132a9af037579a4d0f8634c457651636
encuestabizdevgit <devgit@encuesta.biz>
2022-04-12 16:06:38 -0300

    Fixed issue #17984: Using method="list_questions" does not give the subquestions

--- application/helpers/remotecontrol/remotecontrol_handle.php
+++ application/helpers/remotecontrol/remotecontrol_handle.php
@@ -2307,10 +2307,10 @@ class remotecontrol_handle
                     if ($oGroup->sid != $oSurvey->sid) {
                         return ['status' => 'Error: Mismatch in surveyid and groupid'];
                     } else {
-                        $aQuestionList = $oGroup->questions;
+                        $aQuestionList = Question::model()->findAllByAttributes(["sid" => $oSurvey->sid, "gid" => $iGroupID]);
                     }
                 } else {
-                    $aQuestionList = $oSurvey->baseQuestions;
+                    $aQuestionList = $oSurvey->allQuestions;
                 }
 
                 if (count($aQuestionList) == 0) {
