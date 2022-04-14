74cca6baf8af1d58929da0834405c3704c8894d4
Tim Willig <tim.willig@limesurvey.org>
2022-04-14 16:32:31 +0200

    Revert composer file changes

--- third_party/composer/autoload_real.php
+++ third_party/composer/autoload_real.php
@@ -65,16 +65,11 @@ class ComposerAutoloaderInitddb1a145e450f862353420acc5153e40
     }
 }
 
-/**
- * @param string $fileIdentifier
- * @param string $file
- * @return void
- */
 function composerRequireddb1a145e450f862353420acc5153e40($fileIdentifier, $file)
 {
     if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
         require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
