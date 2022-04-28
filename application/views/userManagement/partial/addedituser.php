f554e0d6c5fde1162c5e43dc1fe7f63ace7de0d3
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2022-04-28 13:19:32 +0200

    Fixed issue LSC-129: Fix add user modal form in user management

--- application/views/userManagement/partial/addedituser.php
+++ application/views/userManagement/partial/addedituser.php
@@ -19,10 +19,10 @@ Yii::app()->getController()->renderPartial(
     <div class="container-center">
 
         <?=$form->hiddenField($oUser, 'uid', ['uid' => 'User_Form_users_id'])?>
-        <div class="row ls-space margin top-5 bottom-5 d-none" id="UserManagement--errors">
+        <div class="mb-3" id="UserManagement--errors">
 
         </div>
-        <div class="row ls-space margin top-5">
+        <div class="mb-3">
             <?php echo $form->labelEx($oUser, 'users_name', ['for' => 'User_Form_users_name']); ?>
             <?php
                 if ($oUser->isNewRecord) {
@@ -34,44 +34,44 @@ Yii::app()->getController()->renderPartial(
 
             <?php echo $form->error($oUser, 'users_name'); ?>
         </div>
-        <div class="row ls-space margin top-5">
+        <div class="mb-3">
             <?php echo $form->labelEx($oUser, 'full_name', ['for'=>'User_Form_full_name']); ?>
             <?php echo $form->textField($oUser, 'full_name', ['id'=>'User_Form_full_name']); ?>
             <?php echo $form->error($oUser, 'full_name'); ?>
         </div>
-        <div class="row ls-space margin top-5">
+        <div class="mb-3">
             <?php echo $form->labelEx($oUser, 'email', ['for'=>'User_Form_email']); ?>
             <?php echo $form->emailField($oUser, 'email', ['id'=>'User_Form_email', 'required' => 'required']); ?>
             <?php echo $form->error($oUser, 'email'); ?>
         </div>
         <?php if (!$oUser->isNewRecord) { ?>
-        <div class="row ls-space margin top-10">
-            <div class="col-xs-12">
+        <div class="mb-3">
                 <input type="checkbox" id="utility_change_password">
                 <label for="utility_change_password"><?=gT("Change password?")?></label>
-            </div>
         </div>
         <?php } else { ?>
-            <div class="row ls-space margin top-10" id="utility_set_password">
-                <div class="col-xs-6" >
+            <div class="mb-3" id="utility_set_password">
+                <div class="col-6" >
                     <label><?=gT("Set password now?")?></label>
                 </div>
-                <div class="btn-group col-xs-6" data-toggle="buttons">
-                    <label for="utility_set_password_yes" class="btn btn-default col-xs-6">
-                        <input type="radio" id="utility_set_password_yes" name="preset_password" value="1">
+                <div class="btn-group col-6" data-bs-toggle="buttons">
+                    <input class="btn-check" type="radio" id="utility_set_password_yes" name="preset_password" value="1">
+                    <label for="utility_set_password_yes" class="btn btn-outline-primary col-6">
                         <?=gT("Yes")?>
                     </label>
-                    <label for="utility_set_password_no" class="btn btn-default col-xs-6 active">
-                        <input type="radio" id="utility_set_password_no" checked="checked" name="preset_password" value="0">
+                    <input class="btn-check" type="radio" id="utility_set_password_no" checked name="preset_password" value="0">
+                    <label for="utility_set_password_no" class="btn btn-outline-primary col-6">
                         <?=gT("No")?>
                     </label>
                 </div>
             </div>
         <?php } ?>        
 
-        <div class="row ls-space margin top-5 d-none" id="utility_change_password_container">
-            <div class="row ls-space margin top-5">
-                <span class="text-warning"><?= gT('If you set a password here, no email will be sent to the new user.')?></span><br><br>
+        <div class="d-none" id="utility_change_password_container">
+            <div class="mb-3">
+                <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                    <?= gT('If you set a password here, no email will be sent to the new user.')?>
+                </div>
                 <?php echo $form->labelEx($oUser,'password', ['for'=>'User_Form_password']); ?>
                 <?php echo $form->passwordField(
                     $oUser,
@@ -83,12 +83,12 @@ Yii::app()->getController()->renderPartial(
                 ); ?>
                 <?php echo $form->error($oUser,'password'); ?>
             </div>
-            <div class="row ls-space margin top-5">
+            <div class="mb-3">
                 <label for="password_repeat" class="required" required><?=gT("Password safety")?> <span class="required">*</span></label>            
                 <input name="password_repeat" placeholder='********' <?=($oUser->isNewRecord ? '' :'disabled="disabled"')?> id="password_repeat" class="form-control" type="password">
             </div>
             <?php if($oUser->isNewRecord) { ?> 
-            <div class="row ls-space margin top-5">
+            <div class="mb-3">
                 <label class="form-label">
                     <?=gT('Random password (suggestion):')?>
                 </label> 
@@ -101,7 +101,7 @@ Yii::app()->getController()->renderPartial(
 </div>
 
 <div class="modal-footer modal-footer-buttons" style="margin-top: 15px;" >
-    <button class="btn btn-cancel" id="exitForm"><?=gT('Cancel')?></button>
+    <button class="btn btn-default" id="exitForm"><?=gT('Cancel')?></button>
     <button class="btn btn-success" id="submitForm"><?=gT('Add')?></button>
 </div>
 <?php $this->endWidget(); ?>
