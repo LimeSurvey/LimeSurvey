e395405eb4b03e80735656c2e75ad695a81c0117
encuestabizdevgit <devgit@encuesta.biz>
2022-05-04 18:22:32 -0300

    Fixed issue #17992: unable to reimport tokens from old table

--- application/helpers/update/updates/Update_487.php
+++ application/helpers/update/updates/Update_487.php
@@ -12,11 +12,11 @@ class Update_487 extends DatabaseUpdateBase
         $columnSchema = $this->db->getSchema()->getTable('{{archived_table_settings}}')->getColumn('attributes');
         if ($columnSchema === null) {
             $this->db->createCommand()->addColumn('{{archived_table_settings}}', 'attributes', 'text NULL');
-        }
-        $archivedTableSettings = \Yii::app()->db->createCommand("SELECT * FROM {{archived_table_settings}}")->queryAll();
-        foreach ($archivedTableSettings as $archivedTableSetting) {
-            if ($archivedTableSetting['tbl_type'] === 'token') {
-                $this->db->createCommand()->update('{{archived_table_settings}}', ['attributes' => json_encode(['unknown'])], 'id = :id', ['id' => $archivedTableSetting['id']]);
+            $archivedTableSettings = \Yii::app()->db->createCommand("SELECT * FROM {{archived_table_settings}}")->queryAll();
+            foreach ($archivedTableSettings as $archivedTableSetting) {
+                if ($archivedTableSetting['tbl_type'] === 'token') {
+                    $this->db->createCommand()->update('{{archived_table_settings}}', ['attributes' => json_encode(['unknown'])], 'id = :id', ['id' => $archivedTableSetting['id']]);
+                }
             }
         }
     }
