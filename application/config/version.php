2cced37bb82839792c2e4ecfbcf5d7a0e51163de
olleharstedt <olleharstedt@yahoo.com>
2022-05-30 11:14:09 +0200

    Release 5.3.18+220530

--- application/config/version.php
+++ application/config/version.php
@@ -11,10 +11,10 @@
  * See COPYRIGHT.php for copyright notices and details.
  */
 
-$config['versionnumber'] = '5.3.17';
+$config['versionnumber'] = '5.3.18';
 $config['dbversionnumber'] = 487;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
 $config['templateapiversion']  = 3;
-$config['assetsversionnumber'] = '30278';
+$config['assetsversionnumber'] = '30279';
 return $config;
