4b9be7205988acf1dd7a31b7bf0fad9da8be9557
Denis Chenu <denis@sondages.pro>
2022-05-31 14:52:40 +0200

    Fixed issue #spurious error "Incorrect username …" in webserver Auth plugin Dev: Add optionnal param \LimeSurvey\PluginManager\PluginEvent when potentially needed

--- application/core/plugins/Authwebserver/Authwebserver.php
+++ application/core/plugins/Authwebserver/Authwebserver.php
@@ -3,7 +3,7 @@
 class Authwebserver extends LimeSurvey\PluginManager\AuthPluginBase
 {
     protected $storage = 'DbStorage';
-    
+
     protected static $description = 'Core: Webserver authentication';
     protected static $name = 'Webserver';
 
@@ -21,12 +21,12 @@ class Authwebserver extends LimeSurvey\PluginManager\AuthPluginBase
             'default' => 'REMOTE_USER',
         ),
         'is_default' => array(
-                'type' => 'checkbox',
-                'label' => 'Check to make default authentication method (This disable Default LimeSurvey authentification by database)',
-                'default' => true,
-                )
+            'type' => 'checkbox',
+            'label' => 'Check to make default authentication method (This disable Default LimeSurvey authentification by database)',
+            'default' => true,
+        )
     );
-    
+
     public function init()
     {
         /**
@@ -61,6 +61,7 @@ class Authwebserver extends LimeSurvey\PluginManager\AuthPluginBase
     {
         // normal login through webserver authentication
         $serverKey = $this->get('serverkey');
+
         if (!empty($serverKey) && isset($_SERVER[$serverKey])) {
             $sUser = $_SERVER[$serverKey];
             // Only strip domain part when desired
@@ -78,12 +79,16 @@ class Authwebserver extends LimeSurvey\PluginManager\AuthPluginBase
                 $sUser = $aUserMappings[$sUser];
             }
             $oUser = $this->api->getUserByName($sUser);
-            if ($oUser || $this->api->getConfigKey('auth_webserver_autocreate_user')) {
+            if (
+                ($oUser && Permission::model()->find('permission = :permission AND uid=:uid AND read_p =1', array(":permission" => 'auth_webserver', ":uid" => $oUser->uid)))
+                || $this->api->getConfigKey('auth_webserver_autocreate_user')
+            ) {
                 $this->setUsername($sUser);
                 $this->setAuthPlugin(); // This plugin handles authentication, halt further execution of auth plugins
                 return;
             }
         }
+
         if (!empty($serverKey) && $this->get('is_default', null, null, $this->settings['is_default']['default'])) {
             throw new CHttpException(401, 'Wrong credentials for LimeSurvey administration.');
         }
@@ -96,10 +101,10 @@ class Authwebserver extends LimeSurvey\PluginManager\AuthPluginBase
         if ($identity->plugin != 'Authwebserver') {
             return;
         }
-
+        /* @var $authEvent LimeSurvey\PluginManager\PluginEvent */
+        $authEvent = $this->getEvent();
         /* @var $identity LSUserIdentity */
         $sUser = $this->getUserName();
-
         $oUser = $this->api->getUserByName($sUser);
         if (is_null($oUser)) {
             if (function_exists("hook_get_auth_webserver_profile")) {
@@ -112,10 +117,10 @@ class Authwebserver extends LimeSurvey\PluginManager\AuthPluginBase
         } else {
             if (Permission::model()->find('permission = :permission AND uid=:uid AND read_p =1', array(":permission" => 'auth_webserver', ":uid" => $oUser->uid))) {
                 // Don't use Permission::model()->hasGlobalPermission : it's update the plugins event (and remove user/pass from event)
-                $this->setAuthSuccess($oUser);
+                $this->setAuthSuccess($oUser, $authEvent);
                 return;
             } else {
-                $this->setAuthFailure(self::ERROR_AUTH_METHOD_INVALID, gT('Web server authentication method is not allowed for this user'));
+                $this->setAuthFailure(self::ERROR_AUTH_METHOD_INVALID, gT('Web server authentication method is not allowed for this user'), $authEvent);
                 return;
             }
         }
@@ -135,10 +140,10 @@ class Authwebserver extends LimeSurvey\PluginManager\AuthPluginBase
                 Permission::model()->setGlobalPermission($oUser->uid, 'auth_webserver');
 
                 // read again user from newly created entry
-                $this->setAuthSuccess($oUser);
+                $this->setAuthSuccess($oUser, $authEvent);
                 return;
             } else {
-                $this->setAuthFailure(self::ERROR_USERNAME_INVALID);
+                $this->setAuthFailure(self::ERROR_USERNAME_INVALID, gT('Unable to create user'), $authEvent);
             }
         }
     }
@@ -154,7 +159,7 @@ class Authwebserver extends LimeSurvey\PluginManager\AuthPluginBase
         $settings = parent::getPluginSettings($getValues);
 
         if (!empty($settings['serverkey']) && !empty($settings['serverkey']['current'])) {
-            if(!isset($_SERVER[$settings['serverkey']['current']])) {
+            if (!isset($_SERVER[$settings['serverkey']['current']])) {
                 $settings['serverkey']['help'] = "<p class='alert alert-danger'>" . gT("The server key is not currently set. If you set this plugin as default you will not be able to log in again.") . "<p>";
             }
         }
