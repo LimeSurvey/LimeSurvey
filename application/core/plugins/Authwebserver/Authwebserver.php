ed7f2dac0645dde441f83e6c9f7ccd8ee139598c
Denis Chenu <denis@sondages.pro>
2022-06-02 09:36:17 +0200

    Fixed issue #18169: Potential redirect loop with Authwebserver Dev: check if user is allwed to connect in beforeLogin and newUserSession Dev: throw 401 if webserver is default, allow DB auth else

--- application/core/plugins/Authwebserver/Authwebserver.php
+++ application/core/plugins/Authwebserver/Authwebserver.php
@@ -81,7 +81,7 @@ class Authwebserver extends LimeSurvey\PluginManager\AuthPluginBase
             $oUser = $this->api->getUserByName($sUser);
             if (
                 ($oUser && Permission::model()->find('permission = :permission AND uid=:uid AND read_p =1', array(":permission" => 'auth_webserver', ":uid" => $oUser->uid)))
-                || $this->api->getConfigKey('auth_webserver_autocreate_user')
+                || (!$oUser && $this->api->getConfigKey('auth_webserver_autocreate_user'))
             ) {
                 $this->setUsername($sUser);
                 $this->setAuthPlugin(); // This plugin handles authentication, halt further execution of auth plugins
