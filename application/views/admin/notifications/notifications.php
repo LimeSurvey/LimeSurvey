5a2ccfdfa4eb109b26014b77a0694b23bfdac295
pstelling <patricia.stelling@limesurvey.org>
2022-06-03 10:40:53 +0200

    dev: alerst started

--- application/views/admin/notifications/notifications.php
+++ application/views/admin/notifications/notifications.php
@@ -1,26 +1,22 @@
 <div class="container-fluid">
     <div class="row">
-        <div id="notif-container" class="col-12 content-right" style="z-index: 999">
-            <?php foreach($aMessage as $message):?>
+        <div id="notif-container" class="col-12 content-right">
+            <?php foreach ($aMessage as $message): ?>
                 <?php
-                    if(isset($message['type']) && in_array($message['type'],array('error','success','danger','warning','info')))
-                    {
-                        $sType=$message['type'];
-                        if($sType=='error')
-                        {
-                            $sType='danger';
-                        }
+                if (isset($message['type']) && in_array($message['type'], array('error', 'success', 'danger', 'warning', 'info'))) {
+                    $sType = $message['type'];
+                    if ($sType == 'error') {
+                        $sType = 'danger';
                     }
-                    else
-                    {
-                        $sType='success';
-                    }
-                    echo CHtml::openTag("div",array('class'=>"alert alert-{$sType} alert-dismissible",'role'=>'alert'));
-                    echo CHtml::htmlButton("<span></span>",array('type'=>'button','class'=>'btn-close','data-bs-dismiss'=>'alert','aria-label'=>gT("Close")));
-                    echo $message['message'];
-                    echo CHtml::closeTag("div");
+                } else {
+                    $sType = 'success';
+                }
+                echo CHtml::openTag("div", array('class' => "alert alert-{$sType} alert-dismissible", 'role' => 'alert'));
+                echo CHtml::htmlButton("<span></span>", array('type' => 'button', 'class' => 'btn-close', 'data-bs-dismiss' => 'alert', 'aria-label' => gT("Close")));
+                echo $message['message'];
+                echo CHtml::closeTag("div");
                 ?>
-            <?php endforeach;?>
+            <?php endforeach; ?>
         </div>
     </div>
 </div>
