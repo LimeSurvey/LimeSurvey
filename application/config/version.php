484c1b2eb33f0aba16129cee83c46599dee2f9f3
c-schmitz <carsten.schmitz.hh@gamil.com>
2022-06-27 14:00:45 +0200

    Release 3.28.17+220627

--- application/config/version.php
+++ application/config/version.php
@@ -12,10 +12,10 @@
  */
 
 
-$config['versionnumber'] = '3.28.16';
+$config['versionnumber'] = '3.28.17';
 $config['dbversionnumber'] = 366;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
 $config['templateapiversion'] = 3;
-$config['assetsversionnumber'] = '30244';
+$config['assetsversionnumber'] = '30245';
 return $config;
