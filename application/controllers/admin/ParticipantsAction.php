c4ef7392274820b14b0ab5f85c722ceb9f6069b4
Tim Willig <92720438+twilligls@users.noreply.github.com>
2022-06-30 13:46:58 +0200

    Lsc 25 ccu21 wells (#2487)
    * removed qTip2 register line, as this package was removed before
    
    * added old bootstrap CSS for well to our CSS to be able to keep class
    
    * added missing row div to Data policy setting page
    
    * rebuilt admintheme

--- application/controllers/admin/ParticipantsAction.php
+++ application/controllers/admin/ParticipantsAction.php
@@ -863,7 +863,6 @@ class ParticipantsAction extends SurveyCommonAction
                 'filterbea' => $filterblankemails,
                 'participant_id_exists' => in_array('participant_id', $fieldlist)
             );
-            App()->getClientScript()->registerPackage('qTip2');
             App()->getClientScript()->registerPackage('jquery-nestedSortable');
             App()->getClientScript()->registerScriptFile(App()->getConfig('adminscripts') . 'attributeMapCSV.js');
 
