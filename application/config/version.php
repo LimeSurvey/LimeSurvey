d7d84aa2cd13bd43d742aba71e9985b467f7d977
Gabriel Jenik <gabriel.jenik@gmail.com>
2022-07-01 09:32:02 -0300

    Fixed issue #18116: Ability to disable animation layer regarding saving in header section (#2462)

--- application/config/version.php
+++ application/config/version.php
@@ -16,5 +16,5 @@ $config['dbversionnumber'] = 488;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
 $config['templateapiversion']  = 3;
-$config['assetsversionnumber'] = '30283';
+$config['assetsversionnumber'] = '30284';
 return $config;
