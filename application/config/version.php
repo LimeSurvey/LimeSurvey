8866d80b51f7c601deaed964034106331a7ad2e2
c_schmitz <c_schmitz>
2022-07-05 11:32:40 +0200

    Release 5.3.23+220705

--- application/config/version.php
+++ application/config/version.php
@@ -11,10 +11,10 @@
  * See COPYRIGHT.php for copyright notices and details.
  */
 
-$config['versionnumber'] = '5.3.22';
+$config['versionnumber'] = '5.3.23';
 $config['dbversionnumber'] = 488;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
 $config['templateapiversion']  = 3;
-$config['assetsversionnumber'] = '30284';
+$config['assetsversionnumber'] = '30285';
 return $config;
