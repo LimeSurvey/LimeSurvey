0ad1d61986ce5e067cca03fb681b4ab622e21451
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2022-07-14 18:34:15 +0200

    Dev: Ignore welcome modal

--- tests/functional/backend/CreateQuestionTest.php
+++ tests/functional/backend/CreateQuestionTest.php
@@ -64,6 +64,20 @@ class CreateQuestionTest extends TestBaseClassWeb
             $web->get($url);
             sleep(1);
 
+            // Ignore welcome modal.
+            try {
+                $button = self::$webDriver->wait(1)->until(
+                    WebDriverExpectedCondition::elementToBeClickable(
+                        WebDriverBy::cssSelector('#welcomeModal button.btn-outline-secondary')
+                    )
+                );
+                $button->click();
+            } catch (NoSuchElementException $ex) {
+                // Do nothing.
+            } catch (TimeOutException $ex) {
+                // Do nothing.
+            }
+
             // Go to structure sidebar
             $selectStructureSidebar = $web->findById('adminsidepanel__sidebar--selectorStructureButton');
             $selectStructureSidebar->click();
