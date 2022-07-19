d668853a69e65cf864b583a373d43a290ae0965e
tonis ormisson <tonis@andmemasin.eu>
2022-07-19 15:49:55 +0300

    upgrade test dependencies to match v5

--- third_party/composer/platform_check.php
+++ third_party/composer/platform_check.php
@@ -4,8 +4,8 @@
 
 $issues = array();
 
-if (!(PHP_VERSION_ID >= 50500)) {
-    $issues[] = 'Your Composer dependencies require a PHP version ">= 5.5.0". You are running ' . PHP_VERSION . '.';
+if (!(PHP_VERSION_ID >= 70100)) {
+    $issues[] = 'Your Composer dependencies require a PHP version ">= 7.1.0". You are running ' . PHP_VERSION . '.';
 }
 
 if ($issues) {
