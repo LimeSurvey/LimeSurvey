8b07930e94240228d4674ebe0d53dd5262ac5801
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2022-07-25 12:01:45 +0200

    Dev: Add missing return types

--- application/models/FailedEmail.php
+++ application/models/FailedEmail.php
@@ -114,14 +114,14 @@ class FailedEmail extends LSActiveRecord
      * Returns the static model of the specified AR class.
      * Please note that you should have this exact method in all your CActiveRecord descendants!
      * @param string $className active record class name.
-     * @return FailedEmail the static model class
+     * @return static the static model class
      */
     public static function model($className = __CLASS__): FailedEmail
     {
         return parent::model($className);
     }
 
-    public function getColumns()
+    public function getColumns(): array
     {
         return [
             [
@@ -175,7 +175,7 @@ class FailedEmail extends LSActiveRecord
         ];
     }
 
-    public function getButtons()
+    public function getButtons(): string
     {
         $permissions = [
             'update' => Permission::model()->hasSurveyPermission($this->surveyid, 'responses', 'update'),
