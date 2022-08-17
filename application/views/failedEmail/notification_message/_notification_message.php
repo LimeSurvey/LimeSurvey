30cca2fdad33b468828d9914d1f98e36c4d506a4
Patrick Teichmann <patrick.teichmann@limesurvey.org>
2022-08-17 16:40:38 +0200

    New feature #18282: Possibility to resend failed admin notification emails(#2539)
    * dev: created new table for failed email notification and created new …
    
    * dev: added column surveyid to new table failed_email
    
    * dev: changed columns table failed_email
    
    * LSC-267: added controller/view/model for failed email notifications
    
    * dev: LSC273 self cleaning after 30 days
    
    * Dev: true equal for string comparison
    
    * LSC-267: added grid and some controller/db adjustments for failedEmail
    
    * Save failed emails to database also fixing database table columns
    
    * LSC-267: added/fixed some fields and added basic resend action and modal
    
    * subject col now called email_type in create-database.php
    
    * Notifications for failed e-mails
    
    * Success state as constant
    
    * LSC-267: added controller actions/massive action modals/buttons for f…
    
    * Only one notification, and only for survey owners
    
    * Changed notification message text
    
    * If a survey is deleted, the failed email notifications belonging to t…
    
    * LSC-267: added buttons and javascript generating modals
    
    * LSC-267: added return messages for modals / added ajax reload for jav…
    
    * Dev: PSR-12 fixes
    
    * Dev: Add missing return types
    
    * Dev: Add missing return types
    
    * Dev: Fix link casing
    
    * LSC-267: capitalized controller name in url
    
    * LSC-267: added check for LimeMailer instance
    
    * LSC-267: added docs to saveFailedEmail and removed redundant return
    
    * LSC-267: updated doc blocks and fixed some conditions / added excepti…
    
    * PSR-12 fix
    
    * LSC-267: updated view test for FailedEmail
    
    * LSC-267: updated view test for FailedEmail
    
    * LSC-267: updated permissions for button group and dynamic modalcontent
    
    * LSC-267: updated view test for FailedEmail
    
    * LSC-267: psr-12
    
    * Merge branch 'master' into task/LSC-267_failed_notification_emails
    
    * LSC-267: fixed for url type for get/path
    
    * LSC-267: fixed some typos
    
    * LSC-267: fixed missing surveyid to display correct data in the table
    
    * LSC-267: functional tests for FailedEmail
    
    * LSC-267: fixed wrong return values causing errors in ajax calls
    
    * LSC-267: WIP testing error on CI
    
    * LSC-267: WIP testing error on CI
    
    * LSC-267: WIP testing error on CI
    
    * LSC-267: WIP testing error on CI
    
    * LSC-267: fixed missing function param for saveFailedEmail() / added c…
    
    * Fixed issue: thissurvey is null; addCondition second argument is not …
    
    * Turn on debug 2 in CI
    
    * Fix yaml
    
    * Fix line in sed call
    
    * Fix sed
    
    * LSC-267: added breadcrumb
    
    * LSC-267: fixed id/language/recipient not being set right / header all…
    
    * LSC-267: added additional checks to wait for modal being closed
    
    * LSC-267: adjust waiting time for tests
    
    * LSC-267: adjust waiting time for tests
    
    * Remove debug 2
    
    * Run entire test suite
    
    * LSC-267: added comment for clarification
    
    * LSC-267: removed todo
    
    * Merge branch 'master' into task/LSC-267_failed_notification_emails
    
    * Apply composer no dev
    
    * LSC-267: reverted wrong yaml setting to master default

---
+++ application/views/failedEmail/notification_message/_notification_message.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * View for the message of the meesage in the notification center
+ * @var array $failedEmailSurveyTitles
+ *
+ *
+ */
+
+?>
+<p>
+    <?php
+    eT('Dear survey administrator') ?>,<br>
+    <?php
+    eT("There were one or more confirmation emails that failed to be sent. Please check the 'Failed email notifications' section in the survey(s) listed below") ?>:
+</p>
+<ul>
+    <?php
+    foreach ($failedEmailSurveyTitles as $surveyId => $surveyTitle) : ?>
+        <li>
+            <?= CHtml::link(
+                $surveyTitle,
+                Yii::app()->createUrl("failedEmail/index/", ['surveyid' => $surveyId])
+            ) ?>
+        </li>
+    <?php
+    endforeach; ?>
+</ul>
+
---
+++ application/views/failedEmail/partials/modal/resend_body.php
@@ -0,0 +1,6 @@
+<?php
+?>
+<label class="control-label" for="preserveResend"><?= gT('Preserve Successfull e-mails?') ?></label>
+<div>
+    <input class="custom-data" type="checkbox" name="preserveResend" id="preserveResend"/>
+</div>
