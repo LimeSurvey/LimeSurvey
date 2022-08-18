0f186523ea0d491d64bd473609d0b3e2502112c5
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2022-08-18 09:55:59 +0200

    Dev: Set version to 5.5.0-dev

--- application/config/version.php
+++ application/config/version.php
@@ -11,7 +11,7 @@
  * See COPYRIGHT.php for copyright notices and details.
  */
 
-$config['versionnumber'] = '5.4.0-dev';
+$config['versionnumber'] = '5.5.0-dev';
 $config['dbversionnumber'] = 489;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
