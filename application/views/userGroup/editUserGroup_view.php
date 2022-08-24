95b15864dd7071520ded42441745c0b0cace8937
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2022-08-24 15:24:30 +0200

    Fixed issue: Apply BS5 to user group management forms

--- application/views/userGroup/editUserGroup_view.php
+++ application/views/userGroup/editUserGroup_view.php
@@ -4,17 +4,16 @@
 ?>
 
 <div class="col-12 list-surveys">
-
     <div class="row">
         <?php echo CHtml::form(array("userGroup/edit/ugid/{$ugid}"), 'post', array('class' => 'col-lg-6 offset-lg-3', 'id' => 'usergroupform', 'name' => 'usergroupform')); ?>
             
             <div class="mb-3">
-                <label for='name'><?php eT("Name:"); ?></label>
+                <label class="form-label" for='name'><?php eT("Name:"); ?></label>
                 <input type='text' size='50' maxlength='20' id='name' name='name' value="<?php echo htmlspecialchars($model['name'], ENT_QUOTES, 'UTF-8'); ?>" class="form-control" />
             </div>
 
             <div class="mb-3">
-                <label for='description'><?php eT("Description:"); ?></label>
+                <label class="form-label " for='description'><?php eT("Description:"); ?></label>
                 <textarea cols='50' rows='4' id='description' name='description' class="form-control"><?php echo htmlspecialchars($model['description'], ENT_QUOTES, 'UTF-8'); ?></textarea>
             </div>
 
