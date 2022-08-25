6a353ac5531daec6ac7bd7fe12a41b8342ce0387
Patrick Teichmann <patrick.teichmann@limesurvey.org>
2022-08-25 18:09:01 +0200

    Dev: readded checked property accidentally removed during merge

--- application/views/failedEmail/partials/modal/resend_body.php
+++ application/views/failedEmail/partials/modal/resend_body.php
@@ -2,5 +2,5 @@
 ?>
 <label class="control-label" for="preserveResend"><?= gT('Preserve successful emails?') ?></label>
 <div>
-    <input class="custom-data" type="checkbox" name="preserveResend" id="preserveResend"/>
+    <input class="custom-data" type="checkbox" name="preserveResend" id="preserveResend" checked/>
 </div>
