bf127c2cf1f4bd74e4ae59a0d214fede754fe8ed
Gabriel Jenik <gabriel.jenik@gmail.com>
2022-08-29 06:25:53 -0300

    Fixed issue #18242: Quick translation: end note not shown (#2556)
    * Fixed issue #18242: Quick translation: end note not shown
    
    * Fixed issue #18242: Quick translation: end note not shown
    
    * Fixed issue #18242: Quick translation: end note not shown
    
    Co-authored-by: encuestabizdevgit <devgit@encuesta.biz>

--- application/controllers/admin/Translate.php
+++ application/controllers/admin/Translate.php
@@ -244,9 +244,12 @@ class Translate extends SurveyCommonAction
                 $gid = ($amTypeOptions["gid"] == true) ? $gid = $aRowfrom['gid'] : null;
                 $qid = ($amTypeOptions["qid"] == true) ? $qid = $aRowfrom['qid'] : null;
 
-                $textform_length = strlen(trim($textfrom));
+                $textfrom_length = strlen(trim($textfrom));
+                $textfrom2_length = $associated ? strlen(trim($textfrom2)) : 0;
 
-                $all_fields_empty = !($textform_length > 0);
+                if ($textfrom_length > 0 || $textfrom2_length > 0) {
+                    $all_fields_empty = false;
+                }
 
                 $aData = array_merge($aData, array(
                                 'textfrom' => $this->cleanup($textfrom, array()),
