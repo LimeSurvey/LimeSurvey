cdcd5671b99f6376395034325cafbebe2f80bf23
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2022-08-29 12:57:29 +0200

    Dev: Fix relative path in test bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -205,17 +205,18 @@ if (!file_exists(APPPATH . 'config/config' . EXT)) {
 // NB: This line might be needed if you have PHP in a docker container.
 //$config['components']['db']['connectionString'] = 'mysql:host=localhost;port=3306;dbname=ls4;';
 
+var_dump(BASEPATH);
 // Check that tmp and upload are set to 777 permission (OK on test system).
-if (substr(sprintf('%o', fileperms(BASEPATH . '../tmp/')), -4) != '0777') {
+if (substr(sprintf('%o', fileperms(BASEPATH . '../../../../tmp/')), -4) != '0777') {
     die('tmp folder not set to 777');
 }
-if (substr(sprintf('%o', fileperms(BASEPATH . '../tmp/runtime/')), -4) != '0777') {
+if (substr(sprintf('%o', fileperms(BASEPATH . '../../../../tmp/runtime/')), -4) != '0777') {
     die('tmp/runtime folder not set to 777');
 }
-if (substr(sprintf('%o', fileperms(BASEPATH . '../upload/')), -4) != '0777') {
+if (substr(sprintf('%o', fileperms(BASEPATH . '../../../../upload/')), -4) != '0777') {
     die('upload folder not set to 777');
 }
-if (substr(sprintf('%o', fileperms(BASEPATH . '../tests/tmp/')), -4) != '0777') {
+if (substr(sprintf('%o', fileperms(BASEPATH . '../../../../tests/tmp/')), -4) != '0777') {
     die('tests/tmp folder not set to 777');
 }
 
