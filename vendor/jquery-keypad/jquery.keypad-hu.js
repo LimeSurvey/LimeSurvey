896027de4a4c311c1566b7b035fccdb9cfd1b815
Carsten Schmitz <carsten.schmitz@limesurvey.org>
2022-09-06 15:58:32 +0200

    Dev Added back missing files for jQuery Keypad

--- vendor/jquery-keypad/jquery.keypad-hu.js
+++ vendor/jquery-keypad/jquery.keypad-hu.js
@@ -1,28 +1,33 @@
 /* http://keith-wood.name/keypad.html
-   German localisation for the jQuery keypad extension
+   Hungarian localisation for the jQuery keypad extension
    Written by Uwe Jakobs(u.jakobs{at}imageco.de) September 2009. */
 (function($) { // hide the namespace
 	'use strict';
-	$.keypad.qwertzAlphabetic = ['qwertzuiopüß', 'asdfghjklöä', 'yxcvbnm'];
+
+	function isAlphabetic(ch) {
+		return ($.keypad.isAlphabetic(ch) || 'áéíőúű'.indexOf(ch) > -1);
+	}
+
+	$.keypad.qwertzAlphabetic = ['qwertzuiopőú', 'asdfghjkléáű', 'íyxcvbnm'];
 	$.keypad.qwertzLayout = 
 		['!"§$%&/()=?`' + $.keypad.BACK + $.keypad.HALF_SPACE + '$£€/',
 		'<>°^@{[]}\\~´;:' + $.keypad.HALF_SPACE + '789*',
 		$.keypad.qwertzAlphabetic[0] + '+*' + $.keypad.HALF_SPACE + '456-',
 		$.keypad.HALF_SPACE + $.keypad.qwertzAlphabetic[1] + '#\'' + $.keypad.SPACE + '123+',
-		'|' + $.keypad.qwertzAlphabetic[2] + 'µ,.-_' + $.keypad.SPACE + $.keypad.HALF_SPACE +'.0,=',
+		'|' + $.keypad.qwertzAlphabetic[2] + 'µ,.-_' + $.keypad.SPACE + $.keypad.HALF_SPACE +'.0,=', 
 		$.keypad.SHIFT + $.keypad.SPACE + $.keypad.SPACE_BAR + $.keypad.SPACE + $.keypad.SPACE + $.keypad.SPACE +
 		$.keypad.CLEAR + $.keypad.SPACE + $.keypad.SPACE + $.keypad.HALF_SPACE + $.keypad.CLOSE];
-	$.keypad.regionalOptions.de = {
+	$.keypad.regionalOptions.hu = {
 		buttonText: '...',
-		buttonStatus: 'Öffnen',
-		closeText: 'schließen',
-		closeStatus: 'schließen',
-		clearText: 'löschen',
-		clearStatus: 'Gesamten Inhalt löschen',
-		backText: 'zurück',
-		backStatus: 'Letzte Eingabe löschen',
-		shiftText: 'umschalten',
-		shiftStatus: 'Zwischen Groß- und Kleinschreibung wechseln',
+		buttonStatus: 'Megnyit',
+		closeText: 'Bezár',
+		closeStatus: 'Bezár',
+		clearText: 'Töröl',
+		clearStatus: 'Teljes szöveg törlés',
+		backText: 'Vissza',
+		backStatus: 'Utolsó karakter törlése',
+		shiftText: 'Nagybetű',
+		shiftStatus: 'Kis- és nagybetűs üzemmód váltása',
 		spacebarText: '&nbsp;',
 		spacebarStatus: '',
 		enterText: 'Enter',
@@ -31,11 +36,11 @@
 		tabStatus: '',
 		alphabeticLayout: $.keypad.qwertzAlphabetic,
 		fullLayout: $.keypad.qwertzLayout,
-		isAlphabetic: $.keypad.isAlphabetic,
+		isAlphabetic: isAlphabetic,
 		isNumeric: $.keypad.isNumeric,
 		toUpper: $.keypad.toUpper,
 		isRTL: false
 	};
-	$.keypad.setDefaults($.keypad.regionalOptions.de);
+	$.keypad.setDefaults($.keypad.regionalOptions.hu);
 
 })(jQuery);
