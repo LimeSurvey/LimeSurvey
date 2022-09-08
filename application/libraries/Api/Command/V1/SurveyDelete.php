68549b5f03d750126bcda60ff8a01f14e1fed417
Kevin Foster <kevin.foster.uk@gmail.com>
2022-09-08 07:09:03 +0100

    lsc-434: Add API command SurveyDelete

---
+++ application/libraries/Api/Command/V1/SurveyDelete.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace LimeSurvey\Api\Command\V1;
+
+use LimeSurvey\Api\ApiSession;
+use LimeSurvey\Api\Command\CommandInterface;
+use LimeSurvey\Api\Command\CommandRequest;
+use LimeSurvey\Api\Command\CommandResponse;
+
+class SurveyDelete implements CommandInterface
+{
+    /**
+     * Run survey delete command.
+     *
+     * @access public
+     * @param LimeSurvey\Api\Command\CommandRequest $request
+     * @return LimeSurvey\Api\Command\CommandResponse
+     */
+    public function run(CommandRequest $request)
+    {
+        $sSessionKey = (string) $request->getData('sessionKey');
+        $iSurveyID = (int) $request->getData('surveyID');
+
+        $apiSession = new ApiSession;
+        if ($apiSession->checkKey($sSessionKey)) {
+            if (\Permission::model()->hasSurveyPermission($iSurveyID, 'survey', 'delete')) {
+                \Survey::model()->deleteSurvey($iSurveyID, true);
+                return new CommandResponse(array('status' => 'OK'));
+            } else {
+                return new CommandResponse(array('status' => 'No permission'));
+            }
+        } else {
+            return new CommandResponse(array('status' => ApiSession::INVALID_SESSION_KEY));
+        }
+    }
+}
