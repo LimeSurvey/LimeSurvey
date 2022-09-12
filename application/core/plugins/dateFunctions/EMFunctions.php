be92db8b2dc03cf24a51f0fbc68d6483951a29f4
encuestabizdevgit <devgit@encuesta.biz>
2022-09-12 13:25:41 -0300

    Fixed issue #18337: Placeholders VALIDFROM and VALIDUNTIL at email templates do not use localized dates
    - Added a 'dateFunctions' core plugin with new EM function: localize_date(date, language)

---
+++ application/core/plugins/dateFunctions/EMFunctions.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * This file is part of dateFunctions plugin
+ * @version 1.0.0
+ */
+
+namespace dateFunctions;
+
+use LimeExpressionManager;
+use Survey;
+
+class EMFunctions
+{
+    /**
+     * Formats a date according to the Survey's date format for the specified language
+     * @param string $date : a date in "Y-m-d H:i:s" format. Example: VALIDFROM.
+     * @param string $language : the language used for localization. If the language is not configured in the survey, the base language will be used. Example: TOKEN:LANGUAGE
+     * @return string
+     */
+    public static function localize_date($date, $language)
+    {
+        if (empty($date)) {
+            return '';
+        }
+
+        $surveyId = LimeExpressionManager::getLEMsurveyId();
+        $survey = Survey::model()->findByPk($surveyId);
+
+        if (empty($survey)) {
+            return '';
+        }
+
+        // If the specified language is note one of the survey's languages, fallback to the survey's base language.
+        if (!in_array($language, $survey->getAllLanguages()) || empty($survey->languagesettings[$language])) {
+            $language = $survey->language;
+        }
+
+        $dateFormat = $survey->languagesettings[$language]->surveyls_dateformat;
+        $dateFormatDetails = getDateFormatData($dateFormat);
+        $datetimeobj = new \Date_Time_Converter($date, "Y-m-d H:i:s");
+        return $datetimeobj->convert($dateFormatDetails['phpdate']);
+    }
+}
