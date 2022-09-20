e5c96b83e6e0ce78c9270d07e36a54b9b07b7426
encuestabizdevgit <devgit@encuesta.biz>
2022-09-20 11:30:07 -0300

    Fixed issue #18356: User with only user update allowed can set/remove any role to any user
    - Add missing file

---
+++ application/models/services/UserManager.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace LimeSurvey\Models\Services;
+
+use LSWebUser;
+use Permission;
+use User;
+
+/**
+ * Service class for managing users and their permissions
+ */
+class UserManager
+{
+    /** @var LSWebUser the user managing other users */
+    private $managingUser;
+
+    /** @var User the user being handled */
+    private $targetUser;
+
+    /**
+     * @param LSWebUser $managingUser
+     * @param User|null $targetUser
+     */
+    public function __construct(
+        LSWebUser $managingUser,
+        User $targetUser = null
+    ) {
+        $this->managingUser = $managingUser;
+        $this->targetUser = $targetUser;
+    }
+
+    /**
+     * Returns true if the managing user can assign permissions to the target user.
+     * @return boolean
+     */
+    public function canAssignPermissions()
+    {
+        if (empty($this->managingUser) || empty($this->targetUser)) {
+            return false;
+        }
+
+        if (
+            Permission::model()->hasGlobalPermission('superadmin', 'read', $this->managingUser->id)
+            || (
+                Permission::model()->hasGlobalPermission('users', 'update', $this->managingUser->id)
+                && $this->targetUser->parent_id == $this->managingUser->id
+            )
+        ) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Returns true if the managing user can assign roles to the target user.
+     * @return boolean
+     */
+    public function canAssignRole()
+    {
+        return Permission::model()->hasGlobalPermission('superadmin', 'read', $this->managingUser->id);
+    }
+}
