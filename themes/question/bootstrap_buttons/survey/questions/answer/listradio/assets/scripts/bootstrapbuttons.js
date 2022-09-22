8e48d45e4769a1cb9c6968a831042ce87fb6ce2f
Tim Willig <tim.willig@limesurvey.org>
2022-09-22 16:34:30 +0200

    fixed other button

--- themes/question/bootstrap_buttons/survey/questions/answer/listradio/assets/scripts/bootstrapbuttons.js
+++ themes/question/bootstrap_buttons/survey/questions/answer/listradio/assets/scripts/bootstrapbuttons.js
@@ -1,20 +1,19 @@
 function doBootstrapRadioOther() {
-    var name = $(this).attr('name');
-
     $("input:radio.button-item.btn-check").on('change', function () {
+        var name = $(this).attr('name');
         // conditionaly show or hide "other" input field
         if ($(this).val() === '-oth-') {
             var value = $("#answer" + name + "othertextaux").val();
             $("#div" + name + "other").removeClass('ls-js-hidden');
             $("#answer" + name + "othertext").val(value).trigger("change");
-            copyOtherInputToHiddenField();
+            copyOtherInputToHiddenField(name);
         } else {
             $("#div" + name + "other").addClass('ls-js-hidden');
             $("#answer" + name + "othertextaux").val('');
         }
     });
 
-    function copyOtherInputToHiddenField() {
+    function copyOtherInputToHiddenField(name) {
         $("#answer" + name + "othertext").on('change keyup paste', function () {
             if ($(this).val()) {
                 $("#answer" + name + "othertextaux").val($(this).val());
