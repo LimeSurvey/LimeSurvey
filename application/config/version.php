dd393453016b04d5a5797f4a7b9d4d8ec037b194
encuestabizdevgit <devgit@encuesta.biz>
2022-09-26 10:46:35 -0300

    Fixed issue #18373: Saving statistics exports to excel fails
    - Needed packages were not loaded by pjax

--- application/config/version.php
+++ application/config/version.php
@@ -16,5 +16,5 @@ $config['dbversionnumber'] = 491;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
 $config['templateapiversion']  = 3;
-$config['assetsversionnumber'] = '30298';
+$config['assetsversionnumber'] = '30299';
 return $config;
