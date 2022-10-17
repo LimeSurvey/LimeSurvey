a4fc2a47871db05d1357d2a5f672f03b50199566
Tim Willig <tim.willig@limesurvey.org>
2022-10-17 09:32:02 +0200

    massiveActionsWidget now uses ButtonWidget

--- application/extensions/ButtonWidget/views/button.php
+++ application/extensions/ButtonWidget/views/button.php
@@ -6,21 +6,21 @@
 /** @var string $icon */
 /** @var string $iconPosition */
 /** @var bool $menu */
-/** @var bool $menuIcon */
+/** @var bool $displayMenuIcon */
+/** @var string $menuIcon */
 /** @var string $menuContent */
 /** @var string $link */
 /** @var array $htmlOptions */
 
 $iconLeft = $icon && $iconPosition != 'right' ? '<i class="' .  $icon . '" ></i> ' : '';
 $iconRight = $icon && $iconPosition == 'right' ? ' <i class="' .  $icon . '" ></i>' : '';
-//@TODO switch to new icon when done
-$menuIcon = $menuIcon ? '<span class="menu-button-divider"></span><i class="fa fa-ellipsis-h" ></i>' : '';
+$menuIconHtml = $displayMenuIcon ? '<span class="menu-button-divider"></span><i class="' . $menuIcon . '" ></i>' : '';
 ?>
 <?php if ($link == '' || $menu) : ?>
     <?php if ($menuContent != '') : ?>
         <div class="dropdown">
     <?php endif; ?>
-        <?= CHtml::htmlButton($iconLeft . $text . $iconRight . $menuIcon, $htmlOptions) ?>
+        <?= CHtml::htmlButton($iconLeft . $text . $iconRight . $menuIconHtml, $htmlOptions) ?>
     <?php if ($menuContent != '') : ?>
             <?= $menuContent ?>
         </div>
