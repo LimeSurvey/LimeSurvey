e695ae3488f1b0fbbf2fb5c81bad251c7ba0515d
Kevin Foster <kevin.foster.uk@gmail.com>
2022-10-17 11:34:58 +0100

    lsc-434: Add REST API tests

--- tests/unit/api/command/v1/SiteSettingsGetTest.php
+++ tests/unit/api/command/v1/SiteSettingsGetTest.php
@@ -2,10 +2,15 @@
 
 namespace ls\tests\unit\api\command\v1;
 
+use Eloquent\Phony\Phpunit\Phony;
+use Permission;
 use ls\tests\TestBaseClass;
 use ls\tests\unit\api\command\mixin\AssertResponse;
-use LimeSurvey\Api\Command\V1\QuestionList;
+use LimeSurvey\Api\Command\V1\SiteSettingsGet;
 use LimeSurvey\Api\Command\Request\Request;
+use LimeSurvey\Api\Command\Response\Status\StatusErrorUnauthorised;
+use LimeSurvey\Api\ApiSession;
+use LimeSurvey\Api\Command\Response\Status\StatusErrorBadRequest;
 
 /**
  * @testdox API command v1 SiteSettingsGet.
@@ -17,14 +22,90 @@ class SiteSettingsGetTest extends TestBaseClass
     /**
      * @testdox Returns invalid session response (error unauthorised) if session key is not valid.
      */
-    public function testSiteSettingsGetTestInvalidSession()
+    public function testSiteSettingsGetInvalidSession()
     {
         $request = new Request(array(
             'sessionKey' => 'not-a-valid-session-id',
             'settingName' => 'settingName'
         ));
-        $response = (new QuestionList())->run($request);
+        $response = (new SiteSettingsGet())->run($request);
 
         $this->assertResponseInvalidSession($response);
+    }
+
+    /**
+     * @testdox Returns error unauthorised if user does not have permission.
+     */
+    public function testSiteSettingsGetNoPermission()
+    {
+        $request = new Request(array(
+            'sessionKey' => 'mock',
+            'settingName' => 'settingName'
+        ));
+
+        $mockApiSessionHandle = Phony::mock(ApiSession::class);
+        $mockApiSessionHandle
+            ->checkKey
+            ->returns(true);
+        $mockApiSession = $mockApiSessionHandle->get();
+
+        $mockModelPermissionHandle = Phony::mock(Permission::class);
+        $mockModelPermissionHandle->hasGlobalPermission
+            ->returns(false);
+        $mockModelPermission = $mockModelPermissionHandle->get();
+
+        $command = new SiteSettingsGet();
+        $command->setApiSession($mockApiSession);
+        $command->setPermissionModel($mockModelPermission);
+
+        $response = $command->run($request);
+
+        $this->assertResponseStatus(
+            $response,
+            new StatusErrorUnauthorised()
+        );
+
+        $this->assertResponseDataStatus(
+            $response,
+            'No permission'
+        );
+    }
+
+    /**
+     * @testdox Returns error bad-request setting name invalid.
+     */
+    public function testSiteSettingsGetInvalidSetting()
+    {
+        $request = new Request(array(
+            'sessionKey' => 'mock',
+            'settingName' => 'invalid-setting-name'
+        ));
+
+        $mockApiSessionHandle = Phony::mock(ApiSession::class);
+        $mockApiSessionHandle
+            ->checkKey
+            ->returns(true);
+        $mockApiSession = $mockApiSessionHandle->get();
+
+        $mockModelPermissionHandle = Phony::mock(Permission::class);
+        $mockModelPermissionHandle->hasGlobalPermission
+            ->returns(true);
+        $mockModelPermission = $mockModelPermissionHandle->get();
+
+        $command = new SiteSettingsGet();
+        $command->setApiSession($mockApiSession);
+        $command->setPermissionModel($mockModelPermission);
+
+        $response = $command->run($request);
+
+        $this->assertResponseStatus(
+            $response,
+            new StatusErrorBadRequest()
+        );
+
+        $this->assertResponseDataStatus(
+            $response,
+            'Invalid setting'
+        );
     }
 }
