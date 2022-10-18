43eecb6eb6113fbc107ff07906638d77e3bb7b06
encuestabizdevgit <devgit@encuesta.biz>
2022-10-18 12:03:16 -0300

    Fixed issue #18338: For participants, expiration date can be set before start date

--- application/config/version.php
+++ application/config/version.php
@@ -16,5 +16,5 @@ $config['dbversionnumber'] = 491;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
 $config['templateapiversion']  = 3;
-$config['assetsversionnumber'] = '30300';
+$config['assetsversionnumber'] = '30301';
 return $config;
