b2525a4cc57153a430fa22d1413cb22026edec52
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2022-10-27 14:35:02 +0200

    Add another test

--- tests/unit/services/SendSubmitNotificationsCommandTest.php
+++ tests/unit/services/SendSubmitNotificationsCommandTest.php
@@ -6,10 +6,18 @@ use PHPUnit\Framework\TestCase;
 use LimeSurvey\Models\Services\SendSubmitNotificationsCommand;
 use LimeSurvey\Models\Services\SessionInterface;
 use LimeMailer;
+use Yii;
 
 class SendSubmitNotificationsCommandTest extends TestCase
 {
-    public function testGetEmailResponseTo()
+    public static function setupBeforeClass(): void
+    {
+        parent::setupBeforeClass();
+        Yii::import('application.helpers.common_helper', true);
+        Yii::import('application.helpers.expressions.em_manager_helper', true);
+    }
+
+    public function testGetEmailResponseToEmpty()
     {
         $mailer = $this
             ->getMockBuilder(LimeMailer::class)
@@ -19,6 +27,38 @@ class SendSubmitNotificationsCommandTest extends TestCase
             'htmlemail' => false
         ];
         $ssnc = new SendSubmitNotificationsCommand($surveyinfo, $mailer, $session);
+        $result = $ssnc->getEmailResponseTo([]);
+        $this->assertEquals([], $result);
+    }
+
+    public function testGetEmailResponseToOneEmail()
+    {
+        $mailer = new LimeMailer();
+        $session = $this->getMockSession();
+        $surveyinfo = [
+            'htmlemail'       => false,
+            'emailresponseto' => 'moo@moo.moo',
+            'adminemail'      => '',
+        ];
+        $ssnc = new SendSubmitNotificationsCommand($surveyinfo, $mailer, $session);
+        $emails = [];
+        $result = $ssnc->getEmailResponseTo($emails);
+        $this->assertEquals(['moo@moo.moo'], $result);
+    }
+
+    public function testGetEmailResponseToTwoEmail()
+    {
+        $mailer = new LimeMailer();
+        $session = $this->getMockSession();
+        $surveyinfo = [
+            'htmlemail'       => false,
+            'emailresponseto' => 'moo@moo.moo;foo@foo.foo',
+            'adminemail'      => '',
+        ];
+        $ssnc = new SendSubmitNotificationsCommand($surveyinfo, $mailer, $session);
+        $emails = [];
+        $result = $ssnc->getEmailResponseTo($emails);
+        $this->assertEquals(['moo@moo.moo', 'foo@foo.foo'], $result);
     }
 
     private function getMockSession()
