2c414aee09adf3bbf1842dc259a6b0ebb0603106
Tim Willig <tim.willig@limesurvey.org>
2022-11-08 13:43:45 +0100

    fixed issue: massive actions dropdown was missing, pagesize and pagination were not working

--- application/views/failedEmail/failedEmail_index.php
+++ application/views/failedEmail/failedEmail_index.php
@@ -17,13 +17,12 @@
         <div class="row">
             <div class="content-right">
                 <?php
-
-                $this->widget('ext.LimeGridView.LimeGridView', [
+                $this->widget('application.extensions.admin.grid.CLSGridView', [
                     'dataProvider'    => $failedEmailModel->search(),
                     'filter'          => $failedEmailModel,
                     'id'              => 'failedemail-grid',
                     'emptyText'       => gT('No failed email notifications found'),
-                    'template'        => "<div class='push-grid-pager'>{items}\n</div><div id='emailFailedEmailPager'><div class=\"col-sm-4\" id=\"massive-action-container\">$massiveAction</div><div class=\"col-sm-4 pager-container ls-ba \">{pager}</div><div class=\"col-sm-4 summary-container\">{summary}</div></div>",
+                    'massiveActionTemplate' => $massiveAction,
                     'summaryText'     => gT('Displaying {start}-{end} of {count} result(s).') . ' ' . sprintf(gT('%s rows per page'),
                             CHtml::dropDownList(
                                 'pageSize',
