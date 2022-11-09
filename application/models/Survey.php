8c24159354e0f52d9e7e34e66f2e9357b210f175
Gabriel Jenik <gabriel.jenik@gmail.com>
2022-11-09 10:17:15 -0300

    Fixed issue #15195: Expiration date can be set before start date

--- application/models/Survey.php
+++ application/models/Survey.php
@@ -398,17 +398,24 @@ class Survey extends LSActiveRecord implements PermissionInterface
         $dateTime = dateShift($dateTime, "Y-m-d H:i:s", '-1 minute');
 
         $model = $this;
+
+        // Set model based on surveyId, if given
+        // If so, set scenario as to be saved later
         if (isset($surveyId)) {
             $model = self::model()->findByPk($surveyId);
             $model->setScenario('update');
         }
 
         // Avoid setting expiration date before start date
+        // If there is a future start date set, set the expiration date to the same date
         if (!empty($model->startdate) && $dateTime < $model->startdate) {
             $dateTime = $model->startdate;
         }
 
+        // Set expiration date
         $model->expires = $dateTime;
+
+        // Save if scenario is update
         if ($model->scenario == 'update') {
             return $model->save();
         }
