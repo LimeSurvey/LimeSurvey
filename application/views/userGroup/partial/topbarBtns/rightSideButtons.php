57227bd9c3b33475909c7fe377a045873c957cf4
pstelling <patricia.stelling@limesurvey.org>
2022-11-14 11:41:09 +0100

    dev: topbar usergroup and all pages for it

---
+++ application/views/userGroup/partial/topbarBtns/rightSideButtons.php
@@ -0,0 +1,35 @@
+<?php
+
+/** @var bool $addGroupSave */
+
+$this->widget(
+    'ext.ButtonWidget.ButtonWidget',
+    [
+        'name' => 'boxes-close-button',
+        'id' => 'boxes-close-button',
+        'text' => gT('Back'),
+        'icon' => 'fa fa-backward',
+        'link' => $backUrl ?? Yii::app()->createUrl('admin/index'),
+        'htmlOptions' => [
+            'class' => 'btn btn-outline-secondary',
+        ],
+    ]
+);
+
+if ($addGroupSave) {
+    $this->widget(
+        'ext.ButtonWidget.ButtonWidget',
+        [
+            'name' => 'save-form-button',
+            'id' => 'save-form-button',
+            'text' => gT('Save'),
+            'icon' => 'fa fa-check',
+            'htmlOptions' => [
+                'class' => 'btn btn-success',
+                'data-form-id' => 'usergroupform',
+                'type' => 'submit',
+                'role' => 'button'
+            ],
+        ]
+    );
+}
---
+++ application/views/userGroup/partial/topbarBtns_mail/rightSideButtons.php
@@ -0,0 +1,47 @@
+<?php
+
+
+$this->widget(
+    'ext.ButtonWidget.ButtonWidget',
+    [
+        'name' => 'boxes-close-button',
+        'id' => 'boxes-close-button',
+        'text' => gT('Back'),
+        'icon' => 'fa fa-backward',
+        'link' => $backUrl ?? Yii::app()->createUrl('userGroup/index'),
+        'htmlOptions' => [
+            'class' => 'btn btn-outline-secondary',
+        ],
+    ]
+);
+
+$this->widget(
+    'ext.ButtonWidget.ButtonWidget',
+    [
+        'name' => 'reset-form-button',
+        'id' => 'reset-form-button',
+        'text' => gT('Reset'),
+        'icon' => 'fa fa-refresh',
+        'htmlOptions' => [
+            'class' => 'btn btn-warning',
+            'role' => 'button',
+            'type' => 'reset',
+            'form' => 'mailusergroup'
+        ],
+    ]
+);
+
+$this->widget(
+    'ext.ButtonWidget.ButtonWidget',
+    [
+        'name' => 'reset-form-button',
+        'id' => 'save-form-button',
+        'text' => gT('Send'),
+        'icon' => 'fa fa-envelope',
+        'htmlOptions' => [
+            'class' => 'btn btn-success',
+            'type' => 'submit',
+            'data-form-id' => 'mailusergroup'
+        ],
+    ]
+);
