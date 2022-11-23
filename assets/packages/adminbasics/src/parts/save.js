e270aa9258557bfbe3616cf9df3b9c4c1dcc6af5
lapiudevgit <devgit@lapiu.biz>
2022-11-23 15:17:53 -0300

    Fixed issue #18449: Impossible to add participant after correcting an e-mail address error

--- assets/packages/adminbasics/src/parts/save.js
+++ assets/packages/adminbasics/src/parts/save.js
@@ -35,11 +35,9 @@ const SaveController = () => {
     },
     isSubmitting = () => formSubmitting,
     displayLoadingState = (el) => {
-        //if($(el).data('form-id') == 'addnewsurvey') {
-            const loadingSpinner = '<i class="fa fa-cog fa-spin lsLoadingStateIndicator"></i>';
-            $(el).append(loadingSpinner);
-            disableButton(el);
-        //}
+        const loadingSpinner = '<i class="fa fa-cog fa-spin lsLoadingStateIndicator"></i>';
+        $(el).append(loadingSpinner);
+        disableButton(el);
     },
     stopDisplayLoadingState = () => {
         $('.lsLoadingStateIndicator').each((i, item) => {
