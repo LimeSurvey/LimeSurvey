31c7088cc4172b8a052c4c317e850d6662b6acb6
Tim Willig <tim.willig@limesurvey.org>
2022-11-29 13:58:04 +0100

    fixed issue: question editor notifier now auto hides again

--- assets/packages/adminbasics/build/adminbasics.js
+++ assets/packages/adminbasics/build/adminbasics.js
@@ -29866,6 +29866,14 @@
    * It should use bootstrap 5 elements
    * For example: a user is added via a controller action. After the modal is
    * closed (by clicking button "Add") a success alert should be shown.
+   *
+   * @param message string  | The text to be displayed
+   * @param alertType string | bs5 alert types ['success','primary','secondary','danger','warning','info','light','dark',]
+   * @param customOptions | possible options are:
+   *                         useHtml (boolean) -> use the @message as html
+   *                         timeout (int) -> the timeout in milliseconds until the notifier will fade/slide out
+   *                         styles (object) -> An object of css-attributes that will be put onto the inner container
+   *                         classes (string) -> The classes that will be put onto the inner container
    */
   window.LS = window.LS || {};
 
@@ -29876,14 +29884,36 @@
 
     _createClass(AjaxAlerts, [{
       key: "createAlert",
-      value: function createAlert(message, alertType) {
-        //bs5 alert types (e.g. alter-success)
+      value: function createAlert(message, alertType, customOptions) {
+        customOptions = customOptions || {};
+        var options = {
+          useHtml: customOptions.useHtml || true,
+          timeout: customOptions.timeout || 3500,
+          styles: customOptions.styles || {},
+          classes: customOptions.classes || ""
+        }; //bs5 alert types (e.g. alter-success)
+
         var alertTypes = ['success', 'primary', 'secondary', 'danger', 'warning', 'info', 'light', 'dark'];
         var alertDefault = 'success';
         var currentAlertType = alertTypes.includes(alertType) ? alertType : alertDefault;
-        var openDivTag = '<div class="alert alert-' + currentAlertType + ' alert-dismissible" role="alert">';
+        var openDivTag = '<div class="alert alert-' + currentAlertType + ' ' + options.classes + ' alert-dismissible" role="alert">';
         var buttonDismiss = '<button type="button" class="btn-close limebutton" data-bs-dismiss="alert" aria-label="Close"></button>';
-        $('#notif-container').append(openDivTag + buttonDismiss + message + '</div>');
+        var container = $(openDivTag + buttonDismiss + '</div>');
+
+        if (options.useHtml) {
+          container.html(message);
+        } else {
+          container.text(message);
+        }
+
+        container.css(options.styles);
+        var timeoutRef = setTimeout(function () {
+          container.alert('close');
+        }, options.timeout);
+        container.on('closed.bs.alert', function () {
+          clearTimeout(timeoutRef);
+        });
+        container.appendTo($('#notif-container'));
       }
     }]);
 
--- assets/packages/adminbasics/src/parts/ajaxAlerts.js
+++ assets/packages/adminbasics/src/parts/ajaxAlerts.js
@@ -3,14 +3,30 @@
  * It should use bootstrap 5 elements
  * For example: a user is added via a controller action. After the modal is
  * closed (by clicking button "Add") a success alert should be shown.
+ *
+ * @param message string  | The text to be displayed
+ * @param alertType string | bs5 alert types ['success','primary','secondary','danger','warning','info','light','dark',]
+ * @param customOptions | possible options are:
+ *                         useHtml (boolean) -> use the @message as html
+ *                         timeout (int) -> the timeout in milliseconds until the notifier will fade/slide out
+ *                         styles (object) -> An object of css-attributes that will be put onto the inner container
+ *                         classes (string) -> The classes that will be put onto the inner container
  */
 
 window.LS = window.LS || {};
 
 class AjaxAlerts {
 
-    createAlert(message, alertType)
+    createAlert(message, alertType, customOptions)
     {
+        customOptions = customOptions || {};
+        const options = {
+            useHtml : customOptions.useHtml || true,
+            timeout : customOptions.timeout || 3500,
+            styles: customOptions.styles || {},
+            classes: customOptions.classes || ""
+        };
+
         //bs5 alert types (e.g. alter-success)
         let alertTypes = [
             'success',
@@ -25,10 +41,25 @@ class AjaxAlerts {
 
         let alertDefault = 'success';
         let currentAlertType = alertTypes.includes(alertType) ? alertType : alertDefault;
-        let openDivTag = '<div class="alert alert-' + currentAlertType + ' alert-dismissible" role="alert">';
+        let openDivTag = '<div class="alert alert-' + currentAlertType + ' ' + options.classes + ' alert-dismissible" role="alert">';
         let buttonDismiss = '<button type="button" class="btn-close limebutton" data-bs-dismiss="alert" aria-label="Close"></button>';
 
-        $('#notif-container').append(openDivTag + buttonDismiss + message + '</div>');
+        const container = $(openDivTag + buttonDismiss + '</div>');
+
+        if (options.useHtml) {
+            container.html(message);
+        } else {
+            container.text(message);
+        }
+
+        container.css(options.styles);
+        let timeoutRef = setTimeout(() => { container.alert('close') }, options.timeout);
+
+        container.on('closed.bs.alert', () => {
+            clearTimeout(timeoutRef);
+        });
+
+        container.appendTo($('#notif-container'));
     }
 }
 
