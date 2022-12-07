ad41df6b481967be5ae76909ff4c7062394fc7dd
Denis Chenu <denis@sondages.pro>
2022-12-07 08:26:20 +0100

    Dev: split comment

--- application/models/Traits/PermissionTrait.php
+++ application/models/Traits/PermissionTrait.php
@@ -36,7 +36,7 @@ trait PermissionTrait
      * Criteria used for list or grid
      * Avoid usage of Permission on each object returned when there are a lot.
      * @see self::withListRight for adding it at scope in find action.
-     * @param integer $userid
+     * @param integer $userid (use current if is null)
      * @return CDbCriteria
      */
     public static function getPermissionCriteria($userid = null)
@@ -44,8 +44,9 @@ trait PermissionTrait
         /* Remind to set $userid if is null to current : Yii::app()->user->id; */
         $criteriaPerm = new CDbCriteria();
         /* New criteria to be added if user didn't have global read permission on this object */
-        /* UserId need to be used with (for example) $criteriaPerm->compare('t.owner_id', $userid, false); */
-        /* Adding object specific Permission with join with read_p = 1 on the minimal permission (set $this->getMinimalPermissionRead()) */
+        /* UserId can be used with (for example) $criteriaPerm->compare('t.owner_id', $userid, false); */
+        /* If object have minimal permission (set $this->getMinimalPermissionRead()) */
+        /* can be added with join request and  read_p = 1*/
         /* etc */
         return $criteriaPerm;
     }
