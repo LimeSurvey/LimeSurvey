3cfd2f159a115076d5fe595226a8d98a02cea2cc
Denis Chenu <denis@sondages.pro>
2022-12-09 07:06:30 +0000

    Fixed issue [security] #18514 : no CSRF control when delete token table (#2760)
    Dev: move to POST action

--- application/controllers/admin/tokens.php
+++ application/controllers/admin/tokens.php
@@ -2366,38 +2366,45 @@ class tokens extends Survey_Common_Action
         $oldtable = "tokens_$iSurveyId";
         $newtable = "old_tokens_{$iSurveyId}_$date";
         $newtableDisplay = Yii::app()->db->tablePrefix . $newtable;
-        if (!Yii::app()->request->getQuery('ok')) {
+        if (!App()->request->getPost('ok')) {
             $aData['sidemenu']['state'] = false;
             $this->_renderWrappedTemplate('token', array(
                 'message' => array(
                     'title' => gT("Delete survey participants table"),
-                    'message' => gT("If you delete this table tokens will no longer be required to access this survey.") . "<br />" . gT("A backup of this table will be made if you proceed. Your system administrator will be able to access this table.") . "<br />\n"
+                    'message' => CHtml::form(
+                            array("admin/tokens/sa/kill", 'surveyid' => $iSurveyId),
+                            'post',
+                            array('id' => 'deletetokentable', 'name' => 'deletetokentable')
+                        )
+                        .gT("If you delete this table tokens will no longer be required to access this survey.") . "<br />" . gT("A backup of this table will be made if you proceed. Your system administrator will be able to access this table.") . "<br />\n"
                         . sprintf('("%s")<br /><br />', $newtableDisplay)
-                        . "<input class='btn btn-danger' type='submit' value='"
-                        . gT("Delete table") . "' onclick=\"window.open('" . $this->getController()->createUrl("admin/tokens/sa/kill/surveyid/{$iSurveyId}/ok/Y") . "', '_top')\" />\n"
-                        . "<input class='btn btn-default' type='submit' value='"
-                        . gT("Cancel") . "' onclick=\"window.open('" . $this->getController()->createUrl("admin/tokens/sa/index/surveyid/{$iSurveyId}") . "', '_top')\" />\n"
+                        . "<button class='btn btn-danger' type='submit' value='Y' name='ok' >"
+                        . gT("Delete table") . "</button>\n"
+                        . "<a class='btn btn-default' href='" . $this->getController()->createUrl("admin/tokens/sa/index/surveyid/{$iSurveyId}") . "'>"
+                        . gT("Cancel") . "</a>\n"
+                        . "</form>"
                 )
             ), $aData);
-        } else /* The user has confirmed they want to delete the tokens table */ {
-            Yii::app()->db->createCommand()->renameTable("{{{$oldtable}}}", "{{{$newtable}}}");
-
-            //Remove any survey_links to the CPDB
-            SurveyLink::model()->deleteLinksBySurvey($iSurveyId);
-
-            $aData['sidemenu']['state'] = false;
-            $this->_renderWrappedTemplate('token', array(
-                'message' => array(
-                    'title' => gT("Delete survey participants table"),
-                    'message' => '<br />' . gT("The tokens table has now been removed and tokens are no longer required to access this survey.") . "<br /> " . gT("A backup of this table has been made and can be accessed by your system administrator.") . "<br />\n"
-                        . sprintf('("%s")<br /><br />', $newtableDisplay)
-                        . "<input type='submit' class='btn btn-default' value='"
-                        . gT("Main Admin Screen") . "' onclick=\"window.open('" . Yii::app()->getController()->createUrl("admin/survey/sa/view/surveyid/" . $iSurveyId) . "', '_top')\" />"
-                )
-            ), $aData);
-
-            LimeExpressionManager::SetDirtyFlag(); // so that knows that survey participants tables have changed
+            App()->end();
         }
+        /* The user has confirmed they want to delete the tokens table */
+        Yii::app()->db->createCommand()->renameTable("{{{$oldtable}}}", "{{{$newtable}}}");
+
+        //Remove any survey_links to the CPDB
+        SurveyLink::model()->deleteLinksBySurvey($iSurveyId);
+
+        $aData['sidemenu']['state'] = false;
+        $this->_renderWrappedTemplate('token', array(
+            'message' => array(
+                'title' => gT("Delete survey participants table"),
+                'message' => '<br />' . gT("The tokens table has now been removed and tokens are no longer required to access this survey.") . "<br /> " . gT("A backup of this table has been made and can be accessed by your system administrator.") . "<br />\n"
+                    . sprintf('("%s")<br /><br />', $newtableDisplay)
+                    . "<a class='btn btn-default' href='" . $this->getController()->createUrl("admin/survey/sa/view/surveyid/" . $iSurveyId) . "'>"
+                    . gT("Main Admin Screen") . "</a>"
+            )
+        ), $aData);
+
+        LimeExpressionManager::SetDirtyFlag(); // so that knows that survey participants tables have changed
     }
 
     /**
