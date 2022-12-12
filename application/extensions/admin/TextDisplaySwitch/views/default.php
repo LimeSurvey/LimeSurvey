6b20d02e5144e33c956528539bd0808214414c8e
Tim Willig <tim.willig@limesurvey.org>
2022-12-12 15:53:14 +0100

    fixed issue: remove checkbox from "show more" button in survey overview

--- application/extensions/admin/TextDisplaySwitch/views/default.php
+++ application/extensions/admin/TextDisplaySwitch/views/default.php
@@ -8,7 +8,7 @@
 
 <div class="selector__toggle_full_text float-end" data-toggle="buttons">
     <label class="btn btn-outline-secondary btn-xs" data-target=".selector__toggle_<?=$this->widgetsJsName?>" data-set-state="full">
-        <input type="checkbox" > <span><?=gT("Show more")?></span>
+        <span><?=gT("Show more")?></span>
         <!-- <span class="d-none"><?=gT("Show less")?></span> -->
     </label>
 </div>
