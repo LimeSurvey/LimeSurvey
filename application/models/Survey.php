3b0a7cb7a3ecd1be9dda777cb5bda7c05c80a646
Denis Chenu <denis@sondages.pro>
2022-12-14 13:27:18 +0100

    Dev: do not set any default for search scenario in model

--- application/models/Survey.php
+++ application/models/Survey.php
@@ -186,7 +186,11 @@ class Survey extends LSActiveRecord implements PermissionInterface
     public function init()
     {
         /** @inheritdoc */
-
+        /* Do not set any default when search, reset gsid */
+        if ($this->scenario == 'search') {
+            $this->gsid = null;
+            return;
+        }
         // Set the default values
         $this->htmlemail = 'Y';
         $this->format = 'G';
