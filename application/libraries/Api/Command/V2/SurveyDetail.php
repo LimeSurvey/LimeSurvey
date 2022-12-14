acf1f917c59fb6890b7515d20f3fe947b25c37a8
Kevin Foster <kevin.foster.uk@gmail.com>
2022-12-14 13:26:03 +0800

    Update REST API v2 - refactor survey-detail transform to retain sort order

--- application/libraries/Api/Command/V2/SurveyDetail.php
+++ application/libraries/Api/Command/V2/SurveyDetail.php
@@ -65,21 +65,41 @@ class SurveyDetail implements CommandInterface
 
         $survey['languages'] = $surveyModel->allLanguages;
 
+        // transformAll() can apply required entity sort so we must retain the sort order going forward
+        // - We use a lookup array later to access entities without needing to know their position in the collection
         $survey['questionGroups'] = (
             new TransformerOutputQuestionGroup()
         )->transformAll(
             $surveyModel->groups
         );
 
+        // An array of groups indexed by gid for easy look up
+        // - helps use to retain sort order when looping over models
+        $groupLookup = $this->createCollectionLookup('gid', $survey['questionGroups']);
+
         $transformerQuestion = new TransformerOutputQuestion();
         $transformerQuestionAttribute = new TransformerOutputQuestionAttribute();
-        foreach ($surveyModel->groups as $groupKey => $questionGroupModel) {
-            $survey['questionGroups'][$groupKey]['questions'] = $transformerQuestion->transformAll(
+        foreach ($surveyModel->groups as $questionGroupModel) {
+            // order of groups from the model relation may be different than from the transformed data
+            // - so we use the lookup to get a reference to the required entity without needing to
+            // - know its position in the output array
+            $group = &$groupLookup[$questionGroupModel->gid];
+
+            // transformAll() can apply required entity sort so we must retain the sort order going forward
+            // - We use a lookup array later to access entities without needing to know their position in the collection
+            $group['questions'] = $transformerQuestion->transformAll(
                 $questionGroupModel->questions
             );
 
-            foreach ($questionGroupModel->questions as $questionKey => $questionModel) {
-                $question = &$survey['questionGroups'][$groupKey]['questions'][$questionKey];
+            $questionLookup = $this->createCollectionLookup('qid', $group['questions']);
+
+            foreach ($questionGroupModel->questions as $questionModel) {
+
+                // questions from the model relation may be different than from the transformed data
+                // - so we use the lookup to get a reference to the required entity without needing to
+                // - know its position in the output array
+                $question = &$questionLookup[$questionModel->qid];
+
                 $question['attributes'] = $transformerQuestionAttribute->transformAll(
                     // questionattributes is returned as an associative array keyed on 'attribute'
                     // - so we need to call array_values to get the array of QuestionAttribute models
@@ -89,5 +109,25 @@ class SurveyDetail implements CommandInterface
         }
 
         return $survey;
+    }
+
+    /**
+     * Creation collection lookup
+     *
+     * Returns an array of entity references keyed on the specified key.
+     *
+     * @param string $key
+     * @param array $entityArray
+     * @return &array Entity reference
+     */
+    private function createCollectionLookup($key, &$entityArray)
+    {
+        $output = [];
+        foreach ($entityArray as &$entity) {
+            if (is_array($entity) && isset($entity[$key])) {
+                $output[$entity[$key]] = &$entity;
+            }
+        }
+        return $output;
     }
 }
