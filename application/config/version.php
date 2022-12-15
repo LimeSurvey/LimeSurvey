2eee6c07a19a5b54cba25cadf9480c8fea6c06b5
Patrick Teichmann <patrick.teichmann@limesurvey.org>
2022-12-15 19:12:47 +0100

    Dev: assetversion increase

--- application/config/version.php
+++ application/config/version.php
@@ -16,5 +16,5 @@ $config['dbversionnumber'] = 496;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
 $config['templateapiversion']  = 3;
-$config['assetsversionnumber'] = '30310';
+$config['assetsversionnumber'] = '30311';
 return $config;
