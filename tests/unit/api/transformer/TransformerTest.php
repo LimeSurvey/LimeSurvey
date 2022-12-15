daf283ca0ca17fe964c9fcdb097b568ec33ce085
Kevin Foster <kevin.foster.uk@gmail.com>
2022-12-15 15:48:51 +0800

    Update REST API - add transformer type cast tests

--- tests/unit/api/transformer/TransformerTest.php
+++ tests/unit/api/transformer/TransformerTest.php
@@ -5,16 +5,15 @@ namespace ls\tests\unit\api;
 use ls\tests\TestBaseClass;
 use LimeSurvey\Api\Transformer\Transformer;
 
-
 /**
- * @testdox API TransformerOutput
+ * @testdox API Transformer
  */
 class TransformerOutputTest extends TestBaseClass
 {
     /**
      * @testdox transform() Transformed data only includes fields specified in map.
      */
-    public function testTrasnformedDataIncludesOnlyFieldsSpecifiedInMap()
+    public function testTransformedDataIncludesOnlyFieldsSpecifiedInMap()
     {
         $transformer = new Transformer;
         $transformer->setDataMap([
@@ -53,5 +52,121 @@ class TransformerOutputTest extends TestBaseClass
             'given_name' => 'Kevin',
             'years_of_existence' => 40
         ], $transformedData);
+    }
+
+    /**
+     * @testdox transform() Maps to specified output fields via key config.
+     */
+    public function testMapsToSpecifiedOutputFieldsViaKeyConfig()
+    {
+        $transformer = new Transformer;
+        $transformer->setDataMap([
+            'first_name' => ['key' => 'given_name'],
+            'age' => ['key' => 'years_of_existence']
+        ]);
+        $transformedData = $transformer->transform([
+            'first_name' => 'Kevin',
+            'last_name' => 'Foster',
+            'age' => 40
+        ]);
+
+        $this->assertEquals([
+            'given_name' => 'Kevin',
+            'years_of_existence' => 40
+        ], $transformedData);
+    }
+
+    /**
+     * @testdox transform() Casts to primitive types.
+     */
+    public function testCastsToPrimitiveType()
+    {
+        $transformer = new Transformer;
+        $transformer->setDataMap([
+            'enable' => ['type' => 'boolean'],
+            'fraction' => ['type' => 'float'],
+            'age' => ['type' => 'int'],
+            'name' => ['type' => 'string'],
+        ]);
+
+        $transformedDataA = $transformer->transform([
+            'enable' => '1',
+            'fraction' => '1.33',
+            'age' => '40',
+            'name' => 123
+        ]);
+        $this->assertEquals([
+            'enable' => true,
+            'fraction' => 1.33,
+            'age' => 40,
+            'name' => '123'
+        ], $transformedDataA);
+
+        $transformedDataA = $transformer->transform([
+            'enable' => '0',
+            'fraction' => '1.33',
+            'age' => '40',
+            'name' => false
+        ]);
+        $this->assertEquals([
+            'enable' => false,
+            'fraction' => 1.33,
+            'age' => 40,
+            'name' => ''
+        ], $transformedDataA);
+    }
+
+    /**
+     * @testdox transform() Casts to via callable.
+     */
+    public function testCastsViaCallable()
+    {
+        $castBoolean = function($value) {
+            return (boolean) $value;
+        };
+        $castFloat = function ($value) {
+            return (float) $value;
+        };
+        $castInt = function ($value) {
+            return (int) $value;
+        };
+        $castString = function ($value) {
+            return (string) $value;
+        };
+
+
+        $transformer = new Transformer;
+        $transformer->setDataMap([
+            'enable' => ['type' => $castBoolean],
+            'fraction' => ['type' => $castFloat],
+            'age' => ['type' => $castInt],
+            'name' => ['type' => $castString],
+        ]);
+
+        $transformedDataA = $transformer->transform([
+            'enable' => '1',
+            'fraction' => '1.33',
+            'age' => '40',
+            'name' => 123
+        ]);
+        $this->assertEquals([
+            'enable' => true,
+            'fraction' => 1.33,
+            'age' => 40,
+            'name' => '123'
+        ], $transformedDataA);
+
+        $transformedDataA = $transformer->transform([
+            'enable' => '0',
+            'fraction' => '1.33',
+            'age' => '40',
+            'name' => false
+        ]);
+        $this->assertEquals([
+            'enable' => false,
+            'fraction' => 1.33,
+            'age' => 40,
+            'name' => ''
+        ], $transformedDataA);
     }
 }
