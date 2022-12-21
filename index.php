b6d501bdb37c648df56effc7596c2faa2e2a8e39
Borys Yermokhin <borys.ermokhin@gmail.com>
2022-12-21 12:10:39 +0100

    Feature request #18565: expand coverage for try-catch

--- index.php
+++ index.php
@@ -1,196 +1,196 @@
 <?php
-/*
- *---------------------------------------------------------------
- * SYSTEM FOLDER NAME
- *---------------------------------------------------------------
- *
- * This variable must contain the name of your "system" folder.
- * Include the path if the folder is not in the same  directory
- * as this file.
- *
- */
-$system_path = "vendor/yiisoft/yii/framework";
+try {
+    /*
+     *---------------------------------------------------------------
+     * SYSTEM FOLDER NAME
+     *---------------------------------------------------------------
+     *
+     * This variable must contain the name of your "system" folder.
+     * Include the path if the folder is not in the same  directory
+     * as this file.
+     *
+     */
+    $system_path = "vendor/yiisoft/yii/framework";
 
-/*
- *---------------------------------------------------------------
- * APPLICATION FOLDER NAME
- *---------------------------------------------------------------
- *
- * If you want this front controller to use a different "application"
- * folder then the default one you can set its name here. The folder
- * can also be renamed or relocated anywhere on your server.  If
- * you do, use a full server path. For more info please see the user guide:
- * http://codeigniter.com/user_guide/general/managing_apps.html
- *
- * NO TRAILING SLASH!
- *
- */
-$application_folder = dirname(__FILE__) . "/application";
+    /*
+     *---------------------------------------------------------------
+     * APPLICATION FOLDER NAME
+     *---------------------------------------------------------------
+     *
+     * If you want this front controller to use a different "application"
+     * folder then the default one you can set its name here. The folder
+     * can also be renamed or relocated anywhere on your server.  If
+     * you do, use a full server path. For more info please see the user guide:
+     * http://codeigniter.com/user_guide/general/managing_apps.html
+     *
+     * NO TRAILING SLASH!
+     *
+     */
+    $application_folder = dirname(__FILE__) . "/application";
 
-/*
- * --------------------------------------------------------------------
- * DEFAULT CONTROLLER
- * --------------------------------------------------------------------
- *
- * Normally you will set your default controller in the routes.php file.
- * You can, however, force a custom routing by hard-coding a
- * specific controller class/function here.  For most applications, you
- * WILL NOT set your routing here, but it's an option for those
- * special instances where you might want to override the standard
- * routing in a specific front controller that shares a common CI installation.
- *
- * IMPORTANT:  If you set the routing here, NO OTHER controller will be
- * callable. In essence, this preference limits your application to ONE
- * specific controller.  Leave the function name blank if you need
- * to call functions dynamically via the URI.
- *
- * Un-comment the $routing array below to use this feature
- *
- */
-// The directory name, relative to the "controllers" folder.  Leave blank
-// if your controller is not in a sub-folder within the "controllers" folder
-// $routing['directory'] = '';
+    /*
+     * --------------------------------------------------------------------
+     * DEFAULT CONTROLLER
+     * --------------------------------------------------------------------
+     *
+     * Normally you will set your default controller in the routes.php file.
+     * You can, however, force a custom routing by hard-coding a
+     * specific controller class/function here.  For most applications, you
+     * WILL NOT set your routing here, but it's an option for those
+     * special instances where you might want to override the standard
+     * routing in a specific front controller that shares a common CI installation.
+     *
+     * IMPORTANT:  If you set the routing here, NO OTHER controller will be
+     * callable. In essence, this preference limits your application to ONE
+     * specific controller.  Leave the function name blank if you need
+     * to call functions dynamically via the URI.
+     *
+     * Un-comment the $routing array below to use this feature
+     *
+     */
+    // The directory name, relative to the "controllers" folder.  Leave blank
+    // if your controller is not in a sub-folder within the "controllers" folder
+    // $routing['directory'] = '';
 
-// The controller class file name.  Example:  Mycontroller.php
-// $routing['controller'] = '';
+    // The controller class file name.  Example:  Mycontroller.php
+    // $routing['controller'] = '';
 
-// The controller function you wish to be called.
-// $routing['function']    = '';
-
-
-/*
- * -------------------------------------------------------------------
- *  CUSTOM CONFIG VALUES
- * -------------------------------------------------------------------
- *
- * The $assign_to_config array below will be passed dynamically to the
- * config class when initialized. This allows you to set custom config
- * items or override any default config values found in the config.php file.
- * This can be handy as it permits you to share one application between
- * multiple front controller files, with each file containing different
- * config values.
- *
- * Un-comment the $assign_to_config array below to use this feature
- *
- */
-    // $assign_to_config['name_of_config_item'] = 'value of config item';
+    // The controller function you wish to be called.
+    // $routing['function']    = '';
 
 
-
-// --------------------------------------------------------------------
-// END OF USER CONFIGURABLE SETTINGS.  DO NOT EDIT BELOW THIS LINE
-// --------------------------------------------------------------------
+    /*
+     * -------------------------------------------------------------------
+     *  CUSTOM CONFIG VALUES
+     * -------------------------------------------------------------------
+     *
+     * The $assign_to_config array below will be passed dynamically to the
+     * config class when initialized. This allows you to set custom config
+     * items or override any default config values found in the config.php file.
+     * This can be handy as it permits you to share one application between
+     * multiple front controller files, with each file containing different
+     * config values.
+     *
+     * Un-comment the $assign_to_config array below to use this feature
+     *
+     */
+        // $assign_to_config['name_of_config_item'] = 'value of config item';
 
 
 
-
-/*
- * ---------------------------------------------------------------
- *  Resolve the system path for increased reliability
- * ---------------------------------------------------------------
- */
-if (realpath($system_path) !== false) {
-    $system_path = realpath($system_path).'/';
-}
-
-// ensure there's a trailing slash
-$system_path = rtrim($system_path, '/').'/';
-
-// Is the system path correct?
-if (!is_dir($system_path)) {
-    exit("Your system folder path does not appear to be set correctly. Please open the following file and correct this: ".pathinfo(__FILE__, PATHINFO_BASENAME));
-}
-
-/*
- * -------------------------------------------------------------------
- *  Now that we know the path, set the main path constants
- * -------------------------------------------------------------------
- */
+    // --------------------------------------------------------------------
+    // END OF USER CONFIGURABLE SETTINGS.  DO NOT EDIT BELOW THIS LINE
+    // --------------------------------------------------------------------
 
 
-// The name of THIS file
-define('SELF', pathinfo(__FILE__, PATHINFO_BASENAME));
-
-define('ROOT', dirname(__FILE__));
-
-// The PHP file extension
-define('EXT', '.php');
-
-// Path to the system folder
-define('BASEPATH', str_replace("\\", "/", $system_path));
-
-// Path to the front controller (this file)
-define('FCPATH', str_replace(SELF, '', __FILE__));
-
-// Name of the "system folder"
-define('SYSDIR', trim(strrchr(trim(BASEPATH, '/'), '/'), '/'));
 
 
-// The path to the "application" folder
-if (is_dir($application_folder)) {
-    define('APPPATH', $application_folder.'/');
-} else {
-    if (!is_dir(BASEPATH . $application_folder . '/')) {
-        exit("Your application folder path does not appear to be set correctly. Please open the following file and correct this: ".SELF);
+    /*
+     * ---------------------------------------------------------------
+     *  Resolve the system path for increased reliability
+     * ---------------------------------------------------------------
+     */
+    if (realpath($system_path) !== false) {
+        $system_path = realpath($system_path).'/';
     }
-    define('APPPATH', BASEPATH . $application_folder . '/');
-}
 
-if (file_exists(APPPATH.'config'.DIRECTORY_SEPARATOR.'config.php')) {
-    $aSettings= include(APPPATH.'config'.DIRECTORY_SEPARATOR.'config.php');
-} else {
-    $aSettings=array();
-}
+    // ensure there's a trailing slash
+    $system_path = rtrim($system_path, '/').'/';
 
-// Set debug : if not set : set to default from PHP 5.3
-if (isset($aSettings['config']['debug'])) {
-    if ($aSettings['config']['debug']>0) {
-        define('YII_DEBUG', true);
-        if ($aSettings['config']['debug']>1) {
-            error_reporting(E_ALL);
+    // Is the system path correct?
+    if (!is_dir($system_path)) {
+        exit("Your system folder path does not appear to be set correctly. Please open the following file and correct this: ".pathinfo(__FILE__, PATHINFO_BASENAME));
+    }
 
-            // @see https://manual.limesurvey.org/Code_quality_guide#Assertions
-            assert_options(ASSERT_ACTIVE, true);
-            assert_options(ASSERT_WARNING, false);
-            assert_options(
-                ASSERT_CALLBACK,
-                function ($file, $line, $assertion, $message) {
-                    throw new Exception("The assertion $assertion in $file on line $line has failed: $message");
-                }
-            );
+    /*
+     * -------------------------------------------------------------------
+     *  Now that we know the path, set the main path constants
+     * -------------------------------------------------------------------
+     */
+
+
+    // The name of THIS file
+    define('SELF', pathinfo(__FILE__, PATHINFO_BASENAME));
+
+    define('ROOT', dirname(__FILE__));
+
+    // The PHP file extension
+    define('EXT', '.php');
+
+    // Path to the system folder
+    define('BASEPATH', str_replace("\\", "/", $system_path));
+
+    // Path to the front controller (this file)
+    define('FCPATH', str_replace(SELF, '', __FILE__));
+
+    // Name of the "system folder"
+    define('SYSDIR', trim(strrchr(trim(BASEPATH, '/'), '/'), '/'));
+
+
+    // The path to the "application" folder
+    if (is_dir($application_folder)) {
+        define('APPPATH', $application_folder.'/');
+    } else {
+        if (!is_dir(BASEPATH . $application_folder . '/')) {
+            exit("Your application folder path does not appear to be set correctly. Please open the following file and correct this: ".SELF);
+        }
+        define('APPPATH', BASEPATH . $application_folder . '/');
+    }
+
+    if (file_exists(APPPATH.'config'.DIRECTORY_SEPARATOR.'config.php')) {
+        $aSettings= include(APPPATH.'config'.DIRECTORY_SEPARATOR.'config.php');
+    } else {
+        $aSettings=array();
+    }
+
+    // Set debug : if not set : set to default from PHP 5.3
+    if (isset($aSettings['config']['debug'])) {
+        if ($aSettings['config']['debug']>0) {
+            define('YII_DEBUG', true);
+            if ($aSettings['config']['debug']>1) {
+                error_reporting(E_ALL);
+
+                // @see https://manual.limesurvey.org/Code_quality_guide#Assertions
+                assert_options(ASSERT_ACTIVE, true);
+                assert_options(ASSERT_WARNING, false);
+                assert_options(
+                    ASSERT_CALLBACK,
+                    function ($file, $line, $assertion, $message) {
+                        throw new Exception("The assertion $assertion in $file on line $line has failed: $message");
+                    }
+                );
+            } else {
+                error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED);
+            }
         } else {
-            error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED);
+            define('YII_DEBUG', false);
+            error_reporting(0);
         }
     } else {
-        define('YII_DEBUG', false);
-        error_reporting(0);
+        error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED);// Not needed if user doesn't remove their 'debug'=>0, for application/config/config.php (Installation is OK with E_ALL)
     }
-} else {
-    error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED);// Not needed if user doesn't remove their 'debug'=>0, for application/config/config.php (Installation is OK with E_ALL)
-}
 
-if (version_compare(PHP_VERSION, '5.3.3', '<')) {
-    die('This script can only be run on PHP version 5.3.3 or later! Your version: '.PHP_VERSION.'<br />');
-}
-require_once __DIR__ . '/vendor/autoload.php';
+    if (version_compare(PHP_VERSION, '5.3.3', '<')) {
+        die('This script can only be run on PHP version 5.3.3 or later! Your version: '.PHP_VERSION.'<br />');
+    }
+    require_once __DIR__ . '/vendor/autoload.php';
 
-/*
-* --------------------------------------------------------------------
-* LOAD THE BOOTSTRAP FILE
-* --------------------------------------------------------------------
-*
-* And away we go...
-*
-*/
+    /*
+    * --------------------------------------------------------------------
+    * LOAD THE BOOTSTRAP FILE
+    * --------------------------------------------------------------------
+    *
+    * And away we go...
+    *
+    */
 
-require_once BASEPATH . 'yii' . EXT;
-require_once APPPATH . 'core/LSYii_Application' . EXT;
+    require_once BASEPATH . 'yii' . EXT;
+    require_once APPPATH . 'core/LSYii_Application' . EXT;
 
-$config = require_once(APPPATH . 'config/internal' . EXT);
+    $config = require_once(APPPATH . 'config/internal' . EXT);
 
-Yii::$enableIncludePath = false;
+    Yii::$enableIncludePath = false;
 
-try {
     Yii::createApplication('LSYii_Application', $config)->run();
 } catch (\Throwable $e) {
     error_log(sprintf("General error: %s, stack trace: %s", $e->getMessage(), $e->getTraceAsString()));
