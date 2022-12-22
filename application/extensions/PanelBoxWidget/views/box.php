5ca44c93552df5f17f4d9d82ee585c8c60764e91
Kevin Foster <kevin.foster.uk@gmail.com>
2022-12-22 15:16:02 +0800

    CR-781: PC01.2.1 Cards - remove old card styles from 000-lime-admin-common.scss

--- application/extensions/PanelBoxWidget/views/box.php
+++ application/extensions/PanelBoxWidget/views/box.php
@@ -1,7 +1,7 @@
 <div class="<?php echo $sizeClass; ?> ls-flex-column ls-panelboxes-panelbox">
     <div class="card card-primary card-clickable ls-panelboxes-panelbox-inner selector__<?php echo CHtml::encode(str_replace(' ', '_', strtolower(strip_tags($title)))) ?>" id="card-<?php echo $position; ?>" data-url="<?php echo CHtml::encode($url); ?>" <?php if ($external) : ?> data-target="_blank" <?php endif; ?>>
         <div class="card-header">
-            <div class=""><?php echo viewHelper::filterScript(gT($title)); ?></div>
+            <div class="card-title"><?php echo viewHelper::filterScript(gT($title)); ?></div>
         </div>
         <div class="card-body d-flex">
             <?php echo viewHelper::filterScript(gT($description)); ?>
