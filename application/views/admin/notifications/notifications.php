1869adc2f7c82a0b8cc85c76f02a302d2a6f75ae
Tim Willig <tim.willig@limesurvey.org>
2023-01-09 15:07:33 +0100

    all #notif-container alerts now with icons

--- application/views/admin/notifications/notifications.php
+++ application/views/admin/notifications/notifications.php
@@ -1,9 +1,21 @@
 <?php
 $script = "
         var alertContainer = $('.non-ajax-alert');
-        LS.autoCloseAlert(alertContainer);
+        LS.autoCloseAlert(alertContainer, 1000000);
         ";
 Yii::app()->clientScript->registerScript('notif-autoclose', $script, CClientScript::POS_END);
+
+$alertTypesAndIcons = [
+    'success' => 'ri-checkbox-circle-fill',
+    'primary' => 'ri-notification-2-line',
+    'secondary' => 'ri-notification-2-line',
+    'danger' => 'ri-error-warning-fill',
+    'error' => 'ri-error-warning-fill',
+    'warning' => 'ri-alert-fill',
+    'info' => 'ri-notification-2-line',
+    'light' => 'ri-notification-2-line',
+    'dark' => 'ri-notification-2-line',
+];
 ?>
 
 <div class="container-fluid">
@@ -11,17 +23,23 @@ Yii::app()->clientScript->registerScript('notif-autoclose', $script, CClientScri
         <div id="notif-container" class="col-12 content-right">
             <?php foreach ($aMessage as $message): ?>
                 <?php
-                if (isset($message['type']) && in_array($message['type'], array('error', 'success', 'danger', 'warning', 'info'))) {
+                if (isset($message['type']) && array_key_exists($message['type'], $alertTypesAndIcons)) {
                     $sType = $message['type'];
                     if ($sType == 'error') {
                         $sType = 'danger';
                     }
+                    $icon = $alertTypesAndIcons[$message['type']];
                 } else {
                     $sType = 'success';
+                    $icon = 'ri-notification-2-line';
                 }
+
+                $iconElement = '<span class="' . $icon . ' me-2"></span>';
                 echo CHtml::openTag("div", array('class' => "alert alert-{$sType} alert-dismissible non-ajax-alert", 'role' => 'alert'));
-                echo CHtml::htmlButton("<span></span>", array('type' => 'button', 'class' => 'btn-close', 'data-bs-dismiss' => 'alert', 'aria-label' => gT("Close")));
+                echo CHtml::openTag("span", array('class' => $icon . ' me-2'));
+                echo CHtml::closeTag("span");
                 echo $message['message'];
+                echo CHtml::htmlButton(false, array('type' => 'button', 'class' => 'btn-close', 'data-bs-dismiss' => 'alert', 'aria-label' => gT("Close")));
                 echo CHtml::closeTag("div");
                 ?>
             <?php endforeach; ?>
