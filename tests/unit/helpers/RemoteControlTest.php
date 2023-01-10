14691025bff9a33538f961e90e5a60f1f76a953b
lapiudevgit <devgit@lapiu.biz>
2023-01-10 14:04:59 -0300

    New feature #07641: Option to use Y/N conversion in RemoteControl API export
    - Add test

--- tests/unit/helpers/RemoteControlTest.php
+++ tests/unit/helpers/RemoteControlTest.php
@@ -589,4 +589,74 @@ class RemoteControlTest extends TestBaseClass
         $result = $handler->get_fieldmap($sessionKey, self::$surveyId, 'es');
         $this->assertEquals('Pregunta de ejemplo', $result[$sgq]['question']);
     }
+
+    /**
+     * Test the export_responses API call.
+     */
+    public function testExportResponses()
+    {
+        \Yii::import('application.helpers.remotecontrol.remotecontrol_handle', true);
+        \Yii::import('application.helpers.viewHelper', true);
+        \Yii::import('application.libraries.BigData', true);
+        $dbo = \Yii::app()->getDb();
+
+        // Make sure the Authdb is in database (might not be the case if no browser login attempt has been made).
+        $plugin = \Plugin::model()->findByAttributes(array('name'=>'Authdb'));
+        if (!$plugin) {
+            $plugin = new \Plugin();
+            $plugin->name = 'Authdb';
+            $plugin->active = 1;
+            $plugin->save();
+        } else {
+            $plugin->active = 1;
+            $plugin->save();
+        }
+        App()->getPluginManager()->loadPlugin('Authdb', $plugin->id);
+        // Clear login attempts.
+        $query = sprintf('DELETE FROM {{failed_login_attempts}}');
+        $dbo->createCommand($query)->execute();
+
+        // Import survey
+        $filename = self::$surveysFolder . '/survey_archive_RemoteControlExportResponses.lsa';
+        self::importSurvey($filename);
+
+        // Create handler.
+        $admin   = new \AdminController('dummyid');
+        $handler = new \remotecontrol_handle($admin);
+
+        // Get session key.
+        $sessionKey = $handler->get_session_key(
+            self::$username,
+            self::$password
+        );
+        $this->assertNotEquals(['status' => 'Invalid user name or password'], $sessionKey);
+
+        // Check default export via API.
+        $result = $handler->export_responses($sessionKey, self::$surveyId, 'json');
+        $this->assertNotNull($result);
+        $responses = json_decode(file_get_contents($result->fileName), true);
+        $this->assertTrue(count($responses['responses']) === 1);
+        $this->assertEquals('Y', $responses['responses'][0]['Q00[SQ001]']);
+        // TODO: Currently cannot test the export of "N" responses because of a bug in
+        // the LSA export/import process (empty answers end up as null).
+        //$this->assertNull('N', $responses['responses'][0]['Q00[SQ002]']);
+
+        // Check export with Y/N conversion via API.
+        $additionalOptions = [
+            'convertN' => true,
+            'convertY' => true,
+            'nValue' => 'A',
+            'yValue' => 'B',
+        ];
+        $result = $handler->export_responses($sessionKey, self::$surveyId, 'json', null, 'all', 'code', 'short', null, null, null, $additionalOptions);
+        $this->assertNotNull($result);
+        $responses = json_decode(file_get_contents($result->fileName), true);
+        $this->assertTrue(count($responses['responses']) === 1);
+        $this->assertEquals('B', $responses['responses'][0]['Q00[SQ001]']);
+        //$this->assertEquals('A', $responses['responses'][0]['Q00[SQ002]']);
+
+        // Cleanup
+        self::$testSurvey->delete();
+        self::$testSurvey = null;
+    }
 }
