1ab19b91cbea65bff902af0e1496296d5cf8f001
Tim Willig <tim.willig@limesurvey.org>
2023-01-18 12:20:31 +0100

    fix test "Create user expired"

--- tests/functional/backend/UserManagementTest.php
+++ tests/functional/backend/UserManagementTest.php
@@ -341,10 +341,6 @@ class UserManagementTest extends TestBaseClassWeb
             );
             $save->click();
 
-            // Wait for "Saved successfully" alert
-            $alert = $this->waitForElementShim($web, '#notif-container .alert-success');
-            $web->wait(10)->until(WebDriverExpectedCondition::visibilityOf($alert));
-
             // Make sure the user was saved in database.
             $users = \User::model()->findAllByAttributes(['users_name' => $username]);
             $this->assertCount(1, $users);
