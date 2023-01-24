1b49f1d744d146234edbde198102c30a48e80c3c
Denis Chenu <denis@sondages.pro>
2023-01-24 18:30:36 +0100

    Fixed issue #18603: Set Minimum number of files for File Upload not working Dev: allow "" and max and min file

--- application/controllers/UploaderController.php
+++ application/controllers/UploaderController.php
@@ -338,9 +338,15 @@ class UploaderController extends SurveyController
 
         $fn = $sFieldName;
         $qid = (int) Yii::app()->request->getParam('qid');
-        $minfiles = (int) Yii::app()->request->getParam('minfiles');
-        $maxfiles = (int) Yii::app()->request->getParam('maxfiles');
         $qidattributes = QuestionAttribute::model()->getQuestionAttributes($qid);
+        $minfiles = "";
+        if (!empty($qidattributes['min_num_of_files'])) {
+            $minfiles = intval($qidattributes['min_num_of_files']);
+        }
+        $maxfiles = "";
+        if (!empty($qidattributes['max_num_of_files'])) {
+            $maxfiles = intval($qidattributes['max_num_of_files']);
+        }
         $maxfilesize = floor(min(intval($qidattributes['max_filesize']), getMaximumFileUploadSize() / 1024));
         if($maxfilesize <=0 ) {
             $maxfilesize = getMaximumFileUploadSize() / 1024;
