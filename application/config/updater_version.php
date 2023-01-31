a9d844b103583956c2ae8134cbdbdf1d8bb40b32
Carsten Schmitz <carsten.schmitz@limesurvey.org>
2023-01-31 10:20:39 +0100

    Dev Raised updater version to 18

--- application/config/updater_version.php
+++ application/config/updater_version.php
@@ -15,7 +15,7 @@
 
     // THIS FILE SHOULD NOT BE CHANGED MANUALLY !
 
-$config['updaterversion'] = 17;
+$config['updaterversion'] = 18;
 $config['comfort_update_server_url'] = 'comfortupdate.limesurvey.org/';
 $config['comfort_update_server_ssl'] = 1;
 return $config;
