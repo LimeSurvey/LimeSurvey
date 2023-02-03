285c013bd93f499ad712eb172793cbb302a5bee9
pstelling <patricia.stelling@limesurvey.org>
2023-02-03 10:23:12 +0100

    dev: added missing topbar for responses view, changed buttons to ButtonWidget

--- application/views/responses/partial/topbarBtns/responseViewTopbarRight_view.php
+++ application/views/responses/partial/topbarBtns/responseViewTopbarRight_view.php
@@ -1,52 +1,125 @@
-<?php if ($exist): ?>
-    <?php if ($hasResponsesUpdatePermission && isset($rlanguage)): ?>
-        <a class="btn btn-outline-secondary"
-           href='<?php echo Yii::App()->createUrl("admin/dataentry/sa/editdata/subaction/edit/surveyid/{$surveyid}/id/{$id}/lang/$rlanguage"); ?>'>
-            <span class="ri-pencil-fill"></span>
-            <?php eT("Edit this entry"); ?>
-        </a>
-    <?php endif; ?>
-    <?php if ($hasResponsesDeletePermission && isset($rlanguage)): ?>
-        <a class="btn btn-outline-secondary" href='#'
-           onclick='if (confirm("<?php eT("Are you sure you want to delete this entry?", "js"); ?>")) { <?php echo convertGETtoPOST(Yii::App()->createUrl("admin/dataentry/sa/delete/", ['id' => $id, 'sid' => $surveyid])); ?>}'>
-            <span class="ri-delete-bin-fill text-danger"></span>
-            <?php eT("Delete this entry"); ?>
-        </a>
-    <?php endif; ?>
+<?php
+if ($exist) {
+    if ($hasResponsesUpdatePermission && isset($rlanguage)) {
+        $this->widget(
+            'ext.ButtonWidget.ButtonWidget',
+            [
+                'name' => 'boxes-close-button',
+                'id' => 'boxes-close-button',
+                'text' => gT('Edit this entry'),
+                'icon' => 'ri-pencil-fill text-success',
+                'link' => Yii::App()->createUrl("admin/dataentry/sa/editdata/subaction/edit/surveyid/{$surveyid}/id/{$id}/lang/$rlanguage"),
+                'htmlOptions' => [
+                    'class' => 'btn btn-outline-secondary',
+                ],
+            ]
+        );
+    }
+    ?>
+    <?php if ($hasResponsesDeletePermission && isset($rlanguage)) {
+        $confirmQuestion = gT("Are you sure you want to delete this entry?", "js");
+        $deleteLink = convertGETtoPOST(Yii::App()->createUrl(
+            "admin/dataentry/sa/delete/",
+            ['id' => $id, 'sid' => $surveyid]
+        ));
+        $this->widget(
+            'ext.ButtonWidget.ButtonWidget',
+            [
+                'name' => 'boxes-close-button',
+                'id' => 'boxes-close-button',
+                'text' => gT('Delete this entry'),
+                'icon' => 'ri-delete-bin-fill text-danger',
+                'link' => '',
+                'htmlOptions' => [
+                    'class' => 'btn btn-outline-secondary',
+                    'onclick' => "if (confirm('$confirmQuestion')) { $deleteLink}"
+                ],
+            ]
+        );
+    }
+    if ($bHasFile) {
+        $this->widget(
+            'ext.ButtonWidget.ButtonWidget',
+            [
+                'name' => 'boxes-close-button',
+                'id' => 'boxes-close-button',
+                'text' => gT('Download files'),
+                'icon' => 'ri-download-fill',
+                'link' => Yii::app()->createUrl(
+                    "responses/downloadfiles",
+                    ["surveyId" => $surveyid,
+                        "responseIds" => $id]
+                ),
+                'htmlOptions' => [
+                    'class' => 'btn btn-outline-secondary',
+                ],
+            ]
+        );
+    }
 
-    <?php if ($bHasFile): ?>
-        <a class="btn btn-outline-secondary"
-           href='<?php echo Yii::app()->createUrl("responses/downloadfiles", ["surveyId" => $surveyid, "responseIds" => $id]); ?>'>
-            <span class="ri-download-fill"></span>
-            <?php eT("Download files"); ?>
-        </a>
-    <?php endif; ?>
+    $this->widget(
+        'ext.ButtonWidget.ButtonWidget',
+        [
+            'name' => 'boxes-close-button',
+            'id' => 'boxes-close-button',
+            'text' => gT('Export this response'),
+            'icon' => 'ri-download-fill downloadfile',
+            'link' => Yii::App()->createUrl("admin/export/sa/exportresults/surveyid/$surveyid/id/$id"),
+            'htmlOptions' => [
+                'class' => 'btn btn-outline-secondary',
+            ],
+        ]
+    );
+}
 
-    <a class="btn btn-outline-secondary"
-       href='<?php echo Yii::App()->createUrl("admin/export/sa/exportresults/surveyid/$surveyid/id/$id"); ?>'>
-        <span class="ri-download-fill downloadfile"></span>
-        <?php eT("Export this response"); ?>
-    </a>
-<?php endif; ?>
+$disablePrevious = '';
+if (!$previous) {
+    $disablePrevious = 'disabled';
+}
 
-<a href='<?php echo Yii::App()->createUrl("responses/view/", ['surveyId' => $surveyid, 'id' => $previous]); ?>'
-   title='<?php eT("Show previous..."); ?>'
-   class="btn btn-outline-secondary <?php if (!$previous) {
-       echo 'disabled';
-   } ?>">
-    <span class="ri-arrow-left-circle-fill" title='<?php eT("Show previous..."); ?>'></span>
-    <?php eT("Show previous..."); ?>
-</a>
-<a href='<?php echo Yii::App()->createUrl("responses/view/", ['surveyId' => $surveyid, 'id' => $next]); ?>'
-   title='<?php eT("Show next..."); ?>'
-   class="btn btn-outline-secondary <?php if (!$next) {
-       echo 'disabled';
-   } ?>">
-    <span class="ri-arrow-right-circle-fill" title='<?php eT("Show next..."); ?>'></span>
-    <?php eT("Show next..."); ?>
-</a>
-<a class="btn btn-danger" href="<?php echo $closeUrl; ?>">
-    <span class="ri-close-fill"></span>
-    <?php eT("Close"); ?>
-</a>
+$this->widget(
+    'ext.ButtonWidget.ButtonWidget',
+    [
+        'name' => 'export-response',
+        'id' => 'export-response',
+        'text' => gT('Show previous...'),
+        'icon' => 'ri-arrow-left-circle-fill',
+        'link' => Yii::App()->createUrl("responses/view/", ['surveyId' => $surveyid, 'id' => $previous]),
+        'htmlOptions' => [
+            'class' => "btn btn-outline-secondary $disablePrevious",
+        ],
+    ]
+);
 
+$disableNext = '';
+if (!$next) {
+    $disableNext =  'disabled';
+}
+
+$this->widget(
+    'ext.ButtonWidget.ButtonWidget',
+    [
+        'name' => 'export-response',
+        'id' => 'export-response',
+        'text' => gT('Show next...'),
+        'icon' => 'ri-arrow-right-circle-fill',
+        'link' => Yii::App()->createUrl("responses/view/", ['surveyId' => $surveyid, 'id' => $next]),
+        'htmlOptions' => [
+            'class' => "btn btn-outline-secondary $disableNext",
+        ],
+    ]
+);
+
+$this->widget(
+    'ext.ButtonWidget.ButtonWidget',
+    [
+        'name' => 'export-response',
+        'id' => 'export-response',
+        'text' => gT('Close'),
+        'icon' => 'ri-close-fill',
+        'link' => $closeUrl,
+        'htmlOptions' => [
+            'class' => "btn btn-danger",
+        ],
+    ]
+);
