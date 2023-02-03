fe5679b65a1da4f16ef5fd997dd956a90943b5fc
Lapiu Dev <devgit@lapiu.biz>
2023-02-03 11:54:54 -0600

    18438: Fixed Show popups, wrong text in inherit-button
    Bug fixed.

--- application/views/themeOptions/options_core.php
+++ application/views/themeOptions/options_core.php
@@ -121,8 +121,13 @@ $bInherit = (!empty($aTemplateConfiguration['sid']) || !empty($aTemplateConfigur
                                 $optionsValues = !empty($attribute['options']) ? explode('|', $attribute['options']) : array();
                                 $optionLabels = !empty($attribute['optionlabels']) ? explode('|', $attribute['optionlabels']) : array();
                                 $options = array_combine($optionsValues, $optionLabels);
-                                if ($bInherit && isset($sParentOption)){
-                                    $options['inherit'] = gT("Inherit") . ' [' . $sParentOption . ']';
+                                if ($bInherit && isset($sParentOption)) {
+                                    if(is_numeric($sParentOption) && array_key_exists($sParentOption, $options)) {
+                                        $sParentLabelOption = $options[$sParentOption];
+                                        $options['inherit'] = gT("Inherit") . ' [' . gT($sParentLabelOption) . ']';
+                                    } else {
+                                        $options['inherit'] = gT("Inherit") . ' [' . gT($sParentOption) . ']';
+                                    }
                                 }
 
                                 echo '<div class="col-sm-12">
