8eb8e31d67ea35b987bf212713128852b8c1acc2
olleharstedt <olleharstedt@yahoo.com>
2023-02-06 10:47:11 +0100

    Release 3.28.48+230207

--- application/config/version.php
+++ application/config/version.php
@@ -12,10 +12,10 @@
  */
 
 
-$config['versionnumber'] = '3.28.47';
+$config['versionnumber'] = '3.28.48';
 $config['dbversionnumber'] = 366;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
 $config['templateapiversion'] = 3;
-$config['assetsversionnumber'] = '30276';
+$config['assetsversionnumber'] = '30277';
 return $config;
