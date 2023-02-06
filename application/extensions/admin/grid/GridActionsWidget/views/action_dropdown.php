6c02e054ecdc1920c0ebe54a25982fd07115dc6f
Patrick Teichmann <patrick.teichmann@limesurvey.org>
2023-02-06 15:07:31 +0100

    task #682: update participant/participantshare table to new design

--- application/extensions/admin/grid/GridActionsWidget/views/action_dropdown.php
+++ application/extensions/admin/grid/GridActionsWidget/views/action_dropdown.php
@@ -2,6 +2,7 @@
 /**
  * @var array $dropdownItems
  */
+
 ?>
 <div class="dropdown">
     <button class="btn btn-sm btn-outline-secondary ls-dropdown-toggle" data-bs-toggle="dropdown" type="button"
@@ -11,13 +12,22 @@
     <ul class="dropdown-menu">
         <?php foreach ($dropdownItems as $dropdownItem) : ?>
             <li>
-                <a class="dropdown-item <?= $dropdownItem['enabledCondition'] ? "" : "disabled" ?>"
-                   href="<?= $dropdownItem['url'] ?>"
-                   role="button"
-                   data-bs-toggle="tooltip"
-                   title="<?= $dropdownItem['title'] ?>">
-                    <i class="ri-add-circle-fill"></i><?= $dropdownItem['title'] ?>
-                </a>
+                <div data-bs-toggle="tooltip" title="<?= $dropdownItem['tooltip'] ?? '' ?>">
+                    <a id="<?= $dropdownItem['linkId'] ?? '' ?>"
+                       class="dropdown-item <?= $dropdownItem['enabledCondition'] ? "" : "disabled" ?> <?= $dropdownItem['linkClass'] ?? '' ?>"
+                       href="<?= $dropdownItem['url'] ?? '#' ?>"
+                       role="button"
+                        <?php if (isset($dropdownItem['linkAttributes']) && is_array($dropdownItem['linkAttributes'])) : ?>
+                            <?php foreach ($dropdownItem['linkAttributes'] as $attribute => $value) : ?>
+                                <?= "$attribute='$value'" ?>
+                            <?php endforeach; ?>
+                        <?php endif; ?>>
+                        <?php if (isset($dropdownItem['iconClass'])) : ?>
+                            <i class="<?= $dropdownItem["iconClass"] ?>"></i>
+                        <?php endif; ?>
+                        <?= $dropdownItem['title'] ?>
+                    </a>
+                </div>
             </li>
         <?php endforeach; ?>
     </ul>
