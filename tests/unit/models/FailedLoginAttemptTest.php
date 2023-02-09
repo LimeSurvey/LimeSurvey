1e4d6669983e0012f76285989ef232e683518f7c
gabrieljenik <gabriel.jenik@gmail.com>
2023-02-09 09:48:06 -0300

    Fixed Issue #18581: Blocking users after X failed attempts counts incorrectly (off by 1)
    DEV: Delete Attempt Review.

--- tests/unit/models/FailedLoginAttemptTest.php
+++ tests/unit/models/FailedLoginAttemptTest.php
@@ -1,47 +1,48 @@
 <?php
 
 namespace ls\tests;
+
 use FailedLoginAttempt;
 
 class FailedLoginAttempTest extends TestBaseClass
-{    
-   
-
-    public function testDeleteAttempts()
+{
+    public function testAddDeleteAttemp()
     {
         // Save Ip
         $ip = substr(getIPAddress(), 0, 40);
-        
+
+        // Make sure there are no records for the ip
+        FailedLoginAttempt::model()->deleteAttempts(FailedLoginAttempt::TYPE_LOGIN);
+        $this->assertNull(FailedLoginAttempt::model()->findByAttributes((array('ip' => $ip))));
+
+        // Verify that the try counter increases by one
+        FailedLoginAttempt::model()->addAttempt();
+        $data = FailedLoginAttempt::model()->findByAttributes((array('ip' => $ip)));
+        $this->assertEquals(1, $data->number_attempts);
+
+        // Verify that the try counter increases by one
+        FailedLoginAttempt::model()->addAttempt();
+        $data = FailedLoginAttempt::model()->findByAttributes((array('ip' => $ip)));
+        $this->assertEquals(2, $data->number_attempts);
+
         // Verify that all records are deleted
-        FailedLoginAttempt::model()->deleteAttempts(FailedLoginAttempt::TYPE_LOGIN);        
+        FailedLoginAttempt::model()->deleteAttempts(FailedLoginAttempt::TYPE_LOGIN);
         $this->assertNull(FailedLoginAttempt::model()->findByAttributes((array('ip' => $ip))));
     }
 
-    public function testAddAttemp()
-    {
-        // Save Ip
-        $ip = substr(getIPAddress(), 0, 40);
-
-        // Verify that the try counter increases by one
-        FailedLoginAttempt::model()->addAttempt(); 
-        $data = FailedLoginAttempt::model()->findByAttributes((array('ip' => $ip)));
-        $this->assertEquals(1, $data->number_attempts);
-    }
-
     public function testIsLockedOut()
-    {        
-      
+    {
         $maxLoginAttempt = \Yii::app()->getConfig('maxLoginAttempt');
 
         // Verify that the user has attempts available
         FailedLoginAttempt::model()->deleteAttempts(FailedLoginAttempt::TYPE_LOGIN);
-        for ($i=0; $i < $maxLoginAttempt - 1; $i++) { 
-            FailedLoginAttempt::model()->addAttempt();            
-            $this->assertFalse( FailedLoginAttempt::model()->isLockedOut(FailedLoginAttempt::TYPE_LOGIN));
+        for ($i = 0; $i < $maxLoginAttempt - 1; $i++) {
+            FailedLoginAttempt::model()->addAttempt();
+            $this->assertFalse(FailedLoginAttempt::model()->isLockedOut(FailedLoginAttempt::TYPE_LOGIN));
         }
 
         // Verify that the user has no attempts available
         FailedLoginAttempt::model()->addAttempt();  
-        $this->assertTrue( FailedLoginAttempt::model()->isLockedOut(FailedLoginAttempt::TYPE_LOGIN));
+        $this->assertTrue(FailedLoginAttempt::model()->isLockedOut(FailedLoginAttempt::TYPE_LOGIN));
     }
 }
