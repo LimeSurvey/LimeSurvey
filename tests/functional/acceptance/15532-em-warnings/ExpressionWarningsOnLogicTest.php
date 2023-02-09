8c3fdb41172423e58331903a6c260ff4e1a97680
victor <victor3790@gmail.com>
2023-02-09 17:52:39 -0600

    18581: Tests fixed. Blocking users after X failed attempts counts incorrectly (off by 1)
    Warning modals for enforced SSL and password change closed before processing the tests.

--- tests/functional/acceptance/15532-em-warnings/ExpressionWarningsOnLogicTest.php
+++ tests/functional/acceptance/15532-em-warnings/ExpressionWarningsOnLogicTest.php
@@ -53,7 +53,14 @@ class ExpressionWarningsOnLogicTest extends TestBaseClassWeb
         );
         try {
             self::$webDriver->get($url);
-            sleep(5);
+            sleep(1);
+
+            // Close modals ( Password and enforced SSL warnings ).
+            while (self::$webDriver->findElement(WebDriverBy::cssSelector('#admin-notification-modal .close'))->isDisplayed()) {
+                self::$webDriver->findElement(WebDriverBy::cssSelector('#admin-notification-modal .close'))->click();
+                sleep(5);
+            }
+
             /* Did we have thew warning alert */
             $this->assertTrue(self::$webDriver->findElement(WebDriverBy::cssSelector('#logicfiletable .alert-warning'))->isDisplayed(), "Unable to find the alert");
             /* We found the count of warnings */
