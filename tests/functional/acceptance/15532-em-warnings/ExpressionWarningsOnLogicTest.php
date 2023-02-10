5ae6781f57666254e680405023046d20cf194d81
Lapiu Dev <devgit@lapiu.biz>
2023-02-10 12:35:17 -0600

    18581 Fixing tests. Blocking users after X failed attempts counts incorrectly (off by 1)
    Adding sleep time after closing modals.

--- tests/functional/acceptance/15532-em-warnings/ExpressionWarningsOnLogicTest.php
+++ tests/functional/acceptance/15532-em-warnings/ExpressionWarningsOnLogicTest.php
@@ -54,17 +54,18 @@ class ExpressionWarningsOnLogicTest extends TestBaseClassWeb
 
         try {
             self::$webDriver->get($url);
-            sleep(5);
+            sleep(1);
 
             // Close modals.
-	    self::ignoreAdminNotification();
+	        self::ignoreAdminNotification();
+            sleep(1);
 
             /* Did we have thew warning alert */
             $this->assertTrue(self::$webDriver->findElement(WebDriverBy::cssSelector('#logicfiletable .alert-warning'))->isDisplayed(), "Unable to find the alert");
             /* We found the count of warnings */
-            /*$elementStrong = self::$webDriver->findElement(WebDriverBy::cssSelector('#logicfiletable .alert-warning strong'));
+            $elementStrong = self::$webDriver->findElement(WebDriverBy::cssSelector('#logicfiletable .alert-warning strong'));
             $strongAlert = $elementStrong->getText();
-            $this->assertEquals($strongAlert, "This question has at least 1 warning.", "Numbers of warning seems invalid, need one warning.");*/
+            $this->assertEquals($strongAlert, "This question has at least 1 warning.", "Numbers of warning seems invalid, need one warning.");
         } catch (Exception $ex) {
             $screenshot = self::$webDriver->takeScreenshot();
             $filename = self::$screenshotsFolder.'/'.__CLASS__ . '_' . __FUNCTION__ . '.png';
