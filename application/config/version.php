ebfbe5e3ebd54be8e6af468f12abc55bbb9acec1
Denis Chenu <denis@sondages.pro>
2023-02-15 19:36:13 +0100

    Fixed issue #18639: Token browse can be unusable with lot of data in attribute Dev: wrap value inside in cell (and encode) Dev: ellipsed bigger value Dec: set a min-width and a max-width with a container

--- application/config/version.php
+++ application/config/version.php
@@ -16,5 +16,5 @@ $config['dbversionnumber'] = 495;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
 $config['templateapiversion']  = 3;
-$config['assetsversionnumber'] = '30319';
+$config['assetsversionnumber'] = '30320';
 return $config;
