821324d615ac94325e8bbd5e62d2329605f1c03d
lapiudevgit <devgit@lapiu.biz>
2023-02-17 08:28:43 -0300

    Fixed issue #18644: YII_DEBUG was not used inside KCFinder

--- vendor/kcfinder/core/bootstrap.php
+++ vendor/kcfinder/core/bootstrap.php
@@ -108,6 +108,42 @@ function checkLSSession()
     {
         define("BASEPATH", realpath($sLimesurveyFolder . "/../vendor/yiisoft/yii/framework") . "/");
     }
+
+    if (!defined('YII_DEBUG')) {
+        if (file_exists(APPPATH.'config'.DIRECTORY_SEPARATOR.'config.php')) {
+            $aSettings= include(APPPATH.'config'.DIRECTORY_SEPARATOR.'config.php');
+        } else {
+            $aSettings=array();
+        }
+        
+        // Set debug : if not set : set to default from PHP 5.3
+        if (isset($aSettings['config']['debug'])) {
+            if ($aSettings['config']['debug']>0) {
+                define('YII_DEBUG', true);
+                if ($aSettings['config']['debug']>1) {
+                    error_reporting(E_ALL);
+        
+                    // @see https://manual.limesurvey.org/Code_quality_guide#Assertions
+                    assert_options(ASSERT_ACTIVE, true);
+                    assert_options(ASSERT_WARNING, false);
+                    assert_options(
+                        ASSERT_CALLBACK,
+                        function ($file, $line, $assertion, $message) {
+                            throw new Exception("The assertion $assertion in $file on line $line has failed: $message");
+                        }
+                    );
+                } else {
+                    error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED);
+                }
+            } else {
+                define('YII_DEBUG', false);
+                error_reporting(0);
+            }
+        } else {
+            error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED);// Not needed if user doesn't remove their 'debug'=>0, for application/config/config.php (Installation is OK with E_ALL)
+        }
+    }
+
     require_once APPPATH . '../vendor/autoload.php';
     require_once BASEPATH . 'yii.php';
     require_once APPPATH . 'core/LSYii_Application.php';
