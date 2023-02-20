20109f62792c8ee5c8c0cc9ffc8180afd39e83f7
olleharstedt <olleharstedt@yahoo.com>
2023-02-20 10:38:52 +0100

    Release 5.6.6+230220

--- application/config/version.php
+++ application/config/version.php
@@ -11,10 +11,10 @@
  * See COPYRIGHT.php for copyright notices and details.
  */
 
-$config['versionnumber'] = '5.6.5';
+$config['versionnumber'] = '5.6.6';
 $config['dbversionnumber'] = 495;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
 $config['templateapiversion']  = 3;
-$config['assetsversionnumber'] = '30319';
+$config['assetsversionnumber'] = '30320';
 return $config;
