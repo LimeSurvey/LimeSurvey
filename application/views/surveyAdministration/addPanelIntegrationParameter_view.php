c88aea34fe25b9d3b79529c5d29e0d9c9b99bcf5
Gabriel Jenik <gabriel.jenik@gmail.com>
2023-02-23 05:03:20 -0300

    Fixed issue #18300: Panel Integration is failing to load (#2607)
    * Fixed issue #18300: Panel Integration is failing to load
    
    * Fixed issue #18300: Panel Integration is failing to load
    
    * Fixed issue #18300: Panel Integration is failing to load
    
    ---------
    
    Co-authored-by: encuestabizdevgit <devgit@encuesta.biz>
    Co-authored-by: lapiudevgit <devgit@lapiu.biz>

--- application/views/surveyAdministration/addPanelIntegrationParameter_view.php
+++ application/views/surveyAdministration/addPanelIntegrationParameter_view.php
@@ -9,19 +9,22 @@
 
 ?>
 
-<div id='dlgEditParameter'>
+<div id='dlgEditParameter' class='hide'
+    data-save-url='<?= Yii::app()->createUrl("surveyAdministration/saveUrlParam") ?>'
+    data-delete-url='<?= Yii::app()->createUrl("surveyAdministration/deleteUrlParam") ?>'
+>
     <div id='dlgForm' class='form-horizontal'>
         <div class='row'>
             <div class='mb-3'>
-                <label class='form-label col-md-3' for='paramname'><?php eT('Parameter name:'); ?></label>
-                <div class='col-md-4'>
+                <label class='form-label col-sm-3' for='paramname'><?php eT('Parameter name:'); ?></label>
+                <div class='col-sm-4'>
                     <input class='form-control' name='paramname' id='paramname' type='text' size='20' />
                 </div>
             </div>
             <div class='mb-3'>
-                <label class='form-label col-md-3' for='targetquestion'><?php eT('Target (sub-)question:'); ?></label>
-                <div class='col-md-4'>
-                    <select class='form-select' name='targetquestion' id='targetquestion' size='1'>
+                <label class='form-label col-sm-3' for='targetquestion'><?php eT('Target (sub-)question:'); ?></label>
+                <div class='col-sm-4'>
+                    <select class='form-control' name='targetquestion' id='targetquestion' size='1'>
                         <option value=''><?php eT('(No target question)'); ?></option>
                         <?php foreach ($questions as $question){?>
                             <option value='<?php echo $question['qid'].'-'.$question['sqid'];?>'><?php echo $question['title'].': '.ellipsize(flattenText($question['question'],true,true),43,.70);
