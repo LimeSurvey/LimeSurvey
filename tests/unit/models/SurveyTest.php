63d60301c8b5ca522f7d141569db42750d6b6d04
Lapiu Dev <devgit@lapiu.biz>
2023-02-27 12:12:08 -0600

    18630: Survey list showing wrong icon for surveys. Tests.
    Unit tests added for getRunning function.

--- tests/unit/models/SurveyTest.php
+++ tests/unit/models/SurveyTest.php
@@ -2,8 +2,44 @@
 
 namespace ls\tests;
 
-
 class SurveyTest extends BaseModelTestCase
 {
     protected $modelClassName = \Survey::class;
+
+    public static function setUpBeforeClass(): void
+    {
+        parent::setupBeforeClass();
+
+        \SettingGlobal::setSetting('timeadjust', '+0 hours');
+    }
+
+    public function testInactiveSurve(): void
+    {
+        $survey = new \Survey();
+        $survey->active = 'N';
+
+        $icon = $survey->getRunning();
+
+        $this->assertSame(
+            '<a href="' . App()->createUrl('/surveyAdministration/view/surveyid/' . $survey->sid) . '" class="survey-state" data-toggle="tooltip" title="' . gT('Inactive') . '"><span class="fa fa-stop text-warning"></span><span class="sr-only">' . gT('Inactive') . '"</span></a>',
+            $icon,
+            'The correct icon for an inactive survey was not returned.'
+        );
+    }
+
+    public function testActiveSurveyNoExpirationDate(): void
+    {
+        $survey = new \Survey();
+        $survey->active = 'Y';
+        $survey->datecreated = '2023-01-20 18:56:37';
+        $survey->startdate = '2023-02-15 18:56:37';
+
+        $icon = $survey->getRunning();
+
+        $this->assertSame(
+            '<a href="' . App()->createUrl('/surveyAdministration/view/surveyid/' . $this->sid) . '" class="survey-state" data-toggle="tooltip" title="' . gT('End: Never') . '"><span class="fa  fa-play text-success"></span><span class="sr-only">SS' . gT('End: Never') . '</span></a>',
+            $icon,
+            'The correct icon for an active survey with no expiration date was not returned.'
+        );
+    }
 }
