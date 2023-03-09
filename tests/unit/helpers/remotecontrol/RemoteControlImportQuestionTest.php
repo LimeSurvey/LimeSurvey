d9515c19ab9e34153513f14f6eea2b2843413285
Lapiu Dev <devgit@lapiu.biz>
2023-03-09 07:42:18 -0600

    Bug fixed #18071: Importing Question through Remote Control may fail if the imported question already exists.
    Unit tests added.

--- tests/unit/helpers/remotecontrol/RemoteControlImportQuestionTest.php
+++ tests/unit/helpers/remotecontrol/RemoteControlImportQuestionTest.php
@@ -98,7 +98,55 @@ class RemoteControlImportQuestionTest extends TestBaseClass
         $questionFile = self::$surveysFolder . '/limesurvey_question_import_question_test.lsq';
         $question = base64_encode(file_get_contents($questionFile));
         $result = $handler->import_question($sessionKey, self::$surveyId, $testGroupId, $question, 'lsq');
-        
+
         $this->assertIsInt($result, 'There was an error importing a question with a code that already exists.');
+    }
+
+    /**
+     * Importing a question with a text in English and one in Spanish.
+     */
+    public function testImportQuestionWithTextInEnglishAndSpanish()
+    {
+        // Create handler.
+        $admin   = new \AdminController('dummyid');
+        $handler = new \remotecontrol_handle($admin);
+
+        // Get session key.
+        $sessionKey = $handler->get_session_key(
+            self::$username,
+            self::$password
+        );
+
+        // Pickup group id of imported survey.
+        // There is only one group
+        $testGroupId = self::$testSurvey->groups[0]->gid;
+
+        // Attempt Improting Question
+        $questionFile = self::$surveysFolder . '/limesurvey_question_import_question_test_III.lsq';
+        $question = base64_encode(file_get_contents($questionFile));
+        $result = $handler->import_question($sessionKey, self::$surveyId, $testGroupId, $question, 'lsq');
+
+        $questionList = \Question::model()->getQuestionList(self::$surveyId);
+        $questionLangs = end($questionList)->questionl10ns;
+
+        $this->assertSame($questionLangs['en']->question, 'Is this a question in English?');
+        $this->assertSame($questionLangs['es']->question, '¿Esta es una pregunta en Español?');
+    }
+
+    /**
+     * Test if the text for each question was set in all the languages.
+     */
+    public function testTheQuestionTextWasSetInAllSurveyLanguages()
+    {
+
+        $questionList = \Question::model()->getQuestionList(self::$surveyId);
+
+        foreach ($questionList as $question) {
+            $questionLangs = $question->questionl10ns;
+
+            foreach ($questionLangs as $questionLang) {
+                $this->assertNotEmpty($questionLang->question, 'The text for the question in language: ' . $questionLang->language . ' was not properly set.');
+            }
+        }
     }
 }
