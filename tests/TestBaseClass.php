21d39192947b2f0c112a39505319557a4fb2b9e0
Lapiu Dev <devgit@lapiu.biz>
2023-03-16 07:25:14 -0600

    Fixed issue #18436: RC API : Inviting members with invalid language sends them an email but they're not marked as invited.
    Tests added.

--- tests/TestBaseClass.php
+++ tests/TestBaseClass.php
@@ -182,7 +182,7 @@ class TestBaseClass extends TestCase
     public static function dispatchPluginEvent($pluginName, $eventName, $eventValues)
     {
         $oEvent = (new \PluginEvent($eventName));
-        foreach($eventValues as $key => $value) {
+        foreach ($eventValues as $key => $value) {
             $oEvent->set($key, $value);
         }
         \Yii::app()->getPluginManager()->dispatchEvent($oEvent, $pluginName);
@@ -199,10 +199,10 @@ class TestBaseClass extends TestCase
         $oUser = new \User();
         $oUser->setAttributes($userData);
 
-        if(!$oUser->save()) {
-            throw new Exception( 
+        if (!$oUser->save()) {
+            throw new Exception(
                 "Could not save user: "
-                .print_r($oUser->getErrors(),true)
+                . print_r($oUser->getErrors(), true)
             );
         };
 
@@ -255,5 +255,30 @@ class TestBaseClass extends TestCase
             ];
         }
         return $results;
+    }
+
+    /**
+     * @param string $pluginName
+     * @return iPlugin
+     */
+    protected static function loadTestPlugin($pluginName)
+    {
+        require_once self::$dataFolder . "/plugins/{$pluginName}.php";
+        $plugin = \Plugin::model()->findByAttributes(['name' => $pluginName]);
+        if (!$plugin) {
+            $plugin = new \Plugin();
+            $plugin->name = $pluginName;
+            $plugin->active = 1;
+            $plugin->save();
+        } else {
+            $plugin->active = 1;
+            $plugin->save();
+        }
+
+        $plugin = App()->getPluginManager()->loadPlugin($pluginName, $plugin->id);
+        if (is_null($plugin)) {
+            throw new Exception(sprintf('Failed to load test plugin %s', $pluginName));
+        }
+        return $plugin;
     }
 }
