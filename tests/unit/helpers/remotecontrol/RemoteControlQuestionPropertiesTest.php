ac72f3d5af10f579a36c09eaf87bc00625668784
Lapiu Dev <devgit@lapiu.biz>
2023-03-28 10:17:32 -0600

    Fixed issue #18679 API Access to Question Type Array Dual Scale merge scales if answer code is not unique.
    Test modified to check answeroptions_multiscale property.

--- tests/unit/helpers/remotecontrol/RemoteControlQuestionPropertiesTest.php
+++ tests/unit/helpers/remotecontrol/RemoteControlQuestionPropertiesTest.php
@@ -28,42 +28,51 @@ class RemoteControlQuestionPropertiesTest extends BaseTest
 
     public function testGetDualQuestionProperties()
     {
+
+        //Options by scale id.
+        $answerOptionsByScaleId = array(
+            0 => array(
+                1 => array(
+                    'code' => '1',
+                    'answer' => 'Good',
+                    'assessment_value' => '0',
+                    'scale_id' => '0',
+                    'order' => '0'
+                ),
+                2 => array(
+                    'code' => '2',
+                    'answer' => 'Important',
+                    'assessment_value' => '0',
+                    'scale_id' => '0',
+                    'order' => '1'
+                )
+            ),
+            1 => array(
+                1 => array(
+                    'code' => '1',
+                    'answer' => 'Bad',
+                    'assessment_value' => '0',
+                    'scale_id' => '1',
+                    'order' => '2'
+                ),
+                2 => array(
+                    'code' => '2',
+                    'answer' => 'Not important',
+                    'assessment_value' => '0',
+                    'scale_id' => '1',
+                    'order' => '3'
+                )
+            ),
+        );
+
         $sessionKey = $this->handler->get_session_key($this->getUsername(), $this->getPassword());
 
         $question = \Question::model()->findByAttributes(array('title' => 'Q00'));
         $qid = $question->qid;
 
-        $answerOptions = $this->getAnswerOptions($qid, 'en');
-
         $result = $this->handler->get_question_properties($sessionKey, $qid);
         echo PHP_EOL . 'The options are: ';
-        var_dump($answerOptions);
-        $this->assertSame($answerOptions['answeroptions'], $result['answeroptions'], 'The options were not returned correctly.');
-    }
-
-    private function getAnswerOptions($iQuestionID, $sLanguage)
-    {
-        $oAttributes = \Answer::model()->with('answerl10ns')
-        ->findAll(
-            't.qid = :qid and answerl10ns.language = :language',
-            array(':qid' => $iQuestionID, ':language' => $sLanguage),
-            array('order' => 'sortorder')
-        );
-        if (count($oAttributes) > 0) {
-            $aData = array();
-            foreach ($oAttributes as $oAttribute) {
-                $aData[$oAttribute['code']][] = array(
-                    'answer' => array_key_exists($sLanguage, $oAttribute->answerl10ns) ? $oAttribute->answerl10ns[$sLanguage]->answer : '',
-                    'assessment_value' => $oAttribute['assessment_value'],
-                    'scale_id' => $oAttribute['scale_id'],
-                    'order' => $oAttribute['sortorder']
-                );
-            }
-            $aResult['answeroptions'] = $aData;
-        } else {
-            $aResult['answeroptions'] = 'No available answer options';
-        }
-
-        return $aResult;
+        var_dump($result['answeroptions']);
+        $this->assertSame($answerOptionsByScaleId, $result['answeroptions_multiscale'], 'The options were not returned correctly.');
     }
 }
