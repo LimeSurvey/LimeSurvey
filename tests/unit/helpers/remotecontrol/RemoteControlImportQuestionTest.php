82ed018288e57875543488c5f56045b30544feb4
Denis Chenu <denis@sondages.pro>
2023-03-28 10:05:40 +0200

    Dev: add text and help update

--- tests/unit/helpers/remotecontrol/RemoteControlImportQuestionTest.php
+++ tests/unit/helpers/remotecontrol/RemoteControlImportQuestionTest.php
@@ -71,8 +71,44 @@ class RemoteControlImportQuestionTest extends BaseTest
         $testGroupId = self::$testSurvey->groups[0]->gid;
         $questionFile = self::$surveysFolder . '/limesurvey_question_import_question_test.lsq';
         $question = base64_encode(file_get_contents($questionFile));
+        /* must return integer */
         $result = $this->handler->import_question($sessionKey, self::$surveyId, $testGroupId, $question, 'lsq', 'N', 'QNewTitle');
-
         $this->assertIsInt($result, 'There was an error importing a question with a code that already exists and new title is set.');
+        /* Validate is set */
+        $oQuestion = \Question::model()->find(
+            "qid = :qid",
+            array(':qid' => $result)
+        );
+        $this->assertNotEmpty($oQuestion);
+        $this->assertEquals('QNewTitle', $oQuestion->title);
+        /* must return array */
+        $result = $this->handler->import_question($sessionKey, self::$surveyId, $testGroupId, $question, 'lsq', 'N', 'QNewTitle');
+        $this->assertIsArray($result, 'There was an error importing a question set a code that already exists.');
+    }
+
+    /**
+     * Importing a question with a question code that already exists.
+     * But set a new title
+     */
+    public function testImportQuestionWithSetTextAndHelp()
+    {
+        $handler = $this->handler;
+        $sessionKey = $this->handler->get_session_key($this->getUsername(), $this->getPassword());
+
+        /** @var integer the only group id */
+        $testGroupId = self::$testSurvey->groups[0]->gid;
+        $questionFile = self::$surveysFolder . '/limesurvey_question_import_question_test.lsq';
+        $question = base64_encode(file_get_contents($questionFile));
+        $result = $this->handler->import_question($sessionKey, self::$surveyId, $testGroupId, $question, 'lsq', 'N', 'QNewTitle2', 'QNewText', 'QNewHelp');
+
+        $this->assertIsInt($result, 'There was an error importing a question with a code that already exists and new title is set when set text and help.');
+        $oQuestionL10n = \QuestionL10n::model()->find(
+            "qid = :qid and language = :language",
+            array(':qid' => $result, ':language' => "en")
+        );
+        $this->assertNotEmpty($oQuestionL10n);
+        $this->assertEquals('QNewText', $oQuestionL10n->question);
+        $this->assertEquals('QNewHelp', $oQuestionL10n->help);
+
     }
 }
