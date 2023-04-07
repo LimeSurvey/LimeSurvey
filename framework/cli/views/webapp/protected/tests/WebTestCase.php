79a2159fdf61636f626a94ece7526b836516dc42
Denis Chenu <denis@sondages.pro>
2023-04-07 19:13:24 +0200

    Fixed issue #18547: Response browsing on MSSQL server could go to 500 error Fixed issue #18420: Different behaviour on grid between MSSQL and MariaDB Dev: update to last Yii version with the fix Dev: improve other for 8.1

--- framework/cli/views/webapp/protected/tests/WebTestCase.php
+++ framework/cli/views/webapp/protected/tests/WebTestCase.php
@@ -17,7 +17,7 @@ class WebTestCase extends CWebTestCase
 	 * Sets up before each test method runs.
 	 * This mainly sets the base URL for the test application.
 	 */
-	protected function setUp(): void
+	protected function setUp()
 	{
 		parent::setUp();
 		$this->setBrowserUrl(TEST_BASE_URL);
