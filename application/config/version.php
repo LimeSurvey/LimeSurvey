3a0db1cefc8169853fdcee93957e702297ccfba9
Denis Chenu <denis@sondages.pro>
2023-04-12 09:50:26 +0200

    Dev: JS part using select2-bootstrap

--- application/config/version.php
+++ application/config/version.php
@@ -16,5 +16,5 @@ $config['dbversionnumber'] = 499;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
 $config['templateapiversion']  = 3;
-$config['assetsversionnumber'] = '30330';
+$config['assetsversionnumber'] = '30331';
 return $config;
