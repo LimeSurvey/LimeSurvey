a365fa8f2ed2ead66e83266b3dec8ea5c709954a
pstelling <patricia.stelling@limesurvey.org>
2023-04-17 14:03:53 +0200

    dev: created testclass for SurveyUrl written a comment for it

--- tests/unit/models/SurveyUrlServiceTest.php
+++ tests/unit/models/SurveyUrlServiceTest.php
@@ -24,19 +24,28 @@ class SurveyUrlServiceTest extends \ls\tests\TestBaseClass
         $surveyURlService = new SurveyUrl('en',[],false);
         $url = $surveyURlService->getUrl(931272, self::$testSurvey->languagesettings);
         $sPublicUrl = \Yii::app()->getConfig("publicurl");
+        //i would expect the result to be something like
+        // --> "http://hostname/index.php/987852?lang=en"
+        //but when running the test i get something like
+        //when manually testing it in the application (see overview page) everything works as expected ...
+        //seems to me, that calling the service class from outside application folder (here in test or vendor folder)
+        //is the reason for it. Pretty hard to test it then ...
         self::assertEquals('survey_url', $url);
     }
 
     /*
-    public function testOneLanguageUrl(){
+     *
 
+    public function testUrlUsingAlias(){
+    }
+
+    public function testOneLanguageUrl(){
     }
 
     public function testMultipleLanguageUrl(){
-
     }
 
     public function testMultipleLanguageAliasUrl(){
-
-    } */
+    }
+    */
 }
