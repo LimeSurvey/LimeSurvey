8f3f9ee37b814e3aaebcafdae8f962cd46f4d0d2
Carsten Schmitz <c_schmitz@users.sourceforge.net>
2023-04-17 17:37:12 +0200

    Dev Text fix

--- application/views/userManagement/partial/showuser.php
+++ application/views/userManagement/partial/showuser.php
@@ -1,7 +1,7 @@
 <?php
 Yii::app()->getController()->renderPartial(
     '/layouts/partial_modals/modal_header',
-    ['modalTitle' => gT('User detail')]
+    ['modalTitle' => gT('User details')]
 );
 ?>
 
