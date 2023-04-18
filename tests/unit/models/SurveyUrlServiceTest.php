2b220ecdc799c886f9f3641edd8990f1ad9183b9
pstelling <patricia.stelling@limesurvey.org>
2023-04-18 14:50:14 +0200

    dev: finished testclass for SurveyUrl

--- tests/unit/models/SurveyUrlServiceTest.php
+++ tests/unit/models/SurveyUrlServiceTest.php
@@ -3,6 +3,7 @@
 namespace ls\tests;
 
 use LimeSurvey\Models\Services\SurveyUrl;
+use LSYii_Application;
 
 class SurveyUrlServiceTest extends \ls\tests\TestBaseClass
 {
@@ -20,32 +21,72 @@ class SurveyUrlServiceTest extends \ls\tests\TestBaseClass
         parent::tearDownAfterClass();
     }
 
-    public function testSurveyUrl(){
-        $surveyURlService = new SurveyUrl('en',[],false);
-        $url = $surveyURlService->getUrl(931272, self::$testSurvey->languagesettings);
-        $sPublicUrl = \Yii::app()->getConfig("publicurl");
-        //i would expect the result to be something like
-        // --> "http://hostname/index.php/987852?lang=en"
-        //but when running the test i get something like
-        //when manually testing it in the application (see overview page) everything works as expected ...
-        //seems to me, that calling the service class from outside application folder (here in test or vendor folder)
-        //is the reason for it. Pretty hard to test it then ...
-        self::assertEquals('survey_url', $url);
-    }
-
-    /*
+    /**
+     * Compares the parameters at the end.
      *
-
-    public function testUrlUsingAlias(){
+     * Inside the application it should be like:
+     * http://develop/index.php/931272?lang=en
+     * --> http://hostname/index.php/931272?lang=en
+     *
+     * In the test environment it is always like:
+     * http://./vendor/bin/phpunit/931272?lang=en
+     *
+     * @return void
+     */
+    public function testSurveyUrl(){
+        $surveyURlService = new SurveyUrl('en', [], false);
+        $url = $surveyURlService->getUrl(
+            931272,
+            self::$testSurvey->languagesettings
+        );
+        //compare parameters at the end
+        self::assertStringContainsString('931272?lang=en', $url);
     }
 
-    public function testOneLanguageUrl(){
+    /**
+     * Test if alias is used
+     *
+     * @return void
+     */
+    public function testUrlUsingAlias(){
+        /** @var \SurveyLanguageSetting $languageSetting */
+        $languageSetting = self::$testSurvey->languagesettings['en'];
+        $languageSetting->surveyls_alias = 'Hogwarts';
+        $languageSetting->save();
+
+        $surveyURlService = new SurveyUrl('en', []);
+        $url = $surveyURlService->getUrl(
+            931272,
+            self::$testSurvey->languagesettings,
+            self::$testSurvey->getAliasForLanguage()
+        );
+        self::assertStringContainsString('Hogwarts', $url);
     }
 
     public function testMultipleLanguageUrl(){
+        //remove alias
+        $languageSetting = self::$testSurvey->languagesettings['en'];
+        $languageSetting->surveyls_alias = null;
+        $languageSetting->save();
+        $surveyURlService = new SurveyUrl('fr', [], false);
+        $url = $surveyURlService->getUrl(
+            931272,
+            self::$testSurvey->languagesettings
+        );
+        //compare parameters at the end
+        self::assertStringContainsString('931272?lang=fr', $url);
     }
 
     public function testMultipleLanguageAliasUrl(){
+        $languageSetting = self::$testSurvey->languagesettings['en'];
+        $languageSetting->surveyls_alias = 'Hogwarts';
+        $languageSetting->save();
+        $surveyURlService = new SurveyUrl('fr', []);
+        $url = $surveyURlService->getUrl(
+            931272,
+            self::$testSurvey->languagesettings,
+            self::$testSurvey->getAliasForLanguage()
+        );
+        self::assertStringContainsString('Hogwarts?lang=fr', $url);
     }
-    */
 }
