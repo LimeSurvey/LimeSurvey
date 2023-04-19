79cbe3f8c7df684709118aeaad8bd74b9701f438
pstelling <patricia.stelling@limesurvey.org>
2023-04-19 11:15:27 +0200

    dev: added view file for modal

---
+++ application/views/surveyAdministration/partial/topbar/_modalSurveyActivation.php
@@ -0,0 +1,16 @@
+<?php
+   /** @var string $title */
+?>
+
+<?php
+  //modal header
+  $this->renderPartial('\layouts\partial_modals\modal_header', $title);
+?>
+
+<?php
+ //modal content
+?>
+
+<?php
+  //modal footer
+?>
