8d2083db95935d4fcc4a3c5a1b94d9406c9a71da
Carsten Schmitz <carsten.schmitz@limesurvey.org>
2023-04-26 19:36:58 +0200

    Fixed issue: Very slow survey start if alot of quotas exist

--- application/config/version.php
+++ application/config/version.php
@@ -12,7 +12,7 @@
  */
 
 $config['versionnumber'] = '6.0.4';
-$config['dbversionnumber'] = 604;
+$config['dbversionnumber'] = 605;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
 $config['templateapiversion']  = 3;
