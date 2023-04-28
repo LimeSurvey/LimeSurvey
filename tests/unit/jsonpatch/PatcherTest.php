545e5a67c75601121c5a7627440f3435dc9530da
Kevin Foster <kevin.foster.uk@gmail.com>
2023-04-28 17:58:23 +0800

    QE-102_json_patch_lib: add patcher unit tests

--- tests/unit/jsonpatch/PatcherTest.php
+++ tests/unit/jsonpatch/PatcherTest.php
@@ -37,13 +37,68 @@ class PatcherTest extends TestBaseClass
             ->andReturn(new PatternRaw($patch[0]['path']));
         $opHandler->shouldReceive('getGroupByParams');
         $opHandler->shouldReceive('getValueKeyParam');
-        $opHandler->shouldReceive('applyOperation');
+        $opHandler->shouldReceive('applyOperation')
+            ->withArgs([[], $patch[0]['value']]);
 
         $patcher = new Patcher();
         $patcher->addOpHandler($opHandler);
         $operationsApplied = $patcher->applyPatch($patch);
 
         $this->assertEquals(1, $operationsApplied);
+    }
+
+    /**
+     * @testdox applyPatch() passes global params to op handlers
+     */
+    public function testApplyPatchPassesGlobalParamToOpHandlers()
+    {
+        $patch = [
+            [
+                'op' => 'add',
+                'path' => '/hello',
+                'value' => 'World'
+            ]
+        ];
+
+        $opHandler = \Mockery::mock(
+            OpHandlerInterface::class
+        );
+        $opHandler->shouldReceive('getOpType')
+            ->andReturn(new OpTypeAdd);
+        $opHandler->shouldReceive('getPattern')
+            ->andReturn(new PatternRaw($patch[0]['path']));
+        $opHandler->shouldReceive('getGroupByParams');
+        $opHandler->shouldReceive('getValueKeyParam');
+        $opHandler->shouldReceive('applyOperation')
+            ->withArgs([['myGlobalValue' => 123], $patch[0]['value']]);
+
+        $patcher = new Patcher();
+        $patcher->setParams(['myGlobalValue' => 123]);
+        $patcher->addOpHandler($opHandler);
+        $operationsApplied = $patcher->applyPatch($patch);
+
+        $this->assertEquals(1, $operationsApplied);
+    }
+
+    /**
+     * @testdox applyPatch() throws JsonPatchException on missing op handler
+     */
+    public function testApplyPatchThrowsJsonPatchExceptionOnMissingOpHandler()
+    {
+        $this->expectException(JsonPatchException::class);
+
+        $patch = [
+            [
+                'op' => 'add',
+                'path' => '/hello',
+                'value' => 'World'
+            ]
+        ];
+
+        $patcher = new Patcher();
+        $operationsApplied = $patcher->applyPatch($patch);
+
+        $this->assertEquals(0, $operationsApplied);
     }
 
     /**
