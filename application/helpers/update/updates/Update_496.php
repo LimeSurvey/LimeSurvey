a8f94973b70ef059f9a4ae5722cb5804c42f94ae
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2023-05-02 14:08:28 +0200

    Dev: PSR-12 fixes

--- application/helpers/update/updates/Update_496.php
+++ application/helpers/update/updates/Update_496.php
@@ -5,15 +5,13 @@ namespace LimeSurvey\Helpers\Update;
 use Exception;
 
 /**
- * This update adds two new indexes to the quota and quota_members table 
- * to speed up quota usage. In certain situations, this can speed up the 
+ * This update adds two new indexes to the quota and quota_members table
+ * to speed up quota usage. In certain situations, this can speed up the
  * quota usage by a factor of 100.
  * This fix will also be included in the update dbversion 605 for version 6.x.
- *  
+ *
  * @package LimeSurvey\Helpers\Update
- * @
  */
-
 class Update_496 extends DatabaseUpdateBase
 {
     /**
@@ -24,5 +22,4 @@ class Update_496 extends DatabaseUpdateBase
         $this->db->createCommand()->createIndex('{{idx1_quota_id}}', '{{quota_languagesettings}}', ['quotals_quota_id']);
         $this->db->createCommand()->createIndex('{{idx2_quota_id}}', '{{quota_members}}', ['quota_id']);
     }
-
 }
