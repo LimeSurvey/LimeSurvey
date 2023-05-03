4920978b1a27514b0dc3404a15a4bec6a79f2980
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2023-05-03 15:07:48 +0200

    Dev: Raise MINIMUM_PHP_VERSION to 7.4

--- application/models/InstallerConfigForm.php
+++ application/models/InstallerConfigForm.php
@@ -40,7 +40,7 @@ class InstallerConfigForm extends CFormModel
     public const DB_TYPE_ODBC = 'odbc';
 
     public const MINIMUM_MEMORY_LIMIT = 128;
-    public const MINIMUM_PHP_VERSION = '7.2.5';
+    public const MINIMUM_PHP_VERSION = '7.4.0';
 
     // Database
     /** @var string $dbtype */
