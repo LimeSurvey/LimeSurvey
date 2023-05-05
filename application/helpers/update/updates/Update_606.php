3606ea03cf5fea4c549ee4034ba66b802e8215aa
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2023-05-05 15:49:48 +0200

    Dev: PSR-12 fix

--- application/helpers/update/updates/Update_606.php
+++ application/helpers/update/updates/Update_606.php
@@ -7,35 +7,35 @@ class Update_606 extends DatabaseUpdateBase
   /**
    * @inheritDoc
    */
-  public function up()
-  {
-    // $this->db->createCommand()->update(
-    //     '{{surveymenu_entries}}',
-    //     array(
-    //         'menu_link' => 'quotas/index',
-    //     ),
-    //     "name='quotas'"
-    // );
-    $this->db->createCommand()->update(
-      '{{surveymenu_entries}}',
-      [
-        "title" => "Overview question & groups",
-        "menu_title" => "Overview question & groups",
-        "menu_description" => "Overview question and groups",
-      ],
-      'name=:name',
-      [':name' => 'listQuestions']
-    );
+    public function up()
+    {
+      // $this->db->createCommand()->update(
+      //     '{{surveymenu_entries}}',
+      //     array(
+      //         'menu_link' => 'quotas/index',
+      //     ),
+      //     "name='quotas'"
+      // );
+        $this->db->createCommand()->update(
+            '{{surveymenu_entries}}',
+            [
+            "title" => "Overview question & groups",
+            "menu_title" => "Overview question & groups",
+            "menu_description" => "Overview question and groups",
+            ],
+            'name=:name',
+            [':name' => 'listQuestions']
+        );
 
-    $this->db->createCommand()->delete(
-      '{{surveymenu_entries}}',
-      'name=:name',
-      [':name' => 'listQuestionGroups']
-    );
-    $this->db->createCommand()->delete(
-      '{{surveymenu_entries}}',
-      'name=:name',
-      [':name' => 'reorder']
-    );
-  }
+        $this->db->createCommand()->delete(
+            '{{surveymenu_entries}}',
+            'name=:name',
+            [':name' => 'listQuestionGroups']
+        );
+        $this->db->createCommand()->delete(
+            '{{surveymenu_entries}}',
+            'name=:name',
+            [':name' => 'reorder']
+        );
+    }
 }
