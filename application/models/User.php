01e27134801999a0b90f2b608dd02b6f241845dd
Valerio Bozzolan <nsa+gitspam@succhia.cz>
2023-05-17 15:13:02 +0200

    New Feature #18742: Allow "User Admins" to see Users' Roles (#3048)
    This feature helps GDPR lovers.
    
    Before this change, the Users List had some useful columns
    visible only to Super Administrators.
    
    After this change, these columns are visible also to more
    appropriate roles:
    
    - User Roles names:  show to Admins of Users (read)
    - User Group names:  show to Admins of Users+Groups (read)
    - Number of surveys: show to Admins of Users+Surveys (read)
    
    This does not affect abilities to change Roles, Groups, etc.
    
    This does not affect Super Administrators, since when you
    save one Super Admin, it gets all the checks, including the
    ones mentioned above.
    
    Example use case:
    
    Now you can allow a GDPR consultant to have Read-Only access
    in your LimeSurvey Users List, to see Users' Roles, and make
    data protection considerations without unnecessary additional
    privileges: you do not need anymore to elevate your GDPR
    Consultant to "Super Administrator" to do so.
    That GDPR consultant just needs "Users Administrator (Read)".

--- application/models/User.php
+++ application/models/User.php
@@ -694,17 +694,37 @@ class User extends LSActiveRecord
             ],
         ];
 
-        if (Permission::model()->hasGlobalPermission('superadmin', 'read')) {
+        // NOTE: Super Administrators with just the "read" flag also have these flags
+        $permission_read_users      = Permission::model()->hasGlobalPermission('users', 'read');
+        $permission_read_usergroups = Permission::model()->hasGlobalPermission('usergroups', 'read');
+        $permission_read_surveys    = Permission::model()->hasGlobalPermission('surveys', 'read');
+
+        // Number of Surveys
+        // This info is already guessable by people able to list all Surveys
+        if ($permission_read_surveys) {
             $cols[] = array(
                 "name" => 'surveysCreated',
                 "header" => gT("No of surveys"),
                 'filter' => false
             );
+        }
+
+        // Usergroups Names
+        // This info is safe to be shown to who can read all Users and Groups.
+        // TODO: When there will be a more robust Group permissions system,
+        //       this column could be enabled by default, since each Group would
+        //       be checked individually.
+        if ($permission_read_users && $permission_read_usergroups) {
             $cols[] = array(
                 "name" => 'groupList',
                 "header" => gT("Usergroups"),
                 'filter' => false
             );
+        }
+
+        // Role Names
+        // Knowing this info makes sense if you can read all Users
+        if ($permission_read_users) {
             $cols[] = array(
                 "name" => 'roleList',
                 "header" => gT("Applied role"),
