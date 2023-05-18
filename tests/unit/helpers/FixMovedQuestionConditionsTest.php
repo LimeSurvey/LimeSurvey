9145c466836f5ad2eece9f4b040d045556787ea1
Lapiu Dev <devgit@lapiu.biz>
2023-05-18 12:19:08 -0600

    Fixed issue #18725: Reorder questions/question groups can broke survey
    Tests for fixMovedQuestionConditions.

---
+++ tests/unit/helpers/FixMovedQuestionConditionsTest.php
@@ -0,0 +1,203 @@
+<?php
+
+namespace ls\tests;
+
+class FixMovedQuestionConditions extends BaseTest
+{
+    public static function setUpBeforeClass(): void
+    {
+        parent::setupBeforeClass();
+
+        \Yii::import('application.helpers.common_helper', true);
+
+        // Import default survey
+        $filename = self::$surveysFolder . '/survey_fixMovedQuestionConditionsDefault.lss';
+        self::importSurvey($filename);
+
+        \Yii::app()->setConfig('sid', self::$surveyId);
+    }
+
+    public function testFixOneCondition()
+    {
+        $groups = self::$testSurvey->groups;
+        $questions = self::$testSurvey->allQuestions;
+
+        $conditions = \Condition::model()
+                            ->getSomeConditions(
+                                array('cfieldname'),
+                                'cqid = ' . $questions[0]->qid,
+                                null,
+                                null
+                            );
+
+        $firstQuestionConditions = $conditions->readAll();
+
+        $this->assertSame(
+            $firstQuestionConditions[0]['cfieldname'],
+            '265486X' . $groups[0]->gid . 'X' . $questions[0]->qid,
+            'The cfieldname field is not correct.'
+        );
+
+        // Fix conditions for questions moved from group one to group two.
+        fixMovedQuestionConditions($questions[0]->qid, $groups[0]->gid, $groups[1]->gid);
+
+        $fixedConditions = \Condition::model()
+                            ->getSomeConditions(
+                                array('cfieldname'),
+                                'cqid = ' . $questions[0]->qid,
+                                null,
+                                null
+                            );
+
+        $firstQuestionFixedConditions = $fixedConditions->readAll();
+
+        $this->assertSame(
+            $firstQuestionFixedConditions[0]['cfieldname'],
+            '265486X' . $groups[1]->gid . 'X' . $questions[0]->qid,
+            'The cfieldname field is not correct.'
+        );
+    }
+
+    public function testFixMultipleConditions()
+    {
+        $groups = self::$testSurvey->groups;
+        $questions = self::$testSurvey->allQuestions;
+
+        $conditions = \Condition::model()
+                            ->getSomeConditions(
+                                array('cfieldname'),
+                                'cqid = ' . $questions[2]->qid,
+                                null,
+                                null
+                            );
+
+        $questionConditions = $conditions->readAll();
+
+        $expectedCfieldname = '265486X' . $groups[2]->gid . 'X' . $questions[2]->qid;
+
+        foreach ($questionConditions as $condition) {
+            $this->assertSame(
+                $expectedCfieldname,
+                $condition['cfieldname'],
+                'The cfieldname field is not correct.'
+            );
+        }
+
+        // Fix conditions for questions moved from group three to group four.
+        fixMovedQuestionConditions($questions[2]->qid, $groups[2]->gid, $groups[3]->gid);
+
+        $fixedConditions = \Condition::model()
+                            ->getSomeConditions(
+                                array('cfieldname'),
+                                'cqid = ' . $questions[2]->qid,
+                                null,
+                                null
+                            );
+
+        $questionFixedConditions = $fixedConditions->readAll();
+
+        $expectedFixedCfieldname = '265486X' . $groups[3]->gid . 'X' . $questions[2]->qid;
+
+        foreach ($questionFixedConditions as $condition) {
+            $this->assertSame(
+                $expectedFixedCfieldname,
+                $condition['cfieldname'],
+                'The cfieldname field is not correct.'
+            );
+        }
+    }
+
+    public function testFixOneConditionInNewSurvey()
+    {
+        // Import new survey
+        $filename = self::$surveysFolder . '/survey_fixMovedQuestionConditionsNew.lss';
+        self::importSurvey($filename);
+
+        $groups = self::$testSurvey->groups;
+        $questions = self::$testSurvey->allQuestions;
+
+        $conditions = \Condition::model()
+                            ->getSomeConditions(
+                                array('cfieldname'),
+                                'cqid = ' . $questions[3]->qid,
+                                null,
+                                null
+                            );
+
+        $firstQuestionConditions = $conditions->readAll();
+
+        $this->assertSame(
+            $firstQuestionConditions[0]['cfieldname'],
+            '849698X' . $groups[1]->gid . 'X' . $questions[3]->qid,
+            'The cfieldname field is not correct.'
+        );
+
+        // Fix conditions for questions moved from group two to group one.
+        fixMovedQuestionConditions($questions[3]->qid, $groups[1]->gid, $groups[0]->gid, self::$surveyId);
+
+        $fixedConditions = \Condition::model()
+                            ->getSomeConditions(
+                                array('cfieldname'),
+                                'cqid = ' . $questions[3]->qid,
+                                null,
+                                null
+                            );
+
+        $firstQuestionFixedConditions = $fixedConditions->readAll();
+
+        $this->assertSame(
+            $firstQuestionFixedConditions[0]['cfieldname'],
+            '849698X' . $groups[0]->gid . 'X' . $questions[3]->qid,
+            'The cfieldname field is not correct.'
+        );
+    }
+
+    public function testFixMultitpleConditionsInNewSurvey()
+    {
+        $groups = self::$testSurvey->groups;
+        $questions = self::$testSurvey->allQuestions;
+
+        $conditions = \Condition::model()
+                            ->getSomeConditions(
+                                array('cfieldname'),
+                                'cqid = ' . $questions[0]->qid,
+                                null,
+                                null
+                            );
+
+        $questionConditions = $conditions->readAll();
+
+        $expectedCfieldname = '849698X' . $groups[0]->gid . 'X' . $questions[0]->qid;
+
+        foreach ($questionConditions as $condition) {
+            $this->assertSame(
+                $expectedCfieldname,
+                $condition['cfieldname'],
+                'The cfieldname field is not correct.'
+            );
+        }
+
+        // Fix conditions for questions moved from group one to group two.
+        fixMovedQuestionConditions($questions[0]->qid, $groups[0]->gid, $groups[1]->gid, self::$surveyId);
+
+        $fixedConditions = \Condition::model()
+                            ->getSomeConditions(
+                                array('cfieldname'),
+                                'cqid = ' . $questions[0]->qid,
+                                null,
+                                null
+                            );
+
+        $questionFixedConditions = $fixedConditions->readAll();
+
+        $expectedFixedCfieldname = '849698X' . $groups[1]->gid . 'X' . $questions[0]->qid;
+
+        foreach ($questionFixedConditions as $condition) {
+            $this->assertSame(
+                $expectedFixedCfieldname,
+                $condition['cfieldname'],
+                'The cfieldname field is not correct.'
+            );
+        }
+    }
+}
