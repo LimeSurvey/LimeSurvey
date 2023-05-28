14dc559a448566680beca37a07b44c0d6fc868d8
Kevin Foster <kevin.foster.uk@gmail.com>
2023-05-28 12:30:48 +0800

    epic/qe-rest-api: Di should inject static instance when type hinting Yii active record

--- application/libraries/DI.php
+++ application/libraries/DI.php
@@ -20,11 +20,11 @@ class DI
     {
         $container = new \DI\Container;
 
-        // Type hinting on a Yii model / active return class should return its static instance
-        // - We use static method to get a static instance of a Yii Model e.g Survey::model()
-        // - It is not possible to type hint on this, so instead we configure
-        // - the container to return the correct object based on the call time class name
-        // - whenever we type hint on CActiveRecord or anything that extends CActiveRecord
+        // Type hinting on a Yii model / active-record class should return its
+        // - static instance e.g Survey::model(). It is not possible to type hint
+        // - on this, so instead we configure the container to return the correct
+        // - object based on the call time class name whenever we type hint on
+        // - CActiveRecord or anything that extends CActiveRecord
         $container->set('CActiveRecord', function (CActiveRecord $entry) {
             $class = $entry->getName();
             return $class::model();
