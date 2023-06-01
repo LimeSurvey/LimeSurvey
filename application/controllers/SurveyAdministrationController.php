1f41e7e3ea383b6dae62707ca266a18cefcf1f9d
Lapiu Dev <devgit@lapiu.biz>
2023-06-01 09:56:30 -0600

    Fixed issue #18725: Reorder questions/question groups can broke survey
    Group helper and its tests added.

--- application/controllers/SurveyAdministrationController.php
+++ application/controllers/SurveyAdministrationController.php
@@ -2,6 +2,7 @@
 
 use LimeSurvey\Models\Services\CopySurveyResources;
 use LimeSurvey\Models\Services\FilterImportedResources;
+use LimeSurvey\Models\Services\GroupHelper;
 
 /**
  * Class SurveyAdministrationController
@@ -2002,8 +2003,19 @@ class SurveyAdministrationController extends LSBaseController
         }
 
         if ($thereIsPostData) {
-            // Save the new ordering
-            $this->reorderGroup($iSurveyID);
+            // Save the new ordering.
+            $orgdata = $this->getOrgdata();
+
+            $groupHelper = new LimeSurvey\Models\Services\GroupHelper();
+            $result = $groupHelper->reorderGroup($iSurveyID, $orgdata);
+
+            if ($result['type'] === 'success') {
+                App()->setFlashMessage(gT("The new question group/question order was successfully saved."));
+            } elseif ($result['type'] === 'error') {
+                foreach ($result['question-titles'] as $questionTitle) {
+                    App()->setFlashMessage(sprintf(gT("Unable to reorder question %s."), $questionTitle), 'warning');
+                }
+            }
 
             $closeAfterSave = $request->getPost('close-after-save') === 'true';
             if ($closeAfterSave) {
@@ -2526,55 +2538,6 @@ class SurveyAdministrationController extends LSBaseController
         }
 
         return $vars;
-    }
-
-    /**
-     * Reorder groups and questions
-     *
-     * REFACTORED in SurveyAdministration
-     *
-     * @param int $iSurveyID Given Survey ID
-     *
-     * @return void
-     */
-    private function reorderGroup($iSurveyID)
-    {
-        $grouporder = 1;
-        $orgdata = $this->getOrgdata();
-        foreach ($orgdata as $ID => $parent) {
-            if ($parent == 'root' && $ID[0] == 'g') {
-                QuestionGroup::model()->updateAll(
-                    array('group_order' => $grouporder),
-                    'gid=:gid',
-                    array(':gid' => (int) substr($ID, 1))
-                );
-                $grouporder++;
-            } elseif ($ID[0] == 'q') {
-                $qid = (int) substr($ID, 1);
-                $gid = (int) substr($parent, 1);
-                if (!isset($aQuestionOrder[$gid])) {
-                    $aQuestionOrder[$gid] = 0;
-                }
-
-                $sBaseLanguage = Survey::model()->findByPk($iSurveyID)->language;
-                $oQuestion = Question::model()->findByPk(array("qid" => $qid, 'language' => $sBaseLanguage));
-                $oldGid = $oQuestion['gid'];
-                if ($oldGid != $gid) {
-                    fixMovedQuestionConditions($qid, $oldGid, $gid, $iSurveyID);
-                }
-                Question::model()->updateAll(
-                    array(
-                    'question_order' => $aQuestionOrder[$gid],
-                    'gid' => $gid),
-                    'qid=:qid',
-                    array(':qid' => $qid)
-                );
-                Question::model()->updateAll(array('gid' => $gid), 'parent_qid=:parent_qid', array(':parent_qid' => $qid));
-                $aQuestionOrder[$gid]++;
-            }
-        }
-        LimeExpressionManager::SetDirtyFlag(); // so refreshes syntax highlighting
-        Yii::app()->session['flashmessage'] = gT("The new question group/question order was successfully saved.");
     }
 
     /**
---
+++ tests/unit/helpers/GroupHelperTest.php
@@ -0,0 +1,199 @@
+<?php
+
+namespace ls\tests;
+
+class GroupHelperTest extends TestBaseClass
+{
+    public static function setUpBeforeClass(): void
+    {
+        parent::setUpBeforeClass();
+
+        \Yii::app()->setController(new DummyController('dummyid'));
+
+        // Import survey
+        $filename = self::$surveysFolder . '/survey_groupHelper_test.lss';
+        self::importSurvey($filename);
+    }
+
+    /**
+     * Test group order change.
+     */
+    public function testGroupOrderChange()
+    {
+        // Check the original order.
+        $currentOrder = \Yii::app()->db->createCommand()->select('group_order')
+                            ->from('{{groups}}')
+                            ->where('sid=' . self::$surveyId)
+                            ->query()
+                            ->readAll();
+
+        $this->assertEquals(self::$testSurvey->groups[0]->group_order, $currentOrder[0]['group_order'], 'The group id is incorrect.');
+        $this->assertEquals(self::$testSurvey->groups[1]->group_order, $currentOrder[1]['group_order'], 'The group id is incorrect.');
+        $this->assertEquals(self::$testSurvey->groups[2]->group_order, $currentOrder[2]['group_order'], 'The group id is incorrect.');
+
+        // Change group order.
+        $groups = array();
+        $groups[0] = self::$testSurvey->groups[1];
+        $groups[1] = self::$testSurvey->groups[0];
+        $groups[2] = self::$testSurvey->groups[2];
+
+        $orgdata = $this->getOrgData($groups, self::$testSurvey->questions);
+
+        fwrite(STDERR, "groupHelper\n");
+        $groupHelper = new \LimeSurvey\Models\Services\GroupHelper();
+        fwrite(STDERR, "groupHelper->reorderGroup\n");
+        $result = $groupHelper->reorderGroup(self::$surveyId, $orgdata);
+
+        // Check the new order.
+        $changedOrder = \Yii::app()->db->createCommand()->select('group_order')
+                            ->from('{{groups}}')
+                            ->where('sid=' . self::$surveyId)
+                            ->query()
+                            ->readAll();
+
+        $this->assertNotEquals(self::$testSurvey->groups[0]->group_order, $changedOrder[0]['group_order'], 'The group id is incorrect.');
+        $this->assertNotEquals(self::$testSurvey->groups[1]->group_order, $changedOrder[1]['group_order'], 'The group id is incorrect.');
+        $this->assertEquals(self::$testSurvey->groups[2]->group_order, $changedOrder[2]['group_order'], 'The group id is incorrect.');
+
+        // Test result.
+        $this->assertArrayHasKey('type', $result, 'The returned value is not correct.');
+        $this->assertSame('success', $result['type'], 'The returned value is not correct.');
+    }
+
+    /**
+     * Test question order change.
+     * Use questions in the first group.
+     */
+    public function testQuestionOrderChange()
+    {
+        $gid = self::$testSurvey->groups[0]->gid;
+        $firstGroupQuestions = self::$testSurvey->groups[0]->getAllQuestions();
+
+        // Check the original order (Use sql to avoid cache).
+        $currentOrder = \Yii::app()->db->createCommand()->select('question_order')
+                            ->from('{{questions}}')
+                            ->where('gid=' . $gid)
+                            ->query()
+                            ->readAll();
+
+        $this->assertEquals($firstGroupQuestions[0]->question_order, $currentOrder[0]['question_order'], 'The question id is incorrect.');
+        $this->assertEquals($firstGroupQuestions[1]->question_order, $currentOrder[1]['question_order'], 'The question id is incorrect.');
+
+        // Change question order.
+        $questions = array();
+        $questions[] = self::$testSurvey->questions[1];
+        $questions[] = self::$testSurvey->questions[0];
+        $questions[] = self::$testSurvey->questions[2];
+        $questions[] = self::$testSurvey->questions[3];
+        $questions[] = self::$testSurvey->questions[4];
+        $questions[] = self::$testSurvey->questions[5];
+
+        $orgdata = $this->getOrgData(self::$testSurvey->groups, $questions);
+
+        $groupHelper = new \LimeSurvey\Models\Services\GroupHelper();
+        $result = $groupHelper->reorderGroup(self::$surveyId, $orgdata);
+
+        // Check the new order.
+        $currentOrder = \Yii::app()->db->createCommand()->select('question_order')
+                            ->from('{{questions}}')
+                            ->where('gid=' . $gid)
+                            ->query()
+                            ->readAll();
+
+        $this->assertNotEquals($firstGroupQuestions[0]->question_order, $currentOrder[0]['question_order'], 'The question id is incorrect.');
+        $this->assertNotEquals($firstGroupQuestions[1]->question_order, $currentOrder[1]['question_order'], 'The question id is incorrect.');
+
+        // Test result.
+        $this->assertArrayHasKey('type', $result, 'The returned value is not correct.');
+        $this->assertSame('success', $result['type'], 'The returned value is not correct.');
+    }
+
+    /**
+     * Test question group change.
+     * Change Q03 from group two to group three.
+     */
+    public function testQuestionGroupChange()
+    {
+        $gid = self::$testSurvey->groups[2]->gid;
+
+        // Check the original order.
+        $currentOrder = \Yii::app()->db->createCommand()->select('title')
+                            ->from('{{questions}}')
+                            ->where('gid=' . $gid)
+                            ->order('question_order, title ASC')
+                            ->query()
+                            ->readAll();
+
+        // Checking initial order status Q05 Q06 and number of questions on the group.
+        $this->assertCount(2, $currentOrder, 'The number of questions in the group is not correct.');
+        $this->assertSame('Q05', $currentOrder[0]['title'], 'The question title is incorrect.');
+        $this->assertSame('Q06', $currentOrder[1]['title'], 'The question title is incorrect.');
+
+        // Change question group.
+        $orgdata = $this->getOrgData(self::$testSurvey->groups, self::$testSurvey->questions);
+
+        // Changing Q03 to another group.
+        $qid = \Yii::app()->db->createCommand()->select('qid')
+                            ->from('{{questions}}')
+                            ->where('title = "Q03"')
+                            ->andWhere('sid=' . self::$surveyId)
+                            ->query()
+                            ->readAll()[0]['qid'];
+
+        $orgdata['q' . $qid] = 'g' . self::$testSurvey->groups[2]->gid;
+
+        $groupHelper = new \LimeSurvey\Models\Services\GroupHelper();
+        $result = $groupHelper->reorderGroup(self::$surveyId, $orgdata);
+
+        // Check the current order.
+        $currentOrder = \Yii::app()->db->createCommand()->select('title')
+                            ->from('{{questions}}')
+                            ->where('gid=' . $gid)
+                            ->order('question_order, title ASC')
+                            ->query()
+                            ->readAll();
+
+        $this->assertCount(3, $currentOrder, 'The number of questions in the group is not correct.');
+        $this->assertSame('Q03', $currentOrder[0]['title'], 'The question title is incorrect.');
+        $this->assertSame('Q05', $currentOrder[1]['title'], 'The question title is incorrect.');
+        $this->assertSame('Q06', $currentOrder[2]['title'], 'The question title is incorrect.');
+
+        // Test result.
+        $this->assertArrayHasKey('type', $result, 'The returned value is not correct.');
+        $this->assertSame('success', $result['type'], 'The returned value is not correct.');
+    }
+
+    /**
+     * Test group order change on an active survey.
+     */
+    public function testQuestionGroupChangeOnActiveSurvey()
+    {
+        $activator = new \SurveyActivator(self::$testSurvey);
+        $result = $activator->activate();
+
+        $orgdata = $this->getOrgData(self::$testSurvey->groups, self::$testSurvey->questions);
+
+        $groupHelper = new \LimeSurvey\Models\Services\GroupHelper();
+        $result = $groupHelper->reorderGroup(self::$surveyId, $orgdata);
+
+        // Asserting Q03 could not be moved back to group two.
+        $this->assertArrayHasKey('type', $result, 'There is no type key in the response');
+        $this->assertSame('error', $result['type'], 'Apparently, the question was moved from one group to another, this should not happen in active surveys.');
+        $this->assertSame('Q03', $result['question-titles'][0], 'The question title is not the one expected.');
+    }
+
+    private function getOrgData($groups, $questions)
+    {
+        $orgdata = array();
+
+        foreach ($groups as $group) {
+            $orgdata['g' . $group->gid] = 'root';
+        }
+
+        foreach ($questions as $key => $question) {
+            $orgdata['q' . $question->qid] = 'g' . $question->gid;
+        }
+
+        return $orgdata;
+    }
+}
