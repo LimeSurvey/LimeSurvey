270476f6085f85493dfdcb9f460dd22da3d06856
lapiudevgit <devgit@lapiu.biz>
2023-06-02 10:08:10 -0300

    Fixed issue #18593: Float attributes are not supported anymore

---
+++ application/extensions/AdvancedSettingWidget/views/float.php
@@ -0,0 +1,23 @@
+<div class="input-group col-12">
+    <?php if (isset($this->setting['aFormElementOptions']['inputGroup']['prefix'])) : ?>
+        <div class="input-group-addon">
+            <?= $this->setting['aFormElementOptions']['inputGroup']['prefix']; ?>
+        </div>
+    <?php endif; ?>
+    <input
+        type="number" 
+        class="form-control" 
+        name="<?= $inputBaseName ?>"
+        id="<?= CHtml::getIdByName($inputBaseName); ?>"
+        <?= ($this->setting['help']) ? 'aria-describedby="help-' . CHtml::getIdByName($inputBaseName) . '"' : "" ?>
+        value="<?= CHtml::encode($this->setting['value']); ?>"
+        min="<?= $this->setting['min'] ?? ''?>"
+        max="<?= $this->setting['max'] ?? ''?>"
+        pattern="^[-+]?[0-9]*\.[0-9]+$"
+    />
+    <?php if (isset($this->setting['aFormElementOptions']['inputGroup']['suffix'])) : ?>
+        <div class="input-group-addon">
+            <?= $this->setting['aFormElementOptions']['inputGroup']['suffix']; ?>
+        </div>
+    <?php endif; ?>
+</div>
