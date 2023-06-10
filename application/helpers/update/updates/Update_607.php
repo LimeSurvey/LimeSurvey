cc1c80f9d63aef1fffb1b5bda8c634591f1d1b21
Denis Chenu <denis@sondages.pro>
2023-06-10 14:48:30 +0200

    Dev: remove unrelated comments

--- application/helpers/update/updates/Update_607.php
+++ application/helpers/update/updates/Update_607.php
@@ -2,13 +2,6 @@
 
 namespace LimeSurvey\Helpers\Update;
 
-use Exception;
-
-/**
- * Fix organizer link : icon and survey activated
- * @package LimeSurvey\Helpers\Update
- */
-
 class Update_607 extends DatabaseUpdateBase
 {
     /**
