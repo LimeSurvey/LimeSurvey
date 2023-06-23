6a429114436da727cf5721f72a3b686fb58352ed
lapiudevgit <devgit@lapiu.biz>
2023-06-23 11:12:16 -0300

    Fixed issue #18281: Users in group are not deleted

--- application/models/UserGroup.php
+++ application/models/UserGroup.php
@@ -404,7 +404,7 @@ class UserGroup extends LSActiveRecord
             array(
                 'header'      => gT('Owner'),
                 'name'        => 'owner',
-                'value'       => '$data->owner->users_name',
+                'value'       => '$data->owner ? $data->owner->users_name : gT("(Deleted user)")',
                 'htmlOptions' => array('class' => ''),
             ),
 
