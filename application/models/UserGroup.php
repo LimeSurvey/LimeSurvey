7aa39b6a2c42cc7d7c9df218605550cefbf71f28
Patrick Teichmann <patrick.teichmann@limesurvey.org>
2023-06-23 16:28:02 +0200

    Fixed issue #18917: [security] Stored XSS in the user group deletion confirmation popup

--- application/models/UserGroup.php
+++ application/models/UserGroup.php
@@ -347,7 +347,6 @@ class UserGroup extends LSActiveRecord
             'url'              => Yii::app()->createUrl("userGroup/mailToAllUsersInGroup/ugid/$this->ugid"),
             'enabledCondition' => $permissionUsergroupsEdit
         ];
-
         $deletePostData = json_encode(['ugid' => $this->ugid]);
         $dropdownItems[] = [
             'title'            => gT('Delete user group'),
@@ -357,7 +356,7 @@ class UserGroup extends LSActiveRecord
                 'data-bs-toggle' => "modal",
                 'data-post-url'  => App()->createUrl("userGroup/deleteGroup"),
                 'data-post-datas' => $deletePostData,
-                'data-message'   => sprintf(gt("Are you sure you want to delete user group '%s'?"), $this->name),
+                'data-message'   => sprintf(gt("Are you sure you want to delete user group %s?"), sanitize_paranoid_string($this->name)),
                 'data-bs-target' => "#confirmation-modal",
                 'data-btnclass'  => 'btn-danger',
                 'data-btntext'   => gt('Delete'),
