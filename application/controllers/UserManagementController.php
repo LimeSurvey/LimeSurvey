4c80faa96f846992f4250f16b64afb03334ae923
Patrick Teichmann <patrick.teichmann@limesurvey.org>
2023-06-28 14:16:07 +0200

    Fixed issue #18923: [security] Admin notification permissions updated

--- application/controllers/UserManagementController.php
+++ application/controllers/UserManagementController.php
@@ -1144,7 +1144,7 @@ class UserManagementController extends LSBaseController
             )
         ) {
             App()->user->setFlash('error', gT("Access denied"));
-            $this->redirect(App()->request->urlReferrer);
+            $this->redirect(App()->createUrl("userManagement/index"));
         }
 
         $oUser->parent_id = App()->user->id;
