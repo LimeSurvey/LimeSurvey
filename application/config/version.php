09ddd5313a922c42018b2d5e246d50eb83411b46
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2023-07-13 17:25:59 +0200

    Dev: Fix faulty dbversionnumber

--- application/config/version.php
+++ application/config/version.php
@@ -12,7 +12,7 @@
  */
 
 $config['versionnumber'] = '6.1.7';
-$config['dbversionnumber'] = 609;
+$config['dbversionnumber'] = 607;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
 $config['templateapiversion']  = 3;
