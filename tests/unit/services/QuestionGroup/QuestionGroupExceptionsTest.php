9a83e0e9258316837e0a5b1d1601266cd484dcc3
twilligls <tim.willig@limesurvey.org>
2023-07-14 15:08:35 +0200

    dev: Tests for QuestionGroupService (WIP)

---
+++ tests/unit/services/QuestionGroup/QuestionGroupExceptionsTest.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace ls\tests\unit\services\QuestionGroup;
+
+use LimeSurvey\Models\Services\Exception\NotFoundException;
+use LimeSurvey\Models\Services\Exception\PermissionDeniedException;
+use LimeSurvey\Models\Services\QuestionGroupService;
+use ls\tests\TestBaseClass;
+use Mockery;
+use Permission;
+use QuestionGroup;
+
+class QuestionGroupExceptionsTest extends TestBaseClass
+{
+    /**
+     * @testdox updateGroup() throws PermissionDeniedException
+     */
+    public function testUpdateThrowsExceptionPermissionDenied()
+    {
+        $this->expectException(
+            PermissionDeniedException::class
+        );
+        $questionGroupService = $this->getMockedServiceForPermissionDeniedException();
+        $questionGroupService->updateGroup(1, 1, []);
+    }
+
+    public function testCreateThrowsExceptionPermissionDenied()
+    {
+        $this->expectException(
+            PermissionDeniedException::class
+        );
+        $questionGroupService = $this->getMockedServiceForPermissionDeniedException();
+        $questionGroupService->createGroup(1, []);
+    }
+
+    public function testDeleteThrowsExceptionPermissionDenied()
+    {
+        $this->expectException(
+            PermissionDeniedException::class
+        );
+        $questionGroupService = $this->getMockedServiceForPermissionDeniedException();
+        $questionGroupService->deleteGroup(1, 1);
+    }
+
+    public function testUpdateThrowsExceptionNotFound()
+    {
+        $this->expectException(
+            NotFoundException::class
+        );
+
+        $questionGroupService = $this->getMockedServiceForQuestionGroupNotFoundException();
+        $questionGroupService->updateGroup(1, 1, []);
+    }
+
+    public function testGetQuestionGroupObjectThrowsExceptionNotFound()
+    {
+        $this->expectException(
+            NotFoundException::class
+        );
+
+        $questionGroupService = $this->getMockedServiceForQuestionGroupNotFoundException();
+        $questionGroupService->getQuestionGroupObject(1, 1);
+    }
+
+    public function testCreateThrowsExceptionNotFound()
+    {
+        $this->expectException(
+            NotFoundException::class
+        );
+
+        $questionGroupService = $this->getMockedServiceForSurveyNotFoundException();
+        $questionGroupService->createGroup(1, 1, []);
+    }
+
+    private function getMockedServiceForPermissionDeniedException(): QuestionGroupService
+    {
+        $mockSetFactory = new QuestionGroupMockSetFactory();
+
+        $mockSet = $mockSetFactory->make();
+        $mockSet->modelPermission = $mockSetFactory->getMockModelNoSurveyPermission();
+
+        return (new QuestionGroupFactory())->make($mockSet);
+    }
+
+    private function getMockedServiceForSurveyNotFoundException(): QuestionGroupService
+    {
+        $mockSetFactory = new QuestionGroupMockSetFactory();
+
+        $mockSet = $mockSetFactory->make();
+        $mockSet->modelSurvey = $mockSetFactory->getMockModelForSurveyNotFound();
+
+        return (new QuestionGroupFactory())->make($mockSet);
+    }
+
+    private function getMockedServiceForQuestionGroupNotFoundException(): QuestionGroupService
+    {
+        $mockSetFactory = new QuestionGroupMockSetFactory();
+
+        $mockSet = $mockSetFactory->make();
+        $mockSet->modelQuestionGroup = $mockSetFactory->getMockModelForQuestionGroupNotFound();
+
+        return (new QuestionGroupFactory())->make($mockSet);
+    }
+
+}
---
+++ tests/unit/services/QuestionGroup/QuestionGroupMockSetFactory.php
@@ -0,0 +1,202 @@
+<?php
+
+namespace ls\tests\unit\services\QuestionGroup;
+
+use LimeSurvey\Models\Services\Proxy\ProxyExpressionManager;
+use LimeSurvey\Models\Services\Proxy\ProxyQuestionGroup;
+use LSYii_Application;
+use Mockery;
+use Permission;
+use Question;
+use QuestionGroup;
+use QuestionGroupL10n;
+use Survey;
+
+class QuestionGroupMockSetFactory
+{
+    /**
+     * @param QuestionGroupMockSet|null $init
+     * @return QuestionGroupMockSet
+     */
+    public function make(QuestionGroupMockSet $init = null)
+    {
+        $mockSet = new QuestionGroupMockSet();
+
+        $mockSet->modelPermission = ($init && isset($init->modelPermission))
+            ? $init->modelPermission
+            : $this->getMockModelPermission();
+
+        $mockSet->survey = ($init && isset($init->survey))
+            ? $init->survey
+            : $this->getMockSurvey();
+
+        $mockSet->modelSurvey = ($init && isset($init->modelSurvey))
+            ? $init->modelSurvey
+            : $this->getMockModelSurvey($mockSet->survey);
+
+        $mockSet->modelQuestion = ($init && isset($init->modelQuestion))
+            ? $init->modelQuestion
+            : $this->getMockModelQuestion();
+
+        $mockSet->questionGroup = ($init && isset($init->questionGroup))
+            ? $init->questionGroup
+            : $this->getMockQuestionGroup();
+
+        $mockSet->modelQuestionGroup = ($init && isset($init->modelQuestionGroup))
+            ? $init->modelQuestionGroup
+            : $this->getMockModelQuestionGroup($mockSet->questionGroup);
+
+        $mockSet->modelQuestionGroupL10n = ($init && isset($init->modelQuestionGroupL10n))
+            ? $init->modelQuestionGroupL10n
+            : $this->getMockModelQuestionGroupL10n($mockSet->modelQuestionGroup);
+
+        $mockSet->proxyExpressionManager = ($init && isset($init->proxyExpressionManager))
+            ? $init->proxyExpressionManager
+            : $this->getMockProxyExpressionManager();
+
+        $mockSet->proxyQuestionGroup = ($init && isset($init->proxyQuestionGroup))
+            ? $init->proxyQuestionGroup
+            : $this->getMockProxyQuestionGroup();
+
+        $mockSet->yiiApp = ($init && isset($init->yiiApp))
+            ? $init->yiiApp
+            : $this->getMockYiiApp();
+
+        return $mockSet;
+    }
+
+    public function getMockModelNoSurveyPermission(): Permission
+    {
+        $modelPermission = Mockery::mock(Permission::class)
+            ->makePartial();
+        $modelPermission->shouldReceive('hasSurveyPermission')
+            ->andReturn(false);
+
+        return $modelPermission;
+    }
+
+    public function getMockModelForSurveyNotFound(): Survey
+    {
+        $modelSurvey = Mockery::mock(Survey::class)
+            ->makePartial();
+        $modelSurvey->shouldReceive('findByPk')
+            ->andReturn(null);
+
+        return $modelSurvey;
+    }
+
+    public function getMockModelForQuestionGroupNotFound(): QuestionGroup
+    {
+        $modelQuestionGroup = Mockery::mock(QuestionGroup::class)
+            ->makePartial();
+        $modelQuestionGroup->shouldReceive('findByPk')
+            ->andReturn(null);
+
+        return $modelQuestionGroup;
+    }
+
+    private function getMockModelPermission(): Permission
+    {
+        $modelPermission = Mockery::mock(Permission::class)
+            ->makePartial();
+        $modelPermission->shouldReceive('hasSurveyPermission')
+            ->andReturn(true);
+
+        return $modelPermission;
+    }
+
+    private function getMockSurvey(): Survey
+    {
+        $survey = Mockery::mock(Survey::class)
+            ->makePartial();
+        $survey->shouldReceive('save')
+            ->andReturn(true);
+        $survey->shouldReceive('setAttributes')
+            ->passthru();
+        $survey->setAttributes([]);
+        $survey->shouldReceive('getAttributes')
+            ->passthru();
+        $survey->getAttributes([]);
+
+        return $survey;
+    }
+
+    private function getMockModelSurvey(Survey $survey): Survey
+    {
+        $modelSurvey = Mockery::mock(Survey::class)
+            ->makePartial();
+        $modelSurvey->shouldReceive('findByPk')
+            ->andReturn($survey);
+
+        return $modelSurvey;
+    }
+
+    private function getMockModelQuestion(): Question
+    {
+        $modelQuestion = Mockery::mock(Question::class)
+            ->makePartial();
+
+        //functions?
+
+        return $modelQuestion;
+    }
+
+    private function getMockModelQuestionGroup($questionGroup): QuestionGroup
+    {
+        $modelQuestionGroup = Mockery::mock(QuestionGroup::class)
+            ->makePartial();
+
+        $modelQuestionGroup->shouldReceive('findByPk')
+            ->andReturn($questionGroup);
+
+        return $modelQuestionGroup;
+    }
+
+    private function getMockQuestionGroup(): Survey
+    {
+        $survey = Mockery::mock(Survey::class)
+            ->makePartial();
+        $survey->shouldReceive('save')
+            ->andReturn(true);
+        $survey->shouldReceive('setAttributes')
+            ->passthru();
+        $survey->setAttributes([]);
+        $survey->shouldReceive('getAttributes')
+            ->passthru();
+        $survey->getAttributes([]);
+
+        return $survey;
+    }
+
+    private function getMockModelQuestionGroupL10n(QuestionGroup $modelQuestionGroup): QuestionGroupL10n
+    {
+        $modelQuestionGroupL10n = Mockery::mock(QuestionGroupL10n::class)
+            ->makePartial();
+
+        //functions?
+
+        return $modelQuestionGroupL10n;
+    }
+
+    private function getMockProxyExpressionManager(): ProxyExpressionManager
+    {
+        $proxyExpressionManager = Mockery::mock(ProxyExpressionManager::class)
+            ->makePartial();
+
+        return $proxyExpressionManager;
+    }
+
+    private function getMockProxyQuestionGroup(): ProxyQuestionGroup
+    {
+        $proxyQuestionGroup = Mockery::mock(ProxyQuestionGroup::class)
+            ->makePartial();
+
+        return $proxyQuestionGroup;
+    }
+
+    private function getMockYiiApp(): LSYii_Application
+    {
+        return Mockery::mock(LSYii_Application::class)
+            ->makePartial();
+    }
+}
