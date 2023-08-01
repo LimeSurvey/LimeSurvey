d284e03b4011f785d768821e2e472f7d89968c44
Lajos Arpad <arpad@endpoint.com>
2023-08-01 21:23:07 +0300

    MX-446: Created basic feature for using the file

--- application/views/admin/super/layout_main.php
+++ application/views/admin/super/layout_main.php
@@ -29,6 +29,42 @@ $containerClass = !Yii::app()->user->isGuest ? 'container-fluid full-page-wrappe
 echo '<!-- Full page, started in SurveyCommonAction::renderWrappedTemplate() -->
 <div class="full-page-wrapper ' . $containerClass . '" id="in_survey_common_action">';
 
+if (Yii::app()->session['templatetoken'] ?? null) {
+    Yii::import('application.helpers.admin.token_helper', true);
+    $filename = decodeFilename(Yii::app()->session['templatetoken']);
+    ?>
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
+    <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
+    <script>
+        function removeTemplateToken() {
+            alert("Kivertek a franciakat Burkina faszabol");
+        }
+    </script>
+    <div id="dialog" title="Import Template?">
+        <?php echo "Shall we import the template file of {$filename}?" ?>
+    </div>
+    <script>
+        jQuery(function() {
+            $("#dialog").dialog({
+                open: function() {
+                    $(this).closest(".ui-dialog")
+                    .find(".ui-dialog-titlebar-close")
+                    .removeClass("ui-dialog-titlebar-close")
+                    .html("<span class=\'ui-button-icon-primary ui-icon ui-icon-closethick\' id=\'dialog-close\'></span>");
+                    $(this).parent().find(".ui-dialog-title").css("width", "calc(100% - 32px)");
+                },
+                buttons: {
+                    Yes: function() {},
+                    No: function() {
+                        jQuery('#dialog-close').click();
+                    }
+                }
+            });
+        })
+    </script>'
+    <?php
+}
+
 echo $content;
 
 echo '</div>';
