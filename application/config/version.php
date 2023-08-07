83918f3ed294e0de5affb3959ee2384316508508
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2023-08-07 13:41:21 +0200

    Set version to 6.3.0-dev

--- application/config/version.php
+++ application/config/version.php
@@ -11,7 +11,7 @@
  * See COPYRIGHT.php for copyright notices and details.
  */
 
-$config['versionnumber'] = '6.2.1';
+$config['versionnumber'] = '6.3.0-dev';
 $config['dbversionnumber'] = 612;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
