7907058ed0ef3376c207a64c24006894a6ed88eb
jack-han-115 <lovelymouse115@gmail.com>
2023-08-15 11:44:34 +0800

    refactor update code

--- application/helpers/update/updates/Update_613.php
+++ application/helpers/update/updates/Update_613.php
@@ -9,245 +9,41 @@ class Update_613 extends DatabaseUpdateBase
      */
     public function up()
     {
+        $fixedIcons = [
+            'overview' => 'ri-bar-chart-horizontal-line',
+            'generalsettings' => 'ri-tools-line',
+            'surveytexts' => 'ri-text-spacing',
+            'datasecurity' => 'ri-shield-line',
+            'theme_options' => 'ri-slideshow-line',
+            'tokens' => 'ri-body-scan-fill',
+            'notification' => 'ri-notification-line',
+            'publication' => 'ri-key-line',
+            'surveypermissions' => 'ri-lock-password-line',
+            'listQuestions' => '',
+            'listQuestionGroups' => '',
+            'reorder' => '',
+            'participants' => '',
+            'emailtemplates' => '',
+            'failedemail' => '',
+            'quotas' => '',
+            'assessments' => '',
+            'panelintegration' => '',
+            'responses' => '',
+            'statistics' => '',
+            'resources' => '',
+            'plugins' => '',
+        ];
 
-        $this->db->createCommand()->update(
-            '{{surveymenu_entries}}',
-            [
-                "menu_icon_type" => "remix",
-                "menu_icon" => "ri-bar-chart-horizontal-line",
-            ],
-            'name=:name',
-            [':name' => 'overview']
-        );
-
-        $this->db->createCommand()->update(
-            '{{surveymenu_entries}}',
-            [
-                "menu_icon_type" => "remix",
-                "menu_icon" => "ri-tools-line",
-            ],
-            'name=:name',
-            [':name' => 'generalsettings']
-        );
-
-        $this->db->createCommand()->update(
-            '{{surveymenu_entries}}',
-            [
-                "menu_icon_type" => "remix",
-                "menu_icon" => "ri-text-spacing",
-            ],
-            'name=:name',
-            [':name' => 'surveytexts']
-        );
-
-        $this->db->createCommand()->update(
-            '{{surveymenu_entries}}',
-            [
-                "menu_icon_type" => "remix",
-                "menu_icon" => "ri-shield-line",
-            ],
-            'name=:name',
-            [':name' => 'datasecurity']
-        );
-
-        $this->db->createCommand()->update(
-            '{{surveymenu_entries}}',
-            [
-                "menu_icon_type" => "remix",
-                "menu_icon" => "ri-contrast-drop-fill",
-            ],
-            'name=:name',
-            [':name' => 'theme_options']
-        );
-
-        $this->db->createCommand()->update(
-            '{{surveymenu_entries}}',
-            [
-                "menu_icon_type" => "remix",
-                "menu_icon" => "ri-slideshow-line",
-            ],
-            'name=:name',
-            [':name' => 'presentation']
-        );
-
-        $this->db->createCommand()->update(
-            '{{surveymenu_entries}}',
-            [
-                "menu_icon_type" => "remix",
-                "menu_icon" => "ri-body-scan-fill",
-            ],
-            'name=:name',
-            [':name' => 'tokens']
-        );
-
-        $this->db->createCommand()->update(
-            '{{surveymenu_entries}}',
-            [
-                "menu_icon_type" => "remix",
-                "menu_icon" => "ri-notification-line",
-            ],
-            'name=:name',
-            [':name' => 'notification']
-        );
-
-        $this->db->createCommand()->update(
-            '{{surveymenu_entries}}',
-            [
-                "menu_icon_type" => "remix",
-                "menu_icon" => "ri-key-line",
-            ],
-            'name=:name',
-            [':name' => 'publication']
-        );
-
-        $this->db->createCommand()->update(
-            '{{surveymenu_entries}}',
-            [
-                "menu_icon_type" => "remix",
-                "menu_icon" => "ri-lock-password-line",
-            ],
-            'name=:name',
-            [':name' => 'surveypermissions']
-        );
-
-        $this->db->createCommand()->update(
-            '{{surveymenu_entries}}',
-            [
-                "menu_icon_type" => "remix",
-                "menu_icon" => "",
-            ],
-            'name=:name',
-            [':name' => 'listQuestions']
-        );
-
-        $this->db->createCommand()->update(
-            '{{surveymenu_entries}}',
-            [
-                "menu_icon_type" => "remix",
-                "menu_icon" => "",
-            ],
-            'name=:name',
-            [':name' => 'listQuestions']
-        );
-
-        $this->db->createCommand()->update(
-            '{{surveymenu_entries}}',
-            [
-                "menu_icon_type" => "remix",
-                "menu_icon" => "",
-            ],
-            'name=:name',
-            [':name' => 'listQuestionGroups']
-        );
-
-        $this->db->createCommand()->update(
-            '{{surveymenu_entries}}',
-            [
-                "menu_icon_type" => "remix",
-                "menu_icon" => "",
-            ],
-            'name=:name',
-            [':name' => 'reorder']
-        );
-
-        $this->db->createCommand()->update(
-            '{{surveymenu_entries}}',
-            [
-                "menu_icon_type" => "remix",
-                "menu_icon" => "",
-            ],
-            'name=:name',
-            [':name' => 'participants']
-        );
-
-        $this->db->createCommand()->update(
-            '{{surveymenu_entries}}',
-            [
-                "menu_icon_type" => "remix",
-                "menu_icon" => "",
-            ],
-            'name=:name',
-            [':name' => 'emailtemplates']
-        );
-
-        $this->db->createCommand()->update(
-            '{{surveymenu_entries}}',
-            [
-                "menu_icon_type" => "remix",
-                "menu_icon" => "",
-            ],
-            'name=:name',
-            [':name' => 'failedemail']
-        );
-
-        $this->db->createCommand()->update(
-            '{{surveymenu_entries}}',
-            [
-                "menu_icon_type" => "remix",
-                "menu_icon" => "",
-            ],
-            'name=:name',
-            [':name' => 'quotas']
-        );
-
-        $this->db->createCommand()->update(
-            '{{surveymenu_entries}}',
-            [
-                "menu_icon_type" => "remix",
-                "menu_icon" => "",
-            ],
-            'name=:name',
-            [':name' => 'assessments']
-        );
-
-        $this->db->createCommand()->update(
-            '{{surveymenu_entries}}',
-            [
-                "menu_icon_type" => "remix",
-                "menu_icon" => "",
-            ],
-            'name=:name',
-            [':name' => 'panelintegration']
-        );
-
-        $this->db->createCommand()->update(
-            '{{surveymenu_entries}}',
-            [
-                "menu_icon_type" => "remix",
-                "menu_icon" => "",
-            ],
-            'name=:name',
-            [':name' => 'responses']
-        );
-
-        $this->db->createCommand()->update(
-            '{{surveymenu_entries}}',
-            [
-                "menu_icon_type" => "remix",
-                "menu_icon" => "",
-            ],
-            'name=:name',
-            [':name' => 'statistics']
-        );
-
-        $this->db->createCommand()->update(
-            '{{surveymenu_entries}}',
-            [
-                "menu_icon_type" => "remix",
-                "menu_icon" => "",
-            ],
-            'name=:name',
-            [':name' => 'resources']
-        );
-
-        $this->db->createCommand()->update(
-            '{{surveymenu_entries}}',
-            [
-                "menu_icon_type" => "remix",
-                "menu_icon" => "",
-            ],
-            'name=:name',
-            [':name' => 'plugins']
-        );
+        foreach ($fixedIcons as $entryName => $newIcon) {
+            $this->db->createCommand()->update(
+                '{{surveymenu_entries}}',
+                [
+                    "menu_icon_type" => "remix",
+                    "menu_icon" => $newIcon,
+                ],
+                'name=:name',
+                [':name' => $entryName]
+            );
+        }
     }
 }
