e444873831f754553f7c3240961cab4fe97e70f0
Lapiu Dev <devgit@lapiu.biz>
2023-08-16 11:02:50 -0600

    Fixed bug #18627 Missing SQL query errors in admin mail (SQL CODE THAT FAILED)
    Tests added.

---
+++ tests/unit/models/SurveyDynamicTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace ls\tests;
+
+use Yii;
+
+class SurveyDynamicTest extends TestBaseClass
+{
+    public static function setUpBeforeClass(): void
+    {
+        parent::setupBeforeClass();
+
+        // Import survey.
+        $filename = self::$surveysFolder . '/limesurvey_survey_161359_quickTranslation.lss';
+        self::importSurvey($filename);
+
+        // Activate survey.
+        $activator = new \SurveyActivator(self::$testSurvey);
+        $activator->activate();
+    }
+
+    public static function tearDownAfterClass(): void
+    {
+        // Expire survey and delete response table.
+        self::$testSurvey->expire();
+        $responseTableName = \SurveyDynamic::model(self::$surveyId)->tableName();
+        \Yii::app()->db->createCommand()->dropTable($responseTableName);
+
+        parent::tearDownAfterClass();
+    }
+
+    /**
+     * Testing that a new response can be correctly inserted.
+     */
+    public function testInsertResponse()
+    {
+        $responseId = \SurveyDynamic::model(self::$surveyId)->insertRecords(array('startlanguage' => 'en'));
+        $response = \SurveyDynamic::model()->findByPk($responseId);
+
+        $this->assertIsNumeric($responseId, 'The newly inserted response id should have been returned.');
+        $this->assertInstanceOf('SurveyDynamic', $response, 'The newly inserted response should have been returned.');
+    }
+
+    /**
+     * Testing that an exception is thrown when
+     * response insertion fails.
+     */
+    public function testErrorInsertingResponse()
+    {
+        $this->expectException(\CException::class);
+
+        // Table column name incorrectly spelled.
+        $responseId = \SurveyDynamic::model(self::$surveyId)->insertRecords(array('starlanguage' => 'en'));
+    }
+}
