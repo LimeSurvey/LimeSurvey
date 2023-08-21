d597504d9637360cdca4d7d96c12f52f2ca8d812
Lajos Arpad <arpad@endpoint.com>
2023-08-21 15:46:34 +0300

    MW-543: Implemented adjustments to make sure that the command is compatible with the client engine

--- application/commands/ImportSurveyCommand.php
+++ application/commands/ImportSurveyCommand.php
@@ -8,13 +8,21 @@ Yii::import('application.helpers.admin.import_helper', true);
 class ImportSurveyCommand extends CConsoleCommand
 {
 
+    const ALLOWED_MAIN_PATHS = [
+        'tmp' .DIRECTORY_SEPARATOR . 'upload',
+    ];
+
     /**
+     * @param string $path
      * @param string $filename
+     * @param string $furtherParams JSON representation
      * 
-     * Sample command: php application/commands/console.php importsurvey import-file abcf.lss
-     *                 php application/commands/console.php importsurvey import-file limesurvey_survey_979573.lss '{"bTranslateLinkFields": true, "sNewSurveyName": "Orsson", "DestSurveyID": 666999}'
+     * @return void
+     * 
+     * Sample command: php application/commands/console.php importsurvey import-file tmp/upload/youfile.lss
+     *                 php application/commands/console.php importsurvey import-file tmp/upload/youfile.lss '{"bTranslateLinkFields": true, "sNewSurveyName": "Orsson", "DestSurveyID": 666999}'
      */
-    protected function importFile($filename, $furtherParams)
+    protected function importFile($path, $filename, $furtherParams)
     {
         $params = [
             "bTranslateLinkFields" => false,
@@ -26,8 +34,8 @@ class ImportSurveyCommand extends CConsoleCommand
                 $params[$key] = $json[$key] ?? $params[$key];
             }
         }
-        importSurveyFile(
-            Yii::app()->getConfig('tempdir') . DIRECTORY_SEPARATOR . "templates" . DIRECTORY_SEPARATOR . $filename,
+        return importSurveyFile(
+            $path . DIRECTORY_SEPARATOR . $filename,
             $params["bTranslateLinkFields"],
             $params["sNewSurveyName"],
             $params["DestSurveyID"],
@@ -46,14 +54,25 @@ class ImportSurveyCommand extends CConsoleCommand
         switch ($command) {
             case "import-file": {
                 if (count($sArgument) < 2) {
-                    throw new Exception("You need to specify the file to import from");
+                    throw new Exception("You need to specify the import path");
                 }
-                $filename = $sArgument[1];
-                if (!preg_match('/^[a-zA-Z0-9_\.]*$/', $filename)) {
+                $fullPath = $sArgument[1];
+
+                if (($lastSlashPosition = strripos($fullPath, DIRECTORY_SEPARATOR)) === false) {
+                    throw new Exception("Invalid path");
+                }
+                $path = substr($fullPath, 0, $lastSlashPosition);
+                $filename = substr($fullPath, $lastSlashPosition + 1);
+                if (!preg_match('/^[a-zA-Z0-9_\. ]*$/', $filename)) {
                     throw new Exception("Your filename can only contain letters, digits and dot");
                 }
                 $furtherParams = $sArgument[2] ?? null;
-                $this->importFile($filename, $furtherParams);
+                $result = $this->importFile($path, $filename, $furtherParams);
+                if (is_array($result) && isset($result['newsid'])) {
+                    echo $result['newsid'];
+                } else {
+                    echo "something went wrong";
+                }
             } break;
             default: throw new Exception("Unsupported command");
         }
