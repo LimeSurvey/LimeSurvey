80b2e9700fa921a352aaa42e85e1ae0e508c07ea
Lajos Arpad <arpad@endpoint.com>
2023-08-21 17:54:23 +0300

    MW-543: Fixed texts to be translatable

--- application/commands/ImportSurveyCommand.php
+++ application/commands/ImportSurveyCommand.php
@@ -48,33 +48,33 @@ class ImportSurveyCommand extends CConsoleCommand
     public function run($sArgument)
     {
         if (!count($sArgument)) {
-            throw new Exception("You need to specify the command to be executed");
+            throw new Exception(gT("You need to specify the command to be executed"));
         }
         $command = $sArgument[0];
         switch ($command) {
             case "import-file": {
                 if (count($sArgument) < 2) {
-                    throw new Exception("You need to specify the import path");
+                    throw new Exception(gT("You need to specify the import path"));
                 }
                 $fullPath = $sArgument[1];
 
                 if (($lastSlashPosition = strripos($fullPath, DIRECTORY_SEPARATOR)) === false) {
-                    throw new Exception("Invalid path");
+                    throw new Exception(gT("Invalid path"));
                 }
                 $path = substr($fullPath, 0, $lastSlashPosition);
                 $filename = substr($fullPath, $lastSlashPosition + 1);
                 if (!preg_match('/^[a-zA-Z0-9_\. ]*$/', $filename)) {
-                    throw new Exception("Your filename can only contain letters, digits and dot");
+                    throw new Exception(gT("Your filename can only contain letters, digits and dot"));
                 }
                 $furtherParams = $sArgument[2] ?? null;
                 $result = $this->importFile($path, $filename, $furtherParams);
                 if (is_array($result) && isset($result['newsid'])) {
                     echo $result['newsid'];
                 } else {
-                    echo "something went wrong";
+                    echo gT("something went wrong");
                 }
             } break;
-            default: throw new Exception("Unsupported command");
+            default: throw new Exception(gT("Unsupported command"));
         }
     }
 }
