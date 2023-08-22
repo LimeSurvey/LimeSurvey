dd6bc4b1e7555193f6f8e19273b18deb9207114b
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2023-08-22 11:33:27 +0200

    Simplify command a bit

--- application/commands/ImportSurveyCommand.php
+++ application/commands/ImportSurveyCommand.php
@@ -7,35 +7,20 @@ Yii::import('application.helpers.admin.import_helper', true);
 
 class ImportSurveyCommand extends CConsoleCommand
 {
-
-    const ALLOWED_MAIN_PATHS = [
-        'tmp' .DIRECTORY_SEPARATOR . 'upload',
-    ];
-
     /**
-     * @param string $path
      * @param string $filename
-     * @param string $furtherParams JSON representation
-     * 
-     * @return void
-     * 
-     * Sample command: php application/commands/console.php importsurvey import-file tmp/upload/youfile.lss
-     *                 php application/commands/console.php importsurvey import-file tmp/upload/youfile.lss '{"bTranslateLinkFields": true, "sNewSurveyName": "Orsson", "DestSurveyID": 666999}'
+     * @return array Import result
      */
-    protected function importFile($path, $filename, $furtherParams)
+    protected function importFile($filename)
     {
+        // TODO: Add support to customize these.
         $params = [
             "bTranslateLinkFields" => false,
             "sNewSurveyName" => null,
             "DestSurveyID" => null,
         ];
-        if ($json = json_decode($furtherParams ?? "{}", true)) {
-            foreach ($params as $key => $value) {
-                $params[$key] = $json[$key] ?? $params[$key];
-            }
-        }
         return importSurveyFile(
-            $path . DIRECTORY_SEPARATOR . $filename,
+            $filename,
             $params["bTranslateLinkFields"],
             $params["sNewSurveyName"],
             $params["DestSurveyID"],
@@ -43,38 +28,19 @@ class ImportSurveyCommand extends CConsoleCommand
     }
 
     /**
-     * @param array $aArguments
+     * Sample command: php application/commands/console.php importsurvey tmp/upload/youfile.lss
+     *
+     * @param string[] $arguments
+     * @return void
      */
-    public function run($sArgument)
+    public function run($arguments)
     {
-        if (!count($sArgument)) {
-            throw new Exception(gT("You need to specify the command to be executed"));
-        }
-        $command = $sArgument[0];
-        switch ($command) {
-            case "import-file": {
-                if (count($sArgument) < 2) {
-                    throw new Exception(gT("You need to specify the import path"));
-                }
-                $fullPath = $sArgument[1];
-
-                if (($lastSlashPosition = strripos($fullPath, DIRECTORY_SEPARATOR)) === false) {
-                    throw new Exception(gT("Invalid path"));
-                }
-                $path = substr($fullPath, 0, $lastSlashPosition);
-                $filename = substr($fullPath, $lastSlashPosition + 1);
-                if (!preg_match('/^[a-zA-Z0-9_\. ]*$/', $filename)) {
-                    throw new Exception(gT("Your filename can only contain letters, digits and dot"));
-                }
-                $furtherParams = $sArgument[2] ?? null;
-                $result = $this->importFile($path, $filename, $furtherParams);
-                if (is_array($result) && isset($result['newsid'])) {
-                    echo $result['newsid'];
-                } else {
-                    echo gT("something went wrong");
-                }
-            } break;
-            default: throw new Exception(gT("Unsupported command"));
+        $file = $arguments[0];
+        $result = $this->importFile($file);
+        if (is_array($result) && isset($result['newsid'])) {
+            echo $result['newsid'];
+        } else {
+            echo gT("Something went wrong");
         }
     }
 }
