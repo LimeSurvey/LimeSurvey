c1fa2faa7a5905a17ff8690ff5eb08c12bc8e8b5
c-schmitz <carsten.schmitz@limesurvey.org>
2023-08-23 10:59:03 +0200

    Release 3.28.71+230824

--- application/config/version.php
+++ application/config/version.php
@@ -12,10 +12,10 @@
  */
 
 
-$config['versionnumber'] = '3.28.69';
+$config['versionnumber'] = '3.28.71';
 $config['dbversionnumber'] = 366;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
 $config['templateapiversion'] = 3;
-$config['assetsversionnumber'] = '30299';
+$config['assetsversionnumber'] = '30300';
 return $config;
