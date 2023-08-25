5f1a664cac6c6772bef19cc09c970f9f66cf43fc
twilligls <tim.willig@limesurvey.org>
2023-08-25 14:55:29 +0200

    dev: preparations for OpHandlers

--- application/libraries/Api/Command/V1/SurveyPatch/OpApiHelper.php
+++ application/libraries/Api/Command/V1/SurveyPatch/OpApiHelper.php
@@ -7,11 +7,16 @@ use LimeSurvey\ObjectPatch\Op\OpInterface;
 
 class OpApiHelper
 {
+    /**
+     * @param OpInterface $op
+     * @param TransformerInterface $transformer
+     * @return mixed|null
+     */
     public static function getEntityId(
         OpInterface $op,
         TransformerInterface $transformer
     ) {
-        return is_array($op->getEntityId()) && $transformer
+        return is_array($op->getEntityId())
             ? $transformer->transform($op->getEntityId())
             : $op->getEntityId();
     }
