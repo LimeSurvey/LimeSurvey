786cb87cc2fd67efe85d1b4f418b081ebcdf493d
twilligls <tim.willig@limesurvey.org>
2023-09-14 10:27:52 +0200

    dev: refactored QuestionService to be able to get a Question object

--- application/libraries/Api/Command/V1/SurveyPatch/OpHandlerSurveyTrait.php
+++ application/libraries/Api/Command/V1/SurveyPatch/OpHandlerSurveyTrait.php
@@ -4,6 +4,7 @@ namespace LimeSurvey\Api\Command\V1\SurveyPatch;
 
 use LimeSurvey\ObjectPatch\Op\OpInterface;
 use LimeSurvey\ObjectPatch\OpHandler\OpHandlerException;
+use Question;
 
 trait OpHandlerSurveyTrait
 {
@@ -16,7 +17,7 @@ trait OpHandlerSurveyTrait
     public function getSurveyIdFromContext(OpInterface $op)
     {
         $context = $op->getContext();
-        $surveyId = isset($context['id']) ? (int) $context['id'] : null;
+        $surveyId = isset($context['id']) ? (int)$context['id'] : null;
         if ($surveyId === null) {
             throw new OpHandlerException(
                 printf(
@@ -26,5 +27,31 @@ trait OpHandlerSurveyTrait
             );
         }
         return $surveyId;
+    }
+
+    /**
+     * Gets a question from the question service, if the entity id is the qid,
+     * and the survey contains this question.
+     * @param OpInterface $op
+     * @return Question
+     * @throws OpHandlerException
+     */
+    public function getQuestion(OpInterface $op)
+    {
+        $surveyId = $this->getSurveyIdFromContext($op);
+        $questionId = $op->getEntityId();
+        $question = $this->questionService->getQuestionBySidAndQid(
+            $surveyId,
+            $questionId
+        );
+        if ($question === null) {
+            throw new OpHandlerException(
+                printf(
+                    'No question found for entity %s',
+                    $op->getEntityType()
+                )
+            );
+        }
+        return $question;
     }
 }
