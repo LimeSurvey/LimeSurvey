469c9eb9d5c991648f0a876cf36fb047a6342e7d
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2023-09-19 17:56:25 +0200

    Use better command method with proper docblock

--- application/commands/AddNotificationCommand.php
+++ application/commands/AddNotificationCommand.php
@@ -8,17 +8,15 @@ Yii::import('application.helpers.admin.import_helper', true);
 class AddNotificationCommand extends CConsoleCommand
 {
     /**
-     * Sample command: php application/commands/console.php importsurvey tmp/upload/youfile.lss
+     * Sample command: php application/commands/console.php addnotification --title='Message title' --body='Hello, this is message body' --importance=1
      *
-     * @param string[] $arguments
+     * @param string $title Title of notification
+     * @param string $body Text body of notification (can contain HTML)
+     * @param int $importance Importance level as defined in application/models/Notification.php
      * @return void
      */
-    public function run($arguments)
+    public function actionIndex($title, $body, $importance = 1)
     {
-        $title = $arguments[0];
-        $body  = $arguments[1];
-        $importance = (int) $arguments[2] ?? Notification::NORMAL_IMPORTANCE;
-
         $not = new Notification([
             'user_id' => 1,
             'title' => $title,
