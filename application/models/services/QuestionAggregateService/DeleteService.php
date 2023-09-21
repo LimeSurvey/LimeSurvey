688281b8f903a1c7400710fdf909d930152bb6e6
pstelling <patricia.stelling@limesurvey.org>
2023-09-21 09:18:04 +0200

    dev: nice code

--- application/models/services/QuestionAggregateService/DeleteService.php
+++ application/models/services/QuestionAggregateService/DeleteService.php
@@ -100,7 +100,7 @@ class DeleteService
         AnswerL10n::model()->deleteAll($aidsCriteria);
         try {
             $answer->delete();
-        }catch (\CDbException $e) {
+        } catch (\CDbException $e) {
             throw new PersistErrorException();
         }
     }
