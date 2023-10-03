f6cea2d81997989310d7d6ebc718e799633153ed
Denis Chenu <contact@sondages.pro>
2023-10-03 11:18:02 +0200

    Dev:currentUserid var must be in Trait Dev: some comments about test and console

--- application/core/Traits/LSApplicationTrait.php
+++ application/core/Traits/LSApplicationTrait.php
@@ -8,6 +8,9 @@
 
 trait LSApplicationTrait
 {
+
+    /* @var integer| null the current userId for all action */
+    private $currentUserId;
     /**
      * get the current id of connected user,
      * check if user exist before return for security
@@ -16,12 +19,17 @@ trait LSApplicationTrait
     public function getCurrentUserId()
     {
         if(empty(App()->session['loginID'])) {
-            // NULL for guest
+            /**
+             * NULL for guest,
+             * null by default for CConsoleapplication, but Permission always return true for console
+             * Test can update only App()->session['loginID'] to set the user
+             */
             return App()->session['loginID'];
         }
         if (!is_null($this->currentUserId) && $this->currentUserId == App()->session['loginID']) {
             return $this->currentUserId;
         }
+        /* use App()->session and not App()->user fot easiest unit test */
         $this->currentUserId = App()->session['loginID'];
         if ($this->currentUserId && !User::model()->findByPk($this->currentUserId)) {
             $this->currentUserId = 0;
