1db856d789339eafea1686a144af03782df2fe38
Kevin Foster <kevin.foster.uk@gmail.com>
2023-10-05 11:36:49 +0100

    dev: REST allow all methods

--- application/libraries/Api/Rest/Renderer/RendererBasic.php
+++ application/libraries/Api/Rest/Renderer/RendererBasic.php
@@ -63,7 +63,7 @@ class RendererBasic implements RendererInterface
     {
         header('Access-Control-Allow-Origin: *');
         header('Access-Control-Allow-Headers: *');
-        header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS');
+        header('Access-Control-Allow-Methods: *');
         http_response_code($responseCode);
 
         if ($data !== null) {
