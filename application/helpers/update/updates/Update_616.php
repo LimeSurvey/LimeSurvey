b3bca9ef8f92e04a65912a0e10eae68e1806854a
Mohab Elsheikh <mohab.elsheikh@limesurvey.org>
2023-10-24 12:19:14 +0200

    Feature #CT-1343: DB update

--- application/helpers/update/updates/Update_616.php
+++ application/helpers/update/updates/Update_616.php
@@ -17,11 +17,11 @@ class Update_616 extends DatabaseUpdateBase
             $this->db->createCommand()->addColumn('{{users}}', 'status', 'BOOLEAN DEFAULT TRUE');
         }
 
-        $users = \Yii::app()->db->createCommand("SELECT * FROM {{users}}")->queryAll();
-        foreach ($users as $user) {
-            if (isset($user['status'])) {
-                $this->db->createCommand()->update('{{users}}', ['status' => 0], 'uid = :uid AND expires <= CURRENT_TIME()', ['uid' => $user['uid']]);
-            }
-        }
+//        $users = \Yii::app()->db->createCommand("SELECT * FROM {{users}}")->queryAll();
+//        foreach ($users as $user) {
+//            if (isset($user['status'])) {
+//                $this->db->createCommand()->update('{{users}}', ['status' => 0], 'uid = :uid AND expires <= CURRENT_TIME()', ['uid' => $user['uid']]);
+//            }
+//        }
     }
 }
