1e72a61bb545e38f7a8ba8d15b1db916d1e7364e
Mohab Elsheikh <mohab.elsheikh@limesurvey.org>
2023-10-24 12:15:03 +0200

    Feature #CT-1343: Update the users count in navigation

--- application/helpers/update/updates/Update_616.php
+++ application/helpers/update/updates/Update_616.php
@@ -12,12 +12,16 @@ class Update_616 extends DatabaseUpdateBase
      */
     public function up()
     {
-        $surveymenuTable = \Yii::app()->db->schema->getTable('{{users}}');
-
-        if (!isset($surveymenuTable->columns['status'])) {
+        $usersTable = \Yii::app()->db->schema->getTable('{{users}}');
+        if (!isset($usersTable->columns['status'])) {
             $this->db->createCommand()->addColumn('{{users}}', 'status', 'BOOLEAN DEFAULT TRUE');
         }
 
-        $this->db->createCommand()->update('{{users}}', array('status' => 0), "expires is not null");
+        $users = \Yii::app()->db->createCommand("SELECT * FROM {{users}}")->queryAll();
+        foreach ($users as $user) {
+            if (isset($user['status'])) {
+                $this->db->createCommand()->update('{{users}}', ['status' => 0], 'uid = :uid AND expires <= CURRENT_TIME()', ['uid' => $user['uid']]);
+            }
+        }
     }
 }
