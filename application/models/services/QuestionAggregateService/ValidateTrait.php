f937bb9de05fea153f025bcf819f400de55d335f
Tim Willig <92720438+twilligls@users.noreply.github.com>
2023-10-24 13:36:33 +0200

    QE-459 dont delete all answers (#3511)
    * update existing answers
    
    * update existing answers rather than deleting all of them
    
    * changed db-update version number because of merging conflicts with remote, added db-update also to create- database.php
    ---------
    
    Co-authored-by: pstelling <patricia.stelling@limesurvey.org>

---
+++ application/models/services/QuestionAggregateService/ValidateTrait.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace LimeSurvey\Models\Services\QuestionAggregateService;
+
+use LimeSurvey\Models\Services\{
+    Exception\PersistErrorException,
+};
+
+/**
+ * Question Aggregate Validate Trait
+ */
+trait ValidateTrait
+{
+    /**
+     * Validate subquestion/answer codes.
+     *
+     * @param array $requestDataArray Data from request.
+     * @return void
+     * @throws PersistErrorException
+     */
+    public function validateCodes($requestDataArray)
+    {
+        // ensure uniqueness of codes
+        $codes = [];
+        foreach ($requestDataArray as $dataArray) {
+            foreach ($dataArray as $scaleId => $data) {
+                if (!isset($codes[$scaleId])) {
+                    $codes[$scaleId] = [];
+                }
+                if (
+                    in_array(
+                        $data['code'],
+                        $codes[$scaleId]
+                    )
+                ) {
+                    throw new PersistErrorException(
+                        'Subquestion/Answer codes must be unique'
+                    );
+                }
+                $codes[$scaleId][] = $data['code'];
+            }
+        }
+    }
+}
