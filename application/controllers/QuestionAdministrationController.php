db05567429c3a9657ae0dae84fd5e26b3679c04a
Gabriel Jenik <gabriel.jenik@gmail.com>
2023-10-26 11:32:08 -0300

    Fixed issue #19093: [security] Improper Authorization in Import Question function  (#3530)
    Co-authored-by: Lapiu Dev <devgit@lapiu.biz>

--- application/controllers/QuestionAdministrationController.php
+++ application/controllers/QuestionAdministrationController.php
@@ -1073,6 +1073,10 @@ class QuestionAdministrationController extends LSBaseController
         $iSurveyID = (int) App()->request->getPost('sid', 0);
         $gid = (int) App()->request->getPost('gid', 0);
 
+        if (!Permission::model()->hasSurveyPermission($iSurveyID, 'surveycontent', 'import')) {
+            throw new CHttpException(403, gT("We are sorry but you don't have permissions to do this."));
+        }
+
         $jumptoquestion = (bool)App()->request->getPost('jumptoquestion', 1);
 
         $oSurvey = Survey::model()->findByPk($iSurveyID);
