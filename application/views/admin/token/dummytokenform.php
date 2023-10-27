28e201fe788800f9fb53856fdecf69bf6427b11e
Matthew <mfavetti@gmail.com>
2023-10-27 04:40:01 -0700

    Fixed issue #19057: incorrect handling of wrong input in create dummy participant form (#3567)

--- application/views/admin/token/dummytokenform.php
+++ application/views/admin/token/dummytokenform.php
@@ -24,7 +24,7 @@
                 <div class="mb-3 col-6">
                     <label  class=" form-label" for='amount'><?php eT("Number of participants:"); ?></label>
                     <div class="">
-                        <input class='form-control' type='text' size='20' id='amount' name='amount' value="<?php echo $amount; ?>" />
+                        <input class='form-control' type='number' min='1' size='20' id='amount' name='amount' value="<?php echo $amount; ?>" />
                     </div>
                 </div>
 
