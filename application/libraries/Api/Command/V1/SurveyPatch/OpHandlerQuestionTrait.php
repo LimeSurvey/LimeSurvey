f710b28e7ccd289d84926f097ebc22cc8dc3ab62
Tim Willig <92720438+twilligls@users.noreply.github.com>
2023-10-30 12:48:51 +0100

    QE-429 op handler subquestion update (#3492)
    * OpHandlerSubquestionUpdate.php first steps
    
    * OpHandlerSubquestionUpdate.php with basic unit tests
    
    * re-added deleted function
    
    * exception when no question found
    
    * OpHandlerSubQuestion.php can now handle create and update
    
    * OpHandlerSubQuestion.php fixed a test
    
    * QE-429 dev: throw exception when prepared data is empty array
    
    * adjustments after change of how subquestions are saved
    
    * OpHandlerSubQuestion.php can now handle create and update after changes in SubQuestionService
    
    * fix psalm issue
    
    ---------
    
    Co-authored-by: pstelling <patricia.stelling@limesurvey.org>

--- application/libraries/Api/Command/V1/SurveyPatch/OpHandlerQuestionTrait.php
+++ application/libraries/Api/Command/V1/SurveyPatch/OpHandlerQuestionTrait.php
@@ -4,7 +4,9 @@ namespace LimeSurvey\Api\Command\V1\SurveyPatch;
 
 use LimeSurvey\Api\Command\V1\Transformer\Input\TransformerInputAnswer;
 use LimeSurvey\Api\Command\V1\Transformer\Input\TransformerInputAnswerL10ns;
+use LimeSurvey\Api\Command\V1\Transformer\Input\TransformerInputQuestion;
 use LimeSurvey\Api\Command\V1\Transformer\Input\TransformerInputQuestionAttribute;
+use LimeSurvey\Api\Command\V1\Transformer\Input\TransformerInputQuestionL10ns;
 use LimeSurvey\ObjectPatch\Op\OpInterface;
 use LimeSurvey\ObjectPatch\OpHandler\OpHandlerException;
 
@@ -197,5 +199,93 @@ trait OpHandlerQuestionTrait
             }
         }
         return $preparedSettings;
+    }
+
+    /**
+     * Converts the subquestions from the raw data to the expected format.
+     * @param OpInterface $op
+     * @param TransformerInputQuestion $transformerQuestion
+     * @param TransformerInputQuestionL10ns $transformerL10n
+     * @param array|null $data
+     * @param array|null $additionalRequiredEntities
+     * @return array
+     * @throws OpHandlerException
+     */
+    private function prepareSubQuestions(
+        OpInterface $op,
+        TransformerInputQuestion $transformerQuestion,
+        TransformerInputQuestionL10ns $transformerL10n,
+        ?array $data,
+        ?array $additionalRequiredEntities = null
+    ): array {
+        $preparedSubQuestions = [];
+        if (is_array($data)) {
+            foreach ($data as $index => $subQuestion) {
+                $tfSubQuestion = $transformerQuestion->transform(
+                    $subQuestion
+                );
+                if (
+                    is_array($tfSubQuestion) && array_key_exists(
+                        'title',
+                        $tfSubQuestion
+                    )
+                ) {
+                    $tfSubQuestion['code'] = $tfSubQuestion['title'];
+                }
+                $this->checkRequiredData(
+                    $op,
+                    $tfSubQuestion,
+                    'subquestions',
+                    $additionalRequiredEntities
+                );
+                if (
+                    is_array($subQuestion) && array_key_exists(
+                        'l10ns',
+                        $subQuestion
+                    ) && is_array($subQuestion['l10ns'])
+                ) {
+                    foreach ($subQuestion['l10ns'] as $lang => $subL10n) {
+                        $tfSubL10n = $transformerL10n->transform(
+                            $subL10n
+                        );
+                        $tfSubQuestion['subquestionl10n'][$lang] =
+                            (
+                                is_array($tfSubL10n)
+                                && isset($tfSubL10n['question'])
+                            ) ?
+                                $tfSubL10n['question'] : null;
+                    }
+                }
+                $qid = $this->getQidFromData($index, $tfSubQuestion);
+                $scaleId = $this->getScaleIdFromData($tfSubQuestion);
+                $preparedSubQuestions[$qid][$scaleId] = $tfSubQuestion;
+            }
+        }
+        return $preparedSubQuestions;
+    }
+
+    /**
+ * @param int $index
+ * @param array $questionData
+ * @return int
+ */
+    private function getQidFromData(int $index, array $questionData)
+    {
+        return array_key_exists(
+            'qid',
+            $questionData
+        ) ? (int)$questionData['qid'] : $index;
+    }
+
+    /**
+     * @param array $questionData
+     * @return int
+     */
+    private function getScaleIdFromData(array $questionData)
+    {
+        return array_key_exists(
+            'scale_id',
+            $questionData
+        ) ? (int)$questionData['scale_id'] : 0;
     }
 }
---
+++ tests/unit/api/opHandlers/OpHandlerSubquestionUpdateTest.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace ls\tests\unit\api\opHandlers;
+
+use LimeSurvey\Api\Command\V1\SurveyPatch\OpHandlerSubQuestion;
+use LimeSurvey\Api\Command\V1\Transformer\Input\TransformerInputQuestionL10ns;
+use LimeSurvey\Api\Command\V1\Transformer\Input\TransformerInputQuestion;
+use LimeSurvey\Models\Services\QuestionAggregateService;
+use LimeSurvey\ObjectPatch\{
+    Op\OpInterface,
+    Op\OpStandard,
+};
+use ls\tests\TestBaseClass;
+
+/**
+ * @testdox OpHandlerSubQuestion
+ */
+class OpHandlerSubquestionUpdateTest extends TestBaseClass
+{
+    protected OpInterface $op;
+
+    /**
+     * @testdox can handle a subquestion update
+     */
+    public function testOpSubquestionUpdateCanHandle()
+    {
+        $this->initializePatcher(
+            $this->getDefaultProps()
+        );
+
+        $opHandler = $this->getOpHandler();
+        self::assertTrue($opHandler->canHandle($this->op));
+    }
+
+    /**
+     * @testdox can handle a subquestion create
+     */
+    public function testOpSubquestionCreateCanHandle()
+    {
+        $this->initializePatcher(
+            $this->getDefaultProps(),
+            'create'
+        );
+
+        $opHandler = $this->getOpHandler();
+        self::assertTrue($opHandler->canHandle($this->op));
+    }
+
+    /**
+     * @testdox can not handle a subquestion delete
+     */
+    public function testOpSubquestionCanNotHandle()
+    {
+        $this->initializePatcher(
+            $this->getDefaultProps(),
+            'delete'
+        );
+
+        $opHandler = $this->getOpHandler();
+        self::assertFalse($opHandler->canHandle($this->op));
+    }
+
+    private function initializePatcher(
+        array $propsArray,
+        string $type = 'update'
+    ) {
+        $this->op = OpStandard::factory(
+            'subquestion',
+            $type,
+            123,
+            $propsArray,
+            [
+                'id' => 123456
+            ]
+        );
+    }
+
+    private function getDefaultProps()
+    {
+        return [
+            '0' => [
+                'qid' => 126,
+                'oldCode' => 'SQ001',
+                'title' => 'SQ001new',
+                'l10ns' => [
+                    'en' => [
+                        'question' => 'sub 1'
+                    ],
+                    'de' => [
+                        'question' => 'subger 1'
+                    ]
+                ]
+            ]
+        ];
+    }
+
+    /**
+     * @return OpHandlerSubQuestion
+     */
+    private function getOpHandler()
+    {
+        $mockQuestionAggregateService = \Mockery::mock(
+            QuestionAggregateService::class
+        )->makePartial();
+        $mockSubQuestionsService = \Mockery::mock(
+            QuestionAggregateService\SubQuestionsService::class
+        )->makePartial();
+        $mockQuestionService = \Mockery::mock(
+            QuestionAggregateService\QuestionService::class
+        )->makePartial();
+
+        return new OpHandlerSubQuestion(
+            $mockQuestionAggregateService,
+            $mockSubQuestionsService,
+            $mockQuestionService,
+            new TransformerInputQuestionL10ns(),
+            new TransformerInputQuestion()
+        );
+    }
+}
