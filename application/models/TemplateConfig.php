fd6e8717d9d45a5122185f8871ea8f65be6becd2
lapiudevgit <devgit@lapiu.biz>
2023-11-17 16:00:14 -0300

    Dev: Create new Global LS Package

--- application/models/TemplateConfig.php
+++ application/models/TemplateConfig.php
@@ -244,6 +244,7 @@ class TemplateConfig extends CActiveRecord
         /* Core package */
         $packages[] = 'limesurvey-public';
         $packages[] = 'template-core';
+        $packages[] = 'survey-theme-global';
         $packages[] = ($dir === "ltr") ? 'template-core-ltr' : 'template-core-rtl'; // Awesome Bootstrap Checkboxes
 
         /* bootstrap */
