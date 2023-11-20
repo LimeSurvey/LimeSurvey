0930eedfadddafc7ee8ac02b8a35f704c1c7bf8f
Lapiu Dev <devgit@lapiu.biz>
2023-11-20 11:46:17 -0600

    Fixed issue #19049: Cannot export LSS from survey where theme is set to one extended from 'bootswatch'
    Test for multiple nodes added.

--- tests/unit/models/TemplateConfigTest.php
+++ tests/unit/models/TemplateConfigTest.php
@@ -25,4 +25,22 @@ class TemplateConfigTest extends TestBaseClass
 
         $this->assertSame('{"container":"on"}', $jsonOptions, 'Unexpected result. The options were not encoded correctly.');
     }
+
+    public function testXmlStringWithMultipleNodes()
+    {
+        $xmlStr =   '<options>
+                        <hideprivacyinfo type="buttons" category="Simple options" width="4" title="Hide privacy info" options="on|off" optionlabels="Yes|No">off</hideprivacyinfo>
+                        <cssframework type="dropdown" category="Simple options" title="Variations" parent="cssframework">
+                                    <dropdownoptions>
+                                        <option data-mode="replace" value="css/variations/basic.min.css">Basic Bootstrap</option>
+                                        <option data-mode="replace" value="css/variations/cerulean.min.css">Cerulean</option>
+                                    </dropdownoptions>
+                        </cssframework>
+                    </options>';
+
+        $xmlData = new \SimpleXMLElement($xmlStr);
+        $jsonOptions = TemplateConfig::convertOptionsToJson($xmlData);
+
+        $this->assertSame('{"hideprivacyinfo":"off","cssframework":""}', $jsonOptions, 'Unexpected result. The options were not encoded correctly.');
+    }
 }
