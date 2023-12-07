77e70baa0a2771fba9dbc40a3e59fcdc88cf642f
jack-han-115 <lovelymouse115@gmail.com>
2023-12-07 17:13:39 +0800

    remove whitespace syntax

--- application/libraries/Api/Command/V1/SurveyDetail.php
+++ application/libraries/Api/Command/V1/SurveyDetail.php
@@ -78,7 +78,7 @@ class SurveyDetail implements CommandInterface
                 'groups.questions.subquestions.questionattributes',
                 'groups.questions.subquestions.answers'
             )->findByPk($surveyId);
-        //set real survey options with inheritance to get value of "inherit" attribute from db 
+        //set real survey options with inheritance to get value of "inherit" attribute from db
         // for example get inherit template value  $surveyModel->options->template
         $surveyModel->setOptionsFromDatabase();
 
