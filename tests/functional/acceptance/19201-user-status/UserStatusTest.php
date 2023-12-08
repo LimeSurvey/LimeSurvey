7891944a79ca7ade16f42b43a52f140fa67c56f2
Olle Haerstedt <olle.haerstedt@limesurvey.org>
2023-12-08 15:30:50 +0100

    WIP new test

--- tests/functional/acceptance/19201-user-status/UserStatusTest.php
+++ tests/functional/acceptance/19201-user-status/UserStatusTest.php
@@ -3,16 +3,17 @@
 namespace ls\tests;
 
 use Throwable;
+use User;
 use Facebook\WebDriver\WebDriverBy;
 use Facebook\WebDriver\WebDriverExpectedCondition;
 
 class UserStatusTest extends TestBaseClassWeb
 {
     // TODO: 
-    // Check that user 1 cannot be deactivated
     // Check that you cannot deactive yourself
     // Deactivate massive action
     // Activate massive action
+    // Deactivate user you do not own?
     // Try to login as not-active
     // Try to login as active
 
@@ -67,6 +68,43 @@ class UserStatusTest extends TestBaseClassWeb
             $this->assertEquals('Deactivate', $deactiveElement->getText(), 'Text is Deactivate');
             $this->assertTrue($deactiveElement->isDisplayed(), 'Element is displayed');
             $this->assertEquals('#', $deactiveElementAnchor->getAttribute('href'), 'Anchor href is #');
+        } catch (Throwable $e) {
+            self::$testHelper->takeScreenshot(self::$webDriver, __CLASS__ . '_' . __FUNCTION__);
+            echo $e->getMessage();
+            debug_print_backtrace();
+            $this->assertFalse(true);
+        }
+    }
+
+    public function testCanDeactivateNewUser()
+    {
+        // Delete all users but superadmin
+        User::model()->deleteAll('uid NOT IN (1)');
+        // Insert new user
+        User::insertUser(
+            $new_user = 'newuser',
+            $new_pass = 'asd',
+            $new_full_name = 'New user',
+            $parent_user = 1,
+            $new_email = 'new@user.com'
+        );
+
+        // Go to user management
+        $urlMan = \Yii::app()->urlManager;
+        $urlMan->setBaseUrl('http://' . self::$domain . '/index.php');
+        $web = self::$webDriver;
+
+        try {
+            // Go to User Management page
+            $url = $urlMan->createUrl('userManagement/index');
+            $web->get($url);
+
+            // Find row for new user
+            // Find action button
+            // Find ul
+            // Click on "Deactivate"
+            // Click on "Save"
+            // Check database
         } catch (Throwable $e) {
             self::$testHelper->takeScreenshot(self::$webDriver, __CLASS__ . '_' . __FUNCTION__);
             echo $e->getMessage();
