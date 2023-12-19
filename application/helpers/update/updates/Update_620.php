3c181427ffd7ff6e0573ab3a4ce5a015b90e80b0
jack-han-115 <lovelymouse115@gmail.com>
2023-12-19 13:44:16 +0800

    add new columns in the populate database

--- application/helpers/update/updates/Update_620.php
+++ application/helpers/update/updates/Update_620.php
@@ -6,7 +6,6 @@ class Update_620 extends DatabaseUpdateBase
 {
     public function up()
     {
-            // Add new tokens setting
             addColumn('{{surveys}}', 'showquestioncode', "string(1) NOT NULL default 'N'");
             addColumn('{{surveys}}', 'cookieconsent', "string(1) NOT NULL default 'N'");
             addColumn('{{surveys}}', 'footerbranding', "string(1) NOT NULL default 'N'");
