07804a9440b34b91d74a74459438633ef2d1202c
Connor Prettyman <29069461+ConnorPrettyman@users.noreply.github.com>
2023-12-22 10:15:30 +0000

    Update Update_619.php
    Updated to hopefully fix bug #19323 (Error upgrading to 6.4.x on MSSQL)

--- application/helpers/update/updates/Update_619.php
+++ application/helpers/update/updates/Update_619.php
@@ -11,6 +11,14 @@ class Update_619 extends DatabaseUpdateBase
      */
     public function up()
     {
-        $this->db->createCommand()->addColumn('{{users}}', 'user_status', 'BOOLEAN DEFAULT TRUE');
+        switch ($this->db->driverName)
+        {
+            case 'mssql':
+              \addColumn('{{users}}', 'user_status', 'BIT DEFAULT 1 NOT NULL');
+              break;
+            default:
+            \addColumn('{{users}}', 'user_status', 'BOOLEAN DEFAULT TRUE');
+        }
+        //$this->db->createCommand()->addColumn('{{users}}', 'user_status', 'BOOLEAN DEFAULT TRUE');
     }
 }
