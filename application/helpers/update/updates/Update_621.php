c65caffec12b0bd72822edc12457893fde6aa88a
Mohab Elsheikh <mohab.elsheikh@limesurvey.org>
2024-01-24 17:35:53 +0100

    Fixed issue #19351: Limesurvey broken after 5.6.50 to 6.4.2 upgrade

--- application/helpers/update/updates/Update_621.php
+++ application/helpers/update/updates/Update_621.php
@@ -11,9 +11,14 @@ class Update_621 extends DatabaseUpdateBase
      */
     public function up()
     {
-        $table = \Yii::app()->db->schema->getTable('{{users}}');
-        if (isset($table->columns['user_status']) && $table->columns['user_status']->dbType != 'integer') {
-            \alterColumn('{{users}}', 'user_status', "integer", false, 1);
+        switch (\Yii::app()->db->driverName) {
+            case 'pgsql':
+                $this->db->createCommand("ALTER TABLE {{users}} ALTER COLUMN user_status DROP DEFAULT;")->execute();
+                $this->db->createCommand("ALTER TABLE {{users}} ALTER COLUMN user_status TYPE INT USING CASE WHEN user_status=TRUE THEN 1 ELSE 0 END;")->execute();
+                $this->db->createCommand("ALTER TABLE {{users}} ALTER COLUMN user_status SET DEFAULT 1;")->execute();
+                break;
+            default:
+                \alterColumn('{{users}}', 'user_status', "integer", false, 1);
         }
     }
 }
