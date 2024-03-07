85e054d6ebba35f838210fe0e36b390c4a2bec4d
Denis Chenu <contact@sondages.pro>
2024-03-07 16:11:21 +0100

    Dev: compare name attribute to name to be sure of existiong value

--- application/models/Template.php
+++ application/models/Template.php
@@ -147,8 +147,8 @@ class Template extends LSActiveRecord
         /* Validate if template is OK in user dir, DIRECTORY_SEPARATOR not needed "/" is OK */
         $oTemplate = self::model()->findByPk($sTemplateName);
 
-        if (is_object($oTemplate) && $oTemplate->checkTemplate() && self::checkTemplateXML($oTemplate->folder)) {
-            self::$aNamesFiltered[$sTemplateName] = $oTemplate->folder;
+        if (is_object($oTemplate) && $oTemplate->checkTemplate() && $oTemplate->name === $sTemplateName && self::checkTemplateXML($oTemplate->folder)) {
+            self::$aNamesFiltered[$sTemplateName] = $sTemplateName;
             return self::$aNamesFiltered[$sTemplateName];
         }
 
