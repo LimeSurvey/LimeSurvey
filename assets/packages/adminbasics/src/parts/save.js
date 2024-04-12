0c30a9ec835b356a1ad169180b1aae051fe22ae4
lapiudevgit <devgit@lapiu.biz>
2024-04-12 17:33:59 -0300

    Fixed issue #19518: Page Does not release once downloaded

--- assets/packages/adminbasics/src/parts/save.js
+++ assets/packages/adminbasics/src/parts/save.js
@@ -41,7 +41,7 @@ const SaveController = () => {
         if(($(el).data('form-id') == 'addnewsurvey')) {
             const loadingSpinner = '<i class="ri-settings-5-fill remix-spin lsLoadingStateIndicator"></i>';
             $(el).prop('disabled', true).append(loadingSpinner);
-        } else if ((el.id === 'save-button') || (el.id === 'save-form-button') || (el.id === 'save-and-close-button') || (el.id = 'save-and-close-button-create-question')) {
+        } else if ((el.id === 'save-button') || (el.id === 'save-form-button') || (el.id === 'save-and-close-button') || (el.id === 'save-and-close-button-create-question')) {
             $('#ls-loading').show();
         }
     },
