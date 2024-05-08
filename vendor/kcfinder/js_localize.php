632e9e719d78769b8aba79afe15bc5e5b144d1b3
Gabriel Jenik <gabriel.jenik@gmail.com>
2024-05-08 11:08:15 -0300

    Fixed issue #19543: KCFinder needs to be updated when using with PHP 8.1 (#3837)
    Co-authored-by: lapiudevgit <devgit@lapiu.biz>

--- vendor/kcfinder/js_localize.php
+++ vendor/kcfinder/js_localize.php
@@ -16,7 +16,7 @@ namespace kcfinder;
 require "core/autoload.php";
 
 if (!isset($_GET['lng']) || ($_GET['lng'] == 'en') ||
-    ($_GET['lng'] != basename($_GET['lng'])) ||
+    ($_GET['lng'] != basename((string) $_GET['lng'])) ||
     !is_file("lang/" . $_GET['lng'] . ".php")
 ) {
     header("Content-Type: text/javascript");
@@ -36,7 +36,7 @@ echo "_.labels={";
 
 $i = 0;
 foreach ($lang as $english => $native) {
-    if (substr($english, 0, 1) != "_") {
+    if (substr((string) $english, 0, 1) != "_") {
         echo "'" . text::jsValue($english) . "':\"" . text::jsValue($native) . "\"";
         if (++$i < count($lang))
             echo ",";
