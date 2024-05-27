987230adc293dadf0a05d85cc2bfd518100acb0c
Kevin Foster <kevin.foster.uk@gmail.com>
2024-05-27 12:12:57 +0100

    Dev: fix question editor JS button redirect

--- assets/scripts/admin/newQuestionEditor.js
+++ assets/scripts/admin/newQuestionEditor.js
@@ -1,9 +1,6 @@
 $(document).on('ready pjax:scriptcomplete', function () {
     $('#editor-link-button').on('click', function(e){
-        let url = this.getAttribute('data-url');
-        $.ajax({
-            url: url,
-            method: 'GET',
-        });
+        window.location.assign(this.getAttribute('data-url'));
+        window.open(newUrl, '_top');
     });
 });
