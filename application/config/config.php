2e5e018c64750901a650424f66b868b637b9fe72
Lars Tijsmans <lars@ag5.com>
2024-05-29 01:13:55 +0200

    Extended config.php

--- application/config/config.php
+++ application/config/config.php
@@ -1,6 +1,15 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 return array(
+    'components' => array(
+        'db' => array(
+            'connectionString' => 'mysql:host=localhost;port=3306;dbname=limesurvey;',
+            'emulatePrepare' => true,
+            'username' => getenv('DB_USERNAME'),
+            'password' => getenv('DB_PASSWORD'),
+            'charset' => 'utf8mb4',
+            'tablePrefix' => 'lime_',
+        ),
     'config'=>array(
         'debug'=>0,
         'debugsql'=>0,
