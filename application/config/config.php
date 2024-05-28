7c4e27f5dca71af60fc89a745fc57c4850e8e408
Lars Tijsmans <lars@ag5.com>
2024-05-29 01:05:42 +0200

    Extended config.php

--- application/config/config.php
+++ application/config/config.php
@@ -1,43 +1,9 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 return array(
-    'components' => array(
-        'db' => array(
-            'connectionString' => 'mysql:host=' . getenv('DB_HOST') . ';dbname=' . getenv('DB_NAME'),
-            'emulatePrepare' => true,
-            'username' => getenv('DB_USERNAME'),
-            'password' => getenv('DB_PASSWORD'),
-            'charset' => 'utf8mb4',
-            'tablePrefix' => 'lime_',
-            'mysqlEngine' => 'InnoDB',
-        ),
-        'urlManager' => array(
-            'urlFormat' => 'path',
-            'rules' => array(
-                '' => 'survey/index',
-                '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
-                '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
-            ),
-            'showScriptName' => true,
-        ),
-        'request' => array(
-            'enableCsrfValidation' => true,
-        ),
-        'log' => array(
-            'class' => 'CLogRouter',
-            'routes' => array(
-                array(
-                    'class' => 'CFileLogRoute',
-                    'levels' => 'error, warning',
-                ),
-                array(
-                    'class' => 'CWebLogRoute',
-                ),
-            ),
-        ),
-    ),
-    'params' => array(
-        'debug' => 2,
-        'debugsql' => 0,
-    ),
+    'config'=>array(
+        'debug'=>0,
+        'debugsql'=>0,
+        'mysqlEngine'=>'InnoDB', // Update default LimeSurvey config here
+    )
 );
