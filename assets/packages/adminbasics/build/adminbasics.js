9a94c4abfab9490a9dda244d69ed1d14e520726a
lapiudevgit <devgit@lapiu.biz>
2024-07-10 10:38:05 -0300

    Fixed issue #19631: Very hard to match attributes in the CPDB when importing CSV data

--- assets/packages/adminbasics/build/adminbasics.js
+++ assets/packages/adminbasics/build/adminbasics.js
@@ -21292,33 +21292,11 @@
 	}
 
 	/**
-	 * Welcome page card animations
+	 * Welcome page animations
 	 * NB: Bootstrap 5 replaced panels with cards
 	 */
 	function panelsAnimation() {
 	  setTimeout(() => {
-	    adminCoreLSConsole.log('Triggering card animation');
-	    /**
-	     * Card shown one by one
-	     */
-	    /**
-	     * @todo Added .welcome to restrict this to the welcome page (as it was intended), but the animation doesn't work
-	     *       anyway because the cards are already visible. On older versions, the panels (now cards) were hidden by
-	     *       the definition of ".welcome .panel" in adminbasics.css. We should either fix all the CSS related to these
-	     *       panels, or clean it and remove this CSS if the animation is not needed.
-	     */
-	    document.querySelectorAll(".welcome .card").forEach(function (e, i) {
-	      setTimeout(() => {
-	        e.animate({
-	          top: '0px',
-	          opacity: 1
-	        }, {
-	          duration: 200,
-	          fill: 'forwards'
-	        });
-	      }, i * 200);
-	    });
-
 	    /**
 	     * Rotate last survey/question
 	     */
