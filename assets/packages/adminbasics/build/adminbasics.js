9dc2b8e49f2c32c360e0a37dcb80194a1a7db90a
Gabriel Jenik <gabriel.jenik@gmail.com>
2024-07-10 11:32:56 -0300

    Fixed issue #19631: Very hard to match attributes in the CPDB when importing CSV data (#3908)
    Co-authored-by: lapiudevgit <devgit@lapiu.biz>

--- assets/packages/adminbasics/build/adminbasics.js
+++ assets/packages/adminbasics/build/adminbasics.js
@@ -21292,27 +21292,11 @@
 	}
 
 	/**
-	 * Welcome page card animations
+	 * Welcome page animations
 	 * NB: Bootstrap 5 replaced panels with cards
 	 */
 	function panelsAnimation() {
 	  setTimeout(() => {
-	    adminCoreLSConsole.log('Triggering card animation');
-	    /**
-	     * Card shown one by one
-	     */
-	    document.querySelectorAll(".card").forEach(function (e, i) {
-	      setTimeout(() => {
-	        e.animate({
-	          top: '0px',
-	          opacity: 1
-	        }, {
-	          duration: 200,
-	          fill: 'forwards'
-	        });
-	      }, i * 200);
-	    });
-
 	    /**
 	     * Rotate last survey/question
 	     */
