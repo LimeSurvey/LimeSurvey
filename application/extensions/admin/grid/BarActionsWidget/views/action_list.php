927f0a2cfbe06f7efb948cfeb6a42e8fa5188245
Mohab Elsheikh <mohab.elsheikh@limesurvey.org>
2024-07-15 20:50:42 +0200

    LA-73: Update dashboard (Surveys list)

---
+++ application/extensions/admin/grid/BarActionsWidget/views/action_list.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @var array $items
+ */
+
+?>
+<div id="<?= 'action-list-' . $id; ?>" class="action-list">
+    <ul id="<?= 'action-list-' . $id; ?>">
+        <?php foreach ($items as $item) : ?>
+            <li class="list-inline-item">
+                <a href="<?= $item['enabledCondition'] ? $item['url'] : '#'?>"
+                   class="<?=$item['enabledCondition'] ? '' : 'disabled'?>"
+                    <?php if ($item['enabledCondition']) : ?>
+                        data-bs-toggle="tooltip"
+                        data-bs-original-title="<?=$item['title'] ?>"
+                    <?php endif;?>
+                >
+                    <i class="<?=$item['iconClass']?>"></i>
+                </a>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+</div>
+<script>
+
+</script>
