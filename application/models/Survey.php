f8d89cd5b46b5050fd87a974661f8705978d15fe
Gabriel Jenik <gabriel.jenik@gmail.com>
2024-07-17 09:22:58 -0300

    Fixed issue #19651: Internal error when taking survey on PostgreSQL (#3919)
    Co-authored-by: lapiudevgit <devgit@lapiu.biz>

--- application/models/Survey.php
+++ application/models/Survey.php
@@ -2020,7 +2020,7 @@ class Survey extends LSActiveRecord implements PermissionInterface
             $sumresult = Question::model()->with('questionattributes')
               ->count(
                   "sid=:sid AND parent_qid=:parent_qid AND((attribute='hidden' AND value!=:hidden )OR attribute IS NULL)",
-                  ['sid' => $this->sid, 'parent_qid' => 0, 'hidden' => 1]
+                  ['sid' => $this->sid, 'parent_qid' => 0, 'hidden' => '1']
               );
         }
 
