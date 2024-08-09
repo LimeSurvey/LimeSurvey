811eb4475974fdafda914b8fe1444e9b90a26025
Patrick Teichmann <patrick.teichmann@limesurvey.org>
2024-08-09 11:31:04 +0200

    Dev: update grid action-list with flexbox Dev: use global vars from scss

--- application/extensions/admin/grid/BarActionsWidget/views/action_list.php
+++ application/extensions/admin/grid/BarActionsWidget/views/action_list.php
@@ -5,19 +5,15 @@
 
 ?>
 <div id="<?= 'action-list-' . $id; ?>" class="action-list">
-    <ul id="<?= 'action-list-' . $id; ?>">
-        <?php foreach ($items as $item) : ?>
-            <li class="list-inline-item">
-                <a href="<?= $item['enabledCondition'] ? $item['url'] : '#'?>"
-                   class="<?=$item['enabledCondition'] ? '' : 'disabled'?>"
-                    <?php if ($item['enabledCondition']) : ?>
-                        data-bs-toggle="tooltip"
-                        data-bs-original-title="<?=$item['title'] ?>"
-                    <?php endif;?>
-                >
-                    <i class="<?=$item['iconClass']?>"></i>
-                </a>
-            </li>
-        <?php endforeach; ?>
-    </ul>
+    <?php foreach ($items as $item) : ?>
+        <a href="<?= $item['enabledCondition'] ? $item['url'] : '#' ?>"
+           class="<?= $item['enabledCondition'] ? '' : 'disabled' ?>"
+            <?php if ($item['enabledCondition']) : ?>
+                data-bs-toggle="tooltip"
+                data-bs-original-title="<?= $item['title'] ?>"
+            <?php endif; ?>
+        >
+            <i class="<?= $item['iconClass'] ?>"></i>
+        </a>
+    <?php endforeach; ?>
 </div>
