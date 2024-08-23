badd4a9090a262b03d55e7d314842f75dc339829
Denis Chenu <contact@sondages.pro>
2024-08-23 19:00:56 +0200

    Fixed issue #19711: Cannot access simple statistics Dev: Remove MYSQL specific character

--- application/controllers/admin/Statistics.php
+++ application/controllers/admin/Statistics.php
@@ -685,7 +685,7 @@ class Statistics extends SurveyCommonAction
         $rows = Question::model()->primary()->getQuestionList($surveyid);
         ;
 
-        $rawQuestions = Question::model()->findAll("sid = :sid and `type` = :type", [
+        $rawQuestions = Question::model()->findAll("sid = :sid and type = :type", [
             ":sid" => $surveyid,
             ":type" => Question::QT_COLON_ARRAY_NUMBERS
         ]);
