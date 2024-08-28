e7ba45f8a560f5a6314ac0b65f78e5be3882a4d0
Denis Chenu <contact@sondages.pro>
2024-08-28 10:14:32 +0200

    Dev: review way for existing previous value

--- application/helpers/remotecontrol/remotecontrol_handle.php
+++ application/helpers/remotecontrol/remotecontrol_handle.php
@@ -3569,20 +3569,17 @@ class remotecontrol_handle
         $oFormattingOptions = new FormattingOptions();
 
         if (is_array($aAdditionalOptions)) {
-            if (isset($aAdditionalOptions['convertY']) && $aAdditionalOptions['convertY']) {
-                $oFormattingOptions->convertY = $aAdditionalOptions['convertY'];
-                $oFormattingOptions->yValue = isset($aAdditionalOptions['yValue']) ? $aAdditionalOptions['yValue'] : 'Y';
-                unset($aAdditionalOptions['convertY']);
-            }
-            if (isset($aAdditionalOptions['convertN']) && $aAdditionalOptions['convertN']) {
-                $oFormattingOptions->convertN = $aAdditionalOptions['convertN'];
-                $oFormattingOptions->nValue = isset($aAdditionalOptions['nValue']) ? $aAdditionalOptions['nValue'] : 'N';
-                unset($aAdditionalOptions['convertN']);
-            }
             foreach ($aAdditionalOptions as $option => $value) {
                 if (property_exists($oFormattingOptions, $option )){
                     $oFormattingOptions->$option = $value;
                 }
+            }
+            /* fix some specific option (GUI use 1 and 2, but default for remote control are set to Y and N before) */
+            if (!empty($oFormattingOptions->convertY) && empty($oFormattingOptions->yValue)) {
+                $oFormattingOptions->yValue = 'Y';
+            }
+            if (!empty($oFormattingOptions->convertN) && empty($oFormattingOptions->nValue)) {
+                $oFormattingOptions->nValue = 'N';
             }
         }
 
