ac38d5d59bbdd1b4c9a8923eba27678279f96fb8
twilligls <tim.willig@limesurvey.org>
2024-08-28 10:28:08 +0200

    conversion in TransformerInputQuestionAttribute for graphtype and showgraph

--- application/libraries/Api/Command/V1/Transformer/Input/TransformerInputQuestionAttribute.php
+++ application/libraries/Api/Command/V1/Transformer/Input/TransformerInputQuestionAttribute.php
@@ -30,24 +30,22 @@ class TransformerInputQuestionAttribute extends Transformer
         foreach ($this->converterMap() as $attrName => $convDataArray) {
             if (array_key_exists($attrName, $collection)) {
                 foreach ($convDataArray as $convData) {
-                    if (array_key_exists($attrName, $collection)) {
-                        foreach ($collection[$attrName] as $language => $value) {
-                            if (
-                                (
-                                    is_array($convData['triggerValue'])
-                                    && in_array(
-                                        $value,
-                                        $convData['triggerValue']
-                                    )
+                    foreach ($collection[$attrName] as $language => $value) {
+                        if (
+                            (
+                                is_array($convData['matchValue'])
+                                && in_array(
+                                    $value,
+                                    $convData['matchValue']
                                 )
-                                || $value === $convData['triggerValue']
-                            ) {
-                                $collection = $this->applyConversion(
-                                    $collection,
-                                    $language,
-                                    $convData
-                                );
-                            }
+                            )
+                            || $value === $convData['matchValue']
+                        ) {
+                            $collection = $this->applyConversion(
+                                $collection,
+                                $language,
+                                $convData
+                            );
                         }
                     }
                 }
@@ -72,9 +70,9 @@ class TransformerInputQuestionAttribute extends Transformer
         $convData
     ) {
         if ($convData['isLanguageBased']) {
-            $collection[$convData['newAttribute']][$language] = $convData['newValue'];
-        }else{
-            $collection[$convData['newAttribute']][''] = $convData['newValue'];
+            $collection[$convData['targetAttribute']][$language] = $convData['newValue'];
+        } else {
+            $collection[$convData['targetAttribute']][''] = $convData['newValue'];
         }
 
         return $collection;
@@ -82,9 +80,9 @@ class TransformerInputQuestionAttribute extends Transformer
 
     /**
      * array organized by attribute name => array of conversion rules.
-     * if attribute has exact same value as 'triggerValue'
-     * or its value is one of those in the 'triggerValue'-array,
-     * the value of 'newAttribute' is supposed to be set to 'newValue'.
+     * if attribute has exact same value as 'matchValue'
+     * or its value is one of those in the 'matchValue'-array,
+     * the value of 'targetAttribute' is supposed to be set to 'newValue'.
      * @return array[]
      */
     private function converterMap()
@@ -92,16 +90,16 @@ class TransformerInputQuestionAttribute extends Transformer
         return [
             'statistics_graphtype' => [
                 [
-                    'triggerValue' => -1,
-                    'newAttribute' => 'statistics_showgraph',
+                    'matchValue' => '-1',
+                    'targetAttribute' => 'statistics_showgraph',
                     'isLanguageBased' => false,
-                    'newValue' => 0
+                    'newValue' => '0'
                 ],
                 [
-                    'triggerValue' => [0, 1, 2, 3, 4, 5],
-                    'newAttribute' => 'statistics_showgraph',
+                    'matchValue' => ['0', '1', '2', '3', '4', '5'],
+                    'targetAttribute' => 'statistics_showgraph',
                     'isLanguageBased' => false,
-                    'newValue' => 1
+                    'newValue' => '1'
                 ]
             ],
         ];
