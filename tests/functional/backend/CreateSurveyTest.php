35b84e09117c439e94906cc854b1fadc011c991e
Mohab Elsheikh <mohab.elsheikh@limesurvey.org>
2024-08-29 15:21:23 +0200

    dev: fix testCreateSurvey

--- tests/functional/backend/CreateSurveyTest.php
+++ tests/functional/backend/CreateSurveyTest.php
@@ -85,7 +85,7 @@ class CreateSurveyTest extends TestBaseClassWeb
             // Click on big "Create survey" button.
             $link = self::$webDriver->wait(10)->until(
                 WebDriverExpectedCondition::elementToBeClickable(
-                    WebDriverBy::id('create-survey')
+                    WebDriverBy::id('createSurvey')
                 )
             );
             $link->click();
