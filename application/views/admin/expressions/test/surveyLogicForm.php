a261c2a05966a376c517b7d75fbe0a2ffe8b2bbc
lapiudevgit <devgit@lapiu.biz>
2024-10-08 15:25:38 -0300

    Fixed issue #19784: No access to the 'Edit Poll Settings' page view in debug mode
    - Fixed view. Checkboxes looked bad.

--- application/views/admin/expressions/test/surveyLogicForm.php
+++ application/views/admin/expressions/test/surveyLogicForm.php
@@ -20,20 +20,60 @@
                     <h4>Debug Log Level</h4>
                     <h5>Specify which debugging features to use</h5>
                     <div class="mb-3">
-                        <label for="LEM_DEBUG_TIMING">Detailed Timing</lable>
-                        <input type="checkbox" class="form-control" name="LEM_DEBUG_TIMING" id="LEM_DEBUG_TIMING" value="1"/>
+                        <?php echo TbHtml::label(gT("Detailed Timing"), 'LEM_DEBUG_TIMING', ['class' => " form-label"]); ?>
+                        <div>
+                            <?php
+                            $this->widget('ext.ButtonGroupWidget.ButtonGroupWidget', [
+                                'name'          => 'LEM_DEBUG_TIMING',
+                                'checkedOption' => 0,
+                                'selectOptions' => [
+                                    '1' => gT("Yes", 'unescaped'),
+                                    '0' => gT("No", 'unescaped'),
+                                ],
+                            ]); ?>
+                        </div>
                     </div>
                     <div class="mb-3">
-                        <label for="LEM_DEBUG_VALIDATION_SUMMARY">Validation Summary</label>
-                        <input class="form-control" type="checkbox" name="LEM_DEBUG_VALIDATION_SUMMARY" id="LEM_DEBUG_VALIDATION_SUMMARY" value="1"/>
+                        <?php echo TbHtml::label(gT("Validation Summary"), 'LEM_DEBUG_VALIDATION_SUMMARY', ['class' => " form-label"]); ?>
+                        <div>
+                            <?php
+                            $this->widget('ext.ButtonGroupWidget.ButtonGroupWidget', [
+                                'name'          => 'LEM_DEBUG_VALIDATION_SUMMARY',
+                                'checkedOption' => 0,
+                                'selectOptions' => [
+                                    '1' => gT("Yes", 'unescaped'),
+                                    '0' => gT("No", 'unescaped'),
+                                ],
+                            ]); ?>
+                        </div>
                     </div>
                     <div class="mb-3">
-                        <label for="LEM_DEBUG_VALIDATION_DETAIL">Validation Detail (Validation Summary must also be checked to see detail)</label>
-                        <input class="form-control" type="checkbox" name="LEM_DEBUG_VALIDATION_DETAIL" id="LEM_DEBUG_VALIDATION_DETAIL" value="1"/>
+                        <?php echo TbHtml::label(gT("Validation Detail (Validation Summary must also be checked to see detail)"), 'LEM_DEBUG_VALIDATION_DETAIL', ['class' => " form-label"]); ?>
+                        <div>
+                            <?php
+                            $this->widget('ext.ButtonGroupWidget.ButtonGroupWidget', [
+                                'name'          => 'LEM_DEBUG_VALIDATION_DETAIL',
+                                'checkedOption' => 0,
+                                'selectOptions' => [
+                                    '1' => gT("Yes", 'unescaped'),
+                                    '0' => gT("No", 'unescaped'),
+                                ],
+                            ]); ?>
+                        </div>
                     </div>
                     <div class="mb-3">
-                        <label for="LEM_PRETTY_PRINT_ALL_SYNTAX">Pretty Print Syntax</label>
-                        <input class="form-control" type="checkbox" name="LEM_PRETTY_PRINT_ALL_SYNTAX" id="LEM_PRETTY_PRINT_ALL_SYNTAX" value="1" checked="checked"/>
+                        <?php echo TbHtml::label(gT("Pretty Print Syntax"), 'LEM_PRETTY_PRINT_ALL_SYNTAX', ['class' => " form-label"]); ?>
+                        <div>
+                            <?php
+                            $this->widget('ext.ButtonGroupWidget.ButtonGroupWidget', [
+                                'name'          => 'LEM_PRETTY_PRINT_ALL_SYNTAX',
+                                'checkedOption' => 1,
+                                'selectOptions' => [
+                                    '1' => gT("Yes", 'unescaped'),
+                                    '0' => gT("No", 'unescaped'),
+                                ],
+                            ]); ?>
+                        </div>
                     </div>
                     <div class="mb-3">
                         <input type="submit" class="btn btn-outline-secondary"/></td>
