032d138b58ad26b41167e02a2b921dd5551c0f41
lapiudevgit <devgit@lapiu.biz>
2024-10-21 12:01:56 -0300

    Fixed issue #19600: Some Tools Menu Items are not visible when having lot of languages
    - Extend solution to Run/Preview Survey/Group/Question

--- application/views/surveyAdministration/partial/topbar/languagesDropdownItems.php
+++ application/views/surveyAdministration/partial/topbar/languagesDropdownItems.php
@@ -3,23 +3,53 @@
 /** @var string $type */
 ?>
 
-<?php foreach ($surveyLanguages as $languageCode => $languageName) : ?>
-    <?php
-    switch ($type) {
-        case 'questionGroup':
-            $link = Yii::App()->createUrl("survey/index/action/previewgroup/sid/{$sid}/gid/{$gid}/lang/" . $languageCode);
-            break;
-        case 'question':
-            $link = Yii::App()->createUrl("survey/index/action/previewquestion/sid/{$sid}/gid/{$gid}/qid/{$qid}/lang/{$languageCode}");
-            break;
-        default:
-            $link = Yii::App()->createUrl("survey/index",array('sid'=>$sid,'newtest'=>"Y",'lang'=>$languageCode));
+<?php
+    $allLanguageOptions = [];
+    foreach ($surveyLanguages as $languageCode => $languageName) {
+        switch ($type) {
+            case 'questionGroup':
+                $link = Yii::App()->createUrl("survey/index/action/previewgroup/sid/{$sid}/gid/{$gid}/lang/" . $languageCode);
+                break;
+            case 'question':
+                $link = Yii::App()->createUrl("survey/index/action/previewquestion/sid/{$sid}/gid/{$gid}/qid/{$qid}/lang/{$languageCode}");
+                break;
+            default:
+                $link = Yii::App()->createUrl("survey/index",array('sid'=>$sid,'newtest'=>"Y",'lang'=>$languageCode));
+        }
 
+        $allLanguageOptions[$languageCode] = [
+            'href' => $link,
+            'text' => $languageName,
+            'target' => '_blank'
+        ];
     }
+?>
+
+<?php if (count($allLanguageOptions) <= 5) : ?>
+    <?php foreach ($allLanguageOptions as $languageCode => $languageData) : ?>
+        <li>
+            <a class="dropdown-item" target="_blank" href="<?= $languageData['href'] ?>" >
+                <?= $languageData['text'] ?>
+            </a>
+        </li>
+    <?php endforeach; ?>
+<?php else: ?>
+    <?php
+        $firstLanguage = array_shift($allLanguageOptions);
     ?>
     <li>
-        <a class="dropdown-item" target="_blank" href="<?= $link; ?>" >
-            <?php echo $languageName; ?>
+        <a class="dropdown-item" target="_blank" href="<?= $firstLanguage['href'] ?>" >
+            <?= $firstLanguage['text'] ?>
         </a>
     </li>
-<?php endforeach; ?>
+    <?php
+        $languagesModal = $this->widget('ext.OptionsModalWidget.OptionsModalWidget', [
+            'modalTitle' => gT("Select language"),
+            'options' => $allLanguageOptions
+        ]);
+        $languagesModalId = $languagesModal->getModalId();
+    ?>
+    <li>
+        <a class="dropdown-item" href="#<?= $languagesModalId ?>" data-bs-toggle="modal"><?= gT("Other languages") ?></a>
+    </li>
+<?php endif; ?>
