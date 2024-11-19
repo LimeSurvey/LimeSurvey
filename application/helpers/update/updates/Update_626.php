5ff008660804d4d94dbf22fb6aa01dad47dfd29b
Mohab Elsheikh <mohab.elsheikh@limesurvey.org>
2024-11-19 16:54:50 +0100

    Rename Update_625 - cherry picked from a62c55a

---
+++ application/helpers/update/updates/Update_626.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace LimeSurvey\Helpers\Update;
+
+use CException;
+
+class Update_626 extends DatabaseUpdateBase
+{
+    /**
+     * @inheritDoc
+     * @throws CException
+     */
+    public function up(): void
+    {
+        $boxes = $this->db->createCommand()
+            ->select("*")
+            ->from("{{boxes}}")
+            ->order('position ASC')
+            ->queryAll();
+
+        if (!empty($boxes)) {
+            if ($boxes[array_key_first($boxes)]['position'] === 1) {
+                // we increase the position of all boxes by 1
+                foreach ($boxes as $box) {
+                    $this->db->createCommand()->update(
+                        '{{boxes}}',
+                        ['position' => $box['position'] + 1],
+                        "id = '{$box['id']}'"
+                    );
+                }
+            }
+            // Then we recreate them
+            $oDB = App()->db;
+            $oDB->createCommand()->insert('{{boxes}}', [
+                'position'   => '1',
+                'url'        => 'dashboard/view',
+                'title'      => gT('Dashboard'),
+                'ico'        => 'ri-function-fill',
+                'desc'       => gT('View dashboard'),
+                'page'       => 'welcome',
+                'usergroup'  => '-1',
+                'buttontext' => gt('View dashboard')
+            ]);
+        }
+    }
+}
