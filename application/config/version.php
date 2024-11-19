bf18da8040c84da9dd1b9767f7dade06ed444fee
c-schmitz <carsten.schmitz@limesurvey.org>
2024-11-19 15:09:41 +0100

    Release 6.8.0+241119

--- application/config/version.php
+++ application/config/version.php
@@ -11,10 +11,10 @@
  * See COPYRIGHT.php for copyright notices and details.
  */
 
-$config['versionnumber'] = '6.6.9';
+$config['versionnumber'] = '6.7.0';
 $config['dbversionnumber'] = 623;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
 $config['templateapiversion']  = 3;
-$config['assetsversionnumber'] = '30410';
+$config['assetsversionnumber'] = '30411';
 return $config;
