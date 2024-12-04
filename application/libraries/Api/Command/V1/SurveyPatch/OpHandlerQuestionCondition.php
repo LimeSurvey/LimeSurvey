3421d019e602e098702e1b865c1e8fea8141a1d1
Lajos Arpad <arpad@endpoint.com>
2024-12-04 17:54:41 +0200

    Implemented adjustments

---
+++ application/libraries/Api/Command/V1/SurveyPatch/OpHandlerQuestionCondition.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace LimeSurvey\Api\Command\V1\SurveyPatch;
+
+use LimeSurvey\Api\Command\V1\Transformer\Input\TransformerInputQuestionCondition;
+use LimeSurvey\Api\Command\V1\SurveyPatch\Traits\{
+    OpHandlerExceptionTrait,
+    OpHandlerSurveyTrait,
+    OpHandlerValidationTrait
+};
+use LimeSurvey\Models\Services\{
+    Exception\NotFoundException,
+    Exception\PermissionDeniedException,
+    SurveyCondition,
+    Exception\PersistErrorException
+};
+use LimeSurvey\ObjectPatch\{
+    Op\OpInterface,
+    OpHandler\OpHandlerException,
+    OpHandler\OpHandlerInterface,
+    OpType\OpTypeUpdate
+};
+
+class OpHandlerQuestionCondition implements OpHandlerInterface
+{
+    use OpHandlerSurveyTrait;
+    use OpHandlerValidationTrait;
+    use OpHandlerExceptionTrait;
+
+    protected string $entity;
+
+    protected SurveyCondition $surveyCondition;
+
+    protected TransformerInputQuestionCondition $transformer;
+
+    public function __construct(
+        SurveyCondition $surveyCondition,
+        TransformerInputQuestionCondition $transformer
+    ) {
+        $this->entity = 'questionCondition';
+        $this->surveyCondition = $surveyCondition;
+        $this->transformer = $transformer;
+    }
+
+    public function canHandle(OpInterface $op): bool
+    {
+        return $op->getEntityType() === $this->entity;
+    }
+
+    public function transformAll($collection, $options = [])
+    {
+        return $collection;
+    }
+
+    public function message($message, $type)
+    {
+        //dummy method at this point, because we do not support success messages yet
+    }
+
+    /**
+     * Updates multiple attributes for a single question. Format is exactly the
+     * same as in Question create, so they share the prepare function.
+     *
+     * patch structure:
+     * {
+     *     deleteAllConditions:
+     *     "patch": [{
+     *             "entity": "questionCondition",
+     *             "op": "delete",
+     *             "id": 809,
+     *             "props": {
+     *                 qid: 15977,
+     *                 action: deleteAllConditions
+     *             }
+     *         }
+     *     ]
+     * }
+     *
+     * @param OpInterface $op
+     * @return void
+     * @throws OpHandlerException
+     * @throws PersistErrorException
+     * @throws NotFoundException
+     * @throws PermissionDeniedException
+     */
+    public function handle(OpInterface $op): void
+    {
+        $qid = $op->getProps()['qid'];
+        if (isset($op->getProps()['action'])) {
+            $action = $op->getProps()['action'];
+            switch ($action) {
+                case "deleteAllConditions":
+                    $this->surveyCondition->deleteAllConditions($qid, $this->message(...));
+            }
+        } else {
+            foreach ($op->getProps()['scenarios'] as $scenario) {
+                //$scid = $scenario['scid'];
+            }
+        }
+        //$preparedData = $this->transformer->transformAll($op->getProps());
+        /*if (empty($preparedData)) {
+            $this->throwNoValuesException($op);
+        }*/
+        /*$questionId = $op->getEntityId();
+        $this->attributesService->saveAdvanced(
+            $this->questionService->getQuestionBySidAndQid(
+                $surveyId,
+                $questionId
+            ),
+            $preparedData
+        );*/
+    }
+
+    protected function validateDeleteAllConditions($props)
+    {
+        //At this point we have already checked everything we needed
+        return true;
+    }
+
+    /**
+     * Checks if patch is valid for this operation.
+     * @param OpInterface $op
+     * @return array
+     */
+    public function validateOperation(OpInterface $op): array
+    {
+        $props = $op->getProps();
+        if (!isset($props['qid'])) {
+            throw new \Exception("Question id is mandatory");
+        }
+        if (isset($props['action'])) {
+            switch ($props['action']) {
+                case 'deleteAllConditions':
+                    if (!$this->validateDeleteAllConditions($props)) {
+                        throw new \Exception("Invalid operation");
+                    }
+            }
+        }
+        return [];
+    }
+}
