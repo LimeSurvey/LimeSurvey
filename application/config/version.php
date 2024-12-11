128973392be88ac25368e88ff663600bde13bfad
Mohab Elsheikh <mohab.elsheikh@limesurvey.org>
2024-12-11 16:50:20 +0100

    Update dbversionnumber to 626

--- application/config/version.php
+++ application/config/version.php
@@ -12,7 +12,7 @@
  */
 
 $config['versionnumber'] = '6.8.2';
-$config['dbversionnumber'] = 623;
+$config['dbversionnumber'] = 626;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
 $config['templateapiversion']  = 3;
