b9bc95a9e37d770633f8b0c96b833429e13e9848
Lajos Arpad <arpad@endpoint.com>
2024-12-12 17:52:15 +0200

    QE-906: Fixed updater

--- application/helpers/update/updates/Update_627.php
+++ application/helpers/update/updates/Update_627.php
@@ -6,7 +6,6 @@ class Update_627 extends DatabaseUpdateBase
 {
     public function up(): void
     {
-        $this->db->createCommand()->addColumn("{{surveys}}", "open_mode", "varchar(1) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'N'");
         $openmode = $this->db->createCommand()
             ->select("COLUMN_NAME")
             ->from("information_schema.columns")
