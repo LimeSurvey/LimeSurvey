137f3786e2879fa020bceb7cfaa1ba7925b255bc
marticos <marticos@gmail.com>
2024-12-20 16:05:30 +0100

    Feature/mw 1407 users cannot download db (#4070)
    * New feature MW-1407: Add check for database size before download
    
    * New feature MW-1407: Add security check before db dump
    
    * MW-1407: Refactor to better check db size
    
    * MW-1407: Add settings to change the size of the db dumps
    
    * MW-1407: Validation for config input field
    
    * MW-1407: Fix for CodeSniffer
    
    * MW-1407: Re-apply fixes for CodeSniffer.
    
    * MW-1407: Add support for plugin
    
    * MW-1407: Fix for whitespace at the end of the line error
    
    * MW-1407: Remove debug comment

--- application/views/admin/dumpdb/dumpdb_view.php
+++ application/views/admin/dumpdb/dumpdb_view.php
@@ -6,41 +6,46 @@
 echo viewHelper::getViewTestTag('dumpdb');
 ?>
 
-<div class="row">
-    <div class="col-12">
-        <!-- Data redundancy check -->
-        <div class="jumbotron message-box">
-            <h2><?php eT("Database size check"); ?></h2>
-            <p class="lead">
-                <?php eT("This check evaluates the database size to determine if an immediate download is possible or if a manual backup is necessary."); ?>
-            </p>
-            <p>
-                <?php echo "DB Size: MB " . $dbSize; ?>
-                <?php if ($downloadable) { // todo: remove testing boolean ?>
-                    <?php
-                    $this->widget('ext.AlertWidget.AlertWidget', [
-                        'text' => gT("Your database can be downloaded now!"),
-                        'type' => 'success',
-                    ]);
-                    ?>
-                    <?php echo CHtml::form(["admin/dumpdb", "sa" => 'outPutDatabase'], 'post'); ?>
-                    <button
-                        type='submit'
-                        value='Y'
-                        name='ok'
-                        class="btn btn-info">
-                        <?php eT("Yes - download now!"); ?>
-                    </button>
-                    </form>
-                <?php } else { ?>
-                    <?php
+<?php if (!$htmlContent) { ?>
+
+    <div class="row">
+        <div class="col-12">
+            <!-- Data redundancy check -->
+            <div class="jumbotron message-box">
+                <h2><?php eT("Database size check"); ?></h2>
+                <p class="lead">
+                    <?php eT("This check evaluates the database size to determine if an immediate download is possible or if a manual backup is necessary."); ?>
+                </p>
+                <p>
+                    <?php echo "DB Size: MB " . $dbSize; ?>
+                    <?php if ($downloadable) { ?>
+                        <?php
                         $this->widget('ext.AlertWidget.AlertWidget', [
-                            'text' => gT("Your database is too large for immediate download. Please use your database client to perform a manual backup."),
-                            'type' => 'warning',
+                            'text' => gT("Your database can be downloaded now!"),
+                            'type' => 'success',
                         ]);
-                    ?>
-                <?php } ?>
-            </p>
+                        ?>
+                        <?php echo CHtml::form(["admin/dumpdb", "sa" => 'outPutDatabase'], 'post'); ?>
+                        <button
+                            type='submit'
+                            value='Y'
+                            name='ok'
+                            class="btn btn-info">
+                            <?php eT("Yes - Download now!"); ?>
+                        </button>
+                        </form>
+                    <?php } else { ?>
+                        <?php
+                            $this->widget('ext.AlertWidget.AlertWidget', [
+                                'text' => gT("Your database is too large for immediate download. Please use your database client to perform a manual backup."),
+                                'type' => 'warning',
+                            ]);
+                        ?>
+                    <?php } ?>
+                </p>
+            </div>
         </div>
     </div>
-</div>
+<?php } else {
+    echo $htmlContent;
+} ?>
