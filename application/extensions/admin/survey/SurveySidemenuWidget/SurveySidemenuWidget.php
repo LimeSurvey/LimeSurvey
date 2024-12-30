761a434ccf1efc7f074457c4bebd15413d968b60
Mohab Elsheikh <mohab.elsheikh@limesurvey.org>
2024-12-30 18:57:17 +0100

    QE-1312: highlight side menu items when sub-item is selected (#4107)

--- application/extensions/admin/survey/SurveySidemenuWidget/SurveySidemenuWidget.php
+++ application/extensions/admin/survey/SurveySidemenuWidget/SurveySidemenuWidget.php
@@ -1,5 +1,7 @@
 <?php
 
+include 'SideMenuActiveItemMapper.php';
+
 class SurveySidemenuWidget extends WhSelect2
 {
     public $sid;
@@ -58,13 +60,11 @@ class SurveySidemenuWidget extends WhSelect2
     public function highlightActiveMenuItem()
     {
         $currentUrl = App()->request->requestUri;
-        foreach ($this->sideMenu as $k => $menu) {
+
+        foreach ($this->sideMenu as $menutype => $menu) {
             foreach ($menu as $i => $menuItem) {
-                if (
-                    $menuItem['url'] == $currentUrl
-                    || str_replace("/index.php", "", $menuItem['url']) == $currentUrl
-                ) {
-                    $this->sideMenu[$k][$i]['selected'] = true;
+                if ((new SideMenuActiveItemMapper)->match($menuItem['url'], $menuItem['name'], $this->sid)) {
+                    $this->sideMenu[$menutype][$i]['selected'] = true;
                 }
             }
         }
