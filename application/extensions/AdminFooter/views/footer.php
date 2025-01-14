b6e1179c3fd2f855138a97deb7921e0d7844d764
Ambrose Ariagiegbe <ambagape@gmail.com>
2025-01-14 17:40:31 +0100

    fix(): Rename app name some more

--- application/extensions/AdminFooter/views/footer.php
+++ application/extensions/AdminFooter/views/footer.php
@@ -2,8 +2,6 @@
     <div style='float:left;width:110px;text-align:left;'>
         <a href='http://manual.gitit-tech.com'><img alt='GititSurvey - <?php eT("Online Manual"); ?>' title='GititSurvey - <?php eT("Online manual"); ?>' src='<?php echo Yii::app()->getConfig('adminimageurl'); ?>docs.png' /></a>
     </div>
-    <div style='float:right;'>
-        <a href='https://donate.cg' target="_blank"><img alt='<?php eT("Support this project - Donate to "); ?>GititSurvey' title='<?php eT("Support this project - Donate to "); ?>GititSurvey!' src='<?php echo Yii::app()->getConfig('adminimageurl'); ?>donate.png'/></a>
-    </div>
-    <div class='subtitle'><a class='subtitle' title='<?php eT("Visit our website!"); ?>' href='https://community.Gitit=tech.com' target='_blank'>GititSurvey Fully Edition</a><br /><?php echo $versiontitle . " " . $versionnumber . $buildtext; ?></div>
+    
+    <div class='subtitle'><a class='subtitle' title='<?php eT("Visit our website!"); ?>' href='https://community.Gitit=tech.com' target='_blank'>GititSurvey</a><br /><?php echo $versiontitle . " " . $versionnumber . $buildtext; ?></div>
 </div>
