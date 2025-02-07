1a194caf9019ab27c993551254ec55b3ce4fe4fd
twilligls <tim.willig@limesurvey.org>
2025-02-07 09:30:38 +0100

    new endpoint to refresh the array of all languages

--- application/config/rest/v1/i18n.php
+++ application/config/rest/v1/i18n.php
@@ -2,14 +2,16 @@
 
 use LimeSurvey\Api\Command\V1\I18n;
 use LimeSurvey\Libraries\Api\Command\V1\I18nMissing;
-use LimeSurvey\Api\Rest\V1\SchemaFactory\{
-    SchemaFactoryError,
+use LimeSurvey\Libraries\Api\Command\V1\I18nRefreshLanguages;
+use LimeSurvey\Libraries\Api\Rest\V1\SchemaFactory\SchemaFactoryI18n;
+use LimeSurvey\Api\Rest\V1\SchemaFactory\{SchemaFactoryError,
     SchemaFactoryI18nMissingTranslations,
     SchemaFactoryI18nMissingTranslationsResponse,
-    SchemaFactoryI18nTranslations};
+    SchemaFactoryI18nRefreshLanguages};
 
 $errorSchema = (new SchemaFactoryError())->make();
-$i18nTranslationsSchema = (new SchemaFactoryI18nTranslations())->make();
+$i18nRefreshLanguagesSchema = (new SchemaFactoryI18nRefreshLanguages())->make();
+$i18nSchema = (new SchemaFactoryI18n())->make();
 $i18nMissingSchemaResponse = (new SchemaFactoryI18nMissingTranslationsResponse())->make();
 $i18nMissingSchema = (new SchemaFactoryI18nMissingTranslations())->make();
 
@@ -19,7 +21,7 @@ $rest['v1/i18n/{lang}'] = [
     'GET' => [
         'tag' => 'i18n',
         'summary' => 'Get translations',
-        'description' => 'Get translations for a specific language',
+        'description' => 'Get all translations for a specific language',
         'operationId' => 'getTranslations',
         'commandClass' => I18n::class,
         'auth' => true,
@@ -39,7 +41,47 @@ $rest['v1/i18n/{lang}'] = [
                 'code' => 200,
                 'description' => 'Success',
                 'content' => null,
-                'schema' => $i18nTranslationsSchema
+                'schema' => $i18nSchema
+            ],
+            'unauthorized' => [
+                'code' => 401,
+                'description' => 'Unauthorized',
+                'schema' => $errorSchema
+            ],
+            'not-found' => [
+                'code' => 404,
+                'description' => 'Not Found',
+                'schema' => $errorSchema
+            ]
+        ]
+    ],
+];
+
+$rest['v1/i18n-refresh-languages/{lang}'] = [
+    'GET' => [
+        'tag' => 'i18n',
+        'summary' => 'Get translated list of all languages',
+        'description' => 'Get translated list of all languages for a specific language',
+        'operationId' => 'getTranslations',
+        'commandClass' => I18nRefreshLanguages::class,
+        'auth' => true,
+        'parameters' => [
+            [
+                'name' => 'lang',
+                'in' => 'path',
+                'required' => true,
+                'schema' => [
+                    'type' => 'string'
+                ],
+                'description' => 'Language code'
+            ]
+        ],
+        'responses' => [
+            'success' => [
+                'code' => 200,
+                'description' => 'Success',
+                'content' => null,
+                'schema' => $i18nRefreshLanguagesSchema
             ],
             'unauthorized' => [
                 'code' => 401,
