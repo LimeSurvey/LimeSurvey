c419a1e847114cdeae8d1c86bdbc89872fbda78e
Tim Willig <92720438+twilligls@users.noreply.github.com>
2025-03-18 14:42:40 +0100

    QE-801 QE-1290 endpoint for translations (#4152)
    * created endpoint for translations
    
    * created endpoint for translations
    
    * changed endpoint for translations
    
    * QE-1289: new service class for mo-files to json for single languages
    
    * QE-1289: new service class for mo-files to json for single languages
    
    * QE-1289: new service class for mo-files to json for single languages
    
    * QE-1289: new service class for mo-files to json for single languages
    
    * fixed param definition in TranslationMoToJson.php
    
    * changed output
    
    * exclude empty string key from translations
    
    * rebuild v1.json
    
    * add adminLang to auth endpoint return
    
    * fix codecheck
    
    * fix codecheck
    
    * fix codecheck
    
    * regenerate v1.json after merge
    
    * changed structure of returned userdetails
    
    * removed adminlang from auth endpoint result
    
    * QE-1288 - endpoint for writing missing translations into PHP file
    
    * QE-1288 - eintermediate result for multiple keys in one call
    
    * QE-1288 - I18nMissing now allows multiple keys in one call
    
    * QE-1288 - I18nMissing now allows multiple keys in one call
    
    * add all languages to i18n endpoint
    
    * new endpoint to refresh the array of all languages
    
    * psalm fixes
    
    * psalm fixes
    
    * psalm fixes
    
    * manipulate translation data to contain versions without colon
    
    * altered missing translations endpoint to avooid false positives and added first result of missing translations
    
    * fix code-check
    
    * fix unit-test
    
    * CHanged/removed some missing translations
    
    * Changed/removed some missing translations
    
    * Changed/removed some missing translations
    
    * Changed/removed some missing translations
    
    * Changed/removed some missing translations
    
    * Changed/removed some missing translations
    
    * Added some missing translations
    
    * fixed some I18nMissing.php stuff for Swagger
    
    * patch of "Fixed issue #19984: No way to update user settings (#4188)"
    
    * removed one missing translation
    
    * added one missing translation
    
    * added more missing  translations
    
    * added more missing  translations
    
    ---------
    
    Co-authored-by: trischi <stelling.pat@gmail.com>

---
+++ application/libraries/Api/Command/V1/I18nMissing.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace LimeSurvey\Libraries\Api\Command\V1;
+
+use LimeSurvey\Models\Services\TranslationMoToJson;
+use Permission;
+use LimeSurvey\Api\Command\{
+    CommandInterface,
+    Request\Request,
+    Response\Response,
+    Response\ResponseFactory
+};
+use LimeSurvey\Api\Command\Mixin\Auth\AuthPermissionTrait;
+
+class I18nMissing implements CommandInterface
+{
+    use AuthPermissionTrait;
+
+    protected ResponseFactory $responseFactory;
+    protected Permission $permission;
+
+    /**
+     * Constructor
+     *
+     * @param ResponseFactory $responseFactory
+     * @param Permission $permission
+     */
+    public function __construct(
+        ResponseFactory $responseFactory,
+        Permission $permission
+    ) {
+        $this->responseFactory = $responseFactory;
+        $this->permission = $permission;
+    }
+
+    /**
+     * Handle missing translations
+     * This will get an array of missing strings which need translation.
+     * Those will be written in the file /application/helpers/newEditorTranslations
+     * as gT('example');
+     * The function checks if each string already exists in the file to avoid duplicates.
+     *
+     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
+     * @param Request $request
+     * @return Response
+     */
+    public function run(Request $request)
+    {
+        $keys = $request->getData('keys');
+
+        if (empty($keys) || !is_array($keys)) {
+            return $this->responseFactory
+                ->makeError('Missing or invalid translation keys');
+        }
+
+        $filename = '/helpers/newEditorTranslations.php';
+        $absolutePath = App()->getBasePath() . $filename;
+
+        $updatedKeys = [];
+        $existingKeys = [];
+        // fetching german to see what is already translated in the core app:
+        App()->setLanguage('de');
+        $transLateService = new TranslationMoToJson('de');
+        $translations = $transLateService->translateMoToJson();
+        if (!file_exists($absolutePath)) {
+            // Create new file with header
+            $content = "<?php\n// Translation-source for new editor. \n// Updated on " . date('Y-m-d H:i:s') . "\n\n";
+            file_put_contents($absolutePath, $content);
+        }
+
+        // Read existing content
+        $content = file_get_contents($absolutePath);
+
+        foreach ($keys as $key) {
+            if (empty($key)) {
+                continue;
+            }
+            // check if key is also a key in array translations, then we can skip it as false positive
+            if (is_array($translations) && array_key_exists($key, $translations)) {
+                $existingKeys[] = $key;
+                continue;
+            }
+
+            $newLine = "gT('" . addslashes($key) . "');";
+
+            // Check if the key already exists
+            if (strpos($content, $newLine) === false) {
+                // Append new line to the file
+                $content = rtrim($content, "\n") . "\n" . $newLine;
+                $updatedKeys[] = $key;
+            } else {
+                $existingKeys[] = $key;
+            }
+        }
+
+        if (!empty($updatedKeys)) {
+            // Update the timestamp
+            $content = preg_replace(
+                '/\/\/ Updated on .*/',
+                '// Updated on ' . date('Y-m-d H:i:s'),
+                $content
+            );
+
+            file_put_contents($absolutePath, $content);
+        }
+
+        $message = '';
+        if (!empty($updatedKeys)) {
+            $message .= "Translation keys saved: " . implode(', ', $updatedKeys) . ". ";
+        }
+        if (!empty($existingKeys)) {
+            $message .= "Translation keys already exist: " . implode(', ', $existingKeys) . ".";
+        }
+
+        return $this->responseFactory
+            ->makeSuccess(['message' => $message]);
+    }
+}
