5018d8056348912e3b9c9c567cce907a19594228
Marco Lombardo <lombardomarco@me.com>
2025-03-20 13:04:42 +0100

    MW1460: Question code numbers must be of 3 digits

--- application/models/QuestionCreate.php
+++ application/models/QuestionCreate.php
@@ -110,8 +110,8 @@ class QuestionCreate extends Question
         $survey = Survey::model()->findByPk($this->sid);
         $group = QuestionGroup::model()->findByPk($this->gid);
         $isTitleValid = false;
-        for ($i = 0; $i <= 9999; $i++) {
-            $this->title = 'Q' . str_pad((safecount($survey->baseQuestions) + $i), 4, '0', STR_PAD_LEFT);
+        for ($i = 0; $i <= 999; $i++) {
+            $this->title = 'Q' . str_pad((safecount($survey->baseQuestions) + $i), 3, '0', STR_PAD_LEFT);
             if ($this->validate(['title'])) {
                 $isTitleValid = true;
                 break;
