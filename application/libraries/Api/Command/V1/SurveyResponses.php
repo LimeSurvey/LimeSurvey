112cc9f0cfd7aa4ed9aa2189414733ff3dc62edd
Mohab Elsheikh <mohab.elsheikh@limesurvey.org>
2025-05-05 16:37:04 +0200

    Refactor SurveyResponses

--- application/libraries/Api/Command/V1/SurveyResponses.php
+++ application/libraries/Api/Command/V1/SurveyResponses.php
@@ -2,7 +2,7 @@
 
 namespace LimeSurvey\Libraries\Api\Command\V1;
 
-use DI\FactoryInterface;
+use LimeSurvey\Api\Transformer\TransformerException;
 use LimeSurvey\Libraries\Api\Command\V1\SurveyResponses\FilterPatcher;
 use Survey;
 use LimeSurvey\Api\Command\{CommandInterface,
@@ -19,32 +19,36 @@ class SurveyResponses implements CommandInterface
     use AuthPermissionTrait;
 
     protected Survey $survey;
-    protected Survey $diFactory;
     protected ResponseFactory $responseFactory;
     protected FilterPatcher $responseFilterPatcher;
     protected TransformerOutputSurveyResponses $transformerOutputSurveyResponses;
     private TransformerOutputQuestion $transformerQuestion;
+    private TransformerOutputQuestionGroup $transformerQuestionGroup;
+
     /**
      * Constructor
      *
      * @param Survey $survey
-     * @param TransformerOutputSurveyResponses $transformerOutputSurveyResponses
+     * @param FilterPatcher $responseFilterPatcher
      * @param ResponseFactory $responseFactory
+     * @param TransformerOutputQuestion $transformerOutputQuestion
+     * @param TransformerOutputQuestionGroup $transformerOutputQuestionGroup
+     * @param TransformerOutputSurveyResponses $transformerOutputSurveyResponses
      */
     public function __construct(
         Survey $survey,
-        TransformerOutputSurveyResponses $transformerOutputSurveyResponses,
         FilterPatcher $responseFilterPatcher,
         ResponseFactory $responseFactory,
         TransformerOutputQuestion $transformerOutputQuestion,
-        TransformerOutputQuestionGroup $transformerOutputQuestionGroup
+        TransformerOutputQuestionGroup $transformerOutputQuestionGroup,
+        TransformerOutputSurveyResponses $transformerOutputSurveyResponses
     ) {
         $this->survey = $survey;
-        $this->transformerOutputSurveyResponses = $transformerOutputSurveyResponses;
         $this->responseFactory = $responseFactory;
         $this->responseFilterPatcher = $responseFilterPatcher;
         $this->transformerQuestion = $transformerOutputQuestion;
         $this->transformerQuestionGroup = $transformerOutputQuestionGroup;
+        $this->transformerOutputSurveyResponses = $transformerOutputSurveyResponses;
     }
 
     /**
@@ -52,41 +56,15 @@ class SurveyResponses implements CommandInterface
      *
      * @param Request $request
      * @return Response
+     * @throws TransformerException
      */
     public function run(Request $request)
     {
-        $surveyId = (string) $request->getData('_id');
-        $searchParams = $request->getData('search', null);
-        $pagination = $request->getData('pagination', [
-            'pageSize' => 15,
-            'currentPage' => 0,
-        ]);
-
-        $model = \SurveyDynamic::model($surveyId);
-
-        $criteria = new \LSDbCriteria();
-        $sort     = new \CSort();
-
-
-//        $searchParams = [
-//            "sort" => [
-//                "id" => 'asc',
-//            ],
-//            "search" => [
-//                [
-//                    "survey" => "198895",
-//                    "group" => "135",
-//                    "question" => "2142",
-//                    "value" => "AO4",
-//                    "type" => "option",
-//                ],
-//            ]
-//        ];
-
-
-        if ($searchParams) {
-            $this->responseFilterPatcher->apply($searchParams, $criteria, $sort);
-        }
+        $this->getSurvey($request);
+        $model = $this->getSurveyDynamicModel($request);
+        $questionGroups = $this->getSurveyGroup();
+        [$criteria, $sort] = $this->buildCriteria($request);
+        $pagination = $this->buildPagination($request);
 
         $dataProvider = new \LSCActiveDataProvider($model, array(
             'sort' => $sort,
@@ -95,42 +73,39 @@ class SurveyResponses implements CommandInterface
         ));
 
         $data = $this->transformerOutputSurveyResponses->transform($dataProvider);
-
-        $questionGroups = $this->survey->findByPk($surveyId)->groups;
-        $data['surveyGroup'] = $this->transform_($questionGroups);
+        $data['surveyGroup'] = $this->transformQuestionGroups($questionGroups);
+        $data['_meta'] = [
+            'pagination' => [
+                'pageSize' => $pagination['pageSize'],
+                'currentPage' => $pagination['currentPage'],
+                'totalItems' => $dataProvider->getTotalItemCount(),
+                'totalPages' => ceil($dataProvider->getTotalItemCount() / $pagination['pageSize']),
+            ],
+            'filters' => $request->getData('search', []),
+            'sort' => $request->getData('sort', []),
+        ];
 
         return $this->responseFactory
             ->makeSuccess(['responses' => $data]);
     }
 
-    public function transform_($data, $options = [])
+    public function transformQuestionGroups($data, $options = []): array
     {
         $options = $options ?? [];
 
-        // transformAll() can apply required entity sort so we must retain the sort order going forward
-        // - We use a lookup array later to access entities without needing to know their position in the collection
         $survey['questionGroups'] = $this->transformerQuestionGroup->transformAll(
             $data,
             $options
         );
 
-        // An array of groups indexed by gid for easy look up
-        // - helps us to retain sort order when looping over models
         $groupLookup = $this->createCollectionLookup(
             'gid',
             $survey['questionGroups']
         );
 
         foreach ($data as $questionGroupModel) {
-            // Order of groups from the model relation may be different than from the transformed data
-            // - so we use the lookup to get a reference to the required entity without needing to
-            // - know its position in the output array
-            // If we don't assign by reference here the, additions to $group will create a new array
-            // - rather than modifying the original array
             $group = &$groupLookup[$questionGroupModel->gid];
 
-            // transformAll() can apply required entity sort so we must retain the sort order going forward
-            // - We use a lookup array later to access entities without needing to know their position in the collection
             $group['questions'] = $this->transformerQuestion->transformAll(
                 $questionGroupModel->questions,
                 $options
@@ -140,7 +115,7 @@ class SurveyResponses implements CommandInterface
         return $survey;
     }
 
-    private function createCollectionLookup($key, &$entityArray)
+    private function createCollectionLookup($key, &$entityArray): array
     {
         $output = [];
         foreach ($entityArray as &$entity) {
@@ -149,5 +124,48 @@ class SurveyResponses implements CommandInterface
             }
         }
         return $output;
+    }
+
+    private function getSurvey($request): void
+    {
+        $this->survey = $this->survey->findByPk($this->getSurveyId($request));
+    }
+
+    private function getSurveyGroup()
+    {
+        return $this->survey->groups ?? [];
+    }
+
+    private function getSurveyId(Request $request): string
+    {
+        $surveyId = (string) $request->getData('_id');
+        if (!is_numeric($surveyId)) {
+            throw new \InvalidArgumentException("Invalid survey ID");
+        }
+        return $surveyId;
+    }
+    private function getSurveyDynamicModel(Request $request): \SurveyDynamic
+    {
+        return \SurveyDynamic::model($this->getSurveyId($request));
+    }
+    private function buildCriteria(Request $request): array
+    {
+        $searchParams = $request->getData('search', null);
+
+        $sort = new \CSort();
+        $criteria = new \LSDbCriteria();
+
+
+        if ($searchParams) {
+            $this->responseFilterPatcher->apply($searchParams, $criteria, $sort);
+        }
+        return [$criteria, $sort];
+    }
+    private function buildPagination(Request $request)
+    {
+        return $request->getData('pagination', [
+            'pageSize' => 15,
+            'currentPage' => 0,
+        ]);
     }
 }
