af96a0d07ba5099322b85c44b8009c6793997c26
Mohab Elsheikh <mohab.elsheikh@limesurvey.org>
2025-05-12 11:35:40 +0200

    dev: remove unused fields

--- application/libraries/Api/Command/V1/SurveyResponses/conditions/OptionConditionHandler.php
+++ application/libraries/Api/Command/V1/SurveyResponses/conditions/OptionConditionHandler.php
@@ -6,8 +6,6 @@ use LimeSurvey\Libraries\Api\Command\V1\SurveyResponses\HandlerInterface;
 
 class OptionConditionHandler implements HandlerInterface
 {
-    private array $order = ['asc', 'desc'];
-
     public function canHandle(array $operation): bool
     {
         if (!empty($operation['type']) && $operation['type'] == 'option') {
