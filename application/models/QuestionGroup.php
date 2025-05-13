f44941df32175c9fd56986ddb7659e9aac3b33b8
twilligls <tim.willig@limesurvey.org>
2025-05-13 10:54:49 +0200

    fixed codestyle

--- application/models/QuestionGroup.php
+++ application/models/QuestionGroup.php
@@ -351,10 +351,10 @@ class QuestionGroup extends LSActiveRecord
                 'data-btnclass'  => 'btn-danger',
                 'data-btntext'   => gT('Delete'),
                 'data-onclick'  => '(function() { ' . CHtml::encode(convertGETtoPOST(
-                        App()->createUrl(
-                            "questionGroupsAdministration/delete/",
-                            ["gid" => $this->gid, "surveyid" => $this->sid]
-                        )
+                    App()->createUrl(
+                        "questionGroupsAdministration/delete/",
+                        ["gid" => $this->gid, "surveyid" => $this->sid]
+                    )
                 )) . '})'
             ]
         ];
