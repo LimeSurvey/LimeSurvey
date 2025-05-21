caca8c9062e566cb790a1a38a425e3107de65e6b
trischi <stelling.pat@gmail.com>
2025-05-21 11:01:49 +0200

    QE-854: short refactoring; put same button in a separate view and reuse it; remove commented out html

---
+++ application/extensions/admin/SearchBoxWidget/views/createSurveyButton.php
@@ -0,0 +1,9 @@
+<?php
+  /** @var $url string */
+
+?>
+
+<a href="<?= $url ?>" id="createSurvey" class="btn btn-outline-info survey-actionbar-button">
+    <i class="ri-add-line"></i>
+    <?= gT('Create survey') ?>
+</a>
--- application/extensions/admin/SearchBoxWidget/views/searchBox.php
+++ application/extensions/admin/SearchBoxWidget/views/searchBox.php
@@ -41,24 +41,16 @@
                         </select>
                     </div>
                 </div>
-
+                <a href="<?= Yii::app()->createUrl('surveyAdministration/newSurvey') ?>" id="createSurvey" class="btn btn-outline-info survey-actionbar-button">
+                    <i class="ri-add-line"></i>
+                    <?= gT('Create survey') ?>
+                </a>
                 <div class="survey-actionbar-actions ms-auto">
                     <div class="survey-actionbar-item">
-                        <?php if (Permission::model()->hasGlobalPermission('surveys', 'create')) : ?>
-                            <a href="<?= Yii::app()->createUrl('surveyAdministration/newSurvey') ?>" id="createSurvey" class="btn btn-outline-info survey-actionbar-button">
-                                <i class="ri-add-line"></i>
-                                <?= gT('Create survey') ?>
-                            </a>
-                        <?php endif; ?>
+                        <?php if (Permission::model()->hasGlobalPermission('surveys', 'create')) {
+                                $this->renderPartial('createSurveyButton', ['url' => Yii::app()->createUrl('surveyAdministration/newSurvey')]);
+                        }?>
                     </div>
-                    <!--<div class="survey-actionbar-item">
-                        <?php if (\Permission::model()->hasGlobalPermission('surveysgroups', 'create')) : ?>
-                            <a href="<?= Yii::app()->createUrl('admin/surveysgroups/sa/create') ?>" id="createSurveyGroup" class="btn btn-outline-g-700 survey-actionbar-button">
-                                <i class="ri-add-line"></i>
-                                <?= gT('Create survey group') ?>
-                            </a>
-                        <?php endif; ?>
-                    </div>-->
                 </div>
 
             <?php else : ?>
@@ -109,21 +101,10 @@
 
                 <div class="survey-actionbar-actions ms-auto">
                     <div class="survey-actionbar-item">
-                        <?php if (Permission::model()->hasGlobalPermission('surveys', 'create')) : ?>
-                            <a href="<?= Yii::app()->createUrl('surveyAdministration/newSurvey') ?>" class="btn btn-outline-info survey-actionbar-button">
-                                <i class="ri-add-line"></i>
-                                <?= gT('Create survey') ?>
-                            </a>
-                        <?php endif; ?>
+                        <?php if (Permission::model()->hasGlobalPermission('surveys', 'create')) {
+                            $this->renderPartial('createSurveyButton', ['url' => Yii::app()->createUrl('surveyAdministration/newSurvey')]);
+                        } ?>
                     </div>
-                    <!--<div class="survey-actionbar-item">
-                        <?php if (Permission::model()->hasGlobalPermission('surveysgroups', 'create')) : ?>
-                            <a href="<?= Yii::app()->createUrl('admin/surveysgroups/sa/create') ?>" class="btn btn-outline-g-700 survey-actionbar-button">
-                                <i class="ri-add-line"></i>
-                                <?= gT('Create survey group') ?>
-                            </a>
-                        <?php endif; ?>
-                    </div>-->
                 </div>
             <?php endif; ?>
 
