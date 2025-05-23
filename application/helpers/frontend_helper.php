c3da46c972bcbe6496f567926e8d7c1c1bde2da2
Alain Lima <3238557+AlainL88@users.noreply.github.com>
2025-05-23 15:35:24 +0200

    Update frontend_helper.php
    ErrorMsg updated as DenisChenu comment in https://bugs.limesurvey.org/view.php?id=20121

--- application/helpers/frontend_helper.php
+++ application/helpers/frontend_helper.php
@@ -1295,7 +1295,7 @@ function testIfTokenIsValid(array $subscenarios, array $thissurvey, array $aEnte
                 }
             } else {
                 //token was wrong
-                $errorMsg    = gT("The access code you have provided is incorrect.");
+                $errorMsg    = gT("The access code you have provided is either not valid.");
                 $FlashError .= $errorMsg;
                 $renderToken = 'main';
                 FailedLoginAttempt::model()->addAttempt(FailedLoginAttempt::TYPE_TOKEN);
