ac9d0d9d959740b319736279db1a70e5739b43cc
Lajos Arpad <lajos.arpad85@gmail.com>
2025-06-04 13:30:28 +0300

    APE-101: Adjusted the script

--- application/helpers/update/updates/Update_632.php
+++ application/helpers/update/updates/Update_632.php
@@ -6,16 +6,19 @@ class Update_632 extends DatabaseUpdateBase
 {
     public function up()
     {
-        addColumn('{{surveys}}', 'access_mode', "string(1) DEFAULT 'O'");
-        $sids = [];
-        foreach (dbGetTablesLike('%token%') as $table) {
-            if (strpos($table, "old") === false) {
-                $split = explode("_", $table);
-                $sids[] = $split[count($split) - 1];
+        $columnNames = \Yii::app()->db->schema->getTable('{{surveys}}')->columnNames;
+        if (!in_array('access_mode', $columnNames)) {
+            addColumn('{{surveys}}', 'access_mode', "string(1) DEFAULT 'O'");
+            $sids = [];
+            foreach (dbGetTablesLike('%token%') as $table) {
+                if (strpos($table, "old") === false) {
+                    $split = explode("_", $table);
+                    $sids[] = $split[count($split) - 1];
+                }
             }
-        }
-        if (count($sids)) {
-            $this->db->createCommand()->update("{{surveys}}", ["access_mode" => "C"], "sid in (" . implode(",", $sids) . ")");
+            if (count($sids)) {
+                $this->db->createCommand()->update("{{surveys}}", ["access_mode" => "C"], "sid in (" . implode(",", $sids) . ")");
+            }
         }
     }
 }
