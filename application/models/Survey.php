5eb200cbe2f8b5b76ea192f2c46c8f22ed019334
Tim Willig <92720438+twilligls@users.noreply.github.com>
2025-06-16 09:31:23 +0200

    Dev: AT-1663 SurveyDetail output needs to contain othersettings (with inheritance) (#4318)
    * SurveyDetail output needs to contain othersettings (with inheritance)
    
    * Fix logic issue (unnecessary else clause)
    
    * Make sure decodedParentSettings is object
    
    * null safety in addOtherSettings
    
    * AT-1663: Fix for null value for othersettings
    
    ---------
    
    Co-authored-by: Marco Lombardo <lombardomarco@me.com>

--- application/models/Survey.php
+++ application/models/Survey.php
@@ -2605,7 +2605,7 @@ class Survey extends LSActiveRecord implements PermissionInterface
      */
     public function setOtherSetting($attribute, $value)
     {
-        $othersettings = json_decode($this->othersettings, true) ?? [];
+        $othersettings = json_decode($this->othersettings ?? '', true) ?? [];
         $othersettings[$attribute] = $value;
         $this->othersettings = json_encode($othersettings);
     }
@@ -2658,5 +2658,29 @@ class Survey extends LSActiveRecord implements PermissionInterface
             $this->addError($attribute, $rule['message']);
         }
         return (bool)$isValid;
+    }
+
+    /**
+     * Retrieves prefix othersettings as an associative array.
+     *
+     * This method collects all the survey's othersettings (prefixes for question codes,
+     * subquestion codes, and answer codes) and returns them as an array.
+     *
+     * @return array An associative array containing the other settings with keys:
+     *               'question_code_prefix', 'subquestion_code_prefix', and 'answer_code_prefix'
+     */
+    public function getOtherSettingsPrefixArray()
+    {
+        $otherSettings['question_code_prefix'] = $this->getOtherSetting(
+            'question_code_prefix'
+        );
+        $otherSettings['subquestion_code_prefix'] = $this->getOtherSetting(
+            'subquestion_code_prefix'
+        );
+        $otherSettings['answer_code_prefix'] = $this->getOtherSetting(
+            'answer_code_prefix'
+        );
+
+        return $otherSettings;
     }
 }
