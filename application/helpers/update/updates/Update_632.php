6b1bf78feda88a354075dc80740663bedb100d14
Mohab Elsheikh <mohab.elsheikh@limesurvey.org>
2025-06-18 14:48:39 +0200

    dev: refactor

--- application/helpers/update/updates/Update_632.php
+++ application/helpers/update/updates/Update_632.php
@@ -4,9 +4,21 @@ namespace LimeSurvey\Helpers\Update;
 
 class Update_632 extends DatabaseUpdateBase
 {
+    /**
+     * @throws \CDbException
+     */
     public function up()
     {
-        $this->db->createCommand()->addColumn('{{surveys}}', 'lastModified', 'datetime NULL');
+        switch ($this->db->driverName) {
+            case 'mysql':
+            case 'mssql':
+                addColumn('{{surveys}}', 'lastModified', 'datetime current_timestamp');
+                break;
+            case 'pgsql':
+                addColumn('{{surveys}}', 'lastModified', 'timestamp current_timestamp');
+                break;
+        }
+
         foreach ($this->getTriggers() as $trigger) {
             $this->db->createCommand($trigger)->execute();
         }
@@ -14,91 +26,154 @@ class Update_632 extends DatabaseUpdateBase
 
     private function getTriggers(): array
     {
-        return App()->db->driverName === 'pgsql'
-            ? $this->getPostgresTriggers()
-            : $this->getMySqlTriggers();
-    }
-
-    private function getPostgresTriggers(): array
-    {
+        $dbType = $this->db->driverName;
         return [
-            $this->triggerAnswers(),
-            $this->triggerGroupL10ns(),
-            $this->triggerGroups(),
-            $this->triggerQuestionL10ns(),
-            $this->triggerQuestions(),
-            $this->triggerSurveys(),
-            $this->triggerLanguageSettings(),
+            $this->triggerAnswers($dbType),
+            $this->triggerGroupL10ns($dbType),
+            $this->triggerGroups($dbType),
+            $this->triggerQuestionL10ns($dbType),
+            $this->triggerQuestions($dbType),
+            $this->triggerSurveys($dbType),
+            $this->triggerLanguageSettings($dbType),
         ];
     }
 
-    private function triggerAnswers(): string
+
+    private function triggerAnswers($dbType): string
     {
-        return <<<SQL
+        $prefix = App()->db->tablePrefix;
+        if ($dbType == 'mysql') {
+            return <<<SQL
+CREATE TRIGGER `lime_answers_last_modified` BEFORE UPDATE ON {$prefix}lime_answers
+FOR EACH ROW BEGIN
+    DECLARE survey_id INT;
+    SELECT sid INTO survey_id FROM {$prefix}lime_questions WHERE lime_questions.qid = NEW.qid;
+    UPDATE {$prefix}lime_surveys SET lastModified = NOW() WHERE {$prefix}lime_surveys.sid = survey_id;
+END;
+SQL;
+        }
+        elseif ($dbType == 'mssql')
+        {
+        }
+        elseif ($dbType == 'pgsql')
+        {
+            return <<<SQL
 CREATE OR REPLACE FUNCTION update_survey_last_modified_from_answers()
 RETURNS TRIGGER AS \$\$
 DECLARE
     survey_id INT;
 BEGIN
-    SELECT sid INTO survey_id FROM lime_questions WHERE qid = NEW.qid;
+    SELECT sid INTO survey_id FROM {$prefix}lime_questions WHERE qid = NEW.qid;
     IF survey_id IS NOT NULL THEN
-        UPDATE lime_surveys SET "lastModified" = NOW() WHERE sid = survey_id;
+        UPDATE {$prefix}lime_surveys SET "lastModified" = NOW() WHERE sid = survey_id;
     END IF;
     RETURN NEW;
 END;
 \$\$ LANGUAGE plpgsql;
 
 CREATE TRIGGER lime_answers_last_modified
-BEFORE UPDATE ON lime_answers
+BEFORE UPDATE ON {$prefix}lime_answers
 FOR EACH ROW
 EXECUTE FUNCTION update_survey_last_modified_from_answers();
 SQL;
+        }
     }
 
-    private function triggerGroupL10ns(): string
+    private function triggerGroupL10ns($dbType): string
     {
-        return <<<SQL
+        $prefix = App()->db->tablePrefix;
+        if ($dbType == 'mysql') {
+            return <<<SQL
+CREATE TRIGGER `lime_group_l10ns_last_modified` BEFORE UPDATE ON {$prefix}lime_group_l10ns
+FOR EACH ROW BEGIN
+    DECLARE survey_id INT;
+    SELECT sid INTO survey_id FROM {$prefix}lime_groups WHERE {$prefix}lime_groups.gid = NEW.gid;
+    UPDATE {$prefix}lime_surveys SET lastModified = NOW() WHERE {$prefix}lime_surveys.sid = survey_id;
+END;
+SQL;
+        }
+        elseif ($dbType == 'mssql')
+        {
+
+        }
+        elseif ($dbType == 'pgsql')
+        {
+            return  <<<SQL
 CREATE OR REPLACE FUNCTION update_survey_last_modified_from_group_l10ns()
 RETURNS TRIGGER AS \$\$
 DECLARE
     survey_id INT;
 BEGIN
-    SELECT sid INTO survey_id FROM lime_groups WHERE gid = NEW.gid;
+    SELECT sid INTO survey_id FROM {$prefix}lime_groups WHERE gid = NEW.gid;
     IF survey_id IS NOT NULL THEN
-        UPDATE lime_surveys SET "lastModified" = NOW() WHERE sid = survey_id;
+        UPDATE {$prefix}lime_surveys SET "lastModified" = NOW() WHERE sid = survey_id;
     END IF;
     RETURN NEW;
 END;
 \$\$ LANGUAGE plpgsql;
 
 CREATE TRIGGER lime_group_l10ns_last_modified
-BEFORE UPDATE ON lime_group_l10ns
+BEFORE UPDATE ON {$prefix}lime_group_l10ns
 FOR EACH ROW
 EXECUTE FUNCTION update_survey_last_modified_from_group_l10ns();
 SQL;
+        }
     }
 
-    private function triggerGroups(): string
+    private function triggerGroups($dbType): string
     {
-        return <<<SQL
+        $prefix = App()->db->tablePrefix;
+        if ($dbType == 'mysql') {
+            return  <<<SQL
+CREATE TRIGGER `lime_groups_last_modified` BEFORE UPDATE ON {$prefix}lime_groups
+FOR EACH ROW BEGIN
+    UPDATE {$prefix}lime_surveys SET lastModified = NOW() WHERE lime_surveys.sid = NEW.sid;
+END;
+SQL;
+        }
+        elseif ($dbType == 'mssql')
+        {
+
+        }
+        elseif ($dbType == 'pgsql')
+        {
+            return  <<<SQL
 CREATE OR REPLACE FUNCTION update_survey_last_modified_from_groups()
 RETURNS TRIGGER AS \$\$
 BEGIN
-    UPDATE lime_surveys SET "lastModified" = NOW() WHERE sid = NEW.sid;
+    UPDATE {$prefix}lime_surveys SET "lastModified" = NOW() WHERE sid = NEW.sid;
     RETURN NEW;
 END;
 \$\$ LANGUAGE plpgsql;
 
 CREATE TRIGGER lime_groups_last_modified
-BEFORE UPDATE ON lime_groups
+BEFORE UPDATE ON {$prefix}lime_groups
 FOR EACH ROW
 EXECUTE FUNCTION update_survey_last_modified_from_groups();
 SQL;
+        }
     }
 
-    private function triggerQuestionL10ns(): string
+    private function triggerQuestionL10ns($dbType): string
     {
-        return <<<SQL
+        $prefix = App()->db->tablePrefix;
+        if ($dbType == 'mysql') {
+            return  <<<SQL
+CREATE TRIGGER `lime_question_l10ns_last_modified` BEFORE UPDATE ON `lime_question_l10ns`
+FOR EACH ROW BEGIN
+    DECLARE survey_id INT;
+    SELECT sid INTO survey_id FROM lime_questions WHERE lime_questions.qid = NEW.qid;
+    UPDATE lime_surveys SET lastModified = NOW() WHERE lime_surveys.sid = survey_id;
+END;
+SQL;
+        }
+        elseif ($dbType == 'mssql')
+        {
+
+        }
+        elseif ($dbType == 'pgsql')
+        {
+            return  <<<SQL
 CREATE OR REPLACE FUNCTION update_survey_last_modified_from_question_l10ns()
 RETURNS TRIGGER AS \$\$
 DECLARE
@@ -117,11 +192,27 @@ BEFORE UPDATE ON lime_question_l10ns
 FOR EACH ROW
 EXECUTE FUNCTION update_survey_last_modified_from_question_l10ns();
 SQL;
+        }
     }
 
-    private function triggerQuestions(): string
+    private function triggerQuestions($dbType): string
     {
-        return <<<SQL
+        $prefix = App()->db->tablePrefix;
+        if ($dbType == 'mysql') {
+            return  <<<SQL
+CREATE TRIGGER `lime_questions_last_modified` BEFORE UPDATE ON `lime_questions`
+FOR EACH ROW BEGIN
+    UPDATE lime_surveys SET lastModified = NOW() WHERE lime_surveys.sid = NEW.sid;
+END;
+SQL;
+        }
+        elseif ($dbType == 'mssql')
+        {
+
+        }
+        elseif ($dbType == 'pgsql')
+        {
+            return  <<<SQL
 CREATE OR REPLACE FUNCTION update_survey_last_modified_from_questions()
 RETURNS TRIGGER AS \$\$
 BEGIN
@@ -135,11 +226,26 @@ BEFORE UPDATE ON lime_questions
 FOR EACH ROW
 EXECUTE FUNCTION update_survey_last_modified_from_questions();
 SQL;
+        }
     }
 
-    private function triggerSurveys(): string
+    private function triggerSurveys($dbType): string
     {
-        return <<<SQL
+        $prefix = App()->db->tablePrefix;
+        if ($dbType == 'mysql') {
+            return  <<<SQL
+CREATE TRIGGER `lime_surveys_last_modified` BEFORE UPDATE ON `lime_surveys`
+FOR EACH ROW BEGIN
+    SET NEW.lastModified = NOW();
+END;
+SQL;}
+        elseif ($dbType == 'mssql')
+        {
+
+        }
+        elseif ($dbType == 'pgsql')
+        {
+            return  <<<SQL
 CREATE OR REPLACE FUNCTION update_last_modified()
 RETURNS TRIGGER AS \$\$
 BEGIN
@@ -153,11 +259,26 @@ BEFORE UPDATE ON lime_surveys
 FOR EACH ROW
 EXECUTE FUNCTION update_last_modified();
 SQL;
+        }
     }
 
-    private function triggerLanguageSettings(): string
+    private function triggerLanguageSettings($dbType): string
     {
-        return <<<SQL
+        $prefix = App()->db->tablePrefix;
+        if ($dbType == 'mysql') {
+            return  <<<SQL
+CREATE TRIGGER `lime_surveys_languagesettings_last_modified` BEFORE UPDATE ON `lime_surveys_languagesettings`
+FOR EACH ROW BEGIN
+    UPDATE lime_surveys SET lastModified = NOW() WHERE lime_surveys.sid = NEW.surveyls_survey_id;
+END;
+SQL;}
+        elseif ($dbType == 'mssql')
+        {
+
+        }
+        elseif ($dbType == 'pgsql')
+        {
+            return  <<<SQL
 CREATE OR REPLACE FUNCTION update_survey_last_modified()
 RETURNS TRIGGER AS \$\$
 BEGIN
@@ -171,65 +292,6 @@ BEFORE UPDATE ON lime_surveys_languagesettings
 FOR EACH ROW
 EXECUTE FUNCTION update_survey_last_modified();
 SQL;
-    }
-
-    private function getMySqlTriggers(): array
-    {
-        return [
-            <<<SQL
-CREATE TRIGGER `lime_answers_last_modified` BEFORE UPDATE ON `lime_answers`
-FOR EACH ROW BEGIN
-    DECLARE survey_id INT;
-    SELECT sid INTO survey_id FROM lime_questions WHERE lime_questions.qid = NEW.qid;
-    UPDATE lime_surveys SET lastModified = NOW() WHERE lime_surveys.sid = survey_id;
-END;
-SQL,
-
-            <<<SQL
-CREATE TRIGGER `lime_group_l10ns_last_modified` BEFORE UPDATE ON `lime_group_l10ns`
-FOR EACH ROW BEGIN
-    DECLARE survey_id INT;
-    SELECT sid INTO survey_id FROM lime_groups WHERE lime_groups.gid = NEW.gid;
-    UPDATE lime_surveys SET lastModified = NOW() WHERE lime_surveys.sid = survey_id;
-END;
-SQL,
-
-            <<<SQL
-CREATE TRIGGER `lime_groups_last_modified` BEFORE UPDATE ON `lime_groups`
-FOR EACH ROW BEGIN
-    UPDATE lime_surveys SET lastModified = NOW() WHERE lime_surveys.sid = NEW.sid;
-END;
-SQL,
-
-            <<<SQL
-CREATE TRIGGER `lime_question_l10ns_last_modified` BEFORE UPDATE ON `lime_question_l10ns`
-FOR EACH ROW BEGIN
-    DECLARE survey_id INT;
-    SELECT sid INTO survey_id FROM lime_questions WHERE lime_questions.qid = NEW.qid;
-    UPDATE lime_surveys SET lastModified = NOW() WHERE lime_surveys.sid = survey_id;
-END;
-SQL,
-
-            <<<SQL
-CREATE TRIGGER `lime_questions_last_modified` BEFORE UPDATE ON `lime_questions`
-FOR EACH ROW BEGIN
-    UPDATE lime_surveys SET lastModified = NOW() WHERE lime_surveys.sid = NEW.sid;
-END;
-SQL,
-
-            <<<SQL
-CREATE TRIGGER `lime_surveys_last_modified` BEFORE UPDATE ON `lime_surveys`
-FOR EACH ROW BEGIN
-    SET NEW.lastModified = NOW();
-END;
-SQL,
-
-            <<<SQL
-CREATE TRIGGER `lime_surveys_languagesettings_last_modified` BEFORE UPDATE ON `lime_surveys_languagesettings`
-FOR EACH ROW BEGIN
-    UPDATE lime_surveys SET lastModified = NOW() WHERE lime_surveys.sid = NEW.surveyls_survey_id;
-END;
-SQL,
-        ];
+        }
     }
 }
