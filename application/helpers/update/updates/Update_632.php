507a13c093af44ddb2e0ee2a9bace2af12789e88
Mohab Elsheikh <mohab.elsheikh@limesurvey.org>
2025-06-23 13:03:16 +0200

    LA-129: optimize query

--- application/helpers/update/updates/Update_632.php
+++ application/helpers/update/updates/Update_632.php
@@ -4,11 +4,15 @@ namespace LimeSurvey\Helpers\Update;
 
 class Update_632 extends DatabaseUpdateBase
 {
+    private string $prefix;
+
     /**
      * @throws \CDbException
      */
     public function up()
     {
+        $this->prefix = App()->db->tablePrefix;
+
         switch ($this->db->driverName) {
             case 'mysql':
             case 'mssql':
@@ -39,290 +43,243 @@ class Update_632 extends DatabaseUpdateBase
         ];
     }
 
-
     private function triggerAnswers($dbType): string
     {
-        $prefix = App()->db->tablePrefix;
         if ($dbType == 'mssql' || $dbType == 'sqlsrv') {
             return <<<SQL
 CREATE TRIGGER answers_last_modified
-ON [{$prefix}answers]
-AFTER UPDATE
-AS
+ON [{$this->prefix}answers]
+AFTER UPDATE AS
 BEGIN
-    SET NOCOUNT ON;
-    DECLARE @survey_id INT;
-
-    SELECT TOP 1 @survey_id = q.sid
-    FROM [{$prefix}questions] q
-    JOIN inserted i ON q.qid = i.qid;
-
-    UPDATE [{$prefix}surveys]
-    SET lastModified = GETDATE()
-    WHERE sid = @survey_id;
+    UPDATE s SET s.lastModified = GETDATE()
+    FROM [{$this->prefix}surveys] s
+    INNER JOIN [{$this->prefix}questions] q ON s.sid = q.sid
+    INNER JOIN inserted i ON q.qid = i.qid;
 END;
 SQL;
         } elseif ($dbType == 'pgsql') {
             return <<<SQL
-CREATE OR REPLACE FUNCTION update_survey_last_modified_from_answers()
-RETURNS TRIGGER AS \$\$
-DECLARE
-    survey_id INT;
+CREATE OR REPLACE FUNCTION answers_last_modified()
+RETURNS TRIGGER AS $$
 BEGIN
-    SELECT sid INTO survey_id FROM {$prefix}questions WHERE qid = NEW.qid;
-    IF survey_id IS NOT NULL THEN
-        UPDATE {$prefix}surveys SET "lastModified" = NOW() WHERE sid = survey_id;
-    END IF;
+    UPDATE {$this->prefix}surveys s SET "lastModified" = NOW()
+    FROM {$this->prefix}questions q
+    WHERE q.qid = NEW.qid AND q.sid = s.sid;
     RETURN NEW;
 END;
-\$\$ LANGUAGE plpgsql;
+$$ LANGUAGE plpgsql;
 
 CREATE TRIGGER answers_last_modified
-BEFORE UPDATE ON {$prefix}answers
-FOR EACH ROW
-EXECUTE FUNCTION update_survey_last_modified_from_answers();
+BEFORE UPDATE ON {$this->prefix}answers
+FOR EACH ROW EXECUTE FUNCTION answers_last_modified();
 SQL;
         }
 
         return <<<SQL
-CREATE TRIGGER `answers_last_modified` BEFORE UPDATE ON {$prefix}answers
+CREATE TRIGGER `answers_last_modified` 
+BEFORE UPDATE ON {$this->prefix}answers
 FOR EACH ROW BEGIN
-    DECLARE survey_id INT;
-    SELECT sid INTO survey_id FROM {$prefix}questions WHERE {$prefix}questions.qid = NEW.qid;
-    UPDATE {$prefix}surveys SET lastModified = NOW() WHERE {$prefix}surveys.sid = survey_id;
-END;
+    UPDATE {$this->prefix}surveys s JOIN {$this->prefix}questions q ON q.sid = s.sid
+    SET s.lastModified = NOW() WHERE q.qid = NEW.qid;
+END
 SQL;
     }
 
     private function triggerGroupL10ns($dbType): string
     {
-        $prefix = App()->db->tablePrefix;
         if ($dbType == 'mssql' || $dbType == 'sqlsrv') {
             return <<<SQL
-            CREATE TRIGGER group_l10ns_last_modified
-ON [{$prefix}group_l10ns]
-AFTER UPDATE
-AS
+CREATE TRIGGER group_l10ns_last_modified
+ON [{$this->prefix}group_l10ns]
+AFTER UPDATE AS
 BEGIN
-    SET NOCOUNT ON;
-
-    UPDATE s
-    SET s.lastModified = GETDATE()
-    FROM [{$prefix}surveys] s
-    JOIN [{$prefix}groups] g ON s.sid = g.sid
+    UPDATE s SET s.lastModified = GETDATE()
+    FROM [{$this->prefix}surveys] s
+    JOIN [{$this->prefix}groups] g ON s.sid = g.sid
     JOIN inserted i ON g.gid = i.gid;
 END;
 SQL;
         } elseif ($dbType == 'pgsql') {
             return  <<<SQL
-CREATE OR REPLACE FUNCTION update_survey_last_modified_from_group_l10ns()
-RETURNS TRIGGER AS \$\$
-DECLARE
-    survey_id INT;
+CREATE OR REPLACE FUNCTION group_l10ns_last_modified()
+RETURNS TRIGGER AS $$
 BEGIN
-    SELECT sid INTO survey_id FROM {$prefix}groups WHERE gid = NEW.gid;
-    IF survey_id IS NOT NULL THEN
-        UPDATE {$prefix}surveys SET "lastModified" = NOW() WHERE sid = survey_id;
-    END IF;
+    UPDATE {$this->prefix}surveys s SET "lastModified" = NOW()
+    FROM {$this->prefix}groups g WHERE g.gid = NEW.gid AND g.sid = s.sid;
     RETURN NEW;
 END;
-\$\$ LANGUAGE plpgsql;
+$$ LANGUAGE plpgsql;
 
 CREATE TRIGGER group_l10ns_last_modified
-BEFORE UPDATE ON {$prefix}group_l10ns
-FOR EACH ROW
-EXECUTE FUNCTION update_survey_last_modified_from_group_l10ns();
+BEFORE UPDATE ON {$this->prefix}group_l10ns
+FOR EACH ROW EXECUTE FUNCTION group_l10ns_last_modified();
 SQL;
         }
 
         return <<<SQL
-CREATE TRIGGER `group_l10ns_last_modified` BEFORE UPDATE ON {$prefix}group_l10ns
+CREATE TRIGGER `group_l10ns_last_modified`
+BEFORE UPDATE ON {$this->prefix}group_l10ns
 FOR EACH ROW BEGIN
-    DECLARE survey_id INT;
-    SELECT sid INTO survey_id FROM {$prefix}groups WHERE {$prefix}groups.gid = NEW.gid;
-    UPDATE {$prefix}surveys SET lastModified = NOW() WHERE {$prefix}surveys.sid = survey_id;
-END;
+    UPDATE {$this->prefix}surveys s JOIN {$this->prefix}groups g ON g.sid = s.sid
+    SET s.lastModified = NOW() WHERE g.gid = NEW.gid;
+END
 SQL;
     }
 
     private function triggerGroups($dbType): string
     {
-        $prefix = App()->db->tablePrefix;
         if ($dbType == 'mssql' || $dbType == 'sqlsrv') {
             return  <<<SQL
 CREATE TRIGGER groups_last_modified
-ON [{$prefix}groups]
-AFTER UPDATE
-AS
+ON [{$this->prefix}groups]
+AFTER UPDATE AS
 BEGIN
-    SET NOCOUNT ON;
-
-    UPDATE s
-    SET s.lastModified = GETDATE()
-    FROM [{$prefix}surveys] s
+    UPDATE s SET s.lastModified = GETDATE()
+    FROM [{$this->prefix}surveys] s
     JOIN inserted i ON s.sid = i.sid;
 END;
 SQL;
         } elseif ($dbType == 'pgsql') {
             return  <<<SQL
-CREATE OR REPLACE FUNCTION update_survey_last_modified_from_groups()
-RETURNS TRIGGER AS \$\$
+CREATE OR REPLACE FUNCTION groups_last_modified()
+RETURNS TRIGGER AS $$
 BEGIN
-    UPDATE {$prefix}surveys SET "lastModified" = NOW() WHERE sid = NEW.sid;
+    UPDATE {$this->prefix}surveys SET "lastModified" = NOW() WHERE sid = NEW.sid;
     RETURN NEW;
 END;
-\$\$ LANGUAGE plpgsql;
+$$ LANGUAGE plpgsql;
 
 CREATE TRIGGER groups_last_modified
-BEFORE UPDATE ON {$prefix}groups
-FOR EACH ROW
-EXECUTE FUNCTION update_survey_last_modified_from_groups();
+BEFORE UPDATE ON {$this->prefix}groups
+FOR EACH ROW EXECUTE FUNCTION groups_last_modified();
 SQL;
         }
 
         return  <<<SQL
-CREATE TRIGGER `groups_last_modified` BEFORE UPDATE ON {$prefix}groups
+CREATE TRIGGER `groups_last_modified`
+BEFORE UPDATE ON {$this->prefix}groups
 FOR EACH ROW BEGIN
-    UPDATE {$prefix}surveys SET lastModified = NOW() WHERE {$prefix}surveys.sid = NEW.sid;
+    UPDATE {$this->prefix}surveys SET lastModified = NOW() 
+    WHERE {$this->prefix}surveys.sid = NEW.sid;
 END;
 SQL;
     }
 
     private function triggerQuestionL10ns($dbType): string
     {
-        $prefix = App()->db->tablePrefix;
         if ($dbType == 'mssql' || $dbType == 'sqlsrv') {
             return  <<<SQL
 CREATE TRIGGER question_l10ns_last_modified
-ON [{$prefix}question_l10ns]
-AFTER UPDATE
-AS
+ON [{$this->prefix}question_l10ns]
+AFTER UPDATE AS
 BEGIN
-    SET NOCOUNT ON;
-
-    UPDATE s
-    SET s.lastModified = GETDATE()
-    FROM [{$prefix}surveys] s
-    JOIN [{$prefix}questions] q ON s.sid = q.sid
+    UPDATE s SET s.lastModified = GETDATE()
+    FROM [{$this->prefix}surveys] s
+    JOIN [{$this->prefix}questions] q ON s.sid = q.sid
     JOIN inserted i ON q.qid = i.qid;
 END;
 
 SQL;
         } elseif ($dbType == 'pgsql') {
             return  <<<SQL
-CREATE OR REPLACE FUNCTION update_survey_last_modified_from_question_l10ns()
-RETURNS TRIGGER AS \$\$
-DECLARE
-    survey_id INT;
+CREATE OR REPLACE FUNCTION question_l10ns_last_modified()
+RETURNS TRIGGER AS $$
 BEGIN
-    SELECT sid INTO survey_id FROM {$prefix}questions WHERE qid = NEW.qid;
-    IF survey_id IS NOT NULL THEN
-        UPDATE {$prefix}surveys SET "lastModified" = NOW() WHERE sid = survey_id;
-    END IF;
+    UPDATE {$this->prefix}surveys s SET "lastModified" = NOW()
+    FROM {$this->prefix}questions q WHERE q.qid = NEW.qid AND s.sid = q.sid;
     RETURN NEW;
 END;
-\$\$ LANGUAGE plpgsql;
+$$ LANGUAGE plpgsql;
 
 CREATE TRIGGER question_l10ns_last_modified
-BEFORE UPDATE ON {$prefix}question_l10ns
-FOR EACH ROW
-EXECUTE FUNCTION update_survey_last_modified_from_question_l10ns();
+BEFORE UPDATE ON {$this->prefix}question_l10ns
+FOR EACH ROW EXECUTE FUNCTION question_l10ns_last_modified();
 SQL;
         }
 
         return  <<<SQL
-CREATE TRIGGER `question_l10ns_last_modified` BEFORE UPDATE ON {$prefix}question_l10ns
+CREATE TRIGGER `question_l10ns_last_modified`
+BEFORE UPDATE ON {$this->prefix}question_l10ns
 FOR EACH ROW BEGIN
-    DECLARE survey_id INT;
-    SELECT sid INTO survey_id FROM {$prefix}questions WHERE {$prefix}questions.qid = NEW.qid;
-    UPDATE surveys SET lastModified = NOW() WHERE {$prefix}surveys.sid = survey_id;
-END;
+    UPDATE {$this->prefix}surveys s JOIN {$this->prefix}questions q ON q.sid = s.sid
+    SET s.lastModified = NOW() WHERE q.qid = NEW.qid;
+END
 SQL;
     }
 
     private function triggerQuestions($dbType): string
     {
-        $prefix = App()->db->tablePrefix;
         if ($dbType == 'mssql' || $dbType == 'sqlsrv') {
             return  <<<SQL
 CREATE TRIGGER questions_last_modified
-ON [{$prefix}questions]
-AFTER UPDATE
-AS
+ON [{$this->prefix}questions]
+AFTER UPDATE AS
 BEGIN
-    SET NOCOUNT ON;
-
-    UPDATE s
-    SET s.lastModified = GETDATE()
-    FROM [{$prefix}surveys] s
+    UPDATE s SET s.lastModified = GETDATE()
+    FROM [{$this->prefix}surveys] s
     JOIN inserted i ON s.sid = i.sid;
 END;
 
 SQL;
         } elseif ($dbType == 'pgsql') {
             return  <<<SQL
-CREATE OR REPLACE FUNCTION update_survey_last_modified_from_questions()
-RETURNS TRIGGER AS \$\$
+CREATE OR REPLACE FUNCTION questions_last_modified()
+RETURNS TRIGGER AS $$
 BEGIN
-    UPDATE {$prefix}surveys SET "lastModified" = NOW() WHERE sid = NEW.sid;
+    UPDATE {$this->prefix}surveys SET "lastModified" = NOW() WHERE sid = NEW.sid;
     RETURN NEW;
 END;
-\$\$ LANGUAGE plpgsql;
+$$ LANGUAGE plpgsql;
 
 CREATE TRIGGER questions_last_modified
-BEFORE UPDATE ON {$prefix}questions
-FOR EACH ROW
-EXECUTE FUNCTION update_survey_last_modified_from_questions();
+BEFORE UPDATE ON {$this->prefix}questions
+FOR EACH ROW EXECUTE FUNCTION questions_last_modified();
 SQL;
         }
 
         return  <<<SQL
-CREATE TRIGGER `questions_last_modified` BEFORE UPDATE ON {$prefix}questions
+CREATE TRIGGER `questions_last_modified`
+BEFORE UPDATE ON {$this->prefix}questions
 FOR EACH ROW BEGIN
-    UPDATE {$prefix}surveys SET lastModified = NOW() WHERE {$prefix}surveys.sid = NEW.sid;
+    UPDATE {$this->prefix}surveys SET lastModified = NOW() 
+    WHERE {$this->prefix}surveys.sid = NEW.sid;
 END;
 SQL;
     }
 
     private function triggerSurveys($dbType): string
     {
-        $prefix = App()->db->tablePrefix;
         if ($dbType == 'mssql' || $dbType == 'sqlsrv') {
             return  <<<SQL
-CREATE TRIGGER [{$prefix}surveys_last_modified]
-ON [{$prefix}surveys]
-AFTER UPDATE
-AS
+CREATE TRIGGER surveys_last_modified
+ON [{$this->prefix}surveys]
+AFTER UPDATE AS
 BEGIN
-    SET NOCOUNT ON;
-
-    UPDATE s
-    SET 
-        s.lastModified = GETDATE()
-    FROM [{$prefix}surveys] s
+    UPDATE s SET s.lastModified = GETDATE()
+    FROM [{$this->prefix}surveys] s
     JOIN inserted i ON s.sid = i.sid;
 END;
 
 SQL;
         } elseif ($dbType == 'pgsql') {
             return  <<<SQL
-CREATE OR REPLACE FUNCTION update_last_modified()
-RETURNS TRIGGER AS \$\$
+CREATE OR REPLACE FUNCTION surveys_last_modified()
+RETURNS TRIGGER AS $$
 BEGIN
     NEW."lastModified" := NOW();
     RETURN NEW;
 END;
-\$\$ LANGUAGE plpgsql;
+$$ LANGUAGE plpgsql;
 
 CREATE TRIGGER surveys_last_modified
-BEFORE UPDATE ON {$prefix}surveys
-FOR EACH ROW
-EXECUTE FUNCTION update_last_modified();
+BEFORE UPDATE ON {$this->prefix}surveys
+FOR EACH ROW EXECUTE FUNCTION surveys_last_modified();
 SQL;
         }
 
         return  <<<SQL
-CREATE TRIGGER {$prefix}surveys_last_modified BEFORE UPDATE ON {$prefix}surveys
+CREATE TRIGGER surveys_last_modified
+BEFORE UPDATE ON {$this->prefix}surveys
 FOR EACH ROW BEGIN
     SET NEW.lastModified = NOW();
 END;
@@ -331,43 +288,40 @@ SQL;
 
     private function triggerLanguageSettings($dbType): string
     {
-        $prefix = App()->db->tablePrefix;
         if ($dbType == 'mssql' || $dbType == 'sqlsrv') {
             return  <<<SQL
-CREATE TRIGGER surveys_languagesettings_last_modified
-ON [{$prefix}surveys_languagesettings]
-AFTER UPDATE
-AS
+CREATE TRIGGER languagesettings_last_modified
+ON [{$this->prefix}surveys_languagesettings]
+AFTER UPDATE AS
 BEGIN
-    SET NOCOUNT ON;
-
-    UPDATE s
-    SET s.lastModified = GETDATE()
-    FROM [{$prefix}surveys] s
+    UPDATE s SET s.lastModified = GETDATE()
+    FROM [{$this->prefix}surveys] s
     JOIN inserted i ON s.sid = i.surveyls_survey_id;
 END;
 SQL;
         } elseif ($dbType == 'pgsql') {
             return  <<<SQL
-CREATE OR REPLACE FUNCTION update_survey_last_modified()
-RETURNS TRIGGER AS \$\$
+CREATE OR REPLACE FUNCTION languagesettings_last_modified()
+RETURNS TRIGGER AS $$
 BEGIN
-    UPDATE {$prefix}surveys SET "lastModified" = NOW() WHERE sid = NEW.surveyls_survey_id;
+    UPDATE {$this->prefix}surveys SET "lastModified" = NOW() 
+    WHERE sid = NEW.surveyls_survey_id;
     RETURN NEW;
 END;
-\$\$ LANGUAGE plpgsql;
+$$ LANGUAGE plpgsql;
 
-CREATE TRIGGER surveys_languagesettings_last_modified
-BEFORE UPDATE ON {$prefix}surveys_languagesettings
-FOR EACH ROW
-EXECUTE FUNCTION update_survey_last_modified();
+CREATE TRIGGER languagesettings_last_modified
+BEFORE UPDATE ON {$this->prefix}surveys_languagesettings
+FOR EACH ROW EXECUTE FUNCTION languagesettings_last_modified();
 SQL;
         }
 
         return  <<<SQL
-CREATE TRIGGER `surveys_languagesettings_last_modified` BEFORE UPDATE ON {$prefix}surveys_languagesettings
+CREATE TRIGGER `languagesettings_last_modified`
+BEFORE UPDATE ON {$this->prefix}surveys_languagesettings
 FOR EACH ROW BEGIN
-    UPDATE {$prefix}surveys SET lastModified = NOW() WHERE {$prefix}surveys.sid = NEW.surveyls_survey_id;
+    UPDATE {$this->prefix}surveys SET lastModified = NOW() 
+    WHERE {$this->prefix}surveys.sid = NEW.surveyls_survey_id;
 END;
 SQL;
     }
