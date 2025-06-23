d5d9a601b314b556f30f2b00a4c7b8fed22778c1
c-schmitz <carsten.schmitz@limesurvey.org>
2025-06-23 15:40:53 +0200

    Release 6.15.0+250623
    (cherry picked from commit c5f45aa82be3e766dc58d9c046b22df12ea606c3)

--- application/config/version.php
+++ application/config/version.php
@@ -16,5 +16,5 @@ $config['dbversionnumber'] = 630;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
 $config['templateapiversion']  = 3;
-$config['assetsversionnumber'] = '30437';
+$config['assetsversionnumber'] = '30438';
 return $config;
