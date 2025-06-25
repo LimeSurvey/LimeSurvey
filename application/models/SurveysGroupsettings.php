36e1a1ee013204bbec931bfd6cd0c16e4a3367c1
Denis Chenu <contact@sondages.pro>
2025-06-25 16:17:44 +0200

    Dev:  Creation of dynamic property SurveysGroupsettings::$othersettings is deprecated

--- application/models/SurveysGroupsettings.php
+++ application/models/SurveysGroupsettings.php
@@ -514,7 +514,10 @@ class SurveysGroupsettings extends LSActiveRecord
         $this->admin = substr((string) App()->getConfig('siteadminname'), 0, 50);
         $this->adminemail = substr((string) App()->getConfig('siteadminemail'), 0, 254);
         $this->template = Template::templateNameFilter(App()->getConfig('defaulttheme'));
-        $this->othersettings = '';
+        if (App()->getConfig("DBVersion") >= 631) {
+            /* avoid  Creation of dynamic property is deprecated */
+            $this->othersettings = '';
+        }
     }
 
     /**
