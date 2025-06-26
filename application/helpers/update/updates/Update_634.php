a50119ce5576fe40b75a76a6212b5217cb5ff7c8
Marco Lombardo <lombardomarco@me.com>
2025-06-26 09:48:14 +0000

    AT-1671: Refactor update script

--- application/helpers/update/updates/Update_634.php
+++ application/helpers/update/updates/Update_634.php
@@ -12,10 +12,41 @@ class Update_634 extends DatabaseUpdateBase
             'answer_code_prefix' => 'I'
         ];
         $other_settings_inherit_json = json_encode($other_settings_inherit);
+
+        // Get all surveys with non-null othersettings
+        $surveys = $this->db->createCommand()
+            ->select(['sid', 'othersettings'])
+            ->from('{{surveys}}')
+            ->queryAll();
+
+        // Process each survey
+        foreach ($surveys as $survey) {
+            $settings = $survey['othersettings'];
+
+            if ($settings === null) {
+                $this->updateSettings($other_settings_inherit_json, $survey);
+                continue;
+            }
+
+            $settings = json_decode($settings, true);
+
+            // Check and update empty prefix values
+            foreach (['question_code_prefix', 'subquestion_code_prefix', 'answer_code_prefix'] as $prefixKey) {
+                if (isset($settings[$prefixKey]) && $settings[$prefixKey] === '') {
+                    $settings[$prefixKey] = 'I';
+                }
+            }
+            $this->updateSettings($settings, $survey);
+        }
+    }
+
+    function updateSettings($settings, $survey)
+    {
         $this->db->createCommand()->update(
             '{{surveys}}',
-            ['othersettings' => $other_settings_inherit_json],
-            'othersettings IS NULL'
+            ['othersettings' => json_encode($settings)],
+            'sid = :sid',
+            [':sid' => $survey['sid']]
         );
     }
 }
