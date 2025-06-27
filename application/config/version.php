9ff061c187d2aaaa2f6f9464a2bf0fbee9588ac9
Patrick Teichmann <patrick.teichmann@limesurvey.org>
2025-06-27 18:47:49 +0200

    revert develop merge

--- application/config/version.php
+++ application/config/version.php
@@ -12,7 +12,7 @@
  */
 
 $config['versionnumber'] = '6.15.1';
-$config['dbversionnumber'] = 633;
+$config['dbversionnumber'] = 634;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
 $config['templateapiversion']  = 3;
