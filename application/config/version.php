b4fec83ffa001bc8d45be1a55a87a6d29f3f9cc5
Patrick Teichmann <patrick.teichmann@limesurvey.org>
2025-07-01 13:36:53 +0200

    Dev: changed dbversion back to 634

--- application/config/version.php
+++ application/config/version.php
@@ -12,7 +12,7 @@
  */
 
 $config['versionnumber'] = '6.15.2';
-$config['dbversionnumber'] = 630;
+$config['dbversionnumber'] = 634;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
 $config['templateapiversion']  = 3;
