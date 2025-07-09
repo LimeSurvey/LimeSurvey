6045cab0f01870ccb0badb18c95a4d7e983b3195
Lajos Arpad <lajos.arpad85@gmail.com>
2025-07-09 15:44:50 +0300

    Revert "Dev: [database] Run triggers during initial setup (#4353)"
    This reverts commit c01b41e62cbaf067bbd9506ba896ffe7e8c24c2e.

--- installer/create-database.php
+++ installer/create-database.php
@@ -1,7 +1,5 @@
 <?php
 
-use LimeSurvey\Helpers\Update\triggers\TriggersManager;
-
 /**
  * @param $oDB DbConnection
  *
@@ -1186,9 +1184,6 @@ function populateDatabase($oDB)
                 'resend_vars' => "mediumtext NOT NULL"
             ]
         );
-
-        $triggersManager = new TriggersManager($oDB);
-        $triggersManager->execute();
 
         // Set database version
         $oDB->createCommand()->insert("{{settings_global}}", ['stg_name' => 'DBVersion' , 'stg_value' => $databaseCurrentVersion]);
