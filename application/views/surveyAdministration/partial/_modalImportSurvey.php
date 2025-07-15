5f8b1589c1edbb6c1a48b72064639d486af6058c
trischi <stelling.pat@gmail.com>
2025-07-15 09:50:45 +0200

    QE-1561: reordered the form. Now its possible to import a survey.

--- application/views/surveyAdministration/partial/_modalImportSurvey.php
+++ application/views/surveyAdministration/partial/_modalImportSurvey.php
@@ -6,7 +6,16 @@
     <div class="modal-dialog">
         <!-- Modal content-->
         <div class="modal-content">
-
+            <?php
+            echo CHtml::form(array('surveyAdministration/import'),
+                'post', array(
+                    'id' => 'importsurvey',
+                    'name' => 'importsurvey',
+                    'class' => '',
+                    'enctype' => 'multipart/form-data',
+                    'onsubmit' => 'return window.LS.validatefilename(this,"' . gT('Please select a file to import!', 'js') . '");'
+                )
+            ); ?>
             <?php
             //modal header
             App()->getController()->renderPartial(
@@ -14,21 +23,8 @@
                 ['modalTitle' => gT('Import survey')]
             );
             ?>
-
             <div class="modal-body" id="modal-body-import-survey">
-                <?php
-                echo CHtml::form(array('surveyAdministration/import'),
-                    'post', array(
-                        'id' => 'importsurvey',
-                        'name' => 'importsurvey',
-                        'class' => '',
-                        'enctype' => 'multipart/form-data',
-                        'onsubmit' => 'return window.LS.validatefilename(this,"' . gT('Please select a file to import!', 'js') . '");'
-                    )
-                ); ?>
                 <div class="row">
-
-                    <!-- Select file -->
                     <div class="mb-3">
                         <label class='form-label ' for='the_file'>
                             <?php printf(gT("Select survey structure file (*.lss, *.txt) or survey archive (*.lsa) (maximum file size: %01.2f MB)"), getMaximumFileUploadSize() / 1024 / 1024); ?>
@@ -37,25 +33,18 @@
                             <input id='the_file' name="the_file" class="form-control" type="file" accept='.lss,.lsa,.tsv,.txt' onchange="$('#import-submit').attr('disabled', false).attr('data-bs-toggle', false);" required />
                         </div>
                     </div>
-
                     <div class='mb-3'>
                         <input id="yttranslinksfields" name="translinksfields" type="hidden" value="0">
                         <input id="translinksfields" name="translinksfields" type="checkbox" value="1" checked>
                         <label class='form-label ' for='translinksfields'><?php eT("Convert resource links and expression fields?"); ?> </label>
                     </div>
                 </div>
-                <?php CHtml::endForm();?>
             </div>
-
             <div class="modal-footer">
-                <!-- <input type='submit' id="import-submit" class="btn btn-primary col-6" value='<?php eT("Import survey"); ?>' /> -->
                 <button type="button" class="btn btn-cancel" data-bs-dismiss="modal"><?php eT("Cancel"); ?></button>
-
-                <button id="saveactivateBtn" type="button" class="btn btn-info" >
-                    <?php eT("Import survey"); ?>
-                </button>
+                <input type='submit' id="import-submit" class="btn btn-info col-6" value='<?php eT("Import survey"); ?>' />
             </div>
-
+            </form>
         </div>
     </div>
 </div>
