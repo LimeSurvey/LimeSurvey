d2e3aaa3f8e8dd5f8d38934a936a5542391a9ba8
haythemch2 <haythem.chibani@gmail.com>
2025-07-17 08:57:44 +0100

    Dev: fix indentation

--- application/models/services/embeds/StandardEmbed.php
+++ application/models/services/embeds/StandardEmbed.php
@@ -6,16 +6,15 @@ use LimeSurvey\Models\Services\embeds\BaseEmbed;
 
 class StandardEmbed extends BaseEmbed
 {
-        /**
-         * Gets the HTML wrapper around the main structure
-         * @param string $placeholder a text placeholder with a default value which will be replaced with the inner structure
-         * @return string
-         */
-        protected function getWrapper(string $placeholder = "PLACEHOLDER")
-        {
-            $width = ($this->embedOptions['width'] ?? $this->defaultWidth);
-            $height = ($this->embedOptions['height'] ?? $this->defaultHeight);
-
-            return "<div style='width:{$width};height:{$height};border:1px solid red;'>{$placeholder}</div>";
-        }
+    /**
+     * Gets the HTML wrapper around the main structure
+     * @param string $placeholder a text placeholder with a default value which will be replaced with the inner structure
+     * @return string
+     */
+    protected function getWrapper(string $placeholder = "PLACEHOLDER")
+    {
+        $width = ($this->embedOptions['width'] ?? $this->defaultWidth);
+        $height = ($this->embedOptions['height'] ?? $this->defaultHeight);
+        return "<div style='width:{$width};height:{$height};border:1px solid red;'>{$placeholder}</div>";
+    }
 }
