645ed5c0cb2bd9a1af534871925d13b0ba488a63
twilligls <tim.willig@limesurvey.org>
2025-08-05 12:03:58 +0200

    Fixed issue AT-1706: survey activation modal bug

---
+++ application/views/surveyAdministration/surveyActivation/_activateFooterBtns.php
@@ -0,0 +1,9 @@
+<?php
+
+?>
+
+<button type="button" class="btn btn-cancel" data-bs-dismiss="modal"><?php eT("Cancel"); ?></button>
+
+<button id="saveactivateBtn" type="button" class="btn btn-primary" onclick="activateWithOptions()">
+    <?php eT("Save and activate"); ?>
+</button>
