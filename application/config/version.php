7d25fb1d770de9eab28bfe422682469450ea32ee
Denis Chenu <contact@sondages.pro>
2025-09-03 17:33:56 +0200

    Dev: update assetsversionnumber to force update of assets

--- application/config/version.php
+++ application/config/version.php
@@ -16,5 +16,5 @@ $config['dbversionnumber'] = 636;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
 $config['templateapiversion']  = 3;
-$config['assetsversionnumber'] = '30448';
+$config['assetsversionnumber'] = '30449';
 return $config;
