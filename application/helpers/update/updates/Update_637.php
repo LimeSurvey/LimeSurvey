1270873c81479219c75ae4373503042f1c4050a6
twilligls <tim.willig@limesurvey.org>
2025-09-04 16:27:47 +0200

    removed othersettings columns from surveys and surveys_groupsettings

---
+++ application/helpers/update/updates/Update_637.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace LimeSurvey\Helpers\Update;
+
+class Update_637 extends DatabaseUpdateBase
+{
+    public function up()
+    {
+       dropColumn('{{surveys}}', 'othersettings'  );
+       dropColumn('{{surveys_groupsettings}}', 'othersettings'  );
+    }
+}
