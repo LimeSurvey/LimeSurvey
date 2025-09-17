e4fa5ad250abb38f8d0e10d6ecb4ed2fe56ae541
twilligls <tim.willig@limesurvey.org>
2025-09-17 15:07:00 +0200

    - removed random_order setting from M and P types - Migration of random_order = 1 attributes to subquestion_order = 'random'

--- application/config/version.php
+++ application/config/version.php
@@ -12,7 +12,7 @@
  */
 
 $config['versionnumber'] = '6.15.12';
-$config['dbversionnumber'] = 636;
+$config['dbversionnumber'] = 637;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
 $config['templateapiversion']  = 3;
