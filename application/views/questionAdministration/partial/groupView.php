afcbfb2be2f89436ec9ecb42d7006d20a606d22f
Anblik Audit Team <anblikauditteam@gmail.com>
2025-09-22 13:32:44 +0530

    fix Table caption not announced

--- application/views/questionAdministration/partial/groupView.php
+++ application/views/questionAdministration/partial/groupView.php
@@ -87,6 +87,7 @@
             'ext.admin.grid.CLSGridView', //done
             [
                 'id'              => 'question-group-grid',
+                'caption'        => gT("List of question groups in this survey"),
                 'dataProvider'    => $groupModel->search(),
                 'emptyText'       => gT('No question groups found.'),
                 'summaryText'     => gT('Displaying {start}-{end} of {count} result(s).') . ' ' . sprintf(
