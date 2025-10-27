c95b3b4b5bf64bed0553b521842386c3ff5e7cde
Raquel Casado <raquelcasadodias@gmail.com>
2025-10-27 12:02:07 -0300

    LE-418-removed-multiple-script-loading

--- application/views/surveyAdministration/surveySummary_view.php
+++ application/views/surveyAdministration/surveySummary_view.php
@@ -12,7 +12,7 @@ if (!isset($iSurveyID)) {
     $iSurveyID = $oSurvey->sid;
 }
 
-App()->getClientScript()->registerScriptFile(App()->getConfig('adminscripts') . 'surveysettings.js', LSYii_ClientScript::POS_BEGIN);
+App()->getClientScript()->registerScriptFile(App()->getConfig('adminscripts') . 'surveysettings.js');
 
 // DO NOT REMOVE This is for automated testing to validate we see that page
 echo viewHelper::getViewTestTag('surveySummary');
