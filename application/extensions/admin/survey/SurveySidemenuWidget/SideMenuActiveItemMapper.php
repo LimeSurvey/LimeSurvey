e394761393de93be8403f61f0af60d4574b2f5ad
twilligls <tim.willig@limesurvey.org>
2025-10-30 15:10:00 +0100

    show correct active item in new sidemenu

--- application/extensions/admin/survey/SurveySidemenuWidget/SideMenuActiveItemMapper.php
+++ application/extensions/admin/survey/SurveySidemenuWidget/SideMenuActiveItemMapper.php
@@ -25,29 +25,59 @@ class SideMenuActiveItemMapper extends WhSelect2
 
     public function matchSubItems()
     {
-        if (
-            array_key_exists($this->menutype, $this->getAllowedSubItems())
-            && in_array($this->currentUrl, $this->getAllowedSubItems()[$this->menutype]['urls'])
-        ) {
-            return true;
+        if (array_key_exists($this->menutype, $this->getAllowedSubItems())) {
+            $normalizedCurrentUrl = str_replace("/index.php", "", $this->currentUrl);
+
+            foreach ($this->getAllowedSubItems()[$this->menutype]['urls'] as $url) {
+                $normalizedUrl = str_replace("/index.php", "", $url);
+                if ($normalizedCurrentUrl === $normalizedUrl) {
+                    return true;
+                }
+            }
+
+            // Check if there's a pattern match
+            if (isset($this->getAllowedSubItems()[$this->menutype]['pattern'])) {
+                $pattern = $this->getAllowedSubItems()[$this->menutype]['pattern'];
+                if (strpos($normalizedCurrentUrl, $pattern) !== false) {
+                    return true;
+                }
+            }
         }
+        return false;
     }
 
+    /**
+     * Subitems are additional pages which belong to the main category,
+     * but have different urls than the provided menuItemUrl.
+     * We can add specific urls here, and/or general patterns.
+     * @return array
+     */
     public function getAllowedSubItems()
     {
         return array(
-            'statistics' =>  [
-                'urls' => [
-                    App()->createUrl('/admin/statistics/sa/simpleStatistics/surveyid/' . $this->surveyid),
-                ]
+            'statistics' => [
+                'pattern' => '/admin/statistics/sa/',
+                'urls' => []
             ],
             'participants' => [
+                'pattern' => '/admin/tokens',
+                'urls' => []
+            ],
+            'responses' => [
+                'pattern' => '/responses/',
                 'urls' => [
-                    App()->createUrl("admin/tokens/sa/managetokenattributes/surveyid/$this->surveyid"),
-                    App()->createUrl("admin/tokens/sa/exportdialog/surveyid/$this->surveyid")
+                    App()->createUrl("admin/export/sa/exportresults/surveyid/$this->surveyid"),
+                    App()->createUrl("admin/export/sa/exportspss/sid/$this->surveyid"),
+                    App()->createUrl("admin/export/sa/vvexport/surveyid/$this->surveyid"),
+                    App()->createUrl("admin/dataentry/sa/vvimport/surveyid/$this->surveyid"),
+                    App()->createUrl("admin/dataentry/sa/iteratesurvey/surveyid/$this->surveyid"),
+                    App()->createUrl("admin/dataentry/sa/import/surveyid/$this->surveyid"),
                 ]
+            ],
+            'quotas' => [
+                'pattern' => '/quotas/',
+                'urls' => []
             ]
         );
     }
-
 }
