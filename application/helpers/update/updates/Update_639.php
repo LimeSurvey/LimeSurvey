e3bc4edcc4678bfd45c5d4ff95e3da5ff608e105
Lajos Arpad <lajos.arpad85@gmail.com>
2025-11-10 17:05:07 +0200

    Fix

--- application/helpers/update/updates/Update_639.php
+++ application/helpers/update/updates/Update_639.php
@@ -6,20 +6,5 @@ class Update_639 extends DatabaseUpdateBase
 {
     public function up()
     {
-        $db = \Yii::app()->db;
-        $columnNames = $db->schema->getTable('{{surveys}}')->columnNames;
-
-        if (!in_array('lastmodified', $columnNames, true)) {
-            // Step 1: Add column allowing NULL.
-            addColumn('{{surveys}}', 'lastmodified', 'datetime NULL');
-
-            // Step 2: Fill it with data using the current UTC time as default.
-            $db->createCommand(
-                'UPDATE {{surveys}} SET lastmodified = :lastmodified WHERE lastmodified IS NULL'
-            )->bindValue(':lastmodified', gmdate('Y-m-d H:i:s'))->execute();
-
-            // Step 3: Set column to NOT NULL.
-            alterColumn('{{surveys}}', 'lastmodified', 'datetime', false);
-        }
     }
 }
