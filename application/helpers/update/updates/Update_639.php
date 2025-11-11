30e4068c8ffdcf0064ccb6501c335e8a14274013
Lajos Arpad <lajos.arpad85@gmail.com>
2025-11-12 01:43:48 +0200

    Added a comment to the empty update helper

--- application/helpers/update/updates/Update_639.php
+++ application/helpers/update/updates/Update_639.php
@@ -6,5 +6,6 @@ class Update_639 extends DatabaseUpdateBase
 {
     public function up()
     {
+        //On cloud we had a change script for sharing features, but it was cloud-only and we need an empty change script here to be in sync with that
     }
 }
