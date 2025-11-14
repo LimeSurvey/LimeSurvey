563d8bcbba76eddbfe90b5f82f03191dd82cc614
twilligls <tim.willig@limesurvey.org>
2025-11-14 11:59:27 +0100

    put submenu on the left when there is no space on the right

--- application/config/version.php
+++ application/config/version.php
@@ -16,5 +16,5 @@ $config['dbversionnumber'] = 639;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
 $config['templateapiversion']  = 3;
-$config['assetsversionnumber'] = '30461';
+$config['assetsversionnumber'] = '30462';
 return $config;
