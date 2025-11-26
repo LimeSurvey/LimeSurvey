93a0bd9e82277cc502e1eb60da9763cd39e0fb86
Anblik Audit Team <anblikauditteam@gmail.com>
2025-11-26 17:37:41 +0530

    fix Incorrect heading level announced

--- application/views/layouts/partial_modals/modal_header.php
+++ application/views/layouts/partial_modals/modal_header.php
@@ -3,6 +3,6 @@
 ?>
 
 <div class="modal-header">
-    <h1 class="modal-title"><?php echo $modalTitle; ?></h1>
+    <h1 class="modal-title" aria-level="2"><?php echo $modalTitle; ?></h1>
     <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
 </div>
