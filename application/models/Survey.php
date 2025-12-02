1de2f750c1462ee133c9bae77c89d586bafc0e91
Richard <richard.aljaste@ut.ee>
2025-12-02 19:44:27 +0200

    Fixed issue #20369: "Add user" button on list surveys view leads to general user management view instead of survey permissions view (#4566)

--- application/models/Survey.php
+++ application/models/Survey.php
@@ -1538,7 +1538,7 @@ class Survey extends LSActiveRecord implements PermissionInterface
         ];
         $dropdownItems[] = [
             'title' => gT('Add user'),
-            'url' => App()->createUrl("/userManagement"),
+            'url' => App()->createUrl('/surveyPermissions/index', ['surveyid' => $this->sid]),
             'enabledCondition' => $permissions['survey_update'],
         ];
 
