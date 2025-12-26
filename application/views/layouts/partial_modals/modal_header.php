4b80be135852d9016ca84ba98ed23f99e3d07395
Anblik Audit Team <anblikauditteam@gmail.com>
2025-12-26 14:02:54 +0530

    comment Fixed

--- application/views/layouts/partial_modals/modal_header.php
+++ application/views/layouts/partial_modals/modal_header.php
@@ -3,6 +3,8 @@
 ?>
 
 <div class="modal-header">
-    <h1  aria-level="2" class="modal-title"><?php echo $modalTitle; ?></h1>
+    <h2 class="modal-title h1" id="modalTitle">
+        <?php echo $modalTitle; ?>
+    </h2>
     <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
 </div>
