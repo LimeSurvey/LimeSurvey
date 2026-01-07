b724483a03024bd5b439ff99db2aa4a1c30c17b2
haythemch2 <haythem.chibani@gmail.com>
2026-01-07 13:46:37 +0100

    LE-579: revert InsertansTags in Answer.php

--- application/models/Answer.php
+++ application/models/Answer.php
@@ -184,7 +184,7 @@ class Answer extends LSActiveRecord
     {
         $criteria = new CDbCriteria();
         $criteria->compare('question.sid', $newsid);
-        $criteria->with = ['answerl10ns' => array('condition' => "answer like '%{INSERTANS::Q%'"), 'question'];
+        $criteria->with = ['answerl10ns' => array('condition' => "answer like '%{INSERTANS::{$oldsid}X%'"), 'question'];
         return $this->findAll($criteria);
     }
 
