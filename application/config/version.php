6682a707a40f33196917885e8e1ab497c9634fb0
twilligls <tim.willig@limesurvey.org>
2026-01-09 17:49:21 +0100

    increase assetsversionnumber

--- application/config/version.php
+++ application/config/version.php
@@ -16,5 +16,5 @@ $config['dbversionnumber'] = 640;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
 $config['templateapiversion']  = 3;
-$config['assetsversionnumber'] = '30466';
+$config['assetsversionnumber'] = '30467';
 return $config;
