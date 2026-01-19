b75de51db820d497414e3f7be14dfb46d79565d2
Anblik Audit Team <anblikauditteam@gmail.com>
2026-01-19 13:10:20 +0530

    fix comment

--- application/views/layouts/partial_modals/modal_header.php
+++ application/views/layouts/partial_modals/modal_header.php
@@ -3,6 +3,6 @@
 ?>
 
 <div class="modal-header">
-    <h2 class="modal-title h1"><?php echo $modalTitle; ?></h1>
+    <h2 class="modal-title h1"><?php echo $modalTitle; ?></h2>
     <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
 </div>
