486ab10c878e894d98bf472c1b03632c6212bab0
Anblik Audit Team <anblikauditteam@gmail.com>
2026-01-30 17:29:38 +0530

    Fixed issue: A11y adjust modal header title for test email (#4604)
    * fix Incorrect heading level announced for the heading
    
    * fix Focus indicator is not visible on the "X" close button
    
    * fix comment
    
    * Dev: apply aria label to test email modal
    
    * Dev: remove redundant code
    
    * Dev: remove redundant code
    
    ---------
    
    Co-authored-by: Patrick Teichmann <patrick.teichmann@limesurvey.org>

--- application/views/admin/globalsettings/_emailTestConfirmation.php
+++ application/views/admin/globalsettings/_emailTestConfirmation.php
@@ -1,6 +1,6 @@
 <div class="modal-header">
-    <h5 class="modal-title"><?php eT("Test email settings"); ?></h5>
-    <button aria-label="close modal" type="button" class="btn-close" data-bs-dismiss="modal"></button>
+    <h2 class="modal-title h5"><?php eT("Test email settings"); ?></h2>
+    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="<?= gT('Close') ?>"></button>
 </div>
 <div class="modal-body">
     <p><?php printf(gT("Test email will be sent to: %s"), "<strong>" . $testEmail . "</strong>"); ?></p>
