c30604ba8e78b5e35b66bc66e8a1f4aec82c7551
Lajos Arpad <lajos.arpad85@gmail.com>
2026-02-18 13:40:31 +0200

    LE-611: Created empty change script

---
+++ application/helpers/update/updates/Update_642.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace LimeSurvey\Helpers\Update;
+
+class Update_642 extends DatabaseUpdateBase
+{
+    /**
+     * @inheritDoc
+     * @throws CException
+     */
+    public function up()
+    {
+    }
+}
