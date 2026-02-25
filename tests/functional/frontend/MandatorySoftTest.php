031debc0acfee4a874a53014ff811bb3e9c75027
Denis Chenu <denis@sondages.pro>
2026-02-25 14:58:05 +0100

    Dev: disable popup in theme Dev: remove new test

--- tests/functional/frontend/MandatorySoftTest.php
+++ tests/functional/frontend/MandatorySoftTest.php
@@ -104,24 +104,12 @@ class MandatorySoftTest extends TestBaseClassWeb
                 !empty(self::$webDriver->findElement(WebDriverBy::cssSelector('#question' . $ManOnQid . ' .ls-question-mandatory.text-danger'))),
                 'Mandatory tip don\'t have text-danger class'
             );
-            /* Close the dialog box */
-            $modalCloseButton = self::$webDriver->wait(10)->until(
-                WebDriverExpectedCondition::elementToBeClickable(
-                    WebDriverBy::cssSelector('#bootstrap-alert-box-modal .modal-header .btn-close')
-                )
-            );
-            $modalCloseButton->click();
-            /* Wait for modal close */
-            $buttonSubmit = self::$webDriver->wait(10)->until(
-                WebDriverExpectedCondition::elementToBeClickable(
-                    WebDriverBy::id('ls-button-submit')
-                )
-            );
             /* Enter value in ManOn and check if move next show end (using id added manually in survey) */
+            /* No dialog box due to theme option */
             /* manSoft have a checkbox name mandSoft checked in help text */
             self::$webDriver->answerTextQuestion($ManOnSgqa, 'Some value');
             self::$webDriver->scrollToBottom();
-            $buttonSubmit->click();
+            self::$webDriver->next();
             /** @var $surveyCompletedElement RemoteWebElement */
             $surveyCompletedElement = self::$webDriver->wait(5)->until(
                 WebDriverExpectedCondition::presenceOfElementLocated(
@@ -131,92 +119,6 @@ class MandatorySoftTest extends TestBaseClassWeb
             $this->assertTrue(
                 !empty($surveyCompletedElement),
                 'Completed are not shown after fill mandatory question'
-            );
-        } catch (\Exception $ex) {
-            self::$testHelper->takeScreenshot(self::$webDriver, __CLASS__ . '_' . __FUNCTION__);
-            $this->assertFalse(
-                true,
-                'Url: ' . $url . PHP_EOL
-                . 'Screenshot taken.' . PHP_EOL
-                . self::$testHelper->javaTrace($ex)
-            );
-        }
-        self::$testSurvey->delete();
-        self::$testSurvey = null;
-    }
-
-    /*
-     * Close dialog box do not disable dialog box
-     * In same page with close button #20409 mantis issue
-     * In next page with Continue without answering #20433 mantis issue
-     * @since 2025-02-25
-     */
-    public function testMandatoryCloseDialog()
-    {
-        $surveyFile = self::$surveysFolder . '/limesurvey_survey_MandatorySoftCloseDialog.lss';
-        self::importSurvey($surveyFile);
-        $url = $this->getSurveyUrl();
-        $questions = $this->getAllSurveyQuestions();
-        try {
-            self::$webDriver->get($url);
-            /* Try to submit */
-            self::$webDriver->scrollToBottom();
-            self::$webDriver->next();
-            /* Must come back to same */
-            $this->assertTrue(
-                !empty(self::$webDriver->findElement(WebDriverBy::id('question' . $questions['G01Q01']->qid))),
-                'Soft mandatory G01Q01 question are not in 1st page after try submit'
-            );
-            /* wait for mandatory soft dialog box, close it with close button*/
-            $modalCloseButton = self::$webDriver->wait(10)->until(
-                WebDriverExpectedCondition::elementToBeClickable(
-                    WebDriverBy::cssSelector('#bootstrap-alert-box-modal .btn-close')
-                )
-            );
-            $modalCloseButton->click();
-            /* Try to submit again */
-            self::$webDriver->scrollToBottom();
-            self::$webDriver->next();
-            $this->assertTrue(
-                !empty(self::$webDriver->findElement(WebDriverBy::id('question' . $questions['G01Q01']->qid))),
-                'Soft mandatory G01Q01 question are not in 1st page after try submit after using close button'
-            );
-            /* Continue without answering action */
-            $mandatorysoftButtonG1 = self::$webDriver->wait(10)->until(
-                WebDriverExpectedCondition::elementToBeClickable(
-                    WebDriverBy::id('mandatory-soft-alert-box-modal')
-                )
-            );
-            $mandatorysoftButtonG1->click();
-            /* Must be at page 2 / Group 2 */
-            $this->assertTrue(
-                !empty(self::$webDriver->findElement(WebDriverBy::id('question' . $questions['G02Q02']->qid))),
-                'Soft mandatory G02Q02 question are not in 2nd page after use Continue without answering action'
-            );
-            /* Try to move next */
-            self::$webDriver->scrollToBottom();
-            self::$webDriver->next();
-            /* Must stay at page 2 / Group 2 */
-            $this->assertTrue(
-                !empty(self::$webDriver->findElement(WebDriverBy::id('question' . $questions['G02Q02']->qid))),
-                'Soft mandatory G02Q02 question are not in 2nd page after use Continue without answering action and move next'
-            );
-            /* finalize and check mandatory-soft-alert-box-modal is still there */
-            $mandatorysoftButtonG2 = self::$webDriver->wait(10)->until(
-                WebDriverExpectedCondition::elementToBeClickable(
-                    WebDriverBy::id('mandatory-soft-alert-box-modal')
-                )
-            );
-            $mandatorysoftButtonG2->click();
-            /* End page */
-            $surveyCompletedElement = self::$webDriver->wait(5)->until(
-                WebDriverExpectedCondition::presenceOfElementLocated(
-                    WebDriverBy::id('text-completed-survey')
-                )
-            );
-            $this->assertTrue(
-                !empty($surveyCompletedElement),
-                'Completed page not shown after bypassing soft mandatory questions via Continue without answering'
             );
         } catch (\Exception $ex) {
             self::$testHelper->takeScreenshot(self::$webDriver, __CLASS__ . '_' . __FUNCTION__);
