c0cdd37b52abe2fb794fbdd93003d694ae45a27d
Anblik Audit Team <anblikauditteam@gmail.com>
2026-03-03 21:55:18 +0530

    fix Incorrect list announced

--- application/views/surveyAdministration/partial/topbar_tokens/tokensInvRemDropdownItems.php
+++ application/views/surveyAdministration/partial/topbar_tokens/tokensInvRemDropdownItems.php
@@ -1,41 +1,41 @@
-<ul class="dropdown-menu">
+<!-- Invite & remind menu -->
+<ul class="dropdown-menu" role="menu" aria-labelledby="ls-inv-rem-button">
     <?php if ($hasTokensUpdatePermission): ?>
 
         <!-- Send email invitation -->
-        <li>
-            <a class="pjax dropdown-item" href="<?php echo Yii::App()->createUrl("admin/tokens/sa/email/surveyid/$oSurvey->sid"); ?>" >
-                <span class="ri-mail-send-fill"></span>
+        <li role="none">
+            <a class="pjax dropdown-item" role="menuitem" href="<?php echo Yii::App()->createUrl("admin/tokens/sa/email/surveyid/$oSurvey->sid"); ?>" >
+                <span class="ri-mail-send-fill" aria-hidden="true"></span>
                 <?php eT("Send email invitation"); ?>
             </a>
         </li>
 
         <!-- Send email reminder -->
-        <li>
-            <a class="pjax dropdown-item" href="<?php echo Yii::App()->createUrl("admin/tokens/sa/email/action/remind/surveyid/$oSurvey->sid"); ?>" >
-                <span class="ri-mail-volume-fill"></span>
+        <li role="none">
+            <a class="pjax dropdown-item" role="menuitem" href="<?php echo Yii::App()->createUrl("admin/tokens/sa/email/action/remind/surveyid/$oSurvey->sid"); ?>" >
+                <span class="ri-mail-volume-fill" aria-hidden="true"></span>
                 <?php eT("Send email reminder"); ?>
             </a>
         </li>
 
         <!-- Edit email template -->
-        <!-- Send email invitation -->
-        <li>
-            <a class="pjax dropdown-item" href="<?php echo Yii::App()->createUrl("admin/emailtemplates/sa/index/surveyid/$oSurvey->sid"); ?>" >
-                <span class="ri-mail-line"></span>
+        <li role="none">
+            <a class="pjax dropdown-item" role="menuitem" href="<?php echo Yii::App()->createUrl("admin/emailtemplates/sa/index/surveyid/$oSurvey->sid"); ?>" >
+                <span class="ri-mail-line" aria-hidden="true"></span>
                 <?php eT("Edit email templates"); ?>
             </a>
         </li>
     <?php endif; ?>
 
-    <li role="separator" class="dropdown-divider"></li>
+    <li role="presentation" class="dropdown-divider" aria-hidden="true"></li>
 
     <!-- Bounce processing -->
     <?php if ($hasTokensUpdatePermission):?>
         <?php if($oSurvey->bounceprocessing != 'N' ||  ($oSurvey->bounceprocessing == 'G' && App()->getConfig('bounceaccounttype') != 'off')):?>
             <?php if (function_exists('imap_open')):?>
-                <li>
-                    <a class="dropdown-item" href="#" id="startbounceprocessing" data-url="<?php echo Yii::App()->createUrl("admin/tokens/sa/bounceprocessing/surveyid/$oSurvey->sid"); ?>" >
-                        <span class="ri-tools-fill"></span>
+                <li role="none">
+                    <a class="dropdown-item" role="menuitem" href="#" id="startbounceprocessing" data-url="<?php echo Yii::App()->createUrl("admin/tokens/sa/bounceprocessing/surveyid/$oSurvey->sid"); ?>" >
+                        <span class="ri-tools-fill" aria-hidden="true"></span>
                         <?php eT("Start bounce processing"); ?>
                     </a>
                 </li>
@@ -50,18 +50,18 @@
     <?php endif;?>
 
     <?php if (isset($eMessage)):?>
-        <li class="disabled">
-            <a  class="dropdown-item" href="#" class="disabled" data-bs-toggle="tooltip" data-bs-placement="bottom" title='<?php echo $eMessage; ?>'>
-                <span class="ri-tools-fill""></span>
+        <li role="none" class="disabled">
+            <a class="dropdown-item disabled" role="menuitem" href="#" aria-disabled="true" data-bs-toggle="tooltip" data-bs-placement="bottom" title='<?php echo $eMessage; ?>'>
+                <span class="ri-tools-fill" aria-hidden="true"></span>
                 <?php eT("Start bounce processing"); ?>
             </a>
         </li>
     <?php endif;?>
 
     <!-- Bounce settings -->
-    <li>
-        <a class="dropdown-item" href="<?php echo Yii::App()->createUrl("admin/tokens/sa/bouncesettings/surveyid/$oSurvey->sid"); ?>" >
-            <span class="ri-settings-5-fill"></span>
+    <li role="none">
+        <a class="dropdown-item" role="menuitem" href="<?php echo Yii::App()->createUrl("admin/tokens/sa/bouncesettings/surveyid/$oSurvey->sid"); ?>" >
+            <span class="ri-settings-5-fill" aria-hidden="true"></span>
             <?php eT("Bounce settings"); ?>
         </a>
     </li>
