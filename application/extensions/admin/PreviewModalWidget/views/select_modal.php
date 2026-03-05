638708e7cfd0829414e8dfd22d59e40fd23f0029
Anblik Audit Team <anblikauditteam@gmail.com>
2026-03-05 15:10:35 +0530

    fix Incorrect heading level announced by the screen reader

--- application/extensions/admin/PreviewModalWidget/views/select_modal.php
+++ application/extensions/admin/PreviewModalWidget/views/select_modal.php
@@ -11,7 +11,7 @@
     <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title"><?= gT($this->modalTitle) ?></h5>
+                <h2 class="modal-title h5"><?= gT($this->modalTitle) ?></h2>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
