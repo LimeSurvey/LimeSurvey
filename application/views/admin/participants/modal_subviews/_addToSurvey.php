57a4443c9354dc44ab66620a61c19a37affa547b
Anblik Audit Team <anblikauditteam@gmail.com>
2026-03-05 15:40:01 +0530

    fix Incorrect heading level announced

--- application/views/admin/participants/modal_subviews/_addToSurvey.php
+++ application/views/admin/participants/modal_subviews/_addToSurvey.php
@@ -3,7 +3,7 @@
     ), 'post'
 ); ?>
     <div class="modal-header">
-        <h5 class="modal-title" id="participant_edit_modal"><?php echo ngT('Add participant to survey|Add participants to survey', $count); ?></h5>
+        <h2 class="modal-title h5" id="participant_edit_modal"><?php echo ngT('Add participant to survey|Add participants to survey', $count); ?></h2>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
     </div>
 
