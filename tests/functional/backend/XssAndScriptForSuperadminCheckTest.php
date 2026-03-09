bd46d6907f0557b22a95638e44d1f39516373649
Denis Chenu <denis@sondages.pro>
2026-03-09 16:03:39 +0100

    Dev: remove extra user for test

--- tests/functional/backend/XssAndScriptForSuperadminCheckTest.php
+++ tests/functional/backend/XssAndScriptForSuperadminCheckTest.php
@@ -10,22 +10,17 @@ namespace ls\tests;
 class XssAndScriptForSuperadminCheckTest extends TestBaseClass
 {
     /**
-     * @var integer $userId
-     */
-    private static $userId;
-
-    /**
      * @inheritdoc
      */
     public static function setUpBeforeClass(): void
     {
         parent::setupBeforeClass();
-        /* Create an random user and login */
-        $username = "test_" . \Yii::app()->securityManager->generateRandomString(8);
-        $password = createPassword();
-        self::$userId = \User::insertUser($username, $password, 'Test superadmin for XSS', 1, 'superadmin@example.org');
-        \Permission::model()->setGlobalPermission(self::$userId, 'superadmin', array('read_p'));
-        \Permission::model()->setGlobalPermission(self::$userId, 'auth_db', array('read_p'));
+        /* be sure to be at default */
+        App()->setConfig('filterxsshtml', true);
+        App()->setConfig('disablescriptwithxss', true);
+        App()->setConfig('filterxsshtml_forcedall', false);
+        App()->setConfig('filterxsshtml_allowforcedsuperadmin', false);
+        App()->setConfig('filterxsshtml_enablescript', '');
     }
 
     /**
@@ -35,11 +30,10 @@ class XssAndScriptForSuperadminCheckTest extends TestBaseClass
     {
         App()->setConfig('filterxsshtml_forcedall', true);
         App()->setConfig('disablescriptwithxss', true);
-        App()->setConfig('filterxsshtml_enablescript', ''); // Be sure to be default
         $questionl10n = $this->importSurveyAndgetQuestionI10n();
-        $this->assertEquals("Question:", $questionl10n->question);
-        $this->assertEquals("Help:", $questionl10n->help);
-        $this->assertEquals("", $questionl10n->script);
+        $this->assertEquals('Question:', $questionl10n->question);
+        $this->assertEquals('Help:', $questionl10n->help);
+        $this->assertEquals('', $questionl10n->script);
         self::$testSurvey->delete();
         /* reset to default */
         App()->setConfig('filterxsshtml_forcedall', false);
@@ -52,17 +46,14 @@ class XssAndScriptForSuperadminCheckTest extends TestBaseClass
     public function testXssOnAndScriptOnSuperadmin()
     {
         App()->setConfig('filterxsshtml_forcedall', true);
-        App()->setConfig('filterxsshtml_allowforcedsuperadmin', true);
-        App()->setConfig('disablescriptwithxss', true);
         App()->setConfig('filterxsshtml_enablescript', 'superadmin');
         $questionl10n = $this->importSurveyAndgetQuestionI10n();
-        $this->assertEquals("Question:", $questionl10n->question);
-        $this->assertEquals("Help:", $questionl10n->help);
+        $this->assertEquals('Question:', $questionl10n->question);
+        $this->assertEquals('Help:', $questionl10n->help);
         $this->assertEquals("alert('script');", $questionl10n->script);
         self::$testSurvey->delete();
         /* reset to default */
         App()->setConfig('filterxsshtml_forcedall', false);
-        App()->setConfig('filterxsshtml_allowforcedsuperadmin', false);
         App()->setConfig('filterxsshtml_enablescript', '');
     }
 
@@ -73,10 +64,6 @@ class XssAndScriptForSuperadminCheckTest extends TestBaseClass
     {
         App()->setConfig('filterxsshtml_forcedall', true);
         App()->setConfig('filterxsshtml_allowforcedsuperadmin', true);
-        /* Keep forced super admin */
-        $keepForcedsuperadmin = App()->getConfig('forcedsuperadmin');
-        /* Check as forcedsuperadmin */
-        App()->setConfig('forcedsuperadmin', [self::$userId]);
         $questionl10n = $this->importSurveyAndgetQuestionI10n();
         $this->assertEquals("Question:<script>alert('question');</script>", $questionl10n->question);
         $this->assertEquals("Help:<script>alert('help');</script>", $questionl10n->help);
@@ -85,15 +72,6 @@ class XssAndScriptForSuperadminCheckTest extends TestBaseClass
         /* reset to default */
         App()->setConfig('filterxsshtml_forcedall', false);
         App()->setConfig('filterxsshtml_allowforcedsuperadmin', false);
-        App()->setConfig('forcedsuperadmin', $keepForcedsuperadmin);
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public static function tearDownAfterClass(): void
-    {
-        \User::model()->deleteByPk(self::$userId);
     }
 
     /**
@@ -103,11 +81,11 @@ class XssAndScriptForSuperadminCheckTest extends TestBaseClass
     private function importSurveyAndgetQuestionI10n()
     {
         $surveyFile = self::$surveysFolder . '/limesurvey_survey_XssAndScriptCheck.lss';
-        self::importSurvey($surveyFile, self::$userId);
+        self::importSurvey($surveyFile, 1);
         $questions = $this->getAllSurveyQuestions();
-        return  \QuestionL10n::model()->find(
-            "qid = :qid and language = :language",
-            [":qid" => $questions['Q00']->qid, ":language" => 'en']
+        return \QuestionL10n::model()->find(
+            'qid = :qid and language = :language',
+            [':qid' => $questions['Q00']->qid, ':language' => 'en']
         );
     }
 }
