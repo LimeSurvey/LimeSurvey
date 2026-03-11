6e911001d0f910944bd6d4763d48bff321e7b57e
ramishenouda <rami.zaki@limesurvey.org>
2026-03-11 09:09:12 +0100

    Fix title and message styles in ResetAllConditionsOverlay for improved readability

--- editor/src/components/ConditionDesigner/Overlays/ResetAllConditionsOverlay.js
+++ editor/src/components/ConditionDesigner/Overlays/ResetAllConditionsOverlay.js
@@ -4,11 +4,11 @@ import { SwalAlert } from 'helpers'
 export const ResetAllConditionsOverlay = ({ onConfirmDelete }) => {
   return (
     <div className="condition-designer-overlay">
-      <div className="condition-designer-overlay-title">
-        <p>{t('Confirm')}</p>
+      <div className="condition-designer-overlay-title reg24">
+        <p>{t('Delete condition')}</p>
       </div>
 
-      <p className="condition-designer-overlay-message">
+      <p className="condition-designer-overlay-message reg14">
         {t('Are you sure you want to delete all conditions for this question?')}
       </p>
 
