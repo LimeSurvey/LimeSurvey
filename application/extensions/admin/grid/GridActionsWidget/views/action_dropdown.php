9031a7aa61856d74585998c6c6d2f9b08b3347f1
ramishenouda <rami.zaki@limesurvey.org>
2026-03-13 16:35:33 +0100

    formatting

--- application/extensions/admin/grid/GridActionsWidget/views/action_dropdown.php
+++ application/extensions/admin/grid/GridActionsWidget/views/action_dropdown.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * @var array $dropdownItems
  * @var int $id
@@ -7,8 +6,8 @@
 
 ?>
 <div id="<?= 'dropdown_' . $id; ?>" class="dropdown ls-action_dropdown">
-    <button class="ls-dropdown-toggle" aria-label="<?= gT('Action Menu'); ?>" data-bs-toggle="dropdown" type="button"
-        aria-expanded="false">
+    <button class="ls-dropdown-toggle"  aria-label="<?= gT('Action Menu'); ?>" data-bs-toggle="dropdown" type="button"
+            aria-expanded="false">
         <i class="ri-more-fill"></i>
     </button>
     <ul id="<?= 'dropdownmenu_' . $id; ?>" class="dropdown-menu">
@@ -17,37 +16,35 @@
             <?php
             if (isset($dropdownItem['submenu']) && $dropdownItem['submenu']) { ?>
                 <li class="has-submenu">
-                    <a href="#" class="dropdown-item d-flex justify-content-between align-items-center"
-                        data-bs-toggle="dropdown-submenu" role="button" aria-expanded="false">
-                        <?= CHtml::encode($dropdownItem['title']) ?>
-                    </a>
-                    <ul class="dropdown-submenu">
-                        <?php
-                        foreach ($dropdownItem['submenu_items'] as $subItem) { ?>
-                            <li>
-                                <?php
-                                $this->render(
-                                    'dropdown_item',
-                                    [
-                                        'tooltip' => $subItem['tooltip'] ?? '',
-                                        'title' => $subItem['title'],
-                                        'linkId' => $subItem['linkId'] ?? '',
-                                        'linkClass' => $subItem['linkClass'] ?? '',
-                                        'url' => $subItem['url'] ?? '#',
-                                        'iconClass' => $subItem["iconClass"] ?? null,
-                                        'linkAttributes' => $subItem['linkAttributes'] ?? null,
-                                        'enabledCondition' => $subItem['enabledCondition'],
-                                    ]
-                                ); ?>
-                            </li>
-                        <?php }
-                        ?>
-                    </ul>
+                <a href="#" class="dropdown-item d-flex justify-content-between align-items-center"
+                   data-bs-toggle="dropdown-submenu" role="button" aria-expanded="false">
+                    <?= CHtml::encode($dropdownItem['title']) ?>
+                </a>
+                <ul class="dropdown-submenu">
+                    <?php
+                    foreach ($dropdownItem['submenu_items'] as $subItem) { ?>
+                        <li>
+                            <?php
+                            $this->render('dropdown_item',
+                                [
+                                    'tooltip' => $subItem['tooltip'] ?? '',
+                                    'title' => $subItem['title'],
+                                    'linkId' => $subItem['linkId'] ?? '',
+                                    'linkClass' => $subItem['linkClass'] ?? '',
+                                    'url' => $subItem['url'] ?? '#',
+                                    'iconClass' => $subItem["iconClass"] ?? null,
+                                    'linkAttributes' => $subItem['linkAttributes'] ?? null,
+                                    'enabledCondition' => $subItem['enabledCondition'],
+                                ]
+                            ); ?>
+                        </li>
+                    <?php }
+                    ?>
+                </ul>
                 <?php
             } else { ?>
                 <li>
-                <?php $this->render(
-                    'dropdown_item',
+                <?php $this->render('dropdown_item',
                     [
                         'tooltip' => $dropdownItem['tooltip'] ?? '',
                         'title' => $dropdownItem['title'],
@@ -60,8 +57,8 @@
                     ]
                 );
             }
-                ?>
-                </li>
-            <?php endforeach; ?>
+            ?>
+            </li>
+        <?php endforeach; ?>
     </ul>
 </div>
--- application/models/Survey.php
+++ application/models/Survey.php
@@ -554,9 +554,9 @@ class Survey extends LSActiveRecord implements PermissionInterface
             array('additional_languages', 'LSYii_FilterValidator', 'filter' => 'trim', 'skipOnEmpty' => true),
             array('additional_languages', 'LSYii_Validators', 'isLanguageMulti' => true),
             array('running', 'safe', 'on' => 'search'),
-            array('expires', 'date','format' => ['yyyy-M-d H:m:s.???', 'yyyy-M-d H:m:s', 'yyyy-M-d H:m'],'allowEmpty' => true),
-            array('startdate', 'date','format' => ['yyyy-M-d H:m:s.???', 'yyyy-M-d H:m:s', 'yyyy-M-d H:m'],'allowEmpty' => true),
-            array('datecreated', 'date','format' => ['yyyy-M-d H:m:s.???', 'yyyy-M-d H:m:s', 'yyyy-M-d H:m'],'allowEmpty' => true),
+            array('expires', 'date','format' => ['yyyy-M-d H:m:s.???','yyyy-M-d H:m:s','yyyy-M-d H:m'],'allowEmpty' => true),
+            array('startdate', 'date','format' => ['yyyy-M-d H:m:s.???','yyyy-M-d H:m:s','yyyy-M-d H:m'],'allowEmpty' => true),
+            array('datecreated', 'date','format' => ['yyyy-M-d H:m:s.???','yyyy-M-d H:m:s','yyyy-M-d H:m'],'allowEmpty' => true),
             array('expires', 'checkExpireAfterStart'),
             // The Google Analytics Tracking ID is inserted in a JS script. If the following rule is changed, make sure
             // that it doesn't render it vulnerable to XSS attacks.
