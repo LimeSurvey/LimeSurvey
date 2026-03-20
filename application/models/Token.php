6259908fcc8fc13e764d6f97f0a746a32b5aace9
Lajos Arpad <lajos.arpad85@gmail.com>
2026-03-20 16:38:38 +0200

    LE-653: Removed wrapped conditional

--- application/models/Token.php
+++ application/models/Token.php
@@ -204,29 +204,27 @@ abstract class Token extends Dynamic
         $db = \Yii::app()->db;
         $sTableName = $oSurvey->tokensTableName;
 
-        if (!tableExists($sTableName)) {
-            try {
-                $db->createCommand()->createTable($sTableName, $fields);
-                /**
-                 * The random component in the index name is needed because Postgres is being the dorky kid and
-                 * complaining about duplicates when renaming the table and trying to use the same index again
-                 * on a new token table (for example on reactivation)
-                 */
-                $db->createCommand()->createIndex("idx_token_token_{$surveyId}_" . rand(1, 50000), $sTableName, 'token');
+        try {
+            $db->createCommand()->createTable($sTableName, $fields);
+            /**
+             * The random component in the index name is needed because Postgres is being the dorky kid and
+             * complaining about duplicates when renaming the table and trying to use the same index again
+             * on a new token table (for example on reactivation)
+             */
+            $db->createCommand()->createIndex("idx_token_token_{$surveyId}_" . rand(1, 50000), $sTableName, 'token');
 
-                // MSSQL does not support indexes on text fields so not needed here
-                switch (Yii::app()->db->driverName) {
-                    case 'mysql':
-                    case 'mysqli':
-                        $db->createCommand()->createIndex('idx_email', $sTableName, 'email(30)', false);
-                        break;
-                    case 'pgsql':
-                        $db->createCommand()->createIndex('idx_email_' . $surveyId . '_' . rand(1, 50000), $sTableName, 'email', false);
-                        break;
-                }
-            } catch (CDbException $ex) {
-                //table already exists, skipping
+            // MSSQL does not support indexes on text fields so not needed here
+            switch (Yii::app()->db->driverName) {
+                case 'mysql':
+                case 'mysqli':
+                    $db->createCommand()->createIndex('idx_email', $sTableName, 'email(30)', false);
+                    break;
+                case 'pgsql':
+                    $db->createCommand()->createIndex('idx_email_' . $surveyId . '_' . rand(1, 50000), $sTableName, 'email', false);
+                    break;
             }
+        } catch (CDbException $ex) {
+            //table already exists, skipping
         }
 
         // Refresh schema cache just in case the table existed in the past, and return if table exist
