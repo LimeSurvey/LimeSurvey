ed0651ca2795c38c8e2d284f6dab62b2abfbf6c5
haythem <haythem.chibani@gmail.com>
2026-03-24 14:09:10 +0100

    Fixed issue #LE-661: visiting survey in closed accessMode & inherited public registration enabled

--- application/controllers/RegisterController.php
+++ application/controllers/RegisterController.php
@@ -104,7 +104,7 @@ class RegisterController extends LSYii_Controller
         Yii::app()->setLanguage($sLanguage);
         if (!$oSurvey) {
             throw new CHttpException(404, "The survey in which you are trying to participate does not seem to exist. It may have been deleted or the link you were given is outdated or incorrect.");
-        } elseif ($oSurvey->allowregister != 'Y' || !tableExists("{{tokens_{$iSurveyId}}}")) {
+        } elseif ($oSurvey->getIsAllowRegister() != 'Y' || !tableExists("{{tokens_{$iSurveyId}}}")) {
             throw new CHttpException(404, "The survey in which you are trying to register don't accept registration. It may have been updated or the link you were given is outdated or incorrect.");
         } elseif (!is_null($oSurvey->expires) && $oSurvey->expires < dateShift(date("Y-m-d H:i:s"), "Y-m-d H:i", Yii::app()->getConfig('timeadjust'))) {
             $this->redirect(array('survey/index', 'sid' => $iSurveyId, 'lang' => $sLanguage));
