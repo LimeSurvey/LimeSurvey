485031cf9e5472294c57a6a1a70ce2e5d76ad8f9
Lajos Arpad <lajos.arpad85@gmail.com>
2026-03-27 17:22:35 +0200

    LE-662: Fix

--- application/helpers/common_helper.php
+++ application/helpers/common_helper.php
@@ -1472,7 +1472,7 @@ function getFieldName(string $tableName, string $fieldName, array $questions, in
                 $prefix = ((strpos($tableName, "timing") !== false) ? "C" : "R");
                 $index = substr($fieldName, strlen("{$sid}X{$gid}X{$qid}"));
                 try {
-                    if (intval(substr($fieldName, strlen("{$sid}X{$gid}X{$qid}")) > 0)) {
+                    if ((intval(substr($fieldName, strlen("{$sid}X{$gid}X{$qid}"))) > 0) && isset($questions[0]->answers[(substr($fieldName, strlen("{$sid}X{$gid}X{$qid}")) - 1)])) {
                         $aid = $cd ? $index : $questions[0]->answers[(substr($fieldName, strlen("{$sid}X{$gid}X{$qid}")) - 1)]->aid;
                         $newFieldName = "Q{$qid}_{$prefix}" . $aid;
                     }
