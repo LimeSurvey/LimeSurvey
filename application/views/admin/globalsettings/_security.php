8e11358a298fd822be05f5a041f624e990bd647a
Denis Chenu <denis@sondages.pro>
2026-03-27 10:07:12 +0100

    New feature #20316: XSS for superadmin too (#4753)

--- application/views/admin/globalsettings/_security.php
+++ application/views/admin/globalsettings/_security.php
@@ -31,20 +31,33 @@
                 <div>
                     <?php $this->widget('ext.ButtonGroupWidget.ButtonGroupWidget', [
                         'name'          => 'filterxsshtml',
+                        'checkedOption' => App()->getConfig('filterxsshtml_forcedall') ? 1 : App()->getConfig('filterxsshtml'),
                         'ariaLabel'=> gT('Filter HTML for XSS:'),
-                        'checkedOption' => App()->getConfig('filterxsshtml'),
                         'selectOptions' => [
                             '1' => gT('On'),
                             '0' => gT('Off'),
+                        ],
+                        'htmlOptions'=> [
+                            'disabled' => App()->getConfig('filterxsshtml_forcedall')
                         ]
                     ]); ?>
                 </div>
                 <div class="help-block mt-1">
-                    <?php
-                    App()->getController()->widget('ext.AlertWidget.AlertWidget', [
-                        'text' => gT("Note: XSS filtering is always disabled for the superadministrator."),
-                        'type' => 'success',
-                    ]);
+                    <?php if (!App()->getConfig('filterxsshtml_forcedall')) {
+                        App()->getController()->widget('ext.AlertWidget.AlertWidget', [
+                            'text' => gT("Note: XSS filtering is always disabled for the superadministrator."),
+                            'type' => 'success',
+                        ]);
+                    } else {
+                        $text = gT("Note: XSS filtering is forced by settings in your config file. You cannot disable it. XSS filtering is enabled for all users.");
+                        if (App()->getConfig('filterxsshtml_allowforcedsuperadmin')) {
+                            $text = gT("Note: XSS filtering is forced by settings in your config file. You cannot disable it. XSS filtering is only disabled for forced super admin(s).");
+                        }
+                        App()->getController()->widget('ext.AlertWidget.AlertWidget', [
+                            'text' => $text,
+                            'type' => 'warning',
+                        ]);
+                    }
                     ?>
                 </div>
             </div>
@@ -54,20 +67,35 @@
                 <div>
                     <?php $this->widget('ext.ButtonGroupWidget.ButtonGroupWidget', [
                         'name'          => 'disablescriptwithxss',
+                        'checkedOption' => (App()->getConfig('filterxsshtml_forcedall') && App()->getConfig('filterxsshtml_enablescript') != 'gui') ? 1 : App()->getConfig('disablescriptwithxss'),
                         'ariaLabel'=> gT('Disable question script for XSS restricted user:'),
-                        'checkedOption' => App()->getConfig('disablescriptwithxss'),
                         'selectOptions' => [
                             '1' => gT('On'),
                             '0' => gT('Off'),
+                        ],
+                        'htmlOptions'=> [
+                            'disabled' => App()->getConfig('filterxsshtml_forcedall') && App()->getConfig('filterxsshtml_enablescript') != 'gui'
                         ]
                     ]); ?>
                 </div>
                 <div class="help-block mt-1">
-                    <?php
-                    App()->getController()->widget('ext.AlertWidget.AlertWidget', [
-                    'text' => gT("If you disable this option : user with XSS restriction still can add script. This allows user to add cross-site scripting javascript system."),
-                    'type' => 'warning',
-                    ]);
+                    <?php if (App()->getConfig('filterxsshtml_forcedall') && App()->getConfig('filterxsshtml_enablescript') != 'gui') {
+                        $text = gT("Script edition is forced by your config file. No user can add or update question script.");
+                        if (App()->getConfig('filterxsshtml_enablescript') == 'superadmin') {
+                            $text = gT("Script edition is forced by your config file. Only the super admin(s) can add or update question script.");
+                        } elseif (App()->getConfig('filterxsshtml_allowforcedsuperadmin') || App()->getConfig('filterxsshtml_enablescript') == 'forcedsuperadmin') {
+                            $text = gT("Script edition is forced by your config file. Only forced super admin(s) can add or update question script.");
+                        }
+                        App()->getController()->widget('ext.AlertWidget.AlertWidget', [
+                            'text' => $text,
+                            'type' => 'warning',
+                        ]);
+                    } else {
+                        App()->getController()->widget('ext.AlertWidget.AlertWidget', [
+                        'text' => gT("If you disable this option : user with XSS restriction still can add script. This allows user to add cross-site scripting javascript system."),
+                        'type' => 'warning',
+                        ]);
+                    }
                     ?>
                 </div>
             </div>
---
+++ tests/functional/backend/XssAndScriptForSuperadminCheckTest.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace ls\tests;
+
+/**
+ * Check filterxsshtml_forcedall and relatyed settings
+ * @since 6.17.0
+ * @group settings
+ */
+class XssAndScriptForSuperadminCheckTest extends TestBaseClass
+{
+    /**
+     * @inheritdoc
+     */
+    public static function setUpBeforeClass(): void
+    {
+        parent::setupBeforeClass();
+        /* be sure to be at default */
+        App()->setConfig('forcedsuperadmin', [1]);
+        App()->setConfig('filterxsshtml', true);
+        App()->setConfig('disablescriptwithxss', true);
+        App()->setConfig('filterxsshtml_forcedall', false);
+        App()->setConfig('filterxsshtml_allowforcedsuperadmin', false);
+        App()->setConfig('filterxsshtml_enablescript', '');
+        App()->user->setId(1);
+    }
+
+    /**
+     * @return void
+     */
+    public function testXssOnAndScriptOffSuperadmin()
+    {
+        App()->setConfig('filterxsshtml_forcedall', true);
+        App()->setConfig('disablescriptwithxss', true);
+        $questionl10n = $this->importSurveyAndgetQuestionI10n();
+        $this->assertEquals('Question:', $questionl10n->question);
+        $this->assertEquals('Help:', $questionl10n->help);
+        $this->assertEquals('', $questionl10n->script);
+        self::$testSurvey->delete();
+        self::$testSurvey = null;
+        /* reset to default */
+        App()->setConfig('filterxsshtml_forcedall', false);
+        App()->setConfig('filterxsshtml_allowforcedsuperadmin', false);
+    }
+
+    /**
+     * @return void
+     */
+    public function testXssOnAndScriptOnSuperadmin()
+    {
+        App()->setConfig('filterxsshtml_forcedall', true);
+        App()->setConfig('filterxsshtml_enablescript', 'superadmin');
+        $questionl10n = $this->importSurveyAndgetQuestionI10n();
+        $this->assertEquals('Question:', $questionl10n->question);
+        $this->assertEquals('Help:', $questionl10n->help);
+        $this->assertEquals("alert('script');", $questionl10n->script);
+        self::$testSurvey->delete();
+        self::$testSurvey = null;
+        /* reset to default */
+        App()->setConfig('filterxsshtml_forcedall', false);
+        App()->setConfig('filterxsshtml_enablescript', '');
+    }
+
+    /**
+     * @return void
+     */
+    public function testXssOffForcedSuperAdmin()
+    {
+        App()->setConfig('filterxsshtml_forcedall', true);
+        App()->setConfig('filterxsshtml_allowforcedsuperadmin', true);
+        $questionl10n = $this->importSurveyAndgetQuestionI10n();
+        $this->assertEquals("Question:<script>alert('question');</script>", $questionl10n->question);
+        $this->assertEquals("Help:<script>alert('help');</script>", $questionl10n->help);
+        $this->assertEquals("alert('script');", $questionl10n->script);
+        self::$testSurvey->delete();
+        self::$testSurvey = null;
+        /* reset to default */
+        App()->setConfig('filterxsshtml_forcedall', false);
+        App()->setConfig('filterxsshtml_allowforcedsuperadmin', false);
+    }
+
+    /**
+     * Import the survey and get the questioni10n
+     * return \QuestionL10n
+     */
+    private function importSurveyAndgetQuestionI10n()
+    {
+        $surveyFile = self::$surveysFolder . '/limesurvey_survey_XssAndScriptCheck.lss';
+        self::importSurvey($surveyFile, 1);
+        $questions = $this->getAllSurveyQuestions();
+        return \QuestionL10n::model()->find(
+            'qid = :qid and language = :language',
+            [':qid' => $questions['Q00']->qid, ':language' => 'en']
+        );
+    }
+}
