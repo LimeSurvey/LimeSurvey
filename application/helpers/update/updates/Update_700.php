b4b980651983da83dda508753510b79d7bcf0982
Lajos Arpad <lajos.arpad85@gmail.com>
2026-03-27 22:30:07 +0200

    LE-662: Fix

--- application/helpers/update/updates/Update_700.php
+++ application/helpers/update/updates/Update_700.php
@@ -1067,7 +1067,7 @@ class Update_700 extends DatabaseUpdateBase
                 }
                 $commaSeparatedQIDs = implode(",", $qids);
                 $questions = Question::model()->with('answers')->findAll([
-                    'condition' => "sid = {$sid} and ((t.qid in ({$commaSeparatedQIDs}) gid = {$gid}) or parent_qid in ({$commaSeparatedQIDs}))"
+                    'condition' => "sid = {$sid} and ((t.qid in ({$commaSeparatedQIDs}) and gid = {$gid}) or parent_qid in ({$commaSeparatedQIDs}))"
                 ]);
             }
             if (count($questions) || ((strpos($tableName, "timings") !== false) && ($split > 1))) {
