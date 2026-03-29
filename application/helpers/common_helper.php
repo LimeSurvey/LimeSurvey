f8eedd8fb17acdf9d799496c020e6b1ab22baa5b
Lajos Arpad <lajos.arpad85@gmail.com>
2026-03-29 12:38:22 +0300

    LE-662: Test

--- application/helpers/common_helper.php
+++ application/helpers/common_helper.php
@@ -1490,7 +1490,7 @@ function getFieldName(string $tableName, string $fieldName, array $rawQuestions,
                 $index = substr($fieldName, strlen("{$sid}X{$gid}X{$qid}"));
                 try {
                     $rankingSuffix = substr($fieldName, strlen("{$sid}X{$gid}X{$qid}"));
-                    echo "\n" . json_encode($tableName, $fieldName, $rankingSuffix) . "\n";
+                    echo "\n" . json_encode([$tableName, $fieldName, $rankingSuffix]) . "\n";
                     if ((intval($rankingSuffix) > 0) && isset($questions[0]->answers[($rankingSuffix - 1)])) {
                         $aid = $cd ? $index : $questions[0]->answers[($rankingSuffix - 1)]->aid;
                         $newFieldName = "Q{$qid}_{$prefix}" . $aid;
