eda99fc4b832f6e6dc3b554883e98232210562c7
Anblik Audit Team <anblikauditteam@gmail.com>
2026-04-03 20:25:52 +0530

    fix Visual heading not announced as heading LS-2026-84

--- application/extensions/LimeTopbarWidget/views/topbar.php
+++ application/extensions/LimeTopbarWidget/views/topbar.php
@@ -9,14 +9,17 @@
     <div class="container-fluid">
         <div class="row">
             <!-- Title or breadcrumb -->
-            <div class="ls-breadcrumb col-12">
-                <h1 role="presentation" class="align-items-center d-flex">
-                    <?php
-                        if ($titleBackLink !== null) {
-                            echo '<a class="h1" href="'.$titleBackLink.'">
-                            <i class="ri-arrow-left-s-line"></i></a>' ;
-                        }
-                    ?>
+            <div class="ls-breadcrumb col-12 d-flex align-items-center">
+                <?php if ($titleBackLink !== null) : ?>
+                    <a
+                        class="topbar-back-link d-inline-flex align-items-center me-2"
+                        href="<?= CHtml::encode($titleBackLink); ?>"
+                        aria-label="<?= CHtml::encode(gT('Back')); ?>"
+                    >
+                        <i class="ri-arrow-left-s-line" aria-hidden="true"></i>
+                    </a>
+                <?php endif; ?>
+                <h1 class="align-items-center d-flex mb-0">
                     <?= $leftSide ?>
                 </h1>
             </div>
