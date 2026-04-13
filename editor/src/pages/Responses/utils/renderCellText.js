735b1a04b02b13908a5b775f4bc6769402474ba8
haythem <haythem.chibani@gmail.com>
2026-04-13 15:17:42 +0100

    Dev: minor fix

--- editor/src/pages/Responses/utils/renderCellText.js
+++ editor/src/pages/Responses/utils/renderCellText.js
@@ -47,9 +47,7 @@ export const renderCellText = ({
         <i className="ri-check-line text-success"></i>
       )}
       {isRankingQuestion(questionThemeName) && `${index + 1}. `}
-      {!isSingleChoiceQuestion(questionThemeName) &&
-        isRankingQuestion(questionThemeName) &&
-        `${subquestionTitle}`}
+      {!isSingleChoiceQuestion(questionThemeName) && `${subquestionTitle}`}
       {isSingleChoiceQuestion(questionThemeName) && answerTitle}
       {comment?.value && (
         <span>
