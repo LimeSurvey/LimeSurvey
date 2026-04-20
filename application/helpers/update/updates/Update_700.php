c85320fae5b600ce50dc9dec9cb57ff1543ef848
Lajos Arpad <lajos.arpad85@gmail.com>
2026-04-20 22:01:09 +0300

    LE-662: Test

--- application/helpers/update/updates/Update_700.php
+++ application/helpers/update/updates/Update_700.php
@@ -1163,6 +1163,9 @@ class Update_700 extends DatabaseUpdateBase
             }
             $scripts[$TABLE_NAME]['CREATE'] = str_replace("{$TABLE_NAME}", "{$scripts[$TABLE_NAME]['new_name']}", $scripts[$TABLE_NAME]['CREATE']);
             foreach ($fields as $oldField => $newField) {
+                if (!$newField) {
+                    echo var_dump($oldField);
+                }
                 $scripts[$TABLE_NAME]['CREATE'] = str_replace($leftSeparator . "{$oldField}" . $rightSeparator, $leftSeparator . "{$newField}" . $rightSeparator, $scripts[$TABLE_NAME]['CREATE']);
             }
             $fromColumns = [];
