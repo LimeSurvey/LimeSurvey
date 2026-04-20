3526d480d34e37231845147fb8dba5535c901577
Lajos Arpad <lajos.arpad85@gmail.com>
2026-04-20 21:53:16 +0300

    LE-662: Debug

--- application/helpers/common_helper.php
+++ application/helpers/common_helper.php
@@ -1504,6 +1504,7 @@ function getFieldName(string $tableName, string $fieldName, array $rawQuestions,
             }
         }
     }
+    echo "\n" . $fieldName . "\n";
     return $newFieldName;
 }
 
