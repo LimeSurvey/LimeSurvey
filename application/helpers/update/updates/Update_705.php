0fd37ad1fc3d63aa850c595be62b0fd2dcb52bae
Mohab Elsheikh <mohab.elsheikh@limesurvey.org>
2026-04-30 11:01:55 +0200

    LE-648: Update plugin priority logic

--- application/helpers/update/updates/Update_705.php
+++ application/helpers/update/updates/Update_705.php
@@ -2,15 +2,30 @@
 
 namespace LimeSurvey\Helpers\Update;
 
+use CDbExpression;
 use LimeSurvey\PluginManager\PluginManager;
 
 class Update_705 extends DatabaseUpdateBase
 {
     public function up()
     {
-        $this->db->createCommand()->update('{{plugins}}', ['priority' => 1], 'name <> :name', [':name' => 'ReactEditor']);
+        // Set all plugins except ReactEditor to priority += 1
+        $this->db->createCommand()->update(
+            '{{plugins}}',
+            ['priority' => new CDbExpression('priority + 1')],
+            '`name` NOT IN (:re, :lsp)',
+            [
+                ':re' => 'ReactEditor',
+                ':lsp' => 'LimeSurveyProfessional',
+            ]
+        );
 
         // Set ReactEditor to priority = 0
-        $this->db->createCommand()->update('{{plugins}}', ['priority' => 0], 'name = :name', [':name' => 'ReactEditor']);
+        $this->db->createCommand()->update(
+            '{{plugins}}',
+            ['priority' => 0],
+            'name = :name',
+            [':name' => 'ReactEditor']
+        );
     }
 }
