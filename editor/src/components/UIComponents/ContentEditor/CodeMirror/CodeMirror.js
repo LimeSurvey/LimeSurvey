12a4ddf5fd7e837bc29cc3aa49ad6469dee0418b
ramishenouda <ramishenouda@outlook.com>
2026-05-13 16:57:54 +0200

    Enhance CodeMirror styling and clean up unused styles in condition designer overlays

--- editor/src/components/UIComponents/ContentEditor/CodeMirror/CodeMirror.js
+++ editor/src/components/UIComponents/ContentEditor/CodeMirror/CodeMirror.js
@@ -1,6 +1,55 @@
 import CodeMirror from '@uiw/react-codemirror'
 import { html } from '@codemirror/lang-html'
+import { EditorView } from '@codemirror/view'
 import { useEffect } from 'react'
+
+const codeEditorTheme = EditorView.theme({
+  '&': {
+    backgroundColor: '#ffffff',
+    border: '2px solid #6e748c',
+    borderRadius: '2px',
+    padding: '12px 8px'
+  },
+  '&.cm-focused': {
+    outline: 'none',
+  },
+  '.cm-gutters': {
+    backgroundColor: '#f8f9fa',
+    color: '#1e1e1e',
+  },
+  '.cm-lineNumbers': {
+    fontSize: '12px',
+    color: '#1e1e1e',
+    textAlign: 'center',
+    minWidth: '30px',
+  },
+  '.cm-gutterElement': {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    minWidth: '40px !important',
+    padding: '0 8px !important',
+  },
+  '.cm-line': {
+    backgroundColor: '#ffffff',
+  },
+  '.cm-lineWrapping': {
+    padding: '0',
+  },
+  '.cm-activeLineGutter': {
+    backgroundColor: '#dddee8',
+    padding: '0 8px !important',
+  },
+  '.cm-content': {
+    fontSize: '14px',
+    backgroundColor: '#ffffff',
+    color: '#1e1e1e',
+  },
+  '.cm-foldGutter': {
+    display: 'none',
+    width: '0px',
+  },
+})
 
 export const CodeEditor = ({
   id = 'code-mirror',
@@ -24,6 +73,7 @@ export const CodeEditor = ({
       height={height}
       width={width}
       className={className}
+      theme={codeEditorTheme}
       extensions={extensions}
       onChange={onChange}
     />
