75f3a05f9f79f524e060762cbdcf2271d53434fe
Lajos Arpad <lajos.arpad85@gmail.com>
2026-05-20 14:32:02 +0300

    LE-755: Fixed an issue related to rankings that causes a crash for 700 when answers were removed on an active survey

--- application/helpers/common_helper.php
+++ application/helpers/common_helper.php
@@ -1504,7 +1504,7 @@ function getFieldName(string $tableName, string $fieldName, array $rawQuestions,
             }
         }
     }
-    return $newFieldName ?? $fieldName;
+    return $newFieldName ? $newFieldName : $fieldName;
 }
 
 
