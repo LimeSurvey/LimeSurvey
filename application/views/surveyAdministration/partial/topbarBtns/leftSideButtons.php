568c4a93c0163d9376db86a4010937e7a62bf222
pstellling <patricia.stelling@limesurvey.org>
2026-05-21 09:45:23 +0200

    AT-2010: removed creating surveys for testing purpose. Could be done somewhere else if needed.

--- application/views/surveyAdministration/partial/topbarBtns/leftSideButtons.php
+++ application/views/surveyAdministration/partial/topbarBtns/leftSideButtons.php
@@ -1,27 +1,4 @@
 <?php
-/**
- * Use this button to quickly add 1000 dummy surveys for testing purposes.
- * It will create surveys with the title "Bulk Survey X" where X is a number from 1 to 1000.
- * The surveys will be created in the default survey group and with the default language of the application.
- *
-if (Permission::model()->hasGlobalPermission('superadmin', 'read')) {
-    $this->widget(
-        'ext.ButtonWidget.ButtonWidget',
-        [
-            'name' => 'btn-create-test-surveys',
-            'id' => 'btn-create-test-surveys',
-            'text' => gT('Create test surveys'),
-            'icon' => 'ri-test-tube-line',
-            'link' => $this->createUrl("surveyAdministration/createTestSurveys"),
-            'htmlOptions' => [
-                'class' => 'btn btn-warning tab-dependent-button',
-                'data-tab' => '#surveys',
-            ],
-        ]
-    );
-}
-*/
-
 if (Permission::model()->hasGlobalPermission('surveysgroups', 'create')) {
     $this->widget(
         'ext.ButtonWidget.ButtonWidget',
