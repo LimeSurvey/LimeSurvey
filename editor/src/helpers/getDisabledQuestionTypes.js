5827e16631dbc2989ab07acaa422d4e6d481ca52
twilligls <tim.willig@limesurvey.org>
2026-05-26 14:47:33 +0200

    Make equation question type available in new editor

--- editor/src/helpers/getDisabledQuestionTypes.js
+++ editor/src/helpers/getDisabledQuestionTypes.js
@@ -2,7 +2,6 @@ import { getQuestionTypeInfo } from '../components'
 
 export const getDisabledQuestionTypes = () => {
   return [
-    getQuestionTypeInfo().EQUATION.theme,
     getQuestionTypeInfo().RANKING.theme,
   ]
 }
