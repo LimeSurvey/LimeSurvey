866cdd5a39c1bd18f53217e4322e983d7d17d1f1
Carsten Schmitz <carsten.schmitz@limesurvey.org>
2026-06-02 14:17:09 +0200

    Fixed issue: [security] Updated twig template engine and updated deprecated twig attribute calls (#5021)

--- vendor/twig/twig/src/TokenParser/SandboxTokenParser.php
+++ vendor/twig/twig/src/TokenParser/SandboxTokenParser.php
@@ -22,7 +22,7 @@ use Twig\Token;
  * Marks a section of a template as untrusted code that must be evaluated in the sandbox mode.
  *
  *    {% sandbox %}
- *        {% include 'user.html' %}
+ *        {% include 'user.html.twig' %}
  *    {% endsandbox %}
  *
  * @see https://twig.symfony.com/doc/api.html#sandbox-extension for details
@@ -34,12 +34,16 @@ final class SandboxTokenParser extends AbstractTokenParser
     public function parse(Token $token): Node
     {
         $stream = $this->parser->getStream();
-        $stream->expect(/* Token::BLOCK_END_TYPE */ 3);
+        trigger_deprecation('twig/twig', '3.15', \sprintf('The "sandbox" tag is deprecated in "%s" at line %d.', $stream->getSourceContext()->getName(), $token->getLine()));
+
+        $stream->expect(Token::BLOCK_END_TYPE);
         $body = $this->parser->subparse([$this, 'decideBlockEnd'], true);
-        $stream->expect(/* Token::BLOCK_END_TYPE */ 3);
+        $stream->expect(Token::BLOCK_END_TYPE);
 
         // in a sandbox tag, only include tags are allowed
-        if (!$body instanceof IncludeNode) {
+        if ($body instanceof IncludeNode) {
+            $body->setAttribute('sandboxed', true);
+        } else {
             foreach ($body as $node) {
                 if ($node instanceof TextNode && ctype_space($node->getAttribute('data'))) {
                     continue;
@@ -48,10 +52,12 @@ final class SandboxTokenParser extends AbstractTokenParser
                 if (!$node instanceof IncludeNode) {
                     throw new SyntaxError('Only "include" tags are allowed within a "sandbox" section.', $node->getTemplateLine(), $stream->getSourceContext());
                 }
+
+                $node->setAttribute('sandboxed', true);
             }
         }
 
-        return new SandboxNode($body, $token->getLine(), $this->getTag());
+        return new SandboxNode($body, $token->getLine());
     }
 
     public function decideBlockEnd(Token $token): bool
