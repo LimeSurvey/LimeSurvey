e5fb74bd23855519dd2766aee32cfcd47274aa68
Anblik Audit Team <anblikauditteam@gmail.com>
2026-06-02 11:48:16 +0530

    fix Label is not provided for the checkboxes LS-2026-179
    fixes:
    1.fix Label is not provided for the checkboxes LS-2026-179
    2.Grouping is not announced for the checkbox

--- application/views/admin/surveymenu_entries/massive_action/_update.php
+++ application/views/admin/surveymenu_entries/massive_action/_update.php
@@ -12,94 +12,116 @@
 
 <form class="custom-modal-datas form form-horizontal">
     <div class="container-fluid">
+        <fieldset class="mb-0" aria-labelledby="massedit-entry-modify-legend">
+            <legend id="massedit-entry-modify-legend" class="visually-hidden"><?php eT("Modify"); ?></legend>
         <div class="ex-form-group mb-3">
             <div class="col-md-1">
-                <label class="" >
-                <?php eT("Modify"); ?>
-                </label>
+                <span id="massedit-entry-modify-group-label" class="form-label"><?php eT("Modify"); ?></span>
             </div>
             <div class="col-md-11"></div>
         </div>
         <div class="ex-form-group mb-3">
             <div class="col-md-1">
-                <label class="" >
-                    <input type="checkbox" class="action_check_to_keep_old_value"></input>
-                </label>
+                <input
+                    type="checkbox"
+                    id="massedit_entry_menu_id_modify"
+                    class="action_check_to_keep_old_value"
+                    aria-labelledby="massedit-entry-modify-legend massedit_entry_menu_id_fieldlabel"
+                />
             </div>
-            <label class="col-md-3 form-label"  for='menu_id'><?php eT("Menu ID:"); ?></label>
+            <label id="massedit_entry_menu_id_fieldlabel" class="col-md-3 form-label" for="menu_id"><?php eT("Menu ID:"); ?></label>
             <div class="col-md-8">
-                <?php echo TbHtml::dropDownList('menu_id', 'lskeep', (['lskeep' => gT('Keep old value')] + $model->getMenuIdOptions()), ['disabled' => 'disabled','class' => 'form-select custom-data selector_submitField']);?>
+                <?php echo TbHtml::dropDownList('menu_id', 'lskeep', (['lskeep' => gT('Keep old value')] + $model->getMenuIdOptions()), ['id' => 'menu_id', 'disabled' => 'disabled', 'class' => 'form-select custom-data selector_submitField', 'aria-label' => gT('Menu ID:')]);?>
             </div>
         </div>
 
         <div class="ex-form-group mb-3">
             <div class="col-md-1">
-                <label class="" >
-                    <input type="checkbox" class="action_check_to_keep_old_value"></input>
-                </label>
+                <input
+                    type="checkbox"
+                    id="massedit_entry_menu_class_modify"
+                    class="action_check_to_keep_old_value"
+                    aria-labelledby="massedit-entry-modify-legend massedit_entry_menu_class_fieldlabel"
+                />
             </div>
-            <label class="col-md-3 form-label"  for='menu_class'><?php eT("Menu class:"); ?></label>
+            <label id="massedit_entry_menu_class_fieldlabel" class="col-md-3 form-label" for="menu_class"><?php eT("Menu class:"); ?></label>
             <div class="col-md-8">
-                <?php echo TbHtml::textField('menu_class', 'lskeep', array('size' => 60,'maxlength' => 255,'disabled' => 'disabled','class' => 'custom-data selector_submitField'));?>
+                <?php echo TbHtml::textField('menu_class', 'lskeep', array('size' => 60,'maxlength' => 255,'id' => 'menu_class', 'disabled' => 'disabled','class' => 'custom-data selector_submitField', 'aria-label' => gT('Menu class:')));?>
             </div>
         </div>
 
 
         <div class="ex-form-group mb-3">
             <div class="col-md-1">
-                <label class="" >
-                    <input type="checkbox" class="action_check_to_keep_old_value"></input>
-                </label>
+                <input
+                    type="checkbox"
+                    id="massedit_entry_permission_modify"
+                    class="action_check_to_keep_old_value"
+                    aria-labelledby="massedit-entry-modify-legend massedit_entry_permission_fieldlabel"
+                />
             </div>
-            <label class="col-md-3 form-label"  for='permission'><?php eT("Permission:"); ?></label>
+            <label id="massedit_entry_permission_fieldlabel" class="col-md-3 form-label" for="permission"><?php eT("Permission:"); ?></label>
             <div class="col-md-8">
-                <?php echo TbHtml::textField('permission', 'lskeep', array('size' => 60,'maxlength' => 255,'disabled' => 'disabled','class' => 'custom-data selector_submitField'));?>
+                <?php echo TbHtml::textField('permission', 'lskeep', array('size' => 60,'maxlength' => 255,'id' => 'permission', 'disabled' => 'disabled','class' => 'custom-data selector_submitField', 'aria-label' => gT('Permission:')));?>
             </div>
         </div>
 
         <div class="ex-form-group mb-3">
             <div class="col-md-1">
-                <label class="" >
-                    <input type="checkbox" class="action_check_to_keep_old_value"></input>
-                </label>
+                <input
+                    type="checkbox"
+                    id="massedit_entry_permission_grade_modify"
+                    class="action_check_to_keep_old_value"
+                    aria-labelledby="massedit-entry-modify-legend massedit_entry_permission_grade_fieldlabel"
+                />
             </div>
-            <label class="col-md-3 form-label"  for='permission_grade'><?php eT("Permission level:"); ?></label>
+            <label id="massedit_entry_permission_grade_fieldlabel" class="col-md-3 form-label" for="permission_grade"><?php eT("Permission level:"); ?></label>
             <div class="col-md-8">
-                <?php echo TbHtml::textField('permission_grade', 'lskeep', array('size' => 60,'maxlength' => 255,'disabled' => 'disabled','class' => 'custom-data selector_submitField'));?>
+                <?php echo TbHtml::textField('permission_grade', 'lskeep', array('size' => 60,'maxlength' => 255,'id' => 'permission_grade', 'disabled' => 'disabled','class' => 'custom-data selector_submitField', 'aria-label' => gT('Permission level:')));?>
             </div>
         </div>
+        </fieldset>
 
         <div class="row ls-space margin bottom-10">
-            <button class="btn btn-warning float-end" type="button" data-bs-toggle="collapse" data-bs-target="#collapseAdvancedOptionsMassEdit">
+            <button class="btn btn-warning float-end" type="button" data-bs-toggle="collapse" data-bs-target="#collapseAdvancedOptionsMassEdit" aria-expanded="false" aria-controls="collapseAdvancedOptionsMassEdit">
                 <?php eT('Toggle advanced options') ?>
             </button>
         </div>
         <!-- Start collapsed advanced options -->
         <div class="collapse" id="collapseAdvancedOptionsMassEdit">
+            <fieldset class="mb-0" aria-labelledby="massedit-entry-advanced-legend">
+                <legend id="massedit-entry-advanced-legend" class="visually-hidden"><?php eT('Advanced options'); ?></legend>
 
             <div class="ex-form-group mb-3">
                 <div class="col-md-1">
-                    <label class="" >
-                        <input type="checkbox" class="action_check_to_keep_old_value"></input>
-                    </label>
+                    <input
+                        type="checkbox"
+                        id="massedit_entry_user_id_modify"
+                        class="action_check_to_keep_old_value"
+                        aria-labelledby="massedit-entry-advanced-legend massedit_entry_user_id_fieldlabel"
+                    />
                 </div>
-                <label class="col-md-3 form-label"  for='permission_grade'><?php eT("User:"); ?></label>
+                <label id="massedit_entry_user_id_fieldlabel" class="col-md-3 form-label" for="user_id"><?php eT("User:"); ?></label>
                 <div class="col-md-8">
-                    <?php echo TbHtml::dropDownList('user_id', 'lskeep', array_merge(['lskeep' => gT('Keep old value')], $model->getUserIdOptions()), ['disabled' => 'disabled','class' => 'form-select custom-data selector_submitField']);?>
+                    <?php echo TbHtml::dropDownList('user_id', 'lskeep', array_merge(['lskeep' => gT('Keep old value')], $model->getUserIdOptions()), ['id' => 'user_id', 'disabled' => 'disabled', 'class' => 'form-select custom-data selector_submitField', 'aria-label' => gT('User:')]);?>
                 </div>
             </div>
             
             <div class="ex-form-group mb-3">
                 <div class="col-md-1">
-                    <label class="" >
-                        <input type="checkbox" class="action_check_to_keep_old_value"></input>
-                    </label>
+                    <input
+                        type="checkbox"
+                        id="massedit_entry_language_modify"
+                        class="action_check_to_keep_old_value"
+                        aria-labelledby="massedit-entry-advanced-legend massedit_entry_language_fieldlabel"
+                    />
                 </div>
-                <label class="col-md-3 form-label"  for='language'><?php eT("Language:"); ?></label>
+                <label id="massedit_entry_language_fieldlabel" class="col-md-3 form-label" for="language"><?php eT("Language:"); ?></label>
                 <div class="col-md-8">
-                    <?php echo TbHtml::textField('language', 'lskeep', array('size' => 60,'maxlength' => 255,'disabled' => 'disabled', 'class' => 'custom-data selector_submitField'));?>
+                    <?php echo TbHtml::textField('language', 'lskeep', array('size' => 60,'maxlength' => 255,'id' => 'language', 'disabled' => 'disabled', 'class' => 'custom-data selector_submitField', 'aria-label' => gT('Language:')));?>
                 </div>
             </div>
+            </fieldset>
         
         </div>
 
