61b6dc0679d8f5e1a06d4fee6ece71ae4a89399e
Anblik Audit Team <anblikauditteam@gmail.com>
2026-06-04 18:36:23 +0530

    Fixed issue: A11y Improved accessibility semantics for multiple modals (#4875)

--- application/views/admin/super/footer.php
+++ application/views/admin/super/footer.php
@@ -89,12 +89,12 @@ $questionEditor = $questionEditor ?? false;
 
 <!-- Modal for system information -->
 
-<div id="modalSystemInformation" class="modal fade" role="dialog">
+<div id="modalSystemInformation" class="modal fade" role="dialog" aria-labelledby="modalSystemInformationTitle" aria-modal="true">
     <div class="modal-dialog">
         <!-- Modal content-->
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title"><?php eT("System information"); ?></h5>
+                <h5 class="modal-title" id="modalSystemInformationTitle"><?php eT("System information"); ?></h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
             </div>
             <div class="modal-body">
@@ -135,12 +135,12 @@ $questionEditor = $questionEditor ?? false;
 ?>
 
 <?php /** this one works with assets/packages/adminbasics/src/parts/confirmationModal.js */ ?>
-<div id="confirmation-modal" class="modal fade" role="dialog">
+<div id="confirmation-modal" class="modal fade" role="dialog" aria-labelledby="confirmationModalTitle" aria-modal="true">
     <div class="modal-dialog">
         <!-- Modal content-->
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title"><?php eT("Confirm"); ?></h5>
+                <h5 class="modal-title" id="confirmationModalTitle"><?php eT("Confirm"); ?></h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
             </div>
             <div class="modal-body">
@@ -173,12 +173,12 @@ $questionEditor = $questionEditor ?? false;
 </div>
 
 <!-- Modal for errors -->
-<div id="error-modal" class="modal fade" role="dialog">
+<div id="error-modal" class="modal fade" role="dialog" aria-labelledby="errorModalTitle" aria-modal="true">
     <div class="modal-dialog">
         <!-- Modal content-->
         <div class="modal-content">
             <div class="modal-header card-header">
-                <h5 class="modal-title"><?php eT("Error"); ?></h5>
+                <h5 class="modal-title" id="errorModalTitle"><?php eT("Error"); ?></h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
             </div>
             <div class="modal-body">
@@ -192,12 +192,12 @@ $questionEditor = $questionEditor ?? false;
 </div>
 
 <!-- Modal for success -->
-<div id="success-modal" class="modal fade" role="dialog">
+<div id="success-modal" class="modal fade" role="dialog" aria-labelledby="successModalTitle" aria-modal="true">
     <div class="modal-dialog">
         <!-- Modal content-->
         <div class="modal-content">
             <div class="modal-header card-header">
-                <h5 class="modal-title"><?php eT("Success"); ?></h5>
+                <h5 class="modal-title" id="successModalTitle"><?php eT("Success"); ?></h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
             </div>
             <div class="modal-body">
