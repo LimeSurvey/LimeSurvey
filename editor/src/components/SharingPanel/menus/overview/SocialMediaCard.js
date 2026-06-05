f84adca0020b0805bcd184f6a7dba96def776756
haythem <haythem.chibani@gmail.com>
2026-06-05 14:41:55 +0100

    Dev: prettier

--- editor/src/components/SharingPanel/menus/overview/SocialMediaCard.js
+++ editor/src/components/SharingPanel/menus/overview/SocialMediaCard.js
@@ -10,7 +10,9 @@ export const SocialMediaCard = ({ link, title }) => {
     <div className="col-md-3 d-flex">
       <Card className="card d-flex flex-column justify-content-between  d-flex flex-column h-100 w-100">
         <SocialMediaShare shareUrl={link} surveyTitle={title} />
-        <PluginSlot slotName={PLUGIN_SLOTS.SHARING_OVERVIEW_SOCIAL_MEDIA_CARD_BOTTOM} />
+        <PluginSlot
+          slotName={PLUGIN_SLOTS.SHARING_OVERVIEW_SOCIAL_MEDIA_CARD_BOTTOM}
+        />
       </Card>
     </div>
   )
--- editor/src/plugins/slots.js
+++ editor/src/plugins/slots.js
@@ -4,7 +4,8 @@ export const PLUGIN_SLOTS = {
   SHARING_PANEL_EXTRA_MENU: 'sharingpanel:extra:menu',
   SURVEY_SETTINGS_BLOCK_TOKENS_BOTTOM: 'surveysettingsblock:tokens:bottom',
   SHARING_OVERVIEW_CARD_BOTTOM: 'sharingoverview:card:bottom',
-  SHARING_OVERVIEW_SOCIAL_MEDIA_CARD_BOTTOM: 'sharingoverview:socialmediacard:bottom',
+  SHARING_OVERVIEW_SOCIAL_MEDIA_CARD_BOTTOM:
+    'sharingoverview:socialmediacard:bottom',
   TRANSLATIONS_PANEL: 'translations:panel:content',
   FEEDBACK_FORM_OPEN: 'feedback:form:open',
   CONTENT_EDITOR: 'content:editor:extra', // extra logic for editable content ( contentEditable & tinymce)
