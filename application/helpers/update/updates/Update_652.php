aad47b1aecdcb2aa3df7865808670166a2e207a0
Denis Chenu <denis@sondages.pro>
2026-06-08 11:20:54 +0200

    Dev: drop existing columns when update to 652

--- application/helpers/update/updates/Update_652.php
+++ application/helpers/update/updates/Update_652.php
@@ -13,8 +13,14 @@ class Update_652 extends DatabaseUpdateBase
      */
     public function up()
     {
+
+        /* Drop already created columns */
+        dropColumn('crypt_method', '{{surveys}}');
+        dropColumn('crypt_method', '{{surveys_groupsettings}}');
+        /* Create with good values */
         addColumn('{{surveys}}', 'crypt_method', "string(1) DEFAULT 'I'");
         addColumn('{{surveys_groupsettings}}', 'crypt_method', "string(1) DEFAULT 'I'");
+        /* Set global one to B (basic), didn't update any response table */
         $this->db->createCommand()->update("{{surveys_groupsettings}}", ["crypt_method" => "B"], "gsid = 0");
     }
 }
