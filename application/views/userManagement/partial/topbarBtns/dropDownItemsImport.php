7275c4e5b1d5c96da4b2547cad11a50032bc62c7
twilligls <tim.willig@limesurvey.org>
2026-06-09 11:13:58 +0200

    removed unnecessary aria-labelledby from dropDownItemsImport.php and code formatting

--- application/views/userManagement/partial/topbarBtns/dropDownItemsImport.php
+++ application/views/userManagement/partial/topbarBtns/dropDownItemsImport.php
@@ -1,22 +1,28 @@
 <?php
 
 ?>
-<ul class="dropdown-menu" aria-labelledby="example2">
+<ul class="dropdown-menu">
     <li>
         <a
-        role="button"
-            data-href="<?= $this->createUrl("userManagement/renderUserImport", ["importFormat" => "csv"]) ?>"
-            class="dropdown-item UserManagement--action--openmodal"
-            data-bs-toggle="modal"
-            href="#"> <?php eT("Import (CSV)") ?>
+                role="button"
+                data-href="<?= $this->createUrl(
+                    "userManagement/renderUserImport",
+                    ["importFormat" => "csv"]
+                ) ?>"
+                class="dropdown-item UserManagement--action--openmodal"
+                data-bs-toggle="modal"
+                href="#"> <?php eT("Import (CSV)") ?>
         </a>
     </li>
     <li>
-        <a       role="button"
-            data-href="<?= App()->createUrl("userManagement/renderUserImport", ["importFormat" => "json"]) ?>"
-            data-bs-toggle="modal"
-            class="dropdown-item UserManagement--action--openmodal"
-            href="#">
+        <a role="button"
+           data-href="<?= App()->createUrl(
+               "userManagement/renderUserImport",
+               ["importFormat" => "json"]
+           ) ?>"
+           data-bs-toggle="modal"
+           class="dropdown-item UserManagement--action--openmodal"
+           href="#">
             <?php eT("Import (JSON)"); ?>
         </a>
     </li>
