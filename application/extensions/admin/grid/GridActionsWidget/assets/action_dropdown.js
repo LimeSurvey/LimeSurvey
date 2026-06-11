92aa8ba70b9db90a2ec1a9ed59dcd360f6803fa4
twilligls <tim.willig@limesurvey.org>
2026-06-11 09:27:02 +0200

    remove dead code in action_dropdown.js

--- application/extensions/admin/grid/GridActionsWidget/assets/action_dropdown.js
+++ application/extensions/admin/grid/GridActionsWidget/assets/action_dropdown.js
@@ -6,9 +6,6 @@ LS.actionDropdown = {
     },
     create: function () {
         'use strict';
-        document.querySelectorAll('.ls-action_dropdown').forEach(function (wrapper) {
-            wrapper.removeAttribute('tabindex');
-        });
         this.removeOrphanedDropdowns();
         this.calculateSubmenuPosition();
         let dropdownElementList = [].slice.call(
@@ -198,7 +195,6 @@ LS.actionDropdown = {
                     return;
                 }
                 wrapper.setAttribute('data-submenu-positioning-initialized', 'true');
-                wrapper.removeAttribute('tabindex');
 
                 // When the main dropdown is shown, configure submenus
                 wrapper.addEventListener('shown.bs.dropdown', function() {
