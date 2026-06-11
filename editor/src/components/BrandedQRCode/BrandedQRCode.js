cbcd86761c4bb0586f83acb8d50a50e76e18aef9
Ahmed azeem <ahmedazeem9889@gmail.com>
2026-06-11 15:32:21 +0300

    CT-1768-branded-qr-code: fixed code style issues

--- editor/src/components/BrandedQRCode/BrandedQRCode.js
+++ editor/src/components/BrandedQRCode/BrandedQRCode.js
@@ -16,65 +16,67 @@ export const BrandedQRCode = ({
   removeQrCodeBehindLogo = true,
   qrStyle = 'squares',
   eyeRadius = [
-      { outer: [10, 10, 10, 10], inner: [0, 0, 0, 0] },
-      { outer: [10, 10, 10, 10], inner: [0, 0, 0, 0] },
-      { outer: [10, 10, 10, 10], inner: [0, 0, 0, 0] },
+    { outer: [10, 10, 10, 10], inner: [0, 0, 0, 0] },
+    { outer: [10, 10, 10, 10], inner: [0, 0, 0, 0] },
+    { outer: [10, 10, 10, 10], inner: [0, 0, 0, 0] },
   ],
   eyeColor = {
-      outer: '#000000',
-      inner: '#25c267',
+    outer: '#000000',
+    inner: '#25c267',
   },
   fgColor = '#000000',
   bgColor = '#ffffff',
   showDownloadButton = true,
 }) => {
-    const qrWrapperRef = useRef(null)
-    const qrDownloadRef = useRef(null)
+  const qrWrapperRef = useRef(null)
+  const qrDownloadRef = useRef(null)
 
-    const handleDownload = () => {
-        if (qrDownloadRef.current) {
-            qrDownloadRef.current.download('png', 'QRCode.png')
-        } else {
-            errorToast(t('Failed to download QR code'), 'center')
-        }
+  const handleDownload = () => {
+    if (qrDownloadRef.current) {
+      qrDownloadRef.current.download('png', 'QRCode.png')
+    } else {
+      errorToast(t('Failed to download QR code'), 'center')
     }
+  }
 
-    const sharedProps = {
-        value,
-        logoImage: lsIcon,
-        quietZone,
-        ecLevel,
-        logoOpacity,
-        removeQrCodeBehindLogo,
-        qrStyle,
-        eyeRadius,
-        eyeColor,
-        fgColor,
-        bgColor,
-        logoPaddingStyle,
-    }
+  const sharedProps = {
+    value,
+    logoImage: lsIcon,
+    ecLevel,
+    logoOpacity,
+    removeQrCodeBehindLogo,
+    qrStyle,
+    eyeRadius,
+    eyeColor,
+    fgColor,
+    bgColor,
+    logoPaddingStyle,
+  }
 
-    return (
-        <>
-            <div className="text-center mb-4" ref={qrWrapperRef}>
-                <QRCode size={size} {...sharedProps} />
-            </div>
+  return (
+    <>
+      <div className="text-center mb-4" ref={qrWrapperRef}>
+        <QRCode size={size} quietZone={quietZone} {...sharedProps} />
+      </div>
 
-            <div
-                style={{ position: 'absolute', left: '-9999px', top: '-9999px' }}
-            >
-                <QRCode ref={qrDownloadRef} size={downloadSize} quietZone={quietZone * (downloadSize / size)} {...sharedProps} />
-            </div>
+      <div style={{ position: 'absolute', left: '-9999px', top: '-9999px' }}>
+        <QRCode
+          ref={qrDownloadRef}
+          size={downloadSize}
+          quietZone={quietZone * (downloadSize / size)}
+          {...sharedProps}
+        />
+      </div>
 
-            {showDownloadButton && (
-                <div
-                    className="med14-c text-primary cursor-pointer"
-                    onClick={handleDownload}
-                >
-                    <img className="me-2" src={downloadArrowIcon} alt="Download QR" />
-                    {t('Download QR code')}
-                </div>
-            )}
-        </>
-    )
+      {showDownloadButton && (
+        <div
+          className="med14-c text-primary cursor-pointer"
+          onClick={handleDownload}
+        >
+          <img className="me-2" src={downloadArrowIcon} alt="Download QR" />
+          {t('Download QR code')}
+        </div>
+      )}
+    </>
+  )
 }
