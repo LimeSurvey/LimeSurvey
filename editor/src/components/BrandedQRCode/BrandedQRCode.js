e056273fff4c843ea421599f7b7151a347d9acc6
Ahmed azeem <ahmedazeem9889@gmail.com>
2026-06-12 16:37:50 +0300

    CT-1768: fixed code style

--- editor/src/components/BrandedQRCode/BrandedQRCode.js
+++ editor/src/components/BrandedQRCode/BrandedQRCode.js
@@ -20,82 +20,82 @@ export const BrandedQRCode = ({
   removeQrCodeBehindLogo = true,
   qrStyle = 'fluid',
   eyeRadius = [
-      { outer: [10, 10, 10, 10], inner: [0, 0, 0, 0] },
-      { outer: [10, 10, 10, 10], inner: [0, 0, 0, 0] },
-      { outer: [10, 10, 10, 10], inner: [0, 0, 0, 0] },
+    { outer: [10, 10, 10, 10], inner: [0, 0, 0, 0] },
+    { outer: [10, 10, 10, 10], inner: [0, 0, 0, 0] },
+    { outer: [10, 10, 10, 10], inner: [0, 0, 0, 0] },
   ],
   eyeColor = {
-      outer: '#000000',
-      inner: '#25c267',
+    outer: '#000000',
+    inner: '#25c267',
   },
   fgColor = '#000000',
   bgColor = '#ffffff',
   showDownloadButton = true,
 }) => {
-    const qrDownloadRef = useRef(null)
+  const qrDownloadRef = useRef(null)
 
-    const handleDownload = () => {
-        const canvas = qrDownloadRef.current?.querySelector('canvas')
-        if (!canvas) {
-            errorToast(t('Failed to download QR code'), 'center')
-            return
-        }
-        downloadCanvasAsImage(canvas, 'QRCode.png')
+  const handleDownload = () => {
+    const canvas = qrDownloadRef.current?.querySelector('canvas')
+    if (!canvas) {
+      errorToast(t('Failed to download QR code'), 'center')
+      return
     }
+    downloadCanvasAsImage(canvas, 'QRCode.png')
+  }
 
-    const previewRenderSize = previewSize * previewPixelRatio
-    const downloadSize = downloadTotal - 2 * downloadQuietZone
+  const previewRenderSize = previewSize * previewPixelRatio
+  const downloadSize = downloadTotal - 2 * downloadQuietZone
 
-    const sharedProps = {
-        value,
-        logoImage: lsIcon,
-        ecLevel,
-        logoOpacity,
-        logoPadding,
-        logoPaddingStyle,
-        removeQrCodeBehindLogo,
-        qrStyle,
-        eyeRadius,
-        eyeColor,
-        fgColor,
-        bgColor,
-    }
+  const sharedProps = {
+    value,
+    logoImage: lsIcon,
+    ecLevel,
+    logoOpacity,
+    logoPadding,
+    logoPaddingStyle,
+    removeQrCodeBehindLogo,
+    qrStyle,
+    eyeRadius,
+    eyeColor,
+    fgColor,
+    bgColor,
+  }
 
-    return (
-        <>
-            <div className="text-center mb-4">
-                <QRCode
-                    size={previewRenderSize}
-                    quietZone={previewQuietZone * previewPixelRatio}
-                    logoWidth={previewRenderSize * logoFraction}
-                    logoHeight={previewRenderSize * logoFraction}
-                    style={{ width: previewSize, height: previewSize }}
-                    {...sharedProps}
-                />
-            </div>
+  return (
+    <>
+      <div className="text-center mb-4">
+        <QRCode
+          size={previewRenderSize}
+          quietZone={previewQuietZone * previewPixelRatio}
+          logoWidth={previewRenderSize * logoFraction}
+          logoHeight={previewRenderSize * logoFraction}
+          style={{ width: previewSize, height: previewSize }}
+          {...sharedProps}
+        />
+      </div>
 
-            <div
-                ref={qrDownloadRef}
-                style={{ position: 'absolute', left: '-9999px', top: '-9999px' }}
-            >
-                <QRCode
-                    size={downloadSize}
-                    quietZone={downloadQuietZone}
-                    logoWidth={downloadSize * logoFraction}
-                    logoHeight={downloadSize * logoFraction}
-                    {...sharedProps}
-                />
-            </div>
+      <div
+        ref={qrDownloadRef}
+        style={{ position: 'absolute', left: '-9999px', top: '-9999px' }}
+      >
+        <QRCode
+          size={downloadSize}
+          quietZone={downloadQuietZone}
+          logoWidth={downloadSize * logoFraction}
+          logoHeight={downloadSize * logoFraction}
+          {...sharedProps}
+        />
+      </div>
 
-            {showDownloadButton && (
-                <div
-                    className="med14-c text-primary cursor-pointer"
-                    onClick={handleDownload}
-                >
-                    <img className="me-2" src={downloadArrowIcon} alt="Download QR" />
-                    {t('Download QR code')}
-                </div>
-            )}
-        </>
-    )
+      {showDownloadButton && (
+        <div
+          className="med14-c text-primary cursor-pointer"
+          onClick={handleDownload}
+        >
+          <img className="me-2" src={downloadArrowIcon} alt="Download QR" />
+          {t('Download QR code')}
+        </div>
+      )}
+    </>
+  )
 }
