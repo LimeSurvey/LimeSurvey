0994704fd506a3714de3172e71f4683e028e0c78
ptelu <patrick.teichmann@limesurvey.org>
2026-06-22 12:57:15 +0200

    Release 6.17.10+260622

--- application/config/version.php
+++ application/config/version.php
@@ -11,10 +11,10 @@
  * See COPYRIGHT.php for copyright notices and details.
  */
 
-$config['versionnumber'] = '6.17.9';
+$config['versionnumber'] = '6.17.10';
 $config['dbversionnumber'] = 651;
 $config['buildnumber'] = '';
 $config['updatable'] = true;
 $config['templateapiversion']  = 3;
-$config['assetsversionnumber'] = '30490';
+$config['assetsversionnumber'] = '30491';
 return $config;
