35308f68dba421cfae3d49d766c8a852c50c6b1c
haythem <haythem.chibani@gmail.com>
2026-06-30 12:48:30 +0100

    Dev: eslint

--- editor/src/components/SurveySettings/SurveySetting.js
+++ editor/src/components/SurveySettings/SurveySetting.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react'
 import { Form } from 'react-bootstrap'
-import classNames from 'classnames'
 import { format } from 'util'
 
 export const SurveySetting = (props) => {
