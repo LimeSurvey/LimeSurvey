cd4011eb05620eeed39bb3dab99ceae687078b80
ramishenouda <ramishenouda@outlook.com>
2026-07-01 11:54:41 +0200

    removed useRichTextEditor

--- editor/src/components/UIComponents/ContentEditor/ContentEditor.test.js
+++ editor/src/components/UIComponents/ContentEditor/ContentEditor.test.js
@@ -7,6 +7,17 @@ import userEvent from '@testing-library/user-event'
 import { renderWithProviders } from 'tests/testUtils'
 import { ContentEditor } from './ContentEditor'
 
+jest.mock('./TinyMCE/TinyMCE', () => {
+  const React = require('react')
+
+  return {
+    TinyMCE: ({ testId = 'content-editor' }) =>
+      React.createElement('div', {
+        'data-testid': `${testId}-tinymce`,
+      }),
+  }
+})
+
 describe('ContentEditor', () => {
   test('Should have the value "random text 123"', async () => {
     await renderWithProviders(
@@ -18,5 +29,17 @@ describe('ContentEditor', () => {
     await userEvent.type(contentEditor, 'random text 123')
 
     expect(contentEditor.innerHTML).toBe('random text 123')
+  })
+
+  test('Should render TinyMCE when toolbar is enabled', async () => {
+    await renderWithProviders(
+      <ContentEditor
+        testId="content-editor"
+        placeholder="Enter your comment"
+        showToolbar={true}
+      />
+    )
+
+    expect(screen.getByTestId('content-editor-tinymce')).toBeInTheDocument()
   })
 })
