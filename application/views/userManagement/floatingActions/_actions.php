4a962ec5f03341bdd05dd2dd873b79211ceffda8
pstellling <patricia.stelling@limesurvey.org>
2026-07-27 12:13:18 +0200

    AT-2167: changes for permission checks on ui side.

--- application/views/userManagement/floatingActions/_actions.php
+++ application/views/userManagement/floatingActions/_actions.php
@@ -8,9 +8,15 @@
  * @var UserManagementController $this
  */
 
-$aActions = [
+$canUsersUpdate = Permission::model()->hasGlobalPermission('users', 'update');
+$canUsersDelete = Permission::model()->hasGlobalPermission('users', 'delete');
+$canSuperadminCreate = Permission::model()->hasGlobalPermission('superadmin', 'create');
+
+$aActions = [];
+
+if ($canUsersUpdate) {
     // ResendLoginData (main button)
-    [
+    $aActions[] = [
         'type'          => 'action',
         'action'        => 'resendlogindata',
         'url'           => App()->createUrl('userManagement/batchSendAndResetLoginData'),
@@ -24,9 +30,10 @@ $aActions = [
         'selectedUrl'   => App()->createUrl('userManagement/renderSelectedItems/'),
         'sModalTitle'   => gT('Resend login data'),
         'htmlModalBody' => gT('Are you sure you want to reset and resend selected users login data?'),
-    ],
+    ];
+
     // Edit Permissions (main button)
-    [
+    $aActions[] = [
         'type'              => 'action',
         'action'            => 'batchPermissions',
         'url'               => App()->createUrl('userManagement/batchPermissions'),
@@ -41,11 +48,11 @@ $aActions = [
         'selectedUrl'       => App()->createUrl('userManagement/renderSelectedItems/'),
         'sModalTitle'       => gT('Edit permissions'),
         'htmlModalBody'     => App()->getController()->renderPartial('/userManagement/massiveAction/_updatepermissions', [], true)
-    ],
-];
+    ];
+}
 
 // Add to user group (main button, for authorized users)
-if (Permission::model()->hasGlobalPermission('users', 'update')) {
+if ($canUsersUpdate) {
     $aActions[] = [
         'type'              => 'action',
         'action'            => 'batchaddtogroup',
@@ -66,44 +73,46 @@ if (Permission::model()->hasGlobalPermission('users', 'update')) {
 // More actions dropdown
 $aMoreActionsItems = [];
 
-// Edit Status (in dropdown)
-$aMoreActionsItems[] = [
-    'type'              => 'action',
-    'id'                => 'edit-status',
-    'action'            => 'batchStatus',
-    'url'               => App()->createUrl('userManagement/batchStatus'),
-    'text'              => gT('Edit status'),
-    'grid-reload'       => 'yes',
-    'actionType'        => 'modal',
-    'modalType'         => 'cancel-apply',
-    'largeModalView'    => true,
-    'keepopen'          => 'yes',
-    'showSelected'      => 'yes',
-    'selectedUrl'       => App()->createUrl('userManagement/renderSelectedItems/'),
-    'sModalTitle'       => gT('Edit status'),
-    'htmlModalBody'     => App()->getController()->renderPartial('/userManagement/massiveAction/_updatestatus', [], true)
-];
+if ($canUsersUpdate) {
+    // Edit Status (in dropdown)
+    $aMoreActionsItems[] = [
+        'type'              => 'action',
+        'id'                => 'edit-status',
+        'action'            => 'batchStatus',
+        'url'               => App()->createUrl('userManagement/batchStatus'),
+        'text'              => gT('Edit status'),
+        'grid-reload'       => 'yes',
+        'actionType'        => 'modal',
+        'modalType'         => 'cancel-apply',
+        'largeModalView'    => true,
+        'keepopen'          => 'yes',
+        'showSelected'      => 'yes',
+        'selectedUrl'       => App()->createUrl('userManagement/renderSelectedItems/'),
+        'sModalTitle'       => gT('Edit status'),
+        'htmlModalBody'     => App()->getController()->renderPartial('/userManagement/massiveAction/_updatestatus', [], true)
+    ];
 
-// Set Expiration Date (in dropdown)
-$aMoreActionsItems[] = [
-    'type'              => 'action',
-    'id'                => 'edit-expires',
-    'action'            => 'batchExpires',
-    'url'               => App()->createUrl('userManagement/batchExpires'),
-    'text'              => gT('Set expiration date'),
-    'grid-reload'       => 'yes',
-    'actionType'        => 'modal',
-    'modalType'         => 'cancel-apply',
-    'largeModalView'    => true,
-    'keepopen'          => 'yes',
-    'showSelected'      => 'yes',
-    'selectedUrl'       => App()->createUrl('userManagement/renderSelectedItems/'),
-    'sModalTitle'       => gT('Set expiration date'),
-    'htmlModalBody'     => App()->getController()->renderPartial('/userManagement/massiveAction/_updateexpires', [], true)
-];
+    // Set Expiration Date (in dropdown)
+    $aMoreActionsItems[] = [
+        'type'              => 'action',
+        'id'                => 'edit-expires',
+        'action'            => 'batchExpires',
+        'url'               => App()->createUrl('userManagement/batchExpires'),
+        'text'              => gT('Set expiration date'),
+        'grid-reload'       => 'yes',
+        'actionType'        => 'modal',
+        'modalType'         => 'cancel-apply',
+        'largeModalView'    => true,
+        'keepopen'          => 'yes',
+        'showSelected'      => 'yes',
+        'selectedUrl'       => App()->createUrl('userManagement/renderSelectedItems/'),
+        'sModalTitle'       => gT('Set expiration date'),
+        'htmlModalBody'     => App()->getController()->renderPartial('/userManagement/massiveAction/_updateexpires', [], true)
+    ];
+}
 
 // Add role (in dropdown, for superadmins only)
-if (Permission::model()->hasGlobalPermission('superadmin', 'read')) {
+if ($canSuperadminCreate) {
     $aMoreActionsItems[] = [
         'type'              => 'action',
         'action'            => 'batchaddrole',
@@ -131,21 +140,23 @@ if (!empty($aMoreActionsItems)) {
 }
 
 // Delete (main button, last)
-$aActions[] = [
-    'type'          => 'action',
-    'action'        => 'delete',
-    'url'           => App()->createUrl('userManagement/deleteMultiple'),
-    'iconClasses'   => 'ri-delete-bin-fill text-danger',
-    'text'          => gT('Delete'),
-    'grid-reload'   => 'yes',
-    'actionType'    => 'modal',
-    'modalType'     => 'cancel-delete',
-    'keepopen'      => 'yes',
-    'showSelected'  => 'yes',
-    'selectedUrl'   => App()->createUrl('userManagement/renderSelectedItems/'),
-    'sModalTitle'   => gT('Delete user'),
-    'htmlModalBody' => gT('Are you sure you want to delete the selected user?'),
-];
+if ($canUsersDelete) {
+    $aActions[] = [
+        'type'          => 'action',
+        'action'        => 'delete',
+        'url'           => App()->createUrl('userManagement/deleteMultiple'),
+        'iconClasses'   => 'ri-delete-bin-fill text-danger',
+        'text'          => gT('Delete'),
+        'grid-reload'   => 'yes',
+        'actionType'    => 'modal',
+        'modalType'     => 'cancel-delete',
+        'keepopen'      => 'yes',
+        'showSelected'  => 'yes',
+        'selectedUrl'   => App()->createUrl('userManagement/renderSelectedItems/'),
+        'sModalTitle'   => gT('Delete user'),
+        'htmlModalBody' => gT('Are you sure you want to delete the selected user?'),
+    ];
+}
 
 return $aActions;
 
