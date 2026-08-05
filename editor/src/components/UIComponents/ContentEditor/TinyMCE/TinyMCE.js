ccdbe8717ddb090a423334ba8e230410335b575d
ramishenouda <ramishenouda@outlook.com>
2026-08-05 12:42:23 +0200

    style fixes for question title code editor

--- editor/src/components/UIComponents/ContentEditor/TinyMCE/TinyMCE.js
+++ editor/src/components/UIComponents/ContentEditor/TinyMCE/TinyMCE.js
@@ -51,13 +51,20 @@ export const TinyMCE = ({
     })
 
     htmlPopup({
-      title: t('Edit HTML'),
-      html: <CodeEditor value={formattedHTML} />,
+      title: t('Edit question using HTML'),
+      html: (
+        <CodeEditor
+          title={t('Use custom HTML to edit and format your question text.')}
+          className="html-editor"
+          value={formattedHTML}
+        />
+      ),
       showCloseButton: true,
       showCancelButton: true,
       showConfirmButton: true,
       confirmButtonText: 'Save',
       cancelButtonText: 'Cancel',
+      containerClass: 'html-editor-popup',
       width: '80vw',
     }).then((result) => {
       if (result.isConfirmed) {
