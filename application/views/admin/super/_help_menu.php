1b731c591895bbb3f174efb84c347a44b5ce483c
twilligls <tim.willig@limesurvey.org>
2026-08-18 16:08:55 +0200

    Improve _help_menu.php for screenreaders

--- application/views/admin/super/_help_menu.php
+++ application/views/admin/super/_help_menu.php
@@ -6,37 +6,41 @@
     </a>
     <ul class="dropdown-menu larger-dropdown" aria-labelledby="helpDropdown">
         <?php $this->renderPartial("/admin/super/_tutorial_menu", []); ?>
-        <li class="dropdown-divider"></li>
+        <li class="dropdown-divider" role="separator" aria-hidden="true"></li>
         <li>
-            <a href="http://manual.limesurvey.org/" target="_blank" class="dropdown-item">
+            <a href="http://manual.limesurvey.org/" target="_blank" rel="noopener noreferrer" class="dropdown-item">
                 <!-- <i class="ri-question-fill"></i> -->
                 <?php eT('LimeSurvey Manual');?>
-                <i aria-label="Open in a new tab" class=" ri-external-link-fill  float-end"></i>
+                <span class="visually-hidden">(<?php eT('Link opens in a new tab'); ?>)</span>
+                <i aria-hidden="true" class="ri-external-link-fill float-end"></i>
             </a>
         </li>
         <li>
-            <a href="https://forums.limesurvey.org" target="_blank" class="dropdown-item">
-                <span class="fa-stack halfed">
-                    <span class="ri-chat-3-fill fa-stack-1x" ></span>
-                    <span class="ri-group-fill fa-inverse fa-stack-1x halfed" ></span>
+            <a href="https://forums.limesurvey.org" target="_blank" rel="noopener noreferrer" class="dropdown-item">
+                <span aria-hidden="true" class="fa-stack halfed">
+                    <span class="ri-chat-3-fill fa-stack-1x"></span>
+                    <span class="ri-group-fill fa-inverse fa-stack-1x halfed"></span>
                 </span>
                 <?php eT('LimeSurvey Forums');?>
-                <i  aria-label="Open in a new tab" class=" ri-external-link-fill  float-end"></i>
+                <span class="visually-hidden">(<?php eT('Link opens in a new tab'); ?>)</span>
+                <i aria-hidden="true" class="ri-external-link-fill float-end"></i>
             </a>
         </li>
-        <li class="dropdown-divider"></li>
+        <li class="dropdown-divider" role="separator" aria-hidden="true"></li>
         <li>
-            <a href="https://bugs.limesurvey.org/" target="_blank" class="dropdown-item">
-                <span class="ri-bug-fill" ></span>
+            <a href="https://bugs.limesurvey.org/" target="_blank" rel="noopener noreferrer" class="dropdown-item">
+                <span aria-hidden="true" class="ri-bug-fill"></span>
                 <?php eT('Report bugs');?>
-                <i  aria-label="Open in a new tab" class=" ri-external-link-fill  float-end"></i>
+                <span class="visually-hidden">(<?php eT('Link opens in a new tab'); ?>)</span>
+                <i aria-hidden="true" class="ri-external-link-fill float-end"></i>
             </a>
         </li>
         <li>
-            <a href="https://limesurvey.org/" target="_blank" class="dropdown-item">
-                <span class="ri-star-fill" ></span>
+            <a href="https://limesurvey.org/" target="_blank" rel="noopener noreferrer" class="dropdown-item">
+                <span aria-hidden="true" class="ri-star-fill"></span>
                 <?php eT('LimeSurvey Homepage');?>
-                <i   aria-label="Open in a new tab"class=" ri-external-link-fill  float-end"></i>
+                <span class="visually-hidden">(<?php eT('Link opens in a new tab'); ?>)</span>
+                <i aria-hidden="true" class="ri-external-link-fill float-end"></i>
             </a>
         </li>
     </ul>
