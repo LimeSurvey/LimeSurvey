89070caa454d167738ab04b17b3b24e5943dbf2b
Carsten Schmitz <carsten.schmitz@limesurvey.org>
2026-08-24 18:49:55 +0200

    Fix

--- application/helpers/admin/import_helper.php
+++ application/helpers/admin/import_helper.php
@@ -638,7 +638,8 @@ function XMLImportGroup($sFullFilePath, $iNewSID, $bTranslateLinksFields, $suppo
     }
 
     // Update question code references in custom conditions and relevance expressions
-    replaceExpressionCodes($iNewSID, $aQuestionCodeReplacements);
+    // Restrict to the imported group so expressions/conditions in other groups of the target survey are left untouched.
+    replaceExpressionCodes($iNewSID, $aQuestionCodeReplacements, array($newgid));
     replaceExpressionFieldnames($newgid, $aQIDReplacements);
 
     LimeExpressionManager::RevertUpgradeConditionsToRelevance($iNewSID);
@@ -3551,10 +3552,8 @@ function XMLImportSurvey($sFullFilePath, $sXMLdata = null, $sNewSurveyName = nul
     $results['FieldReMap'] = $aOldNewFieldmap;
     LimeExpressionManager::SetSurveyId($iNewSID);
     translateInsertansTags($iNewSID, $iOldSID, $aOldNewFieldmap);
-    // Restrict the replacement to the newly imported group(s) so expressions/conditions in other groups are left untouched.
-    $aImportedGids = array_values($aGIDReplacements);
-    replaceExpressionCodes($iNewSID, $aQuestionCodeReplacements, $aImportedGids);
-    replaceExpressionCodes($iNewSID, $aQuestionsMapping, $aImportedGids); // replace question codes in format "38612X105X3011"
+    replaceExpressionCodes($iNewSID, $aQuestionCodeReplacements);
+    replaceExpressionCodes($iNewSID, $aQuestionsMapping); // replace question codes in format "38612X105X3011"
     if (count($aQuestionCodeReplacements)) {
         array_unshift($results['importwarnings'], "<span class='warningtitle'>" . gT('Attention: Several question codes were updated. Please check these carefully as the update  may not be perfect with customized expressions.') . '</span>');
     }
