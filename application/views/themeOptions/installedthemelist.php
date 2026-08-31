6f968f91a6428d9227b0fceeac3152bfbdaa2ab5
pstellling <patricia.stelling@limesurvey.org>
2026-08-31 13:00:03 +0200

    AT-2140: moved js code from view file into js file. Removed unnecessary stuff as much as possible.

--- application/views/themeOptions/installedthemelist.php
+++ application/views/themeOptions/installedthemelist.php
@@ -97,42 +97,12 @@ if (!empty($aFloatingActions)) {
 ?>
 
 <?php
-// todo create a new javascript file and call function from here, related: 1573120573738
-$script = '
-                jQuery(document).on("change", "#questionthemes-pageSize", function () {
-                    $.fn.yiiGridView.update("questionthemes-grid", {
-                        data: {
-                            pageSize: $(this).val()
-                        }
-                    });
-                });
-                // Use event delegation for toggle question themes so it works across pagination
-                jQuery(document).on("change", ".toggle_question_theme", function () {
-                    let $url = $(this).attr("data-url");
-                    let data = new FormData();
-                    let xhttp = new XMLHttpRequest();
-                    data.append(LS.data.csrfTokenName, LS.data.csrfToken);
-                    xhttp.open("POST", $url, true);
-                    xhttp.send(data);
-                });
-                // Ensure floating actions bar is updated whenever checkboxes change  
-                jQuery(document).on("change", "#questionthemes-grid tbody .massiveActionsCheckbox", function () {
-                    if (window.LS && LS.floatingActions && typeof LS.floatingActions.updateBar === "function") {
-                        setTimeout(function() {
-                            LS.floatingActions.updateBar("questionthemes-grid", "questionId");
-                        }, 50);
-                    }
-                });
-                // Hook into yiiGridView after updates
-                jQuery("#questionthemes-grid").on("yiiGridView:afterUpdate", function() {
-                    if (window.LS && LS.floatingActions && typeof LS.floatingActions.updateBar === "function") {
-                        setTimeout(function() {
-                            LS.floatingActions.updateBar("questionthemes-grid", "questionId");
-                        }, 100);
-                    }
-                });
-                ';
-App()->getClientScript()->registerScript('questionthemes-grid', $script, LSYii_ClientScript::POS_POSTSCRIPT);
+App()->getClientScript()->registerScriptFile(
+    Yii::app()->getAssetManager()->publish('assets/scripts/admin/installedThemesList.js'
+    )
+);
+
+
 
 
 
