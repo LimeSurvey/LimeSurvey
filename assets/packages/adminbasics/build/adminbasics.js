ed41fe4303a4d7acab24b152c981913ae58569d9
Adil Moumni Abdou <49393758+adilmoumni@users.noreply.github.com>
2026-09-08 11:05:06 +0200

    Fixed issue #JD-127: Fix prefix validation guidance and loading state (#5272)

--- assets/packages/adminbasics/build/adminbasics.js
+++ assets/packages/adminbasics/build/adminbasics.js
@@ -19959,6 +19959,12 @@
 	            const formid = '#' + $(this).attr('data-form-id'),
 	              $form = $(formid),
 	              $firstSubmit = $form.find('[type="submit"]').first();
+
+	            // prevents the form from being submitted if not valide
+	            if (!$form[0].reportValidity()) {
+	              $('#save-form-button').removeClass('disabled');
+	              return false;
+	            }
 	            if ($firstSubmit.length > 0) {
 	              $firstSubmit.trigger('click');
 	            } else {
@@ -20024,6 +20030,12 @@
 	            ev.preventDefault();
 	            const formid = '#' + $(this).attr('data-form-id'),
 	              $form = $(formid);
+
+	            // check validity before being submitted.
+	            if (!$form[0].reportValidity()) {
+	              $('#save-and-close-form-button').removeClass('disabled');
+	              return false;
+	            }
 
 	            // Add input to tell us to not redirect
 	            // TODO : change that
