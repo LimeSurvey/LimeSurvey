07ffb2f62987690472d998c6c7d4c8bc56e36a1b
Carsten Schmitz <carsten.schmitz@limesurvey.org>
2026-09-11 15:49:02 +0200

    Dev translation fixes

--- editor/src/pages/Responses/utils/getDefaultColumns.js
+++ editor/src/pages/Responses/utils/getDefaultColumns.js
@@ -77,7 +77,7 @@ export const getDefaultColumns = ({
             },
           ]}
           meatballClassName="responses-meatball-menu"
-          actionsTitle={t('Response Actions')}
+          actionsTitle={t('Response actions')}
           placement="left"
         />
       ),
