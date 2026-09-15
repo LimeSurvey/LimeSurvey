5b8e2fb81bbd5e535af4bde82894f4825d2733d1
Carsten Schmitz <carsten.schmitz@limesurvey.org>
2026-09-15 22:33:35 +0200

    Fixed issue #20174: No visual information if  survey is expired

--- application/views/surveyAdministration/partial/topbar/previewOrRunButton_view.php
+++ application/views/surveyAdministration/partial/topbar/previewOrRunButton_view.php
@@ -8,6 +8,9 @@
 //todo: this view comes from old TobarWidget and should be use in new topbar
 
 $notActive = $survey->active == 'N';
+// An active survey past its expiry date can no longer be run/previewed via this button
+// (see SurveyIndex::index(), which blocks it for active surveys regardless of newtest=Y).
+$isExpired = !$notActive && $survey->getIsDateExpired();
 if (!isset($id)) {
     $id = $notActive ? 'ls-preview-button' : 'ls-run-button';
 }
@@ -17,7 +20,7 @@ if (!isset($name)) {
 
 $languagesDropDownItems = '';
 
-if (count($surveyLanguages) > 1) {
+if (!$isExpired && count($surveyLanguages) > 1) {
     $languagesDropDownItems = '<ul class="dropdown-menu" style="min-width : 252px;">';
     $languagesDropDownItems .= $this->renderPartial(
         '/surveyAdministration/partial/topbar/languagesDropdownItems',
@@ -30,23 +33,41 @@ if (count($surveyLanguages) > 1) {
     );
     $languagesDropDownItems .= '</ul>';
 }
+
+if ($isExpired) {
+    $expiredTooltip = sprintf(
+        gT('This survey expired on %s and can no longer be run.'),
+        convertToGlobalSettingFormat(dateShift($survey->expires, "Y-m-d H:i:s"))
+    );
+}
 ?>
 
+<?php if ($isExpired) : ?>
+<span class="btntooltip" style="display: inline-block" data-bs-toggle="tooltip" data-bs-placement="bottom"
+      title="<?php echo CHtml::encode($expiredTooltip); ?>">
+<?php endif; ?>
 <?php
 $this->widget('ext.ButtonWidget.ButtonWidget', [
     'name' => $name,
     'id' => $id,
-    'text' => $notActive ? gT('Preview survey') : gT('Run survey'),
-    'icon' => $notActive ? 'ri-eye-fill' : 'ri-play-fill',
-    'isDropDown' => count($surveyLanguages) > 1,
+    'text' => $isExpired ? gT('Survey expired') : ($notActive ? gT('Preview survey') : gT('Run survey')),
+    'icon' => $isExpired ? 'ri-skip-forward-fill' : ($notActive ? 'ri-eye-fill' : 'ri-play-fill'),
+    'isDropDown' => !$isExpired && count($surveyLanguages) > 1,
     'dropDownContent' => $languagesDropDownItems,
     'link' => Yii::App()->createUrl(
         "survey/index",
         array('sid' => $survey->sid, 'newtest' => "Y", 'lang' => $survey->language)
     ),
-    'htmlOptions' => [
+    'htmlOptions' => $isExpired ? [
+        'class' => 'btn btn-secondary',
+        'role' => 'button',
+        'disabled' => 'disabled',
+    ] : [
         'class' => 'btn btn-secondary btntooltip',
         'role' => 'button',
         'target' => '_blank',
     ],
 ]); ?>
+<?php if ($isExpired) : ?>
+</span>
+<?php endif; ?>
