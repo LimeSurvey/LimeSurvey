bc3525dd8b509f922f3ca24d473549838dd4bfd1
Adil Moumni Abdou <moumniabdou.adil@gmail.com>
2026-09-18 16:15:36 +0200

    Dev: Move quota name mapping into response transformer

--- application/libraries/Api/Command/V1/Transformer/Output/TransformerOutputSurveyResponses.php
+++ application/libraries/Api/Command/V1/Transformer/Output/TransformerOutputSurveyResponses.php
@@ -76,8 +76,11 @@ class TransformerOutputSurveyResponses extends TransformerOutputActiveRecord
         $surveyResponseArray = parent::transform($surveyResponse) ?? [];
         $this->applyTokenData($surveyResponseArray, $surveyResponse, $options);
         $quotaId = $surveyResponseArray['quotaExit'] ?? null;
-        $surveyResponseArray['quotaExitName'] = $options['quotaNames'][(int) $quotaId]
-            ?? null;
+        $surveyResponseArray['quotaExitName'] = \CHtml::listData(
+            $options['survey']->quotas,
+            'id',
+            'name'
+        )[(int) $quotaId] ?? null;
         $surveyResponseArray['completed'] = !empty($surveyResponseArray['submitDate']);
         $surveyResponseArray['answers'] = $this->extractAnswers($surveyResponse->attributes);
         $this->normalizeDateFields($surveyResponseArray);
