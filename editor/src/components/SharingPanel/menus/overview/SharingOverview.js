47e33538d5d464f87ddcdc1988b2986dcea2c99a
haythemch2 <haythem.chibani@gmail.com>
2026-09-21 12:53:40 +0100

    Dev: preserve sharing overview plugins slots for compatibility & rebuild editor

--- editor/src/components/SharingPanel/menus/overview/SharingOverview.js
+++ editor/src/components/SharingPanel/menus/overview/SharingOverview.js
@@ -113,6 +113,8 @@ export const SharingOverview = () => {
           link={link}
           title={survey.languageSettings[selectedLanguage]?.title || ''}
         />
+        <PluginSlot slotName={PLUGIN_SLOTS.SHARING_OVERVIEW_BOTTOM_LEFT} />
+        <PluginSlot slotName={PLUGIN_SLOTS.SHARING_OVERVIEW_BOTTOM_RIGHT} />
         <PluginSlot slotName={PLUGIN_SLOTS.SHARING_OVERVIEW_CARD_BOTTOM} />
         <ParticipantsListCard
           hasUpdatePermission={hasUpdatePermission}
--- editor/src/plugins/slots.js
+++ editor/src/plugins/slots.js
@@ -5,6 +5,8 @@ export const PLUGIN_SLOTS = {
   EDITOR_LAYOUT_EXTRA: 'editor:layout:extra',
   SHARING_PANEL_EXTRA_MENU: 'sharingpanel:extra:menu',
   SURVEY_SETTINGS_BLOCK_TOKENS_BOTTOM: 'surveysettingsblock:tokens:bottom',
+  SHARING_OVERVIEW_BOTTOM_LEFT: 'sharingoverview:bottom:left',
+  SHARING_OVERVIEW_BOTTOM_RIGHT: 'sharingoverview:bottom:right',
   SHARING_OVERVIEW_CARD_BOTTOM: 'sharingoverview:card:bottom',
   SHARING_OVERVIEW_SOCIAL_MEDIA_CARD_BOTTOM:
     'sharingoverview:socialmediacard:bottom',
