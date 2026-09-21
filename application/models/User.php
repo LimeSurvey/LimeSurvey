6f4a6f3f404163be3e35f1ac82e186e146a49f06
Carsten Schmitz <carsten.schmitz@limesurvey.org>
2026-09-21 10:46:57 +0200

    More fixes

--- application/models/User.php
+++ application/models/User.php
@@ -356,9 +356,12 @@ class User extends LSActiveRecord
      * Set user password with hash
      *
      * Also rotates the user's session token, so any other already
-     * authenticated session for this user (which still caches the previous
-     * token) gets logged out on its next request. See
-     * LSApplicationTrait::getCurrentUserId().
+     * authenticated web session for this user (which still caches the
+     * previous token) gets logged out on its next request. See
+     * LSApplicationTrait::getCurrentUserId(). Additionally revokes any
+     * outstanding RemoteControl (JSON-RPC/REST) session tokens for this
+     * user, since those are a separate auth mechanism unaffected by the
+     * session_token rotation above.
      *
      * @param string $sPassword The clear text password
      * @return \User
@@ -367,6 +370,9 @@ class User extends LSActiveRecord
     {
         $this->password = password_hash($sPassword, PASSWORD_DEFAULT);
         $this->session_token = self::generateSessionToken();
+        if (!empty($this->users_name)) {
+            Session::model()->deleteAllByAttributes(['data' => $this->users_name]);
+        }
         if ($save) {
             $this->save();
         }
