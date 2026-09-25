d9a29c087201097d182e422f64c570e635ef0283
Denis Chenu <denis@sondages.pro>
2026-09-25 18:16:02 +0200

    Fixed issue #16572: LSActiveRecord::encryptSave() doesn't run validations by default, while the standard 'save' method does (#3596)

--- application/models/ParticipantAttribute.php
+++ application/models/ParticipantAttribute.php
@@ -102,4 +102,25 @@ class ParticipantAttribute extends LSActiveRecord
     {
         return 0;
     }
+
+    /**
+     * @inheritdoc
+     * But without filter on attributes (not same than activerecords)
+     * @param string $action
+     * @return void
+     */
+    public function decryptEncryptAttributes($action = 'decrypt')
+    {
+        // load sodium library
+        $sodium = Yii::app()->sodium;
+        /* @var [] the attribute_id need encrypt and decrypt */
+        $aParticipantAttributes = CHtml::listData(
+            ParticipantAttributeName::model()->findAll(["select" => "attribute_id", "condition" => "encrypted = 'Y' and core_attribute <> 'Y'"]),
+            'attribute_id',
+            'attribute_id'
+        );
+        if (array_key_exists($this->attribute_id, $aParticipantAttributes)) {
+            $this->value = $sodium->$action($this->value);
+        }
+    }
 }
